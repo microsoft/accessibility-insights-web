9f68afa2475d6b529d23af7ce95a8959d28c68f7
Dan Bjorge <danielbj@microsoft.com>
2019-01-07 17:37:31 -0800

    Add failed attempt to invoke extension via keyboard shortcuts to demo.test.ts

--- src/TestScripts/e2e-tests/demo.test.ts
+++ src/TestScripts/e2e-tests/demo.test.ts
@@ -1,16 +1,23 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as puppeteer from 'puppeteer';
 
 describe('puppeteer demo', () => {
-  let page;
-  beforeAll(async () => {
-    page = await browser.newPage();
+  it('should display "bing" text on bing.com', async () => {
     await page.goto('https://bing.com');
+    await expect(page).toMatch('bing');
   });
 
-  it('should display "bing" text on bing.com', async () => {
+  it ('should show popup.html on Ctrl+Shift+K', async () => {
+    await page.goto('https://example.com');
 
-    await expect(page).toMatch('bing');
+    // This doesn't work, page.keyboard goes directly to the page bypassing the browser-level shortcut
+    await page.keyboard.down('Control');
+    await page.keyboard.down('Shift');
+    await page.keyboard.press('KeyK');
+    await page.keyboard.up('Shift');
+    await page.keyboard.up('Control');
+
+    // This is temporarily hardcoded, we'll want to come up with some sort of ???/test-results/screenshots/ structure or something
+    await page.screenshot({path: 'Q:/temp/test-screenshot-2.png', fullPage: true, omitBackground: false});
   });
 });
