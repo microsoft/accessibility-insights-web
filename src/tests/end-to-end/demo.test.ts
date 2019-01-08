33930ce34341fc5459a6fd72d26a91fa08278cf5
Shobhit Agarwal <shobhit.agarwal@microsoft.com>
2019-01-08 15:51:16 -0800

    get some form of test working

--- src/tests/end-to-end/demo.test.ts
+++ src/tests/end-to-end/demo.test.ts
@@ -2,37 +2,21 @@
 // Licensed under the MIT License.
 
 describe('puppeteer demo', () => {
-  it('should display "bing" text on bing.com', async () => {
-    await page.goto('https://bing.com');
-    await expect(page).toMatch('bing');
-  });
+  let idWithURL: string;
 
-  it ('should show popup.html on Ctrl+Shift+K', async () => {
-    await page.goto('https://example.com');
-
-    // This doesn't work, page.keyboard goes directly to the page bypassing the browser-level shortcut
-    await page.keyboard.down('Control');
-    await page.keyboard.down('Shift');
-    await page.keyboard.press('KeyK');
-    await page.keyboard.up('Shift');
-    await page.keyboard.up('Control');
-
-    // This is temporarily hardcoded, we'll want to come up with some sort of ???/test-results/screenshots/ structure or something
-    await page.screenshot({path: 'Q:/temp/test-screenshot-2.png', fullPage: true, omitBackground: false});
-  });
-
-  it ('should show popup.html when navigated directly', async () => {
-    // Doing it like this would require setting up a stable extension ID
-    await page.goto('chrome-extension://dcjikolnijkefoijmkabeiljojkacmik/popup/popup.html');
-    await page.screenshot({path: 'Q:/temp/test-popup-screenshot-1.png', fullPage: true, omitBackground: false});
-  });
-
-  it ('should identify the popup.html url dynamically', async() => {
+  beforeAll(async () => {
     const allTargets = await browser.targets();
-    const backgroundPageTargets = allTargets.find(t => {
-      return t.type() === 'background_page';
+    const backgroundPageTarget = allTargets.find(t => {
+      return t.type() === 'background_page' && t.url().endsWith('background.html');
     });
 
-    // todo: execute code in background page to identify extension url
-  })
+    const url = await backgroundPageTarget.url();
+    idWithURL = url.match(/(.*)\/background\/background.html/)[1];
+  });
+
+  it ('should identify the popup.html url dynamically', async () => {
+    await page.goto(`${idWithURL}/popup/popup.html`);
+    await page.screenshot({path: 'C:/temp/test-popup-screenshot-1.png', fullPage: true, omitBackground: false});
+  });
+
 });
