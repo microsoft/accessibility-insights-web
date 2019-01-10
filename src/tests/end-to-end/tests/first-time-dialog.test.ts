7ea7de3b202c77af330e1e97eb36ba082be41962
Devaradhan Murugesan <demuruge@microsoft.com>
2019-01-09 21:33:39 -0800

    refactor first time dialog tests to be independent of other tests in the same file

--- src/tests/end-to-end/tests/first-time-dialog.test.ts
+++ src/tests/end-to-end/tests/first-time-dialog.test.ts
@@ -4,41 +4,50 @@ import { Page } from 'puppeteer';
 
 import { ExtensionPuppeteerConnection } from '../common/extension-puppeteer-connection';
 
-describe('Another test', () => {
+describe('FirstTimeDialogTest', () => {
     let extensionConnection: ExtensionPuppeteerConnection;
     let targetPage: Page;
     let targetPageTabId: number;
     let popupPage: Page;
 
-    beforeAll(async () => {
+    beforeEach(async () => {
         extensionConnection = await ExtensionPuppeteerConnection.connect();
 
-        targetPage = await extensionConnection.newPage('https://bing.com');
+        await setupNewTargetPage();
 
-        targetPageTabId = await extensionConnection.getTabId(targetPage);
-
-        // We re-use the default jest-puppeteer page because it automatically handles failing on console.error
         popupPage = await extensionConnection.newExtensionPopupPage(targetPageTabId);
 
         // This is important; without this, UI simulation (like click()) will time out.
         popupPage.bringToFront();
     });
 
-    afterAll(async () => {
+    afterEach(async () => {
         extensionConnection.tearDown();
     });
 
-    it('should open the telemetry prompt on first run', async () => {
+    it('verify first time dialog content', async () => {
         await popupPage.waitForSelector('.telemetry-permission-dialog-modal');
 
         const title = await popupPage.$eval('#telemetry-permission-title', element => element.textContent);
         expect(title).toBe('We need your help');
     });
 
-    it('should dismiss the telemetry prompt after hitting "OK"', async () => {
+    it('should not show telemetry dialog after dismissed', async () => {
         await popupPage.waitForSelector('.telemetry-permission-dialog-modal');
         await popupPage.click('button.start-using-product-button');
 
         await popupPage.waitFor(() => !document.querySelector('.telemetry-permission-dialog-modal'));
+
+        // verify telemetry dialog doesn't show up in new popup
+        setupNewTargetPage();
+        popupPage = await extensionConnection.newExtensionPopupPage(targetPageTabId);
+        popupPage.waitForSelector('#new-launch-pad');
+        await popupPage.waitFor(() => !document.querySelector('.telemetry-permission-dialog-modal'));
     });
+
+    async function setupNewTargetPage() {
+        targetPage = await extensionConnection.newPage('https://bing.com');
+
+        targetPageTabId = await extensionConnection.getTabId(targetPage);
+    }
 });
