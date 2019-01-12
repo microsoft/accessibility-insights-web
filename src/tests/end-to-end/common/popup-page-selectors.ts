f4348d8a35441eed8f9ba780ee806a29c5583d53
Devaradhan Murugesan <demuruge@microsoft.com>
2019-01-11 19:03:34 -0800

    adding helper methods for page; moved popup selector strings to separate file; validating first time dialog by snapshot

---
+++ src/tests/end-to-end/common/popup-page-selectors.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+export let popupPageSelectors = {
+    telemetryDialog: '.telemetry-permission-dialog-modal',
+    startUsingProductButton: 'button.start-using-product-button',
+    launchPad: '#new-launch-pad',
+};
