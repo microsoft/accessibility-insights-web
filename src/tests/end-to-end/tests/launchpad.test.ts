32d88a16c7d0ae09068ef1df2b5d2289abc727ab
Shobhit Agarwal <shobhit.agarwal@microsoft.com>
2019-01-14 13:00:17 -0800

    use common functions to test this

--- src/tests/end-to-end/tests/launchpad.test.ts
+++ src/tests/end-to-end/tests/launchpad.test.ts
@@ -1,14 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { Page } from 'puppeteer';
-import { ExtensionPuppeteerConnection } from '../common/extension-puppeteer-connection';
-import { getTestResourceUrl } from '../common/test-resources';
 
-function generateFormattedHtml(innerHTMLString: string) {
-    const template = document.createElement('template');
-    template.innerHTML = innerHTMLString.trim();
-    return template.content.cloneNode(true);
-}
+import { ExtensionPuppeteerConnection } from '../common/extension-puppeteer-connection';
+import { getPrintableHtmlElement, waitForElementToDisappear } from '../common/page-utils';
+import { popupPageSelectors } from '../common/popup-page-selectors';
+import { getTestResourceUrl } from '../common/test-resources';
 
 describe('Adhoc Panel test', () => {
     let extensionConnection: ExtensionPuppeteerConnection;
@@ -22,7 +19,7 @@ describe('Adhoc Panel test', () => {
         await setupNewTargetPage();
         popupPage = await extensionConnection.newExtensionPopupPage(targetPageTabId);
         await popupPage.bringToFront();
-        await dismissTelemetryPopup();
+        await waitForElementToDisappear(popupPage, popupPageSelectors.telemetryDialog);
     });
 
     afterAll(async () => {
@@ -36,24 +33,11 @@ describe('Adhoc Panel test', () => {
         targetPageTabId = await extensionConnection.getActivePageTabId();
     }
 
-    async function dismissTelemetryPopup() {
-        await popupPage.waitForSelector('.telemetry-permission-dialog-modal');
-        await popupPage.click('button.start-using-product-button');
-        await popupPage.waitFor(() => !document.querySelector('.telemetry-permission-dialog-modal'));
-    }
-
-    it('test if launchpad title is there before pre', async () => {
-        await popupPage.waitForSelector('.launch-pad-title');
-        const launchpadTitle = await popupPage.$eval('.launch-pad-title', element => element.textContent);
-
-        expect(launchpadTitle).toBe('Launch pad');
-    });
-
     it('test snapshot for launchpad', async () => {
         await popupPage.waitForSelector('#new-launch-pad');
-        const launchPad = await popupPage.$eval('#new-launch-pad', el => el.innerHTML);
 
-        expect(generateFormattedHtml(launchPad)).toMatchSnapshot();
+        const element = await getPrintableHtmlElement(popupPage, '#new-launch-pad');
+        expect(element).toMatchSnapshot();
     });
 
     it('test if text for all the links in launchpad show properly', async () => {
