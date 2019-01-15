1541732a33a7696a575d65dea251e9a4faa8f1a1
Devaradhan Murugesan <demuruge@microsoft.com>
2019-01-14 21:21:06 -0800

    fixed flakiness when running without runinband

---
+++ src/tests/end-to-end/setup/test-setup.ts
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { DEFAULT_E2E_TEST_TIMEOUT_MS } from '../common/timeouts';
+
+jest.setTimeout(DEFAULT_E2E_TEST_TIMEOUT_MS);
