4c0d79d5464660b70ccdc08290bb7f561003727a
Shobhit Agarwal <shobhit.agarwal@microsoft.com>
2019-01-14 16:01:49 -0800

    try running e2e in parallel

--- src/tests/end-to-end/tests/popup/telemetry-permission-dialog.test.ts
+++ src/tests/end-to-end/tests/popup/telemetry-permission-dialog.test.ts
@@ -32,21 +32,27 @@ describe('telemetry-permission-dialog', () => {
     }
 
     it('should be dismissed by clicking the OK button', async () => {
-        const firstPopupPage = await newPopupPage();
-        await firstPopupPage.clickSelector(popupPageSelectors.startUsingProductButton);
-        await firstPopupPage.waitForSelectorToDisappear(popupPageSelectors.telemetryDialog);
-        await firstPopupPage.close();
+            const firstPopupPage = await newPopupPage();
+            await firstPopupPage.bringToFront();
 
-        const secondPopupPage = await newPopupPage();
-        await secondPopupPage.waitForSelector(popupPageSelectors.launchPad);
-        await secondPopupPage.waitForSelectorToDisappear(popupPageSelectors.telemetryDialog);
-    }, DEFAULT_E2E_TEST_TIMEOUT_MS);
+            await firstPopupPage.clickSelector(popupPageSelectors.startUsingProductButton);
+            await firstPopupPage.waitForSelectorToDisappear(popupPageSelectors.telemetryDialog);
+            await firstPopupPage.close();
+
+            const secondPopupPage = await newPopupPage();
+            await secondPopupPage.waitForSelector(popupPageSelectors.launchPad);
+            await secondPopupPage.waitForSelectorToDisappear(popupPageSelectors.telemetryDialog);
+        },
+        DEFAULT_E2E_TEST_TIMEOUT_MS,
+    );
 
     it('should have HTML content that matches the snapshot', async () => {
-        const popupPage = await newPopupPage();
-        await popupPage.waitForSelector(popupPageSelectors.telemetryDialog);
+            const popupPage = await newPopupPage();
+            await popupPage.waitForSelector(popupPageSelectors.telemetryDialog);
 
-        const element = await popupPage.getPrintableHtmlElement(popupPageSelectors.telemetryDialog);
-        expect(element).toMatchSnapshot();
-    }, DEFAULT_E2E_TEST_TIMEOUT_MS);
+            const element = await popupPage.getPrintableHtmlElement(popupPageSelectors.telemetryDialog);
+            expect(element).toMatchSnapshot();
+        },
+        DEFAULT_E2E_TEST_TIMEOUT_MS,
+    );
 });
