4d67be7cd9f2dda1d77ec0f09b9875211f386a3e
Shobhit Agarwal <shobhit.agarwal@microsoft.com>
2019-01-14 16:26:30 -0800

    increase timeout

--- src/tests/end-to-end/common/timeouts.ts
+++ src/tests/end-to-end/common/timeouts.ts
@@ -9,7 +9,7 @@
 export const DEFAULT_E2E_TEST_TIMEOUT_MS = 60000;
 
 // How long to wait for an existing page to stop churning in response to a UI action
-export const DEFAULT_PAGE_ELEMENT_WAIT_TIMEOUT_MS = 500;
+export const DEFAULT_PAGE_ELEMENT_WAIT_TIMEOUT_MS = 5000;
 
 // How long to wait for a new page to load in response to the UI action that launched it
 export const DEFAULT_NEW_PAGE_WAIT_TIMEOUT_MS = 2000;
