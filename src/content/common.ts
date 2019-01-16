52fccb810c52da2ddd09f7995529f4252887caea
karanbirsingh <karansin@microsoft.com>
2019-01-16 13:54:06 -0800

    revert changes unrelated to script-link, will separate guidance fix into a different branch

--- src/content/common.ts
+++ src/content/common.ts
@@ -4,16 +4,6 @@ import * as react from 'react';
 
 import { ContentCreator } from '../views/content/content-page';
 import { link } from './link';
-import { Helmet } from 'react-helmet';
-import { productName } from './strings/application';
-
-export function guidanceTitle(name: string) : JSX.Element {
-    return (
-        <Helmet>
-            <title>Guidance for {name} - {productName}</title>
-        </Helmet>
-    );
-}
 
 export const create = ContentCreator(link);
 export const React = react;
--- src/content/test/automated-checks/guidance.tsx
+++ src/content/test/automated-checks/guidance.tsx
@@ -1,13 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { React, create, guidanceTitle} from '../../common';
-import { Helmet } from "react-helmet";
-import { title } from '../../strings/application';
+import { React, create} from '../../common';
+import { productName } from '../../strings/application';
 
-const name = 'Automated checks';
 export const guidance = create(({ Markup }) => <>
-    {guidanceTitle(name)}
-    <h1>{name}</h1>
+    <h1>Automated checks</h1>
     <p>
         The automated checks in {productName} can identify more than 40 types of critical accessibility issues in seconds.
     </p>
