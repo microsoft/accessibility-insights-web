1b82ba8ef40cd7dd2e1266954d143af817f9d432
Dan Bjorge <danielbj@microsoft.com>
2019-01-16 10:49:18 -0800

    Fix floating promises in url-validator.test

--- src/tests/unit/tests/common/url-validator.test.ts
+++ src/tests/unit/tests/common/url-validator.test.ts
@@ -9,27 +9,21 @@ import { UrlValidator } from '../../../../common/url-validator';
 describe('UrlValidatorTest', () => {
     let testSubject: UrlValidator;
 
-    test('isSupportedUrl: http', async done => {
+    test('isSupportedUrl: http', async () => {
         const url: string = 'http://url';
         testSubject = new UrlValidator();
-        const promise = testSubject.isSupportedUrl(url, It.isAny());
-        promise.then(hasAccess => {
-            expect(hasAccess).toBe(true);
-            done();
-        });
+        const isSupported = await testSubject.isSupportedUrl(url, It.isAny());
+        expect(isSupported).toBe(true);
     });
 
-    test('isSupportedUrl: https', async done => {
+    test('isSupportedUrl: https', async () => {
         const url: string = 'https://url';
         testSubject = new UrlValidator();
-        const promise = testSubject.isSupportedUrl(url, It.isAny());
-        promise.then(hasAccess => {
-            expect(hasAccess).toBe(true);
-            done();
-        });
+        const isSupported = await testSubject.isSupportedUrl(url, It.isAny());
+        expect(isSupported).toBe(true);
     });
 
-    test('isSupportedUrl: file', async done => {
+    test('isSupportedUrl: file', async () => {
         const url: string = 'file://test';
         testSubject = new UrlValidator();
         const browserAdapterMock = Mock.ofType(ChromeAdapter);
@@ -41,11 +35,9 @@ describe('UrlValidatorTest', () => {
                 callback(true);
             })
             .verifiable();
-        const promise = testSubject.isSupportedUrl(url, browserAdapterMock.object);
-        promise.then(hasAccess => {
-            expect(hasAccess).toBe(true);
-            done();
-        });
+
+        const isSupported = await testSubject.isSupportedUrl(url, It.isAny());
+        expect(isSupported).toBe(true);
 
         browserAdapterMock.verifyAll();
     });
@@ -53,12 +45,8 @@ describe('UrlValidatorTest', () => {
     test('isNotSupportedUrl: chrome://', async done => {
         const url: string = 'chrome://are/you/ok?';
         testSubject = new UrlValidator();
-
-        const promise = testSubject.isSupportedUrl(url, It.isAny());
-        promise.then(hasAccess => {
-            expect(hasAccess).toBe(false);
-            done();
-        });
+        const isSupported = await testSubject.isSupportedUrl(url, It.isAny());
+        expect(isSupported).toBe(false);
     });
 
     test('isFileUrl, but have no access, so isNotSupportedUrl', async done => {
@@ -73,11 +61,9 @@ describe('UrlValidatorTest', () => {
                 callback(false);
             })
             .verifiable();
-        const promise = testSubject.isSupportedUrl(url, browserAdapterMock.object);
-        promise.then(hasAccess => {
-            expect(hasAccess).toBe(false);
-            done();
-        });
+
+        const isSupported = await testSubject.isSupportedUrl(url, It.isAny());
+        expect(isSupported).toBe(false);
 
         browserAdapterMock.verifyAll();
     });
