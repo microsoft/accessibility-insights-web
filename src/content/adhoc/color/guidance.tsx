57fa9515c9fbd561d2aae4fc9bd177da2c13330b
karanbirsingh <karansin@microsoft.com>
2019-01-17 14:58:21 -0800

    include adhoc and udpate test

--- src/content/adhoc/color/guidance.tsx
+++ src/content/adhoc/color/guidance.tsx
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { React, create } from '../../common';
+import { React, create, GuidanceTitle } from '../../common';
 
 export const guidance = create(({ Markup }) => <React.Fragment>
-    <h1>Color</h1>
+    <GuidanceTitle name={'Color'}/>
 
     <h2>Why color matters</h2>
 
--- src/tests/unit/tests/views/content/guidance-title.test.tsx
+++ src/tests/unit/tests/views/content/guidance-title.test.tsx
@@ -6,13 +6,9 @@ import { productName } from '../../../../../content/strings/application';
 import { GuidanceTitle } from '../../../../../views/content/guidance-title';
 
 describe('guidance title', () => {
-    test('has correct title', () => {
-        const wrapper = shallow(<GuidanceTitle name={'test'}/>);
-        expect(wrapper.find('title').text()).toEqual(`Guidance for test - ${productName}`);
-    });
-
     test('has correct structure', () => {
         const wrapper = shallow(<GuidanceTitle name={'test'}/>);
+        expect(wrapper.find('title').text()).toEqual(`Guidance for test - ${productName}`);
         expect(wrapper.getElement()).toMatchSnapshot();
     });
 });
