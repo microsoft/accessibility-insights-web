0785c2af026cd195d30669ddeaf905c842691f9f
Devaradhan Murugesan <demuruge@microsoft.com>
2019-01-18 16:16:43 -0800

    minor refactoring

--- src/common/get-inner-text-from-jsx-element.ts
+++ src/common/get-inner-text-from-jsx-element.ts
@@ -1,29 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 export function getInnerTextFromJsxElement(element: JSX.Element): string {
     return getInnerTextFromJsxElementRecursive(element).replace(/\s+/g, ' ').trim();
 }
 
 function getInnerTextFromJsxElementRecursive(element: JSX.Element): string {
-    let content = '';
-
-    if (typeof element === 'object') {
-        const childProps = element.props.children || [];
-
-        for (let pos = 0; pos < childProps.length; pos++) {
-            content += getInnerTextFromJsxElementRecursive(childProps[pos]);
-        }
-
-        return content;
-    }
-    else {
-        content = `${element}`;
+    if (typeof element !== 'object') {
+        return `${element}`;
     }
 
-    return content;
+    const childProps = element.props.children || [];
+    if (Array.isArray(childProps)) {
+        return childProps.map(child => getInnerTextFromJsxElementRecursive(child)).join('');
+    }
+
+    return `${childProps}`;
 }
-
-
-
-
