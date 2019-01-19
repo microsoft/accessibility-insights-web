316311651c8a29fa91f4661bd003dde07f9fd1d5
Mark Reay <mreay@microsoft.com>
2019-01-18 19:34:47 -0800

    Removed bookmarklets and replaced with download instructions (#93)
    * Removed bookmarklets and replaced with download instructions
    
    * Added mac keystroke
    
    * Require title to be string only
    
    * Renamed const to helmet
    
    * Switched to data driven test

--- src/content/test/parsing/index.ts
+++ src/content/test/parsing/index.ts
@@ -1,10 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { guidance } from './guidance';
-
+import { keyboardBookmarkletInstructions } from './keyboard-bookmarklet-instructions';
 import * as parsingContent from './parsing';
 
-export const parsing  = {
+export const parsing = {
     guidance,
     parsingContent,
+    keyboardBookmarkletInstructions,
 };
