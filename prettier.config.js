7b0f28071957e45946fafd5036dc5591875fb6d7
Devaradhan Murugesan <demuruge@microsoft.com>
2019-01-19 09:06:56 -0800

    adding prettier (not enabling yet) to format few files only: remove requirePragma from prettier config & run node_modules\.bin\prettier --config prettier.config.js --write --insert-pragma "<path>" & add back the requirePragma config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    printWidth: 140,
+    requirePragma: true,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    useTabs: false,
+    jsxBracketSameLine: false,
+};
