846365061d9773b0607a97b2d31a54795e38e912
Mark Reay <mreay@microsoft.com>
2019-01-18 18:50:48 -0800

    Fixed typos in content (#94)

--- src/content/test/audio-video-only/video-only-equivalent.tsx
+++ src/content/test/audio-video-only/video-only-equivalent.tsx
@@ -6,7 +6,7 @@ export const infoAndExamples = create(({ Markup, Link }) => <>
 
     <h1>Video-only equivalent</h1>
 
-    <p>Pre-recorded video-only content must be accompanied by an equivalent text or audio alternative. (<Link.WCAG_1_2_1 />)</p>
+    <p>Pre-recorded video-only content must be accompanied by an equivalent text or audio alternative.</p>
 
 
     <h2>Why it matters</h2>
@@ -35,7 +35,7 @@ export const infoAndExamples = create(({ Markup, Link }) => <>
         failText={
             <>
                 A web site presents a time-lapse video of a person preparing an entree.
-                As each ingredient is added, its name and amount are displayed.
+                As each ingredient is added, its name and amount are displayed as on-screen text.
             </>
         }
 
@@ -48,10 +48,16 @@ export const infoAndExamples = create(({ Markup, Link }) => <>
 
     />
 
-    <h4>For more examples, see the following articles:</h4>
+    <h2>More examples</h2>
+
+    <h3>WCAG success criteria</h3>
     <Markup.Links>
         <Markup.HyperLink href="https://www.w3.org/WAI/WCAG21/Understanding/audio-only-and-video-only-prerecorded.html">
             Understanding Success Criterion 1.2.1: Audio-only and Video-only (Prerecorded)</Markup.HyperLink>
+    </Markup.Links>
+
+    <h3>Sufficient techniques</h3>
+    <Markup.Links>
         <Markup.HyperLink href="https://www.w3.org/WAI/WCAG21/Techniques/general/G159">
             Providing an alternative for time-based media for video-only content</Markup.HyperLink>
         <Markup.HyperLink href="https://www.w3.org/WAI/WCAG21/Techniques/general/G166">
