047413f5f7f29fd0b895cb3efd43736782d62af1
Devaradhan Murugesan <demuruge@microsoft.com>
2019-01-22 13:23:29 -0800

    removing all the rules that have the default values

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 module.exports = {
-    bracketSpacing: true,
     endOfLine: 'lf',
     printWidth: 140,
     requirePragma: true,
-    semi: true,
     singleQuote: true,
     tabWidth: 4,
     trailingComma: 'all',
-    useTabs: false,
-    jsxBracketSameLine: false,
 };
