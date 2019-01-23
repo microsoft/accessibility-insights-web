e63ea3443c50192cbf120f1e760c477632499b29
Devaradhan Murugesan <demuruge@microsoft.com>
2019-01-22 16:37:39 -0800

    make test summary details a table for screen reader

--- src/DetailsView/reports/components/assessment-summary-details.tsx
+++ src/DetailsView/reports/components/assessment-summary-details.tsx
@@ -1,11 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { chunk } from 'lodash/index';
 import * as React from 'react';
 
 import { IAssessmentSummaryReportModel } from '../assessment-report-model';
-import { OutcomeIconSet } from './outcome-icon-set';
 import { OutcomeChipSet } from './outcome-chip-set';
+import { OutcomeIconSet } from './outcome-icon-set';
 
 export interface IAssessmentSummaryDetailsProps {
     testSummaries: IAssessmentSummaryReportModel[];
@@ -14,32 +13,26 @@ export interface IAssessmentSummaryDetailsProps {
 export class AssessmentSummaryDetails extends React.Component<IAssessmentSummaryDetailsProps> {
     public render(): JSX.Element {
         return (
-            <div className="assessment-summary-details">
-                {this.getTestDetailsColumns()}
+            <div role="table" className="assessment-summary-details">
+                <div role="rowgroup" className="assessment-summary-details-body">
+                    {this.getTestDetailsColumns()}
+                </div>
             </div>
         );
     }
 
     private getTestDetailsColumns(): JSX.Element[] {
-        const summaryBatches = chunk(this.props.testSummaries, Math.ceil(this.props.testSummaries.length / 2));
-
-        return summaryBatches.map((batch, index) => {
-            return (
-                <div className="assessment-summary-details-column" key={index}>
-                    {this.getTestDetailsList(batch, index)}
-                </div>
-            );
-        });
+        return this.getTestDetailsList(this.props.testSummaries, 1);
     }
 
     private getTestDetailsList(summaries: IAssessmentSummaryReportModel[], colIndex: number): JSX.Element[] {
         return summaries.map((testSummary, index) => {
             return (
-                <div key={`${colIndex}-${index}`} className="test-summary">
-                    <div className="test-summary-display-name">
+                <div role="row" key={`${colIndex}-${index}`} className="test-summary">
+                    <div role="cell" className="test-summary-display-name">
                         {testSummary.displayName}
                     </div>
-                    <div key={`${colIndex}-${index}-status`} className="test-summary-status">
+                    <div role="cell" key={`${colIndex}-${index}-status`} className="test-summary-status">
                         {
                             testSummary.pass + testSummary.incomplete + testSummary.fail > 7
                                 ? <OutcomeChipSet {...testSummary} />
