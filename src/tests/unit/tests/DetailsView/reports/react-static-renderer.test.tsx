b1fde3d151de971c85c4cf12e86e6087820d52d4
Shobhit Agarwal <shobhit.agarwal@microsoft.com>
2019-01-23 14:21:48 -0800

    use ReactDOM server rather than our own implementation

--- src/tests/unit/tests/DetailsView/reports/react-static-renderer.test.tsx
+++ src/tests/unit/tests/DetailsView/reports/react-static-renderer.test.tsx
@@ -14,7 +14,7 @@ describe('ReactStaticRendererTest', () => {
         );
 
         const testObject = new ReactStaticRenderer();
-        const actual = testObject.renderToStaticMarkup(element, 'div');
+        const actual = testObject.renderToStaticMarkup(element);
 
         const expected = '<div><h1>Header</h1><a href="https://url/">Link</a></div>';
         expect(actual).toEqual(expected);
