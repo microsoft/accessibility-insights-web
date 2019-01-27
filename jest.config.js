685153e55ae7624bca709c7002d90a2a392452b5
Devaradhan Murugesan <demuruge@microsoft.com>
2019-01-27 12:54:05 -0800

    upgrading jest to support global setup on multi-projects; https://github.com/facebook/jest/pull/6865
    updating deprecated property
    
    remove tsconfig.jest.json
    
    ignore d.ts files form coverage
    
    This reverts commit 7875345cadaee70b4f2cb2c2d5d3d8554fe0354f.

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-var common = require('./jest.common.config');
-
 module.exports = {
-    ...common,
-    coverageDirectory: './test-results/unit/coverage',
-    setupFiles: ['<rootDir>/src/tests/unit/jest-setup.ts'],
-    moduleFileExtensions: ['ts', 'tsx', 'js'],
-    roots: ['<rootDir>/src/tests/unit'],
-    collectCoverage: true,
-    collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/tests/**'],
-    coverageReporters: ['json', 'lcov', 'text', 'cobertura'],
-    testEnvironment: 'jsdom',
-    reporters: ['default', ['jest-junit', { outputDirectory: '.', outputName: './test-results/unit/junit.xml' }]],
+    projects: ['<rootDir>/src/tests/unit', '<rootDir>/src/tests/end-to-end'],
 };
--- src/tests/end-to-end/jest.config.js
+++ src/tests/end-to-end/jest.config.js
@@ -1,18 +1,22 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-var common = require('./jest.common.config');
+var common = require('../jest.common.config');
+const rootDir = '../../../';
+const currentDir = '<rootDir>/src/tests/end-to-end';
 
 module.exports = {
     ...common,
+    displayName: 'e2e tests',
     // using js instead of ts files since globalSetup & globalTeardown files are not transformed. refer https://github.com/facebook/jest/issues/5164
-    globalSetup: '<rootDir>/src/tests/end-to-end/setup/global-setup.js',
-    globalTeardown: '<rootDir>/src/tests/end-to-end/setup/global-teardown.js',
+    globalSetup: `${currentDir}/setup/global-setup.js`,
+    globalTeardown: `${currentDir}/setup/global-teardown.js`,
     moduleFileExtensions: [
         'ts',
         'json', // adding json, since puppeteer.launch throws error - refer https://github.com/GoogleChrome/puppeteer/issues/2754
         'js',
     ],
+    rootDir: rootDir,
     roots: ['<rootDir>/src/tests/end-to-end'],
-    reporters: ['default', ['jest-junit', { outputDirectory: '.', outputName: './test-results/e2e/junit-e2e.xml' }]],
-    setupTestFrameworkScriptFile: '<rootDir>/src/tests/end-to-end/setup/test-setup.ts',
+    reporters: ['default', ['jest-junit', { outputDirectory: '.', outputName: '<rootDir>/test-results/e2e/junit-e2e.xml' }]],
+    setupFilesAfterEnv: [`${currentDir}/setup/test-setup.ts`],
 };
