0513b6b96ccefd1d4e3229640d3d2f2c9a5559ee
Haonan Liu <haonliu@microsoft.com>
2019-01-29 16:12:13 -0800

    adding high contrast settings

---
+++ src/content/settings/high-contrast-mode.tsx
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import * as React from 'react';
+
+
+export const enableHighContrastSettingsTitie = 'Enable high contrast';
+export const highContrastSettingsDescription = <React.Fragment>
+    Make texts easier to see by using more distinct colors.
+</React.Fragment>;
