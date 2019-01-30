4c8e6f7bc96bb2430e7b6558cf07324a8ad983a9
Haonan Liu <haonliu@microsoft.com>
2019-01-29 16:27:19 -0800

    remove extra line

--- src/content/settings/high-contrast-mode.tsx
+++ src/content/settings/high-contrast-mode.tsx
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 import * as React from 'react';
 
-
 export const enableHighContrastSettingsTitie = 'Enable high contrast';
 export const highContrastSettingsDescription = <React.Fragment>
     Make text easier to see by using more distinct colors.
