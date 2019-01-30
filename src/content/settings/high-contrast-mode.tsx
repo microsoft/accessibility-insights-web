3c9db7ec0c8c9e87ad8fb629a9bb2dad02047681
Liu Haonan <haonliu@microsoft.com>
2019-01-29 16:54:47 -0800

    Users/haonliu/high contrast settings (#142)
    * adding high contrast settings
    
    * typo
    
    * remove extra line
    
    * remove react.fragment
    
    * prettier format

---
+++ src/content/settings/high-contrast-mode.tsx
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import * as React from 'react';
+
+export const enableHighContrastSettingsTitie = 'Enable high contrast';
+export const highContrastSettingsDescription = <>Make text easier to see by using more distinct colors.</>;
