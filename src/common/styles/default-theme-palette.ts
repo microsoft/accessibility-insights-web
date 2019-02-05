334b0af92165a8fb61cd5899c1bf3e165ca11330
Haonan Liu <haonliu@microsoft.com>
2019-02-05 12:41:24 -0800

    add copyright header

--- src/common/styles/default-theme-palette.ts
+++ src/common/styles/default-theme-palette.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 export const DefaultThemePalette = {
     themePrimary: '#0078d4',
     themeLighterAlt: '#eff6fc',
--- src/common/styles/high-contrast-theme-palette.ts
+++ src/common/styles/high-contrast-theme-palette.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 export const HighContrastThemePalette = {
     themePrimary: '#0078d4',
     themeLighterAlt: '#eff6fc',
