1a036017ebfef85bef01eeeec7fd13df606f02ab
Devaradhan Murugesan <demuruge@microsoft.com>
2019-02-05 16:41:05 -0800

    fix e2e root dir path

--- src/tests/end-to-end/jest.config.js
+++ src/tests/end-to-end/jest.config.js
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+const path = require('path');
 var common = require('../jest.common.config');
 const rootDir = '../../../';
 const currentDir = '<rootDir>/src/tests/end-to-end';
@@ -19,4 +20,7 @@ module.exports = {
     roots: ['<rootDir>/src/tests/end-to-end'],
     reporters: ['default', ['jest-junit', { outputDirectory: '.', outputName: '<rootDir>/test-results/e2e/junit-e2e.xml' }]],
     setupFilesAfterEnv: [`${currentDir}/setup/test-setup.ts`],
+    globals: {
+        rootDir: path.resolve(__dirname, rootDir),
+    },
 };
