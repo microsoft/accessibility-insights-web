815ee2f2ee40344c734fb4d139ec5461199e5003
Karanbir Singh <karansin@microsoft.com>
2019-02-05 16:42:54 -0800

    adding semantic slots into the themes

--- src/common/styles/high-contrast-theme-palette.ts
+++ src/common/styles/high-contrast-theme-palette.ts
@@ -1,28 +1,35 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { IPartialTheme } from '@uifabric/styling';
 
-// generated from https://developer.microsoft.com/en-us/fabric#/styles/themegenerator with white body text and black background
-export const HighContrastThemePalette = {
-    themePrimary: '#0078d4',
-    themeLighterAlt: '#eff6fc',
-    themeLighter: '#deecf9',
-    themeLight: '#c7e0f4',
-    themeTertiary: '#71afe5',
-    themeSecondary: '#2b88d8',
-    themeDarkAlt: '#106ebe',
-    themeDark: '#005a9e',
-    themeDarker: '#004578',
-    neutralLighterAlt: '#0b0b0b',
-    neutralLighter: '#151515',
-    neutralLight: '#252525',
-    neutralQuaternaryAlt: '#2f2f2f',
-    neutralQuaternary: '#373737',
-    neutralTertiaryAlt: '#595959',
-    neutralTertiary: '#c8c8c8',
-    neutralSecondary: '#d0d0d0',
-    neutralPrimaryAlt: '#dadada',
-    neutralPrimary: '#ffffff',
-    neutralDark: '#f4f4f4',
-    black: '#f8f8f8',
-    white: '#000000',
+export const HighContrastThemePalette: IPartialTheme = {
+    // basic palette from https://developer.microsoft.com/en-us/fabric#/styles/themegenerator with white text and #161616 background
+    palette: {
+        themePrimary: '#0078d4',
+        themeLighterAlt: '#eff6fc',
+        themeLighter: '#deecf9',
+        themeLight: '#c7e0f4',
+        themeTertiary: '#71afe5',
+        themeSecondary: '#2b88d8',
+        themeDarkAlt: '#106ebe',
+        themeDark: '#005a9e',
+        themeDarker: '#004578',
+        neutralLighterAlt: '#212121',
+        neutralLighter: '#2a2a2a',
+        neutralLight: '#393939',
+        neutralQuaternaryAlt: '#424242',
+        neutralQuaternary: '#494949',
+        neutralTertiaryAlt: '#686868',
+        neutralTertiary: '#c8c8c8',
+        neutralSecondary: '#d0d0d0',
+        neutralPrimaryAlt: '#dadada',
+        neutralPrimary: '#ffffff',
+        neutralDark: '#f4f4f4',
+        black: '#f8f8f8',
+        white: '#161616',
+    },
+    semanticColors: {
+        link: '#FFFF00',
+        disabledText: '#C285FF',
+    },
 };
