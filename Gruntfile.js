d2b3a9b6a72d6af8fc5cae4a40fe4c0d4180f823
Shobhit Agarwal <shobhit.agarwal@microsoft.com>
2019-02-07 15:14:16 -0800

    fix PR build

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
         exact_paths: [
             './.vscode',
             './.git',
-            './github/ISSUE_TEMPLATE',
+            './.github/ISSUE_TEMPLATE',
             './dist',
             './drop',
             './extension',
