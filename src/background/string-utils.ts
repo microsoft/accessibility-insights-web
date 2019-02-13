5cd6a9dfa9bdd03d9e4688aacc23cceadf288ff5
Sebastian Morales <semora@microsoft.com>
2019-02-13 13:17:46 -0800

    Remove string-utils.ts from background folder

--- src/background/string-utils.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-interface String {
-    toTitleCase: () => string;
-}
-
-String.prototype.toTitleCase = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
-};
