5fd6f14595b7f979944d369002b68f1cdf9a41e2
Haonan Liu <haonliu@microsoft.com>
2019-02-15 13:49:48 -0800

    add test step enums

---
+++ src/assessments/semantics/test-steps/test-steps.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+export const enum SemanticsTestStep {
+    cssContent = 'cssContent',
+    dataTables = 'dataTables',
+    lists = 'lists',
+    emphasis = 'emphasis',
+    quotes = 'quotes',
+    letterSpacing = 'letterSpacing',
+}
