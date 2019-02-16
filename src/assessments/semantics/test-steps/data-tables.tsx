bf99f5cd942461426ef4f0ca95e71aed06c4a580
Haonan Liu <haonliu@microsoft.com>
2019-02-15 16:46:26 -0800

    add data tables

---
+++ src/assessments/semantics/test-steps/data-tables.tsx
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import * as React from 'react';
+
+import { link } from '../../../content/link';
+import ManualTestRecordYourResults from '../../common/manual-test-record-your-results';
+import { TestStep } from '../../types/test-step';
+import { SemanticsTestStep } from './test-steps';
+
+const dataTablesDescription: JSX.Element = (
+    <span>Semantic elements in a data table must not be coded as decorative.</span>
+);
+
+const dataTablesHowToTest: JSX.Element = (
+    <div>
+        <ol>
+            <li>
+
+            </li>
+            <ManualTestRecordYourResults isMultipleFailurePossible={true} />
+        </ol>
+    </div>
+);
+
+export const dataTables: TestStep = {
+    key: SemanticsTestStep.dataTables,
+    name: 'Data tables',
+    description: dataTablesDescription,
+    howToTest: dataTablesHowToTest,
+    isManual: true,
+    guidanceLinks: [link.WCAG_1_3_3],
+    updateVisibility: false,
+};
