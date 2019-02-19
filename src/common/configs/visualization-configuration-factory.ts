ad99c63634b956ccdb6e6db7cf97bd11dc9b0413
Sebastian Morales <semora@microsoft.com>
2019-02-19 09:40:33 -0800

    Remove unused property

--- src/common/configs/visualization-configuration-factory.ts
+++ src/common/configs/visualization-configuration-factory.ts
@@ -32,7 +32,6 @@ export interface IDisplayableVisualizationTypeData {
     enableMessage: string;
     toggleLabel: string;
     linkToDetailsViewText: string;
-    assessmentContentTitle?: string;
 }
 
 export interface IAssesssmentVisualizationConfiguration {
