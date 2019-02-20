4e5e7a5b09c1539458a8b726f8f08fe0824d5037
Sebastian Morales <semora@microsoft.com>
2019-02-20 14:24:43 -0800

    Add extra guidance content for tab stops

--- src/content/adhoc/tabstops/index.ts
+++ src/content/adhoc/tabstops/index.ts
@@ -1,9 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { extraGuidance } from './extra-guidance';
 import { guidance } from './guidance';
 import { staticContent } from './static-content';
 
 export const tabstops = {
     guidance,
     staticContent,
+    extraGuidance,
 };
