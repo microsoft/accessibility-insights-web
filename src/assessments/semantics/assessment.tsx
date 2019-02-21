109faefd000eabd372dd38b78546cae9c9a1c44d
Shobhit Agarwal <shobhit.agarwal@microsoft.com>
2019-02-21 12:05:13 -0800

    adding semantics getting started content

--- src/assessments/semantics/assessment.tsx
+++ src/assessments/semantics/assessment.tsx
@@ -5,6 +5,7 @@ import * as React from 'react';
 import { FeatureFlags } from '../../common/feature-flags';
 import { VisualizationType } from '../../common/types/visualization-type';
 import { AssessmentBuilder } from '../assessment-builder';
+import * as Markup from '../markup';
 import { CssContent } from './test-steps/css-content';
 import { DataTables } from './test-steps/data-tables';
 import { Emphasis } from './test-steps/emphasis';
@@ -13,12 +14,25 @@ import { SemanticsLists } from './test-steps/lists';
 const key = 'semanticsAssessment';
 const title = 'Semantics';
 
-const SemanticsAssessmentGettingStarted: JSX.Element = <>SemanticsAssessmentGettingStarted</>;
-
+const gettingStarted: JSX.Element = (
+    <React.Fragment>
+        <p>
+            Information and relationships that are implied through visual formatting must be available to non-sighted users. Using semantic
+            markup helps achieve this by introducing meaning into a web page rather than just presentation. For example, HTML tags like{' '}
+            <Markup.Tag tagName="b" /> and <Markup.Tag tagName="i" /> are not semantic because they define only the{' '}
+            <Markup.Emphasis>visual appearance </Markup.Emphasis>of text. On the other hand, tags like <Markup.Tag tagName="blockquote" />
+            {', '}
+            <Markup.Tag tagName="em" /> {' and '}
+            <Markup.Tag tagName="ol" /> communicate the meaning of the text. Access to semantic information allows browsers and assistive
+            technologies to present the content appropriately to users. Using semantic elements correctly ensures all users have equal
+            access to the meaning of content.
+        </p>
+    </React.Fragment>
+);
 export const SemanticsAssessment = AssessmentBuilder.Assisted({
     key,
     title,
-    gettingStarted: SemanticsAssessmentGettingStarted,
+    gettingStarted,
     type: VisualizationType.SemanticsAssessment,
     steps: [CssContent, DataTables, Emphasis, SemanticsLists],
     storeDataKey: 'semanticsAssessment',
