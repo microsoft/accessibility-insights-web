8c2d1f5ed3d7330bfdd601343954c30da5937aad
Shobhit Agarwal <shoagarw@microsoft.com>
2019-02-25 17:11:09 -0800

    intermediate changes

--- src/tests/unit/tests/scanner/css-content-rule.test.ts
+++ src/tests/unit/tests/scanner/css-content-rule.test.ts
@@ -1,8 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { GlobalMock, GlobalScope, IGlobalMock, It, MockBehavior, Times } from 'typemoq';
+import { GlobalMock, GlobalScope, IGlobalMock, It, MockBehavior, Times, Mock } from 'typemoq';
 
 import { cssContentConfiguration } from '../../../../scanner/css-content-rule';
+import * as axe from 'axe-core';
 
 describe('verify meaningful semantic configs', () => {
     it('should have correct props', () => {
@@ -17,38 +18,46 @@ describe('verify meaningful semantic configs', () => {
 
 describe('verify matches', () => {
     const windowMock = GlobalMock.ofInstance(window.getComputedStyle, 'getComputedStyle', window, MockBehavior.Strict);
+    const axeVisibilityMock = GlobalMock.ofInstance(axe.commons.dom.isVisible, 'isVisible', axe.commons.dom, MockBehavior.Strict);
+
     beforeEach(() => {
         windowMock.reset();
     });
 
-    it('does not have any pseudoSelector', () => {
-        const node = {
-            content: 'none',
-        };
+    // it('does not have any pseudoSelector', () => {
+    //     const node = {
+    //         content: 'none',
+    //     };
 
-        testSemantics(node, windowMock, false);
-    });
+    //     testSemantics(node, windowMock, false);
+    // });
 
     it('has before pseudoSelector and matches correctly identifies it', () => {
-        const node = {
-            content: 'test',
-        };
-        testSemantics(node, windowMock, true);
+        const testFixture: HTMLDivElement = document.createElement('div');
+
+        const b: HTMLHeadingElement = document.createElement('h1');
+        testFixture.appendChild(b);
+        testSemantics(testFixture, b, ':before', windowMock, axeVisibilityMock, true);
     });
 });
 
 function testSemantics(
-    node: IDictionaryStringTo<string>,
+    node: HTMLElement,
+    node2: HTMLElement,
+    pseudoSelector: string,
     windowMock: IGlobalMock<typeof window.getComputedStyle>,
+    axeVisibilityMock: IGlobalMock<typeof axe.commons.dom.isVisible>,
     expectedResult: boolean,
 ): void {
     let result: boolean;
+    axeVisibilityMock.setup(v => v(node2)).returns(() => true);
+
     windowMock
-        .setup(m => m(It.isAny(), It.isAny()))
-        .returns(style => ({ getPropertyValue: property => style[property] } as CSSStyleDeclaration))
+        .setup(m => m(node2, It.isAny()))
+        .returns(style => ({ content: 'test' } as CSSStyleDeclaration))
         .verifiable(Times.atLeastOnce());
 
-    GlobalScope.using(windowMock).with(() => {
+    GlobalScope.using(windowMock, axeVisibilityMock).with(() => {
         result = cssContentConfiguration.rule.matches(node, null);
     });
     expect(result).toBe(expectedResult);
