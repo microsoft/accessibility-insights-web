802245d9c74a7680e465a4dbffd11da620d37c2d
Shobhit Agarwal <shobhit.agarwal@microsoft.com>
2019-02-26 11:41:22 -0800

    change clientBody

--- src/injected/visualization/single-target-drawer.ts
+++ src/injected/visualization/single-target-drawer.ts
@@ -10,7 +10,7 @@ import { IDrawer, IDrawerInitData } from './idrawer';
 export class SingleTargetDrawer implements IDrawer {
     protected isEnabled = false;
     protected drawerUtils: DrawerUtils;
-    private clientBody: HTMLElement;
+    private target: HTMLElement;
     private formatter: SingleTargetFormatter;
 
     constructor(drawerUtils: DrawerUtils, formatter: SingleTargetFormatter) {
@@ -22,24 +22,24 @@ export class SingleTargetDrawer implements IDrawer {
         this.eraseLayout();
         const elementResults = drawerInfo.data;
         const myDocument = this.drawerUtils.getDocumentElement();
-        this.clientBody = this.getFirstElementTarget(myDocument, elementResults);
+        this.target = this.getFirstElementTarget(myDocument, elementResults);
     }
 
     public drawLayout(): void {
         const injectedClassName = this.formatter.getDrawerConfiguration().injectedClassName;
-        if (this.clientBody) {
-            this.clientBody.classList.add(injectedClassName);
+        if (this.target) {
+            this.target.classList.add(injectedClassName);
         }
         this.isEnabled = true;
     }
 
     public eraseLayout(): void {
         this.isEnabled = false;
-        if (this.clientBody == null) {
+        if (this.target == null) {
             return;
         }
         const injectedClassName = this.formatter.getDrawerConfiguration().injectedClassName;
-        this.clientBody.classList.remove(injectedClassName);
+        this.target.classList.remove(injectedClassName);
     }
 
     public get isOverlayEnabled(): boolean {
