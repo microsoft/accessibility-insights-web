d0bb243123f791a4020fb0472ed7642a7c723b6b
Fernanda Bonnin <ferbonn@microsoft.com>
2019-02-27 12:11:12 -0800

    modified Tab stops instructions

--- src/content/adhoc/tabstops/how-to-test.tsx
+++ src/content/adhoc/tabstops/how-to-test.tsx
@@ -4,16 +4,25 @@ import { React, create } from '../../common';
 
 export const howToTest = create(({ Markup }) => (
     <>
+        <p>
+            <em>Note: this test requires you to use a keyboard and to visually identify interactive elements.</em>
+        </p>
         <h2>How to test</h2>
         <ol>
             <li>Refresh the target page to put it in its default state.</li>
             <li>Turn on the Show tab stops toggle. An empty circle will highlight the current tab stop.</li>
-            <li>Press the Tab key to move input focus through all the interactive elements in the page.</li>
             <li>
-                As you tab to each element, look for these <Markup.Term>accessibility problems</Markup.Term>:
+                Use your keyboard to move input focus through all the interactive elements in the page:
                 <ul>
-                    <li>An interactive element can't be reached using the Tab key.</li>
-                    <li>An interactive element "traps" input focus and prevents tabbing away.</li>
+                    <li>Use Tab and Shift+Tab to navigate between standalone controls.</li>
+                    <li>Use the arrow keys to navigate between the focusable elements within a composite control.</li>
+                </ul>
+            </li>
+            <li>
+                As you navigate to each element, look for these <Markup.Term>accessibility problems</Markup.Term>:
+                <ul>
+                    <li>An interactive element can't be reached using the Tab and arrow keys.</li>
+                    <li>An interactive element "traps" input focus and prevents navigating away.</li>
                     <li>An interactive element doesn't give a visible indication when it has input focus.</li>
                     <li>The tab order is inconsistent with the logical order that's communicated visually.</li>
                     <li>Input focus moves unexpectedly without the user initiating it.</li>
