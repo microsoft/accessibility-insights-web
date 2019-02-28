eb08f3916e4a17760762cb2d077e65a8fea936d0
Shobhit Agarwal <shobhit.agarwal@microsoft.com>
2019-02-28 14:47:17 -0800

    update how target page link is read by screen reader

--- src/DetailsView/components/details-view-command-bar.tsx
+++ src/DetailsView/components/details-view-command-bar.tsx
@@ -62,11 +62,17 @@ export class DetailsViewCommandBar extends React.Component<IDetailsViewCommandBa
     }
 
     private renderTargetPageInfo(): JSX.Element {
+        const targetPageTitle: string = this.props.tabStoreData.title;
         return (
             <div className="details-view-target-page">
                 Target page:&nbsp;
-                <Link role="link" className="target-page-link" onClick={this.props.actionMessageCreator.switchToTargetTab}>
-                    {this.props.tabStoreData.title}
+                <Link
+                    role="link"
+                    aria-label={`Target Page ${targetPageTitle}`}
+                    className="target-page-link"
+                    onClick={this.props.actionMessageCreator.switchToTargetTab}
+                >
+                    {targetPageTitle}
                 </Link>
             </div>
         );
