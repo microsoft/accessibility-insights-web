09930a4a854678519bf18c67ffd9a7e6ba2e3408
Mark Reay <markreay@live.com>
2019-03-02 12:05:28 -0800

    Added axe version to popup

--- src/popup/scripts/components/launch-pad.tsx
+++ src/popup/scripts/components/launch-pad.tsx
@@ -5,9 +5,11 @@ import * as React from 'react';
 import { ExternalLink, ExternalLinkDeps } from '../../../common/components/external-link';
 import { PopupActionMessageCreator } from '../actions/popup-action-message-creator';
 import { LaunchPadItemRow } from './launch-pad-item-row';
+import { AxeInfo } from '../../../common/axe-info';
 
 export type LaunchPadDeps = {
     popupActionMessageCreator: PopupActionMessageCreator;
+    axeInfo: AxeInfo;
 } & ExternalLinkDeps;
 
 export interface LaunchPadProps {
@@ -52,6 +54,7 @@ export class LaunchPad extends React.Component<LaunchPadProps, undefined> {
     }
 
     public render(): JSX.Element {
+        const { axeInfo } = this.props.deps;
         return (
             <div className="ms-Grid main-section">
                 <main>
@@ -71,6 +74,7 @@ export class LaunchPad extends React.Component<LaunchPadProps, undefined> {
                         >
                             axe-core
                         </ExternalLink>
+                        {axeInfo.version}
                     </div>
                 </div>
             </div>
