9092562d9eb74d1b7a93cc8137b3233d32b69b4e
Mark Reay <mreay@microsoft.com>
2019-03-05 18:20:32 -0800

    Changed Ada's congratulations message in automated checks report to new design (#292)
    * Added Ada holding a sign as congratulations message on fastpass report
    
    * Changed to Ada's laptop
    
    * Revised snapshots
    
    * Alphabetized imports

--- src/DetailsView/reports/components/inline-image.tsx
+++ src/DetailsView/reports/components/inline-image.tsx
@@ -1,15 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as React from 'react';
+import { adaLaptop } from '../../../icons/ada/ada-laptop-base64';
 import { adaMulticolorBubbles } from '../../../icons/ada/ada-multicolor-bubbles-base64';
 import { blue48 } from '../../../icons/brand/blue/brand-blue-48px-base64';
 
 export enum InlineImageType {
     InsightsLogo48,
-    AdaTheCat, // Ada is on vacation for the holidays
+    AdaTheCat,
     FailIcon,
     PassIcon,
     NotApplicableIcon,
+    AdaLaptop,
 }
 
 export interface InlineImageProps {
@@ -29,6 +31,7 @@ export class InlineImage extends React.Component<InlineImageProps> {
     // tslint:disable:max-line-length
     private inlineImageTypeToData: IDictionaryNumberTo<string> = {
         [InlineImageType.AdaTheCat]: adaMulticolorBubbles,
+        [InlineImageType.AdaLaptop]: adaLaptop,
         [InlineImageType.InsightsLogo48]: blue48,
         [InlineImageType.FailIcon]:
             'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAEgSURBVHgBjVO7UcNAFFwdGZFLcAeIChAdEOIIqwJDB1ABUIGVACHqAFwB7oArQY48DvzZlU+e55M09pvR6P12309K0CNfwPACGKyBagT4rpzEGlMmXwKTLfBIc2BCApf0v1uiA/gDSFnpm+oQ/eJJcNsQJE2LVH5OAFsELjheI6CPAJXRVWgqxaldGncmWIjZEKjSNZ/S5GTsNnOcM4uqjNnOTSD41Vt2VEDzpo7BNAKDvkKAewIVl422XAlcdQT8BpiFRc7RcWfGF861A960+m9GOMrjuH9u2W6pDIDCjoDjhanyrL7zJ/DGhAnOl4L7yOs7r4Bn9Hy/HeLZ7ZOUGpxzac1pTgDr0+VhyUkc5YbHdD5gf0L9HEqcE/Qyish3OPNZtBL6xXIAAAAASUVORK5CYII=',
