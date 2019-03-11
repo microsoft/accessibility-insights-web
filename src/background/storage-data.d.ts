40719ffbc5be73d5c02899e428e702c8cd0776a9
Lisa Li <lisli@microsoft.com>
2019-03-11 14:07:54 -0700

    Removed prefix I from IInstallationData

--- src/background/storage-data.d.ts
+++ src/background/storage-data.d.ts
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { FeatureFlagStoreData } from './../types/store-data/feature-flag-store-data.d';
-import { IInstallationData } from './installation-data';
+import { InstallationData } from './installation-data';
 
 // tslint:disable-next-line:interface-name
 export interface ILocalStorageData {
     url?: string;
     featureFlags?: FeatureFlagStoreData;
     launchPanelSetting?: LaunchPanelType;
-    installationData?: IInstallationData;
+    installationData?: InstallationData;
 }
