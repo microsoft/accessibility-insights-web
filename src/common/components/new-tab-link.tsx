a33b3cc5666591ddfe652c662897c6692ba50063
Devaradhan Murugesan <demuruge@microsoft.com>
2019-03-11 11:24:15 -0700

    addressing PR comments

--- src/common/components/new-tab-link.tsx
+++ src/common/components/new-tab-link.tsx
@@ -1,18 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { BaseComponent, css } from '@uifabric/utilities';
-import { isNil, omit } from 'lodash';
+import { omit } from 'lodash';
 import { ILinkProps, Link } from 'office-ui-fabric-react/lib/Link';
-
 import * as React from 'react';
 
 export class NewTabLink extends BaseComponent<ILinkProps> {
     public render(): JSX.Element {
-        const classNames = ['insights-link'];
-        if (!isNil(this.props.className)) {
-            classNames.push(this.props.className);
-        }
-
+        const classNames = ['insights-link', this.props.className];
         const allPropsExceptClassName = omit(this.props, 'className');
 
         return <Link className={css(...classNames)} {...allPropsExceptClassName} target="_blank" />;
