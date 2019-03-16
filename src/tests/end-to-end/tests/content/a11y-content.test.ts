fb3dccfc84a42f8e4665ef201be2c44eb45a7998
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-03-15 17:06:49 -0700

    Add HC mode a11y automated check tests for content

--- src/tests/end-to-end/tests/content/a11y-content.test.ts
+++ src/tests/end-to-end/tests/content/a11y-content.test.ts
@@ -3,35 +3,71 @@
 import { contentPages } from '../../../../content';
 import { Browser } from '../../common/browser';
 import { launchBrowser } from '../../common/browser-factory';
+import { DetailsViewCommonSelectors } from '../../common/element-identifiers/common-selectors';
 import { scanForAccessibilityIssues } from '../../common/scan-for-accessibility-issues';
 
 describe('A11Y for content pages', () => {
-    let browser: Browser;
-
-    beforeAll(async () => {
-        browser = await launchBrowser({ suppressFirstTimeDialog: true });
-    });
-
-    afterEach(async () => {
-        if (browser) {
-            await browser.closeAllPages();
-        }
-    });
-
-    afterAll(async () => {
-        if (browser) {
-            await browser.close();
-            browser = undefined;
-        }
-    });
-
     const contentPaths = contentPages.allPaths();
 
-    it.each(contentPaths)('%s', async path => {
-        const content = await browser.newContentPage(path);
+    describe('Normal mode', () => {
+        let browser: Browser;
 
-        const results = await scanForAccessibilityIssues(content, '*');
+        beforeAll(async () => {
+            browser = await launchBrowser({ suppressFirstTimeDialog: true });
+        });
 
-        expect(results).toHaveLength(0);
+        afterAll(async () => {
+            if (browser) {
+                await browser.close();
+                browser = undefined;
+            }
+        });
+
+        it.each(contentPaths)('%s', async path => {
+            const content = await browser.newContentPage(path);
+
+            const results = await scanForAccessibilityIssues(content, '*');
+
+            expect(results).toHaveLength(0);
+
+            await content.close();
+        });
+    });
+
+    describe('High Contrast mode', () => {
+        let browser: Browser;
+        let targetTabId: number;
+
+        beforeAll(async () => {
+            browser = await launchBrowser({ suppressFirstTimeDialog: true });
+            targetTabId = await generateTargetTabId();
+            const detailsViewPage = await browser.newExtensionDetailsViewPage(targetTabId);
+            await detailsViewPage.clickSelector(DetailsViewCommonSelectors.gearButton);
+            await detailsViewPage.clickSelector(DetailsViewCommonSelectors.settingsButton);
+            await detailsViewPage.clickSelector(DetailsViewCommonSelectors.highContrastToggle);
+        });
+
+        afterAll(async () => {
+            if (browser) {
+                await browser.close();
+                browser = undefined;
+            }
+        });
+
+        it.each(contentPaths)('%s', async path => {
+            const content = await browser.newContentPage(path);
+
+            const results = await scanForAccessibilityIssues(content, '*');
+
+            expect(results).toHaveLength(0);
+
+            await content.close();
+        });
+
+        async function generateTargetTabId(): Promise<number> {
+            const targetPage = await browser.newTestResourcePage('all.html');
+            await targetPage.bringToFront();
+            return await browser.getActivePageTabId();
+        }
     });
 });
