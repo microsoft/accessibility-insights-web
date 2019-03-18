546e1488446a3995064c68ceb065b4f2c27bc0b8
Dan Bjorge <danielbj@microsoft.com>
2019-03-18 15:41:15 -0700

    Avoid wrapping console.log/err

--- src/common/logging/default-logger.ts
+++ src/common/logging/default-logger.ts
@@ -4,7 +4,11 @@ import { Logger } from './logger';
 // Licensed under the MIT License.
 export const createDefaultLogger = (): Logger => {
     return {
-        log: (message?: any, ...otherArgs: any[]) => console.log(message, ...otherArgs),
-        error: (message?: any, ...otherArgs: any[]) => console.error(message, ...otherArgs),
+        // It's important that these use the console functions directly rather than wrapping them
+        // because it ensures that the file/line numbers reported in the devtools console point to
+        // the lines that are actually responsible for emitting log/error calls, rather than always
+        // pointing to a generic wrapping function.
+        log: console.log,
+        error: console.error,
     };
 };
