5d261bd8047786ee624ea0531777e8263353dcbe
Dan Bjorge <danielbj@microsoft.com>
2019-03-18 16:54:10 -0700

    Remove redundant comment

--- src/common/logging/default-logger.ts
+++ src/common/logging/default-logger.ts
@@ -1,13 +1,9 @@
-import { Logger } from './logger';
-
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { Logger } from './logger';
+
 export const createDefaultLogger = (): Logger => {
     return {
-        // It's important that these use the console functions directly rather than wrapping them
-        // because it ensures that the file/line numbers reported in the devtools console point to
-        // the lines that are actually responsible for emitting log/error calls, rather than always
-        // pointing to a generic wrapping function.
         log: console.log,
         error: console.error,
     };
