3673ff7bbc0c06907e5ea521e4f8ede6f36ad67c
Dan Bjorge <danielbj@microsoft.com>
2019-03-18 17:30:20 -0700

    Add another missing copyright header

--- src/tests/unit/common/visualization-toggle-props-builder.ts
+++ src/tests/unit/common/visualization-toggle-props-builder.ts
@@ -1,5 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 import { VisualizationToggleProps } from '../../../common/components/visualization-toggle';
 import { BaseDataBuilder } from './base-data-builder';
+
 export class VisualizationTogglePropsBuilder extends BaseDataBuilder<VisualizationToggleProps> {
     constructor() {
         super();
