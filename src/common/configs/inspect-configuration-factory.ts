610bde47f0d78828eb7e35cf3a5f8d65e43a0d67
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-03-19 15:00:39 -0700

    Add missing tests

--- src/common/configs/inspect-configuration-factory.ts
+++ src/common/configs/inspect-configuration-factory.ts
@@ -1,14 +1,15 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { autobind } from '@uifabric/utilities';
-
 import { InspectMode } from '../../background/inspect-modes';
 import { ScopingInputTypes } from '../../background/scoping-input-types';
-import { DictionaryNumberTo } from '../../types/common-types';
+import { DictionaryStringTo } from '../../types/common-types';
 import { ScopingActionMessageCreator } from '../message-creators/scoping-action-message-creator';
 import { ISingleElementSelector } from '../types/store-data/scoping-store-data';
 
 export type IInspectCallback = (event: MouseEvent, selector: ISingleElementSelector) => void;
+
+export type ConfigurationKey = Exclude<InspectMode, 'off'>;
 
 export class InspectConfigurationFactory {
     private scopingActionMessageCreator: ScopingActionMessageCreator;
@@ -16,16 +17,18 @@ export class InspectConfigurationFactory {
         this.scopingActionMessageCreator = scopingActionMessageCreator;
     }
 
-    private configurationByType: DictionaryNumberTo<IInspectCallback> = {
+    private configurationByType: DictionaryStringTo<IInspectCallback> = {
         [InspectMode.scopingAddInclude]: this.addIncludeSelector,
         [InspectMode.scopingAddExclude]: this.addExcludeSelector,
     };
 
-    public getConfigurationByKey(key: string): (event: MouseEvent, selector: string[]) => void {
+    public getConfigurationByKey(key: ConfigurationKey): (event: MouseEvent, selector: string[]) => void {
         const configuration = this.configurationByType[key];
+
         if (configuration == null) {
             throw new Error(`Unsupported type: ${key}`);
         }
+
         return configuration;
     }
 
