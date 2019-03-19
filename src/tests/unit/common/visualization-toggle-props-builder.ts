286ec63e60480f5cb85986ebdbe257df740bfd64
Dan Bjorge <dbjorge@gmail.com>
2019-03-19 16:26:12 -0400

    Move common test helper code out of individual test files (#382)
    Wallaby complains with warnings when test files import other test files, since it causes it to double-run tests. This fixes the existing cases of these warnings by extracting the common test helpers into their own files.

---
+++ src/tests/unit/common/visualization-toggle-props-builder.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { VisualizationToggleProps } from '../../../common/components/visualization-toggle';
+import { BaseDataBuilder } from './base-data-builder';
+
+export class VisualizationTogglePropsBuilder extends BaseDataBuilder<VisualizationToggleProps> {
+    constructor() {
+        super();
+        this.data = {
+            checked: false,
+            disabled: false,
+            visualizationName: null,
+        } as VisualizationToggleProps;
+    }
+}
