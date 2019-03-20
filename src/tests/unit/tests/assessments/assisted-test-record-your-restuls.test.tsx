9052a2ac42d8a44d6e2a8d9f841da636795fb762
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-03-20 13:05:21 -0700

    Move copyright header to the top of the new file

--- src/tests/unit/tests/assessments/assisted-test-record-your-restuls.test.tsx
+++ src/tests/unit/tests/assessments/assisted-test-record-your-restuls.test.tsx
@@ -1,9 +1,10 @@
-import { shallow } from 'enzyme';
-import * as React from 'react';
-import { AssistedTestRecordYourResults } from '../../../../assessments/common/assisted-test-record-your-results';
-
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+import { AssistedTestRecordYourResults } from '../../../../assessments/common/assisted-test-record-your-results';
+
 describe('AssistedTestRecordYourResults', () => {
     it('renders', () => {
         const wrapped = shallow(<AssistedTestRecordYourResults />);
