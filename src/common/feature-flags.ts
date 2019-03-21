98e2fc443899bb93e981bbbddbf6406007d979cf
Lisa Li <lisli@microsoft.com>
2019-03-20 17:06:46 -0700

    Removed feature flag highContrastMode

--- src/common/feature-flags.ts
+++ src/common/feature-flags.ts
@@ -13,7 +13,6 @@ export class FeatureFlags {
     public static readonly scoping = 'scoping';
     public static readonly showBugFiling = 'showBugFiling';
     public static readonly showInstanceVisibility = 'showInstanceVisibility';
-    public static readonly highContrastMode = 'highContrastMode';
 }
 
 export interface FeatureFlagDetail {
@@ -94,14 +93,6 @@ export function getAllFeatureFlagDetails(): FeatureFlagDetail[] {
                 "(You'll need to go to a different requirement and come back for it to take effect.)",
             isPreviewFeature: false,
             forceDefault: false,
-        },
-        {
-            id: FeatureFlags.highContrastMode,
-            defaultValue: true,
-            displayableName: 'High contrast mode',
-            displayableDescription: 'Show setting for high contrast mode under development',
-            isPreviewFeature: false,
-            forceDefault: true,
         },
     ];
 }
