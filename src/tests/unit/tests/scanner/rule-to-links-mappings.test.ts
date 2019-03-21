bb6a2960858770a49b971553a1ace251181a3c3b
Dan Bjorge <danielbj@microsoft.com>
2019-03-21 14:50:04 -0700

    Remove 2 more best-practice landmark rules and add a test case verifying there arent any more

--- src/tests/unit/tests/scanner/rule-to-links-mappings.test.ts
+++ src/tests/unit/tests/scanner/rule-to-links-mappings.test.ts
@@ -3,13 +3,28 @@
 import * as Axe from 'axe-core';
 
 import { ruleToLinkConfiguration } from '../../../../scanner/rule-to-links-mappings';
+import { difference } from 'lodash';
 
-describe('axe.commons.text.accessibleText examples', () => {
+describe('ruleToLinkConfiguration', () => {
     const axe = Axe as any;
+    const allAxeRules: string[] = axe.getRules().map(rule => rule.ruleId);
+    const bestPracticeAxeRules: string[] = axe.getRules(['best-practice']).map(rule => rule.ruleId);
 
-    axe.getRules().forEach(rule => {
-        it(`should have mapping for ${rule.ruleId}`, () => {
-            expect(ruleToLinkConfiguration[rule.ruleId]).not.toEqual(undefined);
-        });
+    // We're grandfathering this case in while we validate why axe considers it best-practice
+    const bestPracticeAxeRulesWeWantToEnforceAnyway = ['accesskeys'];
+
+    it.each(allAxeRules)(`should have a mapping for axe rule %s`, rule => {
+        expect(ruleToLinkConfiguration[rule]).toBeDefined();
     });
+
+    it.each(difference(bestPracticeAxeRules, bestPracticeAxeRulesWeWantToEnforceAnyway))(
+        `should map axe best-practice rule %s as BestPractice`,
+        rule => {
+            expect(hasBestPracticeLink(ruleToLinkConfiguration[rule])).toBe(true);
+        },
+    );
+
+    function hasBestPracticeLink(links: HyperTextDefinition[]): boolean {
+        return links.findIndex(link => link.text === 'Best Practice') !== -1;
+    }
 });
