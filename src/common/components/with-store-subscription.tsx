ee24dca6e2d604768cf3392c45b06c30e56df01d
Shobhit Agarwal <shoagarw@microsoft.com>
2019-03-22 12:03:01 -0700

    fix typedef all

--- src/common/components/with-store-subscription.tsx
+++ src/common/components/with-store-subscription.tsx
@@ -15,8 +15,9 @@ export type WithStoreSubscriptionDeps<T> = {
     storeActionMessageCreator: IStoreActionMessageCreator;
 };
 
-// tslint:disable-next-line: typedef
-export function withStoreSubscription<P extends WithStoreSubscriptionProps<S>, S>(WrappedComponent: React.ComponentType<P>) {
+export function withStoreSubscription<P extends WithStoreSubscriptionProps<S>, S>(
+    WrappedComponent: React.ComponentType<P>,
+): React.ComponentClass<P, S> {
     return class extends React.Component<P, S> {
         constructor(props: P) {
             super(props);
