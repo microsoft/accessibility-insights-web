79da2f57f6a9b4e98cba9e69e94b780778c5ad5d
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-03-27 17:47:37 -0700

    Move ICurrentPanel to it's own file

--- src/common/types/store-data/idetails-view-data.d.ts
+++ src/common/types/store-data/idetails-view-data.d.ts
@@ -1,18 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { DetailsViewRightContentPanelType } from '../../../DetailsView/components/left-nav/details-view-right-content-panel-type';
+import { CurrentPanel } from './current-panel';
 
 // tslint:disable-next-line:interface-name
 export interface IDetailsViewData {
     detailsViewRightContentPanel: DetailsViewRightContentPanelType;
-    currentPanel: ICurrentPanel;
+    currentPanel: CurrentPanel;
     contentPath: string;
-}
-
-// tslint:disable-next-line:interface-name
-export interface ICurrentPanel {
-    isPreviewFeaturesOpen: boolean;
-    isScopingOpen: boolean;
-    isContentOpen: boolean;
-    isSettingsOpen: boolean;
 }
