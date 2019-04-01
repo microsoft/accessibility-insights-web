33a86092340be59685cf25eaf72af4043478cc49
Lisa Li <lisli@microsoft.com>
2019-04-01 16:54:19 -0700

    Moved bug and copy SVG icon colors to injected.scss and detailsview.scss

--- src/common/icons/lady-bug-solid-icon.tsx
+++ src/common/icons/lady-bug-solid-icon.tsx
@@ -46,6 +46,6 @@ const d =
 
 export const LadyBugSolidIcon = NamedSFC('LadyBugSolidIcon', () => (
     <svg width="12" height="16" viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d={d} fill="black" fillOpacity="0.9" />
+        <path d={d} />
     </svg>
 ));
