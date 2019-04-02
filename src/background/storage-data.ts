4c05d1007f64bbfcebebf1ff930c1c21c3009458
Liu Haonan <haonliu@microsoft.com>
2019-04-02 13:05:44 -0700

    Rename local var name in assessment builder (#491)
    * rename local var name in assessment builder
    
    * minor change
    
    * fix build failure
    
    * sort imports

--- src/background/storage-data.ts
+++ src/background/storage-data.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { FeatureFlagStoreData } from '../common/types/store-data/feature-flag-store-data';
-import { LaunchPanelType } from '../popup/scripts/components/popup-view';
+import { LaunchPanelType } from '../popup/components/popup-view';
 import { InstallationData } from './installation-data';
 
 export interface LocalStorageData {
