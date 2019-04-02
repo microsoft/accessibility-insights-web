84a46a9cc59dbda0ecb07a49ea5580ddd353f3ea
Haonan Liu <haonliu@microsoft.com>
2019-04-02 12:55:13 -0700

    fix build failure

--- src/background/storage-data.ts
+++ src/background/storage-data.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { FeatureFlagStoreData } from '../common/types/store-data/feature-flag-store-data';
-import { LaunchPanelType } from '../popup/scripts/components/popup-view';
 import { InstallationData } from './installation-data';
+import { LaunchPanelType } from '../popup/components/popup-view';
 
 export interface LocalStorageData {
     url?: string;
