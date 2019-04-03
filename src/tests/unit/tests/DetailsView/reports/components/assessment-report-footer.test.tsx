c81ec530adae9d8136661065d88e3aedf725bef4
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-04-03 16:00:49 -0700

    Migrate assessment-report-footer test

--- src/tests/unit/tests/DetailsView/reports/components/assessment-report-footer.test.tsx
+++ src/tests/unit/tests/DetailsView/reports/components/assessment-report-footer.test.tsx
@@ -1,8 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { shallow } from 'enzyme';
 import * as React from 'react';
-import * as renderer from 'react-test-renderer';
-
 import {
     AssessmentReportFooter,
     AssessmentReportFooterProps,
@@ -15,8 +14,9 @@ describe('AssessmentReportFooter', () => {
             axeVersion: 'axeVersion',
             chromeVersion: 'chromeVersion',
         };
-        const tree = renderer.create(<AssessmentReportFooter {...props} />).toJSON();
 
-        expect(tree).toMatchSnapshot();
+        const wrapper = shallow(<AssessmentReportFooter {...props} />);
+
+        expect(wrapper.getElement()).toMatchSnapshot();
     });
 });
