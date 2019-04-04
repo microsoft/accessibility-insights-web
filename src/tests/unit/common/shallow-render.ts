c71202b7384d36c2dfcdf6a892aa87d54a7b46a3
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-04-03 17:11:10 -0700

    Remove shallow-renderer

--- src/tests/unit/common/shallow-render.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-import * as ReactShallowRenderer from 'react-test-renderer/shallow';
-
-export function shallowRender(el): React.ReactElement<any> {
-    const renderer = ReactShallowRenderer.createRenderer();
-    renderer.render(el);
-    return renderer.getRenderOutput();
-}
