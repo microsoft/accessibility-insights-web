fe63a062fd2aef44d10429dbce445370bea71b62
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-04-05 09:12:51 -0700

    Convert SettingsPanel to NamedSFC

--- src/DetailsView/components/settings-panel/settings-panel.tsx
+++ src/DetailsView/components/settings-panel/settings-panel.tsx
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as React from 'react';
-
 import { UserConfigMessageCreator } from '../../../common/message-creators/user-config-message-creator';
+import { NamedSFC } from '../../../common/react/named-sfc';
 import { FeatureFlagStoreData } from '../../../common/types/store-data/feature-flag-store-data';
 import { UserConfigurationStoreData } from '../../../common/types/store-data/user-configuration-store';
 import { DetailsViewActionMessageCreator } from '../../actions/details-view-action-message-creator';
@@ -22,23 +22,22 @@ export interface SettingsPanelProps {
     isOpen: boolean;
     featureFlagData: FeatureFlagStoreData;
 }
-export class SettingsPanel extends React.Component<SettingsPanelProps> {
-    public render(): JSX.Element {
-        const { deps, userConfigStoreState, featureFlagData, isOpen } = this.props;
 
-        return (
-            <GenericPanel
-                isOpen={isOpen}
-                className="settings-panel"
-                onDismiss={deps.detailsViewActionMessageCreator.closeSettingsPanel}
-                closeButtonAriaLabel="Close settings panel"
-                hasCloseButton={true}
-                title="Settings"
-            >
-                <TelemetrySettings deps={deps} enabled={userConfigStoreState.enableTelemetry} />
-                <HighContrastSettings deps={deps} enabled={userConfigStoreState.enableHighContrast} />
-                <BugFilingSettings deps={deps} featureFlagData={featureFlagData} userConfigStoreState={userConfigStoreState} />
-            </GenericPanel>
-        );
-    }
-}
+export const SettingsPanel = NamedSFC<SettingsPanelProps>('SettingsPanel', props => {
+    const { deps, userConfigStoreState, featureFlagData, isOpen } = props;
+
+    return (
+        <GenericPanel
+            isOpen={isOpen}
+            className="settings-panel"
+            onDismiss={deps.detailsViewActionMessageCreator.closeSettingsPanel}
+            closeButtonAriaLabel="Close settings panel"
+            hasCloseButton={true}
+            title="Settings"
+        >
+            <TelemetrySettings deps={deps} enabled={userConfigStoreState.enableTelemetry} />
+            <HighContrastSettings deps={deps} enabled={userConfigStoreState.enableHighContrast} />
+            <BugFilingSettings deps={deps} featureFlagData={featureFlagData} userConfigStoreState={userConfigStoreState} />
+        </GenericPanel>
+    );
+});
