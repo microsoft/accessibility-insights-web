9e481186bf8763e21049ddc979de5647f6d58cb4
Liu Haonan <haonliu@microsoft.com>
2019-04-08 15:53:02 -0700

    Settings panel code clean up (#508)
    * address comments on the previous PR
    
    * rename

--- src/DetailsView/components/settings-panel/settings/settings-provider-impl.ts
+++ src/DetailsView/components/settings-panel/settings/settings-provider-impl.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 import { BugFilingSettings } from './bug-filing/bug-filing-settings';
 import { HighContrastSettings } from './high-contrast/high-contrast-settings';
-import { create } from './settings-provider';
+import { createSettingsProvider } from './settings-provider';
 import { TelemetrySettings } from './telemetry/telemetry-settings';
 
-export const SettingsProviderImpl = create([TelemetrySettings, HighContrastSettings, BugFilingSettings]);
+export const SettingsProviderImpl = createSettingsProvider([TelemetrySettings, HighContrastSettings, BugFilingSettings]);
