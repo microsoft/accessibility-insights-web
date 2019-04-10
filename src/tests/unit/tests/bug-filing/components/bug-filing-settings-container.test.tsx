75b53b9f563521fc3e1a879e849c2dc91e1680a7
Liu Haonan <haonliu@microsoft.com>
2019-04-10 13:25:20 -0700

    Add bug filing settings container (#518)
    * Add bug filing settings container
    
    * revert renaming
    
    * modify interfaces
    
    * avoid snapshot actual implement of an obj

---
+++ src/tests/unit/tests/bug-filing/components/bug-filing-settings-container.test.tsx
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+import {
+    BugFilingSettingsContainer,
+    BugFilingSettingsContainerProps,
+} from '../../../../../bug-filing/components/bug-filing-settings-container';
+import { BugFilingService } from '../../../../../bug-filing/types/bug-filing-service';
+import { UserConfigMessageCreator } from '../../../../../common/message-creators/user-config-message-creator';
+import { BugServiceProperties, UserConfigurationStoreData } from '../../../../../common/types/store-data/user-configuration-store';
+
+describe('BugFilingSettingsContainerTest', () => {
+    const selectedBugFilingService: BugFilingService = {
+        key: 'test',
+        displayName: 'TEST',
+        renderSettingsForm: formProps => {
+            return <>{formProps}</>;
+        },
+    } as BugFilingService;
+    const bugFilingServices = [selectedBugFilingService];
+    const userConfigurationStoreData: UserConfigurationStoreData = {
+        bugService: 'test',
+    } as UserConfigurationStoreData;
+    const selectedBugFilingServiceData: BugServiceProperties = {
+        repository: 'none',
+    };
+    const userConfigMessageCreatorStub: UserConfigMessageCreator = {} as UserConfigMessageCreator;
+    const props: BugFilingSettingsContainerProps = {
+        deps: {
+            userConfigMessageCreator: userConfigMessageCreatorStub,
+        },
+        selectedBugFilingService,
+        bugFilingServices,
+        userConfigurationStoreData,
+        selectedBugFilingServiceData,
+    };
+
+    test('render', () => {
+        const wrapper = shallow(<BugFilingSettingsContainer {...props} />);
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+});
