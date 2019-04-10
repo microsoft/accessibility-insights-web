27b97529af3fb997aac83185ba1c371450240318
Haonan Liu <haonliu@microsoft.com>
2019-04-10 16:45:52 -0700

    EnvironmentInfoProvider UTs

---
+++ src/common/environment-info-provider.ts
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { AxeInfo } from './axe-info';
+
+export type EnvironmentInfo = {
+    extensionVersion: string;
+    browserSpec: string;
+    axeCoreVersion: string;
+};
+
+export class EnvironmentInfoProvider {
+    constructor(private extensionVersion: string, private browserSpec: string, private axeVersion: string) {}
+    public get(): EnvironmentInfo {
+        return {
+            extensionVersion: this.extensionVersion,
+            browserSpec: this.browserSpec,
+            axeCoreVersion: this.axeVersion,
+        };
+    }
+}
