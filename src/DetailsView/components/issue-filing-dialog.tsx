9219e13f17430d249205df7b5dd5171a45936f1c
Wahaj Abid <waabid@microsoft.com>
2019-04-10 10:18:44 -0700

    Update issue filing dialog to be a Named SFC and re-factor tests for easier readability and simpler cases

--- src/DetailsView/components/issue-filing-dialog.tsx
+++ src/DetailsView/components/issue-filing-dialog.tsx
@@ -7,6 +7,7 @@ import { BugFilingService } from '../../bug-filing/types/bug-filing-service';
 import { CreateIssueDetailsTextData } from '../../common/types/create-issue-details-text-data';
 import { BugServiceProperties } from '../../common/types/store-data/user-configuration-store';
 import { ActionAndCancelButtonsComponent } from './action-and-cancel-buttons-component';
+import { NamedSFC } from '../../common/react/named-sfc';
 
 export interface IssueFilingDialogProps {
     deps: IssueFilingDialogDeps;
@@ -15,61 +16,44 @@ export interface IssueFilingDialogProps {
     selectedBugData: CreateIssueDetailsTextData;
     selectedBugFilingServiceData: BugServiceProperties;
     bugFileTelemetryCallback: (ev: React.SyntheticEvent) => void;
+    onClose: (ev: React.SyntheticEvent) => void;
 }
 
 export interface IssueFilingDialogDeps {}
 
-export interface IssueFilingState {
-    isOpen: boolean;
-}
-
 const titleLabel = 'Specify issue filing location';
 
-export class IssueFilingDialog extends React.Component<IssueFilingDialogProps, IssueFilingState> {
-    constructor(props: IssueFilingDialogProps) {
-        super(props);
-        this.state = {
-            isOpen: true,
-        };
-    }
+export const IssueFilingDialog = NamedSFC<IssueFilingDialogProps>('IssueFilingDialog', props => {
+    const { selectedBugFilingService, selectedBugFilingServiceData, selectedBugData, bugFileTelemetryCallback, onClose, isOpen } = props;
 
-    public render(): JSX.Element {
-        const { selectedBugFilingService, selectedBugFilingServiceData, selectedBugData, bugFileTelemetryCallback } = this.props;
-
-        return (
-            <Dialog
-                className={'issue-filing-dialog'}
-                hidden={!this.state.isOpen}
-                dialogContentProps={{
-                    type: DialogType.normal,
-                    title: titleLabel,
-                    titleId: 'issue-filing-dialog-title',
-                    subText: 'This configuration can be changed again in settings.',
-                    subTextId: 'issue-filing-dialog-subtext',
-                    showCloseButton: false,
-                }}
-                modalProps={{
-                    isBlocking: false,
-                    containerClassName: 'insights-dialog-main-override',
-                }}
-            >
-                <DialogFooter>
-                    <ActionAndCancelButtonsComponent
-                        isHidden={false}
-                        primaryButtonDisabled={selectedBugFilingService.isSettingsValid(selectedBugFilingServiceData)}
-                        primaryButtonOnClick={bugFileTelemetryCallback}
-                        cancelButtonOnClick={this.closeDialog}
-                        primaryButtonHref={selectedBugFilingService.createBugFilingUrl(selectedBugFilingServiceData, selectedBugData)}
-                        primaryButtonText={'File issue'}
-                    />
-                </DialogFooter>
-            </Dialog>
-        );
-    }
-
-    private closeDialog = (): void => {
-        this.setState({
-            isOpen: false,
-        });
-    };
-}
+    return (
+        <Dialog
+            className={'issue-filing-dialog'}
+            hidden={!isOpen}
+            dialogContentProps={{
+                type: DialogType.normal,
+                title: titleLabel,
+                titleId: 'issue-filing-dialog-title',
+                subText: 'This configuration can be changed again in settings.',
+                subTextId: 'issue-filing-dialog-subtext',
+                showCloseButton: false,
+            }}
+            modalProps={{
+                isBlocking: false,
+                containerClassName: 'insights-dialog-main-override',
+            }}
+            onDismiss={onClose}
+        >
+            <DialogFooter>
+                <ActionAndCancelButtonsComponent
+                    isHidden={false}
+                    primaryButtonDisabled={selectedBugFilingService.isSettingsValid(selectedBugFilingServiceData)}
+                    primaryButtonOnClick={bugFileTelemetryCallback}
+                    cancelButtonOnClick={onClose}
+                    primaryButtonHref={selectedBugFilingService.createBugFilingUrl(selectedBugFilingServiceData, selectedBugData)}
+                    primaryButtonText={'File issue'}
+                />
+            </DialogFooter>
+        </Dialog>
+    );
+});
--- src/tests/unit/tests/DetailsView/components/issue-filing-dialog.test.tsx
+++ src/tests/unit/tests/DetailsView/components/issue-filing-dialog.test.tsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 import { shallow } from 'enzyme';
 import * as React from 'react';
-import { Mock, MockBehavior, Times } from 'typemoq';
+import { Mock, MockBehavior, Times, IMock } from 'typemoq';
 
 import { BugFilingService } from '../../../../../bug-filing/types/bug-filing-service';
 import { CreateIssueDetailsTextData } from '../../../../../common/types/create-issue-details-text-data';
@@ -12,54 +12,84 @@ import {
     IssueFilingDialogDeps,
     IssueFilingDialogProps,
 } from '../../../../../DetailsView/components/issue-filing-dialog';
-import { EventStubFactory } from '../../../common/event-stub-factory';
+import { EventStubFactory, EventStub } from '../../../common/event-stub-factory';
 
 describe('IssueFilingDialog', () => {
-    it('renders', () => {
-        const eventStub = new EventStubFactory().createMouseClickEvent();
-        const isSettingsValidMock = Mock.ofInstance(data => null, MockBehavior.Strict);
-        const createBugFilingUrlMock = Mock.ofInstance((serviceData, bugData) => null, MockBehavior.Strict);
-        const telemetryCallbackMock = Mock.ofInstance(data => null, MockBehavior.Strict);
-        const selectedBugDataStub = {
+    let eventStub: EventStub;
+    let isSettingsValidMock: IMock<Function>;
+    let createBugFilingUrlMock: IMock<Function>;
+    let onCloseMock: IMock<(ev) => void>;
+    let telemetryCallbackMock: IMock<(ev) => void>;
+    let selectedBugDataStub: CreateIssueDetailsTextData;
+    let selectedServiceData;
+    let deps: IssueFilingDialogDeps;
+    let bugFilingServiceStub: BugFilingService;
+    let props: IssueFilingDialogProps;
+
+    beforeEach(() => {
+        eventStub = new EventStubFactory().createMouseClickEvent();
+        isSettingsValidMock = Mock.ofInstance(data => null, MockBehavior.Strict);
+        onCloseMock = Mock.ofInstance(() => null, MockBehavior.Strict);
+        createBugFilingUrlMock = Mock.ofInstance((serviceData, bugData) => null, MockBehavior.Strict);
+        telemetryCallbackMock = Mock.ofInstance(data => null, MockBehavior.Strict);
+        selectedBugDataStub = {
             pageTitle: 'some pageTitle',
         } as CreateIssueDetailsTextData;
-        const selectedServiceData = {
+        selectedServiceData = {
             someServiceData: null,
         };
-        const deps: IssueFilingDialogDeps = {
+        deps = {
             bugFilingServiceProvider: null,
         };
-        const bugFilingServiceStub = {
+        bugFilingServiceStub = {
             isSettingsValid: isSettingsValidMock.object,
             createBugFilingUrl: createBugFilingUrlMock.object,
         } as BugFilingService;
-        const props: IssueFilingDialogProps = {
+        props = {
             deps,
             isOpen: true,
-            onClose: null,
+            onClose: onCloseMock.object,
             selectedBugFilingService: bugFilingServiceStub,
             selectedBugData: selectedBugDataStub,
             selectedBugFilingServiceData: selectedServiceData,
             bugFileTelemetryCallback: telemetryCallbackMock.object,
         };
 
-        isSettingsValidMock.setup(isSettingsValid => isSettingsValid(selectedServiceData)).verifiable(Times.exactly(2));
-        createBugFilingUrlMock
-            .setup(createBugFilingUrl => createBugFilingUrl(selectedServiceData, selectedBugDataStub))
-            .verifiable(Times.exactly(2));
+        isSettingsValidMock.setup(isSettingsValid => isSettingsValid(selectedServiceData)).verifiable();
+        createBugFilingUrlMock.setup(createBugFilingUrl => createBugFilingUrl(selectedServiceData, selectedBugDataStub)).verifiable();
+    });
+
+    it('render open', () => {
+        const testSubject = shallow(<IssueFilingDialog {...props} />);
+
+        isSettingsValidMock.verifyAll();
+        createBugFilingUrlMock.verifyAll();
+        expect(testSubject.getElement()).toMatchSnapshot();
+    });
+
+    it('render closed', () => {
+        props.isOpen = false;
+        const testSubject = shallow(<IssueFilingDialog {...props} />);
+
+        isSettingsValidMock.verifyAll();
+        createBugFilingUrlMock.verifyAll();
+        expect(testSubject.getElement()).toMatchSnapshot();
+    });
+
+    it('render: validate correct callbacks (file issue on click and cancel/dismiss)', () => {
+        isSettingsValidMock.setup(isSettingsValid => isSettingsValid(selectedServiceData)).verifiable();
+        createBugFilingUrlMock.setup(createBugFilingUrl => createBugFilingUrl(selectedServiceData, selectedBugDataStub)).verifiable();
         telemetryCallbackMock.setup(telemetryCallback => telemetryCallback(eventStub)).verifiable();
+        onCloseMock.setup(onClose => onClose(null)).verifiable();
 
         const testSubject = shallow(<IssueFilingDialog {...props} />);
-        expect(testSubject.getElement()).toMatchSnapshot('Opened Dialog');
-
         const actionCancelButtons = testSubject.find(ActionAndCancelButtonsComponent);
-        actionCancelButtons.prop('primaryButtonOnClick')(eventStub);
         actionCancelButtons.prop('cancelButtonOnClick')(null);
+        actionCancelButtons.prop('primaryButtonOnClick')(eventStub);
 
         isSettingsValidMock.verifyAll();
         createBugFilingUrlMock.verifyAll();
         telemetryCallbackMock.verifyAll();
-
-        expect(testSubject.getElement()).toMatchSnapshot('Closed Dialog');
+        onCloseMock.verifyAll();
     });
 });
