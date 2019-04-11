d02298368bfcb3dc99cbc5a50e313bee3176cdf2
Haonan Liu <haonliu@microsoft.com>
2019-04-10 17:03:26 -0700

    remove unused import

--- src/common/environment-info-provider.ts
+++ src/common/environment-info-provider.ts
@@ -1,7 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { AxeInfo } from './axe-info';
-
 export type EnvironmentInfo = {
     extensionVersion: string;
     browserSpec: string;
