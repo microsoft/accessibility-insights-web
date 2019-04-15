56ad0e7583589b11e3ee341a94233db8f42727d1
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-04-15 12:52:39 -0700

    Revert back null issue filing service (settingForm should not be actually executed, so returning null should be fine)

--- src/bug-filing/null-issue-filing-service/null-issue-filing-service.tsx
+++ src/bug-filing/null-issue-filing-service/null-issue-filing-service.tsx
@@ -1,16 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as React from 'react';
-
 import { NamedSFC } from '../../common/react/named-sfc';
 import { BugFilingService } from '../types/bug-filing-service';
 import { SettingsFormProps } from '../types/settings-form-props';
 
 const nullServiceKey = 'none';
 
-const settingsForm = NamedSFC<SettingsFormProps<{}>>('NullIssueFilingService', () => {
-    return <div>Please select a configuration.</div>;
-});
+const settingsForm = NamedSFC<SettingsFormProps<{}>>('NullIssueFilingService', () => null);
 
 export const NullIssueFilingService: BugFilingService = {
     key: nullServiceKey,
