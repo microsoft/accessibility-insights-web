0feb7260ec90137e982eebfc7b70f3608d829d8f
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-04-16 12:03:09 -0700

    Hook up Azure Boards settings

--- src/bug-filing/azure-boards/azure-boards-settings-form.tsx
+++ src/bug-filing/azure-boards/azure-boards-settings-form.tsx
@@ -44,7 +44,7 @@ export const AzureBoardsSettingsForm = NamedSFC<SettingsFormProps<AzureBoardsBug
             <TextField
                 className="issue-setting"
                 label="Enter the desired Azure Boards project link:"
-                value={props.settings ? props.settings.projectURL : null}
+                value={props.settings ? props.settings.projectURL : ''}
                 placeholder="https://dev.azure.com/org/project"
                 onChange={onProjectURLChange}
             />
--- src/bug-filing/bug-filing-service-provider-impl.ts
+++ src/bug-filing/bug-filing-service-provider-impl.ts
@@ -3,5 +3,10 @@
 import { BugFilingServiceProvider } from './bug-filing-service-provider';
 import { GitHubBugFilingService } from './github/github-bug-filing-service';
 import { NullIssueFilingService } from './null-issue-filing-service/null-issue-filing-service';
+import { AzureBoardsBugFilingService } from './azure-boards/azure-boards-bug-filing-service';
 
-export const BugFilingServiceProviderImpl = new BugFilingServiceProvider([NullIssueFilingService, GitHubBugFilingService]);
+export const BugFilingServiceProviderImpl = new BugFilingServiceProvider([
+    NullIssueFilingService,
+    GitHubBugFilingService,
+    AzureBoardsBugFilingService,
+]);
