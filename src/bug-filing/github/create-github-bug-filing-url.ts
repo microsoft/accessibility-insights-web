baae45b98b6fa574ccd51361888b3fd26986d483
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-04-16 13:47:56 -0700

    Alphabetize imports

--- src/bug-filing/github/create-github-bug-filing-url.ts
+++ src/bug-filing/github/create-github-bug-filing-url.ts
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { getIssueDetailsMarkdown, IssueDetailsGetter } from '../common/get-issue-details-markdown';
 import { EnvironmentInfo } from './../../common/environment-info-provider';
 import { CreateIssueDetailsTextData } from './../../common/types/create-issue-details-text-data';
 import { IssueFilingUrlStringUtils, IssueUrlCreationUtils } from './../common/issue-filing-url-string-utils';
 import { GitHubBugFilingSettings } from './github-bug-filing-service';
-import { IssueDetailsGetter, getIssueDetailsMarkdown } from '../common/get-issue-details-markdown';
 
 function buildTitle(stringUtils: IssueUrlCreationUtils, data: CreateIssueDetailsTextData): string {
     const standardTags = stringUtils.standardizeTags(data);
