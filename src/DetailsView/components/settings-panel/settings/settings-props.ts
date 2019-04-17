83f337da48517972ab1476c3d14f07fb246ed4b6
Haonan Liu <haonliu@microsoft.com>
2019-04-17 15:19:06 -0700

    tslint fix

--- src/DetailsView/components/settings-panel/settings/settings-props.ts
+++ src/DetailsView/components/settings-panel/settings/settings-props.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { EnvironmentInfoProvider } from '../../../../common/environment-info-provider';
 import { UserConfigMessageCreator } from '../../../../common/message-creators/user-config-message-creator';
 import { FeatureFlagStoreData } from '../../../../common/types/store-data/feature-flag-store-data';
 import { UserConfigurationStoreData } from '../../../../common/types/store-data/user-configuration-store';
