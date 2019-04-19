3c34c4e6593fbafed14da40af9cd6e364ae331b6
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-04-19 10:00:28 -0700

    Introduce getIssueDetailsHtml

---
+++ src/bug-filing/common/get-issue-details-html.ts
@@ -0,0 +1,64 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { escape } from 'lodash';
+import { EnvironmentInfo } from '../../common/environment-info-provider';
+import { CreateIssueDetailsTextData } from '../../common/types/create-issue-details-text-data';
+import { IssueUrlCreationUtils } from './issue-filing-url-string-utils';
+
+const buildBodySection = (headingHtml: string, contentHtml: string): string => {
+    return `<div><b>${headingHtml}</b></div><div>${contentHtml}</div><br>`;
+};
+
+const buildIssueDetails = (help: string, helpUrl: string, ruleId: string): string => {
+    const helpEscapedForHtml = escape(help);
+    const ruleIdEscapedForHtml = escape(ruleId);
+
+    return `${helpEscapedForHtml}<br><a href="${helpUrl}">${ruleIdEscapedForHtml}</a>`;
+};
+
+const buildApplicationHtml = (pageTitle: string, pageUrl: string): string => {
+    const pageTitleEscapedForHtml = escape(pageTitle);
+    const pageUrlEscapedForUrl = encodeURI(pageUrl);
+    const pageUrlEscapedForHtml = escape(pageUrl);
+
+    return `<a href="${pageUrlEscapedForUrl}">${pageTitleEscapedForHtml}<br>${pageUrlEscapedForHtml}</a>`;
+};
+
+const buildSnippetHtml = (snippet: string): string => {
+    const maxSnippetLength = 256;
+    let constrainedSnippet = snippet;
+
+    if (snippet.length > maxSnippetLength) {
+        constrainedSnippet = snippet.substr(0, maxSnippetLength) + '...';
+    }
+
+    return escape(constrainedSnippet);
+};
+
+const buildHowToFixHtml = (failureSummary: string): string => {
+    return escape(failureSummary)
+        .replace(/\n  /g, '<br>- ')
+        .replace(/\n /g, '<br> ')
+        .replace(/\n/g, '<br>');
+};
+
+export const getIssueDetailsHtml = (
+    stringUtils: IssueUrlCreationUtils,
+    environmentInfo: EnvironmentInfo,
+    data: CreateIssueDetailsTextData,
+): string => {
+    const body =
+        buildBodySection('Issue Details', buildIssueDetails(data.ruleResult.help, data.ruleResult.helpUrl, data.ruleResult.ruleId)) +
+        buildBodySection('Application', buildApplicationHtml(data.pageTitle, data.pageUrl)) +
+        buildBodySection('Element Path', escape(data.ruleResult.selector)) +
+        buildBodySection('Snippet', buildSnippetHtml(data.ruleResult.html)) +
+        buildBodySection('How to fix', buildHowToFixHtml(data.ruleResult.failureSummary)) +
+        buildBodySection(
+            'To reproduce',
+            'Use <a href="http://aka.ms/AccessibilityInsights">Accessibility Insights for Web</a> to investigate the issue details',
+        ) +
+        buildBodySection('Environment', escape(environmentInfo.browserSpec)) +
+        stringUtils.getFooterContent(environmentInfo);
+
+    return body;
+};
---
+++ src/tests/unit/tests/bug-filing/common/get-issue-details-html.test.ts
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { IMock, Mock } from 'typemoq';
+import { getIssueDetailsHtml } from '../../../../../bug-filing/common/get-issue-details-html';
+import { IssueUrlCreationUtils } from '../../../../../bug-filing/common/issue-filing-url-string-utils';
+import { EnvironmentInfo } from '../../../../../common/environment-info-provider';
+
+describe('getIssueDetailsHtml', () => {
+    let stringUtilsMock: IMock<IssueUrlCreationUtils>;
+
+    const sampleIssueDetailsData = {
+        pageTitle: 'pageTitle<x>',
+        pageUrl: 'pageUrl',
+        ruleResult: {
+            failureSummary: 'RR-failureSummary',
+            guidanceLinks: [{ text: 'WCAG-1.4.1' }, { text: 'wcag-2.8.2' }],
+            help: 'RR-help',
+            html: 'RR-html',
+            ruleId: 'RR-rule-id',
+            helpUrl: 'RR-help-url',
+            selector: 'RR-selector<x>',
+            snippet: 'RR-snippet   space',
+        } as any,
+    };
+
+    const environmentInfo: EnvironmentInfo = {
+        extensionVersion: '1.1.1',
+        axeCoreVersion: '2.2.2',
+        browserSpec: 'test spec',
+    };
+
+    const testObject = getIssueDetailsHtml;
+
+    beforeEach(() => {
+        stringUtilsMock = Mock.ofType<IssueUrlCreationUtils>();
+    });
+
+    it('returns issue details text as html', () => {
+        stringUtilsMock.setup(utils => utils.getFooterContent(environmentInfo)).returns(() => 'test footer content');
+
+        const result = testObject(stringUtilsMock.object, environmentInfo, sampleIssueDetailsData);
+
+        expect(result).toMatchSnapshot();
+    });
+});
