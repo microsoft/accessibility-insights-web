43424bbc096ed355543f4236d9cd13760e7b0b8e
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-04-19 13:18:19 -0700

    Add missing header

--- src/bug-filing/common/issue-details-getter.ts
+++ src/bug-filing/common/issue-details-getter.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 import { EnvironmentInfo } from '../../common/environment-info-provider';
 import { CreateIssueDetailsTextData } from '../../common/types/create-issue-details-text-data';
 import { IssueUrlCreationUtils } from './issue-filing-url-string-utils';
