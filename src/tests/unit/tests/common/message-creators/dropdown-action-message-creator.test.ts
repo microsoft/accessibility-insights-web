6bc660129e2719f5b4e5d2a87ebb56a629844429
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-04-23 15:24:27 -0700

    Dropdown action message creator using action message dispatcher

--- src/tests/unit/tests/common/message-creators/dropdown-action-message-creator.test.ts
+++ src/tests/unit/tests/common/message-creators/dropdown-action-message-creator.test.ts
@@ -1,7 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { IMock, It, Mock, Times } from 'typemoq';
+import { IMock, Mock, Times } from 'typemoq';
 import { Message } from '../../../../../common/message';
+import { ActionMessageDispatcher } from '../../../../../common/message-creators/action-message-dispatcher';
 import { DropdownActionMessageCreator } from '../../../../../common/message-creators/dropdown-action-message-creator';
 import { Messages } from '../../../../../common/messages';
 import { TelemetryDataFactory } from '../../../../../common/telemetry-data-factory';
@@ -11,67 +12,58 @@ import { EventStubFactory } from './../../../common/event-stub-factory';
 describe('DropdownActionMessageCreatorTest', () => {
     const eventStubFactory = new EventStubFactory();
     const testSource: TelemetryEventSource = -1 as TelemetryEventSource;
-    let postMessageMock: IMock<(message) => {}>;
     let telemetryFactoryMock: IMock<TelemetryDataFactory>;
-    let testObject: DropdownActionMessageCreator;
-    const tabId: number = -1;
+    const dispatcherMock = Mock.ofType<ActionMessageDispatcher>();
     let event: any;
     let telemetryData: any;
 
+    let testObject: DropdownActionMessageCreator;
+
     beforeEach(() => {
-        postMessageMock = Mock.ofInstance(message => {
-            return null;
-        });
+        dispatcherMock.reset();
         telemetryFactoryMock = Mock.ofType(TelemetryDataFactory);
         event = eventStubFactory.createMouseClickEvent() as any;
         telemetryData = {
             triggeredBy: 'mouseclick',
             source: testSource,
         };
-        testObject = new DropdownActionMessageCreator(postMessageMock.object, tabId, telemetryFactoryMock.object);
+        testObject = new DropdownActionMessageCreator(telemetryFactoryMock.object, dispatcherMock.object);
     });
 
-    test('openPreviewFeatures', () => {
+    it('dispatches message for openPreviewFeaturesPanel', () => {
         const expectedMessage = getExpectedMessage(Messages.PreviewFeatures.OpenPanel);
 
         setupTelemetryFactoryWithTriggeredByAndSourceCall();
-        setupPostMessage(expectedMessage);
 
         testObject.openPreviewFeaturesPanel(event, testSource);
 
-        postMessageMock.verifyAll();
-        telemetryFactoryMock.verifyAll();
+        dispatcherMock.verify(dispatcher => dispatcher.dispatchMessage(expectedMessage), Times.once());
     });
 
-    test('openSettings', () => {
+    it('dispatches message for openScopingPanel', () => {
+        const expectedMessage = getExpectedMessage(Messages.Scoping.OpenPanel);
+
+        setupTelemetryFactoryWithTriggeredByAndSourceCall();
+
+        testObject.openScopingPanel(event, testSource);
+
+        dispatcherMock.verify(dispatcher => dispatcher.dispatchMessage(expectedMessage), Times.once());
+    });
+
+    it('dispatches message foropenSettingsPanel', () => {
         const expectedMessage = getExpectedMessage(Messages.SettingsPanel.OpenPanel);
         const sourceItem: SettingsOpenSourceItem = 'menu';
         expectedMessage.payload.telemetry.sourceItem = sourceItem;
 
         setupTelemetryFactoryForSettingsPanelOpenCall(sourceItem);
-        setupPostMessage(expectedMessage);
 
         testObject.openSettingsPanel(event, testSource);
 
-        postMessageMock.verifyAll();
-        telemetryFactoryMock.verifyAll();
-    });
-
-    it('dispatch message for openScopingPanel', () => {
-        const expectedMessage = getExpectedMessage(Messages.Scoping.OpenPanel);
-
-        setupTelemetryFactoryWithTriggeredByAndSourceCall();
-        setupPostMessage(expectedMessage);
-
-        testObject.openScopingPanel(event, testSource);
-
-        postMessageMock.verifyAll();
-        telemetryFactoryMock.verifyAll();
+        dispatcherMock.verify(dispatcher => dispatcher.dispatchMessage(expectedMessage), Times.once());
     });
 
     function getExpectedMessage(messageType: string): Message {
         return {
-            tabId: tabId,
             messageType: messageType,
             payload: {
                 telemetry: telemetryData,
@@ -80,10 +72,7 @@ describe('DropdownActionMessageCreatorTest', () => {
     }
 
     function setupTelemetryFactoryWithTriggeredByAndSourceCall(): void {
-        telemetryFactoryMock
-            .setup(tf => tf.withTriggeredByAndSource(event, testSource))
-            .returns(() => telemetryData)
-            .verifiable(Times.once());
+        telemetryFactoryMock.setup(tf => tf.withTriggeredByAndSource(event, testSource)).returns(() => telemetryData);
     }
 
     function setupTelemetryFactoryForSettingsPanelOpenCall(sourceItem: SettingsOpenSourceItem): void {
@@ -91,9 +80,5 @@ describe('DropdownActionMessageCreatorTest', () => {
             .setup(tf => tf.forSettingsPanelOpen(event, testSource, sourceItem))
             .returns(() => telemetryData)
             .verifiable(Times.once());
-    }
-
-    function setupPostMessage(expectedMessage: Message): void {
-        postMessageMock.setup(post => post(It.isValue(expectedMessage))).verifiable(Times.once());
     }
 });
