4098cf7a97afe68fd61dd691576feec0e7697379
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-04-25 12:30:46 -0700

    Add fileIssue API for action message creator

--- src/common/message-creators/bug-action-message-creator.ts
+++ src/common/message-creators/bug-action-message-creator.ts
@@ -1,9 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { BaseActionPayload } from '../../background/actions/action-payloads';
+import { BaseActionPayload, FileIssuePayload } from '../../background/actions/action-payloads';
+import { Message } from '../message';
 import { Messages } from '../messages';
 import { TelemetryDataFactory } from '../telemetry-data-factory';
 import { FILE_ISSUE_CLICK, FileIssueClickService, TelemetryEventSource } from '../telemetry-events';
+import { CreateIssueDetailsTextData } from '../types/create-issue-details-text-data';
 import { ActionMessageDispatcher } from './action-message-dispatcher';
 
 export class BugActionMessageCreator {
@@ -28,5 +30,20 @@ export class BugActionMessageCreator {
     public trackFileIssueClick(event: React.MouseEvent<HTMLElement>, service: FileIssueClickService): void {
         const telemetry = this.telemetryFactory.forFileIssueClick(event, this.source, service);
         this.dispatcher.sendTelemetry(FILE_ISSUE_CLICK, telemetry);
+    }
+
+    public fileIssue(event: React.MouseEvent<HTMLElement>, service: FileIssueClickService, issueData: CreateIssueDetailsTextData): void {
+        const messageType = Messages.IssueFiling.FileIssue;
+        const telemetry = this.telemetryFactory.forFileIssueClick(event, this.source, service);
+        const payload: FileIssuePayload = {
+            telemetry,
+            issueData,
+            service,
+        };
+        const message: Message = {
+            messageType,
+            payload,
+        };
+        this.dispatcher.dispatchMessage(message);
     }
 }
--- src/tests/unit/tests/common/message-creators/bug-action-message-creator.test.ts
+++ src/tests/unit/tests/common/message-creators/bug-action-message-creator.test.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { IMock, Mock, Times } from 'typemoq';
+import { IMock, It, Mock, MockBehavior, Times } from 'typemoq';
+
 import { ActionMessageDispatcher } from '../../../../../common/message-creators/action-message-dispatcher';
 import { BugActionMessageCreator } from '../../../../../common/message-creators/bug-action-message-creator';
 import { Messages } from '../../../../../common/messages';
@@ -13,6 +14,8 @@ import {
     TelemetryEventSource,
     TriggeredBy,
 } from '../../../../../common/telemetry-events';
+import { CreateIssueDetailsTextData } from '../../../../../common/types/create-issue-details-text-data';
+import { DecoratedAxeNodeResult } from '../../../../../injected/scanner-utils';
 import { EventStubFactory } from '../../../common/event-stub-factory';
 
 describe('BugActionMessageCreator', () => {
@@ -30,12 +33,12 @@ describe('BugActionMessageCreator', () => {
 
     beforeEach(() => {
         telemetryFactoryMock = Mock.ofType<TelemetryDataFactory>();
-        dispatcherMock = Mock.ofType<ActionMessageDispatcher>();
+        dispatcherMock = Mock.ofType<ActionMessageDispatcher>(null, MockBehavior.Strict);
 
         testSubject = new BugActionMessageCreator(dispatcherMock.object, telemetryFactoryMock.object, source);
     });
 
-    it('dispatch message for openSettingsPanel', () => {
+    it('dispatches message for openSettingsPanel', () => {
         const telemetry = { ...telemetryStub, sourceItem: 'sourceItem' as SettingsOpenSourceItem };
         telemetryFactoryMock
             .setup(factory => factory.forSettingsPanelOpen(eventStub, source, 'fileIssueSettingsPrompt'))
@@ -57,7 +60,7 @@ describe('BugActionMessageCreator', () => {
         dispatcherMock.verifyAll();
     });
 
-    it('dispatch message for trackFileIssueClick', () => {
+    it('dispatches message for trackFileIssueClick', () => {
         const testService: FileIssueClickService = 'test file issue service' as FileIssueClickService;
         const telemetry = { ...telemetryStub, service: testService };
 
@@ -66,6 +69,44 @@ describe('BugActionMessageCreator', () => {
         dispatcherMock.setup(dispatcher => dispatcher.sendTelemetry(FILE_ISSUE_CLICK, telemetry)).verifiable(Times.once());
 
         testSubject.trackFileIssueClick(eventStub, testService);
+
+        dispatcherMock.verifyAll();
+    });
+
+    it('dispatches message for fileIssue', () => {
+        const testService: FileIssueClickService = 'test file issue service' as FileIssueClickService;
+        const telemetry = { ...telemetryStub, service: testService };
+
+        telemetryFactoryMock.setup(factory => factory.forFileIssueClick(eventStub, source, testService)).returns(() => telemetry);
+        const issueDetailsData: CreateIssueDetailsTextData = {
+            pageTitle: 'pageTitle<x>',
+            pageUrl: 'pageUrl',
+            ruleResult: {
+                failureSummary: 'RR-failureSummary',
+                guidanceLinks: [{ text: 'WCAG-1.4.1' }, { text: 'wcag-2.8.2' }],
+                help: 'RR-help',
+                html: 'RR-html',
+                ruleId: 'RR-rule-id',
+                helpUrl: 'RR-help-url',
+                selector: 'RR-selector<x>',
+                snippet: 'RR-snippet   space',
+            } as DecoratedAxeNodeResult,
+        };
+
+        dispatcherMock.setup(dispatcher =>
+            dispatcher.dispatchMessage(
+                It.isValue({
+                    messageType: Messages.IssueFiling.FileIssue,
+                    payload: {
+                        service: testService,
+                        issueData: issueDetailsData,
+                        telemetry,
+                    },
+                }),
+            ),
+        );
+
+        testSubject.fileIssue(eventStub, testService, issueDetailsData);
 
         dispatcherMock.verifyAll();
     });
