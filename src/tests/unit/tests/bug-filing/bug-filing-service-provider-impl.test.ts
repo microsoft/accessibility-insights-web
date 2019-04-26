c96713ec095c09d626a57984fd0f2b7490153c77
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-04-26 09:16:34 -0700

    tslint fix

--- src/tests/unit/tests/bug-filing/bug-filing-service-provider-impl.test.ts
+++ src/tests/unit/tests/bug-filing/bug-filing-service-provider-impl.test.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { BugFilingServiceProviderImpl } from '../../../../bug-filing/bug-filing-service-provider-impl';
 import { uniq } from 'lodash';
+import { BugFilingServiceProviderImpl } from '../../../../bug-filing/bug-filing-service-provider-impl';
 
 describe('Name of the group', () => {
     it('holds unique key services', () => {
