2c082b6690042d4cda2ab4c396061d06d6354a7d
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-04-29 13:29:05 -0700

    Move azure boards service and test to new folder

--- src/issue-filing/issue-filing-service-provider-impl.ts
+++ src/issue-filing/issue-filing-service-provider-impl.ts
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { AzureBoardsIssueFilingService } from './azure-boards/azure-boards-issue-filing-service';
 import { GitHubIssueFilingService } from './github/github-issue-filing-service';
 import { IssueFilingServiceProvider } from './issue-filing-service-provider';
-import { NullIssueFilingService } from './null-issue-filing-service/null-issue-filing-service';
+import { AzureBoardsIssueFilingService } from './services/azure-boards/azure-boards-issue-filing-service';
+import { NullIssueFilingService } from './services/null-issue-filing-service/null-issue-filing-service';
 
 export const IssueFilingServiceProviderImpl = new IssueFilingServiceProvider([
     NullIssueFilingService,
