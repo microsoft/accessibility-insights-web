48729e4175bb6572fba0219971e7dffab92d24b0
Haonan Liu <haonliu@microsoft.com>
2019-04-30 14:13:29 -0700

    format

--- src/tests/unit/tests/DetailsView/components/switcher.test.tsx
+++ src/tests/unit/tests/DetailsView/components/switcher.test.tsx
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { shallow, mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { Dropdown } from 'office-ui-fabric-react/lib/Dropdown';
 import * as React from 'react';
 import { IMock, Mock, Times } from 'typemoq';
