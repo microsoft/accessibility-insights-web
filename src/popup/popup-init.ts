2587b99be397746c99ae85112a0ceb2a89892544
Wahaj <32555133+waabid@users.noreply.github.com>
2019-04-30 17:24:56 -0700

    Show graceful incompatible message for when popup is opened in older Microsoft Edge browsers (below v75) (#637)
    * initial implementation: popup shows message about incompatible browser in Edge
    
    * Prettier fix
    
    * Added styling specifically for Microsoft Edge browser for the height to fix e2e tests
    
    * Updated message and styling in browser
    
    * Updated text + styling for old Edge messaging
    
    * Fixed tslint issues
    
    * Addressed PR comments: minor test changes, fix class name, set persistence manifest setting to true

--- src/popup/popup-init.ts
+++ src/popup/popup-init.ts
@@ -1,5 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { UAParser } from 'ua-parser-js';
+
 import { ChromeAdapter } from '../background/browser-adapter';
 import { initializeFabricIcons } from '../common/fabric-icons';
 import { UrlParser } from '../common/url-parser';
@@ -11,7 +13,8 @@ initializeFabricIcons();
 const browserAdapter = new ChromeAdapter();
 const urlValidator = new UrlValidator();
 const targetTabFinder = new TargetTabFinder(window, browserAdapter, urlValidator, new UrlParser());
-const popupInitializer: PopupInitializer = new PopupInitializer(browserAdapter, targetTabFinder);
+const userAgentParser = new UAParser(window.navigator.userAgent);
+const popupInitializer: PopupInitializer = new PopupInitializer(browserAdapter, targetTabFinder, userAgentParser.getBrowser());
 
 // tslint:disable-next-line:no-floating-promises - top-level entry points are intentionally floating promises
 popupInitializer.initialize();
