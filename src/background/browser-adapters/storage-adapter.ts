565aca704cee56402a6fd9984fdcd855bd634430
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-02 14:48:03 -0700

    Remove StorageAdapter class

--- src/background/browser-adapters/storage-adapter.ts
+++ src/background/browser-adapters/storage-adapter.ts
@@ -5,17 +5,3 @@ export type StorageAPI = {
     getUserData(keys: string | string[] | Object, callback: (items: { [key: string]: any }) => void): void;
     removeUserData(key: string): void;
 };
-
-export class StorageAdapter implements StorageAPI {
-    public setUserData(items: Object, callback?: () => void): void {
-        chrome.storage.local.set(items, callback);
-    }
-
-    public getUserData(keys: string | string[] | Object, callback: (items: { [key: string]: any }) => void): void {
-        chrome.storage.local.get(keys, callback);
-    }
-
-    public removeUserData(key: string): void {
-        chrome.storage.local.remove(key);
-    }
-}
