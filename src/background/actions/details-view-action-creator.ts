23736101ada9f7c6a26e0c8379818740cb5462c4
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-03 15:44:01 -0700

    Introduce DetailsViewActionCreator

---
+++ src/background/actions/details-view-action-creator.ts
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { RegisterTypeToPayloadCallback } from '../../common/message';
+import { Messages } from '../../common/messages';
+import { SETTINGS_PANEL_CLOSE, SETTINGS_PANEL_OPEN } from '../../common/telemetry-events';
+import { DetailsViewRightContentPanelType } from '../../DetailsView/components/left-nav/details-view-right-content-panel-type';
+import { DetailsViewController } from '../details-view-controller';
+import { TelemetryEventHandler } from '../telemetry/telemetry-event-handler';
+import { BaseActionPayload } from './action-payloads';
+import { DetailsViewActions } from './details-view-actions';
+
+export class DetailsViewActionCreator {
+    constructor(
+        private readonly registerTypeToPayloadCallback: RegisterTypeToPayloadCallback,
+        private readonly detailsViewActions: DetailsViewActions,
+        private readonly detailsViewController: DetailsViewController,
+        private readonly telemetryEventHandler: TelemetryEventHandler,
+    ) {}
+
+    public registerCallback(): void {
+        this.registerTypeToPayloadCallback(Messages.SettingsPanel.OpenPanel, this.onOpenSettingsPanel);
+        this.registerTypeToPayloadCallback(Messages.SettingsPanel.ClosePanel, this.onCloseSettingsPanel);
+        this.registerTypeToPayloadCallback(Messages.Visualizations.DetailsView.GetState, this.onGetDetailsViewCurrentState);
+        this.registerTypeToPayloadCallback(
+            Messages.Visualizations.DetailsView.SetDetailsViewRightContentPanel,
+            this.onSetDetailsViewRightContentPanel,
+        );
+    }
+
+    private onOpenSettingsPanel = (payload: BaseActionPayload, tabId: number): void => {
+        this.detailsViewActions.openSettingsPanel.invoke(null);
+        this.detailsViewController.showDetailsView(tabId);
+        this.telemetryEventHandler.publishTelemetry(SETTINGS_PANEL_OPEN, payload);
+    };
+
+    private onCloseSettingsPanel = (payload: BaseActionPayload): void => {
+        this.detailsViewActions.closeSettingsPanel.invoke(null);
+        this.telemetryEventHandler.publishTelemetry(SETTINGS_PANEL_CLOSE, payload);
+    };
+
+    private onSetDetailsViewRightContentPanel = (payload: DetailsViewRightContentPanelType): void => {
+        this.detailsViewActions.setSelectedDetailsViewRightContentPanel.invoke(payload);
+    };
+
+    private onGetDetailsViewCurrentState = (): void => {
+        this.detailsViewActions.getCurrentState.invoke(null);
+    };
+}
