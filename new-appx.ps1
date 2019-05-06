7b3f354696a63d0aff76cdaf4620c9d95af122c5
Peter Durham <peterdur@microsoft.com>
2019-05-06 16:06:16 -0700

    feat: Add manifest, icons, build script for Microsoft Edge store package (#648)
    * Add APPX manifest
    
    * Point to extension location for icons
    
    * Create APPX from a copy of production extension
    
    * Update manifest to add author required for Edge
    
    * Add blue-on-white icons for store
    
    * Reference new blue-on-white store images
    
    * Use larger logo image
    
    * Restore to dev version number
    
    * Don't check formatting of manifest, build script
    
    * Add copyright header to PowerShell script
    
    * Add copyright header to APPX manifest

---
+++ new-appx.ps1
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+if (Test-Path ./drop/productionEdge) { Remove-Item ./drop/productionEdge -Recurse }
+if (Test-Path ./drop/productionEdge.appx) { Remove-Item ./drop/productionEdge.appx }
+Copy-Item ./drop/production ./drop/productionEdge -Recurse
+Copy-Item ./AppXManifest.xml ./drop/productionEdge/AppXManifest.xml
+&"c:\Program Files (x86)\Windows Kits\10\bin\10.0.17134.0\x64\makeappx.exe" pack /h SHA256 /d .\drop\productionEdge /p .\drop\productionEdge.appx
