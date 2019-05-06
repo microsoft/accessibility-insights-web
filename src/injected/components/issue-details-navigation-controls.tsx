44d4a7fb832edc762ed4dd48e23abd321a7d7f34
Sebastian Morales <smoralesd@users.noreply.github.com>
2019-05-06 15:53:16 -0700

    feat: Hide back & next button for elements with only 1 failure (#652)
    * Introduce IssueDetailsNavigationControls
    
    * Using new component on the details dialog
    
    * Not nav controls if 0 or 1 failures
    
    * Add missing line on header
    
    * Remove extra margin from fix list
    
    * Not rendering back/next button if there is not prev/next failure for the element

---
+++ src/injected/components/issue-details-navigation-controls.tsx
@@ -0,0 +1,63 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import * as React from 'react';
+
+import { FeatureFlags } from '../../common/feature-flags';
+import { NamedSFC } from '../../common/react/named-sfc';
+import { FeatureFlagStoreData } from '../../common/types/store-data/feature-flag-store-data';
+import { DetailsDialog } from './details-dialog';
+
+export type IssueDetailsNavigationClickHandler = {
+    nextButtonClickHandler: (container: DetailsDialog) => void;
+    backButtonClickHandler: (container: DetailsDialog) => void;
+    isBackButtonDisabled: (container: DetailsDialog) => boolean;
+    isNextButtonDisabled: (container: DetailsDialog) => boolean;
+    getFailureInfo: (container: DetailsDialog) => string;
+};
+
+export type IssueDetailsNavigationControlsProps = {
+    container: DetailsDialog;
+    dialogHandler: IssueDetailsNavigationClickHandler;
+    featureFlagStoreData: FeatureFlagStoreData;
+    failuresCount: number;
+};
+
+export const IssueDetailsNavigationControls = NamedSFC<IssueDetailsNavigationControlsProps>('IssueDetailsNavigationControls', props => {
+    const onClickNextButton = event => props.dialogHandler.nextButtonClickHandler(props.container);
+    const onClickBackButton = event => props.dialogHandler.backButtonClickHandler(props.container);
+
+    const getOnClickWhenNotInShadowDom = (func: (ev: any) => void): ((ev: any) => void) => {
+        if (props.featureFlagStoreData[FeatureFlags.shadowDialog]) {
+            return null;
+        } else {
+            return func;
+        }
+    };
+
+    if (props.failuresCount <= 1) {
+        return null;
+    }
+
+    const renderBackButton = () =>
+        !props.dialogHandler.isBackButtonDisabled(props.container) && (
+            <DefaultButton data-automation-id="back" text="< Back" onClick={getOnClickWhenNotInShadowDom(onClickBackButton)} />
+        );
+
+    const renderNextButton = () =>
+        !props.dialogHandler.isNextButtonDisabled(props.container) && (
+            <DefaultButton data-automation-id="next" text="Next >" onClick={getOnClickWhenNotInShadowDom(onClickNextButton)} />
+        );
+
+    return (
+        <div className="ms-Grid insights-dialog-next-and-back-container">
+            <div className="ms-Grid-row">
+                <div className="ms-Grid-col ms-sm3 ms-md3 ms-lg3 insights-dialog-button-left">{renderBackButton()}</div>
+                <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6 insights-dialog-footer">
+                    <div>{props.dialogHandler.getFailureInfo(props.container)}</div>
+                </div>
+                <div className="ms-Grid-col ms-sm3 ms-md3 ms-lg3 insights-dialog-button-right">{renderNextButton()}</div>
+            </div>
+        </div>
+    );
+});
---
+++ src/tests/unit/tests/injected/components/issue-details-navigation-controls.test.tsx
@@ -0,0 +1,156 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import { IMock, Mock, Times } from 'typemoq';
+
+import { FeatureFlags } from '../../../../../common/feature-flags';
+import { DetailsDialog } from '../../../../../injected/components/details-dialog';
+import {
+    IssueDetailsNavigationClickHandler,
+    IssueDetailsNavigationControls,
+    IssueDetailsNavigationControlsProps,
+} from '../../../../../injected/components/issue-details-navigation-controls';
+
+describe('IssueDetailsNavigationControls', () => {
+    let controlProps: IssueDetailsNavigationControlsProps;
+    let navigationHandlerMock: IMock<IssueDetailsNavigationClickHandler>;
+
+    beforeEach(() => {
+        navigationHandlerMock = Mock.ofType<IssueDetailsNavigationClickHandler>();
+        controlProps = {
+            container: Mock.ofType<DetailsDialog>().object,
+            dialogHandler: navigationHandlerMock.object,
+            featureFlagStoreData: {},
+            failuresCount: 5,
+        };
+    });
+
+    describe('render', () => {
+        type ButtonStateTestCase = {
+            backButtonDisabled: boolean;
+            nextButtonDisabled: boolean;
+        };
+
+        const testCases: ButtonStateTestCase[] = [
+            {
+                backButtonDisabled: true,
+                nextButtonDisabled: true,
+            },
+            {
+                backButtonDisabled: true,
+                nextButtonDisabled: false,
+            },
+            {
+                backButtonDisabled: false,
+                nextButtonDisabled: true,
+            },
+            {
+                backButtonDisabled: false,
+                nextButtonDisabled: false,
+            },
+        ];
+
+        it.each(testCases)('handles button states: %o', testCase => {
+            navigationHandlerMock
+                .setup(handler => handler.isBackButtonDisabled(controlProps.container))
+                .returns(() => testCase.backButtonDisabled);
+
+            navigationHandlerMock
+                .setup(handler => handler.isNextButtonDisabled(controlProps.container))
+                .returns(() => testCase.nextButtonDisabled);
+
+            const wrapper = shallow(<IssueDetailsNavigationControls {...controlProps} />);
+
+            expect(wrapper.getElement()).toMatchSnapshot();
+        });
+
+        describe('handles failures count', () => {
+            it('0 failures', () => {
+                controlProps.failuresCount = 0;
+
+                const wrapper = shallow(<IssueDetailsNavigationControls {...controlProps} />);
+
+                expect(wrapper.getElement()).toMatchSnapshot();
+            });
+
+            it('1 failure', () => {
+                controlProps.failuresCount = 1;
+
+                const wrapper = shallow(<IssueDetailsNavigationControls {...controlProps} />);
+
+                expect(wrapper.getElement()).toMatchSnapshot();
+            });
+
+            it('multiple failures', () => {
+                controlProps.failuresCount = 5;
+
+                const wrapper = shallow(<IssueDetailsNavigationControls {...controlProps} />);
+
+                expect(wrapper.getElement()).toMatchSnapshot();
+            });
+        });
+    });
+
+    describe('user interaction', () => {
+        it('handles next button activation', () => {
+            const wrapper = shallow(<IssueDetailsNavigationControls {...controlProps} />);
+
+            const nextButton = wrapper.find({ 'data-automation-id': 'next' });
+
+            expect(nextButton).not.toBeNull();
+            expect(nextButton).toHaveLength(1);
+
+            nextButton.simulate('click');
+
+            navigationHandlerMock.verify(handler => handler.nextButtonClickHandler(controlProps.container), Times.once());
+        });
+
+        it('handles next button activation, shadow dom on', () => {
+            controlProps.featureFlagStoreData = {
+                [FeatureFlags.shadowDialog]: true,
+            };
+
+            const wrapper = shallow(<IssueDetailsNavigationControls {...controlProps} />);
+
+            const nextButton = wrapper.find({ 'data-automation-id': 'next' });
+
+            expect(nextButton).not.toBeNull();
+            expect(nextButton).toHaveLength(1);
+
+            nextButton.simulate('click');
+
+            navigationHandlerMock.verify(handler => handler.nextButtonClickHandler(controlProps.container), Times.never());
+        });
+
+        it('handles back button activation', () => {
+            const wrapper = shallow(<IssueDetailsNavigationControls {...controlProps} />);
+
+            const backButton = wrapper.find({ 'data-automation-id': 'back' });
+
+            expect(backButton).not.toBeNull();
+            expect(backButton).toHaveLength(1);
+
+            backButton.simulate('click');
+
+            navigationHandlerMock.verify(handler => handler.backButtonClickHandler(controlProps.container), Times.once());
+        });
+
+        it('handles back button activation, shadow dom on', () => {
+            controlProps.featureFlagStoreData = {
+                [FeatureFlags.shadowDialog]: true,
+            };
+
+            const wrapper = shallow(<IssueDetailsNavigationControls {...controlProps} />);
+
+            const backButton = wrapper.find({ 'data-automation-id': 'back' });
+
+            expect(backButton).not.toBeNull();
+            expect(backButton).toHaveLength(1);
+
+            backButton.simulate('click');
+
+            navigationHandlerMock.verify(handler => handler.backButtonClickHandler(controlProps.container), Times.never());
+        });
+    });
+});
