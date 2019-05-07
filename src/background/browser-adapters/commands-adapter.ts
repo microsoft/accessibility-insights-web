bb2caec08835a29d96c9084161e1dfcd46fc25d9
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-06 17:05:00 -0700

    Introduce CommandsAdapter

---
+++ src/background/browser-adapters/commands-adapter.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+export type CommandsAdapter = {
+    addCommandListener(callback: (command: string) => void): void;
+    getCommands(callback: (commands: chrome.commands.Command[]) => void): void;
+};
