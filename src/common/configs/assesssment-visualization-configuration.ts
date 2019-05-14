f5330819127b403e40a7a71593f61d4c6c1774e4
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-14 14:29:47 -0700

    add file header

--- src/common/configs/assesssment-visualization-configuration.ts
+++ src/common/configs/assesssment-visualization-configuration.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 import { ToggleActionPayload } from '../../background/actions/action-payloads';
 import { UniquelyIdentifiableInstances } from '../../background/instance-identifier-generator';
 import { TestViewProps } from '../../DetailsView/components/test-view';
