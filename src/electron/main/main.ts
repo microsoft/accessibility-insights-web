1f2500f95dbd8b22a30f7a00713456cd117c6de1
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-20 12:48:47 -0700

    loading main.html file
    (cherry picked from commit f821acf2bab217d7659f34db3c1cea39e5adfad6)

--- src/electron/main/main.ts
+++ src/electron/main/main.ts
@@ -1,14 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { app, BrowserWindow } from 'electron';
+import { join } from 'path';
 
 let mainWindow: BrowserWindow;
 
 const createWindow = () => {
     mainWindow = new BrowserWindow({ show: false });
 
+    const mainPath = join(__dirname, '../electron/main/main.html');
+
     mainWindow
-        .loadURL('https://ada-cat.github.io/AU/before.html')
+        .loadFile(mainPath)
         .then(() => console.log('url loaded'))
         .catch(console.log);
 
