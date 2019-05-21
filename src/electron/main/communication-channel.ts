6a868e7255b8218b5855a047332fa98f09288ce8
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-21 10:21:45 -0700

    Make communication channel exported const
    (cherry picked from commit dbc490cb13215a2177b52c5d5aa844cc3e53036e)

---
+++ src/electron/main/communication-channel.ts
@@ -0,0 +1,4 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+export const fromBackgroundChannel = 'from-background';
+export const fromDetailsViewChannel = 'from-details-view';
