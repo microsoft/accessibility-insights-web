cad5d6e64de8a17dabfa7a93fac3f916c46404f2
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-21 13:19:14 -0700

    Loading target page side by side with details view
    (cherry picked from commit 197cad758f601c408b11b34756ef493d4ee7b87f)

--- src/electron/main/main.ts
+++ src/electron/main/main.ts
@@ -1,20 +1,33 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { app, BrowserWindow, ipcMain } from 'electron';
+import { app, BrowserWindow, ipcMain, screen } from 'electron';
 import { join } from 'path';
 import { fromBackgroundChannel, fromDetailsViewChannel } from './communication-channel';
 
+type WindowBounds = {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+};
+
+const defaultBounds: WindowBounds = {
+    x: 0,
+    y: 0,
+    width: 800,
+    height: 600,
+};
+
 let detailsViewWindow: BrowserWindow;
 
-const createDetailsViewWindow = () => {
-    detailsViewWindow = new BrowserWindow({ show: false, webPreferences: { nodeIntegration: true } });
+const createDetailsViewWindow = (windowBounds: WindowBounds = defaultBounds) => {
+    detailsViewWindow = new BrowserWindow({ show: false, webPreferences: { nodeIntegration: true }, ...windowBounds });
 
     const detailsViewPath = join(__dirname, '../DetailsView/detailsView.html');
 
     detailsViewWindow.loadFile(detailsViewPath).catch(console.log);
 
     detailsViewWindow.on('ready-to-show', () => {
-        detailsViewWindow.maximize();
         detailsViewWindow.show();
 
         detailsViewWindow.webContents.openDevTools({ mode: 'bottom' });
@@ -23,18 +36,31 @@ const createDetailsViewWindow = () => {
 
 let backgroundWindow: BrowserWindow;
 
-const createBackgroundWindow = () => {
-    backgroundWindow = new BrowserWindow({ show: false, webPreferences: { nodeIntegration: true } });
+const createBackgroundWindow = (windowBounds: WindowBounds = defaultBounds) => {
+    backgroundWindow = new BrowserWindow({ show: false, webPreferences: { nodeIntegration: true }, ...windowBounds });
 
     const backgroundPath = join(__dirname, '../background/background.html');
 
     backgroundWindow.loadFile(backgroundPath).catch(console.log);
 
     backgroundWindow.on('ready-to-show', () => {
-        backgroundWindow.maximize();
         backgroundWindow.show();
 
         backgroundWindow.webContents.openDevTools({ mode: 'bottom' });
+    });
+};
+
+let targetPageWindow: BrowserWindow;
+
+const createTargetPageWindow = (windowBounds: WindowBounds = defaultBounds) => {
+    targetPageWindow = new BrowserWindow({ show: false, ...windowBounds });
+
+    const targetPageUrl = 'https://ada-cat.github.io/AU/before.html';
+
+    targetPageWindow.loadURL(targetPageUrl);
+
+    targetPageWindow.on('ready-to-show', () => {
+        targetPageWindow.show();
     });
 };
 
@@ -43,6 +69,7 @@ const setupCommunication = () => {
         console.log('on main', { channel: fromDetailsViewChannel, argsZero: args[0] });
         backgroundWindow.webContents.send(fromDetailsViewChannel, args);
     });
+
     ipcMain.on(fromBackgroundChannel, (event, ...args) => {
         console.log('on main', { channel: fromBackgroundChannel, argsZero: args[0] });
         detailsViewWindow.webContents.send(fromBackgroundChannel, args);
@@ -50,7 +77,34 @@ const setupCommunication = () => {
 };
 
 app.on('ready', () => {
+    const displays = screen.getAllDisplays();
+
+    const firstDisplay = displays[0];
+
+    const fullWidth = firstDisplay.workAreaSize.width;
+    const fullHeight = firstDisplay.workAreaSize.height;
+
+    const portion = 2;
+
+    const fractionWidth = Math.floor(fullWidth / portion);
+
+    const targetPageBounds: WindowBounds = {
+        x: 0,
+        y: 0,
+        height: fullHeight,
+        width: fractionWidth,
+    };
+
+    const detailsViewBounds: WindowBounds = {
+        x: fractionWidth,
+        y: 0,
+        height: fullHeight,
+        width: Math.ceil(fullWidth - fractionWidth),
+    };
+
     createBackgroundWindow();
-    createDetailsViewWindow();
+    createDetailsViewWindow(detailsViewBounds);
+    createTargetPageWindow(targetPageBounds);
+
     setupCommunication();
 });
