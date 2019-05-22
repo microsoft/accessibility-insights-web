6169f9ab1cb4871d6687eba8a714014d9eabff82
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-22 10:14:20 -0700

    Fix async behaviour on injectJs
    (cherry picked from commit b73250f8e624bb75a5fabdbed2c54a70eb8c9ad8)

--- src/electron/main/communication-channel.ts
+++ src/electron/main/communication-channel.ts
@@ -4,4 +4,5 @@ export const fromBackgroundChannel = 'from-background';
 export const fromDetailsViewChannel = 'from-details-view';
 
 export const injectJsChannel = 'inject-js';
+export const jsInjectionCompleted = 'js-injection-completed';
 export const injectCssChannel = 'inject-css';
