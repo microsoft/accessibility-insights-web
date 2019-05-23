ddb8dbb621b8b3ca6f665fc0b6b13cc4b78e772b
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-22 17:10:47 -0700

    Add content url decorator and implementations
    (cherry picked from commit d7a3b322db2c5f51e828105c04b02f988aa47f0a)

---
+++ src/views/content/url-decorator/null-url-decorator.ts
@@ -0,0 +1,4 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { ContentUrlDecorator } from './content-url-decorator';
+export const NullUrlDecorator: ContentUrlDecorator = (url: string) => url;
