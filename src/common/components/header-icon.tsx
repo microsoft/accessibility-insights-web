a0ea56b42e016de348faac932e331a5ee7abeffc
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-23 14:51:31 -0700

    Using icon component instead of img with src attribute

--- src/common/components/header-icon.tsx
+++ src/common/components/header-icon.tsx
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 import * as React from 'react';
 
+import { BrandBlue } from '../../icons/brand/blue/brand-blue';
+import { BrandWhite } from '../../icons/brand/white/brand-white';
 import { NamedSFC } from '../react/named-sfc';
 import { ThemeInnerState } from './theme';
 import { withStoreSubscription, WithStoreSubscriptionDeps, WithStoreSubscriptionProps } from './with-store-subscription';
@@ -13,12 +15,10 @@ export type HeaderIconProps = WithStoreSubscriptionProps<HeaderIconState>;
 export type HeaderIconState = ThemeInnerState;
 
 export const HeaderIconComponent = NamedSFC<HeaderIconProps>('HeaderIconComponent', (props: HeaderIconProps) => {
-    const WHITE_ICON = '../../icons/brand/white/brand-white-48px.png';
-    const BLUE_ICON = '../../icons/brand/blue/brand-blue-48px.png';
     const state = props.storeState.userConfigurationStoreData;
     const enableHighContrast = state && state.enableHighContrast;
-    const iconPath = enableHighContrast ? BLUE_ICON : WHITE_ICON;
-    return <img className="header-icon" src={iconPath} alt="" />;
+
+    return enableHighContrast ? <BrandBlue /> : <BrandWhite />;
 });
 
 export const HeaderIcon = withStoreSubscription<HeaderIconProps, HeaderIconState>(HeaderIconComponent);
