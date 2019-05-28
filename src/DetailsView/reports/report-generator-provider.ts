e45499976d6b2a248ae47912d1ed0c90f28c91ea
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-28 10:20:29 -0700

    Support feature flag on the provider

--- src/DetailsView/reports/report-generator-provider.ts
+++ src/DetailsView/reports/report-generator-provider.ts
@@ -1,10 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { BaseStore } from '../../common/base-store';
+import { FeatureFlags } from '../../common/feature-flags';
 import { FeatureFlagStoreData } from '../../common/types/store-data/feature-flag-store-data';
 import { AssessmentReportHtmlGenerator } from './assessment-report-html-generator';
 import { ReportGenerator } from './report-generator';
 import { ReportGeneratorV1 } from './report-generator-v1';
+import { ReportGeneratorV2 } from './report-generator-v2';
 import { ReportHtmlGenerator } from './report-html-generator';
 import { ReportNameGenerator } from './report-name-generator';
 
@@ -19,6 +21,12 @@ export const createReportGeneratorProvider = (
     featureFlagStore: BaseStore<FeatureFlagStoreData>,
 ): ReportGeneratorProvider => {
     const getGenerator = () => {
+        const featureFlag = featureFlagStore.getState();
+
+        if (featureFlag[FeatureFlags.newAutomatedChecksReport]) {
+            return new ReportGeneratorV2(reportNameGenerator, reportHtmlGenerator, assessmentReportHtmlGenerator);
+        }
+
         return new ReportGeneratorV1(reportNameGenerator, reportHtmlGenerator, assessmentReportHtmlGenerator);
     };
 
