99421bb10a0440e92c124e85cefb1d2e497e984a
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-29 09:52:23 -0700

    Introduce new ReportBody using half abstract factory for the sections

---
+++ src/DetailsView/reports/components/report-sections/automated-checks-report-section-factory.tsx
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { EnvironmentInfo } from '../../../../common/environment-info-provider';
+import { ReactSFCWithDisplayName } from '../../../../common/react/named-sfc';
+import { RuleResult, ScanResults } from '../../../../scanner/iruleresults';
+
+export type SummaryProps = {
+    scanResult: ScanResults;
+};
+
+export type DetailsProps = {
+    pageTitle: string;
+    pageUrl: string;
+    description: string;
+    scanDate: Date;
+    environmentInfo: EnvironmentInfo;
+};
+
+export type CheckListProps = {
+    results: RuleResult[];
+    idPrefix: string;
+    showInstanceCount: boolean;
+    showInstances: boolean;
+    congratulateIfEmpty: boolean;
+};
+
+export type AutomatedChecksReportSectionFactory = {
+    BodySection: ReactSFCWithDisplayName;
+    Header: ReactSFCWithDisplayName;
+    Title: ReactSFCWithDisplayName;
+    Summary: ReactSFCWithDisplayName<SummaryProps>;
+    Details: ReactSFCWithDisplayName<DetailsProps>;
+    ResultSection: ReactSFCWithDisplayName;
+    FailedInstances: ReactSFCWithDisplayName<CheckListProps>;
+    PassedChecks: ReactSFCWithDisplayName<CheckListProps>;
+    NotApplicableChecks: ReactSFCWithDisplayName<CheckListProps>;
+    Footer: ReactSFCWithDisplayName;
+};
---
+++ src/tests/unit/tests/DetailsView/reports/components/report-sections/report-body.test.tsx
@@ -0,0 +1,95 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import { NamedSFC } from '../../../../../../../common/react/named-sfc';
+import {
+    AutomatedChecksReportSectionFactory,
+    CheckListProps,
+    DetailsProps,
+    SummaryProps,
+} from '../../../../../../../DetailsView/reports/components/report-sections/automated-checks-report-section-factory';
+import { ReportBody, ReportBodyProps } from '../../../../../../../DetailsView/reports/components/report-sections/report-body';
+
+describe('ReportBody', () => {
+    it('renders', () => {
+        const pageTitle = 'page-title';
+        const pageUrl = 'url:target-page';
+
+        const summaryProps: SummaryProps = {
+            scanResult: {
+                passes: [],
+                violations: [],
+                inapplicable: [],
+                incomplete: [],
+                timestamp: 'today',
+                targetPageTitle: pageTitle,
+                targetPageUrl: pageUrl,
+            },
+        };
+        const detailsProps: DetailsProps = {
+            pageTitle,
+            pageUrl,
+            description: 'test description',
+            scanDate: new Date(2019, 5, 29, 9, 47, 0, 0),
+            environmentInfo: {
+                axeCoreVersion: 'axe-core-version',
+                browserSpec: 'browser-spec',
+                extensionVersion: 'extension-version',
+            },
+        };
+
+        const checkListProps: CheckListProps = {
+            congratulateIfEmpty: true,
+            idPrefix: 'prefix',
+            results: [],
+            showInstanceCount: true,
+            showInstances: true,
+        };
+
+        const props: ReportBodyProps = {
+            sectionFactory: createSectionFactoryStub(),
+            ...summaryProps,
+            ...detailsProps,
+            ...checkListProps,
+        };
+
+        const wrapper = shallow(<ReportBody {...props} />);
+
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+
+    const createSectionFactoryStub = () => {
+        const createBasicComponent = (name: string) => {
+            return NamedSFC(name, () => {
+                return <div />;
+            });
+        };
+
+        const BodySection = createBasicComponent('body-section');
+        const Header = createBasicComponent('header-section');
+        const Title = createBasicComponent('title-section');
+        const Summary = createBasicComponent('summary-section');
+        const Details = createBasicComponent('details-section');
+        const ResultSection = createBasicComponent('result-section');
+        const FailedInstances = createBasicComponent('failed-instances-section');
+        const PassedChecks = createBasicComponent('passed-checks-section');
+        const NotApplicableChecks = createBasicComponent('not-applicable-checks-section');
+        const Footer = createBasicComponent('footer-section');
+
+        const sectionFactoryStub: AutomatedChecksReportSectionFactory = {
+            BodySection: BodySection,
+            Header: Header,
+            Title: Title,
+            Summary: Summary,
+            Details: Details,
+            ResultSection: ResultSection,
+            FailedInstances: FailedInstances,
+            PassedChecks: PassedChecks,
+            NotApplicableChecks: NotApplicableChecks,
+            Footer: Footer,
+        };
+
+        return sectionFactoryStub;
+    };
+});
