98988c061cb3cde2991948a887fe14813000a5bb
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-29 11:30:59 -0700

    Add title section
    (cherry picked from commit c1912413be3ff0299954d90bac0d32807bc0b983)

---
+++ src/DetailsView/reports/components/report-sections/title-section.tsx
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import * as React from 'react';
+import { NamedSFC } from '../../../../common/react/named-sfc';
+
+export const TitleSection = NamedSFC('TitleSection', () => {
+    return <h1>Automated checks results</h1>;
+});
