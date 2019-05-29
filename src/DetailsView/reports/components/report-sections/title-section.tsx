2554cc5e2e46cd526b3c5f9271e060bedcce868d
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-29 13:34:26 -0700

    add classname to title section
    (cherry picked from commit 558ef7f2cd8b7c0b15f0cc8acc901942ff9b0b78)

--- src/DetailsView/reports/components/report-sections/title-section.tsx
+++ src/DetailsView/reports/components/report-sections/title-section.tsx
@@ -4,5 +4,9 @@ import * as React from 'react';
 import { NamedSFC } from '../../../../common/react/named-sfc';
 
 export const TitleSection = NamedSFC('TitleSection', () => {
-    return <h1>Automated checks results</h1>;
+    return (
+        <div className="title-section">
+            <h1>Automated checks results</h1>
+        </div>
+    );
 });
