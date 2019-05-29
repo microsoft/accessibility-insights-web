b3d254c8dbc3276bae2c12fac691f189e0912974
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-29 14:39:56 -0700

    Add details section
    (cherry picked from commit 9a4ec69f0e13e0a55d732d0716769893eaa5a1de)

--- src/DetailsView/reports/components/report-sections/automated-checks-report-section-factory.tsx
+++ src/DetailsView/reports/components/report-sections/automated-checks-report-section-factory.tsx
@@ -1,8 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as React from 'react';
+
 import { NamedSFC } from '../../../../common/react/named-sfc';
+import { BodySection } from './body-section';
+import { DetailsSection } from './details-section';
+import { FooterSection } from './footer-section';
+import { HeaderSection } from './header-section';
 import { ReportSectionFactory } from './report-section-factory';
+import { TitleSection } from './title-section';
 
 const createBasicComponent = (name: string) => {
     return NamedSFC(name, () => {
@@ -16,27 +22,22 @@ const createWrappingComponent = (name: string) => {
     });
 };
 
-const BodySection = createWrappingComponent('body-section');
-const Header = createBasicComponent('header-section');
-const Title = createBasicComponent('title-section');
 const Summary = createBasicComponent('summary-section');
-const Details = createBasicComponent('details-section');
 const ResultSection = createWrappingComponent('result-section');
 const FailedInstances = createBasicComponent('failed-instances-section');
 const PassedChecks = createBasicComponent('passed-checks-section');
 const NotApplicableChecks = createBasicComponent('not-applicable-checks-section');
-const Footer = createBasicComponent('footer-section');
 
 // TODO most of this sections are dummy sections, the point is to replace them as we develop them
 export const AutomatedChecksReportSectionFactory: ReportSectionFactory = {
     BodySection,
-    Header,
-    Title,
+    Header: HeaderSection,
+    Title: TitleSection,
     Summary,
-    Details,
+    Details: DetailsSection,
     ResultSection,
     FailedInstances,
     PassedChecks,
     NotApplicableChecks,
-    Footer,
+    Footer: FooterSection,
 };
