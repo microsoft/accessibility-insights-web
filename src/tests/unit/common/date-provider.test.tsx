5faeae380677a831d9508065e88f0717ae30cdb2
Haonan Liu <haonliu@microsoft.com>
2019-05-30 17:12:37 -0700

    add line

--- src/tests/unit/common/date-provider.test.tsx
+++ src/tests/unit/common/date-provider.test.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { DateProvider } from '../../../common/date-provider';
+
 describe('DateProviderTest', () => {
     test('getDateFromTimestamp', () => {
         const timestamp = 'Thu May 30 2019 16:57:54 GMT-0700 (Pacific Daylight Time)';
