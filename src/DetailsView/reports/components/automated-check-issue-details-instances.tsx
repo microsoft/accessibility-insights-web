51491ca183804a8479c6e42afe78eecac398206a
Shobhit Agarwal <shoagarw@microsoft.com>
2019-06-03 11:54:02 -0700

    interim work on the styling for the issue details card

--- src/DetailsView/reports/components/automated-check-issue-details-instances.tsx
+++ src/DetailsView/reports/components/automated-check-issue-details-instances.tsx
@@ -14,19 +14,19 @@ export const AutomatedChecksIssueDetailsInstances = NamedSFC<AutomatedChecksIssu
         const rows: JSX.Element[] = [];
         nodeResults.forEach((nodeResult, index) => {
             rows.push(
-                <tr className="path-row" key={`path-row-${index}`}>
+                <tr className="row" key={`path-row-${index}`}>
                     <td className="label">Path</td>
                     <td className="content">{nodeResult.selector}</td>
                 </tr>,
             );
             rows.push(
-                <tr className="snippet-row" key={`snippet-row-${index}`}>
+                <tr className="row" key={`snippet-row-${index}`}>
                     <td className="label">Snippet</td>
                     <td className="content">{nodeResult.snippet}</td>
                 </tr>,
             );
             rows.push(
-                <tr className="snippet-row" key={`how-to-fix-row-${index}`}>
+                <tr className="row" key={`how-to-fix-row-${index}`}>
                     <td className="label">How to fix</td>
                     <td className="content">{nodeResult.failureSummary}</td>
                 </tr>,
