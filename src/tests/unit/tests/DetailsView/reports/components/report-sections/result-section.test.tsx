327b16d7f99bb5f88db5cf77a95504ad604506a0
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-06-04 12:29:15 -0700

    Add result section component

---
+++ src/tests/unit/tests/DetailsView/reports/components/report-sections/result-section.test.tsx
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+import { ResultSection, ResultSectionProps } from '../../../../../../../DetailsView/reports/components/report-sections/result-section';
+import { RuleResult } from '../../../../../../../scanner/iruleresults';
+
+describe('PassedChecksSection', () => {
+    it('renders', () => {
+        const props: ResultSectionProps = {
+            title: 'result section title',
+            containerClassName: 'result-section-class-name',
+            rules: [{} as RuleResult, {} as RuleResult],
+            outcomeType: 'pass',
+        };
+
+        const wrapper = shallow(<ResultSection {...props} />);
+
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+});
