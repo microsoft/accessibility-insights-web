adee158007ac8b0c841efacb15a66e8f4a981c99
Wahaj Abid <waabid@microsoft.com>
2019-06-06 12:33:11 -0700

    Added help link to Overview Help section

--- src/tests/unit/tests/DetailsView/components/overview-content/overview-content-container.test.tsx
+++ src/tests/unit/tests/DetailsView/components/overview-content/overview-content-container.test.tsx
@@ -39,6 +39,7 @@ describe('OverviewContainer', () => {
     const filteredProvider = {} as AssessmentsProvider;
     const detailsViewActionMessageCreatorStub = {} as DetailsViewActionMessageCreator;
     const assessmentsProviderWithFeaturesEnabledMock = Mock.ofInstance((provider, featureFlagData) => null, MockBehavior.Strict);
+
     const getAssessmentSummaryModelFromProviderAndStoreData = jest.fn();
 
     const deps: OverviewContainerDeps = {
