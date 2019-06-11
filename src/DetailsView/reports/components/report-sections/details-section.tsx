d0bb938996ead1f85f56bfcc34271ac07d49f77b
Shobhit Agarwal <shoagarw@microsoft.com>
2019-06-11 13:55:54 -0700

    add a11y screen reader capabiliites to the details section

--- src/DetailsView/reports/components/report-sections/details-section.tsx
+++ src/DetailsView/reports/components/report-sections/details-section.tsx
@@ -12,6 +12,12 @@ export type DetailsSectionProps = Pick<SectionProps, 'pageUrl' | 'description' |
 
 export const DetailsSection = NamedSFC<DetailsSectionProps>('DetailsSection', props => {
     const { pageUrl, description, scanDate, toUtcString } = props;
+    const scanDateUTC: string = toUtcString(scanDate);
+    const screenReaderTexts = {
+        targetPageLink: `Target Page: ${pageUrl}`,
+        scanDate: `Scan date: ${scanDateUTC}`,
+        comment: `Comment: ${description}`,
+    };
     return (
         <div className="scan-details-section">
             <h2>Scan details</h2>
@@ -21,7 +27,10 @@ export const DetailsSection = NamedSFC<DetailsSectionProps>('DetailsSection', pr
                         <td className="icon" aria-hidden="true">
                             <UrlIcon />
                         </td>
-                        <td className="text">
+                        <span className="screen-reader-only" id="target-page-text">
+                            {screenReaderTexts.targetPageLink}
+                        </span>
+                        <td className="text" aria-labelledby="target-page-text">
                             <NewTabLink href={pageUrl} title="Navigate to target page">
                                 {pageUrl}
                             </NewTabLink>
@@ -31,13 +40,23 @@ export const DetailsSection = NamedSFC<DetailsSectionProps>('DetailsSection', pr
                         <td className="icon" aria-hidden="true">
                             <DateIcon />
                         </td>
-                        <td className="text">{toUtcString(scanDate)}</td>
+                        <span className="screen-reader-only" id="scan-date-text">
+                            {screenReaderTexts.scanDate}
+                        </span>
+                        <td className="text" aria-labelledby="scan-date-text">
+                            {scanDateUTC}
+                        </td>
                     </tr>
                     <tr>
                         <td className="icon" aria-hidden="true">
                             <CommentIcon />
                         </td>
-                        <td className="text description-text">{description}</td>
+                        <span className="screen-reader-only" id="comment-text">
+                            {screenReaderTexts.comment}
+                        </span>
+                        <td className="text description-text" aria-labelledby="comment-text">
+                            {description}
+                        </td>
                     </tr>
                 </tbody>
             </table>
