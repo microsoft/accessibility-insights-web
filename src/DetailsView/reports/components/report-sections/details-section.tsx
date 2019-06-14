c5d2e0e5055b9b81849e7b676932055a5ce107b2
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-06-14 09:43:19 -0700

    clean up component

--- src/DetailsView/reports/components/report-sections/details-section.tsx
+++ src/DetailsView/reports/components/report-sections/details-section.tsx
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { css } from '@uifabric/utilities';
 import * as React from 'react';
+
 import { NewTabLink } from '../../../../common/components/new-tab-link';
 import { CommentIcon } from '../../../../common/icons/comment-icon';
 import { DateIcon } from '../../../../common/icons/date-icon';
@@ -10,53 +12,29 @@ import { SectionProps } from './report-section-factory';
 
 export type DetailsSectionProps = Pick<SectionProps, 'pageUrl' | 'description' | 'scanDate' | 'toUtcString'>;
 
-const createCommentRow = (description: string, screenReaderText: string) => (
-    <li>
-        <span className="icon" aria-hidden="true">
-            <CommentIcon />
-        </span>
-        <span className="screen-reader-only" id="comment-text">
-            {screenReaderText}
-        </span>
-        <span className="text description-text" aria-labelledby="comment-text" aria-hidden="true">
-            {description}
-        </span>
-    </li>
-);
-
 export const DetailsSection = NamedSFC<DetailsSectionProps>('DetailsSection', props => {
     const { pageUrl, description, scanDate, toUtcString } = props;
+
+    const createListItem = (icon: JSX.Element, label: string, content: string | JSX.Element, contentClassName?: string) => (
+        <li>
+            <span className="icon" aria-hidden="true">
+                {icon}
+            </span>
+            <span className="screen-reader-only">{label}</span>
+            <span className={css('text', contentClassName)}>{content}</span>
+        </li>
+    );
+
     const scanDateUTC: string = toUtcString(scanDate);
-    const screenReaderTexts = {
-        scanDate: `Scan date: ${scanDateUTC}`,
-        comment: `Comment: ${description}`,
-    };
     const showCommentRow = !!description && description !== '';
+
     return (
         <div className="scan-details-section">
             <h2>Scan details</h2>
             <ul className="details-section-list">
-                <li>
-                    <span className="icon" aria-hidden="true">
-                        <UrlIcon />
-                    </span>
-                    <span className="screen-reader-only">Target page: </span>
-                    <span className="text">
-                        <NewTabLink href={pageUrl}>{pageUrl}</NewTabLink>
-                    </span>
-                </li>
-                <li>
-                    <span className="icon" aria-hidden="true">
-                        <DateIcon />
-                    </span>
-                    <span className="screen-reader-only" id="scan-date-text">
-                        {screenReaderTexts.scanDate}
-                    </span>
-                    <span className="text" aria-labelledby="scan-date-text" aria-hidden="true">
-                        {scanDateUTC}
-                    </span>
-                </li>
-                {showCommentRow ? createCommentRow(description, screenReaderTexts.comment) : null}
+                {createListItem(<UrlIcon />, 'target page:', <NewTabLink href={pageUrl}>{pageUrl}</NewTabLink>)}
+                {createListItem(<DateIcon />, 'scan date:', scanDateUTC)}
+                {showCommentRow && createListItem(<CommentIcon />, 'comment:', description)}
             </ul>
         </div>
     );
