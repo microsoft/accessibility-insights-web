a511ca72a1a0d4b2bd5415554f19ba718fad87d9
Shobhit Agarwal <shoagarw@microsoft.com>
2019-06-17 11:13:33 -0700

    revert some changes to have a different level of refactor

--- src/DetailsView/reports/components/report-sections/details-section.tsx
+++ src/DetailsView/reports/components/report-sections/details-section.tsx
@@ -7,7 +7,6 @@ import { CommentIcon } from '../../../../common/icons/comment-icon';
 import { DateIcon } from '../../../../common/icons/date-icon';
 import { UrlIcon } from '../../../../common/icons/url-icon';
 import { NamedSFC } from '../../../../common/react/named-sfc';
-import { detailsSectionList, icon as iconClass, scanDetailsSection, screenReaderOnly } from './details-section.scss';
 import { SectionProps } from './report-section-factory';
 
 export type DetailsSectionProps = Pick<SectionProps, 'pageUrl' | 'description' | 'scanDate' | 'toUtcString'>;
@@ -17,10 +16,10 @@ export const DetailsSection = NamedSFC<DetailsSectionProps>('DetailsSection', pr
 
     const createListItem = (icon: JSX.Element, label: string, content: string | JSX.Element, contentClassName?: string) => (
         <li>
-            <span className={iconClass} aria-hidden="true">
+            <span className="icon" aria-hidden="true">
                 {icon}
             </span>
-            <span className={screenReaderOnly}>{label}</span>
+            <span className="screen-reader-only">{label}</span>
             <span className={css('text', contentClassName)}>{content}</span>
         </li>
     );
@@ -29,9 +28,9 @@ export const DetailsSection = NamedSFC<DetailsSectionProps>('DetailsSection', pr
     const showCommentRow = !!description && description !== '';
 
     return (
-        <div className={scanDetailsSection}>
+        <div className="scan-details-section">
             <h2>Scan details</h2>
-            <ul className={detailsSectionList}>
+            <ul className="details-section-list">
                 {createListItem(<UrlIcon />, 'target page:', <NewTabLink href={pageUrl}>{pageUrl}</NewTabLink>)}
                 {createListItem(<DateIcon />, 'scan date:', scanDateUTC)}
                 {showCommentRow && createListItem(<CommentIcon />, 'comment:', description, 'description-text')}
