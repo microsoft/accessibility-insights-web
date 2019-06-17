a06135212c8476419c911c8446d26f7d05356a2c
Shobhit Agarwal <shoagarw@microsoft.com>
2019-06-17 10:10:05 -0700

    refactor details-section scss

--- src/DetailsView/reports/components/report-sections/details-section.tsx
+++ src/DetailsView/reports/components/report-sections/details-section.tsx
@@ -2,12 +2,12 @@
 // Licensed under the MIT License.
 import { css } from '@uifabric/utilities';
 import * as React from 'react';
-
 import { NewTabLink } from '../../../../common/components/new-tab-link';
 import { CommentIcon } from '../../../../common/icons/comment-icon';
 import { DateIcon } from '../../../../common/icons/date-icon';
 import { UrlIcon } from '../../../../common/icons/url-icon';
 import { NamedSFC } from '../../../../common/react/named-sfc';
+import { detailsSectionList, icon as iconClass, scanDetailsSection, screenReaderOnly } from './details-section.scss';
 import { SectionProps } from './report-section-factory';
 
 export type DetailsSectionProps = Pick<SectionProps, 'pageUrl' | 'description' | 'scanDate' | 'toUtcString'>;
@@ -17,10 +17,10 @@ export const DetailsSection = NamedSFC<DetailsSectionProps>('DetailsSection', pr
 
     const createListItem = (icon: JSX.Element, label: string, content: string | JSX.Element, contentClassName?: string) => (
         <li>
-            <span className="icon" aria-hidden="true">
+            <span className={iconClass} aria-hidden="true">
                 {icon}
             </span>
-            <span className="screen-reader-only">{label}</span>
+            <span className={screenReaderOnly}>{label}</span>
             <span className={css('text', contentClassName)}>{content}</span>
         </li>
     );
@@ -29,9 +29,9 @@ export const DetailsSection = NamedSFC<DetailsSectionProps>('DetailsSection', pr
     const showCommentRow = !!description && description !== '';
 
     return (
-        <div className="scan-details-section">
+        <div className={scanDetailsSection}>
             <h2>Scan details</h2>
-            <ul className="details-section-list">
+            <ul className={detailsSectionList}>
                 {createListItem(<UrlIcon />, 'target page:', <NewTabLink href={pageUrl}>{pageUrl}</NewTabLink>)}
                 {createListItem(<DateIcon />, 'scan date:', scanDateUTC)}
                 {showCommentRow && createListItem(<CommentIcon />, 'comment:', description, 'description-text')}
