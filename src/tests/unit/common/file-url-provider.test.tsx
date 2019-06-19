7d7441912bb93e3bd45fb48cc78c82ba62fe335c
Annika Kouhia <t-ankouh@microsoft.com>
2019-06-19 09:36:47 -0700

    still fixing test

--- src/tests/unit/common/file-url-provider.test.tsx
+++ src/tests/unit/common/file-url-provider.test.tsx
@@ -7,9 +7,8 @@ import { provideBlob } from '../../../common/blob-provider';
 
 describe('FileURLProviderTest', () => {
     it('provideURL', () => {
-        const windowUtilsMock = Mock.ofType<WindowUtils>();
+        const windowUtilsMock = Mock.ofType(WindowUtils);
         const provideBlobMock = Mock.ofType<(blobParts?: any[], mimeType?: string) => Blob>();
-        const blobStub = {} as Blob;
         const content = ['<a></a>'];
         const mimeType = 'text/html';
         const returnedURL = 'returned url';
