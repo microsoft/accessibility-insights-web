2ccfbea010ffbfe81b4b52a7134badad73449711
Wahaj <32555133+waabid@users.noreply.github.com>
2019-06-21 14:49:34 -0700

    React-based visualization components for highlight drawer (#838)
    * Created a highlight box component
    
    * Added the element highlight component with tests (broken due to mocks?)
    
    * Fixed tests by not using proxy mocks
    
    * Fixed missing test for dialog render
    
    * Fixed tslint issues
    
    * addressed minor PR comments

---
+++ src/tests/unit/tests/injected/visualization/highlight-box.test.tsx
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+import { BoxConfig, SimpleHighlightDrawerConfiguration } from '../../../../../injected/visualization/formatter';
+import { HightlightBox, HightlightBoxDeps, HightlightBoxProps } from '../../../../../injected/visualization/highlight-box';
+
+describe('HighlightBox', () => {
+    let deps: HightlightBoxDeps;
+    let classNameStub: string;
+    let drawerConfiguration: SimpleHighlightDrawerConfiguration;
+    let boxConfig: BoxConfig;
+    let onClickHandlerStub: () => void;
+    let props: HightlightBoxProps;
+
+    beforeEach(() => {
+        deps = {};
+
+        boxConfig = {
+            background: 'green',
+            fontColor: 'red',
+            boxWidth: '100',
+            text: 'some text',
+        };
+
+        drawerConfiguration = {
+            cursor: 'auto',
+            textAlign: 'center',
+        };
+
+        onClickHandlerStub = () => {};
+        classNameStub = 'some class name';
+        props = {
+            deps: deps,
+            className: classNameStub,
+            drawerConfig: drawerConfiguration,
+            boxConfig: boxConfig,
+            onClickHandler: onClickHandlerStub,
+        };
+    });
+
+    test('renders null when boxConfig is null', () => {
+        props.boxConfig = null;
+        const testObject = shallow(<HightlightBox {...props} />);
+        expect(testObject.getElement()).toMatchSnapshot();
+    });
+
+    test('renders with appropriate styles/elements when boxConfig is set', () => {
+        const testObject = shallow(<HightlightBox {...props} />);
+        expect(testObject.getElement()).toMatchSnapshot();
+    });
+});
