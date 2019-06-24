cc355bb4ba8340ff89e4d8249c00b0b6a9a05556
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-06-24 14:09:56 -0700

    update @types/enzyme

--- src/tests/unit/tests/DetailsView/components/switcher.test.tsx
+++ src/tests/unit/tests/DetailsView/components/switcher.test.tsx
@@ -40,7 +40,7 @@ describe('Switcher', () => {
         actionCreatorMock
             .setup(creator => creator.sendPivotItemClicked(DetailsViewPivotType[DetailsViewPivotType.assessment]))
             .verifiable(Times.once());
-        const wrapper = mount(<Switcher {...defaultProps} />);
+        const wrapper = mount<Switcher>(<Switcher {...defaultProps} />);
         const dropdown = wrapper.find(Dropdown);
 
         expect(wrapper.state().selectedKey).toBe(DetailsViewPivotType.fastPass);
