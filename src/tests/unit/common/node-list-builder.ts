ba805d0c034d5fda03a8f9a7aa92629ea1db5f81
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-06-27 10:08:49 -0700

    Add HTMLCollectionOfBuilder

--- src/tests/unit/common/node-list-builder.ts
+++ src/tests/unit/common/node-list-builder.ts
@@ -1,13 +1,36 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 export class NodeListBuilder {
-    public static createNodeList<T extends Node>(items: T[]): NodeListOf<T> {
+    public static create<T extends Node>(items: T[]): NodeListOf<T> {
         const length = items.length;
 
         const nodeList: NodeListOf<T> = {
             length: length,
             item: (pos: number) => {
                 return items[pos];
+            },
+            forEach: () => {},
+        };
+
+        for (let i = 0; i < items.length; i++) {
+            nodeList[i] = items[i];
+        }
+
+        return nodeList;
+    }
+}
+
+export class HTMLCollectionOfBuilder {
+    public static create<T extends Element>(items: T[]): HTMLCollectionOf<T> {
+        const length = items.length;
+
+        const nodeList: HTMLCollectionOf<T> = {
+            length: length,
+            item: (pos: number) => {
+                return items[pos];
+            },
+            namedItem: (name: string) => {
+                return null;
             },
         };
 
