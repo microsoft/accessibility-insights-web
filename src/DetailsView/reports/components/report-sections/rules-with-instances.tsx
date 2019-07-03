774bd615af5b90993b06790b878bb6e38d86f02f
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-07-03 12:00:23 -0700

    use MinimalRuleDetail for failed instances section

--- src/DetailsView/reports/components/report-sections/rules-with-instances.tsx
+++ src/DetailsView/reports/components/report-sections/rules-with-instances.tsx
@@ -1,14 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as React from 'react';
-
 import { NamedSFC } from '../../../../common/react/named-sfc';
 import { FixInstructionProcessor } from '../../../../injected/fix-instruction-processor';
 import { RuleResult } from '../../../../scanner/iruleresults';
 import { InstanceOutcomeType } from '../instance-outcome-type';
 import { CollapsibleContainer } from './collapsible-container';
-import { FullRuleDetail, FullRuleDetailDeps } from './full-rule-detail';
+import { FullRuleDetailDeps } from './full-rule-detail';
 import { InstanceDetailsGroup } from './instance-details-group';
+import { MinimalRuleDetail } from './minimal-rule-detail';
 
 export type RulesWithInstancesDeps = FullRuleDetailDeps;
 
@@ -29,12 +29,7 @@ export const RulesWithInstances = NamedSFC<RulesWithInstancesProps>(
                         <CollapsibleContainer
                             key={`summary-details-${idx + 1}`}
                             id={rule.id}
-                            accessibleHeadingContent={
-                                <h3 className="screen-reader-only">
-                                    rule {rule.id}, {rule.nodes.length} failures
-                                </h3>
-                            }
-                            visibleHeadingContent={<FullRuleDetail deps={deps} key={rule.id} rule={rule} outcomeType={outcomeType} />}
+                            visibleHeadingContent={<MinimalRuleDetail key={rule.id} rule={rule} outcomeType={outcomeType} />}
                             collapsibleContent={
                                 <InstanceDetailsGroup
                                     fixInstructionProcessor={fixInstructionProcessor}
@@ -44,6 +39,7 @@ export const RulesWithInstances = NamedSFC<RulesWithInstancesProps>(
                             }
                             buttonAriaLabel="show failed instance list"
                             containerClassName="collapsible-rule-details-group"
+                            titleHeadingLevel={3}
                         />
                     );
                 })}
