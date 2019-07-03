17fe2b84196e187bca16b1b143b5769d51d2a588
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-07-03 16:15:45 -0700

    Use button aria label on rules with instances

--- src/DetailsView/reports/components/report-sections/rules-with-instances.tsx
+++ src/DetailsView/reports/components/report-sections/rules-with-instances.tsx
@@ -1,10 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as React from 'react';
+
 import { NamedSFC } from '../../../../common/react/named-sfc';
 import { FixInstructionProcessor } from '../../../../injected/fix-instruction-processor';
 import { RuleResult } from '../../../../scanner/iruleresults';
 import { InstanceOutcomeType } from '../instance-outcome-type';
+import { outcomeTypeSemantics } from '../outcome-type';
 import { CollapsibleContainer } from './collapsible-container';
 import { FullRuleDetailDeps } from './full-rule-detail';
 import { InstanceDetailsGroup } from './instance-details-group';
@@ -25,6 +27,8 @@ export const RulesWithInstances = NamedSFC<RulesWithInstancesProps>(
         return (
             <div className="rule-details-group">
                 {rules.map((rule, idx) => {
+                    const { pastTense } = outcomeTypeSemantics[outcomeType];
+                    const buttonAriaLabel = `${rule.id} ${rule.nodes.length} ${pastTense} ${rule.description}`;
                     return (
                         <CollapsibleContainer
                             key={`summary-details-${idx + 1}`}
@@ -39,6 +43,7 @@ export const RulesWithInstances = NamedSFC<RulesWithInstancesProps>(
                             }
                             containerClassName="collapsible-rule-details-group"
                             titleHeadingLevel={3}
+                            buttonAriaLabel={buttonAriaLabel}
                         />
                     );
                 })}
