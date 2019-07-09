276388e3e2750fe3ccbadbce167fda20b071a947
Lisa Li <lisli@microsoft.com>
2019-07-09 13:22:36 -0700

    basic e2e test for details view headings page

---
+++ src/tests/end-to-end/tests/details-view/headings.test.ts
@@ -0,0 +1,85 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { Browser } from '../../common/browser';
+import { launchBrowser } from '../../common/browser-factory';
+import { popupPageElementIdentifiers } from '../../common/element-identifiers/popup-page-element-identifiers';
+import { enableHighContrast } from '../../common/enable-high-contrast';
+import { Page } from '../../common/page';
+import { scanForAccessibilityIssues } from '../../common/scan-for-accessibility-issues';
+
+describe('Headings Page', () => {
+    describe('Normal mode', () => {
+        let browser: Browser;
+        let targetTabId: number;
+        let headingsPage: Page;
+
+        beforeAll(async () => {
+            browser = await launchBrowser({ suppressFirstTimeDialog: true });
+            targetTabId = (await browser.setupNewTargetPage()).tabId;
+            headingsPage = await openHeadingsPage(browser, targetTabId);
+        });
+
+        afterAll(async () => {
+            if (browser) {
+                await browser.close();
+                browser = undefined;
+            }
+        });
+
+        it('should pass accessibility validation', async () => {
+            const results = await scanForAccessibilityIssues(headingsPage, '.details-content');
+            expect(results).toHaveLength(0);
+        });
+    });
+
+    describe('High contrast mode', () => {
+        let browser: Browser;
+        let targetTabId: number;
+        let headingsPage: Page;
+
+        beforeAll(async () => {
+            browser = await launchBrowser({ suppressFirstTimeDialog: true });
+
+            targetTabId = (await browser.setupNewTargetPage()).tabId;
+            await setupHighContrastMode();
+
+            headingsPage = await openHeadingsPage(browser, targetTabId);
+        });
+
+        afterAll(async () => {
+            if (browser) {
+                await browser.close();
+                browser = undefined;
+            }
+        });
+
+        it('should pass accessibility validation', async () => {
+            const results = await scanForAccessibilityIssues(headingsPage, '.details-content');
+            expect(results).toHaveLength(0);
+        });
+
+        async function setupHighContrastMode(): Promise<void> {
+            const tempDetailsViewPage = await browser.newExtensionDetailsViewPage(targetTabId);
+            await enableHighContrast(tempDetailsViewPage);
+            await tempDetailsViewPage.close();
+        }
+    });
+
+    async function openHeadingsPage(browser: Browser, targetTabId: number): Promise<Page> {
+        const popupPage = await browser.newExtensionPopupPage(targetTabId);
+
+        let detailsViewPage: Page;
+
+        await Promise.all([
+            browser.waitForPageMatchingUrl(await browser.getDetailsViewPageUrl(targetTabId)).then(page => (detailsViewPage = page)),
+            popupPage.clickSelector(popupPageElementIdentifiers.launchPadAssessmentButton),
+        ]);
+
+        // await detailsViewPage.waitForSelector('.details-view-test-nav-area');
+
+        await detailsViewPage.clickSelector('a[title="5: Headings (Incomplete)"]');
+        await detailsViewPage.waitForSelectorToDisappear('.ms-Spinner');
+
+        return detailsViewPage;
+    }
+});
