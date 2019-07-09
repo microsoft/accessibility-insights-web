4a77f8749b3fa77f8f4c0ec1919986c46006e65e
Sebastian Morales <smoralesd@users.noreply.github.com>
2019-07-09 11:51:10 -0700

    feat: add rule resources card (#912)
    * add more resources info for failed rules
    
    * styles
    
    * Add RuleResources component
    
    * Add RuleContent component
    
    * Update InstanceDetailsGroups, removing RuleResources content
    
    * Update RulesWithInstances to use RuleContent instead of InstanceDetailsGroup
    
    * Remove unused import. Fix deps type definition

--- src/DetailsView/reports/components/report-sections/rules-with-instances.tsx
+++ src/DetailsView/reports/components/report-sections/rules-with-instances.tsx
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as React from 'react';
-
 import { NamedSFC } from '../../../../common/react/named-sfc';
 import { FixInstructionProcessor } from '../../../../injected/fix-instruction-processor';
 import { RuleResult } from '../../../../scanner/iruleresults';
@@ -9,10 +8,10 @@ import { InstanceOutcomeType } from '../instance-outcome-type';
 import { outcomeTypeSemantics } from '../outcome-type';
 import { CollapsibleContainer } from './collapsible-container';
 import { FullRuleDetailDeps } from './full-rule-detail';
-import { InstanceDetailsGroup } from './instance-details-group';
 import { MinimalRuleDetail } from './minimal-rule-detail';
+import { RuleContent, RuleContentDeps } from './rule-content';
 
-export type RulesWithInstancesDeps = FullRuleDetailDeps;
+export type RulesWithInstancesDeps = FullRuleDetailDeps & RuleContentDeps;
 
 export type RulesWithInstancesProps = {
     deps: RulesWithInstancesDeps;
@@ -35,10 +34,11 @@ export const RulesWithInstances = NamedSFC<RulesWithInstancesProps>(
                             id={rule.id}
                             visibleHeadingContent={<MinimalRuleDetail key={rule.id} rule={rule} outcomeType={outcomeType} />}
                             collapsibleContent={
-                                <InstanceDetailsGroup
-                                    fixInstructionProcessor={fixInstructionProcessor}
+                                <RuleContent
                                     key={`${rule.id}-rule-group`}
-                                    nodeResults={rule.nodes}
+                                    deps={deps}
+                                    rule={rule}
+                                    fixInstructionProcessor={fixInstructionProcessor}
                                 />
                             }
                             containerClassName="collapsible-rule-details-group"
