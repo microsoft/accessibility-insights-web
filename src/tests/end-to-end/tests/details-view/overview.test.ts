bda134b49ad9afe49ff7e984b422c8ac2b6461c2
Lisa Li <lisli@microsoft.com>
2019-07-08 19:21:40 -0700

    overview page test for normal and high contrast mode

--- src/tests/end-to-end/tests/details-view/overview.test.ts
+++ src/tests/end-to-end/tests/details-view/overview.test.ts
@@ -2,30 +2,82 @@
 // Licensed under the MIT License.
 import { Browser } from '../../common/browser';
 import { launchBrowser } from '../../common/browser-factory';
+import { overviewSelectors } from '../../common/element-identifiers/details-view-selectors';
+import { popupPageElementIdentifiers } from '../../common/element-identifiers/popup-page-element-identifiers';
+import { enableHighContrast } from '../../common/enable-high-contrast';
 import { Page } from '../../common/page';
+import { scanForAccessibilityIssues } from '../../common/scan-for-accessibility-issues';
 
 describe('Overview Page', () => {
     describe('Normal mode', () => {
         let browser: Browser;
         let targetTabId: number;
-        let detailsViewPage: Page;
+        let overviewPage: Page;
 
         beforeAll(async () => {
             browser = await launchBrowser({ suppressFirstTimeDialog: true });
             targetTabId = (await browser.setupNewTargetPage()).tabId;
-            // detailsViewPage = await openOverviewPage(browser, targetTabId);
+            overviewPage = await openOverviewPage(browser, targetTabId);
+        });
+
+        afterAll(async () => {
+            if (browser) {
+                await browser.close();
+                browser = undefined;
+            }
+        });
+
+        it('should pass accessibility validation', async () => {
+            const results = await scanForAccessibilityIssues(overviewPage, overviewSelectors.overview);
+            expect(results).toHaveLength(0);
         });
     });
 
-    // async function openOverviewPage(browser: Browser, targetTabId: number): Promise<Page> {
-    //     const popupPage = await browser.newExtensionPopupPage(targetTabId);
+    describe('High contrast mode', () => {
+        let browser: Browser;
+        let targetTabId: number;
+        let overviewPage: Page;
 
-    //     await popupPage.clickSelector(CommonSelectors.settingsGearButton);
+        beforeAll(async () => {
+            browser = await launchBrowser({ suppressFirstTimeDialog: true });
 
-    //     const detailsViewPage = await waitForDetailsViewWithPreviewFeaturesPanel(browser, popupPage, targetTabId);
+            targetTabId = (await browser.setupNewTargetPage()).tabId;
+            await setupHighContrastMode();
 
-    //     await detailsViewPage.waitForId(componentId);
+            overviewPage = await openOverviewPage(browser, targetTabId);
+        });
 
-    //     return detailsViewPage;
-    // }
+        afterAll(async () => {
+            if (browser) {
+                await browser.close();
+                browser = undefined;
+            }
+        });
+
+        it('should pass accessibility validation', async () => {
+            const results = await scanForAccessibilityIssues(overviewPage, overviewSelectors.overview);
+            expect(results).toHaveLength(0);
+        });
+
+        async function setupHighContrastMode(): Promise<void> {
+            const tempDetailsViewPage = await browser.newExtensionDetailsViewPage(targetTabId);
+            await enableHighContrast(tempDetailsViewPage);
+            await tempDetailsViewPage.close();
+        }
+    });
+
+    async function openOverviewPage(browser: Browser, targetTabId: number): Promise<Page> {
+        const popupPage = await browser.newExtensionPopupPage(targetTabId);
+
+        let overviewPage: Page;
+
+        await Promise.all([
+            browser.waitForPageMatchingUrl(await browser.getDetailsViewPageUrl(targetTabId)).then(page => (overviewPage = page)),
+            popupPage.clickSelector(popupPageElementIdentifiers.launchPadAssessmentButton),
+        ]);
+
+        await overviewPage.waitForSelector(overviewSelectors.overviewHeading);
+
+        return overviewPage;
+    }
 });
