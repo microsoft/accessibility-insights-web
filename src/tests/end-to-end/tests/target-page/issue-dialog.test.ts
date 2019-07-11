da34549feeb6d794961046e1ab18136ec65bb6f3
Lisa Li <lisli@microsoft.com>
2019-07-10 17:06:50 -0700

    initial commit issue dialog test

---
+++ src/tests/end-to-end/tests/target-page/issue-dialog.test.ts
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { Browser } from '../../common/browser';
+import { launchBrowser } from '../../common/browser-factory';
+import { detailsViewSelectors } from '../../common/element-identifiers/details-view-selectors';
+import { popupPageElementIdentifiers } from '../../common/element-identifiers/popup-page-element-identifiers';
+import { Page } from '../../common/page';
+import { scanForAccessibilityIssues } from '../../common/scan-for-accessibility-issues';
+
+describe('Target Page', () => {
+    describe('issue dialog', () => {
+        let browser: Browser;
+        let targetTabId: number;
+        let headingsPage: Page;
+
+        beforeAll(async () => {
+            browser = await launchBrowser({ suppressFirstTimeDialog: true });
+            targetTabId = (await browser.setupNewTargetPage()).tabId;
+            headingsPage = await openHeadingsPage(browser, targetTabId);
+        });
+
+        afterAll(async () => {
+            if (browser) {
+                await browser.close();
+                browser = undefined;
+            }
+        });
+
+        it('should pass accessibility validation', async () => {
+            // const results = await scanForAccessibilityIssues(headingsPage, GuidanceContentSelectors.detailsContent);
+            // expect(results).toHaveLength(0);
+        });
+    });
+
+    async function openHeadingsPage(browser: Browser, targetTabId: number): Promise<Page> {
+        const popupPage = await browser.newExtensionPopupPage(targetTabId);
+
+        let detailsViewPage: Page;
+
+        // await Promise.all([
+        //     browser.waitForPageMatchingUrl(await browser.getDetailsViewPageUrl(targetTabId)).then(page => (detailsViewPage = page)),
+        //     popupPage.clickSelector(popupPageElementIdentifiers.launchPadAssessmentButton),
+        // ]);
+
+        return detailsViewPage;
+    }
+});
