00e96d544f8743285eaeb469df7fd71c81216035
Lisa Li <lisli@microsoft.com>
2019-07-12 17:25:46 -0700

    remove unused import

--- src/tests/end-to-end/common/enable-high-contrast.ts
+++ src/tests/end-to-end/common/enable-high-contrast.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { Browser } from './browser';
 import { CommonSelectors, DetailsViewCommonSelectors } from './element-identifiers/common-selectors';
 import { Page } from './page';
 
