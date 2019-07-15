49be657ef0e7d74481485623bbdd0756df36bb5e
lisli1 <48259897+lisli1@users.noreply.github.com>
2019-07-15 16:20:41 -0700

    refactor: common functions for e2e tests (#949)
    - Set high contrast mode **after** opening an Assessment details view page to remove the extra function to set up high contrast mode (previously set it before by opening a temp details view page) in `overview.test.ts` and `headings.tests.ts`
    - Added new function `newExtensionAssessmentDetailsViewPage` to `browser.ts` to open a details view page from Assessment. The existing `newExtensionDetailsViewPage` opens a details view page from Fast Pass.
    - Updated `overview.test.ts` and `headings.test.ts` to use `newExtensionAssessmentDetailsViewPage`

--- src/tests/end-to-end/tests/details-view/headings.test.ts
+++ src/tests/end-to-end/tests/details-view/headings.test.ts
@@ -4,7 +4,6 @@ import { Browser } from '../../common/browser';
 import { launchBrowser } from '../../common/browser-factory';
 import { GuidanceContentSelectors } from '../../common/element-identifiers/common-selectors';
 import { detailsViewSelectors } from '../../common/element-identifiers/details-view-selectors';
-import { popupPageElementIdentifiers } from '../../common/element-identifiers/popup-page-element-identifiers';
 import { enableHighContrast } from '../../common/enable-high-contrast';
 import { Page } from '../../common/page';
 import { scanForAccessibilityIssues } from '../../common/scan-for-accessibility-issues';
@@ -41,11 +40,9 @@ describe('Headings Page', () => {
 
         beforeAll(async () => {
             browser = await launchBrowser({ suppressFirstTimeDialog: true });
-
             targetTabId = (await browser.setupNewTargetPage()).tabId;
-            await setupHighContrastMode();
-
             headingsPage = await openHeadingsPage(browser, targetTabId);
+            await enableHighContrast(headingsPage);
         });
 
         afterAll(async () => {
@@ -59,23 +56,10 @@ describe('Headings Page', () => {
             const results = await scanForAccessibilityIssues(headingsPage, GuidanceContentSelectors.detailsContent);
             expect(results).toHaveLength(0);
         });
-
-        async function setupHighContrastMode(): Promise<void> {
-            const tempDetailsViewPage = await browser.newExtensionDetailsViewPage(targetTabId);
-            await enableHighContrast(tempDetailsViewPage);
-            await tempDetailsViewPage.close();
-        }
     });
 
     async function openHeadingsPage(browser: Browser, targetTabId: number): Promise<Page> {
-        const popupPage = await browser.newExtensionPopupPage(targetTabId);
-
-        let detailsViewPage: Page;
-
-        await Promise.all([
-            browser.waitForPageMatchingUrl(await browser.getDetailsViewPageUrl(targetTabId)).then(page => (detailsViewPage = page)),
-            popupPage.clickSelector(popupPageElementIdentifiers.launchPadAssessmentButton),
-        ]);
+        const detailsViewPage: Page = await browser.newExtensionAssessmentDetailsViewPage(targetTabId);
 
         await detailsViewPage.waitForSelector(detailsViewSelectors.testNavArea);
 
