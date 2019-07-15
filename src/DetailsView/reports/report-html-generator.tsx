fa6fbf07401bbbb3e67d63359310c6459dca8131
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-07-15 15:20:15 -0700

    remove unsued interface

--- src/DetailsView/reports/report-html-generator.tsx
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-import { ScanResults } from '../../scanner/iruleresults';
-
-export interface ReportHtmlGenerator {
-    generateHtml(scanResult: ScanResults, scanDate: Date, pageTitle: string, pageUrl: string, description: string): string;
-}
