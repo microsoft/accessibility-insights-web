0ce0eb41f5a6f6e7aaf688f83f71f8e12b037d46
Annika Kouhia <t-ankouh@microsoft.com>
2019-07-15 14:52:43 -0700

    removing double copyright

--- src/background/actions/path-snippet-action-creator.ts
+++ src/background/actions/path-snippet-action-creator.ts
@@ -1,7 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
 import { RegisterTypeToPayloadCallback } from '../../common/message';
 import { Messages } from '../../common/messages';
 import { PathSnippetActions } from './path-snippet-actions';
