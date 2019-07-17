fcac95c6a603dd920106d6f2f629dc84d05634f1
Dan Bjorge <danielbj@microsoft.com>
2019-07-17 16:57:35 -0700

    test: fix Windows-hosted-agent-specific "Page crashed!" puppeteer errors (#958)
    We were seeing our Windows hosted Pipelines agents hit "Page crashed!" errors somewhat reliably (~50% of e2e runs). I added functionality to collect detailed chrome logs and crash dumps and collect them as part of our published e2e results, but adding this actually fixed the issue. After some experimentation, I think the key to fixing it was in specifying a custom userDataDir launch parameter to chromium that uses a temporary userDataDir under our test-results folder, rather than deferring to Puppeteer's default behavior of putting it in the system %TEMP% directory. I'm not sure why this fixes the problem; maybe some sort of permissions issue, maybe some sort of overaggressive cleanup of the temp folder while the test is still running.
    
    Validated that 15x runs on Windows with the userDataDir in place never saw the "Page crashed!" issue. Prior to this fix, we see it in about 50% of runs.
    
    Out of scope: other buckets of e2e flakiness issues. In particular, it's expected that there will be some e2e-reliability failures still in the checks section of this PR (but none of them should be "Page crashed!" errors)

--- src/tests/end-to-end/common/browser.ts
+++ src/tests/end-to-end/common/browser.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as Puppeteer from 'puppeteer';
-
+import { browserLogPath } from './browser-factory';
 import { popupPageElementIdentifiers } from './element-identifiers/popup-page-element-identifiers';
 import { forceTestFailure } from './force-test-failure';
 import { Page } from './page';
@@ -12,7 +12,7 @@ export class Browser {
     private memoizedBackgroundPage: Page;
     private pages: Array<Page> = [];
 
-    constructor(private readonly underlyingBrowser: Puppeteer.Browser) {
+    constructor(private readonly browserInstanceId: string, private readonly underlyingBrowser: Puppeteer.Browser) {
         underlyingBrowser.on('disconnected', onBrowserDisconnected);
     }
 
@@ -23,7 +23,7 @@ export class Browser {
 
     public async newPage(url: string): Promise<Page> {
         const underlyingPage = await this.underlyingBrowser.newPage();
-        const page = new Page(underlyingPage);
+        const page = new Page(underlyingPage, { onPageCrash: this.onPageCrash });
         await page.goto(url);
         this.pages.push(page);
         return page;
@@ -93,7 +93,7 @@ export class Browser {
     public async getLastOpenPage(): Promise<Page> {
         const targets = await this.underlyingBrowser.targets();
         const puppeteerPage = await targets[targets.length - 1].page();
-        return new Page(puppeteerPage);
+        return new Page(puppeteerPage, { onPageCrash: this.onPageCrash });
     }
 
     public async getActivePageTabId(): Promise<number> {
@@ -108,7 +108,7 @@ export class Browser {
     public async waitForPageMatchingUrl(url: string): Promise<Page> {
         const underlyingTarget = await this.underlyingBrowser.waitForTarget(t => t.url().toLowerCase() === url.toLowerCase());
         const underlyingPage = await underlyingTarget.page();
-        return new Page(underlyingPage);
+        return new Page(underlyingPage, { onPageCrash: this.onPageCrash });
     }
 
     private async getExtensionUrl(relativePath: string): Promise<string> {
@@ -128,10 +128,15 @@ export class Browser {
             t => t.type() === 'background_page' && Browser.isExtensionBackgroundPage(t.url()),
         );
 
-        this.memoizedBackgroundPage = new Page(await backgroundPageTarget.page());
+        this.memoizedBackgroundPage = new Page(await backgroundPageTarget.page(), { onPageCrash: this.onPageCrash });
 
         return this.memoizedBackgroundPage;
     }
+
+    private onPageCrash = () => {
+        const errorMessage = `!!! Browser.onPageCrashed: see detailed chrome logs '${browserLogPath(this.browserInstanceId)}'`;
+        console.error(errorMessage);
+    };
 
     private static isExtensionBackgroundPage(url: string): boolean {
         return new URL(url).pathname === '/background/background.html';
