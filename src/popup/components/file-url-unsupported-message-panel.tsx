d1b6f78a242b36a3d41161251656e267a9feaf54
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-07-23 14:52:24 -0700

    Add FileUrlUnsupportedMessagePanel

---
+++ src/popup/components/file-url-unsupported-message-panel.tsx
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import * as React from 'react';
+import { BrowserAdapter } from '../../background/browser-adapters/browser-adapter';
+import { NewTabLink } from '../../common/components/new-tab-link';
+import { DisplayableStrings } from '../../common/constants/displayable-strings';
+import { NamedSFC } from '../../common/react/named-sfc';
+
+export type FileUrlUnsupportedMessagePanelDeps = {
+    browserAdapter: BrowserAdapter;
+};
+export type FileUrlUnsupportedMessagePanelProps = {
+    deps: FileUrlUnsupportedMessagePanelDeps;
+    header: JSX.Element;
+    title: string;
+};
+
+export const FileUrlUnsupportedMessagePanel = NamedSFC<FileUrlUnsupportedMessagePanelProps>(
+    'FileUrlUnsupportedMessagePanel',
+    ({ deps, header, title }) => {
+        const { browserAdapter } = deps;
+
+        return (
+            <div className="ms-Fabric unsupported-url-info-panel">
+                {header}
+                <div className="ms-Grid main-section">
+                    <div className="launch-panel-general-container">{DisplayableStrings.fileUrlDoesNotHaveAccess}</div>
+                    <div>
+                        <div>To allow this extension to run on file URLs:</div>
+                        <div>
+                            {'1. Open '}
+                            <NewTabLink onClick={browserAdapter.openManageExtensionPage} aria-label={`open ${title} extension page`}>
+                                {`${title} extension page`}
+                            </NewTabLink>
+                            {'.'}
+                        </div>
+                        <div>
+                            {'2. Enable '}
+                            <span className="ms-fontWeight-semibold">Allow Access to file URLs</span>.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    },
+);
