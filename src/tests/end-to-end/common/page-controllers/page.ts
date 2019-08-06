7d2f82e111ddf92c0bdffcb3d5b60ae1d969d4e7
Shobhit Agarwal <shoagarw@microsoft.com>
2019-08-06 14:18:54 -0700

    remove code that PR review suggested

--- src/tests/end-to-end/common/page-controllers/page.ts
+++ src/tests/end-to-end/common/page-controllers/page.ts
@@ -109,12 +109,6 @@ export class Page {
         );
     }
 
-    public async waitForNavigation(options?: Puppeteer.NavigationOptions): Promise<Puppeteer.Response> {
-        return await this.screenshotOnError(
-            async () => await this.underlyingPage.waitForNavigation({ timeout: DEFAULT_PAGE_ELEMENT_WAIT_TIMEOUT_MS, ...options }),
-        );
-    }
-
     public async waitForSelector(selector: string, options?: Puppeteer.WaitForSelectorOptions): Promise<Puppeteer.ElementHandle<Element>> {
         return await this.screenshotOnError(
             async () => await this.underlyingPage.waitForSelector(selector, { timeout: DEFAULT_PAGE_ELEMENT_WAIT_TIMEOUT_MS, ...options }),
