838525fe7e0fa4f338de228f1927a5ec024c78e4
John Alkire <john.alkire@microsoft.com>
2019-08-12 11:39:07 -0700

    Fix spelling

--- src/electron/android-connect/android-connect-initializer.ts
+++ src/electron/android-connect/android-connect-initializer.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as ReactDOM from 'react-dom';
-import { AndroidConnectRender } from './android-connect-renderer';
+import { AndroidConnectRenderer } from './android-connect-renderer';
 
 const dom = document;
-const renderer = new AndroidConnectRender(ReactDOM.render, dom);
+const renderer = new AndroidConnectRenderer(ReactDOM.render, dom);
 renderer.render();
--- src/electron/android-connect/android-connect-renderer.tsx
+++ src/electron/android-connect/android-connect-renderer.tsx
@@ -3,8 +3,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-export class AndroidConnectRender {
-    constructor(private readonly renderer: typeof ReactDOM.render, private readonly dom: Document) {}
+export class AndroidConnectRenderer {
+    constructor(private readonly renderer: typeof ReactDOM.render, private readonly dom: ParentNode) {}
 
     public render(): void {
         const androidConnectContainer = this.dom.querySelector('#android-connect-container');
