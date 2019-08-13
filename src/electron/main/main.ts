5c28c1863a3e45a74852afbe31103ba4cc78998e
Karan <karansin@microsoft.com>
2019-08-13 16:32:33 -0700

    Modify file path of HTML file used in electron-builder's generated exe (#1064)

--- src/electron/main/main.ts
+++ src/electron/main/main.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
 
 let mainWindow: BrowserWindow;
 
@@ -8,7 +9,7 @@ const createWindow = () => {
     mainWindow = new BrowserWindow({ show: false, webPreferences: { nodeIntegration: true } });
 
     mainWindow
-        .loadFile('../electron/device-connect-view/deviceConnectView.html')
+        .loadFile(path.resolve(__dirname, '../electron/device-connect-view/deviceConnectView.html'))
         .then(() => console.log('url loaded'))
         .catch(console.log);
 
