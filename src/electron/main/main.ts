43fd02f9126aaa07a468b1208fefe778fe435958
DaveTryon <Dave.Tryon@microsoft.com>
2019-08-13 16:53:10 -0700

    Add code to extract Axe.Android results from a mapped port

--- src/electron/main/main.ts
+++ src/electron/main/main.ts
@@ -1,12 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { app, BrowserWindow } from 'electron';
+import { fetchScanResults } from '../platform/android/fetch-scan-results';
 import * as path from 'path';
 
 let mainWindow: BrowserWindow;
 
 const createWindow = () => {
     mainWindow = new BrowserWindow({ show: false, webPreferences: { nodeIntegration: true } });
+
+    // These are just to test the connection and should later be removed.
+    checkPort(48485);
+    checkPort(48486);
+    checkPort(48487);
 
     mainWindow
         .loadFile(path.resolve(__dirname, '../electron/device-connect-view/deviceConnectView.html'))
@@ -20,5 +26,15 @@ const createWindow = () => {
         mainWindow.webContents.openDevTools({ mode: 'bottom' });
     });
 };
+
+function checkPort(port: number): void {
+    fetchScanResults(port)
+        .then(data => {
+            console.log(`Scanned port ${port}, found device ${data.deviceName} running ${data.appIdentifier}`);
+        })
+        .catch(error => {
+            console.log(`Scanned port ${port}, received error ${error}`);
+        });
+}
 
 app.on('ready', createWindow);
