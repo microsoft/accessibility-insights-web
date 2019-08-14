beac0c6ee2400c375c2e30751482b0b646bcf668
John Alkire <john.alkire@microsoft.com>
2019-08-14 16:03:53 -0700

    Don't show debug window

--- src/tests/unit/electron/device-connect-view/components/window-footer.test.tsx
+++ src/tests/unit/electron/device-connect-view/components/window-footer.test.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { shallow } from 'enzyme';
-import { BaseButton, Button } from 'office-ui-fabric-react/lib/Button';
+import { Button } from 'office-ui-fabric-react/lib/Button';
 import * as React from 'react';
 import { Mock, Times } from 'typemoq';
 import { WindowFooter, WindowFooterProps } from '../../../../../electron/device-connect-view/components/window-footer';
