35355ada5febb6db4a806db44809d11cadc8a5b1
John Alkire <john.alkire@microsoft.com>
2019-08-14 15:32:51 -0700

    Update/add applicable unit tests

---
+++ src/tests/unit/electron/device-connect-view/components/window-footer.test.tsx
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { shallow } from 'enzyme';
+import { BaseButton, Button } from 'office-ui-fabric-react/lib/Button';
+import * as React from 'react';
+import { Mock, Times } from 'typemoq';
+import { WindowFooter, WindowFooterProps } from '../../../../../electron/device-connect-view/components/window-footer';
+import { EventStubFactory } from '../../../common/event-stub-factory';
+
+describe('WindowTitleTest', () => {
+    test('constructor', () => {
+        expect(new WindowFooter({} as WindowFooterProps)).toBeDefined();
+    });
+
+    test('render', () => {
+        const props: WindowFooterProps = {
+            cancelClick: () => {
+                return;
+            },
+        };
+        const rendered = shallow(<WindowFooter {...props} />);
+
+        expect(rendered.getElement()).toMatchSnapshot();
+    });
+
+    test('cancel click', () => {
+        const onClickMock = Mock.ofInstance(
+            (event: React.MouseEvent<Button | BaseButton | HTMLDivElement | HTMLAnchorElement | HTMLButtonElement, MouseEvent>) => {},
+        );
+
+        const eventStub = new EventStubFactory().createMouseClickEvent() as React.MouseEvent<Button>;
+
+        const props: WindowFooterProps = {
+            cancelClick: onClickMock.object,
+        };
+
+        const rendered = shallow(<WindowFooter {...props} />);
+        const button = rendered.find('.window-footer-button-cancel');
+        button.simulate('click', eventStub);
+
+        onClickMock.verify(onClick => onClick(eventStub), Times.once());
+    });
+});
---
+++ src/tests/unit/electron/device-connect-view/components/window-title.test.tsx
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import { WindowTitle, WindowTitleProps } from '../../../../../electron/device-connect-view/components/window-title';
+
+describe('WindowTitleTest', () => {
+    test('constructor', () => {
+        expect(new WindowTitle({} as WindowTitleProps)).toBeDefined();
+    });
+
+    test('render', () => {
+        const props: WindowTitleProps = {
+            title: 'Test',
+            children: <span>test</span>,
+        };
+
+        const rendered = shallow(<WindowTitle {...props} />);
+
+        expect(rendered.getElement()).toMatchSnapshot();
+    });
+});
