f75a5a848d768613a574e9b175d2560078961fb2
John Alkire <john.alkire@microsoft.com>
2019-08-14 15:32:31 -0700

    Refactor window-footer to pass in browserwindow

--- src/electron/device-connect-view/components/window-footer.tsx
+++ src/electron/device-connect-view/components/window-footer.tsx
@@ -1,22 +1,29 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { css } from '@uifabric/utilities';
-import { remote } from 'electron';
-import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import { BaseButton, DefaultButton } from 'office-ui-fabric-react/lib/Button';
 import * as React from 'react';
 
-export class WindowFooter extends React.Component {
+export interface WindowFooterProps {
+    cancelClick: React.MouseEventHandler<BaseButton>;
+}
+
+export class WindowFooter extends React.Component<WindowFooterProps> {
+    constructor(props: WindowFooterProps) {
+        super(props);
+    }
+
     public render(): JSX.Element {
         return (
             <footer className="ms-Grid window-footer">
                 <div className={css('ms-Grid-row')}>
-                    <DefaultButton onClick={this.cancelClick} text="Cancel"></DefaultButton>
+                    <DefaultButton
+                        className={css('window-footer-button-cancel')}
+                        onClick={this.props.cancelClick}
+                        text="Cancel"
+                    ></DefaultButton>
                 </div>
             </footer>
         );
-    }
-
-    private cancelClick(): void {
-        remote.getCurrentWindow().close();
     }
 }
--- src/electron/device-connect-view/device-connect-view-renderer.tsx
+++ src/electron/device-connect-view/device-connect-view-renderer.tsx
@@ -1,22 +1,28 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { BrowserWindow } from 'electron';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { brand } from '../../content/strings/application';
 import { BrandBlue } from '../../icons/brand/blue/brand-blue';
 import { WindowFooter } from './components/window-footer';
 import { WindowTitle } from './components/window-title';
 
 export class DeviceConnectViewRenderer {
-    constructor(private readonly renderer: typeof ReactDOM.render, private readonly dom: ParentNode) {}
+    constructor(
+        private readonly renderer: typeof ReactDOM.render,
+        private readonly dom: ParentNode,
+        private readonly CurrentWindow: BrowserWindow,
+    ) {}
 
     public render(): void {
         const deviceConnectViewContainer = this.dom.querySelector('#device-connect-view-container');
         this.renderer(
             <>
-                <WindowTitle title="Accessibility Insights">
+                <WindowTitle title={brand}>
                     <BrandBlue />
                 </WindowTitle>
-                <WindowFooter></WindowFooter>
+                <WindowFooter cancelClick={this.CurrentWindow.close}></WindowFooter>
             </>,
             deviceConnectViewContainer,
         );
