dc8268d5221fb8c120b0d0d63b7974730be5fdb0
DaveTryon <45672944+DaveTryon@users.noreply.github.com>
2019-08-15 11:32:57 -0700

    Add code to extract Axe.Android results from a mapped port (#1077)
    * Add code to extract Axe.Android results from a mapped port
    
    * Updates based on Shobit's PR feedback
    
    * Re-add the code to return a new Promise, so that we maintain proper failure in all scenarios
    
    * Add unit tests for ScanResults, return null instead of leaking exceptions if data is missing
    
    * Rename scan-results.test.tsx to scan-results.test.ts
    
    * Add code to extract Axe.Android results from a mapped port
    
    * Updates based on Shobit's PR feedback
    
    * Re-add the code to return a new Promise, so that we maintain proper failure in all scenarios
    
    * Add unit tests for ScanResults, return null instead of leaking exceptions if data is missing
    
    * Rename scan-results.test.tsx to scan-results.test.ts
    
    * Fix ordering issue I added during merge

--- src/electron/main/main.ts
+++ src/electron/main/main.ts
@@ -2,11 +2,17 @@
 // Licensed under the MIT License.
 import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
+import { fetchScanResults } from '../platform/android/fetch-scan-results';
 
 let mainWindow: BrowserWindow;
 
 const createWindow = () => {
     mainWindow = new BrowserWindow({ show: false, webPreferences: { nodeIntegration: true } });
+
+    // These are just to test the connection and should later be removed.
+    checkPort(48485);
+    checkPort(48486);
+    checkPort(48487);
 
     mainWindow
         .loadFile(path.resolve(__dirname, '../electron/device-connect-view/deviceConnectView.html'))
@@ -20,5 +26,15 @@ const createWindow = () => {
         mainWindow.webContents.openDevTools({ mode: 'bottom' });
     });
 };
+
+function checkPort(port: number): void {
+    fetchScanResults(port)
+        .then(data => {
+            console.log(`Scanned port ${port}, found device ${data.deviceName} running ${data.appIdentifier}`);
+        })
+        .catch(error => {
+            console.log(`Scanned port ${port}, received error ${error}`);
+        });
+}
 
 app.on('ready', createWindow);
