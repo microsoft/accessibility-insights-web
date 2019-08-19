d868fed8c97d0ddc0dff4e82973cc6429c9b7a70
Shobhit <shoagarw@microsoft.com>
2019-08-19 10:50:39 -0700

    feature: introducing interfaces for unified data model (#1086)
    * starting point for unified interfaces
    
    * continued progress in unified data interface
    
    * UnifiedAxeResults in ScanResults to contain arrays of UnifiedRule that each contain ResultInstance arrays
    
    * rename UnifiedRule to UnifiedRuleResult
    
    * added UnifiedResultInstance interface
    
    * removed ScannedElementInfo
    
    * renamed UnifiedAxeResults to UnifiedResults
    
    * add newline
    
    * UnifiedRuleResult does not contain UnifiedResultInstance nodes
    
    * results in ScanResults to be an array of UnifiedResultInstance
    
    * rename UnifiedRuleResult to UnifiedRule and add type for status
    
    * remove ScanMetaData
    
    * move unified-data-interface.ts from src/cards/ to src/common/types/store-data/
    
    * removed src/cards/
    
    * add exports and rename status to ResultInstanceStatus
    
    * make properties not needed for cards as optional
    
    * ResultInstanceStatus: change incomplete and inapplicable to unknown
    
    * renamed some properties
    
    * replace 'T' with 'any'
    
    * updated to use 'properties' in some interface names
    
    * add status to UnifiedResultInstance
    
    * rename ScanResults to UnifiedResults and UnifiedResultInstance to UnifiedResult
    
    * make osVersion non-optional

---
+++ src/common/types/store-data/unified-data-interface.ts
@@ -0,0 +1,56 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// this is similar to `TestEngine` interface from axe-core
+export interface ScanEngineProperties {
+    name: string;
+    version: string;
+}
+
+export interface OSProperties {
+    name: string;
+    version: string;
+    userAgent?: string;
+}
+
+export interface ViewPortProperties {
+    height?: number;
+    width?: number;
+}
+
+export interface PlatformData {
+    osInfo: OSProperties;
+    viewPortInfo: ViewPortProperties;
+}
+
+export interface ToolData {
+    scanEngineProperties: ScanEngineProperties;
+}
+
+export interface UnifiedResults {
+    results: UnifiedResult[];
+    platformInfo?: PlatformData;
+    toolInfo?: ToolData;
+}
+
+export interface UnifiedRule {
+    id: string;
+    description: string;
+}
+
+export interface InstancePropertyBag {
+    [property: string]: any;
+}
+
+export type StoredInstancePropertyBag = InstancePropertyBag;
+
+export interface UnifiedResult {
+    id: string;
+    status: ResultStatus;
+    evaluation: StoredInstancePropertyBag;
+    identifiers: StoredInstancePropertyBag;
+    descriptors: StoredInstancePropertyBag;
+    resolution: StoredInstancePropertyBag;
+}
+
+export type ResultStatus = 'pass' | 'fail' | 'unknown';
