626990b60178acaa5242cf8799a42e167d79762b
John Alkire <john.alkire@microsoft.com>
2019-08-19 16:21:59 -0700

    Add test for ElectronExternalLink

--- src/electron/device-connect-view/components/device-connect-header.tsx
+++ src/electron/device-connect-view/components/device-connect-header.tsx
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { shell } from 'electron';
 import * as React from 'react';
 import { NamedSFC } from '../../../common/react/named-sfc';
 import { ElectronExternalLink } from './electron-external-link';
@@ -8,7 +9,11 @@ export const DeviceConnectHeader = NamedSFC('DeviceConnectHeader', () => {
     return (
         <header className="device-connect-header">
             <h2>Connect to your android device</h2>
-            <ElectronExternalLink href="https://go.microsoft.com/fwlink/?linkid=2101252" text="How do I connect to my device?" />
+            <ElectronExternalLink
+                href="https://go.microsoft.com/fwlink/?linkid=2101252"
+                text="How do I connect to my device?"
+                shell={shell}
+            />
         </header>
     );
 });
