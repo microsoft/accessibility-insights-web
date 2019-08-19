ab8a0566041933d282482f1f0895a2dfa650c99a
Dan Bjorge <danielbj@microsoft.com>
2019-08-19 14:48:16 -0700

    fix alignment issues in assessment overview

--- src/reports/components/assessment-summary-details.tsx
+++ src/reports/components/assessment-summary-details.tsx
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 import * as React from 'react';
 
+import { css } from '@uifabric/utilities';
 import { AssessmentSummaryReportModel } from '../assessment-report-model';
 import { OutcomeChipSet } from './outcome-chip-set';
 import { OutcomeIconSet } from './outcome-icon-set';
@@ -15,24 +16,20 @@ export class AssessmentSummaryDetails extends React.Component<AssessmentSummaryD
         return (
             <div role="table" className="assessment-summary-details">
                 <div role="rowgroup" className="assessment-summary-details-body">
-                    {this.getTestDetailsColumns()}
+                    {this.getTestDetailsList(this.props.testSummaries)}
                 </div>
             </div>
         );
     }
 
-    private getTestDetailsColumns(): JSX.Element[] {
-        return this.getTestDetailsList(this.props.testSummaries, 1);
-    }
-
-    private getTestDetailsList(summaries: AssessmentSummaryReportModel[], colIndex: number): JSX.Element[] {
-        return summaries.map((testSummary, index) => {
-            return (
-                <div role="row" key={`${colIndex}-${index}`} className="test-summary">
+    private getTestDetailsList(summaries: AssessmentSummaryReportModel[]): JSX.Element[] {
+        return summaries.map(testSummary => (
+            <div role="row" key={testSummary.displayName} className="assessment-summary-details-row">
+                <div className="test-summary">
                     <div role="cell" className="test-summary-display-name">
                         {testSummary.displayName}
                     </div>
-                    <div role="cell" key={`${colIndex}-${index}-status`} className="test-summary-status">
+                    <div role="cell" className="test-summary-status">
                         {testSummary.pass + testSummary.incomplete + testSummary.fail > 7 ? (
                             <OutcomeChipSet {...testSummary} />
                         ) : (
@@ -40,7 +37,7 @@ export class AssessmentSummaryDetails extends React.Component<AssessmentSummaryD
                         )}
                     </div>
                 </div>
-            );
-        });
+            </div>
+        ));
     }
 }
