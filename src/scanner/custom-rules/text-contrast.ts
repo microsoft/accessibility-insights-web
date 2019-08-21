d6afbf0acc45d3acf0c0ff915168a3e3a2afe87d
Sebastian Morales <smoralesd@users.noreply.github.com>
2019-08-21 15:02:05 -0700

    refactor: move custom rules to proper folder (#1122)
    In preparation for the new rules we need for the new WCAG 2.1 assisted tests, this PR move all custom rules & checks to the new `custom-rules` folder inside `scanner`
    
    * move rules to custom-rules folder
    * move rules & checks tests

---
+++ src/scanner/custom-rules/text-contrast.ts
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import * as AxeUtils from '../axe-utils';
+
+import { RuleConfiguration } from '../iruleresults';
+
+export const textContrastConfiguration: RuleConfiguration = {
+    checks: [
+        {
+            id: 'text-contrast',
+            evaluate: evaluateTextContrast,
+        },
+    ],
+    rule: {
+        id: 'text-contrast',
+        selector: '*',
+        any: ['text-contrast'],
+        all: [],
+        matches: AxeUtils.getMatchesFromRule('color-contrast'),
+        excludeHidden: false,
+        options: {
+            noScroll: false,
+        },
+        enabled: false,
+    },
+};
+
+function evaluateTextContrast(node: HTMLElement, options: any, virtualNode: any, context: any): boolean {
+    // tslint:disable-next-line:no-invalid-this
+    const checkResult = AxeUtils.getEvaluateFromCheck('color-contrast').call(this, node, options, virtualNode, context);
+    const nodeStyle = window.getComputedStyle(node);
+    const fontSize = parseFloat(nodeStyle.getPropertyValue('font-size'));
+    const fontWeight = nodeStyle.getPropertyValue('font-weight');
+    const bold = ['bold', 'bolder', '600', '700', '800', '900'].indexOf(fontWeight) !== -1;
+
+    const data = {
+        textString: node.innerText,
+        size: isLargeText(fontSize, bold) ? 'large' : 'regular',
+    };
+
+    // tslint:disable-next-line:no-invalid-this
+    this.data(data);
+
+    return checkResult;
+}
+
+function isLargeText(fontSize: number, bold: boolean): boolean {
+    fontSize = (fontSize * 72) / 96;
+    return fontSize >= 18 || (fontSize >= 14 && bold);
+}
