0b31ac62c8375b0fc3afd84c0be4b45be7b93f07
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-08-21 10:59:56 -0700

    add instructions and labels note component

---
+++ src/assessments/common/instructions-and-labels-note.tsx
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import * as React from 'react';
+
+import * as Markup from '../markup';
+
+export const InstructionsAndLabelsNotes = () => (
+    <Markup.Emphasis>
+        Note: For WCAG 2.1 compliance both visible instructions and labels must be programatically related to a widget. For WCAG 2.0
+        compliance, only visible instructions must be programatically related to a widget.
+    </Markup.Emphasis>
+);
