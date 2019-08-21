9cdcbbf1d1bfef2c2bf311a00d312ccc461b2d2d
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-08-21 11:23:16 -0700

    update how to test and guidance links for Custom Widgets -> Instructions

--- src/assessments/custom-widgets/test-steps/instructions.tsx
+++ src/assessments/custom-widgets/test-steps/instructions.tsx
@@ -1,19 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as React from 'react';
-
 import { CustomWidgetPropertyBag } from 'common/types/property-bag/icustom-widgets';
 import { VisualizationType } from 'common/types/visualization-type';
 import { link } from 'content/link';
-import { productName } from 'content/strings/application';
-import { TestAutomaticallyPassedNotice } from 'content/test/common/test-automatically-passed-notice';
 import * as content from 'content/test/custom-widgets/instructions';
 import { AssessmentVisualizationEnabledToggle } from 'DetailsView/components/assessment-visualization-enabled-toggle';
 import { ScannerUtils } from 'injected/scanner-utils';
+import * as React from 'react';
+
+import { NewTabLink } from '../../../common/components/new-tab-link';
 import { AnalyzerConfigurationFactory } from '../../common/analyzer-configuration-factory';
 import { AssistedTestRecordYourResults } from '../../common/assisted-test-record-your-results';
+import { InstructionsAndLabelsNotes } from '../../common/instructions-and-labels-note';
 import { NoValue } from '../../common/property-bag-column-renderer';
-import * as Markup from '../../markup';
 import { ReportInstanceField } from '../../types/report-instance-field';
 import { Requirement } from '../../types/requirement';
 import { getFlatDesignPatternStringFromRole } from '../custom-widgets-column-renderer';
@@ -26,12 +25,19 @@ const instructionsDescription: JSX.Element = (
 
 const instructionsHowToTest: JSX.Element = (
     <div>
-        <p>For this requirement, {productName} highlights custom widgets.</p>
-        <TestAutomaticallyPassedNotice />
+        <p>
+            The visual helper for this requirement highlights custom widgets. (A custom widget is an element with a valid{' '}
+            <NewTabLink href="https://www.w3.org/TR/wai-aria/#widget_roles">ARIA widget role</NewTabLink>.)
+        </p>
+        <InstructionsAndLabelsNotes />
         <ol>
+            <li>In the target page, examine each highlighted element to determine whether it has a visible label or instructions.</li>
             <li>
-                For each widget, verify that any instructions visible in the target page are also visible in the{' '}
-                <Markup.Term>Instances</Markup.Term> list.
+                Verify that all visible labels and instructions are displayed in the Instances list:
+                <ol>
+                    <li>Any label should appear in the accessible name.</li>
+                    <li>Any additional instructions should appear in the accessible description.</li>
+                </ol>
             </li>
             <AssistedTestRecordYourResults />
         </ol>
@@ -44,7 +50,7 @@ export const Instructions: Requirement = {
     description: instructionsDescription,
     howToTest: instructionsHowToTest,
     isManual: false,
-    guidanceLinks: [link.WCAG_1_3_1],
+    guidanceLinks: [link.WCAG_1_3_1, link.WCAG_2_5_3],
     ...content,
     columnsConfig: [
         {
