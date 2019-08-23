202cf662d9927d806ccf1976c6e060933f07a696
Lisa Li <lisli@microsoft.com>
2019-08-23 16:55:49 -0700

    update rule-based-view-model-provider test

--- src/tests/unit/common/rule-based-view-model-provider.test.ts
+++ src/tests/unit/common/rule-based-view-model-provider.test.ts
@@ -1,11 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import {
-    getUnifiedRuleResults,
-    UnifiedRuleResult,
-    UnifiedRuleResultStatus,
-    UnifiedStatusResults,
-} from '../../../common/rule-based-view-model-provider';
+import { getUnifiedRuleResults, UnifiedRuleResult, UnifiedStatusResults } from '../../../common/rule-based-view-model-provider';
 import { InstanceResultStatus, UnifiedResult, UnifiedRule } from '../../../common/types/store-data/unified-data-interface';
 
 describe('RuleBasedViewModelProvider', () => {
@@ -18,8 +13,9 @@ describe('RuleBasedViewModelProvider', () => {
         const resultStub1: UnifiedResult = createUnifiedResultStub('pass', 'rule1');
         const resultStub2: UnifiedResult = createUnifiedResultStub('fail', 'rule1');
         const resultStub3: UnifiedResult = createUnifiedResultStub('unknown', 'rule2');
+        const resultStub4: UnifiedResult = createUnifiedResultStub('unknown', 'rule2');
 
-        const results: UnifiedResult[] = [resultStub1, resultStub2, resultStub3];
+        const results: UnifiedResult[] = [resultStub1, resultStub2, resultStub3, resultStub4];
 
         const expectedResults: UnifiedStatusResults = {
             pass: [
@@ -27,27 +23,39 @@ describe('RuleBasedViewModelProvider', () => {
                     id: 'rule1',
                     status: 'pass',
                     nodes: [resultStub1],
-                } as UnifiedRuleResult,
+                    description: 'stub_description_rule1',
+                    url: 'stub_url_rule1',
+                    guidance: [],
+                },
             ],
             fail: [
                 {
                     id: 'rule1',
                     status: 'fail',
                     nodes: [resultStub2],
-                } as UnifiedRuleResult,
+                    description: 'stub_description_rule1',
+                    url: 'stub_url_rule1',
+                    guidance: [],
+                },
             ],
             unknown: [
                 {
                     id: 'rule2',
                     status: 'unknown',
-                    nodes: [resultStub3],
-                } as UnifiedRuleResult,
+                    nodes: [resultStub3, resultStub4],
+                    description: 'stub_description_rule2',
+                    url: 'stub_url_rule2',
+                    guidance: [],
+                },
             ],
             inapplicable: [
                 {
                     id: 'rule3',
                     status: 'inapplicable',
                     nodes: [],
+                    description: 'stub_description_rule3',
+                    url: 'stub_url_rule3',
+                    guidance: [],
                 } as UnifiedRuleResult,
             ],
         };
@@ -60,13 +68,16 @@ describe('RuleBasedViewModelProvider', () => {
     function createUnifiedRuleStub(id: string): UnifiedRule {
         return {
             id: id,
-        } as UnifiedRule;
+            description: `stub_description_${id}`,
+            url: `stub_url_${id}`,
+            guidance: [],
+        };
     }
 
-    function createUnifiedResultStub(status: string, id: string): UnifiedResult {
+    function createUnifiedResultStub(status: InstanceResultStatus, id: string): UnifiedResult {
         return {
             uid: 'stub_uid',
-            status: status as InstanceResultStatus,
+            status: status,
             ruleId: id,
         } as UnifiedResult;
     }
