523d0355b8b4742885a32f798c1925c404bb649e
Karanbir Singh <karansin@microsoft.com>
2019-08-27 16:22:37 -0700

    wip

--- src/tests/unit/tests/injected/analyzers/unified-result-sender.test.ts
+++ src/tests/unit/tests/injected/analyzers/unified-result-sender.test.ts
@@ -3,20 +3,33 @@
 import { IMock, Mock, Times } from 'typemoq';
 import { Message } from '../../../../../common/message';
 import { Messages } from '../../../../../common/messages';
+import { UnifiedResults } from '../../../../../common/types/store-data/unified-data-interface';
+import { ConvertResultsDelegate } from '../../../../../injected/adapters/scan-results-to-unified-results';
 import { MessageDelegate } from '../../../../../injected/analyzers/rule-analyzer';
 import { UnifiedResultSender } from '../../../../../injected/analyzers/unified-result-sender';
 
 describe('sendConvertedResults', () => {
     it('should send a message', () => {
         const sendDelegate: IMock<MessageDelegate> = Mock.ofInstance(message => null);
-        const expected: Message = {
-            messageType: Messages.UnifiedScan.ScanCompleted,
-            payload: {
-                results: null,
-            },
+        const convertToUnifiedMock: IMock<ConvertResultsDelegate> = Mock.ofInstance((scanResults, uuidGenerator) => null);
+        const uuidGeneratorStub = () => null;
+        const testSubject = new UnifiedResultSender(sendDelegate.object, convertToUnifiedMock.object, uuidGeneratorStub);
+
+        const axeInputResults = {} as any;
+        const unifiedResults: UnifiedResults = {
+            results: [],
         };
-        const testSubject = new UnifiedResultSender(sendDelegate.object);
-        testSubject.sendResults(null);
-        sendDelegate.verify(m => m(expected), Times.once());
+        convertToUnifiedMock.setup(m => m(axeInputResults, uuidGeneratorStub)).returns(val => unifiedResults);
+        testSubject.sendResults({
+            results: null,
+            originalResult: axeInputResults,
+        });
+        convertToUnifiedMock.verifyAll();
+
+        const expectedMessage: Message = {
+            messageType: Messages.UnifiedScan.ScanCompleted,
+            payload: unifiedResults,
+        };
+        sendDelegate.verify(m => m(expectedMessage), Times.once());
     });
 });
