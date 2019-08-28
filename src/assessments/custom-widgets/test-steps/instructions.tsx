4d4ca4f2b6e5cded37b7a2fad1623d42e090113d
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-08-28 16:15:53 -0700

    conver import to root-relative

--- src/assessments/custom-widgets/test-steps/instructions.tsx
+++ src/assessments/custom-widgets/test-steps/instructions.tsx
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { NewTabLink } from 'common/components/new-tab-link';
 import { CustomWidgetPropertyBag } from 'common/types/property-bag/icustom-widgets';
 import { VisualizationType } from 'common/types/visualization-type';
 import { link } from 'content/link';
@@ -8,7 +9,6 @@ import { AssessmentVisualizationEnabledToggle } from 'DetailsView/components/ass
 import { ScannerUtils } from 'injected/scanner-utils';
 import * as React from 'react';
 
-import { NewTabLink } from '../../../common/components/new-tab-link';
 import { AnalyzerConfigurationFactory } from '../../common/analyzer-configuration-factory';
 import { AssistedTestRecordYourResults } from '../../common/assisted-test-record-your-results';
 import { InstructionsAndLabelsNotes } from '../../common/instructions-and-labels-note';
