e5cc4943b6dbe5c9fdb7842f15ea77a7504b9e28
Lisa Li <lisli@microsoft.com>
2019-08-27 17:54:16 -0700

    initial commit wip test

---
+++ src/tests/unit/tests/injected/adapters/scan-results-to-unified-rules.test.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { ScanResults } from '../../../../../scanner/iruleresults';
+
+describe('ScanResults to UnifiedRule[] test', () => {
+    test('convertScanResultsToUnifiedRules', () => {
+        const scanResultsStub: ScanResults = {};
+    });
+});
