0211f0825e59f7fa4a000d4abe5d43b0163cc74d
Lisa Li <lisli@microsoft.com>
2019-08-28 14:00:20 -0700

    added set back in to scan results to rules adapter

--- src/injected/adapters/scan-results-to-unified-rules.ts
+++ src/injected/adapters/scan-results-to-unified-rules.ts
@@ -12,11 +12,13 @@ export function convertScanResultsToUnifiedRules(scanResults: ScanResults): Unif
 
 function createUnifiedRulesFromScanResults(scanResults: ScanResults): UnifiedRule[] {
     const unifiedRules: UnifiedRule[] = [];
+    const ruleIds: Set<string> = new Set();
     const allRuleResults = getAllRuleResults(scanResults);
 
     for (const ruleResult of allRuleResults) {
-        if (!unifiedRules.find(rule => rule.id === ruleResult.id)) {
+        if (!ruleIds.has(ruleResult.id)) {
             unifiedRules.push(createUnifiedRuleFromRuleResult(ruleResult));
+            ruleIds.add(ruleResult.id);
         }
     }
 
