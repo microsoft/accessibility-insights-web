51245b821a7b5359e0f04a14f156147c3be57065
lisli1 <48259897+lisli1@users.noreply.github.com>
2019-08-28 14:17:34 -0700

    Introduce adapter for scan results to unified rules (#1159)
    `scan-results-to-unified-rules` parses `scanResults` input and returns an array of `UnifiedRule`. We decided to get the rules from scan results, so the unified results, which are also generated from scan results, and unified rules will correspond to the same data.
    
    *Note: We considered 2 approaches to check for duplicate rules in the results: using a Set of ruleIds or checking the UnifiedRule array directly, as rules are added to it. When run on inputs of 2,000 and 20,000 scan results, the implementation **with the Set** took 55ms and 501ms, respectively, while the implementation **without the Set** took 117ms and 3276ms, respectively. Since using the Set could save a significant amount of time, we decided to add the set back in exchange for the extra space complexity.*

---
+++ src/injected/adapters/scan-results-to-unified-rules.ts
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { UnifiedRule } from '../../common/types/store-data/unified-data-interface';
+import { RuleResult, ScanResults } from '../../scanner/iruleresults';
+
+export function convertScanResultsToUnifiedRules(scanResults: ScanResults): UnifiedRule[] {
+    if (!scanResults) {
+        return [] as UnifiedRule[];
+    }
+    return createUnifiedRulesFromScanResults(scanResults);
+}
+
+function createUnifiedRulesFromScanResults(scanResults: ScanResults): UnifiedRule[] {
+    const unifiedRules: UnifiedRule[] = [];
+    const ruleIds: Set<string> = new Set();
+    const allRuleResults = getAllRuleResults(scanResults);
+
+    for (const ruleResult of allRuleResults) {
+        if (!ruleIds.has(ruleResult.id)) {
+            unifiedRules.push(createUnifiedRuleFromRuleResult(ruleResult));
+            ruleIds.add(ruleResult.id);
+        }
+    }
+
+    return unifiedRules;
+}
+
+function getAllRuleResults(scanResults: ScanResults): RuleResult[] {
+    return [...scanResults.passes, ...scanResults.violations, ...scanResults.incomplete, ...scanResults.inapplicable];
+}
+
+function createUnifiedRuleFromRuleResult(ruleResult: RuleResult): UnifiedRule {
+    return {
+        id: ruleResult.id,
+        description: ruleResult.description,
+        url: ruleResult.helpUrl,
+        guidance: ruleResult.guidanceLinks,
+    };
+}
