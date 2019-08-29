6094130fad0bbf2d487959a2d8dba5da18b3827e
Sebastian Morales <smoralesd@users.noreply.github.com>
2019-08-29 10:49:33 -0700

    feat: new requirement native widgets -> autocomplete (#1137)
    Add new requirement: Native widgets -> Autocomplete
    
    **Note** this PR does not hook up the new requirement just yet. We do want to make that change in at specific time.
    
    * WIP new requirement
    * hook up autocopmlete rule, setup analyzer and drawer
    * add native-widgets autocomplete info & examples content
    * hook up info & examples into autocomplete requirement
    * Update link to not be an aka.ms forward link
    * update snapshot
    * update snapshots after url change
    * remove ... from failExample
    * change type to password on examples
    * use square brakets. format
    * update highlighting
    * update link to direct link
    * update snapshots

---
+++ src/assessments/native-widgets/test-steps/autocomplete.tsx
@@ -0,0 +1,88 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { link } from 'content/link';
+import * as content from 'content/test/native-widgets/autocomplete';
+import * as React from 'react';
+
+import { NewTabLink } from '../../../common/components/new-tab-link';
+import { DefaultWidgetPropertyBag } from '../../../common/types/property-bag/default-widget';
+import { VisualizationType } from '../../../common/types/visualization-type';
+import { AssessmentVisualizationEnabledToggle } from '../../../DetailsView/components/assessment-visualization-enabled-toggle';
+import { ScannerUtils } from '../../../injected/scanner-utils';
+import { AnalyzerConfigurationFactory } from '../../common/analyzer-configuration-factory';
+import { AssistedTestRecordYourResults } from '../../common/assisted-test-record-your-results';
+import { NoValue, PropertyBagColumnRendererConfig } from '../../common/property-bag-column-renderer';
+import { PropertyBagColumnRendererFactory } from '../../common/property-bag-column-renderer-factory';
+import * as Markup from '../../markup';
+import { ReportInstanceField } from '../../types/report-instance-field';
+import { Requirement } from '../../types/requirement';
+import { NativeWidgetsTestStep } from './test-steps';
+
+const description: JSX.Element = (
+    <span>
+        Text fields that serve certain purposes must have the correct HTML5 <Markup.CodeTerm>autocomplete</Markup.CodeTerm> attribute.
+    </span>
+);
+
+const howToTest: JSX.Element = (
+    <div>
+        The visual helper for this requirement highlights text fields.
+        <ol>
+            <li>
+                In the target page, examine each highlighted text field to determine whether it serves an{' '}
+                <NewTabLink href="https://www.w3.org/TR/WCAG21/#input-purposes">identified input purpose</NewTabLink>.
+            </li>
+
+            <li>
+                If a text field does serve an identified input purpose, verify that it has an <Markup.Term>autocomplete</Markup.Term>{' '}
+                attribute with the correct value.
+            </li>
+
+            <AssistedTestRecordYourResults />
+        </ol>
+    </div>
+);
+
+const key = NativeWidgetsTestStep.autocomplete;
+
+const propertyBagConfig: PropertyBagColumnRendererConfig<DefaultWidgetPropertyBag>[] = [
+    {
+        propertyName: 'inputType',
+        displayName: 'Input type',
+    },
+    {
+        propertyName: 'autocomplete',
+        displayName: 'Autocomplete',
+        defaultValue: NoValue,
+    },
+];
+
+export const Autocomplete: Requirement = {
+    key,
+    name: 'Autocomplete',
+    description,
+    howToTest,
+    isManual: false,
+    guidanceLinks: [link.WCAG_1_3_5],
+    ...content,
+    columnsConfig: [
+        {
+            key: 'autocomplete-info',
+            name: 'Autocomplete',
+            onRender: PropertyBagColumnRendererFactory.getRenderer<DefaultWidgetPropertyBag>(propertyBagConfig),
+        },
+    ],
+    reportInstanceFields: ReportInstanceField.fromColumns(propertyBagConfig),
+    getAnalyzer: provider =>
+        provider.createRuleAnalyzer(
+            AnalyzerConfigurationFactory.forScanner({
+                rules: ['autocomplete'],
+                key,
+                testType: VisualizationType.NativeWidgets,
+                resultProcessor: (scanner: ScannerUtils) => scanner.getPassingInstances,
+            }),
+        ),
+    getDrawer: provider => provider.createHighlightBoxDrawer(),
+    updateVisibility: true,
+    getVisualHelperToggle: props => <AssessmentVisualizationEnabledToggle {...props} />,
+};
