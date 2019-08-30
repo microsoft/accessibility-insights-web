bee44ffb007ac100dfdaebcf2aae320e75cf7897
Dan Bjorge <danielbj@microsoft.com>
2019-08-29 17:55:38 -0700

    fix dangling import to renamed file

--- src/content/test/custom-widgets/index.ts
+++ src/content/test/custom-widgets/index.ts
@@ -6,7 +6,6 @@ import * as expectedInput from './expected-input';
 import { guidance } from './guidance';
 import * as instructions from './instructions';
 import * as keyboardInteraction from './keyboard-interaction';
-import * as label from './label';
 import * as roleStateProperty from './role-state-property';
 
 export const customWidgets = {
