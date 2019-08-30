765e23b1671fd0fc6009f1ab21c59b3603042fd4
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-08-30 15:07:41 -0700

    remove unused import

--- src/DetailsView/components/test-step-view.tsx
+++ src/DetailsView/components/test-step-view.tsx
@@ -1,11 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { Spinner, SpinnerSize } from 'office-ui-fabric-react/lib/Spinner';
-import * as React from 'react';
-
 import { AssessmentDefaultMessageGenerator } from 'assessments/assessment-default-message-generator';
 import { AssessmentsProvider } from 'assessments/types/assessments-provider';
 import { Requirement, VisualHelperToggleConfig } from 'assessments/types/requirement';
+import { Spinner, SpinnerSize } from 'office-ui-fabric-react/lib/Spinner';
+import * as React from 'react';
 import { ContentPanelButton, ContentPanelButtonDeps } from 'views/content/content-panel-button';
 import { CollapsibleComponent } from '../../common/components/collapsible-component';
 import { GuidanceTags, GuidanceTagsDeps } from '../../common/components/guidance-tags';
@@ -14,7 +13,6 @@ import {
     GeneratedAssessmentInstance,
     ManualTestStepResult,
 } from '../../common/types/store-data/assessment-result-data';
-import { FeatureFlagStoreData } from '../../common/types/store-data/feature-flag-store-data';
 import { PathSnippetStoreData } from '../../common/types/store-data/path-snippet-store-data';
 import { DictionaryStringTo } from '../../types/common-types';
 import { DetailsViewActionMessageCreator } from '../actions/details-view-action-message-creator';
