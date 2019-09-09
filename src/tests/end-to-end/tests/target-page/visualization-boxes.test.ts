9e4edcf9ba32fd8cd809659d5def69ed603ec93a
Shobhit Agarwal <shoagarw@microsoft.com>
2019-09-09 13:44:16 -0700

    change back the test content to its original state to minimize test changes

--- src/tests/end-to-end/tests/target-page/visualization-boxes.test.ts
+++ src/tests/end-to-end/tests/target-page/visualization-boxes.test.ts
@@ -31,17 +31,12 @@ describe('Target Page visualization boxes', () => {
     it.each(adhocTools)('for adhoc tool "%s" should pass accessibility validation', async adhocTool => {
         await popupPage.enableToggleByAriaLabel(adhocTool);
 
-        const results = await getShadowRootAndScanPage(targetPage);
-        expect(results).toHaveLength(0);
-    });
-
-    const getShadowRootAndScanPage = async scannedPage => {
-        const shadowRoot = await scannedPage.getShadowRoot();
+        const shadowRoot = await targetPage.getShadowRoot();
         await targetPage.waitForDescendentSelector(shadowRoot, TargetPageInjectedComponentSelectors.insightsVisualizationBox, {
             visible: true,
         });
 
-        const results = await scanForAccessibilityIssues(scannedPage, TargetPageInjectedComponentSelectors.insightsRootContainer);
-        return results;
-    };
+        const results = await scanForAccessibilityIssues(targetPage, TargetPageInjectedComponentSelectors.insightsRootContainer);
+        expect(results).toHaveLength(0);
+    });
 });
