f297ab81e20e8720260d7f4ffc59fc8522d06684
Lisa Li <lisli@microsoft.com>
2019-09-10 22:55:33 -0700

    update telemetry-settings.test to reflect change in TelemetrySettings props

--- src/tests/unit/tests/DetailsView/components/settings-panel/settings/telemetry/telemetry-settings.test.tsx
+++ src/tests/unit/tests/DetailsView/components/settings-panel/settings/telemetry/telemetry-settings.test.tsx
@@ -6,16 +6,18 @@ import * as React from 'react';
 import { Mock, Times } from 'typemoq';
 import { UserConfigMessageCreator } from '../../../../../../../../common/message-creators/user-config-message-creator';
 import { UserConfigurationStoreData } from '../../../../../../../../common/types/store-data/user-configuration-store';
-import { SettingsDeps, SettingsProps } from '../../../../../../../../DetailsView/components/settings-panel/settings/settings-props';
-import { TelemetrySettings } from '../../../../../../../../DetailsView/components/settings-panel/settings/telemetry/telemetry-settings';
+import {
+    TelemetrySettings,
+    TelemetrySettingsProps,
+} from '../../../../../../../../DetailsView/components/settings-panel/settings/telemetry/telemetry-settings';
 
 describe('TelemetrySettings', () => {
     const enableStates = [true, false];
 
     describe('renders', () => {
         it.each(enableStates)('with enabled = %s', enabled => {
-            const props: SettingsProps = {
-                deps: Mock.ofType<SettingsDeps>().object,
+            const props: TelemetrySettingsProps = {
+                deps: Mock.ofType<TelemetrySettingsProps['deps']>().object,
                 userConfigurationStoreState: {
                     enableTelemetry: enabled,
                 } as UserConfigurationStoreData,
@@ -33,8 +35,8 @@ describe('TelemetrySettings', () => {
             const userConfigMessageCreatorMock = Mock.ofType<UserConfigMessageCreator>();
             const deps = {
                 userConfigMessageCreator: userConfigMessageCreatorMock.object,
-            } as SettingsDeps;
-            const props: SettingsProps = {
+            } as TelemetrySettingsProps['deps'];
+            const props: TelemetrySettingsProps = {
                 deps,
                 userConfigurationStoreState: {
                     enableTelemetry: enabled,
