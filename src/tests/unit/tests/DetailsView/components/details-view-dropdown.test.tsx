afb1cfc68b76de4f34d934254f76385b231cfe02
Sebastian Morales <smoralesd@users.noreply.github.com>
2019-09-11 12:42:54 -0700

    refactor: render test using enzyme and snapshot (#1235)
    We have some warnings showing up on our unit test runs. Some of them are around react deprecated notices.
    
    In this case, we're not explicitly using `componentWillMount` but I noticed that updating the test to use snapshot fix the warning.

--- src/tests/unit/tests/DetailsView/components/details-view-dropdown.test.tsx
+++ src/tests/unit/tests/DetailsView/components/details-view-dropdown.test.tsx
@@ -1,65 +1,37 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { shallow } from 'enzyme';
-import { DirectionalHint } from 'office-ui-fabric-react/lib/Callout';
 import { ContextualMenu, IContextualMenuItem } from 'office-ui-fabric-react/lib/ContextualMenu';
-import { Icon } from 'office-ui-fabric-react/lib/Icon';
 import { Link } from 'office-ui-fabric-react/lib/Link';
 import * as React from 'react';
-import * as TestUtils from 'react-dom/test-utils';
 
 import { DetailsViewDropDown, DetailsViewDropDownProps } from '../../../../../DetailsView/components/details-view-dropdown';
 
-class TestableDetailsViewDropDown extends DetailsViewDropDown {
-    public getOpenDropdown(): (target: React.MouseEvent<HTMLElement>) => void {
-        return this.openDropdown;
-    }
-
-    public getDismissDropdown(): () => void {
-        return this.dismissDropdown;
-    }
-}
-
 describe('DetailsViewDropDownTest', () => {
-    test('render', () => {
-        const menuItemsStub: IContextualMenuItem[] = [
-            {
-                key: 'my-test-item',
-                iconProps: {
-                    iconName: 'contactCard',
+    describe('renders', () => {
+        const contextMenuVisibleValues = [true, false];
+
+        it.each(contextMenuVisibleValues)('with isContextMenuVisible = %s', isContextMenuVisible => {
+            const menuItemsStub: IContextualMenuItem[] = [
+                {
+                    key: 'my-test-item',
+                    iconProps: {
+                        iconName: 'contactCard',
+                    },
+                    onClick: null,
+                    name: 'My test item',
                 },
-                onClick: null,
-                name: 'My test item',
-            },
-        ];
-        const props: DetailsViewDropDownProps = {
-            menuItems: menuItemsStub,
-        };
-        const component = React.createElement(TestableDetailsViewDropDown, props);
-        const testObject = TestUtils.renderIntoDocument(component);
+            ];
 
-        testObject.setState({ target: null, isContextMenuVisible: true });
+            const props: DetailsViewDropDownProps = {
+                menuItems: menuItemsStub,
+            };
 
-        const expectedComponent = (
-            <div className="details-view-dropdown">
-                <Link className={'gear-button'} onClick={testObject.getOpenDropdown()}>
-                    <Icon className="gear-options-icon" iconName="Gear" ariaLabel={'Manage Settings'} />
-                </Link>
-                <ContextualMenu
-                    doNotLayer={false}
-                    gapSpace={12}
-                    shouldFocusOnMount={true}
-                    target={null}
-                    onDismiss={testObject.getDismissDropdown()}
-                    directionalHint={DirectionalHint.bottomRightEdge}
-                    directionalHintForRTL={DirectionalHint.bottomLeftEdge}
-                    items={menuItemsStub}
-                    id="settings-dropdown-menu"
-                />
-            </div>
-        );
+            const wrapper = shallow(<DetailsViewDropDown {...props} />);
+            wrapper.setState({ isContextMenuVisible });
 
-        expect(testObject.render()).toEqual(expectedComponent);
+            expect(wrapper.getElement()).toMatchSnapshot();
+        });
     });
 
     test('verify open/close menu', () => {
@@ -75,7 +47,7 @@ describe('DetailsViewDropDownTest', () => {
             isContextMenuVisible: false,
             target: null,
         };
-        const testObject = shallow(<TestableDetailsViewDropDown {...props} />);
+        const testObject = shallow(<DetailsViewDropDown {...props} />);
         const link = testObject.find(Link);
         link.prop('onClick')(target as React.MouseEvent<HTMLElement>);
 
