b19e39a24cae428cca6cac0ec98c044d3163f8a4
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-09-11 12:03:28 -0700

    alphabetize imports

--- src/issue-filing/types/issue-filing-service.ts
+++ src/issue-filing/types/issue-filing-service.ts
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { BrowserAdapter } from '../../common/browser-adapters/browser-adapter';
+import { ReactFCWithDisplayName } from '../../common/react/named-fc';
 import { IssueFilingServicePropertiesMap } from '../../common/types/store-data/user-configuration-store';
 import { EnvironmentInfo } from './../../common/environment-info-provider';
-import { ReactFCWithDisplayName } from '../../common/react/named-fc';
 import { CreateIssueDetailsTextData } from './../../common/types/create-issue-details-text-data';
 import { SettingsFormProps } from './settings-form-props';
 
