bdade86b10ec71003324836e0beebb4e8193cfa6
Lisa Li <lisli@microsoft.com>
2019-09-13 14:45:10 -0700

    introduce common link-component-type

---
+++ src/common/types/link-component-type.ts
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { ILinkProps } from 'office-ui-fabric-react';
+
+export type LinkComponentType = React.FC<ILinkProps>;
