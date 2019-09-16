2a2285bfc0f5befbd4bb04ed72539e2de94c9a03
Shobhit Agarwal <shoagarw@microsoft.com>
2019-09-16 10:55:15 -0700

    refactor a little bit

--- src/electron/device-connect-view/device-connect-view-initializer.ts
+++ src/electron/device-connect-view/device-connect-view-initializer.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { remote } from 'electron';
-import { Store } from 'idb-keyval';
+import { Store as IndexedDBStore } from 'idb-keyval';
 import * as ReactDOM from 'react-dom';
 
 import { UserConfigurationActions } from '../../background/actions/user-configuration-actions';
@@ -10,28 +10,18 @@ import { UserConfigurationStore } from '../../background/stores/global/user-conf
 import { COMMON_CONSTANTS } from '../../common/constants';
 import { initializeFabricIcons } from '../../common/fabric-icons';
 import { IndexedDBAPI, IndexedDBUtil } from '../../common/indexedDB/indexedDB';
-import { StoreNames } from '../../common/stores/store-names';
-import { UserConfigurationStoreData } from '../../common/types/store-data/user-configuration-store';
 import { DeviceConnectViewRenderer } from './device-connect-view-renderer';
-
-function createUserConfigStore(
-    persistedState: UserConfigurationStoreData,
-    userConfigActionsLocal: UserConfigurationActions,
-    indexedDBAPI: IndexedDBAPI,
-): UserConfigurationStore {
-    const userConfigurationStore = new UserConfigurationStore(persistedState, userConfigActionsLocal, indexedDBAPI);
-    return userConfigurationStore;
-}
 
 initializeFabricIcons();
 
-const store = new Store(COMMON_CONSTANTS.defaultIndexedDBName, COMMON_CONSTANTS.defaultIndexedDBStoreName);
-const indexedDBInstance: IndexedDBAPI = new IndexedDBUtil(store);
+const indexedDBStore = new IndexedDBStore(COMMON_CONSTANTS.defaultIndexedDBName, COMMON_CONSTANTS.defaultIndexedDBStoreName);
+const indexedDBInstance: IndexedDBAPI = new IndexedDBUtil(indexedDBStore);
 const userConfigActions = new UserConfigurationActions();
 
 // tslint:disable-next-line:no-floating-promises - top-level entry points are intentionally floating promises
 getPersistedUserConfigData(indexedDBInstance).then((persistedData: Partial<PersistedData>) => {
-    createUserConfigStore(persistedData.userConfigurationData, userConfigActions, indexedDBInstance);
+    const userConfigurationStore = new UserConfigurationStore(persistedData.userConfigurationData, userConfigActions, indexedDBInstance);
+
     const dom = document;
     const renderer = new DeviceConnectViewRenderer(ReactDOM.render, dom, remote.getCurrentWindow());
     renderer.render();
