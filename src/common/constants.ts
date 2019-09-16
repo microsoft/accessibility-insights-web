8cddaf89864015eb9a6af238880368ff2b61d62d
Shobhit Agarwal <shoagarw@microsoft.com>
2019-09-16 11:18:44 -0700

    insert header

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 export const COMMON_CONSTANTS = {
     defaultIndexedDBName: 'default-db',
     defaultIndexedDBStoreName: 'default-store',
