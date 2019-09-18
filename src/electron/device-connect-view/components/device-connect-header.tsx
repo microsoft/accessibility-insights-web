785b59afec9087a1b42203bbee57d6fc73df47ef
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-09-18 14:55:21 -0700

    update device connect header component

--- src/electron/device-connect-view/components/device-connect-header.tsx
+++ src/electron/device-connect-view/components/device-connect-header.tsx
@@ -1,17 +1,15 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { shell } from 'electron';
 import * as React from 'react';
+
 import { NamedFC } from '../../../common/react/named-fc';
-import { ElectronExternalLink } from './electron-external-link';
+import { ElectronLink } from './electron-link';
 
 export const DeviceConnectHeader = NamedFC('DeviceConnectHeader', () => {
     return (
         <header className="device-connect-header">
             <h2>Connect to your android device</h2>
-            <ElectronExternalLink href="https://go.microsoft.com/fwlink/?linkid=2101252" shell={shell}>
-                How do I connect to my device?
-            </ElectronExternalLink>
+            <ElectronLink href="https://go.microsoft.com/fwlink/?linkid=2101252">How do I connect to my device?</ElectronLink>
         </header>
     );
 });
