d099b79190d152eddcb7d5846c780bbb9339e75a
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-09-20 09:41:52 -0700

    fix app data adapter

--- src/electron/adapters/electron-app-data-adapter.ts
+++ src/electron/adapters/electron-app-data-adapter.ts
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { app } from 'electron';
+import { remote } from 'electron';
 import { AppDataAdapter } from '../../common/browser-adapters/app-data-adapter';
 
 export class ElectronAppDataAdapter implements AppDataAdapter {
     public getVersion(): string {
-        return app.getVersion();
+        return remote.app.getVersion();
     }
 }
