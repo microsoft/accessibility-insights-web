ae0e012737a605ef1d3e52723ede521f67c83af1
Shobhit Agarwal <shoagarw@microsoft.com>
2019-09-20 11:01:21 -0700

    make changes based on PR review

--- src/electron/adapters/electron-storage-adapter.ts
+++ src/electron/adapters/electron-storage-adapter.ts
@@ -3,24 +3,35 @@
 import { IndexedDBDataKeys } from '../../background/IndexedDBDataKeys';
 import { StorageAdapter } from '../../common/browser-adapters/storage-adapter';
 import { IndexedDBAPI } from '../../common/indexedDB/indexedDB';
+import { createDefaultLogger } from '../../common/logging/default-logger';
+import { Logger } from '../../common/logging/logger';
 
 export class ElectronStorageAdapter implements StorageAdapter {
-    constructor(private readonly indexedDBInstance: IndexedDBAPI) {}
+    constructor(private readonly indexedDBInstance: IndexedDBAPI, private logger: Logger = createDefaultLogger()) {}
 
     public setUserData(items: Object, callback?: () => void): void {
-        // tslint:disable-next-line:no-floating-promises - top-level entry points are intentionally floating promises
-        this.indexedDBInstance.setItem(IndexedDBDataKeys.installation, items).then(() => callback);
+        this.indexedDBInstance
+            .setItem(IndexedDBDataKeys.installation, items)
+            .then(() => callback)
+            .catch(error => {
+                this.logger.error('Error occured when trying to set user data:', error);
+            });
     }
 
     public getUserData(keys: string | Object | string[], callback: (items: { [key: string]: any }) => void): void {
-        // tslint:disable-next-line:no-floating-promises - top-level entry points are intentionally floating promises
-        this.indexedDBInstance.getItem(IndexedDBDataKeys.installation).then(data => {
-            return callback(data);
-        });
+        this.indexedDBInstance
+            .getItem(IndexedDBDataKeys.installation)
+            .then(data => {
+                return callback(data);
+            })
+            .catch(error => {
+                this.logger.error('Error occured when trying to get user data:', error);
+            });
     }
 
     public removeUserData(key: string): void {
-        // tslint:disable-next-line:no-floating-promises - top-level entry points are intentionally floating promises
-        this.indexedDBInstance.removeItem(IndexedDBDataKeys.installation);
+        this.indexedDBInstance
+            .removeItem(IndexedDBDataKeys.installation)
+            .catch(error => this.logger.error('Error removing user data', error));
     }
 }
