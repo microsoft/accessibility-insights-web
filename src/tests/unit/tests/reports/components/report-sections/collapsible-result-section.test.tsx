1839632f814d166c86719dc4e4002f5685ba60db
Wahaj Abid <waabid@microsoft.com>
2019-09-24 13:10:30 -0700

    Fixed tests

--- src/tests/unit/tests/reports/components/report-sections/collapsible-result-section.test.tsx
+++ src/tests/unit/tests/reports/components/report-sections/collapsible-result-section.test.tsx
@@ -2,18 +2,27 @@
 // Licensed under the MIT License.
 import { shallow } from 'enzyme';
 import * as React from 'react';
+import {
+    CollapsibleResultSection,
+    CollapsibleResultSectionDeps,
+    CollapsibleResultSectionProps,
+} from 'reports/components/report-sections/collapsible-result-section';
+import { Mock } from 'typemoq';
 
-import { CollapsibleResultSection, CollapsibleResultSectionProps } from 'reports/components/report-sections/collapsible-result-section';
+import { CollapsibleComponentCardsProps } from '../../../../../../DetailsView/components/cards/collapsible-component-cards';
 
 describe('CollapsibleResultSection', () => {
     it('renders', () => {
+        const collapsibleControlMock = Mock.ofType<(props: CollapsibleComponentCardsProps) => JSX.Element>();
         const props: CollapsibleResultSectionProps = {
+            deps: {
+                collapsibleControl: collapsibleControlMock.object,
+            } as CollapsibleResultSectionDeps,
             containerClassName: 'result-section-class-name',
             containerId: 'container-id',
         } as CollapsibleResultSectionProps;
 
         const wrapper = shallow(<CollapsibleResultSection {...props} />);
-
         expect(wrapper.getElement()).toMatchSnapshot();
     });
 });
