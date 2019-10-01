7f2f93deb0201e603152a76ee93cd2d9fc6e2c07
Lisa Li <lisli@microsoft.com>
2019-10-01 13:37:08 -0700

    add environmentInfoProvider and toolInfo to UnifiedResultSender

--- src/injected/analyzers/unified-result-sender.ts
+++ src/injected/analyzers/unified-result-sender.ts
@@ -1,8 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { UnifiedScanCompletedPayload } from '../../background/actions/action-payloads';
+import { EnvironmentInfoProvider } from '../../common/environment-info-provider';
 import { Messages } from '../../common/messages';
 import { UUIDGeneratorType } from '../../common/uid-generator';
+import { title } from '../../content/strings/application';
 import { ConvertScanResultsToUnifiedResultsDelegate } from '../adapters/scan-results-to-unified-results';
 import { ConvertScanResultsToUnifiedRulesDelegate } from '../adapters/scan-results-to-unified-rules';
 import { AxeAnalyzerResult } from './analyzer';
@@ -13,6 +15,7 @@ export class UnifiedResultSender {
         private readonly sendMessage: MessageDelegate,
         private readonly convertScanResultsToUnifiedResults: ConvertScanResultsToUnifiedResultsDelegate,
         private readonly convertScanResultsToUnifiedRules: ConvertScanResultsToUnifiedRulesDelegate,
+        private readonly environmentInfoProvider: EnvironmentInfoProvider,
         private readonly generateUID: UUIDGeneratorType,
     ) {}
 
@@ -20,6 +23,12 @@ export class UnifiedResultSender {
         const payload: UnifiedScanCompletedPayload = {
             scanResult: this.convertScanResultsToUnifiedResults(axeResults.originalResult, this.generateUID),
             rules: this.convertScanResultsToUnifiedRules(axeResults.originalResult),
+            toolInfo: {
+                scanEngineProperties: {
+                    name: title,
+                    version: this.environmentInfoProvider.getEnvironmentInfo().extensionVersion,
+                },
+            },
         };
 
         this.sendMessage({
