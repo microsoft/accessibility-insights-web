69689f751fda0d3a93162319af668d792abff5e7
AhmedAbdoOrtiga <45858632+AhmedAbdoOrtiga@users.noreply.github.com>
2019-10-02 15:27:57 -0700

    Card Footer UI (#1342)
    * Card Footer UI

--- src/common/components/cards/instance-details-footer.tsx
+++ src/common/components/cards/instance-details-footer.tsx
@@ -3,8 +3,12 @@
 import { NamedFC } from 'common/react/named-fc';
 import { UnifiedResult } from 'common/types/store-data/unified-data-interface';
 import { some, values } from 'lodash';
+import { Icon, Label } from 'office-ui-fabric-react';
 import * as React from 'react';
+import { foot, highlightDiv } from './card-footer.scss';
+
 import { CardInteractionSupport } from './card-interaction-support';
+import { CardKebabMenuButton } from './card-kebab-menu-button';
 
 export type HighlightState = 'visible' | 'hidden' | 'unavailable';
 
@@ -27,5 +31,24 @@ export const InstanceDetailsFooter = NamedFC<InstanceDetailsFooterProps>('Instan
         return null;
     }
 
-    return <div>Highlight {highlightState}</div>;
+    const kebabMenuIcon = () => {
+        return <CardKebabMenuButton />;
+    };
+
+    const HighlightButton = () => {
+        const label = 'HighLight ' + highlightState;
+        return (
+            <div className={highlightDiv}>
+                <Icon iconName="redEye" ariaLabel={label} />
+                <Label>{label}</Label>
+            </div>
+        );
+    };
+
+    return (
+        <div className={foot}>
+            {HighlightButton()}
+            {kebabMenuIcon()}
+        </div>
+    );
 });
