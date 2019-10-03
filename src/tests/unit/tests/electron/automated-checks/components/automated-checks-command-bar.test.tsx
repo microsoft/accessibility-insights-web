6095d4f68c83dbf96d423f90d04755e6144c4321
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-10-03 12:36:01 -0700

    move automated-checks to views-folder

--- src/tests/unit/tests/electron/automated-checks/components/automated-checks-command-bar.test.tsx
+++ src/tests/unit/tests/electron/automated-checks/components/automated-checks-command-bar.test.tsx
@@ -5,11 +5,11 @@ import { Button } from 'office-ui-fabric-react/lib/Button';
 import * as React from 'react';
 import { Mock, MockBehavior, Times } from 'typemoq';
 
+import { DeviceConnectActionCreator } from 'electron/flux/action-creator/device-connect-action-creator';
 import {
     AutomatedChecksCommandBar,
     AutomatedChecksCommandBarProps,
-} from 'electron/automated-checks/components/automated-checks-command-bar';
-import { DeviceConnectActionCreator } from 'electron/flux/action-creator/device-connect-action-creator';
+} from 'electron/views/automated-checks/components/automated-checks-command-bar';
 import { EventStubFactory } from 'tests/unit/common/event-stub-factory';
 
 describe('AutomatedChecksCommandBar', () => {
