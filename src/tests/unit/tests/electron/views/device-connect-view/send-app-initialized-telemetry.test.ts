ffa17c9eb0c2d86259dec22ff7b33c82b1e18faf
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-10-03 12:43:52 -0700

    move send-app-initialized-telemetry test

--- src/tests/unit/tests/electron/views/device-connect-view/send-app-initialized-telemetry.test.ts
+++ src/tests/unit/tests/electron/views/device-connect-view/send-app-initialized-telemetry.test.ts
@@ -1,14 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+import { BaseActionPayload } from 'background/actions/action-payloads';
 import { TelemetryEventHandler } from 'background/telemetry/telemetry-event-handler';
+import { TelemetryEventSource, TriggeredByNotApplicable } from 'common/extension-telemetry-events';
+import { APP_INITIALIZED } from 'electron/common/electron-telemetry-events';
+import { sendAppInitializedTelemetryEvent } from 'electron/views/device-connect-view/send-app-initialized-telemetry';
 import { IMock, It, Mock, Times } from 'typemoq';
-
-import { BaseActionPayload } from '../../../../background/actions/action-payloads';
-import { TelemetryEventSource, TriggeredByNotApplicable } from '../../../../common/extension-telemetry-events';
-import { APP_INITIALIZED } from '../../../../electron/common/electron-telemetry-events';
-import { sendAppInitializedTelemetryEvent } from '../../../../electron/views/device-connect-view/send-app-initialized-telemetry';
 
 describe('sendAppInitializedTelemetry', () => {
     let telemetryEventHandlerMock: IMock<TelemetryEventHandler>;
