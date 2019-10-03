fef376e086fa9ec2455b9e30bb8b8ec87bd48bea
JGibson2019 <jacqueline.gibson@microsoft.com>
2019-10-03 15:12:13 -0700

    Basic Window State Store Implementation to be able to update the route id between different views (#1350)
    * added a basic electron window state store, store data, and test for the store. added reference to the electron window state store to the common store names file
    
    * added window state actions and updated the window state store to have listeners and update the routeid based on these actions
    
    * simplified the ElectronWindowStateStoreData to only worry about the routeID and not the width and height of the windows
    
    * created a route
    
    * removed electron from the store name and related classes; updated window-state-store.test to use snapshot
    
    * fixed the import statement before the header information
    
    * refactored windowStateActions to just have one that changes the route id based on information contained in the payload. updated the store and appropriate test file
    
    * added the no update test case and renamed the singular window state action

---
+++ src/tests/unit/tests/electron/flux/store/window-state-store.test.ts
@@ -0,0 +1,62 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { RoutePayload } from '../../../../../../electron/flux/action/route-payloads';
+import { WindowStateActions } from '../../../../../../electron/flux/action/window-state-actions';
+import { WindowStateStore } from '../../../../../../electron/flux/store/window-state-store';
+import { WindowStateStoreData } from '../../../../../../electron/flux/types/window-state-store-data';
+import { createStoreWithNullParams, StoreTester } from '../../../../common/store-tester';
+
+describe('WindowStateStore', () => {
+    describe('constructor', () => {
+        it('has no side effects', () => {
+            const testSubject = createStoreWithNullParams(WindowStateStore);
+            expect(testSubject).toBeDefined();
+        });
+    });
+
+    it('returns device connect as the default state', () => {
+        const testSubject = new WindowStateStore(new WindowStateActions());
+        testSubject.initialize();
+
+        expect(testSubject.getState()).toMatchSnapshot();
+    });
+
+    it('changes route id from default to resultsView', () => {
+        const payload: RoutePayload = {
+            routeId: 'resultsView',
+        };
+        const expectedState: WindowStateStoreData = {
+            routeId: payload.routeId,
+        };
+
+        const initialState = createStoreWithNullParams(WindowStateStore).getDefaultState();
+
+        createStoreTesterForWindowStateActions('setRoute')
+            .withActionParam(payload)
+            .testListenerToBeCalledOnce(initialState, expectedState);
+    });
+
+    it('does not emit a change when the route is not changing', () => {
+        const payload: RoutePayload = {
+            routeId: 'deviceConnectView',
+        };
+        const expectedState: WindowStateStoreData = {
+            routeId: payload.routeId,
+        };
+
+        const initialState = createStoreWithNullParams(WindowStateStore).getDefaultState();
+
+        createStoreTesterForWindowStateActions('setRoute')
+            .withActionParam(payload)
+            .testListenerToNeverBeCalled(initialState, expectedState);
+    });
+
+    function createStoreTesterForWindowStateActions(
+        actionName: keyof WindowStateActions,
+    ): StoreTester<WindowStateStoreData, WindowStateActions> {
+        const factory = (actions: WindowStateActions) => new WindowStateStore(actions);
+
+        return new StoreTester(WindowStateActions, actionName, factory);
+    }
+});
