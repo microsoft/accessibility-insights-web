788467b2a6c43e94b54486e5bf446299ef0c3bb4
Jacqueline Gibson <jagibson@microsoft.com>
2019-10-03 11:45:43 -0700

    fixed the import statement before the header information

--- src/electron/flux/action/route-payloads.ts
+++ src/electron/flux/action/route-payloads.ts
@@ -1,7 +1,6 @@
-import { ViewRoutes } from '../types/window-state-store-data';
-
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { ViewRoutes } from '../types/window-state-store-data';
 export interface RoutePayload {
     routeId: ViewRoutes;
 }
