25e4b403dabf49f6dab3b606263644bfc9d60838
Haonan Liu <haonliu@microsoft.com>
2019-10-07 13:22:23 -0700

    lint fix

--- src/tests/unit/tests/issue-filing/unified-result-to-issue-filing-data.test.ts
+++ src/tests/unit/tests/issue-filing/unified-result-to-issue-filing-data.test.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { UnifiedResult, TargetAppData, UnifiedRule } from 'common/types/store-data/unified-data-interface';
-import { It, Mock, Times } from 'typemoq';
+import { TargetAppData, UnifiedResult, UnifiedRule } from 'common/types/store-data/unified-data-interface';
 
 import { CreateIssueDetailsTextData } from '../../../../common/types/create-issue-details-text-data';
 import { UnifiedResultToIssueFilingDataConverter } from '../../../../issue-filing/unified-result-to-issue-filing-data';
