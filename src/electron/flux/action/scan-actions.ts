a76ad289e19598098885d6615830e8a7cc9efaa0
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-10-08 10:23:58 -0700

    add scan actions

---
+++ src/electron/flux/action/scan-actions.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { Action } from 'common/flux/action';
+
+export class ScanActions {
+    public readonly scanStarted = new Action<void>();
+    public readonly scanCompleted = new Action<void>();
+}
