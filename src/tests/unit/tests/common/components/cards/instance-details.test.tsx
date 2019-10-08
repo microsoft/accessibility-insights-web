8447715e7dd420df7885afaacff76644bc66c23d
Shobhit Agarwal <shoagarw@microsoft.com>
2019-10-08 10:01:42 -0700

    update instance details test

--- src/tests/unit/tests/common/components/cards/instance-details.test.tsx
+++ src/tests/unit/tests/common/components/cards/instance-details.test.tsx
@@ -3,8 +3,9 @@
 import { InstanceDetails, InstanceDetailsDeps, InstanceDetailsProps } from 'common/components/cards/instance-details';
 import { shallow } from 'enzyme';
 import * as React from 'react';
-import { IMock, Mock } from 'typemoq';
+import { IMock, It, Mock, Times } from 'typemoq';
 
+import { CardSelectionMessageCreator } from 'common/message-creators/card-selection-message-creator';
 import {
     AllPropertyTypes,
     CardRowProps,
@@ -18,15 +19,18 @@ describe('InstanceDetails', () => {
     let props: InstanceDetailsProps;
     let deps: InstanceDetailsDeps;
     let getPropertyConfigByIdMock: IMock<(id: string) => PropertyConfiguration>;
+    let cardSelectionMessageCreatorMock: IMock<CardSelectionMessageCreator>;
     let resultStub: UnifiedResult;
     let indexStub: number;
 
     beforeEach(() => {
         getPropertyConfigByIdMock = Mock.ofInstance(_ => null);
+        cardSelectionMessageCreatorMock = Mock.ofType(CardSelectionMessageCreator);
         resultStub = exampleUnifiedResult;
         indexStub = 22;
         deps = {
             getPropertyConfigById: getPropertyConfigByIdMock.object,
+            cardSelectionMessageCreator: cardSelectionMessageCreatorMock.object,
         } as InstanceDetailsDeps;
         props = {
             deps,
@@ -36,18 +40,38 @@ describe('InstanceDetails', () => {
     });
 
     it('renders', () => {
+        setupGetPropertyConfigByIdMock();
+        cardSelectionMessageCreatorMock.setup(mock => mock.toggleCardSelection(It.isAnyString())).verifiable(Times.never());
+
+        const testSubject = shallow(<InstanceDetails {...props} />);
+        expect(testSubject.getElement()).toMatchSnapshot();
+        cardSelectionMessageCreatorMock.verifyAll();
+    });
+
+    it('dispatches the card selection message when card is clicked', () => {
+        setupGetPropertyConfigByIdMock();
+
+        cardSelectionMessageCreatorMock.setup(mock => mock.toggleCardSelection(It.isAnyString())).verifiable(Times.once());
+
+        const wrapper = shallow(<InstanceDetails {...props} />);
+        const tableElem = wrapper.find('table');
+        expect(tableElem.length).toBe(1);
+
+        tableElem.simulate('click');
+
+        cardSelectionMessageCreatorMock.verifyAll();
+    });
+
+    function getCardRowStub(name: string): ReactFCWithDisplayName<CardRowProps> {
+        return NamedFC<CardRowProps>(name, _ => null);
+    }
+
+    function setupGetPropertyConfigByIdMock(): void {
         AllPropertyTypes.forEach(propertyType => {
             const propertyConfigurationStub: PropertyConfiguration = {
                 cardRow: getCardRowStub(propertyType),
             };
             getPropertyConfigByIdMock.setup(mock => mock(propertyType)).returns(() => propertyConfigurationStub);
         });
-
-        const testSubject = shallow(<InstanceDetails {...props} />);
-        expect(testSubject.getElement()).toMatchSnapshot();
-    });
-
-    function getCardRowStub(name: string): ReactFCWithDisplayName<CardRowProps> {
-        return NamedFC<CardRowProps>(name, _ => null);
     }
 });
