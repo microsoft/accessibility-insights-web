b89bc99a25615156054cec00f8b3430e4a9b832e
Sebastian Morales <smoralesd@users.noreply.github.com>
2019-10-08 13:34:51 -0700

    feat: add more components to accessibility checks view (#1388)
    Add more content to the accessibility checks view. 
    
    Not part of this PR:
    - hook up the Cards UI
    - handle the "scanning" state

--- src/electron/views/automated-checks/components/automated-checks-view.tsx
+++ src/electron/views/automated-checks/components/automated-checks-view.tsx
@@ -1,10 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 import * as React from 'react';
 
-export class AutomatedChecksView extends React.Component {
-    public render(): JSX.Element {
-        return <>automated checks</>;
-    }
-}
+import { NamedFC } from 'common/react/named-fc';
+import { CommandBar, CommandBarProps } from './command-bar';
+import { HeaderSection } from './header-section';
+
+export type AutomatedChecksViewProps = CommandBarProps;
+
+export const AutomatedChecksView = NamedFC<AutomatedChecksViewProps>('AutomatedChecksView', props => {
+    return (
+        <>
+            <CommandBar {...props} />
+            <HeaderSection />
+        </>
+    );
+});
