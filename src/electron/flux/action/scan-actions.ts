8e3b940fb9e9c65b52afea12a17e450415574d01
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-10-09 11:57:19 -0700

    üpdate scanStarted action payload type

--- src/electron/flux/action/scan-actions.ts
+++ src/electron/flux/action/scan-actions.ts
@@ -1,9 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { Action } from 'common/flux/action';
+import { PortPayload } from 'electron/flux/action/device-action-payloads';
 
 export class ScanActions {
-    public readonly scanStarted = new Action<void>();
+    public readonly scanStarted = new Action<PortPayload>();
     public readonly scanCompleted = new Action<void>();
     public readonly scanFailed = new Action<void>();
 }
