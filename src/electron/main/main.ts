db97a16b81544584cf0ecd80d66ccc6826f6be4b
Jacqueline Gibson <jagibson@microsoft.com>
2019-10-09 11:27:01 -0700

    moved index.html to views root level and updated the reference to the html in main.ts

--- src/electron/main/main.ts
+++ src/electron/main/main.ts
@@ -15,7 +15,7 @@ const createWindow = () => {
     });
 
     mainWindow
-        .loadFile(path.resolve(__dirname, '../electron/views/device-connect-view/index.html'))
+        .loadFile(path.resolve(__dirname, '../electron/views/index.html'))
         .then(() => console.log('url loaded'))
         .catch(console.log);
 
