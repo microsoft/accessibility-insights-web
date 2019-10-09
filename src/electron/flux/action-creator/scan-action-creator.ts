75f0b68515978f4a9505ea2d599af8567fcb62f7
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-10-09 12:36:24 -0700

    update scan action creator to only invoke scanStarted action

--- src/electron/flux/action-creator/scan-action-creator.ts
+++ src/electron/flux/action-creator/scan-action-creator.ts
@@ -2,18 +2,11 @@
 // Licensed under the MIT License.
 import { TelemetryEventHandler } from 'background/telemetry/telemetry-event-handler';
 import { TelemetryEventSource } from 'common/extension-telemetry-events';
-import { SCAN_COMPLETED, SCAN_FAILED, SCAN_STARTED } from 'electron/common/electron-telemetry-events';
+import { SCAN_STARTED } from 'electron/common/electron-telemetry-events';
 import { ScanActions } from 'electron/flux/action/scan-actions';
-import { FetchScanResultsType } from 'electron/platform/android/fetch-scan-results';
-import { ScanResults } from 'electron/platform/android/scan-results';
 
 export class ScanActionCreator {
-    constructor(
-        private readonly scanActions: ScanActions,
-        private readonly fetchScanResults: FetchScanResultsType,
-        private readonly telemetryEventHandler: TelemetryEventHandler,
-        private readonly getCurrentDate: () => Date,
-    ) {}
+    constructor(private readonly scanActions: ScanActions, private readonly telemetryEventHandler: TelemetryEventHandler) {}
 
     public scan(port: number): void {
         this.telemetryEventHandler.publishTelemetry(SCAN_STARTED, {
@@ -23,57 +16,6 @@ export class ScanActionCreator {
             },
         });
 
-        this.scanActions.scanStarted.invoke(null);
-
-        const scanStartedTime = this.getCurrentDate().getTime();
-
-        this.fetchScanResults(port)
-            .then(this.scanCompleted.bind(this, scanStartedTime, port))
-            .catch(this.scanFailed.bind(this, scanStartedTime, port));
-    }
-
-    private scanCompleted(scanStartedTime: number, port: number, data: ScanResults): void {
-        const scanCompletedTime = this.getCurrentDate().getTime();
-
-        const scanDuration = scanCompletedTime - scanStartedTime;
-
-        const instanceCount = data.ruleResults.reduce((acc, cur) => {
-            if (acc[cur.status] == null) {
-                acc[cur.status] = {};
-            }
-
-            if (acc[cur.status][cur.ruleId] == null) {
-                acc[cur.status][cur.ruleId] = 1;
-            } else {
-                acc[cur.status][cur.ruleId]++;
-            }
-
-            return acc;
-        }, {});
-
-        this.telemetryEventHandler.publishTelemetry(SCAN_COMPLETED, {
-            telemetry: {
-                port,
-                scanDuration,
-                ...instanceCount,
-            },
-        });
-
-        this.scanActions.scanCompleted.invoke(null);
-    }
-
-    private scanFailed(scanStartedTime: number, port: number): void {
-        const scanCompletedTime = this.getCurrentDate().getTime();
-
-        const scanDuration = scanCompletedTime - scanStartedTime;
-
-        this.telemetryEventHandler.publishTelemetry(SCAN_FAILED, {
-            telemetry: {
-                port,
-                scanDuration,
-            },
-        });
-
-        this.scanActions.scanFailed.invoke(null);
+        this.scanActions.scanStarted.invoke({ port });
     }
 }
--- src/tests/unit/tests/electron/flux/action-creator/scan-action-creator.test.ts
+++ src/tests/unit/tests/electron/flux/action-creator/scan-action-creator.test.ts
@@ -3,15 +3,11 @@
 import { TelemetryEventHandler } from 'background/telemetry/telemetry-event-handler';
 import { TelemetryEventSource } from 'common/extension-telemetry-events';
 import { Action } from 'common/flux/action';
-import { SCAN_COMPLETED, SCAN_FAILED, SCAN_STARTED } from 'electron/common/electron-telemetry-events';
+import { SCAN_STARTED } from 'electron/common/electron-telemetry-events';
 import { ScanActionCreator } from 'electron/flux/action-creator/scan-action-creator';
 import { PortPayload } from 'electron/flux/action/device-action-payloads';
 import { ScanActions } from 'electron/flux/action/scan-actions';
-import { FetchScanResultsType } from 'electron/platform/android/fetch-scan-results';
-import { ScanResults } from 'electron/platform/android/scan-results';
-import { tick } from 'tests/unit/common/tick';
-import { ExpectedCallType, IMock, It, Mock, Times } from 'typemoq';
-import { axeRuleResultExample } from './scan-result-example';
+import { IMock, It, Mock, Times } from 'typemoq';
 
 describe('ScanActionCreator', () => {
     const port = 1111;
@@ -25,107 +21,28 @@ describe('ScanActionCreator', () => {
 
     let telemetryEventHandlerMock: IMock<TelemetryEventHandler>;
     let scanActionsMock: IMock<ScanActions>;
-    let fetchScanResultsMock: IMock<FetchScanResultsType>;
     let scanStartedMock: IMock<Action<PortPayload>>;
-    let scanCompletedMock: IMock<Action<void>>;
-    let scanFailedMock: IMock<Action<void>>;
-    let getCurrentDateMock: IMock<() => Date>;
 
     let testSubject: ScanActionCreator;
 
     beforeEach(() => {
         telemetryEventHandlerMock = Mock.ofType<TelemetryEventHandler>();
-        fetchScanResultsMock = Mock.ofType<FetchScanResultsType>();
         scanActionsMock = Mock.ofType<ScanActions>();
 
         scanStartedMock = Mock.ofType<Action<PortPayload>>();
-        scanCompletedMock = Mock.ofType<Action<void>>();
-        scanFailedMock = Mock.ofType<Action<void>>();
 
-        scanActionsMock.setup(actions => actions.scanCompleted).returns(() => scanCompletedMock.object);
         scanActionsMock.setup(actions => actions.scanStarted).returns(() => scanStartedMock.object);
-        scanActionsMock.setup(actions => actions.scanFailed).returns(() => scanFailedMock.object);
 
-        getCurrentDateMock = Mock.ofType<() => Date>();
-        getCurrentDateMock.setup(getter => getter()).returns(() => new Date(2019, 10, 8, 9, 0, 0));
-        getCurrentDateMock.setup(getter => getter()).returns(() => new Date(2019, 10, 8, 9, 2, 15));
+        testSubject = new ScanActionCreator(scanActionsMock.object, telemetryEventHandlerMock.object);
+    });
 
-        testSubject = new ScanActionCreator(
-            scanActionsMock.object,
-            fetchScanResultsMock.object,
-            telemetryEventHandlerMock.object,
-            getCurrentDateMock.object,
+    it('scans', () => {
+        testSubject.scan(port);
+
+        scanStartedMock.verify(scanStarted => scanStarted.invoke(It.isValue({ port })), Times.once());
+        telemetryEventHandlerMock.verify(
+            handler => handler.publishTelemetry(SCAN_STARTED, It.isValue(expectedScanStartedTelemetry)),
+            Times.once(),
         );
-    });
-
-    it('scans successfully', async () => {
-        fetchScanResultsMock.setup(fetch => fetch(port)).returns(() => Promise.resolve(new ScanResults(axeRuleResultExample)));
-
-        telemetryEventHandlerMock
-            .setup(handler => handler.publishTelemetry(SCAN_STARTED, It.isValue(expectedScanStartedTelemetry)))
-            .verifiable(Times.once(), ExpectedCallType.InSequence);
-
-        telemetryEventHandlerMock
-            .setup(handler =>
-                handler.publishTelemetry(
-                    SCAN_COMPLETED,
-                    It.isValue({
-                        telemetry: {
-                            port,
-                            scanDuration: 135000,
-                            PASS: {
-                                ImageViewName: 2,
-                                ColorContrast: 2,
-                                ActiveViewName: 1,
-                                EditTextValue: 1,
-                                DontMoveAccessibilityFocus: 1,
-                            },
-                            FAIL: { TouchSizeWcag: 1, EditTextName: 1, ColorContrast: 1 },
-                            INCOMPLETE: { ColorContrast: 1 },
-                        },
-                    }),
-                ),
-            )
-            .verifiable(Times.once(), ExpectedCallType.InSequence);
-
-        testSubject.scan(port);
-
-        await tick();
-
-        scanStartedMock.verify(scanStarted => scanStarted.invoke(null), Times.once());
-        scanCompletedMock.verify(scanCompleted => scanCompleted.invoke(null), Times.once());
-
-        telemetryEventHandlerMock.verifyAll();
-    });
-
-    it('scans and fail', async () => {
-        fetchScanResultsMock.setup(fetch => fetch(port)).returns(() => Promise.reject());
-
-        telemetryEventHandlerMock
-            .setup(handler => handler.publishTelemetry(SCAN_STARTED, It.isValue(expectedScanStartedTelemetry)))
-            .verifiable(Times.once(), ExpectedCallType.InSequence);
-
-        telemetryEventHandlerMock
-            .setup(handler =>
-                handler.publishTelemetry(
-                    SCAN_FAILED,
-                    It.isValue({
-                        telemetry: {
-                            port,
-                            scanDuration: 135000,
-                        },
-                    }),
-                ),
-            )
-            .verifiable(Times.once(), ExpectedCallType.InSequence);
-
-        testSubject.scan(port);
-
-        await tick();
-
-        scanStartedMock.verify(scanStarted => scanStarted.invoke(null), Times.once());
-        scanFailedMock.verify(scanCompleted => scanCompleted.invoke(null), Times.once());
-
-        telemetryEventHandlerMock.verifyAll();
     });
 });
