dd88e814a9b4703ed253bfa96742203ebd1b78fa
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-10-10 12:39:56 -0700

    convert from NamedFC to React.Component

--- src/electron/views/automated-checks/automated-checks-view.tsx
+++ src/electron/views/automated-checks/automated-checks-view.tsx
@@ -1,20 +1,24 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { TitleBar, TitleBarDeps } from 'electron/views/automated-checks/components/title-bar';
 import * as React from 'react';
 
-import { NamedFC } from 'common/react/named-fc';
-import { TitleBar, TitleBarProps } from 'electron/views/automated-checks/components/title-bar';
-import { CommandBar, CommandBarProps } from './components/command-bar';
+import { CommandBar, CommandBarDeps } from './components/command-bar';
 import { HeaderSection } from './components/header-section';
 
-export type AutomatedChecksViewProps = CommandBarProps & TitleBarProps;
+export type AutomatedChecksViewDeps = CommandBarDeps & TitleBarDeps;
+export type AutomatedChecksViewProps = {
+    deps: AutomatedChecksViewDeps;
+};
 
-export const AutomatedChecksView = NamedFC<AutomatedChecksViewProps>('AutomatedChecksView', props => {
-    return (
-        <>
-            <TitleBar deps={props.deps}></TitleBar>
-            <CommandBar {...props} />
-            <HeaderSection />
-        </>
-    );
-});
+export class AutomatedChecksView extends React.Component<AutomatedChecksViewProps> {
+    public render(): JSX.Element {
+        return (
+            <>
+                <TitleBar deps={this.props.deps}></TitleBar>
+                <CommandBar deps={this.props.deps} />
+                <HeaderSection />
+            </>
+        );
+    }
+}
