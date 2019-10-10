523e47dd66b688f22c835a32179e8e00c24ea456
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2019-10-09 17:04:06 -0700

    [WIP] Responsive Congrats Componenet

--- src/reports/components/report-sections/no-failed-instances-congrats.tsx
+++ src/reports/components/report-sections/no-failed-instances-congrats.tsx
@@ -3,27 +3,13 @@
 import * as React from 'react';
 
 import { NamedFC } from 'common/react/named-fc';
-import { InlineImage, InlineImageType } from '../inline-image';
-import {
-    reportCongrats,
-    reportCongratsHead,
-    reportCongratsInfo,
-    reportCongratsMessage,
-    reportCongratsScreen,
-} from './no-failed-instances-congrats.scss';
+import { reportCongratsHead, reportCongratsInfo, reportCongratsMessage } from './no-failed-instances-congrats.scss';
 
 export const NoFailedInstancesCongrats = NamedFC('NoFailedInstancesCongrats', () => {
     return (
-        <div className={reportCongrats} key="report-congrats">
-            <div>
-                <InlineImage imageType={InlineImageType.AdaLaptop} alt="" />
-            </div>
-            <div className={reportCongratsScreen}>
-                <div className={reportCongratsMessage}>
-                    <div className={reportCongratsHead}>Congratulations!</div>
-                    <div className={reportCongratsInfo}>No failed automated checks were found.</div>
-                </div>
-            </div>
+        <div className={reportCongratsMessage}>
+            <div className={reportCongratsHead}>Congratulations!</div>
+            <div className={reportCongratsInfo}>No failed automated checks were found.</div>
         </div>
     );
 });
