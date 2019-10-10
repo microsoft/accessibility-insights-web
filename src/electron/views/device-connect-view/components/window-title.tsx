8cef211d2b0f9f8e4607aa65c225de632c6db92f
Devaradhan Murugesan <demuruge@microsoft.com>
2019-10-10 15:02:47 -0700

    refactored to create a common window title & added title

--- src/electron/views/device-connect-view/components/window-title.tsx
+++ src/electron/views/device-connect-view/components/window-title.tsx
@@ -2,20 +2,31 @@
 // Licensed under the MIT License.
 import * as React from 'react';
 import { NamedFC } from '../../../../common/react/named-fc';
-import { headerText, windowTitle } from './window-title.scss';
+import { actionableIconsContainer, headerText, titleContainer, windowTitle } from './window-title.scss';
 
 export interface WindowTitleProps {
     title: string;
     children?: JSX.Element;
+    actionableIcons?: JSX.Element[];
+    className?: string;
 }
 
 export const WindowTitle = NamedFC<WindowTitleProps>('WindowTitle', (props: WindowTitleProps) => {
     return (
-        <header className={windowTitle}>
-            <div>
+        <header className={[windowTitle, props.className].filter(c => c != null).join(' ')}>
+            <div className={titleContainer}>
                 {props.children}
                 <h1 className={headerText}>{props.title}</h1>
             </div>
+            {getIconsContainer(props.actionableIcons)}
         </header>
     );
 });
+
+function getIconsContainer(icons?: JSX.Element[]): JSX.Element {
+    if (icons != null && icons.length > 0) {
+        return <div className={actionableIconsContainer}>{icons}</div>;
+    }
+
+    return null;
+}
