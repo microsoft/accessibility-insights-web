ecf8fd027c806ec1a09bcd0a926948fa6f625664
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-10-10 18:24:37 -0700

    render scanning dialog when status is scanning. hook up data from root

--- src/tests/unit/tests/electron/views/automated-checks/automated-checks-view.test.tsx
+++ src/tests/unit/tests/electron/views/automated-checks/automated-checks-view.test.tsx
@@ -1,25 +1,47 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { ScanActionCreator } from 'electron/flux/action-creator/scan-action-creator';
+import { ScanStatus } from 'electron/flux/types/scan-status';
 import { AutomatedChecksView, AutomatedChecksViewProps } from 'electron/views/automated-checks/automated-checks-view';
 import { shallow } from 'enzyme';
 import * as React from 'react';
 import { Mock, Times } from 'typemoq';
 
 describe('AutomatedChecksView', () => {
-    it('renders the automated checks view', () => {
-        const props: AutomatedChecksViewProps = {
-            deps: {
-                deviceConnectActionCreator: null,
-                currentWindow: null,
-                windowStateActionCreator: null,
-                scanActionCreator: Mock.ofType(ScanActionCreator).object,
-            },
-        } as AutomatedChecksViewProps;
+    describe('renders', () => {
+        it('the automated checks view', () => {
+            const props: AutomatedChecksViewProps = {
+                deps: {
+                    deviceConnectActionCreator: null,
+                    currentWindow: null,
+                    windowStateActionCreator: null,
+                    scanActionCreator: Mock.ofType(ScanActionCreator).object,
+                },
+                scanStoreData: {},
+            } as AutomatedChecksViewProps;
 
-        const wrapped = shallow(<AutomatedChecksView {...props} />);
+            const wrapped = shallow(<AutomatedChecksView {...props} />);
 
-        expect(wrapped.getElement()).toMatchSnapshot('automated checks view');
+            expect(wrapped.getElement()).toMatchSnapshot('automated checks view');
+        });
+
+        it('scanning spinner', () => {
+            const props: AutomatedChecksViewProps = {
+                deps: {
+                    deviceConnectActionCreator: null,
+                    currentWindow: null,
+                    windowStateActionCreator: null,
+                    scanActionCreator: Mock.ofType(ScanActionCreator).object,
+                },
+                scanStoreData: {
+                    status: ScanStatus.Scanning,
+                },
+            } as AutomatedChecksViewProps;
+
+            const wrapped = shallow(<AutomatedChecksView {...props} />);
+
+            expect(wrapped.getElement()).toMatchSnapshot('automated checks view');
+        });
     });
 
     it('triggers scan when first mounted', () => {
@@ -33,6 +55,7 @@ describe('AutomatedChecksView', () => {
                 scanActionCreator: scanActionCreatorMock.object,
             },
             devicePort,
+            scanStoreData: {},
         } as AutomatedChecksViewProps;
 
         shallow(<AutomatedChecksView {...props} />);
