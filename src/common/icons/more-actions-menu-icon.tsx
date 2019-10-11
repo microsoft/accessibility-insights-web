0d6f0d851afc6a7a67dec0bd985e144e4f982b21
Dan Bjorge <danielbj@microsoft.com>
2019-10-10 17:18:41 -0700

    add icon component

---
+++ src/common/icons/more-actions-menu-icon.tsx
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import * as React from 'react';
+
+import { NamedFC } from '../react/named-fc';
+
+export const MoreActionsMenuIcon = NamedFC('MoreActionsMenuIcon', () => (
+    <svg width="34" height="32" viewBox="0 0 34 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="17" cy="22" r="1.5" stroke="black" strokeOpacity="0.9" />
+        <circle cx="17" cy="16" r="1.5" stroke="black" strokeOpacity="0.9" />
+        <circle cx="17" cy="10" r="1.5" stroke="black" strokeOpacity="0.9" />
+    </svg>
+));
