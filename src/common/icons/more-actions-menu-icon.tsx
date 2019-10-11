c93efe36733027250ea0a110e5af79c560c22354
Dan Bjorge <danielbj@microsoft.com>
2019-10-10 17:22:10 -0700

    aria-hide the icon (the button has an ariaLabel)

--- src/common/icons/more-actions-menu-icon.tsx
+++ src/common/icons/more-actions-menu-icon.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { NamedFC } from '../react/named-fc';
 
 export const MoreActionsMenuIcon = NamedFC('MoreActionsMenuIcon', () => (
-    <svg width="34" height="32" viewBox="0 0 34 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="34" height="32" viewBox="0 0 34 32" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true">
         <circle cx="17" cy="22" r="1.5" stroke="black" strokeOpacity="0.9" />
         <circle cx="17" cy="16" r="1.5" stroke="black" strokeOpacity="0.9" />
         <circle cx="17" cy="10" r="1.5" stroke="black" strokeOpacity="0.9" />
