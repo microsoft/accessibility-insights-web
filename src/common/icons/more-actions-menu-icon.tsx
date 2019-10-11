cda581d7a995d7fec370367e9299d7702bfe63a3
Dan Bjorge <danielbj@microsoft.com>
2019-10-10 17:31:32 -0700

    Crop icon so it can render without shrinking in button

--- src/common/icons/more-actions-menu-icon.tsx
+++ src/common/icons/more-actions-menu-icon.tsx
@@ -5,9 +5,9 @@ import * as React from 'react';
 import { NamedFC } from '../react/named-fc';
 
 export const MoreActionsMenuIcon = NamedFC('MoreActionsMenuIcon', () => (
-    <svg width="34" height="32" viewBox="0 0 34 32" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true">
-        <circle cx="17" cy="22" r="1.5" stroke="black" strokeOpacity="0.9" />
-        <circle cx="17" cy="16" r="1.5" stroke="black" strokeOpacity="0.9" />
-        <circle cx="17" cy="10" r="1.5" stroke="black" strokeOpacity="0.9" />
+    <svg width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true">
+        <circle cx="5" cy="14" r="1.5" stroke="black" strokeOpacity="0.9" />
+        <circle cx="5" cy="8" r="1.5" stroke="black" strokeOpacity="0.9" />
+        <circle cx="5" cy="2" r="1.5" stroke="black" strokeOpacity="0.9" />
     </svg>
 ));
