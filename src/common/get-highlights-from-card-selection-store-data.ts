7a560a72ac7d9e482c2b68f5f584f457f0b29a6f
RobGallo <43080011+RobGallo@users.noreply.github.com>
2019-10-14 14:08:38 -0400

    Add getHighlightsFromCardSelectionStoreData function (#1431)
    * Add getHighlightsFromCardSelectionStoreData function
    
    The function returns a list of unified result ids which can be mapped to elements to be highlighted.
    
    * Remove selectedCardCount from CardSelectionStoreData because its value can be calculated in get-highlights-from-card-selection-store-data

---
+++ src/common/get-highlights-from-card-selection-store-data.ts
@@ -0,0 +1,72 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { flatMap, forOwn, keys } from 'lodash';
+import {
+    CardSelectionStoreData,
+    RuleExpandCollapseData,
+    RuleExpandCollapseDataDictionary,
+} from './types/store-data/card-selection-store-data';
+
+// Returns an array of result uids indicating which results should be highlighted
+export function getHighlightsFromCardSelectionStoreData(storeData: CardSelectionStoreData): string[] {
+    if (!storeData) {
+        return [];
+    }
+
+    const expandedRuleIds = getRuleIdsOfExpandedRules(storeData.rules);
+
+    if (expandedRuleIds.length === 0) {
+        return getAllResultUids(storeData.rules);
+    }
+
+    const selectedResultUids = getOnlyResultUidsFromSelectedCards(storeData.rules, expandedRuleIds);
+    if (selectedResultUids.length > 0) {
+        return selectedResultUids;
+    }
+
+    return getAllResultUidsFromRuleIdArray(storeData.rules, expandedRuleIds);
+}
+
+function getRuleIdsOfExpandedRules(ruleDictionary: RuleExpandCollapseDataDictionary): string[] {
+    if (!ruleDictionary) {
+        return [];
+    }
+
+    const expandedRuleIds: string[] = [];
+
+    forOwn(ruleDictionary, (rule, ruleId) => {
+        if (rule.isExpanded) {
+            expandedRuleIds.push(ruleId);
+        }
+    });
+
+    return expandedRuleIds;
+}
+
+function getAllResultUids(ruleDictionary: RuleExpandCollapseDataDictionary): string[] {
+    return getAllResultUidsFromRuleIdArray(ruleDictionary, keys(ruleDictionary));
+}
+
+function getAllResultUidsFromRuleIdArray(ruleDictionary: RuleExpandCollapseDataDictionary, ruleIds: string[]): string[] {
+    return flatMap(ruleIds, key => getAllResultUidsFromRule(ruleDictionary[key]));
+}
+
+function getAllResultUidsFromRule(rule: RuleExpandCollapseData): string[] {
+    return keys(rule.cards);
+}
+
+function getOnlyResultUidsFromSelectedCards(ruleDictionary: RuleExpandCollapseDataDictionary, ruleIds: string[]): string[] {
+    return flatMap(ruleIds, key => getResultUidsFromSelectedCards(ruleDictionary[key]));
+}
+
+function getResultUidsFromSelectedCards(rule: RuleExpandCollapseData): string[] {
+    const results: string[] = [];
+
+    forOwn(rule.cards, (value, key) => {
+        if (value) {
+            results.push(key);
+        }
+    });
+
+    return results;
+}
