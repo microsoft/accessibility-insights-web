23b07cf987b965c3d704c166e80ebe2d058f6816
Sebastian Morales <smoralesd@users.noreply.github.com>
2019-10-14 11:53:56 -0700

    feat(android-cards-view): wire up device disconnected popup (#1446)
    Wiring up the "Device disconnected" popup. When the user reaches the "Automated checks" view, we should the "scanning" spinner, if the scan failes, we'll be showing this popup that let's the user rescan the same device or navigate back to input a different port number.
    
    **Notes for the reviews**
    - This is only a wiring up PR, the dialog was already there
    - The only exception to the previous point is: I made the device name a bold text to make it easier to catch
    - There is a flickering on the window when navigating back to the port entry view. I had left that out of the scope of this PR as it touches a different piece of code and technically speaking, that bug was already there.

--- src/electron/views/automated-checks/automated-checks-view.tsx
+++ src/electron/views/automated-checks/automated-checks-view.tsx
@@ -4,26 +4,30 @@ import { TitleBar, TitleBarDeps } from 'electron/views/automated-checks/componen
 import * as React from 'react';
 
 import { ScanActionCreator } from 'electron/flux/action-creator/scan-action-creator';
+import { WindowStateActionCreator } from 'electron/flux/action-creator/window-state-action-creator';
+import { DeviceStoreData } from 'electron/flux/types/device-store-data';
 import { ScanStatus } from 'electron/flux/types/scan-status';
 import { ScanStoreData } from 'electron/flux/types/scan-store-data';
 import { ScanningSpinner } from 'electron/views/automated-checks/components/scanning-spinner';
+import { DeviceDisconnectedPopup } from 'electron/views/device-disconnected-popup/device-disconnected-popup';
 import { CommandBar, CommandBarDeps } from './components/command-bar';
 import { HeaderSection } from './components/header-section';
 
 export type AutomatedChecksViewDeps = CommandBarDeps &
     TitleBarDeps & {
         scanActionCreator: ScanActionCreator;
+        windowStateActionCreator: WindowStateActionCreator;
     };
 
 export type AutomatedChecksViewProps = {
     deps: AutomatedChecksViewDeps;
-    devicePort: number;
     scanStoreData: ScanStoreData;
+    deviceStoreData: DeviceStoreData;
 };
 
 export class AutomatedChecksView extends React.Component<AutomatedChecksViewProps> {
     public componentDidMount(): void {
-        this.props.deps.scanActionCreator.scan(this.props.devicePort);
+        this.props.deps.scanActionCreator.scan(this.props.deviceStoreData.port);
     }
 
     public render(): JSX.Element {
@@ -33,7 +37,22 @@ export class AutomatedChecksView extends React.Component<AutomatedChecksViewProp
                 <CommandBar deps={this.props.deps} />
                 <HeaderSection />
                 {this.renderScanning()}
+                {this.renderDeviceDisconnected()}
             </>
+        );
+    }
+
+    private renderDeviceDisconnected(): JSX.Element {
+        if (this.props.scanStoreData.status !== ScanStatus.Failed) {
+            return null;
+        }
+
+        return (
+            <DeviceDisconnectedPopup
+                deviceName={this.props.deviceStoreData.connectedDevice}
+                onConnectNewDevice={() => this.props.deps.windowStateActionCreator.setRoute({ routeId: 'deviceConnectView' })}
+                onRescanDevice={() => this.props.deps.scanActionCreator.scan(this.props.deviceStoreData.port)}
+            />
         );
     }
 
