7f32cd55753cb8b54db99ba1bd9802ab6e31a3b4
Jacqueline Gibson <jagibson@microsoft.com>
2019-10-15 14:43:34 -0700

    added a div to hold the device connect view and maintain the -webkit-app-region: drag functionality soley for this view; added the -webkit-app-region: no-drag to the automatedChecksView class so that users can select within the container

--- src/electron/views/device-connect-view/components/device-connect-view-container.tsx
+++ src/electron/views/device-connect-view/components/device-connect-view-container.tsx
@@ -9,6 +9,7 @@ import * as React from 'react';
 import { NamedFC } from 'common/react/named-fc';
 import { WindowStateStoreData } from 'electron/flux/types/window-state-store-data';
 import { DeviceStoreData } from '../../../flux/types/device-store-data';
+import { deviceConnectView } from '../device-connect-view.scss';
 import { DeviceConnectBody, DeviceConnectBodyDeps } from './device-connect-body';
 import { WindowTitle, WindowTitleDeps } from './window-title';
 
@@ -24,16 +25,18 @@ export type DeviceConnectViewContainerProps = {
 export const DeviceConnectViewContainer = NamedFC<DeviceConnectViewContainerProps>('DeviceConnectViewContainer', props => {
     return (
         <>
-            <WindowTitle title={brand} deps={props.deps} windowStateStoreData={props.windowStateStoreData}>
-                <BrandBlue />
-            </WindowTitle>
-            <DeviceConnectBody
-                deps={props.deps}
-                viewState={{
-                    deviceConnectState: props.deviceStoreData.deviceConnectState,
-                    connectedDevice: props.deviceStoreData.connectedDevice,
-                }}
-            />
+            <div className={deviceConnectView}>
+                <WindowTitle title={brand} deps={props.deps} windowStateStoreData={props.windowStateStoreData}>
+                    <BrandBlue />
+                </WindowTitle>
+                <DeviceConnectBody
+                    deps={props.deps}
+                    viewState={{
+                        deviceConnectState: props.deviceStoreData.deviceConnectState,
+                        connectedDevice: props.deviceStoreData.connectedDevice,
+                    }}
+                />
+            </div>
             <TelemetryPermissionDialog deps={props.deps} isFirstTime={props.userConfigurationStoreData.isFirstTime} />
         </>
     );
