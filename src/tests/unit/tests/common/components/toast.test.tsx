f02fa7c89c85754dbb53218718eb718cff684c5f
Haonan Liu <haonliu@microsoft.com>
2019-10-15 14:57:14 -0700

    tslint fix

--- src/tests/unit/tests/common/components/toast.test.tsx
+++ src/tests/unit/tests/common/components/toast.test.tsx
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import * as React from 'react';
 import { IMock, It, Mock, Times } from 'typemoq';
 
