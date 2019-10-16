90c6e134b009b86ba81239ebb5c10ed26a25c8db
Wahaj <32555133+waabid@users.noreply.github.com>
2019-10-16 11:53:19 -0700

    feat(card-selection): convert unified result for target page (#1478)
    * WIP
    
    * Minor modification to filtering selected/highlighted instances
    
    * Split up converter to two files & added tests for selector map helper
    
    * Added tests and separated converter tasks
    
    * Updated to use correct objects
    
    * Updated name for variable per suggestion

--- src/injected/scanner-utils.ts
+++ src/injected/scanner-utils.ts
@@ -1,5 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { CheckData } from 'injected/element-based-view-model-creator';
+
 import { createDefaultLogger } from '../common/logging/default-logger';
 import { Logger } from '../common/logging/logger';
 import { scan as scanRunner } from '../scanner/exposed-apis';
@@ -10,11 +12,7 @@ import { DictionaryStringTo } from '../types/common-types';
 
 declare var axe: any;
 
-export interface DecoratedAxeNodeResult {
-    // tslint:disable-next-line: no-reserved-keywords
-    any: FormattedCheckResult[];
-    none: FormattedCheckResult[];
-    all: FormattedCheckResult[];
+export type DecoratedAxeNodeResult = {
     status: boolean;
     ruleId: string;
     failureSummary: string;
@@ -24,7 +22,7 @@ export interface DecoratedAxeNodeResult {
     id: string;
     guidanceLinks: GuidanceLink[];
     helpUrl: string;
-}
+} & CheckData;
 
 export interface HtmlElementAxeResults {
     ruleResults: DictionaryStringTo<DecoratedAxeNodeResult>;
---
+++ src/tests/unit/tests/injected/get-decorated-axe-node.test.ts
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { HowToFixWebPropertyData } from 'common/components/cards/how-to-fix-card-row';
+import { UnifiedResult, UnifiedRule } from 'common/types/store-data/unified-data-interface';
+import { getDecoratedAxeNode } from 'injected/get-decorated-axe-node';
+import { GuidanceLink } from 'scanner/rule-to-links-mappings';
+import { exampleUnifiedResult } from 'tests/unit/tests/common/components/cards/sample-view-model-data';
+
+describe('GetDecoratedAxeNodeResult', () => {
+    test('gets relevant node', () => {
+        const unifiedResult: UnifiedResult = exampleUnifiedResult;
+        const unifiedRule: UnifiedRule = {
+            id: 'some rule',
+            guidance: [{} as GuidanceLink],
+            description: 'some description',
+            url: 'some url',
+        };
+        const selectorStub = 'some selector';
+
+        const howToFixData = unifiedResult.resolution['how-to-fix-web'] as HowToFixWebPropertyData;
+        const expectedResult = {
+            status: false,
+            ruleId: unifiedResult.ruleId,
+            failureSummary: unifiedResult.resolution.howToFixSummary,
+            selector: selectorStub,
+            guidanceLinks: unifiedRule.guidance,
+            help: unifiedRule.description,
+            helpUrl: unifiedRule.url,
+            html: unifiedResult.descriptors.snippet,
+            id: unifiedResult.uid,
+            any: howToFixData.any.map(s => {
+                return {
+                    message: s,
+                };
+            }),
+            all: howToFixData.all.map(s => {
+                return {
+                    message: s,
+                };
+            }),
+            none: howToFixData.none.map(s => {
+                return {
+                    message: s,
+                };
+            }),
+        };
+
+        expect(getDecoratedAxeNode(unifiedResult, unifiedRule, selectorStub)).toEqual(expectedResult);
+    });
+});
