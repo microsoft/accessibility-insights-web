4d0a74520dd91a442b5be1dd9edb5083b397a77d
Devaradhan Murugesan <demuruge@microsoft.com>
2019-10-16 16:50:05 -0700

    revert changes to fetch scan results

--- src/electron/platform/android/fetch-scan-results.ts
+++ src/electron/platform/android/fetch-scan-results.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import axios from 'axios';
-import { axeRuleResultExample } from 'tests/unit/tests/electron/flux/action-creator/scan-result-example';
 import { ScanResults } from './scan-results';
 
 export type FetchScanResultsType = (port: number) => Promise<ScanResults>;
@@ -10,7 +9,7 @@ export type HttpGet = typeof axios.get;
 
 export const createFetchScanResults = (httpGet: HttpGet): FetchScanResultsType => {
     return async (port: number) => {
-        //  const response = await httpGet(`http://localhost:${port}/axe/result`);
-        return new ScanResults(axeRuleResultExample);
+        const response = await httpGet(`http://localhost:${port}/axe/result`);
+        return new ScanResults(response.data);
     };
 };
