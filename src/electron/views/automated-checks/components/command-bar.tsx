ef2fa12da8515b0cc330a38a6a4d4a5db8bb6679
Sebastian Morales <smoralesd@users.noreply.github.com>
2019-10-18 14:14:05 -0700

    feat(android-cards-view): update command bar (#1515)
    We need to have the rescan button push to the right side of the "command bar" (on LTR mode).
    In the near future, we'll have buttons close to the left and others close to the right (when we add "undock screenshot" button).
    
    Additionally, our current command bar is a custom component which doesn't handle this behavior.
    
    In this PR:
    - Using office ui fabric's CommandBar to implement our custom command bar
    - Put the Rescan button on the "farItems" prop so it shows up on the right side of the command bar (on LTR mode)
    - Update styles of the command bar so it matches our figma mocks
    
    **NOTES**
    
    - I check the command bar accessibility with both JAWS and NVDA, it works properly altough the are differences on the SC behaviors, most notably, navigation between the command buttons in JAWS is done by using left/right arrows while NVDA does this with up/down arrows. This specific test was run by using the "Export to CodePen" option on CommandBar's Office UI Fabric [webpage](https://developer.microsoft.com/en-us/fabric#/controls/web/commandbar), since our command bar only have one button.
    - We only update the backround color of the command bar as well as added the box-shadow on the bottom. All the other styles (specially the ones highlighted on the screenshots) come directly from office fabric as we don't want to change that too much. The only expection is the color of the button icon which was override with some shade of gray.
    
    * use office ui fabric command bar and farItems to move rescan button to the right side
    * adjust css styles

--- src/electron/views/automated-checks/components/command-bar.tsx
+++ src/electron/views/automated-checks/components/command-bar.tsx
@@ -3,12 +3,12 @@
 import { NamedFC } from 'common/react/named-fc';
 import { ScanActionCreator } from 'electron/flux/action-creator/scan-action-creator';
 import { DeviceStoreData } from 'electron/flux/types/device-store-data';
+import { ScanStatus } from 'electron/flux/types/scan-status';
 import { ScanStoreData } from 'electron/flux/types/scan-store-data';
-import { ActionButton } from 'office-ui-fabric-react/lib/Button';
+import { CommandBar as UICommandBar, ICommandBarItemProps } from 'office-ui-fabric-react/lib/CommandBar';
 import * as React from 'react';
 
-import { ScanStatus } from 'electron/flux/types/scan-status';
-import { commandBar, rescanButton } from './command-bar.scss';
+import { buttonIcon, commandBar, menuItemButton } from './command-bar.scss';
 
 export type CommandBarDeps = {
     scanActionCreator: ScanActionCreator;
@@ -23,19 +23,22 @@ export interface CommandBarProps {
 export const CommandBar = NamedFC<CommandBarProps>('CommandBar', (props: CommandBarProps) => {
     const { deps, deviceStoreData } = props;
 
-    const onClick = () => deps.scanActionCreator.scan(deviceStoreData.port);
+    const onRescanClick = () => deps.scanActionCreator.scan(deviceStoreData.port);
 
-    return (
-        <div className={commandBar}>
-            <ActionButton
-                iconProps={{
-                    className: rescanButton,
-                    iconName: 'Refresh',
-                }}
-                onClick={onClick}
-                text="Rescan"
-                disabled={props.scanStoreData.status === ScanStatus.Scanning}
-            />
-        </div>
-    );
+    const farItems: ICommandBarItemProps[] = [
+        {
+            key: 'rescan',
+            name: 'Rescan',
+            iconProps: {
+                className: buttonIcon,
+                iconName: 'Refresh',
+            },
+            className: menuItemButton,
+            onClick: onRescanClick,
+            disabled: props.scanStoreData.status === ScanStatus.Scanning,
+        },
+    ];
+
+    const items: ICommandBarItemProps[] = []; // UICommandBar expects items to not be null (it does not check)
+    return <UICommandBar items={items} farItems={farItems} className={commandBar} />;
 });
