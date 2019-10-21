7253d72fba6d7acb2b65dd4438d15dbb78084286
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-10-21 16:38:06 -0700

    move resource server code to miscellaneous. make it more generic by introducing a config type

--- src/tests/end-to-end/setup/global-teardown.ts
+++ src/tests/end-to-end/setup/global-teardown.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as testResourceServer from './test-resource-server';
+import * as testResourceServer from '../../miscellaneous/test-resource-server/resource-server';
 
 // tslint:disable-next-line:no-default-export
 export default function(): void {
