164e80fb70047898126384c741107cbf370e4c7d
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-10-22 11:40:27 -0700

    update resource server to serve json files to mock axe-android service
    (cherry picked from commit 3fbeb94f75da5c65dafb8b68d389597782172064)

---
+++ src/tests/electron/setup/global-setup.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import * as testResourceServer from '../../miscellaneous/test-resource-server/resource-server';
+import { testResourceServerConfig } from './test-resource-server-config';
+
+// tslint:disable-next-line:no-default-export
+export default function(): void {
+    testResourceServer.startServer(testResourceServerConfig);
+}
---
+++ src/tests/electron/setup/test-resource-server-config.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { ResourceServerConfig } from 'tests/miscellaneous/test-resource-server/resource-server-config';
+
+export const testResourceServerConfig: ResourceServerConfig = {
+    port: 9052,
+    path: '../../miscellaneous/mock-axe-android',
+    extensions: ['json'],
+};
