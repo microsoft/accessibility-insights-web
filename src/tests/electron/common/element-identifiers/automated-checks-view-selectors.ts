97a62dfc3990e7ecba3536c663a73e449bfddeac
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-10-22 16:41:19 -0700

    add e2e test for automated checks view

---
+++ src/tests/electron/common/element-identifiers/automated-checks-view-selectors.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+export const AutomatedChecksViewSelectors = {
+    mainContainer: '.automated-checks-view',
+    resultSectionContainer: '.result-section',
+};
