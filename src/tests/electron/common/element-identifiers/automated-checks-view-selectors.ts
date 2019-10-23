6f4dac29258c101b70518e3a5c3dddf841f7b45f
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-10-23 16:48:57 -0700

    move selectors to proper file

--- src/tests/electron/common/element-identifiers/automated-checks-view-selectors.ts
+++ src/tests/electron/common/element-identifiers/automated-checks-view-selectors.ts
@@ -3,4 +3,11 @@
 export const AutomatedChecksViewSelectors = {
     mainContainer: '.automated-checks-view',
     resultSectionContainer: '.result-section',
+    collapsibleRuleDetailsGroup: 'div.collapsible-rule-details-group',
+    collapsibleContainerContent: '.collapsible-container-content',
+    getRuleDetailsIdSelector: (position: number) =>
+        `${AutomatedChecksViewSelectors.collapsibleRuleDetailsGroup}:nth-of-type(${position}) .rule-details-id`,
+    getLiFailuresSelector: (position: number) => `${AutomatedChecksViewSelectors.collapsibleRuleDetailsGroup}:nth-of-type(${position}) li`,
+    getCollapseExpandButtonByGroupPosition: (position: number) =>
+        `${AutomatedChecksViewSelectors.collapsibleRuleDetailsGroup}:nth-of-type(${position}) button`,
 };
