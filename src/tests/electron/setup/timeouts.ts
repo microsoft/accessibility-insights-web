c23b8b351d620771962b67448b449b34483e80e0
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-10-23 16:49:12 -0700

    new timeout for waitForVisible

--- src/tests/electron/setup/timeouts.ts
+++ src/tests/electron/setup/timeouts.ts
@@ -2,4 +2,7 @@
 // Licensed under the MIT License.
 
 // How long to allow an entire test to execute
-export const DEFAULT_ELECTRON_TEST_TIMEOUT_MS: number = 20000;
+export const DEFAULT_ELECTRON_TEST_TIMEOUT_MS = 20000;
+
+// How long to wait for an element to be visible
+export const DEFAULT_WAIT_FOR_ELEMENT_TO_BE_VISIBLE_TIMEOUT_MS = 5000;
