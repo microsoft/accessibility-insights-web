ab669320a885cf1a2d13d4cb4bbc2ca8be1fea64
Lisa Li <lisli@microsoft.com>
2019-10-23 17:22:28 -0700

    updated names

---
+++ src/tests/unit/tests/electron/views/screenshot/screenshot.test.tsx
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { Screenshot, ScreenshotProps } from 'electron/views/screenshot/screenshot';
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+describe('Screenshot', () => {
+    it('renders', () => {
+        const props: ScreenshotProps = {
+            encodedImage: 'test-image-string',
+            altText: 'test-alt-caption',
+        };
+        const wrapper = shallow(<Screenshot {...props} />);
+
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+});
