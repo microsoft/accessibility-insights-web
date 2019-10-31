ed660beb67e2a2b712b34300c455080d9fc73072
JGibson2019 <jacqueline.gibson@microsoft.com>
2019-10-31 14:59:12 -0700

    feat: Screenshot view (#1589)
    * created the screenshot view component; added the screenshot view as a subview of automated-checks and added CSS changes to render the automated checks result and screenshot side by side
    
    * updated automated checks view screenshot and fixed tslint errors in screenshot-view
    
    * added tabIndex to the h1 so it is reachable via keyboard navigation
    
    * created a basic screenshot view test

---
+++ src/electron/views/screenshot/screenshot-view.tsx
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { NamedFC } from 'common/react/named-fc';
+import * as React from 'react';
+
+export type ScreenshotViewProps = {};
+export const ScreenshotView = NamedFC<ScreenshotViewProps>('ScreenshotView', props => {
+    return (
+        <div>
+            <h1 tabIndex={0}>This is the screenshot view</h1>
+        </div>
+    );
+});
