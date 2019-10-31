ba03234fdb79de3a114ca4de1ac6fb3518bcd1b8
Dan Bjorge <danielbj@microsoft.com>
2019-10-31 15:40:07 -0700

    refactor: move electron e2e tests to use "view controllers" similar to web's page controllers (#1592)
    #### Description of changes
    
    In preparation for adding more e2e tests in the and-screenshot feature, updating the electron E2E tests to use the same type of "controller" model that we liked in the web e2e tests.
    
    #### Pull request checklist
    
    - [n/a] Addresses an existing issue: Fixes #0000
    - [n/a] Added relevant unit test for your changes. (`yarn test`)
    - [n/a] Verified code coverage for the changes made. Check coverage report at: `<rootDir>/test-results/unit/coverage`
    - [x] Ran precheckin (`yarn precheckin`)
    - [x] Added a suitable semantic tag to PR title (fix, chore, feat., refactor). Check workflow guide at: `<rootDir>/docs/workflow.md` <!-- Please leave it blank if you are unsure -->
    - [n/a] (UI changes only) Added screenshots/GIFs to description above
    - [n/a] (UI changes only) Verified usability with NVDA/JAWS

---
+++ src/tests/electron/common/element-identifiers/device-connection-dialog-selectors.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+export const DeviceConnectionDialogSelectors = {
+    portNumber: '.port-number-field',
+    cancelButton: '.footer-button-cancel',
+    startButton: '.footer-button-start',
+    validateButton: '.button-validate-port',
+};
---
+++ src/tests/electron/common/view-controllers/automated-checks-view-controller.ts
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import * as WebDriverIO from 'webdriverio';
+import { AutomatedChecksViewSelectors } from '../element-identifiers/automated-checks-view-selectors';
+import { ViewController } from './view-controller';
+
+export class AutomatedChecksViewController extends ViewController {
+    constructor(client: WebDriverIO.Client<void>) {
+        super(client);
+    }
+
+    public async queryRuleGroups(): Promise<any[]> {
+        return this.client.$$(AutomatedChecksViewSelectors.collapsibleRuleDetailsGroup);
+    }
+
+    public async collapseRuleGroupAtPosition(position: number): Promise<void> {
+        await this.client.click(AutomatedChecksViewSelectors.getCollapseExpandButtonByGroupPosition(position));
+    }
+
+    public async waitForVisible(): Promise<void> {
+        await this.waitForSelectorVisible(AutomatedChecksViewSelectors.mainContainer);
+    }
+}
