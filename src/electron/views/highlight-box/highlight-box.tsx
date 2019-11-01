9bc0cb67cc46488f3df4de1ee9f2f3b65124fa6a
Lisa Li <lisli@microsoft.com>
2019-10-31 17:02:02 -0700

    simplify highlight box props

--- src/electron/views/highlight-box/highlight-box.tsx
+++ src/electron/views/highlight-box/highlight-box.tsx
@@ -1,44 +1,33 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { NamedFC } from 'common/react/named-fc';
+import { BoundingRectangle } from 'electron/platform/android/scan-results';
 import { highlightBox, highlightBoxLabel } from 'electron/views/highlight-box/highlight-box.scss';
-import { BoxConfig } from 'injected/visualization/formatter';
 import * as React from 'react';
 import { CSSProperties } from 'react';
 
 export interface HighlightBoxDeps {}
 export interface HighlightBoxProps {
     deps: HighlightBoxDeps;
-    boxConfig: BoxConfig;
-    width: string;
-    height: string;
-    top: string;
-    left: string;
+    boundingRectangle: BoundingRectangle;
 }
 
 export const HighlightBox = NamedFC<HighlightBoxProps>('HighlightBox', props => {
-    const { width, height, top, left, boxConfig } = props;
-    if (boxConfig == null) {
-        return null;
-    }
-    const boxStyles: CSSProperties = {
-        background: boxConfig.background,
-        outline: boxConfig.outline,
-        width: width,
-        height: height,
-        top: top,
-        left: left,
-    };
+    const { boundingRectangle } = props;
 
-    const labelStyles: CSSProperties = {
-        color: boxConfig.fontColor,
+    const width = boundingRectangle.right - boundingRectangle.left;
+    const height = boundingRectangle.bottom - boundingRectangle.top;
+
+    const boxStyles: CSSProperties = {
+        width: `${width}px`,
+        height: `${height}px`,
+        top: `${boundingRectangle.top}px`,
+        left: `${boundingRectangle.left}px`,
     };
 
     return (
         <div className={highlightBox} style={boxStyles}>
-            <div className={highlightBoxLabel} style={labelStyles}>
-                {boxConfig.text}
-            </div>
+            <div className={highlightBoxLabel}>!</div>
         </div>
     );
 });
