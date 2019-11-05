a31c10e0ae53974f8ee0b1d2570a48149c599f10
Dave Tryon <dtryon@microsoft.com>
2019-11-05 14:02:51 -0800

    Unit tests for ReportExportComponentPropsFactory

--- src/tests/unit/tests/DetailsView/components/report-export-component-props-factory.test.ts
+++ src/tests/unit/tests/DetailsView/components/report-export-component-props-factory.test.ts
@@ -2,20 +2,28 @@
 // Licensed under the MIT License.
 import { AssessmentsProvider } from 'assessments/types/assessments-provider';
 import { AssessmentStoreData } from 'common/types/store-data/assessment-result-data';
+import { CardRuleResultsByStatus } from 'common/types/store-data/card-view-model';
 import { FeatureFlagStoreData } from 'common/types/store-data/feature-flag-store-data';
 import { TabStoreData } from 'common/types/store-data/tab-store-data';
+import { VisualizationScanResultData } from 'common/types/store-data/visualization-scan-result-data';
 import { DetailsViewActionMessageCreator } from 'DetailsView/actions/details-view-action-message-creator';
 import { DetailsViewCommandBarDeps, DetailsViewCommandBarProps } from 'DetailsView/components/details-view-command-bar';
 import { ReportExportComponentProps } from 'DetailsView/components/report-export-component';
-import { getReportExportComponentPropsForAssessment } from 'DetailsView/components/report-export-component-props-factory';
+import {
+    getReportExportComponentPropsForAssessment,
+    getReportExportComponentPropsForAutomatedChecks,
+} from 'DetailsView/components/report-export-component-props-factory';
 import { ReportGenerator } from 'reports/report-generator';
+import { ScanResults } from 'scanner/iruleresults';
 import { IMock, Mock, MockBehavior } from 'typemoq';
 
 describe('ReportExportComponentPropsFactory', () => {
     const theDate = new Date(2019, 2, 12, 9, 0);
+    const theTimestamp = 'test timestamp';
     const thePageTitle = 'command-bar-test-tab-title';
     const theDescription = 'test description';
     const theGeneratorOutput = 'generator output';
+    const thePageUrl = 'test page url';
 
     let assessmentsProviderMock: IMock<AssessmentsProvider>;
     let featureFlagStoreData: FeatureFlagStoreData;
@@ -24,12 +32,16 @@ describe('ReportExportComponentPropsFactory', () => {
     let assessmentStoreData: AssessmentStoreData;
     let reportGeneratorMock: IMock<ReportGenerator>;
     let renderStartOver: boolean;
+    let visualizationScanResultData: VisualizationScanResultData;
+    let cardRuleResultsByStatus: CardRuleResultsByStatus;
+    let scanResult: ScanResults;
 
     beforeEach(() => {
         featureFlagStoreData = {};
         actionMessageCreatorMock = Mock.ofType(DetailsViewActionMessageCreator, MockBehavior.Strict);
         tabStoreData = {
             title: thePageTitle,
+            url: thePageUrl,
         } as TabStoreData;
         renderStartOver = true;
         assessmentStoreData = {
@@ -40,6 +52,38 @@ describe('ReportExportComponentPropsFactory', () => {
         } as AssessmentStoreData;
         assessmentsProviderMock = Mock.ofType<AssessmentsProvider>(undefined, MockBehavior.Loose);
         reportGeneratorMock = Mock.ofType<ReportGenerator>(undefined, MockBehavior.Strict);
+        cardRuleResultsByStatus = null;
+        scanResult = null;
+    });
+
+    function getProps(): DetailsViewCommandBarProps {
+        const deps = {
+            detailsViewActionMessageCreator: actionMessageCreatorMock.object,
+            getCurrentDate: () => theDate,
+            reportGenerator: reportGeneratorMock.object,
+            getDateFromTimestamp: value => theDate,
+        } as DetailsViewCommandBarDeps;
+
+        visualizationScanResultData = {
+            issues: {
+                scanResult: scanResult,
+            },
+        } as VisualizationScanResultData;
+
+        return {
+            deps,
+            featureFlagStoreData,
+            actionMessageCreator: actionMessageCreatorMock.object,
+            assessmentsProvider: assessmentsProviderMock.object,
+            tabStoreData,
+            renderStartOver,
+            assessmentStoreData,
+            visualizationScanResultData,
+            ruleResultsByStatus: cardRuleResultsByStatus,
+        } as DetailsViewCommandBarProps;
+    }
+
+    function setAssessmentReportGenerator(): void {
         reportGeneratorMock
             .setup(x =>
                 x.generateAssessmentReport(
@@ -51,32 +95,71 @@ describe('ReportExportComponentPropsFactory', () => {
                 ),
             )
             .returns(() => theGeneratorOutput);
-    });
+    }
 
-    function getProps(): DetailsViewCommandBarProps {
-        const deps = {
-            detailsViewActionMessageCreator: actionMessageCreatorMock.object,
-            getCurrentDate: () => theDate,
-            reportGenerator: reportGeneratorMock.object,
-        } as DetailsViewCommandBarDeps;
+    function setAutomatedChecksReportGenerator(): void {
+        reportGeneratorMock
+            .setup(x =>
+                x.generateFastPassAutomateChecksReport(
+                    scanResult,
+                    theDate,
+                    tabStoreData.title,
+                    tabStoreData.url,
+                    cardRuleResultsByStatus,
+                    theDescription,
+                ),
+            )
+            .returns(() => theGeneratorOutput);
+    }
 
-        return {
-            deps,
-            featureFlagStoreData,
-            actionMessageCreator: actionMessageCreatorMock.object,
-            assessmentsProvider: assessmentsProviderMock.object,
-            tabStoreData,
-            renderStartOver,
-            assessmentStoreData,
-        } as DetailsViewCommandBarProps;
+    function setCardsUiFlag(flag: boolean): void {
+        featureFlagStoreData['universalCardsUI'] = true;
     }
 
     test('getReportExportComponentPropsForAssessment expected properties are set', () => {
+        setAssessmentReportGenerator();
         const props = getProps();
         const rendered: ReportExportComponentProps = getReportExportComponentPropsForAssessment(props);
 
         expect(rendered.exportResultsType).toBe('Assessment');
         expect(rendered.getExportDescription()).toBe(theDescription);
+        expect(rendered.pageTitle).toBe(thePageTitle);
+        expect(rendered.scanDate).toBe(theDate);
+        expect(rendered.reportGenerator).toBe(reportGeneratorMock.object);
+        expect(rendered.htmlGenerator(theDescription)).toBe(theGeneratorOutput);
+
+        reportGeneratorMock.verifyAll();
+        actionMessageCreatorMock.verifyAll();
+    });
+
+    test('getReportExportComponentPropsForAutomatedChecks, CardsUI is false, props is null', () => {
+        const props = getProps();
+        const rendered: ReportExportComponentProps = getReportExportComponentPropsForAutomatedChecks(props);
+
+        expect(rendered).toBeNull();
+    });
+
+    test('getReportExportComponentPropsForAutomatedChecks, CardsUI is true, scanResults is null, props is null', () => {
+        setCardsUiFlag(true);
+        const props = getProps();
+        const rendered: ReportExportComponentProps = getReportExportComponentPropsForAutomatedChecks(props);
+
+        expect(rendered).toBeNull();
+    });
+
+    test('getReportExportComponentPropsForAutomatedChecks, CardsUI is true, scanResults is not null, expected properties are set', () => {
+        scanResult = {
+            timestamp: theTimestamp,
+        } as ScanResults;
+        cardRuleResultsByStatus = {} as CardRuleResultsByStatus;
+
+        setCardsUiFlag(true);
+        setAutomatedChecksReportGenerator();
+        const props = getProps();
+        const rendered: ReportExportComponentProps = getReportExportComponentPropsForAutomatedChecks(props);
+
+        expect(rendered.exportResultsType).toBe('AutomatedChecks');
+        expect(rendered.getExportDescription()).toBe('');
         expect(rendered.pageTitle).toBe(thePageTitle);
         expect(rendered.scanDate).toBe(theDate);
         expect(rendered.reportGenerator).toBe(reportGeneratorMock.object);
