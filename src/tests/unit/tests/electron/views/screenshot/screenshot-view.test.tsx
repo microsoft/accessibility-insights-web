3dda5178d1de4774e37d18b5924e4a1d52ae6f20
Lisa Li <lisli@microsoft.com>
2019-11-05 17:43:55 -0800

    remove obsolete tests from screenshot view and updated snapshot

--- src/tests/unit/tests/electron/views/screenshot/screenshot-view.test.tsx
+++ src/tests/unit/tests/electron/views/screenshot/screenshot-view.test.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { ScreenshotData } from 'common/types/store-data/unified-data-interface';
 import { ScreenshotView } from 'electron/views/screenshot/screenshot-view';
 import { ScreenshotViewModel } from 'electron/views/screenshot/screenshot-view-model';
 import { shallow } from 'enzyme';
@@ -11,18 +10,6 @@ describe('ScreenshotView', () => {
         it('when passed a value for screenshotData', () => {
             const viewModel: ScreenshotViewModel = {
                 screenshotData: { base64PngData: 'test-base-64-png-data' },
-                highlightBoxRectangles: [],
-                deviceName: null,
-            };
-            const wrapper = shallow(<ScreenshotView viewModel={viewModel} />);
-
-            expect(wrapper.getElement()).toMatchSnapshot();
-        });
-
-        const emptyScreenshotDataCases: ScreenshotData[] = [null, undefined, {} as ScreenshotData];
-        it.each(emptyScreenshotDataCases)('when passed empty screenshotData %p', (screenshotDataCase: ScreenshotData) => {
-            const viewModel: ScreenshotViewModel = {
-                screenshotData: screenshotDataCase,
                 highlightBoxRectangles: [],
                 deviceName: null,
             };
