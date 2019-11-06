50d5265a8cead99556206c1b4d43ca6ea388452b
Dave Tryon <dtryon@microsoft.com>
2019-11-05 19:46:59 -0800

    Add tests for assessment command bar

--- src/DetailsView/components/assessment-command-bar.tsx
+++ src/DetailsView/components/assessment-command-bar.tsx
@@ -5,10 +5,15 @@ import * as React from 'react';
 import { NamedFC } from 'common/react/named-fc';
 import { ReportExportComponentProps } from 'DetailsView/components/report-export-component';
 import { getReportExportComponentPropsForAssessment } from 'DetailsView/components/report-export-component-props-factory';
-import { CommandBarProps } from './details-view-command-bar';
+import { CommandBarProps, ReportExportComponentPropertyConverter } from './details-view-command-bar';
 import { DetailsViewCommandBar } from './details-view-command-bar';
 
 export const AssessmentCommandBar = NamedFC<CommandBarProps>('AssessmentCommandBar', props => {
-    const reportExportComponentProps: ReportExportComponentProps = getReportExportComponentPropsForAssessment(props);
+    let converter: ReportExportComponentPropertyConverter = props.reportExportComponentPropertyFactory;
+    if (converter === null) {
+        converter = getReportExportComponentPropsForAssessment;
+    }
+    const reportExportComponentProps: ReportExportComponentProps = converter(props);
+
     return <DetailsViewCommandBar reportExportComponentProps={reportExportComponentProps} renderStartOver={true} {...props} />;
 });
--- src/tests/unit/tests/DetailsView/components/assessment-command-bar.test.tsx
+++ src/tests/unit/tests/DetailsView/components/assessment-command-bar.test.tsx
@@ -4,14 +4,30 @@ import { shallow } from 'enzyme';
 import * as React from 'react';
 
 import { AssessmentCommandBar } from 'DetailsView/components/assessment-command-bar';
-import { CommandBarProps } from '../../../../../DetailsView/components/details-view-command-bar';
+import { CommandBarProps } from 'DetailsView/components/details-view-command-bar';
 
-describe('getAssessmentCommandBar', () => {
+function getProps(returnCommandBarProps: boolean): CommandBarProps {
+    const reportExportComponentProps = returnCommandBarProps ? {} : null;
+
+    const props = {
+        reportExportComponentPropertyFactory: commandBarProps => reportExportComponentProps,
+    } as CommandBarProps;
+
+    return props;
+}
+
+describe('getAutomatedChecksCommandBar reportExportComponentPropertyFactory returns null', () => {
+    it('should return command bar with renderExportComponentProps as null and renderStartOver as true', () => {
+        const props = getProps(false);
+        const actual = shallow(<AssessmentCommandBar {...props} />);
+
+        expect(actual.getElement()).toMatchSnapshot();
+    });
+});
+
+describe('getAutomatedChecksCommandBar reportExportComponentPropertyFactory returns non-null', () => {
     it('should return command bar with renderExportComponentProps as non-null and renderStartOver as true', () => {
-        // TODO : Create properties!
-        const props = {
-            assessmentsProvider: null,
-        } as CommandBarProps;
+        const props = getProps(true);
         const actual = shallow(<AssessmentCommandBar {...props} />);
 
         expect(actual.getElement()).toMatchSnapshot();
