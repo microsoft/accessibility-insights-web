8b2cd70ef162aca2bca0b4965a10292f8b1b67f8
Lisa Li <lisli@microsoft.com>
2019-11-05 18:11:53 -0800

    remove HighlightBoxDeps from highlight-box.test and removed unnecessary beforeEach

--- src/tests/unit/tests/electron/views/screenshot/highlight-box.test.tsx
+++ src/tests/unit/tests/electron/views/screenshot/highlight-box.test.tsx
@@ -3,30 +3,21 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { BoundingRectangle } from 'electron/platform/android/scan-results';
-import { HighlightBox, HighlightBoxDeps, HighlightBoxProps } from 'electron/views/screenshot/highlight-box';
+import { HighlightBox, HighlightBoxProps } from 'electron/views/screenshot/highlight-box';
 import { shallow } from 'enzyme';
 import * as React from 'react';
 
 describe('HighlightBox', () => {
-    let deps: HighlightBoxDeps;
-    let boundingRectangle: BoundingRectangle;
-    let props: HighlightBoxProps;
+    const boundingRectangle: BoundingRectangle = {
+        top: 100,
+        bottom: 250,
+        left: 20,
+        right: 400,
+    };
 
-    beforeEach(() => {
-        deps = {};
-
-        boundingRectangle = {
-            top: 100,
-            bottom: 250,
-            left: 20,
-            right: 400,
-        };
-
-        props = {
-            deps,
-            boundingRectangle,
-        };
-    });
+    const props: HighlightBoxProps = {
+        boundingRectangle,
+    };
 
     it('renders with appropriate position and dimensions', () => {
         const testObject = shallow(<HighlightBox {...props} />);
