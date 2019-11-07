df99c566f229c7593082682d4b4dbe4487eac284
Sebastian Morales <smoralesd@users.noreply.github.com>
2019-11-07 11:44:25 -0800

    refactor: promisify inject api calls (#1640)
    As part of the permissions works, I'd like to convert `executeScriptInTab` and `insertCSSInTab` from using callbacks to use promises. This will help on how we handle permissions when injecting in a future PR.
    
    As a note: we already converted `getUserData`, `setUserData` and `removeUserData` (all part of `StorageAdpater` type) successfully in the past.

--- src/background/injector/content-script-injector.ts
+++ src/background/injector/content-script-injector.ts
@@ -1,7 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { BrowserAdapter } from '../../common/browser-adapters/browser-adapter';
-import { PromiseFactory } from '../../common/promises/promise-factory';
+import { BrowserAdapter } from 'common/browser-adapters/browser-adapter';
+import { PromiseFactory } from 'common/promises/promise-factory';
+import { flatten } from 'lodash';
 
 export class ContentScriptInjector {
     public static readonly jsFiles: string[] = ['bundle/injected.bundle.js'];
@@ -13,49 +14,36 @@ export class ContentScriptInjector {
     constructor(private readonly browserAdapter: BrowserAdapter, private readonly promiseFactory: PromiseFactory) {}
 
     public injectScripts(tabId: number): Promise<void> {
-        const inject = new Promise<null>(resolve => {
-            // We need the JS to be injected before we can continue (ie, before we resolve the promise),
-            // because the tab can't receive other messages until that's done, but it's okay for the CSS
-            // to keep loading in the background after-the-fact, so it's fire-and-forget.
-            this.injectCssFilesConcurrently(tabId, ContentScriptInjector.cssFiles);
-            this.injectJsFilesInOrder(tabId, ContentScriptInjector.jsFiles, resolve);
-        });
+        // We need the JS to be injected before we can continue (ie, before we resolve the promise),
+        // because the tab can't receive other messages until that's done, but it's okay for the CSS
+        // to keep loading in the background after-the-fact, so it's fire-and-forget.
+        this.injectCssFilesConcurrently(tabId);
+        const inject = Promise.all([this.injectJsFilesInOrder(tabId)]).then(() => Promise.resolve());
 
         return this.promiseFactory.timeout(inject, ContentScriptInjector.timeoutInMilliSec);
     }
 
-    private injectCssFilesConcurrently(tabId: number, files: string[]): void {
-        ContentScriptInjector.cssFiles.forEach(file => {
-            this.injectCssFile(tabId, file);
+    private injectCssFilesConcurrently(tabId: number): void {
+        ContentScriptInjector.cssFiles.forEach(file => this.injectCssFile(tabId, file));
+    }
+
+    private injectJsFilesInOrder(tabId: number): Promise<any[]> {
+        const files = ContentScriptInjector.jsFiles;
+        return Promise.all(files.map(file => this.injectJsFile(tabId, file))).then(results => flatten(results));
+    }
+
+    private injectJsFile(tabId: number, file: string): Promise<any[]> {
+        return this.browserAdapter.executeScriptInTab(tabId, {
+            allFrames: true,
+            file,
+            runAt: 'document_start',
         });
     }
 
-    private injectJsFilesInOrder(tabId: number, files: string[], callback: Function): void {
-        if (files.length > 0) {
-            this.injectJsFile(tabId, files[0], () => {
-                this.injectJsFilesInOrder(tabId, files.slice(1, files.length), callback);
-            });
-        } else {
-            callback();
-        }
-    }
-
-    private injectJsFile(tabId: number, file: string, callback?: (result: any[]) => void): void {
-        this.browserAdapter.executeScriptInTab(
-            tabId,
-            {
-                allFrames: true,
-                file: file,
-                runAt: 'document_start',
-            },
-            callback,
-        );
-    }
-
-    private injectCssFile(tabId: number, file: string): void {
-        this.browserAdapter.insertCSSInTab(tabId, {
+    private injectCssFile(tabId: number, file: string): Promise<void> {
+        return this.browserAdapter.insertCSSInTab(tabId, {
             allFrames: true,
-            file: file,
+            file,
         });
     }
 }
