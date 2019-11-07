651ec0f9e7d44e5b1afbbbbc0046a63d001312d6
Wahaj Abid <waabid@microsoft.com>
2019-11-07 11:20:11 -0800

    Removed unused telemetry event name

--- src/common/extension-telemetry-events.ts
+++ src/common/extension-telemetry-events.ts
@@ -56,7 +56,6 @@ export const CONTENT_PAGE_OPENED: string = 'contentPageOpened';
 export const CONTENT_HYPERLINK_OPENED: string = 'contentHyperLinkOpened';
 export const CARD_SELECTION_TOGGLED: string = 'cardSelectionToggled';
 export const RULE_EXPANSION_TOGGLED: string = 'ruleExpansionToggled';
-export const CARDS_VISUAL_HELPER_TOGGLED: string = 'cardsVisualHelperToggled';
 
 export const TriggeredByNotApplicable: TriggeredBy = 'N/A';
 export type TriggeredBy = 'mouseclick' | 'keypress' | 'shortcut' | 'N/A';
