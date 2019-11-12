304924363836ceb021757b9e5d401cbf36fb583e
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-11-12 11:24:08 -0800

    add injection action creator

---
+++ src/background/actions/injection-action-creator.ts
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { Messages } from 'common/messages';
+import { InjectionActions } from '../actions/injection-actions';
+import { Interpreter } from '../interpreter';
+
+export class InjectionActionCreator {
+    constructor(private readonly interpreter: Interpreter, private readonly injectionActions: InjectionActions) {}
+
+    public registerCallbacks(): void {
+        this.interpreter.registerTypeToPayloadCallback(Messages.Visualizations.State.InjectionStarted, this.injectionStarted);
+        this.interpreter.registerTypeToPayloadCallback(Messages.Visualizations.State.InjectionCompleted, this.injectionCompleted);
+    }
+
+    private injectionStarted = (): void => this.injectionActions.injectionStarted.invoke(null);
+
+    private injectionCompleted = (): void => this.injectionActions.injectionCompleted.invoke(null);
+}
---
+++ src/tests/unit/tests/background/actions/injection-action-creator.test.ts
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { InjectionActionCreator } from 'background/actions/injection-action-creator';
+import { InjectionActions } from 'background/actions/injection-actions';
+import { Messages } from 'common/messages';
+import { createActionMock, createInterpreterMock } from 'tests/unit/tests/background/global-action-creators/action-creator-test-helpers';
+import { IMock, Mock } from 'typemoq';
+
+describe('InjectionActionCreator', () => {
+    it('handles InjectionStarted message', () => {
+        const injectionStartedMock = createActionMock(null);
+        const actionsMock = createActionsMock('injectionStarted', injectionStartedMock.object);
+        const interpreterMock = createInterpreterMock(Messages.Visualizations.State.InjectionStarted, null);
+
+        const testSubject = new InjectionActionCreator(interpreterMock.object, actionsMock.object);
+
+        testSubject.registerCallbacks();
+
+        injectionStartedMock.verifyAll();
+    });
+
+    it.only('handles InjectionCompleted message', () => {
+        const injectionCompletedMock = createActionMock<void>(null);
+        const actionsMock = createActionsMock('injectionCompleted', injectionCompletedMock.object);
+        const interpreterMock = createInterpreterMock(Messages.Visualizations.State.InjectionCompleted, null);
+
+        const testSubject = new InjectionActionCreator(interpreterMock.object, actionsMock.object);
+
+        testSubject.registerCallbacks();
+
+        injectionCompletedMock.verifyAll();
+    });
+
+    function createActionsMock<ActionName extends keyof InjectionActions>(
+        actionName: ActionName,
+        action: InjectionActions[ActionName],
+    ): IMock<InjectionActions> {
+        const actionsMock = Mock.ofType<InjectionActions>();
+        actionsMock.setup(actions => actions[actionName]).returns(() => action);
+        return actionsMock;
+    }
+});
