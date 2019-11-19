491057e1d89d1b7bd0cba4e14aec7423fd977412
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-11-18 17:22:29 -0800

    normalize imports

--- src/DetailsView/components/preview-features-container.tsx
+++ src/DetailsView/components/preview-features-container.tsx
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { DisplayableStrings } from 'common/constants/displayable-strings';
 import { NamedFC } from 'common/react/named-fc';
+import { DisplayableFeatureFlag } from 'common/types/store-data/displayable-feature-flag';
+import { FeatureFlagStoreData } from 'common/types/store-data/feature-flag-store-data';
 import * as React from 'react';
 
-import { DisplayableStrings } from '../../common/constants/displayable-strings';
-import { DisplayableFeatureFlag } from '../../common/types/store-data/displayable-feature-flag';
-import { FeatureFlagStoreData } from '../../common/types/store-data/feature-flag-store-data';
 import { DetailsViewActionMessageCreator } from '../actions/details-view-action-message-creator';
 import { PreviewFeatureFlagsHandler } from '../handlers/preview-feature-flags-handler';
 import { NoDisplayableFeatureFlagMessage } from './no-displayable-preview-features-message';
