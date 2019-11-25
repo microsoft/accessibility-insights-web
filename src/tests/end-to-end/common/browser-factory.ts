ffc2ed1f6caa17486ae1f74cd463f7219f4cfd38
Dan Bjorge <danielbj@microsoft.com>
2019-11-25 11:07:26 -0800

    Use smaller window size to improve rendering speed in docker containers

--- src/tests/end-to-end/common/browser-factory.ts
+++ src/tests/end-to-end/common/browser-factory.ts
@@ -67,6 +67,10 @@ async function launchNewBrowser(browserInstanceId: string): Promise<Puppeteer.Br
         headless: false,
         defaultViewport: null,
         args: [
+            // We use the smallest size we support both because we want to ensure functionality works there
+            // and also because it improves test runtime to render fewer pixels, especially in environments
+            // that can't hardware-accelerate rendering (eg, docker)
+            '--window-size=320x240',
             // Required to work around https://github.com/GoogleChrome/puppeteer/pull/774
             `--disable-extensions-except=${extensionPath}`,
             `--load-extension=${extensionPath}`,
