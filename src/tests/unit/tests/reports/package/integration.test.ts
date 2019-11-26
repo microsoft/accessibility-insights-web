78d5020b2711840f62e2671a59fe5c9863aef56d
Mark Reay <markreay@gmail.com>
2019-11-25 19:04:50 -0800

    Cleaned up duplication in integration test

--- src/tests/unit/tests/reports/package/integration.test.ts
+++ src/tests/unit/tests/reports/package/integration.test.ts
@@ -1,10 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { AxeResults } from 'axe-core';
 import { reporterFactory } from 'reports/package/reporter-factory';
 import { scanIssues } from 'tests/unit/tests/reports/package/scans/scan-issues';
 import { scanNoIssues } from 'tests/unit/tests/reports/package/scans/scan-no-issues';
 
-describe('ReporterFactory', () => {
+describe('report package integration', () => {
     const options = {
         browserVersion: 'BROWSER_VERSION',
         browserSpec: 'BROWSER_SPEC',
@@ -16,15 +17,14 @@ describe('ReporterFactory', () => {
     // constructs interfering with snapshot determinism.
     const rexScriptBlock = /\<script\>.*\<\/script\>/ms;
 
-    it('works end-to-end with no issues scan', () => {
-        const reporter = reporterFactory();
-        const html = reporter.fromAxeResult(scanNoIssues, options).asHTML().replace(rexScriptBlock, '');
-        expect(html).toMatchSnapshot();
-    });
+    const scans = [
+        { scan: scanIssues, name: 'with issues' },
+        { scan: scanNoIssues, name: 'with no issues' },
+    ];
 
-    it('works end-to-end with issues scan', () => {
+    scans.forEach(({ scan, name }) => it(name, () => {
         const reporter = reporterFactory();
-        const html = reporter.fromAxeResult(scanIssues, options).asHTML().replace(rexScriptBlock, '');
+        const html = reporter.fromAxeResult(scan as AxeResults, options).asHTML().replace(rexScriptBlock, '');
         expect(html).toMatchSnapshot();
-    });
+    }));
 });
