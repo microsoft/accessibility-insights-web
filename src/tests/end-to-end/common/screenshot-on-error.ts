617c5adc80868582bfa30c7aee974a71cfa9ca3f
Dan Bjorge <danielbj@microsoft.com>
2019-11-26 15:44:29 -0800

    Add withTracing helper to page controller

---
+++ src/tests/end-to-end/common/screenshot-on-error.ts
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import * as Puppeteer from 'puppeteer';
+import { prepareTestResultFilePath } from './prepare-test-result-file-path';
+
+async function takeScreenshot(pageInstance: Puppeteer.Page): Promise<void> {
+    const path = await prepareTestResultFilePath('failure-screenshots', 'png');
+    await pageInstance.screenshot({ path, fullPage: true });
+
+    // We don't log this until after screenshot() succeeds to avoid misleading logs if it throws
+    console.log(`Screenshot file is located at: ${path}`);
+}
+
+export async function screenshotOnError<T>(pageInstance: Puppeteer.Page, wrappedFn: () => Promise<T>): Promise<T> {
+    try {
+        return await wrappedFn();
+    } catch (originalError) {
+        try {
+            await takeScreenshot(pageInstance);
+        } catch (secondaryTakeScreenshotError) {
+            console.error(
+                `screenshotOnError: Detected an error, and then *additionally* hit a second error while trying to take a screenshot of the page state after the first error.\n` +
+                    `screenshotOnError: The secondary error from taking the screenshot is: ${secondaryTakeScreenshotError.stack}\n` +
+                    `screenshotOnError: rethrowing the original error...`,
+            );
+        }
+        throw originalError;
+    }
+}
