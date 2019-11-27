0963e51a7b9771e0b90ce4f16fbfea10ba31b9f1
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-11-27 14:45:30 -0800

    update content, update test filename, re-create snapshot file

--- src/DetailsView/components/no-content-available.tsx
+++ src/DetailsView/components/no-content-available.tsx
@@ -14,7 +14,7 @@ export const NoContentAvailable = NamedFC('NoContentAvailable', () => {
             <ul>
                 <li>If the target page was closed, you can close this tab or reuse it for something else.</li>
                 <li>
-                    If the url was change on the target page, you can reactivate this tab by pressing{' '}
+                    If the URL was change on the target page, you can reactivate this tab by pressing{' '}
                     <Markup.Term>{productName}</Markup.Term> extension icon on the browser toolbar or by using the extension shortcuts.
                 </li>
             </ul>
