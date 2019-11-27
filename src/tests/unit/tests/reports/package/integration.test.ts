107aa238c04a628cb1db5782059389da77012ed5
Mark Reay <markreay@gmail.com>
2019-11-27 15:28:58 -0800

    No code change. Just formatted the snapshot. (#1770)

--- src/tests/unit/tests/reports/package/integration.test.ts
+++ src/tests/unit/tests/reports/package/integration.test.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 import { AxeResults } from 'axe-core';
 import { reporterFactory } from 'reports/package/reporter-factory';
+import { formatHtmlForSnapshot } from 'tests/end-to-end/common/element-snapshot-formatter';
 import { scanIssues } from 'tests/unit/tests/reports/package/scans/scan-issues';
 import { scanNoIssues } from 'tests/unit/tests/reports/package/scans/scan-no-issues';
 
@@ -30,7 +31,7 @@ describe('report package integration', () => {
             description,
             scanContext,
         };
-        const html = reporter.fromAxeResult(parameters).asHTML().replace(rexScriptBlock, '');
+        const html = formatHtmlForSnapshot(reporter.fromAxeResult(parameters).asHTML().replace(rexScriptBlock, ''));
         expect(html).toMatchSnapshot();
     }));
 });
