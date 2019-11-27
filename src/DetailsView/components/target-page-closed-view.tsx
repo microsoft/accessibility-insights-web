8ce3097ef3f7025b32e2f3b37f951dbd79e3ba0c
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-11-27 14:15:42 -0800

    update NoContentAvailable instructions

--- src/DetailsView/components/target-page-closed-view.tsx
+++ src/DetailsView/components/target-page-closed-view.tsx
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import * as Markup from 'assessments/markup';
 import { NamedFC } from 'common/react/named-fc';
+import { productName } from 'content/strings/application';
 import * as React from 'react';
 
 import { targetPageClosed } from './target-page-closed-view.scss';
@@ -9,7 +11,13 @@ export const NoContentAvailable = NamedFC('NoContentAvailable', () => {
     return (
         <main className={targetPageClosed}>
             <h1>No content available</h1>
-            <p>The target page was closed. You can close this tab or reuse it for something else.</p>
+            <ul>
+                <li>If the target page was closed, you can close this tab or reuse it for something else.</li>
+                <li>
+                    If the url was change on the target page, you can reactivate this tab by pressing{' '}
+                    <Markup.Term>{productName}</Markup.Term> extension icon on the browser toolbar or by using the extension shortcuts.
+                </li>
+            </ul>
         </main>
     );
 });
