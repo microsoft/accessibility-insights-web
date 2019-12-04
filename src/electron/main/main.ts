ad0950ba701e7dd5a479b57e956d086667aadbbd
Shobhit Agarwal <shoagarw@microsoft.com>
2019-12-04 14:43:34 -0800

    remove comment

--- src/electron/main/main.ts
+++ src/electron/main/main.ts
@@ -41,11 +41,8 @@ const createWindow = () => {
         enableDevMode(mainWindow);
     });
 
-    // Emitted when the window is closed.
     mainWindow.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
+        // Dereference the window object, to force garbage collection
         mainWindow = null;
     });
 
