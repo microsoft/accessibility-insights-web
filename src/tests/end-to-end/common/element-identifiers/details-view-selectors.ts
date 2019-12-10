43c43748656de105eb9ed91a1dcaf5cb15bb3c1a
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-12-10 13:26:32 -0800

    add start over button selector for e2e

--- src/tests/end-to-end/common/element-identifiers/details-view-selectors.ts
+++ src/tests/end-to-end/common/element-identifiers/details-view-selectors.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { overviewHeadingAutomationId } from 'DetailsView/components/overview-content/overview-heading';
+import { startOverAutomationId } from 'DetailsView/components/start-over-component-factory';
 
 export const detailsViewSelectors = {
     previewFeaturesPanel: '.preview-features-panel',
@@ -12,6 +13,9 @@ export const detailsViewSelectors = {
 
     gearButton: '.gear-options-icon',
     settingsButton: 'button[name="Settings"]',
+
+    automatedChecksResultSection: '.result-section', // should change this for data-automation-id
+    startOverButton: `[data-automation-id=${startOverAutomationId}]`,
 };
 
 export const overviewSelectors = {
