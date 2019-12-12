a25ba9e4d052a3e50c3cb3389cc0b7870dd3e9ea
Lisa Li <lisli@microsoft.com>
2019-12-11 17:35:20 -0800

    format fix in global action hub

--- src/background/actions/global-action-hub.ts
+++ src/background/actions/global-action-hub.ts
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { PermissionsStateActions } from 'background/actions/permissions-state-actions';
 import { FeatureFlagActions } from '../actions/feature-flag-actions';
 import { LaunchPanelStateActions } from '../actions/launch-panel-state-action';
 import { AssessmentActions } from './assessment-actions';
 import { CommandActions } from './command-actions';
 import { ScopingActions } from './scoping-actions';
 import { UserConfigurationActions } from './user-configuration-actions';
-import { PermissionsStateActions } from 'background/actions/permissions-state-actions';
 
 export class GlobalActionHub {
     public commandActions: CommandActions;
