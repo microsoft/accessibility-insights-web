609a089786a25fb3f08cfd21e99cc23a42235f5d
Lisa Li <lisli@microsoft.com>
2019-12-13 14:40:25 -0800

    create permissions state tracker

---
+++ src/background/permissions-state-tracker.ts
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { Interpreter } from 'background/interpreter';
+import { BrowserAdapter } from 'common/browser-adapters/browser-adapter';
+import { Message } from 'common/message';
+import { Messages } from 'common/messages';
+import { Permissions } from 'webextension-polyfill-ts';
+
+export class PermissionsStateTracker {
+    constructor(private browserAdapter: BrowserAdapter, private interpreter: Interpreter) {}
+
+    public initialize(): void {
+        this.browserAdapter.addListenerOnPermissionsAdded(this.notifyChange);
+        this.browserAdapter.addListenerOnPermissionsRemoved(this.notifyChange);
+        this.notifyChange();
+    }
+
+    public notifyChange(): void {
+        const allUrlAndFilePermissions: Permissions.Permissions = { origins: ['*://*/*'] };
+
+        const permissionsState = this.browserAdapter.containsPermissions(allUrlAndFilePermissions);
+
+        const message: Message = {
+            messageType: Messages.PermissionsState.PermissionsStateChanged,
+            payload: permissionsState,
+            tabId: null,
+        };
+
+        this.interpreter.interpret(message);
+    }
+}
