f5a8bac0e2410186f50cd395692b8d5fc0f84c3a
Karanbir Singh <karansin@microsoft.com>
2019-12-12 17:27:02 -0800

    Add basic logging for autoUpdater

--- src/electron/auto-update/auto-updater-client.ts
+++ src/electron/auto-update/auto-updater-client.ts
@@ -1,8 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import log from 'electron-log';
 
 export class AutoUpdaterClient {
-    constructor(private readonly autoUpdater) {}
+    constructor(private readonly autoUpdater) {
+        log.transports.file.level = 'info';
+        autoUpdater.logger = log;
+    }
 
     public check = async () => {
         return await this.autoUpdater.checkForUpdatesAndNotify();
