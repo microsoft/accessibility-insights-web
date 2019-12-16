3cbd35c7bc789ab27463c7a455562627fb42bd33
Karanbir Singh <karansin@microsoft.com>
2019-12-16 12:47:36 -0800

    Add screenshots for electron as well

--- src/tests/electron/common/view-controllers/view-controller.ts
+++ src/tests/electron/common/view-controllers/view-controller.ts
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import * as fs from 'fs';
 import { DEFAULT_WAIT_FOR_ELEMENT_TO_BE_VISIBLE_TIMEOUT_MS } from 'tests/electron/setup/timeouts';
 import * as WebDriverIO from 'webdriverio';
+import { screenshotOnError } from '../../../end-to-end/common/screenshot-on-error';
 
 export interface ElementController {
     keys(keys: string): Promise<void>;
@@ -16,18 +18,25 @@ export abstract class ViewController {
         // semantics than Puppeteer; in particular, it requires the element be in the viewport
         // but doesn't scroll the page to the element, so it's easy for it to fail in ways that
         // are dependent on the test environment.
-        await this.client.waitForExist(selector, timeout);
+        await this.screenshotOnError(async () => this.client.waitForExist(selector, timeout));
     }
 
     public async click(selector: string): Promise<void> {
-        await this.client.click(selector);
+        await this.screenshotOnError(async () => this.client.click(selector));
     }
 
     public async isEnabled(selector: string): Promise<boolean> {
-        return await this.client.isEnabled(selector);
+        return await this.screenshotOnError(async () => this.client.isEnabled(selector));
     }
 
     public findElement(selector: string): ElementController {
         return this.client.element(selector);
+    }
+
+    private async screenshotOnError<T>(wrappedFunction: () => Promise<T>): Promise<T> {
+        return await screenshotOnError(
+            path => this.client.browserWindow.capturePage().then(buffer => fs.writeFileSync(path, buffer)),
+            wrappedFunction,
+        );
     }
 }
