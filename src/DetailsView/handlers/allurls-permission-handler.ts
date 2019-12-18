026d35558bdeb5e01259dcef6df5b7cde64d4626
Wahaj Abid <waabid@microsoft.com>
2019-12-18 15:55:10 -0800

    fixed tests and a param typing

--- src/DetailsView/handlers/allurls-permission-handler.ts
+++ src/DetailsView/handlers/allurls-permission-handler.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { BrowserAdapter } from 'common/browser-adapters/browser-adapter';
+import { SupportedMouseEvent } from 'common/telemetry-data-factory';
 import { DetailsViewActionMessageCreator } from 'DetailsView/actions/details-view-action-message-creator';
-import { SyntheticEvent } from 'react';
 
 export const allUrlAndFilePermissions = { origins: ['*://*/*'] };
 
@@ -12,7 +12,7 @@ export class AllUrlsPermissionHandler {
         private readonly actionMessageCreator: Pick<DetailsViewActionMessageCreator, 'setAllUrlsPermissionState'>,
     ) {}
 
-    public requestAllUrlsPermission = async (e: SyntheticEvent, onSuccess: () => void) => {
+    public requestAllUrlsPermission = async (e: SupportedMouseEvent, onSuccess: () => void) => {
         const newAllUrlsPermissionState = await this.browserAdapter.requestPermissions(allUrlAndFilePermissions);
         this.actionMessageCreator.setAllUrlsPermissionState(e, newAllUrlsPermissionState);
 
