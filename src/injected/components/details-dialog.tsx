8c65d176c980e56b6c1b3f605e2c1b015ad57273
Jacqueline Gibson <jagibson@microsoft.com>
2019-12-18 13:15:06 -0800

    added section tags around the labels so they will be read on NVDA; this passes for Narrator as well

--- src/injected/components/details-dialog.tsx
+++ src/injected/components/details-dialog.tsx
@@ -186,9 +186,11 @@ export class DetailsDialog extends React.Component<DetailsDialogProps, DetailsDi
         const ruleNameID = 'rule-name';
 
         return (
-            <div className="insights-dialog-rule-name" aria-labelledby={ruleNameID}>
-                {this.renderSectionTitle('Rule name', ruleNameID)}
-                <NewTabLink href={fixUrl(rule.helpUrl)}>{rule.ruleId}</NewTabLink>
+            <div className="insights-dialog-rule-name">
+                <section aria-labelledby={ruleNameID}>
+                    {this.renderSectionTitle('Rule name', ruleNameID)}
+                    <NewTabLink href={fixUrl(rule.helpUrl)}>{rule.ruleId}</NewTabLink>
+                </section>
             </div>
         );
     }
@@ -201,9 +203,13 @@ export class DetailsDialog extends React.Component<DetailsDialogProps, DetailsDi
         const successTitleId = 'success-criteria';
 
         return (
-            <div className="insights-dialog-success-criteria" aria-labelledby={successTitleId}>
-                {this.renderSectionTitle(sectionTitle, successTitleId)}
-                <GuidanceLinks links={ruleGuidanceLinks} />
+            <div className="insights-dialog-success-criteria">
+                <section aria-labelledby={successTitleId}>
+                    {this.renderSectionTitle(sectionTitle, successTitleId)}
+                    <div>
+                        <GuidanceLinks links={ruleGuidanceLinks} />
+                    </div>
+                </section>
             </div>
         );
     }
