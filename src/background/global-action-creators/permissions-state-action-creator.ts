e00484f4889b4058a34986e9fc88d818b14cabf0
Dan Bjorge <danielbj@microsoft.com>
2019-12-17 16:18:42 -0800

    feat(permissions): Implement and wire up permissions state action creator (#1883)
    * Implement and wire up permissions state action creator
    
    * switch to baseUrl imports in new class
    
    * Use common helper for createInterpreterMock
    
    * clean up imports

---
+++ src/background/global-action-creators/permissions-state-action-creator.ts
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { PermissionsStateActions } from 'background/actions/permissions-state-actions';
+import { Interpreter } from 'background/interpreter';
+import { getStoreStateMessage, Messages } from 'common/messages';
+import { StoreNames } from 'common/stores/store-names';
+
+export class PermissionsStateActionCreator {
+    constructor(
+        private readonly interpreter: Interpreter,
+        private readonly permissionsStateActions: PermissionsStateActions,
+    ) {}
+
+    public registerCallbacks(): void {
+        this.interpreter.registerTypeToPayloadCallback(
+            getStoreStateMessage(StoreNames.PermissionsStateStore),
+            this.onGetCurrentState,
+        );
+        this.interpreter.registerTypeToPayloadCallback(
+            Messages.PermissionsState.SetPermissionsState,
+            this.onSetPermissionsState,
+        );
+    }
+
+    private onGetCurrentState = (): void => {
+        this.permissionsStateActions.getCurrentState.invoke(null);
+    };
+
+    private onSetPermissionsState = (payload: boolean): void => {
+        this.permissionsStateActions.setPermissionsState.invoke(payload);
+    };
+}
---
+++ src/tests/unit/tests/background/global-action-creators/permissions-state-action-creator.test.ts
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { PermissionsStateActions } from 'background/actions/permissions-state-actions';
+import { PermissionsStateActionCreator } from 'background/global-action-creators/permissions-state-action-creator';
+import { getStoreStateMessage, Messages } from 'common/messages';
+import { StoreNames } from 'common/stores/store-names';
+import { IMock, Mock } from 'typemoq';
+
+import { createActionMock, createInterpreterMock } from './action-creator-test-helpers';
+
+describe('PermissionsStateActionCreator', () => {
+    let permissionsStateActionsMock: IMock<PermissionsStateActions>;
+
+    beforeEach(() => {
+        permissionsStateActionsMock = Mock.ofType<PermissionsStateActions>();
+    });
+
+    it('handles getStoreState message', () => {
+        const expectedMessage = getStoreStateMessage(StoreNames.PermissionsStateStore);
+        const interpreterMock = createInterpreterMock(expectedMessage, null);
+        const getCurrentStateMock = createActionMock(null);
+        setupActionsMock('getCurrentState', getCurrentStateMock.object);
+        const testSubject = new PermissionsStateActionCreator(interpreterMock.object, permissionsStateActionsMock.object);
+
+        testSubject.registerCallbacks();
+
+        getCurrentStateMock.verifyAll();
+    });
+
+    it.each([true, false])('handles SetPermissionsState message for payload %p', (payload: boolean) => {
+        const expectedMessage = Messages.PermissionsState.SetPermissionsState;
+        const interpreterMock = createInterpreterMock(expectedMessage, payload);
+        const setPermissionsStateMock = createActionMock(payload);
+        setupActionsMock('setPermissionsState', setPermissionsStateMock.object);
+        const testSubject = new PermissionsStateActionCreator(interpreterMock.object, permissionsStateActionsMock.object);
+
+        testSubject.registerCallbacks();
+
+        setPermissionsStateMock.verifyAll();
+    });
+
+    const setupActionsMock = <ActionName extends keyof PermissionsStateActions>(
+        actionName: ActionName,
+        action: PermissionsStateActions[ActionName],
+    ) => {
+        permissionsStateActionsMock.setup(actions => actions[actionName]).returns(() => action);
+    };
+});
