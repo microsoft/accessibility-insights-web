bedab2ed3aab166cc23de90f4573687238b979d5
lisli1 <48259897+lisli1@users.noreply.github.com>
2019-12-18 17:01:52 -0800

    feat(permissions): create BrowserPermissionsTracker (#1865)
    Created BrowserPermissionsTracker to check for all URL and all file permissions on the extension when it initializes and whenever permissions are added or removed.

---
+++ src/background/browser-permissions-tracker.ts
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { Interpreter } from 'background/interpreter';
+import { BrowserAdapter } from 'common/browser-adapters/browser-adapter';
+import { Logger } from 'common/logging/logger';
+import { Message } from 'common/message';
+import { Messages } from 'common/messages';
+import { Permissions } from 'webextension-polyfill-ts';
+
+export const allUrlAndFilePermissions: Permissions.Permissions = { origins: ['*://*/*'] };
+export const permissionsCheckErrorMessage: string =
+    'Error occurred while checking browser permissions';
+
+export class BrowserPermissionsTracker {
+    constructor(
+        private browserAdapter: BrowserAdapter,
+        private interpreter: Interpreter,
+        private readonly logger: Logger,
+    ) {}
+
+    public async initialize(): Promise<void> {
+        this.browserAdapter.addListenerOnPermissionsAdded(this.updatePermissionState);
+        this.browserAdapter.addListenerOnPermissionsRemoved(this.updatePermissionState);
+        await this.updatePermissionState();
+    }
+
+    private updatePermissionState = async (): Promise<void> => {
+        let payload: boolean;
+
+        try {
+            payload = await this.browserAdapter.containsPermissions(allUrlAndFilePermissions);
+        } catch (error) {
+            payload = false;
+            this.logger.log(permissionsCheckErrorMessage);
+        } finally {
+            const message: Message = {
+                messageType: Messages.PermissionsState.SetPermissionsState,
+                payload: payload,
+            };
+
+            this.interpreter.interpret(message);
+        }
+    };
+}
