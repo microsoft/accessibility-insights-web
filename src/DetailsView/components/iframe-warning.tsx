fbb634407b791d311c8d8cbc0272caf4dc7369e8
Wahaj Abid <waabid@microsoft.com>
2019-12-19 11:12:59 -0800

    updated learn more here link to point to FAQ

--- src/DetailsView/components/iframe-warning.tsx
+++ src/DetailsView/components/iframe-warning.tsx
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { NewTabLink } from 'common/components/new-tab-link';
 import { NamedFC } from 'common/react/named-fc';
 import { SupportedMouseEvent } from 'common/telemetry-data-factory';
 import { VisualizationType } from 'common/types/visualization-type';
@@ -16,7 +17,7 @@ export const IframeWarning = NamedFC<IframeWarningProps>('IframeWarning', props 
     <>
         There are iframes in the target page. To have complete results,{' '}
         <Link onClick={props.onAllowPermissionsClick}>give Accessibility Insights additional permissions</Link>; this will trigger a re-scan{' '}
-        of the test. <Link onClick={console.log}>Learn more here.</Link>
+        of the test. <NewTabLink href={'https://accessibilityinsights.io/docs/en/faq'}>Learn more here.</NewTabLink>
     </>
 ));
 
