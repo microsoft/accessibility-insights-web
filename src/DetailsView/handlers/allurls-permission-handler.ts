b971df3ed6038fa5500d5366e95e9c0becfd2716
Wahaj Abid <waabid@microsoft.com>
2019-12-18 17:36:28 -0800

    removed unnecessary variable

--- src/DetailsView/handlers/allurls-permission-handler.ts
+++ src/DetailsView/handlers/allurls-permission-handler.ts
@@ -1,10 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { allUrlAndFilePermissions } from 'background/browser-permissions-tracker';
 import { BrowserAdapter } from 'common/browser-adapters/browser-adapter';
 import { SupportedMouseEvent } from 'common/telemetry-data-factory';
 import { DetailsViewActionMessageCreator } from 'DetailsView/actions/details-view-action-message-creator';
-
-export const allUrlAndFilePermissions = { origins: ['*://*/*'] };
 
 export class AllUrlsPermissionHandler {
     constructor(
