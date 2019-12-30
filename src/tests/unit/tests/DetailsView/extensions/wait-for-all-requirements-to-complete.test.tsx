f1b6944d201f3e11c5c04a735e084e13c8d19387
Sebastian Morales <smoralesd@users.noreply.github.com>
2019-12-30 11:36:55 -0800

    fix: scanning spinner is not announced on assessment's automated checks (#1926)
    Currently, Assessment -> Automated checks shows a "scanning" spinner while we run axe. We are using Office Fabric `<Spinner>` which does not get properly announce (it does not get announced at all).
    
    In this PR, we are switching to use `<ScanningSpinner>` to get a proper announcement. This is the same component we are already using in the new cards UI (so it's already being use on electron app and fast pass -> automated checks)

--- src/tests/unit/tests/DetailsView/extensions/wait-for-all-requirements-to-complete.test.tsx
+++ src/tests/unit/tests/DetailsView/extensions/wait-for-all-requirements-to-complete.test.tsx
@@ -1,11 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { ScanningSpinner } from 'common/components/scanning-spinner/scanning-spinner';
+import { WithAssessmentTestResult } from 'DetailsView/components/assessment-view';
+import { waitForAllRequirementsToComplete } from 'DetailsView/extensions/wait-for-all-requirements-to-complete';
 import { shallow } from 'enzyme';
-import { Spinner } from 'office-ui-fabric-react';
 import * as React from 'react';
-
-import { WithAssessmentTestResult } from '../../../../../DetailsView/components/assessment-view';
-import { waitForAllRequirementsToComplete } from '../../../../../DetailsView/extensions/wait-for-all-requirements-to-complete';
 
 describe('WaitForAllRequirementsToComplete', () => {
     const zeroPercent = {
@@ -30,21 +29,21 @@ describe('WaitForAllRequirementsToComplete', () => {
 
     it('renders spinner when zeroPercent', () => {
         const rendered = shallow(<Extension {...zeroPercent}>INSIDE</Extension>);
-        const spinner = rendered.find(Spinner);
+        const spinner = rendered.find(ScanningSpinner);
         expect(spinner.prop('label')).toEqual('Scanning 0%');
         expect(rendered.debug()).toMatchSnapshot();
     });
 
     it('renders spinner when fiftyPercent', () => {
         const rendered = shallow(<Extension {...fiftyPercent}>INSIDE</Extension>);
-        const spinner = rendered.find(Spinner);
+        const spinner = rendered.find(ScanningSpinner);
         expect(spinner.prop('label')).toEqual('Scanning 50%');
         expect(rendered.debug()).toMatchSnapshot();
     });
 
     it('renders children when oneHundredPercent', () => {
         const rendered = shallow(<Extension {...oneHundredPercent}>INSIDE</Extension>);
-        const spinner = rendered.find(Spinner);
+        const spinner = rendered.find(ScanningSpinner);
         expect(spinner.exists()).toEqual(false);
         expect(rendered.text()).toEqual('INSIDE');
         expect(rendered.debug()).toMatchSnapshot();
