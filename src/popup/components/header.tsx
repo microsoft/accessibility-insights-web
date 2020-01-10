3589899f7796e790c1d9a91aeea43055c93d02e6
Sebastian Morales <sebastian.moralesd@gmail.com>
2020-01-10 15:23:10 -0800

    clean up header component

--- src/popup/components/header.tsx
+++ src/popup/components/header.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { css } from '@uifabric/utilities';
 import { NamedFC } from 'common/react/named-fc';
 import * as React from 'react';
 import * as styles from './header.scss';
@@ -14,19 +13,9 @@ export interface HeaderProps {
 export const Header = NamedFC<HeaderProps>('Header', props => {
     return (
         <header className={styles.launchPanelHeader}>
-            <div className="ms-Grid-row">
-                <div
-                    role="heading"
-                    aria-level={1}
-                    className="ms-Grid-col ms-u-sm10 ms-fontColor-neutralPrimary ms-font-xl old"
-                >
-                    {props.title}
-                </div>
-                {props.children}
-            </div>
-            <div className={css(styles.headerSubtitle, 'ms-fontWeight-semilight ms-fontSize-xs')}>
-                {props.subtitle}
-            </div>
+            <h1 className={styles.title}>{props.title}</h1>
+            <div>{props.children}</div>
+            <div className={styles.subtitle}>{props.subtitle}</div>
         </header>
     );
 });
