3291b86ff8bdda73a63370b4bd35bad91064fac2
Sebastian Morales <sebastian.moralesd@gmail.com>
2020-01-22 14:24:19 -0800

    remove unused require

--- deploy/Gruntfile.js
+++ deploy/Gruntfile.js
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-const path = require('path');
 const YAML = require('js-yaml');
 
 module.exports = function(grunt) {
