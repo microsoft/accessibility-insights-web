8c0f2067b292f0b0f52722d8127b3c5feb5f0ff2
Sebastian Morales <smoralesd@users.noreply.github.com>
2020-01-23 10:39:23 -0800

    chore: update line width for some subfolders under src/common (#2032)
    This PR changes files on some subfolders under `src/common/` to comply with our new prettier config of 100 line width.

--- src/common/message-creators/store-action-message-creator-impl.ts
+++ src/common/message-creators/store-action-message-creator-impl.ts
@@ -5,7 +5,10 @@ import { ActionMessageDispatcher } from 'common/message-creators/types/dispatche
 import { StoreActionMessageCreator } from './store-action-message-creator';
 
 export class StoreActionMessageCreatorImpl implements StoreActionMessageCreator {
-    constructor(private readonly getStateMessages: string[], private readonly dispatcher: ActionMessageDispatcher) {}
+    constructor(
+        private readonly getStateMessages: string[],
+        private readonly dispatcher: ActionMessageDispatcher,
+    ) {}
 
     public getAllStates(): void {
         this.getStateMessages.forEach(message => this.dispatcher.dispatchType(message));
