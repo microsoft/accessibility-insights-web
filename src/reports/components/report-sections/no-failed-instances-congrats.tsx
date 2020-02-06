1b5fc97be45962588eec6b314a68558e102696e9
Sebastian Morales <sebastian.moralesd@gmail.com>
2020-02-05 16:18:17 -0800

    update NoFailedInstancesCongrats component to include sleeping ada

--- src/reports/components/report-sections/no-failed-instances-congrats.tsx
+++ src/reports/components/report-sections/no-failed-instances-congrats.tsx
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as React from 'react';
-
 import { NamedFC } from 'common/react/named-fc';
+import * as React from 'react';
+import { InlineImage, InlineImageType } from 'reports/components/inline-image';
 import * as styles from './no-failed-instances-congrats.scss';
 
 export const NoFailedInstancesCongrats = NamedFC('NoFailedInstancesCongrats', () => {
@@ -10,6 +10,7 @@ export const NoFailedInstancesCongrats = NamedFC('NoFailedInstancesCongrats', ()
         <div className={styles.reportCongratsMessage}>
             <div className={styles.reportCongratsHead}>Congratulations!</div>
             <div className={styles.reportCongratsInfo}>No failed automated checks were found.</div>
+            <InlineImage imageType={InlineImageType.SleepingAda} alt="" />
         </div>
     );
 });
