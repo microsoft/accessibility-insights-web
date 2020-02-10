d89f6fcd2019d9f7beebcb9a69362dd77eeda82f
Sebastian Morales <sebastian.moralesd@gmail.com>
2020-02-10 13:57:13 -0800

    move settings-panel tests under details-view-overlay test folder

--- src/tests/unit/tests/DetailsView/components/details-view-overlay/settings-panel/settings/high-contrast/high-contrast-settings.test.tsx
+++ src/tests/unit/tests/DetailsView/components/details-view-overlay/settings-panel/settings/high-contrast/high-contrast-settings.test.tsx
@@ -1,17 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { UserConfigMessageCreator } from 'common/message-creators/user-config-message-creator';
+import { UserConfigurationStoreData } from 'common/types/store-data/user-configuration-store';
+import { HighContrastSettings } from 'DetailsView/components/details-view-overlay/settings-panel/settings/high-contrast/high-contrast-settings';
+import { SettingsDeps, SettingsProps } from 'DetailsView/components/details-view-overlay/settings-panel/settings/settings-props';
 import { shallow } from 'enzyme';
 import { Toggle } from 'office-ui-fabric-react';
 import * as React from 'react';
 import { Mock, Times } from 'typemoq';
-
-import { UserConfigMessageCreator } from '../../../../../../../../common/message-creators/user-config-message-creator';
-import { UserConfigurationStoreData } from '../../../../../../../../common/types/store-data/user-configuration-store';
-import { HighContrastSettings } from '../../../../../../../../DetailsView/components/details-view-overlay/settings-panel/settings/high-contrast/high-contrast-settings';
-import {
-    SettingsDeps,
-    SettingsProps,
-} from '../../../../../../../../DetailsView/components/details-view-overlay/settings-panel/settings/settings-props';
 
 describe('HighContrastSettings', () => {
     const enableStates = [true, false];
--- src/tests/unit/tests/DetailsView/components/details-view-overlay/settings-panel/settings/telemetry/telemetry-settings.test.tsx
+++ src/tests/unit/tests/DetailsView/components/details-view-overlay/settings-panel/settings/telemetry/telemetry-settings.test.tsx
@@ -1,18 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { EnableTelemetrySettingDescription } from 'common/components/enable-telemetry-setting-description';
+import { NewTabLink } from 'common/components/new-tab-link';
+import { UserConfigMessageCreator } from 'common/message-creators/user-config-message-creator';
+import { UserConfigurationStoreData } from 'common/types/store-data/user-configuration-store';
+import {
+    TelemetrySettings,
+    TelemetrySettingsProps,
+} from 'DetailsView/components/details-view-overlay/settings-panel/settings/telemetry/telemetry-settings';
+import { GenericToggle } from 'DetailsView/components/generic-toggle';
 import { shallow } from 'enzyme';
 import { Toggle } from 'office-ui-fabric-react';
 import * as React from 'react';
 import { Mock, Times } from 'typemoq';
-import { EnableTelemetrySettingDescription } from '../../../../../../../../common/components/enable-telemetry-setting-description';
-import { NewTabLink } from '../../../../../../../../common/components/new-tab-link';
-import { UserConfigMessageCreator } from '../../../../../../../../common/message-creators/user-config-message-creator';
-import { UserConfigurationStoreData } from '../../../../../../../../common/types/store-data/user-configuration-store';
-import { GenericToggle } from '../../../../../../../../DetailsView/components/generic-toggle';
-import {
-    TelemetrySettings,
-    TelemetrySettingsProps,
-} from '../../../../../../../../DetailsView/components/details-view-overlay/settings-panel/settings/telemetry/telemetry-settings';
 
 describe('TelemetrySettings', () => {
     const enableStates = [true, false];
