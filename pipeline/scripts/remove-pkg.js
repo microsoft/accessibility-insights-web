50758d2ed56c210a47786ee7a03a639d9df6df46
Karanbir Singh <karansin@microsoft.com>
2020-02-10 09:48:41 -0800

    change worknig directory before executing 7z

--- pipeline/scripts/remove-pkg.js
+++ pipeline/scripts/remove-pkg.js
@@ -8,8 +8,11 @@ const fs = require('fs');
 const parentDir = process.argv[2];
 const signedFiles = fs.readdirSync(parentDir);
 const zipTarget = signedFiles.find(f => path.extname(f) === '.zip' && f.startsWith('Accessibility'));
+
 console.log(zipTarget);
 console.log(sevenBin.path7za);
+
+process.chdir(parentDir);
 Seven.delete(zipTarget, '*.pkg', {
     $bin: sevenBin.path7za,
 });
