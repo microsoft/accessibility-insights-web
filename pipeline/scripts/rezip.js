cf5707b05b87193641ac6a568a4b6b67c1877b8d
Karanbir Singh <karansin@microsoft.com>
2020-02-10 13:12:29 -0800

    create zip ourselves

---
+++ pipeline/scripts/rezip.js
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+const Seven = require('node-7z');
+const path = require('path');
+const sevenBin = require('7zip-bin');
+const fs = require('fs');
+
+const parentDir = process.argv[2];
+const files = fs.readdirSync(parentDir);
+const existingDmg = files.find(f => path.extname(f) === '.exe' && f.startsWith('Accessibility'));
+
+const appName = path.basename(existingDmg, path.extname(existingDmg));
+
+console.log(appName);
+console.log(existingDmg);
+console.log(sevenBin.path7za);
+
+process.chdir(parentDir);
+
+Seven.add(`${appName}.zip`, '*', {
+    $bin: sevenBin.path7za,
+    recursive: true,
+});
