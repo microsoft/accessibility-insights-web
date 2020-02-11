fc9e5e3f96ed4cec89df83d296384485f516defe
Karanbir Singh <karansin@microsoft.com>
2020-02-11 09:31:33 -0800

    try cwd instead of cd

--- pipeline/scripts/zip-mac-folder.js
+++ pipeline/scripts/zip-mac-folder.js
@@ -14,9 +14,7 @@ console.log(`existingDmg: ${existingDmg}`);
 console.log(`appName: ${appName}`);
 console.log(`path to 7z: ${sevenBin.path7za}`);
 
-child_process.execSync(`cd ${parentDir}`, {
-    stdio: 'inherit',
-});
 child_process.execSync(`${sevenBin.path7za} a ${appName}.zip mac`, {
+    cwd: parentDir,
     stdio: 'inherit',
 });
