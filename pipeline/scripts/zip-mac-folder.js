b47b2c50a2ff5b4bfeab783aff5372dd970f4a4f
Karanbir Singh <karansin@microsoft.com>
2020-02-10 16:54:11 -0800

    update 7z scripts to remove wrapper dependency, move zipping into grunt

--- pipeline/scripts/zip-mac-folder.js
+++ pipeline/scripts/zip-mac-folder.js
@@ -1,19 +1,19 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const sevenBin = require('7zip-bin');
 
 const parentDir = process.argv[2];
-const signedFiles = fs.readdirSync(parentDir);
-const zipTarget = signedFiles.find(f => path.extname(f) === '.zip');
+const files = fs.readdirSync(parentDir);
+const existingDmg = files.find(f => path.extname(f) === '.dmg');
+const appName = path.basename(existingDmg, path.extname(existingDmg));
 
-console.log(`parentDir: ${parentDir}`);
-console.log(`zipTarget to remove pkg from: ${zipTarget}`);
+console.log(`existingDmg: ${existingDmg}`);
+console.log(`appName: ${appName}`);
 console.log(`path to 7z: ${sevenBin.path7za}`);
 
 let stdout = child_process.execSync(`cd ${parentDir}`);
 console.log(stdout);
-stdout = child_process.execSync(`${sevenBin.path7za} d ${zipTarget} *.pkg`);
+stdout = child_process.execSync(`${sevenBin.path7za} a ${appName}.zip mac`);
 console.log(stdout);
