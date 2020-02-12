d6d395f0741b5e455fde4a171962aaeb47501545
Karanbir Singh <karansin@microsoft.com>
2020-02-11 17:10:28 -0800

    Revert "Revert zipping to use wrapper library (worked in prior build)to help isolate what needs to change"
    This reverts commit ac401996a32af354a2a25b2e0324cb626d2d902e.

--- pipeline/scripts/zip-mac-folder.js
+++ pipeline/scripts/zip-mac-folder.js
@@ -1,23 +1,20 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-const Seven = require('node-7z');
+const child_process = require('child_process');
+const fs = require('fs');
 const path = require('path');
 const sevenBin = require('7zip-bin');
-const fs = require('fs');
 
 const parentDir = process.argv[2];
 const files = fs.readdirSync(parentDir);
-const existingDmg = files.find(f => path.extname(f) === '.dmg' && f.startsWith('Accessibility'));
-
+const existingDmg = files.find(f => path.extname(f) === '.dmg');
 const appName = path.basename(existingDmg, path.extname(existingDmg));
 
-console.log(appName);
-console.log(existingDmg);
-console.log(sevenBin.path7za);
+console.log(`existingDmg: ${existingDmg}`);
+console.log(`appName: ${appName}`);
+console.log(`path to 7z: ${sevenBin.path7za}`);
 
-process.chdir(parentDir);
-
-Seven.add(`${appName}.zip`, 'mac', {
-    $bin: sevenBin.path7za,
-    recursive: true,
+child_process.execSync(`${sevenBin.path7za} a "${appName}.zip" -r mac`, {
+    cwd: parentDir,
+    stdio: 'inherit',
 });
