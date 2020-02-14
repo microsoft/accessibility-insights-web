2d6c289641212f1957801af6eddde3e5e3ce2b23
Sebastian Morales <sebastian.moralesd@gmail.com>
2020-02-13 17:40:23 -0800

    normalize imports

--- src/tests/unit/tests/DetailsView/components/preview-features-toggle-list.test.tsx
+++ src/tests/unit/tests/DetailsView/components/preview-features-toggle-list.test.tsx
@@ -1,15 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { DisplayableFeatureFlag } from 'common/types/store-data/displayable-feature-flag';
+import { DetailsViewActionMessageCreator } from 'DetailsView/actions/details-view-action-message-creator';
+import { PreviewFeaturesToggleList, PreviewFeaturesToggleListProps } from 'DetailsView/components/preview-features-toggle-list';
+import { shallow } from 'enzyme';
 import * as React from 'react';
 import { Mock } from 'typemoq';
-
-import { shallow } from 'enzyme';
-import { DisplayableFeatureFlag } from '../../../../../common/types/store-data/displayable-feature-flag';
-import { DetailsViewActionMessageCreator } from '../../../../../DetailsView/actions/details-view-action-message-creator';
-import {
-    PreviewFeaturesToggleList,
-    PreviewFeaturesToggleListProps,
-} from '../../../../../DetailsView/components/preview-features-toggle-list';
 
 describe('PreviewFeaturesToggleListTest', () => {
     test('constructor', () => {
