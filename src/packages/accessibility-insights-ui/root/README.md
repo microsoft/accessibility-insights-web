5760e849813d7797dcd20d8f5625e19782efbfe2
Mark Reay <mreay@microsoft.com>
2020-02-19 12:56:12 -0800

    Created accessibility-insights-ui package

---
+++ src/packages/accessibility-insights-ui/root/README.md
@@ -0,0 +1,20 @@
+<!--
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+-->
+
+# accessibility-insights-ui
+
+This library is published was created so the Accessibility Insights website can share the overall
+UI presentation and style with our other products. It was built and is being maintained for that
+purpose alone and we are not intended it to be consumed by the broader open-source community.
+
+## Contributing
+
+To contribute, please visit [accessibility-insights-web](https://github.com/microsoft/accessibility-insights-web/blob/master/README.md) for more information.
+
+### Code of Conduct
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
+For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
+contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
---
+++ src/tests/unit/tests/packages/accessibility-insights-ui/ui-factory.test.tsx
@@ -0,0 +1,73 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+import { CheckIcon } from 'common/icons/check-icon';
+import { CrossIcon } from 'common/icons/cross-icon';
+import { UIFactory } from 'packages/accessibility-insights-ui/ui-factory';
+import { GuidanceTitle } from 'views/content/guidance-title';
+
+describe('UIFactory', () => {
+    const applicationTitle = 'THE_APPLICATION_TITLE';
+    const options = {
+        applicationTitle,
+        setPageTitle: true,
+    };
+    const ui = UIFactory(options);
+
+    it('exports CheckIcon', () => {
+        expect(ui.CheckIcon).toBe(CheckIcon);
+    });
+    it('exports CrossIcon', () => {
+        expect(ui.CrossIcon).toBe(CrossIcon);
+    });
+    it('exports GuidanceTitle', () => {
+        expect(ui.GuidanceTitle).toBe(GuidanceTitle);
+    });
+    describe('exports Markup', () => {
+        const markup = ui.Markup;
+
+        const exports = [
+            'Code',
+            'CodeExample',
+            'Column',
+            'Columns',
+            'Do',
+            'Dont',
+            'Emphasis',
+            'Fail',
+            'Highlight',
+            'HyperLink',
+            'Inline',
+            'LandmarkLegend',
+            'Links',
+            'Pass',
+            'PassFail',
+            'ProblemList',
+            'Table',
+            'Tag',
+            'Term',
+            'Title',
+        ];
+
+        it.each(exports)('exports %s', exp => {
+            expect(markup).toHaveProperty(exp);
+        });
+
+        it('does not export Include', () => {
+            expect(markup).not.toHaveProperty('Include');
+        });
+
+        it('has correct options', () => {
+            const opts = (markup as any).options;
+            expect(opts.applicationTitle).toEqual(applicationTitle);
+            expect(opts.setPageTitle).toEqual(true);
+        });
+    });
+
+    it('exports ContentView', () => {
+        const wrapper = shallow(<ui.ContentView>CONTENTS</ui.ContentView>);
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+});
