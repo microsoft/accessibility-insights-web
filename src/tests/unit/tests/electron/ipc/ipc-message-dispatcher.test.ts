d51f83abfb144ed30d3eb4d6e05ca114dc420d2e
Dan Bjorge <danielbj@microsoft.com>
2020-02-21 17:00:06 -0800

    Implement IpcMessageDispatcher tests

--- src/tests/unit/tests/electron/ipc/ipc-message-dispatcher.test.ts
+++ src/tests/unit/tests/electron/ipc/ipc-message-dispatcher.test.ts
@@ -1,11 +1,29 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { IPC_MESSAGE_CHANNEL_NAME } from 'electron/ipc/ipc-channel-names';
+import { IpcMessageDispatcher } from 'electron/ipc/ipc-message-dispatcher';
+
 describe('IpcMessageDispatcher', () => {
     it('should send dispatched messages as-is on IPC_MESSAGE_CHANNEL_NAME to each registered sink', () => {
-        throw new Error('TODO: implement me before marking PR non-draft!');
+        const stubMessage = { messageType: 'test message', payload: 'test payload' };
+
+        const testSubject = new IpcMessageDispatcher();
+        const mockSink = jest.fn();
+        testSubject.registerMessageSink(mockSink);
+
+        testSubject.dispatchMessage(stubMessage);
+
+        expect(mockSink.mock.calls).toEqual([[IPC_MESSAGE_CHANNEL_NAME, stubMessage]]);
     });
 
     it('should stop dispatching messages to unregistered sinks', () => {
-        throw new Error('TODO: implement me before marking PR non-draft!');
+        const testSubject = new IpcMessageDispatcher();
+        const mockSink = jest.fn();
+        testSubject.registerMessageSink(mockSink);
+        testSubject.unregisterMessageSink(mockSink);
+
+        testSubject.dispatchMessage({ messageType: 'test message', payload: 'test payload' });
+
+        expect(mockSink.mock.calls.length).toBe(0);
     });
 });
