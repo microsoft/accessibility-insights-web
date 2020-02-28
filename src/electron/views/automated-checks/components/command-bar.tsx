384d85bf55e03a94f30c1d7a099e4e018a6b63e8
Dan Bjorge <danielbj@microsoft.com>
2020-02-28 15:42:51 -0800

    Remove extraneous attribute

--- src/electron/views/automated-checks/components/command-bar.tsx
+++ src/electron/views/automated-checks/components/command-bar.tsx
@@ -27,7 +27,7 @@ export const CommandBar = NamedFC<CommandBarProps>('CommandBar', props => {
     const { deps, deviceStoreData } = props;
 
     return (
-        <section className={styles.commandBar} role="region" aria-label="command bar">
+        <section className={styles.commandBar} aria-label="command bar">
             <div className={styles.items}>
                 <CommandButton
                     data-automation-id={commandButtonRefreshId}
