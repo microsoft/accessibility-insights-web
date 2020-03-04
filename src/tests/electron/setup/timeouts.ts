429e809524a14feef971814526b25a4f015a9658
Sebastian Morales <sebastian.moralesd@gmail.com>
2020-03-04 11:09:09 -0800

    add waitForHighContrastMode on the app controller

--- src/tests/electron/setup/timeouts.ts
+++ src/tests/electron/setup/timeouts.ts
@@ -6,3 +6,6 @@ export const DEFAULT_ELECTRON_TEST_TIMEOUT_MS = 20000;
 
 // How long to wait for an element to be visible
 export const DEFAULT_WAIT_FOR_ELEMENT_TO_BE_VISIBLE_TIMEOUT_MS = 5000;
+
+// How long to wait for an element to have (or not have) a class
+export const DEFAULT_WAIT_FOR_ELEMENT_CLASS_TIMEOUT_MS = 500;
