1cd626f87191f73705310212f88d95cdbe84a144
Sebastian Morales <sebastian.moralesd@gmail.com>
2020-03-07 22:14:33 -0800

    use optional chaining

--- src/electron/platform/android/device-config.ts
+++ src/electron/platform/android/device-config.ts
@@ -1,22 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 export class DeviceConfig {
     constructor(readonly rawData: any) {}
 
     public get deviceName(): string {
-        try {
-            return this.rawData.deviceName;
-        } catch {
-            return null;
-        }
+        return this.rawData?.deviceName || null;
     }
 
     public get appIdentifier(): string {
-        try {
-            return this.rawData.packageName;
-        } catch {
-            return null;
-        }
+        return this.rawData?.packageName || null;
     }
 }
