1bc51ece11fd46f00a2b567d69c83208c58e7ead
Sebastian Morales <sebastian.moralesd@gmail.com>
2020-03-17 10:44:47 -0700

    add style to remove body margins

--- src/debug-tools/components/debug-tools-view.tsx
+++ src/debug-tools/components/debug-tools-view.tsx
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { HeaderDeps, Header } from 'common/components/header';
+import { Header, HeaderDeps } from 'common/components/header';
 import {
     withStoreSubscription,
     WithStoreSubscriptionDeps,
