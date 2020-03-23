06e3948abf2bf7dc3a1d4f080aa19329c6074413
Lisa Li <lisli@microsoft.com>
2020-03-23 16:37:15 -0700

    update test and snapshot

--- src/tests/unit/tests/reports/components/report-sections/link-script-provider.test.ts
+++ src/tests/unit/tests/reports/components/report-sections/link-script-provider.test.ts
@@ -3,6 +3,7 @@
 import { isFunction } from 'lodash';
 import {
     addEventListenerForLink,
+    ConfirmType,
     getAddListenerForLink,
 } from 'reports/components/report-sections/link-script-provider';
 import { It, Mock, MockBehavior, Times } from 'typemoq';
@@ -22,16 +23,51 @@ describe('LinkScriptProvider', () => {
         it('adds listener for click event on target-page-link', () => {
             const targetPageLinkMock = Mock.ofType<HTMLElement>();
             const docMock = Mock.ofType<Document>(undefined, MockBehavior.Strict);
+            const confirmMock = Mock.ofType<ConfirmType>();
+
             docMock
                 .setup(doc => doc.getElementById('target-page-link'))
                 .returns(() => targetPageLinkMock.object);
 
-            addEventListenerForLink(docMock.object);
+            targetPageLinkMock
+                .setup(link => link.addEventListener('click', It.isAny()))
+                .verifiable(Times.once());
 
-            targetPageLinkMock.verify(
-                link => link.addEventListener('click', It.is(isFunction)),
-                Times.once(),
-            );
+            addEventListenerForLink(docMock.object, confirmMock.object);
+
+            targetPageLinkMock.verifyAll();
         });
+        it.each([true, false])(
+            'listener calls confirm and triggers proper event when confirm result is %s',
+            isConfirmed => {
+                const targetPageLinkMock = Mock.ofType<HTMLElement>();
+                const docMock = Mock.ofType<Document>(undefined, MockBehavior.Strict);
+                const confirmMock = Mock.ofType<ConfirmType>();
+                const eventMock = Mock.ofType<Event>();
+
+                docMock
+                    .setup(doc => doc.getElementById('target-page-link'))
+                    .returns(() => targetPageLinkMock.object);
+
+                confirmMock
+                    .setup(confirmCallback => confirmCallback(It.isAnyString()))
+                    .returns(() => isConfirmed)
+                    .verifiable(Times.once());
+
+                const times: Times = isConfirmed ? Times.never() : Times.once();
+                eventMock.setup(event => event.preventDefault()).verifiable(times);
+
+                let clickListener: Function;
+                targetPageLinkMock
+                    .setup(link => link.addEventListener('click', It.isAny()))
+                    .callback((_, listener) => (clickListener = listener));
+
+                addEventListenerForLink(docMock.object, confirmMock.object);
+                clickListener(eventMock.object);
+
+                confirmMock.verifyAll();
+                eventMock.verifyAll();
+            },
+        );
     });
 });
