2ee6021f82ccb4e69cbd574f554564ce2b40ab6c
Sebastian Morales <sebastian.moralesd@gmail.com>
2020-03-23 14:06:17 -0700

    remove executeScriptInInspectedWindow

--- src/background/dev-tools-chrome-adapter.ts
+++ src/background/dev-tools-chrome-adapter.ts
@@ -5,15 +5,10 @@ import { ChromeAdapter } from '../common/browser-adapters/chrome-adapter';
 
 export interface DevToolsChromeAdapter extends BrowserAdapter {
     getInspectedWindowTabId(): number;
-    executeScriptInInspectedWindow(script: string, frameUrl: string): void;
 }
 
 export class DevToolsChromeAdapterImpl extends ChromeAdapter implements DevToolsChromeAdapter {
     public getInspectedWindowTabId(): number {
         return chrome.devtools.inspectedWindow.tabId;
-    }
-
-    public executeScriptInInspectedWindow(script: string, frameUrl: string): void {
-        chrome.devtools.inspectedWindow.eval(script, { frameURL: frameUrl } as any);
     }
 }
