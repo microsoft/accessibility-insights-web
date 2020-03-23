6fec83ec130660a90c5dc49d925ae8cc6c55006e
Dan Bjorge <dan@dbjorge.net>
2020-03-23 12:02:55 -0700

    Disable the new arrowParens default to avoid a too-large PR

--- prettier.config.js
+++ prettier.config.js
@@ -6,6 +6,9 @@ module.exports = {
     singleQuote: true,
     tabWidth: 4,
     trailingComma: 'all',
+    // We'd like to remove this arrowParens setting and use the new Prettier 2.0 default
+    // But since it has a lot of conflict potential, defering that until a quiet weekend
+    arrowParens: 'avoid',
     overrides: [
         {
             files: ['src/content/**/*', 'src/tests/unit/tests/DetailsView/components/**/*'],
