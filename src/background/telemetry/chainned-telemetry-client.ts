05194257c8ed65b1d6436c794dfb5db38b9ccf51
Sebastian Morales <sebastian.moralesd@gmail.com>
2020-03-26 14:06:45 -0700

    add multiplexing telemetry client

--- src/background/telemetry/chainned-telemetry-client.ts
+++ src/background/telemetry/chainned-telemetry-client.ts
@@ -2,6 +2,22 @@
 // Licensed under the MIT License.
 import { TelemetryClient } from 'background/telemetry/telemetry-client';
 
+export class MultiplexingTelemetryClient implements TelemetryClient {
+    constructor(private readonly wrappedClients: TelemetryClient[]) {}
+
+    public enableTelemetry(): void {
+        this.wrappedClients.forEach(c => c.enableTelemetry());
+    }
+
+    public disableTelemetry(): void {
+        this.wrappedClients.forEach(c => c.disableTelemetry());
+    }
+
+    public trackEvent(name: string, properties?: Object): void {
+        this.wrappedClients.forEach(c => c.trackEvent(name, properties));
+    }
+}
+
 export class ChainnedTelemetryClient implements TelemetryClient {
     private nextInTheChain: ChainnedTelemetryClient;
 
