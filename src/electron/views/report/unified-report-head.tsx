8432ab907d892fc2888f2d5510e8bf8057c71808
Wahaj Abid <waabid@microsoft.com>
2020-04-07 13:55:02 -0700

    updated title

--- src/electron/views/report/unified-report-head.tsx
+++ src/electron/views/report/unified-report-head.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { NamedFC } from 'common/react/named-fc';
-import { title } from 'content/strings/application';
+import { androidAppTitle } from 'content/strings/application';
 import * as React from 'react';
 import * as reportStyles from 'reports/automated-checks-report.styles';
 
@@ -12,7 +12,7 @@ export const UnifiedReportHead = NamedFC('UnifiedReportHead', () => {
     return (
         <head>
             <meta charSet="UTF-8" />
-            <title>{title} automated checks result</title>
+            <title>{androidAppTitle} - Report</title>
             <style dangerouslySetInnerHTML={{ __html: reportStyles.styleSheet }} />
             <style dangerouslySetInnerHTML={{ __html: bundledStyles.styleSheet }} />
         </head>
