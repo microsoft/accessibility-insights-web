dcd3758bdf436604ee8c0f0453e9405b97872fbc
AhmedAbdoOrtiga <45858632+AhmedAbdoOrtiga@users.noreply.github.com>
2020-04-17 06:49:16 -0700

    feat(android-report): Add Time to unified report name (#2500)

--- src/tests/unit/tests/electron/report/unified-report-name-generator.test.ts
+++ src/tests/unit/tests/electron/report/unified-report-name-generator.test.ts
@@ -1,24 +1,29 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { WebReportNameGenerator } from 'reports/report-name-generator';
+import { UnifiedReportNameGenerator } from 'electron/views/report/unified-report-name-generator';
 import { ReportNameGeneratorBuilder } from 'reports/report-name-generator-builder';
 import { Mock, Times } from 'typemoq';
 
-describe('WebReportNameGenerator', () => {
+describe('UnifiedReportNameGenerator', () => {
     const theBase = 'BASE';
     const theTitle = 'Title';
     const theDate = new Date();
 
     const dateSegment = 'date segment';
+    const timeSegment = 'time segment';
     const titleSegment = 'title segment';
 
     const reportNameGeneratorBuilderMock = Mock.ofType(ReportNameGeneratorBuilder);
-    const testObject = new WebReportNameGenerator(reportNameGeneratorBuilderMock.object);
+    const testObject = new UnifiedReportNameGenerator(reportNameGeneratorBuilderMock.object);
 
     beforeEach(() => {
         reportNameGeneratorBuilderMock
             .setup(rngbm => rngbm.getDateSegment(theDate))
             .returns(() => dateSegment)
+            .verifiable(Times.once());
+        reportNameGeneratorBuilderMock
+            .setup(rngbm => rngbm.getTimeSegment(theDate))
+            .returns(() => timeSegment)
             .verifiable(Times.once());
         reportNameGeneratorBuilderMock
             .setup(rngbm => rngbm.getTitleSegment(theTitle))
@@ -29,7 +34,7 @@ describe('WebReportNameGenerator', () => {
     it('generateName', () => {
         const actual = testObject.generateName(theBase, theDate, theTitle);
 
-        const expected = `${theBase}_${dateSegment}_${titleSegment}.html`;
+        const expected = `${theBase}_${dateSegment}_${timeSegment}_${titleSegment}.html`;
         expect(actual).toEqual(expected);
 
         reportNameGeneratorBuilderMock.verifyAll();
