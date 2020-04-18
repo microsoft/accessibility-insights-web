096dc1d0d4d64795c4cba4cb46e6899148a650b5
Wahaj Abid <waabid@microsoft.com>
2020-04-17 17:10:22 -0700

    lint fix

--- src/common/get-unavailable-highlight-status.ts
+++ src/common/get-unavailable-highlight-status.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { HighlightState } from 'common/components/cards/instance-details-footer';
 import {
     PlatformData,
     UnifiedResult,
