3f964f4a8b8acae26a00855b2d6462693b00cd0b
John Alkire <john.alkire@microsoft.com>
2020-04-24 09:56:48 -0700

    Introduce ContentPageConfiguration & pageTitle

--- src/content/common.ts
+++ src/content/common.ts
@@ -6,6 +6,15 @@ import { ContentCreator } from 'views/content/content-page';
 import { GuidanceTitle as gt } from 'views/content/guidance-title';
 import { link } from './link';
 
+interface ContentPageConfiguration {
+    pageTitle: string;
+}
+
 export const create = ContentCreator(link);
+export const createWithTitle = (config: ContentPageConfiguration, props) => {
+    const component = create(props);
+    component.pageTitle = config.pageTitle;
+    return component;
+};
 export const React = react;
 export const GuidanceTitle = gt;
