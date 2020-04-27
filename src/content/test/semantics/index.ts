2e1cac41714a7cf18d8261dce5ffc9777f48469b
Liu Haonan <haonliu@microsoft.com>
2020-04-27 08:49:16 -0700

    feat(tables-requirements): Add Headers requirement content (#2551)
    * add headers requirement content
    
    * add example header
    
    * make code bold
    
    * Update src/assessments/semantics/test-steps/headers.tsx
    
    Co-Authored-By: Rob Gallo <43080011+RobGallo@users.noreply.github.com>
    
    * Update src/assessments/semantics/test-steps/headers.tsx
    
    Co-Authored-By: Rob Gallo <43080011+RobGallo@users.noreply.github.com>
    
    * make the new requirement visible
    
    * update snapshot for guidance content
    
    Co-authored-by: Rob Gallo <43080011+RobGallo@users.noreply.github.com>

--- src/content/test/semantics/index.ts
+++ src/content/test/semantics/index.ts
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 import * as cssContent from './css-content';
 import * as dataTables from './data-tables';
 import * as emphasis from './emphasis';
 import { guidance } from './guidance';
+import * as headers from './headers';
 import * as letterSpacing from './letter-spacing';
 import * as lists from './lists';
 import * as quotes from './quotes';
@@ -17,4 +17,5 @@ export const semantics = {
     lists,
     quotes,
     letterSpacing,
+    headers,
 };
