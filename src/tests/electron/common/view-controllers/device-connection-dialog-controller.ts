8199026c1a7ad3805f437047efae9cb5f2e2a3dc
Karan <karansin@microsoft.com>
2020-04-28 16:35:11 -0700

    feat(electron-8x): update electron from 7.0.1 to 8.2.2  (#2575)

--- src/tests/electron/common/view-controllers/device-connection-dialog-controller.ts
+++ src/tests/electron/common/view-controllers/device-connection-dialog-controller.ts
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as WebDriverIO from 'webdriverio';
+import { SpectronAsyncClient } from 'tests/electron/common/view-controllers/spectron-async-client';
 import { CommonSelectors } from '../element-identifiers/common-selectors';
 import { DeviceConnectionDialogSelectors } from '../element-identifiers/device-connection-dialog-selectors';
 import { ViewController } from './view-controller';
 
 export class DeviceConnectionDialogController extends ViewController {
-    constructor(client: WebDriverIO.Client<void>) {
+    constructor(client: SpectronAsyncClient) {
         super(client);
     }
 
@@ -17,8 +17,8 @@ export class DeviceConnectionDialogController extends ViewController {
     // inputs the port, validates, and starts scanning
     public async connectToPort(port: number): Promise<void> {
         await this.client.click(DeviceConnectionDialogSelectors.portNumber);
-        await this.client.element(DeviceConnectionDialogSelectors.portNumber).keys(port.toString());
-
+        await this.client.$(DeviceConnectionDialogSelectors.portNumber);
+        await this.client.keys(port.toString());
         await this.client.waitForEnabled(DeviceConnectionDialogSelectors.validateButton);
         await this.client.click(DeviceConnectionDialogSelectors.validateButton);
 
