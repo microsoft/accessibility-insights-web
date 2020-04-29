fa12317f5a05d41d7d95cc2367c59589c3896497
Dave Tryon <dtryon@microsoft.com>
2020-04-29 12:03:57 -0700

    Revert "Start listening to events only after main window creation"
    This reverts commit 1d942144864b42d9bd63dbdda088947af9ce4edf.

--- src/electron/main/main.ts
+++ src/electron/main/main.ts
@@ -56,7 +56,6 @@ const createWindow = () => {
     ipcMain.on(IPC_MAIN_WINDOW_INITIALIZED_CHANNEL_NAME, () => {
         ipcMessageDispatcher.registerMessageSink(mainWindowMessageSink);
         nativeHighContrastModeListener.startListening();
-        mainWindowRendererMessageHandlers.startListening();
     });
 
     mainWindowRendererMessageHandlers = new MainWindowRendererMessageHandlers(
@@ -64,6 +63,7 @@ const createWindow = () => {
         mainWindow,
         ipcMain,
     );
+    mainWindowRendererMessageHandlers.startListening();
 
     mainWindow
         .loadFile(path.resolve(__dirname, '../electron/views/index.html'))
