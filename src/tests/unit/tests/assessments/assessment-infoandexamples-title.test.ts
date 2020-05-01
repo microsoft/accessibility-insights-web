6acc14da016b70482246dc6eccff35359f58e103
John Alkire <john.alkire@microsoft.com>
2020-05-01 09:11:55 -0700

    Verify pageTitle equals requirement name

--- src/tests/unit/tests/assessments/assessment-infoandexamples-title.test.ts
+++ src/tests/unit/tests/assessments/assessment-infoandexamples-title.test.ts
@@ -4,20 +4,20 @@ import { Assessments } from 'assessments/assessments';
 import { Requirement } from 'assessments/types/requirement';
 
 describe('assessment-infoandexamples-title', () => {
-    test('all infoAndExamples have a pageTitle value', () => {
+    test('all infoAndExamples have the correct pageTitle value', () => {
         Assessments.all().forEach(assessment => {
-            assessment.requirements.forEach(assertInfoAndExamplesHasPageTitle);
+            assessment.requirements.forEach(assertInfoAndExamplesHasCorrectPageTitle);
         });
     });
 
-    function infoAndExamplesHasPageTitle(requirement: Requirement): boolean {
+    function infoAndExamplesHasCorrectPageTitle(requirement: Requirement): boolean {
         return (
             requirement.infoAndExamples === undefined ||
-            requirement.infoAndExamples.pageTitle !== undefined
+            requirement.infoAndExamples.pageTitle === requirement.name
         );
     }
 
-    function assertInfoAndExamplesHasPageTitle(requirement: Requirement): void {
-        expect(infoAndExamplesHasPageTitle(requirement)).toBe(true);
+    function assertInfoAndExamplesHasCorrectPageTitle(requirement: Requirement): void {
+        expect(infoAndExamplesHasCorrectPageTitle(requirement)).toBe(true);
     }
 });
