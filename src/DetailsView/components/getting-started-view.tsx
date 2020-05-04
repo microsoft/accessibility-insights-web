08733b7f85688d9c0fcd6d5976d501c90aee7505
Lisa Li <lisli@microsoft.com>
2020-05-04 13:50:29 -0700

    create/use getting-started-view.scss

--- src/DetailsView/components/getting-started-view.tsx
+++ src/DetailsView/components/getting-started-view.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { NamedFC } from 'common/react/named-fc';
-import * as styles from 'DetailsView/components/assessment-view.scss';
+import * as styles from 'DetailsView/components/getting-started-view.scss';
 import * as React from 'react';
 
 export interface GettingStartedViewProps {
@@ -10,7 +10,7 @@ export interface GettingStartedViewProps {
 
 export const GettingStartedView = NamedFC<GettingStartedViewProps>('GettingStartedView', props => {
     return (
-        <div className={styles.assessmentGettingStartedContainer}>
+        <div className={styles.gettingStartedView}>
             <h2>Getting Started</h2>
             {props.gettingStartedContent}
         </div>
