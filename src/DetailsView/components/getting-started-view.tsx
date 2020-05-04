a1168a56f45a3da2ec97c13e5b2f7fadf5a96cc9
Lisa Li <lisli@microsoft.com>
2020-05-04 13:01:14 -0700

    change to NamedFC

--- src/DetailsView/components/getting-started-view.tsx
+++ src/DetailsView/components/getting-started-view.tsx
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { NamedFC } from 'common/react/named-fc';
 import * as styles from 'DetailsView/components/assessment-view.scss';
 import * as React from 'react';
 
@@ -7,13 +8,11 @@ export interface GettingStartedViewProps {
     gettingStartedContent: JSX.Element;
 }
 
-export class GettingStartedView extends React.Component<GettingStartedViewProps> {
-    public render(): JSX.Element {
-        return (
-            <div className={styles.assessmentGettingStartedContainer}>
-                <h2>Getting Started</h2>
-                {this.props.gettingStartedContent}
-            </div>
-        );
-    }
-}
+export const GettingStartedView = NamedFC<GettingStartedViewProps>('GettingStartedView', props => {
+    return (
+        <div className={styles.assessmentGettingStartedContainer}>
+            <h2>Getting Started</h2>
+            {props.gettingStartedContent}
+        </div>
+    );
+});
