f007d669c05e2c87daec1f152a5850bab5cc634c
Dan Bjorge <dan@dbjorge.net>
2020-05-12 12:06:04 -0700

    Fix placement of copyright header

--- src/assessments/landmarks/does-result-have-main-role.ts
+++ src/assessments/landmarks/does-result-have-main-role.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 import { DecoratedAxeNodeResult } from 'injected/scanner-utils';
 import { some } from 'lodash';
 
-// Licensed under the MIT License.
 export function doesResultHaveMainRole(instance: DecoratedAxeNodeResult): boolean {
     return (
         some(instance.any, checkResult => checkResult.data['role'] === 'main') ||
