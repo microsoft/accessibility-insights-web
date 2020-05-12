461c31a57ecc405abacb146b63ed106226341e51
Lisa Li <lisli@microsoft.com>
2020-05-11 18:14:04 -0700

    remove extra copyright header

--- src/tests/unit/tests/DetailsView/components/requirement-view.test.tsx
+++ src/tests/unit/tests/DetailsView/components/requirement-view.test.tsx
@@ -1,7 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
 import { Requirement } from 'assessments/types/requirement';
 import { RequirementView, RequirementViewProps } from 'DetailsView/components/requirement-view';
 import { shallow } from 'enzyme';
