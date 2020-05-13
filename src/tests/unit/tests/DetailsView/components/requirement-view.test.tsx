f72f116df1d830225f00bf0a33e7efeb0e92818e
Lisa Li <lisli@microsoft.com>
2020-05-13 15:06:32 -0700

    update requirement view props in test

--- src/tests/unit/tests/DetailsView/components/requirement-view.test.tsx
+++ src/tests/unit/tests/DetailsView/components/requirement-view.test.tsx
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { AssessmentsProviderImpl } from 'assessments/assessments-provider';
 import { Requirement } from 'assessments/types/requirement';
+import { VisualizationType } from 'common/types/visualization-type';
 import {
     RequirementView,
     RequirementViewDeps,
@@ -8,6 +10,7 @@ import {
 } from 'DetailsView/components/requirement-view';
 import { shallow } from 'enzyme';
 import * as React from 'react';
+import { Mock } from 'typemoq';
 
 describe('RequirementViewTest', () => {
     it('renders with content from props', () => {
@@ -17,9 +20,16 @@ describe('RequirementViewTest', () => {
             howToTest: <p>how-to-test-stub</p>,
         } as Requirement;
 
+        const assessmentsProviderMock = Mock.ofType(AssessmentsProviderImpl);
+        const assessmentNavState = {
+            selectedTestSubview: 'test-requirement-name',
+            selectedTestType: VisualizationType.Headings,
+        };
         const props: RequirementViewProps = {
             deps: {} as RequirementViewDeps,
             requirement: requirementStub,
+            assessmentsProvider: assessmentsProviderMock.object,
+            assessmentNavState: assessmentNavState,
         };
 
         const rendered = shallow(<RequirementView {...props} />);
