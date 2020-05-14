010f34e1d2936e2415f530f928d99caaf4860837
Dan Bjorge <danielbj@microsoft.com>
2020-05-14 14:58:34 -0700

    fix: auto-pass main landmark requirements for no-landmark pages (#2644)
    #### Description of changes
    
    #2521 tracks an issue where it wasn't clear what the expectations on a user were for testing the Landmarks "primary content" and "no repeating content" requirements on pages with no landmarks.
    
    The proposed fix for that was a text update + making the requirements pass automatically on pages that have no landmarks.
    
    This fix involves a significant bit of new behavior. We wanted to *keep* the 2 requirements in question as manual requirements, not assisted requirements, since it's possible for a page to have no main landmarks and for a user to need to manually enter a failure not associated with any instance we might show in an instance list. However, this is the first time we're assigning auto-pass behavior to a manual test; in all other cases where we support auto-pass, it works by looking for an assisted requirement with no matching instances.
    
    This new behavior is especially challenging because the set of instances required for auto-passing ("are there **any** landmarks on the page?") is different from the set of instances required for visualization ("what are all the **main** landmarks on the page?").
    
    To support this, I've added 2 new features to `AssessmentStore`'s `Requirement` processing:
    * A new `isVisualizationSupportedForResult` property which `Requirement`s can use to indicate that certain results returned by their analyzers should not support being visualized (by default, all results support visualization)
    * A new `getInitialManualTestStatus` property which manual `Requirement`s can use to infer an initial pass/fail state after scanning for results (by default, a manual requirement is put in the UNKNOWN state regardless of analysis results)
    
    These are used by the updated requirements by updating their analyzers to use the `unique-landmark` rule to look for *all* landmarks instead of the old behavior of looking only for main landmarks, use `isVisualizationSupportedForResult` to only visualize the main landmarks (like before), and use `getInitialManualTestStatus` to set a "PASS" status if the scan completes and there are no instances with the landmark role data from the `unique-landmark` rule.
    
    New behavior:
    
    **no landmarks: "Primary content" and "No repeating content" automatically pass**
    ![screen recording of no-landmarks interactions](https://user-images.githubusercontent.com/376284/81753659-1ef3ea00-9469-11ea-8646-e394ffff61de.gif)
    
    **only non-main landmarks: "Primary content" and "No repeating content" don't auto-pass and show no matching instances to visualize**
    ![screen recording of banner-landmark-only interactions](https://user-images.githubusercontent.com/376284/81753648-1ac7cc80-9469-11ea-9c90-05f8d7243088.gif)
    
    **Mix of main and non-main landmarks: "Primary content" and "No repeating content" don't auto-pass and visualize only the main landmark**
    ![screen recording of mixed-landmarks interactions](https://user-images.githubusercontent.com/376284/81753619-0a175680-9469-11ea-9d6e-40131889cf46.gif)
    
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a" in the checkbox -->
    - [x] Addresses an existing issue: #2521
    - [x] Ran `yarn fastpass`
    - [x] Added/updated relevant unit test(s) (and ran `yarn test`)
    - [x] Verified code coverage for the changes made. Check coverage report at: `<rootDir>/test-results/unit/coverage`
    - [x] PR title *AND* final merge commit title both start with a semantic tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`). See `CONTRIBUTING.md`.
    - [x] (UI changes only) Added screenshots/GIFs to description above
    - [x] (UI changes only) Verified usability with NVDA/JAWS

--- src/tests/unit/tests/DetailsView/components/assessment-visualization-enabled-toggle.test.tsx
+++ src/tests/unit/tests/DetailsView/components/assessment-visualization-enabled-toggle.test.tsx
@@ -1,200 +1,234 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as Enzyme from 'enzyme';
-import * as React from 'react';
-import { IMock, Mock, Times } from 'typemoq';
 import {
     VisualizationToggle,
     VisualizationToggleProps,
-} from '../../../../../common/components/visualization-toggle';
-import { DetailsViewActionMessageCreator } from '../../../../../DetailsView/actions/details-view-action-message-creator';
-import { AssessmentVisualizationEnabledToggle } from '../../../../../DetailsView/components/assessment-visualization-enabled-toggle';
-import { visualHelperText } from '../../../../../DetailsView/components/base-visual-helper-toggle';
-import { VisualHelperToggleConfigBuilder } from '../../../common/visual-helper-toggle-config-builder';
-import { VisualizationTogglePropsBuilder } from '../../../common/visualization-toggle-props-builder';
+} from 'common/components/visualization-toggle';
+import { DetailsViewActionMessageCreator } from 'DetailsView/actions/details-view-action-message-creator';
+import { AssessmentVisualizationEnabledToggle } from 'DetailsView/components/assessment-visualization-enabled-toggle';
+import { visualHelperText } from 'DetailsView/components/base-visual-helper-toggle';
+import * as Enzyme from 'enzyme';
+import * as React from 'react';
+import { VisualHelperToggleConfigBuilder } from 'tests/unit/common/visual-helper-toggle-config-builder';
+import { VisualizationTogglePropsBuilder } from 'tests/unit/common/visualization-toggle-props-builder';
+import { IMock, Mock, Times } from 'typemoq';
 
 describe('AssessmentVisualizationEnabledToggle', () => {
     const actionMessageCreatorMock: IMock<DetailsViewActionMessageCreator> = Mock.ofType(
         DetailsViewActionMessageCreator,
     );
 
-    it('render with disabled message', () => {
-        const props = new VisualHelperToggleConfigBuilder()
-            .withToggleStepEnabled(true)
-            .withToggleStepScanned(false)
-            .withActionMessageCreator(actionMessageCreatorMock.object)
-            .withEmptyFilteredMap()
-            .build();
+    describe('render', () => {
+        it('is disabled when no instances exist', () => {
+            const props = new VisualHelperToggleConfigBuilder()
+                .withToggleStepEnabled(true)
+                .withToggleStepScanned(false)
+                .withActionMessageCreator(actionMessageCreatorMock.object)
+                .withEmptyFilteredMap()
+                .build();
 
-        const wrapper = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
+            const testSubject = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
 
-        const visualHelperClass = 'visual-helper';
-        const toggleDiv = wrapper.find(`.${visualHelperClass}`);
+            expectDisabledTextLayout(testSubject);
 
-        expect(toggleDiv.exists()).toBeTruthy();
+            const expectedToggleProps = getDefaultVisualizationTogglePropsBuilder()
+                .with('checked', false)
+                .with('disabled', true)
+                .build();
 
-        const textDiv = toggleDiv.find(`.${visualHelperClass}-text`);
+            expectChildVisualizationToggleWith(expectedToggleProps, testSubject);
+        });
 
-        expect(textDiv.exists()).toBeTruthy();
-        expect(textDiv.childAt(0).text()).toEqual(visualHelperText);
+        it("is disabled when only instances that don't support visualization exist", () => {
+            const props = new VisualHelperToggleConfigBuilder()
+                .withToggleStepEnabled(true)
+                .withToggleStepScanned(false)
+                .withActionMessageCreator(actionMessageCreatorMock.object)
+                .withNonEmptyFilteredMap(false, false)
+                .build();
 
-        const noMatchesWarningClass = 'no-matching-elements';
-        expect(wrapper.find(`.${noMatchesWarningClass}`).exists()).toBeTruthy();
+            const testSubject = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
 
-        const toggle = wrapper.find(VisualizationToggle);
+            expectDisabledTextLayout(testSubject);
 
-        const expectedToggleProps = getDefaultVisualizationTogglePropsBuilder()
-            .with('checked', false)
-            .with('disabled', true)
-            .build();
+            const expectedToggleProps = getDefaultVisualizationTogglePropsBuilder()
+                .with('checked', false)
+                .with('disabled', true)
+                .build();
 
-        assertVisualizationToggle(expectedToggleProps, toggle);
+            expectChildVisualizationToggleWith(expectedToggleProps, testSubject);
+        });
+
+        it('is enabled but unchecked if step is enabled but all instance visualizations are disabled', () => {
+            const props = new VisualHelperToggleConfigBuilder()
+                .withToggleStepEnabled(true)
+                .withToggleStepScanned(false)
+                .withActionMessageCreator(actionMessageCreatorMock.object)
+                .withNonEmptyFilteredMap(false)
+                .build();
+
+            const testSubject = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
+
+            expectEnabledTextLayout(testSubject);
+
+            const expectedToggleProps = getDefaultVisualizationTogglePropsBuilder()
+                .with('checked', false)
+                .with('disabled', false)
+                .build();
+
+            expectChildVisualizationToggleWith(expectedToggleProps, testSubject);
+        });
+
+        it('is enabled and checked if step and instance visualizations are both enabled', () => {
+            const props = new VisualHelperToggleConfigBuilder()
+                .withToggleStepEnabled(false)
+                .withToggleStepScanned(false)
+                .withActionMessageCreator(actionMessageCreatorMock.object)
+                .withNonEmptyFilteredMap(true)
+                .build();
+
+            const testSubject = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
+
+            expectEnabledTextLayout(testSubject);
+
+            const expectedToggleProps = getDefaultVisualizationTogglePropsBuilder()
+                .with('checked', true)
+                .with('disabled', false)
+                .build();
+
+            expectChildVisualizationToggleWith(expectedToggleProps, testSubject);
+        });
+
+        it("is enabled and checked if some instances support visualization and some don't", () => {
+            const props = new VisualHelperToggleConfigBuilder()
+                .withToggleStepEnabled(false)
+                .withToggleStepScanned(false)
+                .withActionMessageCreator(actionMessageCreatorMock.object)
+                .withMixedVisualizationSupportFilteredMap()
+                .build();
+
+            const testSubject = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
+
+            expectEnabledTextLayout(testSubject);
+
+            const expectedToggleProps = getDefaultVisualizationTogglePropsBuilder()
+                .with('checked', true)
+                .with('disabled', false)
+                .build();
+
+            expectChildVisualizationToggleWith(expectedToggleProps, testSubject);
+        });
+
+        it('is enabled and unchecked if step and instance visualizations are both disabled', () => {
+            const props = new VisualHelperToggleConfigBuilder()
+                .withToggleStepEnabled(false)
+                .withToggleStepScanned(false)
+                .withActionMessageCreator(actionMessageCreatorMock.object)
+                .withNonEmptyFilteredMap(false)
+                .build();
+
+            const testSubject = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
+
+            expectEnabledTextLayout(testSubject);
+
+            const expectedToggleProps = getDefaultVisualizationTogglePropsBuilder()
+                .with('checked', false)
+                .with('disabled', false)
+                .build();
+
+            expectChildVisualizationToggleWith(expectedToggleProps, testSubject);
+        });
+
+        function expectEnabledTextLayout(
+            testSubject: Enzyme.ShallowWrapper<AssessmentVisualizationEnabledToggle>,
+        ): void {
+            const visualHelperClass = 'visual-helper';
+            const toggleDiv = testSubject.find(`.${visualHelperClass}`);
+
+            expect(toggleDiv.exists()).toBeTruthy();
+
+            const textDiv = toggleDiv.find(`.${visualHelperClass}-text`);
+
+            expect(textDiv.exists()).toBeTruthy();
+            expect(textDiv.childAt(0).text()).toEqual(visualHelperText);
+            expect(testSubject.find('strong').exists()).toBeFalsy();
+        }
+
+        function expectDisabledTextLayout(
+            testSubject: Enzyme.ShallowWrapper<AssessmentVisualizationEnabledToggle>,
+        ): void {
+            const visualHelperClass = 'visual-helper';
+            const toggleDiv = testSubject.find(`.${visualHelperClass}`);
+
+            expect(toggleDiv.exists()).toBeTruthy();
+
+            const textDiv = toggleDiv.find(`.${visualHelperClass}-text`);
+
+            expect(textDiv.exists()).toBeTruthy();
+            expect(textDiv.childAt(0).text()).toEqual(visualHelperText);
+
+            const noMatchesWarningClass = 'no-matching-elements';
+            expect(testSubject.find(`.${noMatchesWarningClass}`).exists()).toBeTruthy();
+        }
     });
 
-    it('render: toggle not disabled', () => {
-        const props = new VisualHelperToggleConfigBuilder()
-            .withToggleStepEnabled(true)
-            .withToggleStepScanned(false)
-            .withActionMessageCreator(actionMessageCreatorMock.object)
-            .withNonEmptyFilteredMap()
-            .build();
+    describe('toggle behavior', () => {
+        it('enables all visualizations when none are shown', () => {
+            const props = new VisualHelperToggleConfigBuilder()
+                .withToggleStepEnabled(true)
+                .withToggleStepScanned(false)
+                .withActionMessageCreator(actionMessageCreatorMock.object)
+                .build();
 
-        const wrapper = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
+            const wrapper = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
+            actionMessageCreatorMock.reset();
+            actionMessageCreatorMock
+                .setup(acm =>
+                    acm.changeAssessmentVisualizationStateForAll(
+                        true,
+                        props.assessmentNavState.selectedTestType,
+                        props.assessmentNavState.selectedTestSubview,
+                    ),
+                )
+                .verifiable(Times.once());
 
-        const visualHelperClass = 'visual-helper';
-        const toggleDiv = wrapper.find(`.${visualHelperClass}`);
+            wrapper.find(VisualizationToggle).simulate('click');
 
-        expect(toggleDiv.exists()).toBeTruthy();
+            actionMessageCreatorMock.verifyAll();
+        });
 
-        const textDiv = toggleDiv.find(`.${visualHelperClass}-text`);
+        it('disables all visualizations when some are shown', () => {
+            const props = new VisualHelperToggleConfigBuilder()
+                .withToggleStepEnabled(true)
+                .withToggleStepScanned(false)
+                .withActionMessageCreator(actionMessageCreatorMock.object)
+                .withNonEmptyFilteredMap(true)
+                .build();
 
-        expect(textDiv.exists()).toBeTruthy();
-        expect(textDiv.childAt(0).text()).toEqual(visualHelperText);
-        expect(wrapper.find('strong').exists()).toBeFalsy();
-        const toggle = wrapper.find(VisualizationToggle);
+            const wrapper = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
+            actionMessageCreatorMock.reset();
+            actionMessageCreatorMock
+                .setup(acm =>
+                    acm.changeAssessmentVisualizationStateForAll(
+                        false,
+                        props.assessmentNavState.selectedTestType,
+                        props.assessmentNavState.selectedTestSubview,
+                    ),
+                )
+                .verifiable(Times.once());
 
-        const expectedToggleProps = getDefaultVisualizationTogglePropsBuilder()
-            .with('checked', false)
-            .with('disabled', false)
-            .build();
+            wrapper.find(VisualizationToggle).simulate('click');
 
-        assertVisualizationToggle(expectedToggleProps, toggle);
+            actionMessageCreatorMock.verifyAll();
+        });
     });
 
-    it('render: have non empty instance map with a visible instance', () => {
-        const props = new VisualHelperToggleConfigBuilder()
-            .withToggleStepEnabled(false)
-            .withToggleStepScanned(false)
-            .withActionMessageCreator(actionMessageCreatorMock.object)
-            .withNonEmptyFilteredMap(true)
-            .build();
-
-        const wrapper = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
-
-        const visualHelperClass = 'visual-helper';
-        const toggleDiv = wrapper.find(`.${visualHelperClass}`);
-
-        expect(toggleDiv.exists()).toBeTruthy();
-
-        const textDiv = toggleDiv.find(`.${visualHelperClass}-text`);
-
-        expect(textDiv.exists()).toBeTruthy();
-        expect(textDiv.childAt(0).text()).toEqual(visualHelperText);
-        expect(wrapper.find('strong').exists()).toBeFalsy();
-
-        const toggle = wrapper.find(VisualizationToggle);
-
-        const expectedToggleProps = getDefaultVisualizationTogglePropsBuilder()
-            .with('checked', true)
-            .with('disabled', false)
-            .build();
-
-        assertVisualizationToggle(expectedToggleProps, toggle);
-    });
-
-    it('render: have non empty instance map without a visible instance', () => {
-        const props = new VisualHelperToggleConfigBuilder()
-            .withToggleStepEnabled(false)
-            .withToggleStepScanned(false)
-            .withActionMessageCreator(actionMessageCreatorMock.object)
-            .withNonEmptyFilteredMap()
-            .build();
-
-        const wrapper = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
-
-        const visualHelperClass = 'visual-helper';
-        const toggleDiv = wrapper.find(`.${visualHelperClass}`);
-
-        expect(toggleDiv.exists()).toBeTruthy();
-
-        const textDiv = toggleDiv.find(`.${visualHelperClass}-text`);
-
-        expect(textDiv.exists()).toBeTruthy();
-        expect(textDiv.childAt(0).text()).toEqual(visualHelperText);
-        expect(wrapper.find('strong').exists()).toBeFalsy();
-        const toggle = wrapper.find(VisualizationToggle);
-
-        const expectedToggleProps = getDefaultVisualizationTogglePropsBuilder()
-            .with('checked', false)
-            .with('disabled', false)
-            .build();
-
-        assertVisualizationToggle(expectedToggleProps, toggle);
-    });
-
-    it('enables all visualizations when none are shown', () => {
-        const props = new VisualHelperToggleConfigBuilder()
-            .withToggleStepEnabled(true)
-            .withToggleStepScanned(false)
-            .withActionMessageCreator(actionMessageCreatorMock.object)
-            .build();
-
-        const wrapper = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
-        actionMessageCreatorMock.reset();
-        actionMessageCreatorMock
-            .setup(acm =>
-                acm.changeAssessmentVisualizationStateForAll(
-                    true,
-                    props.assessmentNavState.selectedTestType,
-                    props.assessmentNavState.selectedTestSubview,
-                ),
-            )
-            .verifiable(Times.once());
-
-        wrapper.find(VisualizationToggle).simulate('click');
-
-        actionMessageCreatorMock.verifyAll();
-    });
-
-    it('disables all visualizations when some are shown', () => {
-        const props = new VisualHelperToggleConfigBuilder()
-            .withToggleStepEnabled(true)
-            .withToggleStepScanned(false)
-            .withActionMessageCreator(actionMessageCreatorMock.object)
-            .withNonEmptyFilteredMap(true)
-            .build();
-
-        const wrapper = Enzyme.shallow(<AssessmentVisualizationEnabledToggle {...props} />);
-        actionMessageCreatorMock.reset();
-        actionMessageCreatorMock
-            .setup(acm =>
-                acm.changeAssessmentVisualizationStateForAll(
-                    false,
-                    props.assessmentNavState.selectedTestType,
-                    props.assessmentNavState.selectedTestSubview,
-                ),
-            )
-            .verifiable(Times.once());
-
-        wrapper.find(VisualizationToggle).simulate('click');
-
-        actionMessageCreatorMock.verifyAll();
-    });
-
-    function assertVisualizationToggle(
+    function expectChildVisualizationToggleWith(
         expectedProps: VisualizationToggleProps,
-        visualizationToggle: Enzyme.ShallowWrapper<VisualizationToggleProps>,
+        testSubject: Enzyme.ShallowWrapper,
     ): void {
+        const visualizationToggle = testSubject.find(VisualizationToggle);
+
         expect(visualizationToggle.exists()).toBeTruthy();
 
         const actualProps = visualizationToggle.props();
---
+++ src/tests/unit/tests/assessments/landmarks/does-result-have-main-role.test.ts
@@ -0,0 +1,71 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { doesResultHaveMainRole } from 'assessments/landmarks/does-result-have-main-role';
+import { DecoratedAxeNodeResult } from 'injected/scanner-utils';
+
+describe('doesResultHaveMainRole', () => {
+    it('returns false for results with no check results', () => {
+        const input = { any: [], all: [] } as DecoratedAxeNodeResult;
+
+        expect(doesResultHaveMainRole(input)).toBe(false);
+    });
+
+    it('returns false for results with check results without role data', () => {
+        const input = {
+            any: [makeCheckResultWithRole(undefined)],
+            all: [makeCheckResultWithRole(undefined)],
+        } as DecoratedAxeNodeResult;
+
+        expect(doesResultHaveMainRole(input)).toBe(false);
+    });
+
+    it('returns false for results with check results with only non-main role data', () => {
+        const input = {
+            any: [makeCheckResultWithRole('complementary')],
+            all: [makeCheckResultWithRole('banner')],
+        } as DecoratedAxeNodeResult;
+
+        expect(doesResultHaveMainRole(input)).toBe(false);
+    });
+
+    it('returns true for results with an "any" check result with "main" role data', () => {
+        const input = {
+            any: [makeCheckResultWithRole('main')],
+            all: [],
+        } as DecoratedAxeNodeResult;
+
+        expect(doesResultHaveMainRole(input)).toBe(true);
+    });
+
+    it('returns true for results with an "all" check result with "main" role data', () => {
+        const input = {
+            any: [],
+            all: [makeCheckResultWithRole('main')],
+        } as DecoratedAxeNodeResult;
+
+        expect(doesResultHaveMainRole(input)).toBe(true);
+    });
+
+    it('returns true for results with mixed check results', () => {
+        const input = {
+            any: [makeCheckResultWithRole('banner'), makeCheckResultWithRole(undefined)],
+            all: [
+                makeCheckResultWithRole('main'),
+                makeCheckResultWithRole('banner'),
+                makeCheckResultWithRole(undefined),
+            ],
+        } as DecoratedAxeNodeResult;
+
+        expect(doesResultHaveMainRole(input)).toBe(true);
+    });
+
+    function makeCheckResultWithRole(role: string): FormattedCheckResult {
+        return {
+            id: 'test-check-id',
+            message: 'test check message',
+            data: {
+                role,
+            },
+        };
+    }
+});
