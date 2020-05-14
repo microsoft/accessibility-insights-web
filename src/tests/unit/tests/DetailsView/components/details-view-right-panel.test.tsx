e7c1ba560b92136411b55c3ecf07c0fb36be8a53
Kelsey Pownell <kepownel@microsoft.com>
2020-05-14 14:40:38 -0700

    test DetailsViewRightPanel changes

--- src/tests/unit/tests/DetailsView/components/details-view-right-panel.test.tsx
+++ src/tests/unit/tests/DetailsView/components/details-view-right-panel.test.tsx
@@ -6,7 +6,9 @@ import {
     GetDetailsRightPanelConfiguration,
 } from '../../../../../DetailsView/components/details-view-right-panel';
 import {
+    getGettingStartedViewKey,
     getOverviewKey,
+    getRequirementViewKey,
     getTestViewKey,
 } from '../../../../../DetailsView/components/left-nav/get-left-nav-selected-key';
 import { OverviewContainer } from '../../../../../DetailsView/components/overview-content/overview-content-container';
@@ -36,13 +38,31 @@ describe('DetailsViewRightPanelTests', () => {
             validateTestView(testSubject);
         });
 
-        it('GetDetailsRightPanelConfiguration: return TestView object when assessment selected', () => {
+        it('GetDetailsRightPanelConfiguration: return Overview object when assessment selected', () => {
             const testSubject = GetDetailsRightPanelConfiguration({
                 selectedDetailsViewPivot: DetailsViewPivotType.assessment,
                 detailsViewRightContentPanel: 'Overview',
             });
 
             validateOverview(testSubject);
+        });
+
+        it('GetDetailsRightPanelConfiguration: return RequirementView object when assessment selected', () => {
+            const testSubject = GetDetailsRightPanelConfiguration({
+                selectedDetailsViewPivot: DetailsViewPivotType.assessment,
+                detailsViewRightContentPanel: 'RequirementView',
+            });
+
+            validateRequirementView(testSubject);
+        });
+
+        it('GetDetailsRightPanelConfiguration: return GettingStarted object when assessment selected', () => {
+            const testSubject = GetDetailsRightPanelConfiguration({
+                selectedDetailsViewPivot: DetailsViewPivotType.assessment,
+                detailsViewRightContentPanel: 'GettingStartedView',
+            });
+
+            validateGettingStartedView(testSubject);
         });
     });
 
@@ -58,5 +78,19 @@ describe('DetailsViewRightPanelTests', () => {
         expect(configuration.GetTitle).toEqual(getOverviewTitle);
         expect(configuration.RightPanel).toEqual(OverviewContainer);
         expect(configuration.GetStartOverContextualMenuItemKeys()).toEqual(['assessment']);
+    }
+
+    function validateRequirementView(configuration: DetailsRightPanelConfiguration): void {
+        expect(configuration.GetLeftNavSelectedKey).toEqual(getRequirementViewKey);
+        expect(configuration.GetTitle).toEqual(getTestViewTitle);
+        expect(configuration.RightPanel).toEqual(TestViewContainer);
+        expect(configuration.GetStartOverContextualMenuItemKeys()).toEqual(['assessment', 'test']);
+    }
+
+    function validateGettingStartedView(configuration: DetailsRightPanelConfiguration): void {
+        expect(configuration.GetLeftNavSelectedKey).toEqual(getGettingStartedViewKey);
+        expect(configuration.GetTitle).toEqual(getTestViewTitle);
+        expect(configuration.RightPanel).toEqual(TestViewContainer);
+        expect(configuration.GetStartOverContextualMenuItemKeys()).toEqual(['assessment', 'test']);
     }
 });
