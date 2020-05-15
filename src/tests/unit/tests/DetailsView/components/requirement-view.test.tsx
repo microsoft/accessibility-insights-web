dd08300a561199f0321df4b86b7680a597ef662b
Wahaj <32555133+waabid@users.noreply.github.com>
2020-05-15 13:09:32 -0700

    feat(nav-bar-content): add table section to requirements view (#2690)

--- src/tests/unit/tests/DetailsView/components/requirement-view.test.tsx
+++ src/tests/unit/tests/DetailsView/components/requirement-view.test.tsx
@@ -1,37 +1,61 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { AssessmentsProviderImpl } from 'assessments/assessments-provider';
+import { AssessmentsProvider } from 'assessments/types/assessments-provider';
 import { Requirement } from 'assessments/types/requirement';
-import { GeneratedAssessmentInstance } from 'common/types/store-data/assessment-result-data';
+import {
+    AssessmentNavState,
+    GeneratedAssessmentInstance,
+    ManualTestStepResult,
+} from 'common/types/store-data/assessment-result-data';
+import { FeatureFlagStoreData } from 'common/types/store-data/feature-flag-store-data';
+import { PathSnippetStoreData } from 'common/types/store-data/path-snippet-store-data';
 import { VisualizationType } from 'common/types/visualization-type';
+import {
+    AssessmentViewUpdateHandler,
+    AssessmentViewUpdateHandlerProps,
+} from 'DetailsView/components/assessment-view-update-handler';
 import {
     RequirementView,
     RequirementViewDeps,
     RequirementViewProps,
 } from 'DetailsView/components/requirement-view';
+import { AssessmentInstanceTableHandler } from 'DetailsView/handlers/assessment-instance-table-handler';
 import { shallow } from 'enzyme';
+import { cloneDeep } from 'lodash';
 import * as React from 'react';
-import { Mock } from 'typemoq';
+import { IMock, It, Mock, Times } from 'typemoq';
 import { DictionaryStringTo } from 'types/common-types';
 
 describe('RequirementViewTest', () => {
-    it('renders with content from props', () => {
-        const requirementStub = {
+    let requirementStub: Requirement;
+    let assessmentNavState: AssessmentNavState;
+    let selectedRequirementStub: Requirement;
+    let assessmentsProviderMock: IMock<AssessmentsProvider>;
+    let props: RequirementViewProps;
+    let manualRequirementResultMapStub: DictionaryStringTo<ManualTestStepResult>;
+    let assessmentInstanceTableHandlerStub: AssessmentInstanceTableHandler;
+    let featureFlagStoreDataStub: FeatureFlagStoreData;
+    let pathSnippetStoreDataStub: PathSnippetStoreData;
+    let updateHandlerMock: IMock<AssessmentViewUpdateHandler>;
+
+    beforeEach(() => {
+        requirementStub = {
             name: 'test-requirement-name',
             description: <div>test-description</div>,
             howToTest: <p>how-to-test-stub</p>,
         } as Requirement;
 
-        const assessmentNavState = {
+        assessmentNavState = {
             selectedTestType: VisualizationType.Headings,
             selectedTestSubview: 'test-requirement-name',
         };
 
-        const selectedRequirementStub = {
+        selectedRequirementStub = {
             getVisualHelperToggle: ({}) => <div>test-visual-helper-toggle</div>,
         } as Readonly<Requirement>;
 
-        const assessmentsProviderMock = Mock.ofType(AssessmentsProviderImpl);
+        assessmentsProviderMock = Mock.ofType(AssessmentsProviderImpl);
         assessmentsProviderMock
             .setup(ap =>
                 ap.getStep(
@@ -40,19 +64,100 @@ describe('RequirementViewTest', () => {
                 ),
             )
             .returns(() => selectedRequirementStub);
+        assessmentInstanceTableHandlerStub = {
+            changeRequirementStatus: null,
+        } as AssessmentInstanceTableHandler;
+        manualRequirementResultMapStub = {
+            'some manual test step result id': null,
+        };
+        featureFlagStoreDataStub = {
+            'some feature flag': true,
+        };
+        pathSnippetStoreDataStub = {
+            path: null,
+        } as PathSnippetStoreData;
+        updateHandlerMock = Mock.ofType(AssessmentViewUpdateHandler);
 
-        const props: RequirementViewProps = {
-            deps: {} as RequirementViewDeps,
+        props = {
+            deps: { assessmentViewUpdateHandler: updateHandlerMock.object } as RequirementViewDeps,
             requirement: requirementStub,
             assessmentsProvider: assessmentsProviderMock.object,
             assessmentNavState: assessmentNavState,
             instancesMap: {} as DictionaryStringTo<GeneratedAssessmentInstance>,
-            isStepEnabled: true,
-            isStepScanned: true,
-        };
+            isRequirementEnabled: true,
+            isRequirementScanned: true,
+            manualRequirementResultMap: manualRequirementResultMapStub,
+            assessmentInstanceTableHandler: assessmentInstanceTableHandlerStub,
+            featureFlagStoreData: featureFlagStoreDataStub,
+            pathSnippetStoreData: pathSnippetStoreDataStub,
+            prevTarget: { id: 4 },
+            currentTarget: { id: 5 },
+            assessmentData: {},
+        } as RequirementViewProps;
+    });
 
+    it('renders with content from props', () => {
         const rendered = shallow(<RequirementView {...props} />);
 
         expect(rendered.getElement()).toMatchSnapshot();
     });
+
+    test('componentDidUpdate', () => {
+        const newProps = cloneDeep(props);
+        newProps.deps.assessmentViewUpdateHandler = updateHandlerMock.object;
+        const prevProps = props;
+        prevProps.assessmentNavState.selectedTestSubview = 'prevTestStep';
+
+        updateHandlerMock
+            .setup(u =>
+                u.update(
+                    It.isValue(getUpdateHandlerProps(prevProps)),
+                    It.isValue(getUpdateHandlerProps(newProps)),
+                ),
+            )
+            .verifiable(Times.once());
+
+        const testObject = new RequirementView(newProps);
+
+        testObject.componentDidUpdate(prevProps);
+
+        updateHandlerMock.verifyAll();
+    });
+
+    test('componentDidMount', () => {
+        updateHandlerMock
+            .setup(u => u.onMount(getUpdateHandlerProps(props)))
+            .verifiable(Times.once());
+
+        const testObject = new RequirementView(props);
+
+        testObject.componentDidMount();
+
+        updateHandlerMock.verifyAll();
+    });
+
+    test('componentWillUnmount', () => {
+        updateHandlerMock
+            .setup(u => u.onUnmount(getUpdateHandlerProps(props)))
+            .verifiable(Times.once());
+
+        const testObject = new RequirementView(props);
+
+        testObject.componentWillUnmount();
+
+        updateHandlerMock.verifyAll();
+    });
+
+    function getUpdateHandlerProps(
+        givenProps: RequirementViewProps,
+    ): AssessmentViewUpdateHandlerProps {
+        return {
+            deps: givenProps.deps,
+            isRequirementEnabled: givenProps.isRequirementEnabled,
+            assessmentNavState: givenProps.assessmentNavState,
+            assessmentData: givenProps.assessmentData,
+            prevTarget: givenProps.prevTarget,
+            currentTarget: givenProps.currentTarget,
+        };
+    }
 });
