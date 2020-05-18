9b57fc645ed702dc0aaf83e62e33849de1633049
lisli1 <48259897+lisli1@users.noreply.github.com>
2020-05-18 13:49:00 -0700

    feat(nav-bar-content): add RequirementView to ReflowAssessmentView (#2711)
    Render RequirementView instead of null in ReflowAssessmentView

--- src/DetailsView/components/reflow-assessment-view.tsx
+++ src/DetailsView/components/reflow-assessment-view.tsx
@@ -1,5 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { AssessmentDefaultMessageGenerator } from 'assessments/assessment-default-message-generator';
+import { RequirementResult } from 'common/assessment/requirement';
+import { NamedFC } from 'common/react/named-fc';
+import { FeatureFlagStoreData } from 'common/types/store-data/feature-flag-store-data';
+import { PathSnippetStoreData } from 'common/types/store-data/path-snippet-store-data';
+import { RequirementView, RequirementViewDeps } from 'DetailsView/components/requirement-view';
+import { AssessmentInstanceTableHandler } from 'DetailsView/handlers/assessment-instance-table-handler';
 import * as React from 'react';
 import { AssessmentTestResult } from '../../common/assessment/assessment-test-result';
 import { Tab } from '../../common/itab';
@@ -12,40 +19,75 @@ import {
 import { GettingStartedView } from './getting-started-view';
 import { TargetChangeDialog, TargetChangeDialogDeps } from './target-change-dialog';
 
-export type ReflowAssessmentViewDeps = TargetChangeDialogDeps;
+export type ReflowAssessmentViewDeps = TargetChangeDialogDeps & RequirementViewDeps;
 
 export type ReflowAssessmentViewProps = {
     deps: ReflowAssessmentViewDeps;
-    assessmentNavState: AssessmentNavState;
-    assessmentData: AssessmentData;
     currentTarget: Tab;
     prevTarget: PersistedTabInfo;
+    scanningInProgress: boolean;
+    selectedRequirementIsEnabled: boolean;
+    assessmentNavState: AssessmentNavState;
+    assessmentData: AssessmentData;
+    assessmentDefaultMessageGenerator: AssessmentDefaultMessageGenerator;
     assessmentTestResult: AssessmentTestResult;
+    assessmentInstanceTableHandler: AssessmentInstanceTableHandler;
+    featureFlagStoreData: FeatureFlagStoreData;
+    pathSnippetStoreData: PathSnippetStoreData;
 };
 
-export class ReflowAssessmentView extends React.Component<ReflowAssessmentViewProps> {
-    public render(): JSX.Element {
-        const { assessmentTestResult } = this.props;
-        if (this.props.assessmentNavState.selectedTestSubview === gettingStartedSubview) {
+export const ReflowAssessmentView = NamedFC<ReflowAssessmentViewProps>(
+    'ReflowAssessmentView',
+    props => {
+        const renderGettingStartedView = () => {
             return (
-                <div>
-                    {this.renderTargetChangeDialog()}
-                    <GettingStartedView
-                        gettingStartedContent={assessmentTestResult.definition.gettingStarted}
-                    />
-                </div>
+                <GettingStartedView
+                    gettingStartedContent={props.assessmentTestResult.definition.gettingStarted}
+                />
             );
-        }
-        return null;
-    }
+        };
 
-    private renderTargetChangeDialog(): JSX.Element {
+        const renderRequirementView = () => {
+            const selectedRequirement: RequirementResult = props.assessmentTestResult.getRequirementResult(
+                props.assessmentNavState.selectedTestSubview,
+            );
+
+            return (
+                <RequirementView
+                    deps={props.deps}
+                    requirement={selectedRequirement.definition}
+                    assessmentsProvider={props.deps.assessmentsProvider}
+                    assessmentNavState={props.assessmentNavState}
+                    instancesMap={props.assessmentData.generatedAssessmentInstancesMap}
+                    isRequirementEnabled={props.selectedRequirementIsEnabled}
+                    isRequirementScanned={selectedRequirement.data.isStepScanned}
+                    assessmentInstanceTableHandler={props.assessmentInstanceTableHandler}
+                    featureFlagStoreData={props.featureFlagStoreData}
+                    pathSnippetStoreData={props.pathSnippetStoreData}
+                    scanningInProgress={props.scanningInProgress}
+                    manualRequirementResultMap={props.assessmentData.manualTestStepResultMap}
+                    assessmentDefaultMessageGenerator={props.assessmentDefaultMessageGenerator}
+                    assessmentData={props.assessmentData}
+                    currentTarget={props.currentTarget}
+                    prevTarget={props.prevTarget}
+                />
+            );
+        };
+
+        const mainContent: JSX.Element =
+            props.assessmentNavState.selectedTestSubview === gettingStartedSubview
+                ? renderGettingStartedView()
+                : renderRequirementView();
+
         return (
-            <TargetChangeDialog
-                deps={this.props.deps}
-                prevTab={this.props.prevTarget}
-                newTab={this.props.currentTarget}
-            />
+            <div>
+                <TargetChangeDialog
+                    deps={props.deps}
+                    prevTab={props.prevTarget}
+                    newTab={props.currentTarget}
+                />
+                {mainContent}
+            </div>
         );
-    }
-}
+    },
+);
--- src/tests/unit/tests/DetailsView/components/reflow-assessment-view.test.tsx
+++ src/tests/unit/tests/DetailsView/components/reflow-assessment-view.test.tsx
@@ -1,15 +1,19 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { Requirement } from 'assessments/types/requirement';
 import { AssessmentTestResult } from 'common/assessment/assessment-test-result';
+import { RequirementData, RequirementResult } from 'common/assessment/requirement';
 import {
     AssessmentData,
     GettingStarted,
     gettingStartedSubview,
     RequirementName,
 } from 'common/types/store-data/assessment-result-data';
+import { FeatureFlagStoreData } from 'common/types/store-data/feature-flag-store-data';
+import { PathSnippetStoreData } from 'common/types/store-data/path-snippet-store-data';
+import { AssessmentInstanceTableHandler } from 'DetailsView/handlers/assessment-instance-table-handler';
 import { shallow } from 'enzyme';
 import * as React from 'react';
-import { Mock } from 'typemoq';
 import {
     ReflowAssessmentView,
     ReflowAssessmentViewDeps,
@@ -30,24 +34,53 @@ describe('AssessmentViewTest', () => {
     });
 
     function generateProps(subview: RequirementName | GettingStarted): ReflowAssessmentViewProps {
-        const assessmentDataMock = Mock.ofType<AssessmentData>();
+        const assessmentDataStub = {} as AssessmentData;
 
+        const requirementStub = {
+            name: 'test-requirement-name',
+            description: <div>test-description</div>,
+            howToTest: <p>how-to-test-stub</p>,
+        } as Requirement;
+        const requirementResultStub: RequirementResult = {
+            definition: requirementStub,
+            data: { isStepScanned: true } as RequirementData,
+        };
+        const getRequirementResultStub = (requirementKey: string) => {
+            return requirementResultStub;
+        };
         const assessmentTestResultStub: AssessmentTestResult = {
             definition: {
                 gettingStarted: <h1>Hello</h1>,
             },
+            getRequirementResult: getRequirementResultStub,
         } as AssessmentTestResult;
+
+        const assessmentInstanceTableHandlerStub = {
+            changeRequirementStatus: null,
+        } as AssessmentInstanceTableHandler;
+
+        const featureFlagStoreDataStub: FeatureFlagStoreData = {
+            'some feature flag': true,
+        };
+        const pathSnippetStoreDataStub = {
+            path: null,
+        } as PathSnippetStoreData;
 
         const reflowProps = {
             deps: {} as ReflowAssessmentViewDeps,
             prevTarget: { id: 4 },
             currentTarget: { id: 5 },
+            scanningInProgress: true,
+            selectedRequirementIsEnabled: true,
             assessmentNavState: {
                 selectedTestSubview: subview,
                 selectedTestType: -1,
             },
-            assessmentData: assessmentDataMock.object,
+            assessmentData: assessmentDataStub,
             assessmentTestResult: assessmentTestResultStub,
+            assessmentInstanceTableHandler: assessmentInstanceTableHandlerStub,
+            featureFlagStoreData: featureFlagStoreDataStub,
+            pathSnippetStoreData: pathSnippetStoreDataStub,
         } as ReflowAssessmentViewProps;
         return reflowProps;
     }
