96c2feb41de3b4d63a258361d00089a22be864a9
Dan Bjorge <dan@dbjorge.net>
2020-05-21 15:14:49 -0700

    Add UTs for new factory

---
+++ src/tests/unit/tests/common/browser-adapters/browser-adapter-factory.test.ts
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+import { BrowserAdapterFactory } from 'common/browser-adapters/browser-adapter-factory';
+import { ChromiumAdapter } from 'common/browser-adapters/chromium-adapter';
+import { FirefoxAdapter } from 'common/browser-adapters/firefox-adapter';
+import { IMock, Mock } from 'typemoq';
+import { UAParser } from 'ua-parser-js';
+
+// Licensed under the MIT License.
+describe('BrowserAdapterFactory', () => {
+    describe('makeFromUserAgent', () => {
+        let mockUAParser: IMock<UAParser>;
+
+        it('produces a FirefoxAdapter for a Gecko-engine user agent', () => {
+            setupMockEngine('Gecko');
+            const testSubject = new BrowserAdapterFactory(mockUAParser.object);
+            expect(testSubject.makeFromUserAgent()).toBeInstanceOf(FirefoxAdapter);
+        });
+
+        it('produces a ChromiumAdapter for a WebKit-engine user agent (Chrome, new Edge)', () => {
+            setupMockEngine('WebKit');
+            const testSubject = new BrowserAdapterFactory(mockUAParser.object);
+            expect(testSubject.makeFromUserAgent()).toBeInstanceOf(ChromiumAdapter);
+        });
+
+        it('produces a ChromiumAdapter as a fallback for an unrecognized user agent', () => {
+            setupMockEngine('some unrecognized engine name');
+            const testSubject = new BrowserAdapterFactory(mockUAParser.object);
+            expect(testSubject.makeFromUserAgent()).toBeInstanceOf(ChromiumAdapter);
+        });
+
+        beforeEach(() => {
+            mockUAParser = Mock.ofType<UAParser>();
+        });
+
+        function setupMockEngine(engineName: string): void {
+            mockUAParser
+                .setup(m => m.getEngine())
+                .returns(() => ({
+                    name: engineName,
+                    version: '0.0.0',
+                }));
+        }
+    });
+});
