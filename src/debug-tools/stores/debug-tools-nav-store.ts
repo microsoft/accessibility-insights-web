2352c677845a80b8f4a6864efc3f97ef6e6250d7
Sebastian Morales <sebastian.moralesd@gmail.com>
2020-05-22 14:30:54 -0700

    add debug tools nave store

---
+++ src/debug-tools/stores/debug-tools-nav-store.ts
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { BaseStoreImpl } from 'background/stores/base-store-impl';
+import { StoreNames } from 'common/stores/store-names';
+import { DebugToolsNavActions } from 'debug-tools/actions/debug-tools-nav-actions';
+
+export type ToolsNavKey = 'stores' | 'telemetryViewer';
+
+export type DebugToolsNavStoreData = {
+    selectedTool: ToolsNavKey;
+};
+
+export class DebugToolsNavStore extends BaseStoreImpl<DebugToolsNavStoreData> {
+    constructor(private readonly toolsNavActions: DebugToolsNavActions) {
+        super(StoreNames.DebugToolsNavStore);
+    }
+
+    public getDefaultState(): DebugToolsNavStoreData {
+        return {
+            selectedTool: 'stores',
+        };
+    }
+
+    protected addActionListeners(): void {
+        this.toolsNavActions.setSelectedTool.addListener(this.onSetSelectedTool);
+    }
+    private onSetSelectedTool = (selectedTool: ToolsNavKey) => {
+        if (this.state.selectedTool !== selectedTool) {
+            this.state.selectedTool = selectedTool;
+            this.emitChanged();
+        }
+    };
+}
