0c77d62a0aac9d0e9cecdfd4ae664900624814fa
Lisa Li <lisli@microsoft.com>
2020-06-01 14:31:22 -0700

    add hamburger menu to header

--- src/tests/unit/tests/common/components/left-nav-hamburger-button.test.tsx
+++ src/tests/unit/tests/common/components/left-nav-hamburger-button.test.tsx
@@ -7,11 +7,22 @@ import {
     LeftNavHamburgerButton,
     LeftNavHamburgerButtonProps,
 } from 'common/components/left-nav-hamburger-button';
+import { DetailsViewPivotType } from 'common/types/details-view-pivot-type';
 
 describe('LeftNavHamburgerButton', () => {
-    it('renders per snapshot', () => {
+    it('renders per snapshot for FastPass', () => {
         const props: LeftNavHamburgerButtonProps = {
-            ariaLabel: 'test-aria-label',
+            selectedPivot: DetailsViewPivotType.fastPass,
+        };
+
+        const wrapper = shallow(<LeftNavHamburgerButton {...props} />);
+
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+
+    it('renders per snapshot for Assessment', () => {
+        const props: LeftNavHamburgerButtonProps = {
+            selectedPivot: DetailsViewPivotType.assessment,
         };
 
         const wrapper = shallow(<LeftNavHamburgerButton {...props} />);
