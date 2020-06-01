347117f996132b130a36b9f9e0d8057ab25175c5
lisli1 <48259897+lisli1@users.noreply.github.com>
2020-06-01 16:46:32 -0700

    feat(nav-bar-functionality): add left nav hamburger menu to header (#2777)
    - Updated Header to take in the nav menu as an optional prop
    - Updated InteractiveHeader to pass LeftNavHamburgerButton under a feature flag to Header
    - Updated LeftNavHamburgerButton to use the selectedPivot prop to determine which aria label to use (different labels for Assessment and FastPass)

--- src/common/components/header.tsx
+++ src/common/components/header.tsx
@@ -1,10 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as React from 'react';
-
 import { HeaderIcon, HeaderIconDeps } from 'common/components/header-icon';
 import { NamedFC } from 'common/react/named-fc';
 import { TextContent } from 'content/strings/text-content';
+import * as React from 'react';
 import * as styles from './header.scss';
 
 export type HeaderDeps = { textContent: Pick<TextContent, 'applicationTitle'> } & HeaderIconDeps;
@@ -13,12 +12,14 @@ export type HeaderProps = {
     deps: HeaderDeps;
     items?: JSX.Element;
     farItems?: JSX.Element;
+    navMenu?: JSX.Element;
 };
 
 export const Header = NamedFC<HeaderProps>('Header', props => {
     const { applicationTitle } = props.deps.textContent;
     return (
         <header className={styles.headerBar}>
+            {props.navMenu}
             <HeaderIcon deps={props.deps} />
             <span className={styles.headerTitle}>{applicationTitle}</span>
             <div>{props.items}</div>
