fbad0b545d97b6c44c9f95808259045080550623
Sebastian Morales <smoralesd@users.noreply.github.com>
2020-06-01 16:10:59 -0700

    tools: debug tools nav + telemetry viewer  (#2750)
    This PR is a follow up of #2515, where the **Telemetry Viewer** was introduced.
    
    Adding a left nav for the debug tools page, which contains 2 navs: stores & telemetry viewer.
    
    Add actions, store and action creator and add them to the dependency tree from the debug tools init file.
    
    * add debug tools nav component
    * add debug tools nav actions
    * add debug tools nav action creator
    * add debug tools nave store
    * update telemetry viewer
    * add current view component
    * update debug tools view to rendere the nav and the current view, update tests, update debug tools init
    * address comments

---
+++ src/debug-tools/components/debug-tools-nav.tsx
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { NamedFC } from 'common/react/named-fc';
+import { DebugToolsNavActionCreator } from 'debug-tools/action-creators/debug-tools-nav-action-creator';
+import { DebugToolsNavStoreData, ToolsNavKey } from 'debug-tools/stores/debug-tools-nav-store';
+import { Nav } from 'office-ui-fabric-react';
+import * as React from 'react';
+
+export type DebugToolsNavState = {
+    debugToolsNavStoreData: DebugToolsNavStoreData;
+};
+export type DebugToolsNavDeps = {
+    debugToolsNavActionCreator: DebugToolsNavActionCreator;
+};
+
+export type DebugToolsNavProps = {
+    deps: DebugToolsNavDeps;
+    state: DebugToolsNavState;
+    className: string;
+};
+
+export const DebugToolsNav = NamedFC<DebugToolsNavProps>(
+    'ToolsNav',
+    ({ className, deps, state }) => {
+        const { debugToolsNavStoreData } = state;
+        return (
+            <Nav
+                className={className}
+                selectedKey={debugToolsNavStoreData.selectedTool}
+                groups={[
+                    {
+                        links: [
+                            {
+                                key: 'stores',
+                                name: 'Stores',
+                                url: '',
+                                icon: 'OfflineStorage',
+                            },
+                            {
+                                key: 'telemetryViewer',
+                                name: 'TelemetryViewer',
+                                url: '',
+                                icon: 'Diagnostic',
+                            },
+                        ],
+                    },
+                ]}
+                onLinkClick={(ev, item) =>
+                    deps.debugToolsNavActionCreator.onSelectTool(item.key as ToolsNavKey)
+                }
+            />
+        );
+    },
+);
