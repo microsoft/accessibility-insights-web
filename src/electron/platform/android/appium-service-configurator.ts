113f4f8072b5a9bedc66327af339f5e83fb9241c
Dave Tryon <dtryon@microsoft.com>
2020-06-02 15:27:56 -0700

    Add code and tests for getConnectedDevices

--- src/electron/platform/android/appium-service-configurator.ts
+++ src/electron/platform/android/appium-service-configurator.ts
@@ -9,6 +9,10 @@ import {
     PermissionInfo,
 } from 'electron/platform/android/android-service-configurator';
 
+type AdbDevice = {
+    udid: string;
+};
+
 export class AppiumServiceConfigurator implements AndroidServiceConfigurator {
     private readonly adb: ADB;
 
@@ -16,8 +20,59 @@ export class AppiumServiceConfigurator implements AndroidServiceConfigurator {
         this.adb = adb;
     }
 
-    public getConnectedDevices(): Promise<Array<DeviceInfo>> {
-        return null;
+    public getConnectedDevices = (): Promise<Array<DeviceInfo>> => {
+        return new Promise<Array<DeviceInfo>>(async (resolve, reject) => {
+            try {
+                // First add devices that are flagged as emulator, then
+                // add devices that aren't flagged as eulators
+                const detectedDevices = {};
+                await this.AddDevices(
+                    await this.adb.getConnectedEmulators(),
+                    true,
+                    detectedDevices,
+                );
+                await this.AddDevices(await this.adb.getConnectedDevices(), false, detectedDevices);
+
+                const output: Array<DeviceInfo> = [];
+                Object.values(detectedDevices).forEach(device => output.push(device as DeviceInfo));
+                resolve(output);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    };
+
+    private AddDevices(
+        devices: Array<AdbDevice>,
+        isEmulator: boolean,
+        detectedDevices: any,
+    ): Promise<void> {
+        return new Promise<void>(async (resolve, reject) => {
+            try {
+                for (let loop = 0; loop < devices.length; loop++) {
+                    const id = devices[loop].udid;
+                    if (!detectedDevices[id]) {
+                        const deviceInfo: DeviceInfo = await this.getDeviceInfo(id, isEmulator);
+                        detectedDevices[id] = deviceInfo;
+                    }
+                }
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    private getDeviceInfo(id: string, isEmulator: boolean): Promise<DeviceInfo> {
+        return new Promise<DeviceInfo>(async (resolve, reject) => {
+            try {
+                this.adb.setDeviceId(id);
+                const friendlyName = await this.adb.getModel();
+                resolve({ id, isEmulator, friendlyName });
+            } catch (err) {
+                reject(err);
+            }
+        });
     }
 
     public getPackageInfo(deviceId: string): Promise<PackageInfo> {
