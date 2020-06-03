16df899407e43a6e2e7dd3ab63e96e6b5b854eb9
Lisa Li <lisli@microsoft.com>
2020-06-03 03:33:02 -0700

    change left nav hamburger button to receive aria label prop

--- src/common/components/left-nav-hamburger-button.tsx
+++ src/common/components/left-nav-hamburger-button.tsx
@@ -2,30 +2,22 @@
 // Licensed under the MIT License.
 
 import { NamedFC } from 'common/react/named-fc';
-import { DetailsViewPivotType } from 'common/types/details-view-pivot-type';
 import { IconButton } from 'office-ui-fabric-react';
 import * as React from 'react';
 import * as styles from './left-nav-hamburger-button.scss';
 
 export type LeftNavHamburgerButtonProps = {
-    selectedPivot: DetailsViewPivotType;
+    ariaLabel: string;
 };
 
 export const LeftNavHamburgerButton = NamedFC<LeftNavHamburgerButtonProps>(
     'LeftNavHamburgerButton',
     props => {
-        const assessmentAriaLabel = 'Assessment expand to see list of all tests and requirements';
-        const fastPassAriaLabel = 'FastPass expand to see a list of all tests';
-        const ariaLabel =
-            props.selectedPivot === DetailsViewPivotType.assessment
-                ? assessmentAriaLabel
-                : fastPassAriaLabel;
-
         return (
             <IconButton
                 className={styles.leftNavHamburgerButton}
                 iconProps={{ iconName: 'GlobalNavButton' }}
-                ariaLabel={ariaLabel}
+                ariaLabel={props.ariaLabel}
             />
         );
     },
--- src/tests/unit/tests/common/components/left-nav-hamburger-button.test.tsx
+++ src/tests/unit/tests/common/components/left-nav-hamburger-button.test.tsx
@@ -1,28 +1,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { shallow } from 'enzyme';
-import * as React from 'react';
-
 import {
     LeftNavHamburgerButton,
     LeftNavHamburgerButtonProps,
 } from 'common/components/left-nav-hamburger-button';
-import { DetailsViewPivotType } from 'common/types/details-view-pivot-type';
+import { shallow } from 'enzyme';
+import * as React from 'react';
 
 describe('LeftNavHamburgerButton', () => {
-    it('renders per snapshot for FastPass', () => {
+    it('renders per snapshot', () => {
         const props: LeftNavHamburgerButtonProps = {
-            selectedPivot: DetailsViewPivotType.fastPass,
-        };
-
-        const wrapper = shallow(<LeftNavHamburgerButton {...props} />);
-
-        expect(wrapper.getElement()).toMatchSnapshot();
-    });
-
-    it('renders per snapshot for Assessment', () => {
-        const props: LeftNavHamburgerButtonProps = {
-            selectedPivot: DetailsViewPivotType.assessment,
+            ariaLabel: 'test-aria-label',
         };
 
         const wrapper = shallow(<LeftNavHamburgerButton {...props} />);
