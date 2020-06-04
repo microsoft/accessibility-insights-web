1b73e02dc4448fcd1d8606df4f868f1e21e7bf68
JGibson2019 <jacqueline.gibson@utexas.edu>
2020-06-04 16:29:37 -0700

    added the spinner to the detect-adb-step ui

--- src/electron/views/device-connect-view/components/android-setup/detect-adb-step.tsx
+++ src/electron/views/device-connect-view/components/android-setup/detect-adb-step.tsx
@@ -2,11 +2,25 @@
 // Licensed under the MIT License.
 import { NamedFC } from 'common/react/named-fc';
 import { CommonAndroidSetupStepProps } from 'electron/views/device-connect-view/components/android-setup/android-setup-types';
+import { Spinner, SpinnerSize } from 'office-ui-fabric-react';
 import * as React from 'react';
+
+import { androidSetupSpinner } from '../../device-connect-view.scss';
 
 export const DetectAdbStep = NamedFC<CommonAndroidSetupStepProps>(
     'DetectAdbStep',
     (props: CommonAndroidSetupStepProps) => {
-        return <>step: {props.androidSetupStoreData.currentStepId}</>;
+        const renderSpinner = () => {
+            return (
+                <Spinner
+                    className={androidSetupSpinner}
+                    size={SpinnerSize.large}
+                    label="Loading..."
+                    role="alert"
+                    aria-live={props['aria-live']}
+                />
+            );
+        };
+        return renderSpinner();
     },
 );
