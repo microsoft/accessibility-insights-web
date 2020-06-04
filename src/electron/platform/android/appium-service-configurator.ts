2559b74c0edc2542c47000c23e142c79783de1a0
Dave Tryon <dtryon@microsoft.com>
2020-06-04 16:25:28 -0700

    Refactor to use async keyword

--- src/electron/platform/android/appium-service-configurator.ts
+++ src/electron/platform/android/appium-service-configurator.ts
@@ -22,111 +22,67 @@ export class AppiumServiceConfigurator implements AndroidServiceConfigurator {
         this.adb = adb;
     }
 
-    public getConnectedDevices = (): Promise<Array<DeviceInfo>> => {
-        return new Promise<Array<DeviceInfo>>(async (resolve, reject) => {
-            try {
-                // First add devices that are flagged as emulator, then
-                // add devices that aren't flagged as emulators
-                const detectedDevices = {};
-                await this.addDevices(
-                    await this.adb.getConnectedEmulators(),
-                    true,
-                    detectedDevices,
-                );
-                await this.addDevices(await this.adb.getConnectedDevices(), false, detectedDevices);
+    public getConnectedDevices = async (): Promise<Array<DeviceInfo>> => {
+        // First add devices that are flagged as emulator, then
+        // add devices that aren't flagged as emulators
+        const detectedDevices = {};
+        await this.addDevices(await this.adb.getConnectedEmulators(), true, detectedDevices);
+        await this.addDevices(await this.adb.getConnectedDevices(), false, detectedDevices);
 
-                const output: Array<DeviceInfo> = [];
-                Object.values(detectedDevices).forEach(device => output.push(device as DeviceInfo));
-                resolve(output);
-            } catch (error) {
-                reject(error);
-            }
-        });
+        const output: Array<DeviceInfo> = [];
+        Object.values(detectedDevices).forEach(device => output.push(device as DeviceInfo));
+        return output;
     };
 
-    private addDevices(
+    private async addDevices(
         devices: Array<AdbDevice>,
         isEmulator: boolean,
         detectedDevices: any,
     ): Promise<void> {
-        return new Promise<void>(async (resolve, reject) => {
-            try {
-                for (let loop = 0; loop < devices.length; loop++) {
-                    const id = devices[loop].udid;
-                    if (!detectedDevices[id]) {
-                        const deviceInfo: DeviceInfo = await this.getDeviceInfo(id, isEmulator);
-                        detectedDevices[id] = deviceInfo;
-                    }
-                }
-                resolve();
-            } catch (error) {
-                reject(error);
+        for (let loop = 0; loop < devices.length; loop++) {
+            const id = devices[loop].udid;
+            if (!detectedDevices[id]) {
+                const deviceInfo: DeviceInfo = await this.getDeviceInfo(id, isEmulator);
+                detectedDevices[id] = deviceInfo;
             }
-        });
+        }
     }
 
-    private getDeviceInfo(id: string, isEmulator: boolean): Promise<DeviceInfo> {
-        return new Promise<DeviceInfo>(async (resolve, reject) => {
-            try {
-                this.adb.setDeviceId(id);
-                const friendlyName = await this.adb.getModel();
-                resolve({ id, isEmulator, friendlyName });
-            } catch (err) {
-                reject(err);
-            }
-        });
+    private async getDeviceInfo(id: string, isEmulator: boolean): Promise<DeviceInfo> {
+        this.adb.setDeviceId(id);
+        const friendlyName = await this.adb.getModel();
+        return { id, isEmulator, friendlyName };
     }
 
-    public getPackageInfo = (deviceId: string): Promise<PackageInfo> => {
-        return new Promise<PackageInfo>(async (resolve, reject) => {
-            try {
-                this.adb.setDeviceId(deviceId);
-                const info: PackageInfo = await this.adb.getPackageInfo(servicePackageName);
-                if (info?.versionCode !== undefined) {
-                    resolve({ versionCode: info.versionCode, versionName: info.versionName });
-                }
-                reject(new Error('Unable to obtain package version information'));
-            } catch (error) {
-                reject(error);
-            }
-        });
+    public getPackageInfo = async (deviceId: string): Promise<PackageInfo> => {
+        this.adb.setDeviceId(deviceId);
+        const info: PackageInfo = await this.adb.getPackageInfo(servicePackageName);
+        if (info?.versionCode === undefined) {
+            throw new Error('Unable to obtain package version information');
+        }
+        return { versionCode: info.versionCode, versionName: info.versionName };
     };
 
-    public getPermissionInfo = (deviceId: string): Promise<PermissionInfo> => {
-        return new Promise<PermissionInfo>(async (resolve, reject) => {
-            try {
-                const dumpsys = 'dumpsys';
+    public getPermissionInfo = async (deviceId: string): Promise<PermissionInfo> => {
+        const dumpsys = 'dumpsys';
 
-                this.adb.setDeviceId(deviceId);
-                let stdout: string = await this.adb.shell([dumpsys, 'accessibility']);
-                if (!stdout.includes('label=Accessibility Insights')) {
-                    reject(new Error('Accessibility Insights for Android Service is not running'));
-                } else {
-                    stdout = await this.adb.shell([dumpsys, 'media_projection']);
-                    const screenshotGranted: boolean = stdout.includes(servicePackageName);
-                    resolve({ screenshotGranted });
-                }
-            } catch (error) {
-                reject(error);
-            }
-        });
+        this.adb.setDeviceId(deviceId);
+        let stdout: string = await this.adb.shell([dumpsys, 'accessibility']);
+        if (!stdout.includes('label=Accessibility Insights')) {
+            throw new Error('Accessibility Insights for Android Service is not running');
+        }
+        stdout = await this.adb.shell([dumpsys, 'media_projection']);
+        const screenshotGranted: boolean = stdout.includes(servicePackageName);
+        return { screenshotGranted };
     };
 
-    public installService = (deviceId: string): Promise<void> => {
-        return new Promise<void>(async (resolve, reject) => {
-            try {
-                const pathToApk = './ServiceForAndroid/AccessibilityInsightsforAndroidService.apk';
-                this.adb.setDeviceId(deviceId);
-                const stdout: string = await this.adb.adbExec(['install', '-d', pathToApk]);
-                if (stdout.includes('Success')) {
-                    resolve();
-                } else {
-                    reject(new Error(stdout));
-                }
-            } catch (error) {
-                reject(error);
-            }
-        });
+    public installService = async (deviceId: string): Promise<void> => {
+        const pathToApk = './ServiceForAndroid/AccessibilityInsightsforAndroidService.apk';
+        this.adb.setDeviceId(deviceId);
+        const stdout: string = await this.adb.adbExec(['install', '-d', pathToApk]);
+        if (!stdout.includes('Success')) {
+            throw new Error(stdout);
+        }
     };
 
     public setTcpForwarding = async (deviceId: string): Promise<void> => {
