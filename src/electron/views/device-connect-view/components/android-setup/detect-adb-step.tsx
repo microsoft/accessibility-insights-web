4e7c346aa8f34ba7db31ba2365fd18c42e2597ad
JGibson2019 <jacqueline.gibson@microsoft.com>
2020-06-05 11:31:06 -0700

    feat(android-setup): detect-adb-step dialog implementation (#2802)
    * added the spinner to the detect-adb-step ui
    
    * added a standalone scss file for android-setup styling. simplified detect-adb-step to just return the spinner and not call a method

--- src/electron/views/device-connect-view/components/android-setup/detect-adb-step.tsx
+++ src/electron/views/device-connect-view/components/android-setup/detect-adb-step.tsx
@@ -2,11 +2,22 @@
 // Licensed under the MIT License.
 import { NamedFC } from 'common/react/named-fc';
 import { CommonAndroidSetupStepProps } from 'electron/views/device-connect-view/components/android-setup/android-setup-types';
+import { Spinner, SpinnerSize } from 'office-ui-fabric-react';
 import * as React from 'react';
+
+import { androidSetupSpinner } from '../android-setup/android-setup.scss';
 
 export const DetectAdbStep = NamedFC<CommonAndroidSetupStepProps>(
     'DetectAdbStep',
     (props: CommonAndroidSetupStepProps) => {
-        return <>step: {props.androidSetupStoreData.currentStepId}</>;
+        return (
+            <Spinner
+                className={androidSetupSpinner}
+                size={SpinnerSize.large}
+                label="Loading..."
+                role="alert"
+                aria-live={props['aria-live']}
+            />
+        );
     },
 );
