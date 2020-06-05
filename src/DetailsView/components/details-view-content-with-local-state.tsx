01126a8f5306abacc66ae4cbc2506f3560175cb5
Haonan Liu <haonliu@microsoft.com>
2020-06-05 02:56:40 -0700

    lint fix

--- src/DetailsView/components/details-view-content-with-local-state.tsx
+++ src/DetailsView/components/details-view-content-with-local-state.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { NamedFC } from 'common/react/named-fc';
 import { DetailsViewContent } from 'DetailsView/components/details-view-content';
 import { DetailsViewContainerProps } from 'DetailsView/details-view-container';
 import * as React from 'react';
