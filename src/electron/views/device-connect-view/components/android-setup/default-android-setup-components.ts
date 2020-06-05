b71eb2f82267afa07d0cdf45603f45f5cb7218b7
Dan Bjorge <dan@dbjorge.net>
2020-06-05 13:33:47 -0700

    Add skeleton of new step UI component

--- src/electron/views/device-connect-view/components/android-setup/default-android-setup-components.ts
+++ src/electron/views/device-connect-view/components/android-setup/default-android-setup-components.ts
@@ -2,7 +2,9 @@
 // Licensed under the MIT License.
 import { AndroidSetupStepComponentProvider } from 'electron/views/device-connect-view/components/android-setup/android-setup-types';
 import { DetectAdbStep } from 'electron/views/device-connect-view/components/android-setup/detect-adb-step';
+import { PromptLocateAdbStep } from 'electron/views/device-connect-view/components/android-setup/prompt-locate-adb-step';
 
 export const defaultAndroidSetupComponents: AndroidSetupStepComponentProvider = {
     'detect-adb': DetectAdbStep,
+    'prompt-locate-adb': PromptLocateAdbStep,
 };
