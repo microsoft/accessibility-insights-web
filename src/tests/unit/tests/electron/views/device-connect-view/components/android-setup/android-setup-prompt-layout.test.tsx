691ab4545793cf16af075a663f84d9ce0afbaefa
Dan Bjorge <dan@dbjorge.net>
2020-06-05 19:07:25 -0700

    Add prompt layout tests

--- src/tests/unit/tests/electron/views/device-connect-view/components/android-setup/android-setup-prompt-layout.test.tsx
+++ src/tests/unit/tests/electron/views/device-connect-view/components/android-setup/android-setup-prompt-layout.test.tsx
@@ -1,37 +1,36 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { UserConfigurationStoreData } from 'common/types/store-data/user-configuration-store';
-import { CommonAndroidSetupStepProps } from 'electron/views/device-connect-view/components/android-setup/android-setup-types';
-import { PromptLocateAdbStep } from 'electron/views/device-connect-view/components/android-setup/prompt-locate-adb-step';
+import {
+    AndroidSetupPromptLayout,
+    AndroidSetupPromptLayoutProps,
+} from 'electron/views/device-connect-view/components/android-setup/android-setup-prompt-layout';
 import { shallow } from 'enzyme';
 import * as React from 'react';
 
-describe('PromptLocateAdbStep', () => {
-    let props: CommonAndroidSetupStepProps;
-
+describe('AndroidSetupPromptLayout', () => {
+    let props: AndroidSetupPromptLayoutProps;
+    const stubChildContent = <p>child content</p>;
     beforeEach(() => {
         props = {
-            userConfigurationStoreData: {} as UserConfigurationStoreData,
-            androidSetupStoreData: {
-                currentStepId: 'prompt-locate-adb',
-            },
-            deps: {
-                androidSetupActionCreator: null,
-                androidSetupStepComponentProvider: null,
-                LinkComponent: null,
-            },
+            headerText: 'Header text',
+            leftFooterButtonProps: { text: 'left footer button' },
+            rightFooterButtonProps: { text: 'right footer button' },
         };
     });
 
-    it('renders per snapshot with adbLocation not set', () => {
-        props.userConfigurationStoreData.adbLocation = null;
-        const rendered = shallow(<PromptLocateAdbStep {...props} />);
+    it('renders per snapshot without a moreInfoLink', () => {
+        props.moreInfoLink = undefined;
+        const rendered = shallow(
+            <AndroidSetupPromptLayout {...props}>{stubChildContent}</AndroidSetupPromptLayout>,
+        );
         expect(rendered.getElement()).toMatchSnapshot();
     });
 
-    it('renders per snapshot with adbLocation set', () => {
-        props.userConfigurationStoreData.adbLocation = '/some/path/to/android/home';
-        const rendered = shallow(<PromptLocateAdbStep {...props} />);
+    it('renders per snapshot with a moreInfoLink', () => {
+        props.moreInfoLink = <a href="https://test">test link</a>;
+        const rendered = shallow(
+            <AndroidSetupPromptLayout {...props}>{stubChildContent}</AndroidSetupPromptLayout>,
+        );
         expect(rendered.getElement()).toMatchSnapshot();
     });
 });
