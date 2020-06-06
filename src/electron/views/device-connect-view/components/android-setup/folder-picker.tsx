291b586b8e6aa172ab1b81ca7521c114c4f392f1
Dan Bjorge <dan@dbjorge.net>
2020-06-05 17:48:09 -0700

    import order

--- src/electron/views/device-connect-view/components/android-setup/folder-picker.tsx
+++ src/electron/views/device-connect-view/components/android-setup/folder-picker.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { NamedFC } from 'common/react/named-fc';
-import { PrimaryButton, TextField, getId } from 'office-ui-fabric-react';
+import { getId, PrimaryButton, TextField } from 'office-ui-fabric-react';
 import * as React from 'react';
 import * as styles from './folder-picker.scss';
 
