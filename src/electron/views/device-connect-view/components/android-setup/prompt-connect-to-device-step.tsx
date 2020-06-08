1d919b4dddb7bc9d1cb2b44bacb1fded5b4fc5a8
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2020-06-08 15:26:02 -0700

    Add "detect my device" dialog

---
+++ src/electron/views/device-connect-view/components/android-setup/prompt-connect-to-device-step.tsx
@@ -0,0 +1,51 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { NamedFC } from 'common/react/named-fc';
+import { PrimaryButton } from 'office-ui-fabric-react';
+import * as React from 'react';
+import {
+    AndroidSetupPromptLayout,
+    AndroidSetupPromptLayoutProps,
+} from './android-setup-prompt-layout';
+import { CommonAndroidSetupStepProps } from './android-setup-types';
+
+export const PromptConnectToDeviceStep = NamedFC<CommonAndroidSetupStepProps>(
+    'PromptConnectToDeviceStep',
+    (props: CommonAndroidSetupStepProps) => {
+        const { LinkComponent } = props.deps;
+
+        const onCloseButton = () => {
+            // To be implemented in future feature work
+            console.log(`androidSetupActionCreator.close()`);
+        };
+
+        const onDetectButton = () => {
+            // To be implemented in future feature work
+            console.log(`androidSetupActionCreator.detectDevices()`);
+        };
+
+        const layoutProps: AndroidSetupPromptLayoutProps = {
+            headerText: 'Connect to your Android device',
+            moreInfoLink: (
+                <LinkComponent href="https://aka.ms/accessibility-insights-for-android/connectDevice">
+                    How do I connect to my device?
+                </LinkComponent>
+            ),
+            leftFooterButtonProps: {
+                text: 'Close',
+                onClick: onCloseButton,
+            },
+            rightFooterButtonProps: {
+                text: 'Next',
+                disabled: true,
+                onClick: null,
+            },
+        };
+
+        return (
+            <AndroidSetupPromptLayout {...layoutProps}>
+                <PrimaryButton text="Detect my device" onClick={onDetectButton} />
+            </AndroidSetupPromptLayout>
+        );
+    },
+);
