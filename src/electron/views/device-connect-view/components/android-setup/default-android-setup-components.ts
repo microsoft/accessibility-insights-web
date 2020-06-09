f42062065f44a154f900481c0a7a224edf721d34
JGibson2019 <jacqueline.gibson@microsoft.com>
2020-06-09 16:06:08 -0700

    feat (android-setup): detect-devices and installing-services UI components (#2831)
    * added the new UIs and their test files
    
    * added the new UI to the component provider

--- src/electron/views/device-connect-view/components/android-setup/default-android-setup-components.ts
+++ src/electron/views/device-connect-view/components/android-setup/default-android-setup-components.ts
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 import { AndroidSetupStepComponentProvider } from 'electron/views/device-connect-view/components/android-setup/android-setup-types';
 import { DetectAdbStep } from 'electron/views/device-connect-view/components/android-setup/detect-adb-step';
+import { DetectDevicesStep } from 'electron/views/device-connect-view/components/android-setup/detect-devices-step';
+import { InstallingServiceStep } from 'electron/views/device-connect-view/components/android-setup/installing-service-step';
 import { PromptChooseDeviceStep } from 'electron/views/device-connect-view/components/android-setup/prompt-choose-device-step';
 import { PromptConnectToDeviceStep } from 'electron/views/device-connect-view/components/android-setup/prompt-connect-to-device-step';
 import { PromptLocateAdbStep } from 'electron/views/device-connect-view/components/android-setup/prompt-locate-adb-step';
@@ -11,4 +13,6 @@ export const defaultAndroidSetupComponents: AndroidSetupStepComponentProvider = 
     'prompt-locate-adb': PromptLocateAdbStep,
     'prompt-choose-device': PromptChooseDeviceStep,
     'prompt-connect-to-device': PromptConnectToDeviceStep,
+    'installing-service': InstallingServiceStep,
+    'detect-devices': DetectDevicesStep,
 };
