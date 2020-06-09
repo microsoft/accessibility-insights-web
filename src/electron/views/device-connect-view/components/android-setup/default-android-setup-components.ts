cdadd3cfa8b5a4174b6d364d50bac65c57b9ed4f
JGibson2019 <jacqueline.gibson@utexas.edu>
2020-06-09 14:19:46 -0700

    added the new UI to the component provider

--- src/electron/views/device-connect-view/components/android-setup/default-android-setup-components.ts
+++ src/electron/views/device-connect-view/components/android-setup/default-android-setup-components.ts
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 import { AndroidSetupStepComponentProvider } from 'electron/views/device-connect-view/components/android-setup/android-setup-types';
 import { DetectAdbStep } from 'electron/views/device-connect-view/components/android-setup/detect-adb-step';
+import { DetectDevicesStep } from 'electron/views/device-connect-view/components/android-setup/detect-devices-step';
+import { InstallingServiceStep } from 'electron/views/device-connect-view/components/android-setup/installing-service-step';
 import { PromptConnectToDeviceStep } from 'electron/views/device-connect-view/components/android-setup/prompt-connect-to-device-step';
 import { PromptLocateAdbStep } from 'electron/views/device-connect-view/components/android-setup/prompt-locate-adb-step';
 
@@ -9,4 +11,6 @@ export const defaultAndroidSetupComponents: AndroidSetupStepComponentProvider = 
     'detect-adb': DetectAdbStep,
     'prompt-locate-adb': PromptLocateAdbStep,
     'prompt-connect-to-device': PromptConnectToDeviceStep,
+    'installing-service': InstallingServiceStep,
+    'detect-devices': DetectDevicesStep,
 };
