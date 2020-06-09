4d8493c6175c6c578e1a2cffc6933dd03d8e69f7
JGibson2019 <jacqueline.gibson@utexas.edu>
2020-06-09 12:06:47 -0700

    added the new UIs and their test files

---
+++ src/electron/views/device-connect-view/components/android-setup/detect-devices-step.tsx
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { NamedFC } from 'common/react/named-fc';
+import * as React from 'react';
+import { AndroidSetupSpinnerStep } from './android-setup-spinner-step';
+import { CommonAndroidSetupStepProps } from './android-setup-types';
+
+export const DetectDevicesStep = NamedFC<CommonAndroidSetupStepProps>(
+    'DetectDevicesStep',
+    (props: CommonAndroidSetupStepProps) => {
+        return (
+            <main>
+                <AndroidSetupSpinnerStep deps={props.deps} spinnerLabel="Scanning for devices..." />
+            </main>
+        );
+    },
+);
