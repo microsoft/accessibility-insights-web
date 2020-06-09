d8509ed8fcdccc9b91b49d4fd6eacf69d2f6f077
Karan <karansin@microsoft.com>
2020-06-09 14:32:04 -0700

    feat(android-setup): skeleton UI for prompt-choose-device step (#2828)

--- src/electron/views/device-connect-view/components/android-setup/default-android-setup-components.ts
+++ src/electron/views/device-connect-view/components/android-setup/default-android-setup-components.ts
@@ -2,11 +2,13 @@
 // Licensed under the MIT License.
 import { AndroidSetupStepComponentProvider } from 'electron/views/device-connect-view/components/android-setup/android-setup-types';
 import { DetectAdbStep } from 'electron/views/device-connect-view/components/android-setup/detect-adb-step';
+import { PromptChooseDeviceStep } from 'electron/views/device-connect-view/components/android-setup/prompt-choose-device-step';
 import { PromptConnectToDeviceStep } from 'electron/views/device-connect-view/components/android-setup/prompt-connect-to-device-step';
 import { PromptLocateAdbStep } from 'electron/views/device-connect-view/components/android-setup/prompt-locate-adb-step';
 
 export const defaultAndroidSetupComponents: AndroidSetupStepComponentProvider = {
     'detect-adb': DetectAdbStep,
     'prompt-locate-adb': PromptLocateAdbStep,
+    'prompt-choose-device': PromptChooseDeviceStep,
     'prompt-connect-to-device': PromptConnectToDeviceStep,
 };
