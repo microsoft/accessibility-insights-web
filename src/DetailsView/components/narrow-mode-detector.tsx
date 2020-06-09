804fa0d860a53bf5800f6c9f0ea22231b0d2b042
Liu Haonan <haonliu@microsoft.com>
2020-06-09 11:40:07 -0700

    fix: avoid state update during rendering in narrow mode detector  (#2826)
    * fix component rendering
    
    * fix lint error

--- src/DetailsView/components/narrow-mode-detector.tsx
+++ src/DetailsView/components/narrow-mode-detector.tsx
@@ -1,25 +1,24 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { NamedFC } from 'common/react/named-fc';
+import { ReactFCWithDisplayName } from 'common/react/named-fc';
 import * as React from 'react';
 import ReactResizeDetector from 'react-resize-detector';
 
-export type NarrowModeDetectorProps = {
+export type NarrowModeDetectorProps<P = {}> = {
     isNarrowModeEnabled: boolean;
-    setNarrowMode: (isNarrowMode: boolean) => void;
+    Component: ReactFCWithDisplayName<P & { isNarrowMode: boolean }>;
+    childrenProps: P;
 };
 
 const NARROW_MODE_THRESHOLD_IN_PIXEL = 600;
 
-export const NarrowModeDetector = NamedFC<NarrowModeDetectorProps>('NarrowModeDetector', props => {
-    if (props.isNarrowModeEnabled !== true) {
-        return null;
-    }
-
-    const updateNarrowMode = (dimensions: { width: number }) => {
-        props.setNarrowMode(dimensions.width < NARROW_MODE_THRESHOLD_IN_PIXEL);
-        return <></>;
+export function NarrowModeDetector<P>(props: NarrowModeDetectorProps<P>): JSX.Element {
+    const render = (dimensions: { width: number }) => {
+        const isNarrowMode =
+            props.isNarrowModeEnabled === true && dimensions.width < NARROW_MODE_THRESHOLD_IN_PIXEL;
+        const childrenProps = props.childrenProps;
+        return <props.Component {...childrenProps} isNarrowMode={isNarrowMode} />;
     };
 
-    return <ReactResizeDetector handleWidth querySelector="body" render={updateNarrowMode} />;
-});
+    return <ReactResizeDetector handleWidth querySelector="body" render={render} />;
+}
--- src/tests/unit/tests/DetailsView/components/details-view-content-with-local-state.test.tsx
+++ src/tests/unit/tests/DetailsView/components/details-view-content-with-local-state.test.tsx
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { FeatureFlags } from 'common/feature-flags';
-import { DetailsViewContent } from 'DetailsView/components/details-view-content';
+import { DetailsViewContentProps } from 'DetailsView/components/details-view-content';
 import {
     DetailsViewContentWithLocalState,
     DetailsViewContentWithLocalStateProps,
 } from 'DetailsView/components/details-view-content-with-local-state';
-import { NarrowModeDetector } from 'DetailsView/components/narrow-mode-detector';
+import { NarrowModeDetectorProps } from 'DetailsView/components/narrow-mode-detector';
 import { shallow } from 'enzyme';
 import * as React from 'react';
 
@@ -28,28 +28,14 @@ describe(DetailsViewContentWithLocalState, () => {
             } as any,
         } as DetailsViewContentWithLocalStateProps;
         const wrapper = shallow(<DetailsViewContentWithLocalState {...props} />);
-        const setNavOpen = wrapper.find(DetailsViewContent).props().setSideNavOpen;
+        const setNavOpen = (wrapper.childAt(0).props() as NarrowModeDetectorProps<
+            DetailsViewContentProps
+        >).childrenProps.setSideNavOpen;
 
         expect(wrapper.state('isSideNavOpen')).toBe(false);
 
         setNavOpen(true);
 
         expect(wrapper.state('isSideNavOpen')).toBe(true);
-    });
-
-    test('verify narrow mode state change', () => {
-        const props = {
-            storeState: {
-                featureFlagStoreData: { [FeatureFlags.reflowUI]: true },
-            } as any,
-        } as DetailsViewContentWithLocalStateProps;
-        const wrapper = shallow(<DetailsViewContentWithLocalState {...props} />);
-        const setNarrowMode = wrapper.find(NarrowModeDetector).props().setNarrowMode;
-
-        expect(wrapper.state('isNarrowMode')).toBe(false);
-
-        setNarrowMode(true);
-
-        expect(wrapper.state('isNarrowMode')).toBe(true);
     });
 });
