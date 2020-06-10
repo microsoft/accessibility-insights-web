7d7b523f61cdcb3a4aad5a13765ebe1f8aac9b8e
Dan Bjorge <dan@dbjorge.net>
2020-06-10 13:55:28 -0700

    Add a android-setup-step-props-builder to simplify unit tests

---
+++ src/tests/unit/common/android-setup-step-props-builder.tsx
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { UserConfigurationStoreData } from 'common/types/store-data/user-configuration-store';
+import { AndroidSetupStepId } from 'electron/platform/android/setup/android-setup-step-id';
+import {
+    AndroidSetupPageDeps,
+    CommonAndroidSetupStepProps,
+} from 'electron/views/device-connect-view/components/android-setup/android-setup-types';
+import * as React from 'react';
+import { BaseDataBuilder } from './base-data-builder';
+
+export class AndroidSetupStepPropsBuilder extends BaseDataBuilder<CommonAndroidSetupStepProps> {
+    constructor(stepId: AndroidSetupStepId) {
+        super();
+        this.data = {
+            userConfigurationStoreData: {
+                adbLocation: undefined,
+            } as UserConfigurationStoreData,
+            androidSetupStoreData: {
+                currentStepId: stepId,
+            },
+            deps: {
+                LinkComponent: props => <a {...props} />,
+                androidSetupActionCreator: null,
+                androidSetupStepComponentProvider: null,
+                showOpenFileDialog: null,
+            },
+        };
+    }
+
+    public withDep<P extends keyof AndroidSetupPageDeps>(
+        property: P,
+        value: AndroidSetupPageDeps[P],
+    ): this {
+        this.data.deps[property] = value;
+        return this;
+    }
+}
--- src/tests/unit/tests/electron/views/device-connect-view/components/android-setup/installing-service-step.test.tsx
+++ src/tests/unit/tests/electron/views/device-connect-view/components/android-setup/installing-service-step.test.tsx
@@ -1,22 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { CommonAndroidSetupStepProps } from 'electron/views/device-connect-view/components/android-setup/android-setup-types';
 import { InstallingServiceStep } from 'electron/views/device-connect-view/components/android-setup/installing-service-step';
 import { shallow } from 'enzyme';
 import * as React from 'react';
+import { AndroidSetupStepPropsBuilder } from 'tests/unit/common/android-setup-step-props-builder';
+
 describe('InstallingServiceStep', () => {
-    const props: CommonAndroidSetupStepProps = {
-        userConfigurationStoreData: null,
-        androidSetupStoreData: {
-            currentStepId: 'installing-service',
-        },
-        deps: {
-            androidSetupActionCreator: null,
-            androidSetupStepComponentProvider: null,
-            LinkComponent: null,
-            closeApp: null,
-        },
-    };
+    const props = new AndroidSetupStepPropsBuilder('installing-service').build();
 
     it('renders', () => {
         const rendered = shallow(<InstallingServiceStep {...props} />);
