9226ab36048d1726b272bf06013d1e92ec68a6ac
karan <karansin@microsoft.com>
2020-06-11 15:57:55 -0700

    unit test for next button becoming enabled

--- src/tests/unit/tests/electron/views/device-connect-view/components/android-setup/prompt-choose-device-step.test.tsx
+++ src/tests/unit/tests/electron/views/device-connect-view/components/android-setup/prompt-choose-device-step.test.tsx
@@ -3,7 +3,7 @@
 import { AndroidSetupStepLayout } from 'electron/views/device-connect-view/components/android-setup/android-setup-step-layout';
 import { CommonAndroidSetupStepProps } from 'electron/views/device-connect-view/components/android-setup/android-setup-types';
 import { PromptChooseDeviceStep } from 'electron/views/device-connect-view/components/android-setup/prompt-choose-device-step';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import * as React from 'react';
 import { AndroidSetupStepPropsBuilder } from 'tests/unit/common/android-setup-step-props-builder';
 import { IMock, Mock, Times } from 'typemoq';
@@ -29,5 +29,12 @@ describe('PromptChooseDeviceStep', () => {
         const rendered = shallow(<PromptChooseDeviceStep {...props} />);
         rendered.find(AndroidSetupStepLayout).prop('leftFooterButtonProps').onClick(stubEvent);
         closeAppMock.verify(m => m(), Times.once());
+    });
+
+    it('next button becomes enabled after device is selected', () => {
+        const rendered = mount(<PromptChooseDeviceStep {...props} />);
+        expect(rendered.find('button').at(2).props().disabled).toBeTruthy();
+        rendered.find('DeviceDescription').at(0).simulate('click');
+        expect(rendered.find('button').at(2).props().disabled).toBeUndefined();
     });
 });
