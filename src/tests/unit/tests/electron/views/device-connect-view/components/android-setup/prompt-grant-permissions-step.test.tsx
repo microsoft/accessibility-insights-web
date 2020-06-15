e55993a4d1969af882b2bf49184d84ddbdd67ec3
Karan <karansin@microsoft.com>
2020-06-15 16:55:09 -0700

    feat(android-setup): add actions to prompt-grant-permissions-step (#2880)

--- src/tests/unit/tests/electron/views/device-connect-view/components/android-setup/prompt-grant-permissions-step.test.tsx
+++ src/tests/unit/tests/electron/views/device-connect-view/components/android-setup/prompt-grant-permissions-step.test.tsx
@@ -1,17 +1,26 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { AndroidSetupActionCreator } from 'electron/flux/action-creator/android-setup-action-creator';
 import { DeviceInfo } from 'electron/platform/android/android-service-configurator';
 import { CommonAndroidSetupStepProps } from 'electron/views/device-connect-view/components/android-setup/android-setup-types';
-import { PromptGrantPermissionsStep } from 'electron/views/device-connect-view/components/android-setup/prompt-grant-permissions-step';
+import {
+    PromptGrantPermissionsStep,
+    tryAgainAutomationId,
+} from 'electron/views/device-connect-view/components/android-setup/prompt-grant-permissions-step';
 import { shallow } from 'enzyme';
 import * as React from 'react';
 import { AndroidSetupStepPropsBuilder } from 'tests/unit/common/android-setup-step-props-builder';
+import { IMock, Mock, Times } from 'typemoq';
 
 describe('PromptGrantPermissionsStep', () => {
     let props: CommonAndroidSetupStepProps;
+    let androidSetupActionCreatorMock: IMock<AndroidSetupActionCreator>;
 
     beforeEach(() => {
-        props = new AndroidSetupStepPropsBuilder('prompt-grant-permissions').build();
+        androidSetupActionCreatorMock = Mock.ofType(AndroidSetupActionCreator);
+        props = new AndroidSetupStepPropsBuilder('prompt-grant-permissions')
+            .withDep('androidSetupActionCreator', androidSetupActionCreatorMock.object)
+            .build();
     });
 
     it('renders with device', () => {
@@ -38,5 +47,11 @@ describe('PromptGrantPermissionsStep', () => {
 
         const rendered = shallow(<PromptGrantPermissionsStep {...props} />);
         expect(rendered.getElement()).toMatchSnapshot();
+    });
+
+    it('calls next action when try again button is selected', () => {
+        const rendered = shallow(<PromptGrantPermissionsStep {...props} />);
+        rendered.find({ 'data-automation-id': tryAgainAutomationId }).simulate('click');
+        androidSetupActionCreatorMock.verify(m => m.next(), Times.once());
     });
 });
