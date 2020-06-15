8ec7761ee7725f8887421adf9cec527c2ec95045
karan <karansin@microsoft.com>
2020-06-15 16:15:11 -0700

    add actions

--- src/tests/unit/tests/electron/views/device-connect-view/components/android-setup/prompt-install-failed-step.test.tsx
+++ src/tests/unit/tests/electron/views/device-connect-view/components/android-setup/prompt-install-failed-step.test.tsx
@@ -1,17 +1,26 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { AndroidSetupActionCreator } from 'electron/flux/action-creator/android-setup-action-creator';
 import { DeviceInfo } from 'electron/platform/android/android-service-configurator';
 import { CommonAndroidSetupStepProps } from 'electron/views/device-connect-view/components/android-setup/android-setup-types';
-import { PromptInstallFailedStep } from 'electron/views/device-connect-view/components/android-setup/prompt-install-failed-step';
+import {
+    PromptInstallFailedStep,
+    tryAgainAutomationId,
+} from 'electron/views/device-connect-view/components/android-setup/prompt-install-failed-step';
 import { shallow } from 'enzyme';
 import * as React from 'react';
 import { AndroidSetupStepPropsBuilder } from 'tests/unit/common/android-setup-step-props-builder';
+import { IMock, Mock, Times } from 'typemoq';
 
 describe('PromptInstallFailedStep', () => {
     let props: CommonAndroidSetupStepProps;
+    let androidSetupActionCreatorMock: IMock<AndroidSetupActionCreator>;
 
     beforeEach(() => {
-        props = new AndroidSetupStepPropsBuilder('prompt-install-failed').build();
+        androidSetupActionCreatorMock = Mock.ofType(AndroidSetupActionCreator);
+        props = new AndroidSetupStepPropsBuilder('prompt-install-failed')
+            .withDep('androidSetupActionCreator', androidSetupActionCreatorMock.object)
+            .build();
     });
 
     it('renders with device', () => {
@@ -38,5 +47,12 @@ describe('PromptInstallFailedStep', () => {
 
         const rendered = shallow(<PromptInstallFailedStep {...props} />);
         expect(rendered.getElement()).toMatchSnapshot();
+    });
+
+    it('routes try again button to next action', () => {
+        const rendered = shallow(<PromptInstallFailedStep {...props} />);
+        const button = rendered.find({ 'data-automation-id': tryAgainAutomationId });
+        button.simulate('click');
+        androidSetupActionCreatorMock.verify(m => m.next(), Times.once());
     });
 });
