f25edfd348d68678ba8e66869a0b48a63b79fcb6
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2020-06-22 13:03:34 -0700

    fix(android-setup): Enable "choose device" dialog on cancel, use real data (#2932)

--- src/tests/unit/tests/electron/views/device-connect-view/components/android-setup/prompt-choose-device-step.test.tsx
+++ src/tests/unit/tests/electron/views/device-connect-view/components/android-setup/prompt-choose-device-step.test.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { AndroidSetupActionCreator } from 'electron/flux/action-creator/android-setup-action-creator';
+import { AndroidSetupStoreData } from 'electron/flux/types/android-setup-store-data';
 import { AndroidSetupStepLayout } from 'electron/views/device-connect-view/components/android-setup/android-setup-step-layout';
 import { CommonAndroidSetupStepProps } from 'electron/views/device-connect-view/components/android-setup/android-setup-types';
 import { PromptChooseDeviceStep } from 'electron/views/device-connect-view/components/android-setup/prompt-choose-device-step';
@@ -21,6 +22,26 @@ describe('PromptChooseDeviceStep', () => {
             .withDep('closeApp', closeAppMock.object)
             .withDep('androidSetupActionCreator', actionMessageCreatorMock.object)
             .build();
+
+        props.androidSetupStoreData = {
+            availableDevices: [
+                {
+                    id: '1',
+                    friendlyName: 'Phone 1',
+                    isEmulator: true,
+                },
+                {
+                    id: '2',
+                    friendlyName: 'Phone 2',
+                    isEmulator: false,
+                },
+                {
+                    id: '3',
+                    friendlyName: 'Phone 3',
+                    isEmulator: true,
+                },
+            ],
+        } as AndroidSetupStoreData;
     });
 
     it('renders per snapshot', () => {
