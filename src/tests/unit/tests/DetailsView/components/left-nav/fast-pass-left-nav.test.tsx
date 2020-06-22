46ec90edfa7cbb2048dbf733ea5382317a83d9ab
Jules Perkins <t-julper@microsoft.com>
2020-06-22 19:58:11 -0400

    Removed unused imports

--- src/tests/unit/tests/DetailsView/components/left-nav/fast-pass-left-nav.test.tsx
+++ src/tests/unit/tests/DetailsView/components/left-nav/fast-pass-left-nav.test.tsx
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { FeatureFlags } from 'common/feature-flags';
-import { FeatureFlagStoreData } from 'common/types/store-data/feature-flag-store-data';
 import { shallow } from 'enzyme';
 import * as React from 'react';
 import {
