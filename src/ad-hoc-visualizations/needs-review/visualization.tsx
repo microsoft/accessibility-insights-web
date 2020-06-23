4d1c33e97a3e20d3ec15c9dd4d62fba017e6b5ae
Jules Perkins <t-julper@microsoft.com>
2020-06-22 20:04:36 -0400

    Added license phrase

--- src/ad-hoc-visualizations/needs-review/visualization.tsx
+++ src/ad-hoc-visualizations/needs-review/visualization.tsx
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 import { AdHocTestkeys } from 'common/configs/adhoc-test-keys';
 import { TestMode } from 'common/configs/test-mode';
 import { VisualizationConfiguration } from 'common/configs/visualization-configuration';
