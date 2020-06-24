333359edc94021d4600342cf94edb1314ac776fa
Dan Bjorge <dan@dbjorge.net>
2020-06-24 14:07:58 -0700

    Copyright

--- tools/strict-null-checks/config.js
+++ tools/strict-null-checks/config.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 module.exports = {
     repoRoot: `${__dirname}/../..`,
     targetTsconfig: 'tsconfig.strictNullChecks.json',
