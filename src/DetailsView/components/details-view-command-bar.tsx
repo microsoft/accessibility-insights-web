a49aff0754461039d46914c4f2f06f492095fc9a
Kelsey Pownell <kepownel@microsoft.com>
2020-06-29 09:30:23 -0700

    collapse command bar buttons at 960 pixels

--- src/DetailsView/components/details-view-command-bar.tsx
+++ src/DetailsView/components/details-view-command-bar.tsx
@@ -14,6 +14,8 @@ import * as React from 'react';
 import { ReportGenerator } from 'reports/report-generator';
 
 import { ScanMetadata } from 'common/types/store-data/unified-data-interface';
+import { CommandBarButtonsMenu } from 'DetailsView/components/command-bar-buttons-menu';
+import { NarrowModeStatus } from 'DetailsView/components/narrow-mode-detector';
 import { StartOverFactoryProps } from 'DetailsView/components/start-over-component-factory';
 import { AssessmentStoreData } from '../../common/types/store-data/assessment-result-data';
 import { FeatureFlagStoreData } from '../../common/types/store-data/feature-flag-store-data';
@@ -48,6 +50,7 @@ export interface DetailsViewCommandBarProps {
     cardsViewData: CardsViewModel;
     switcherNavConfiguration: DetailsViewSwitcherNavConfiguration;
     scanMetadata: ScanMetadata;
+    narrowModeStatus: NarrowModeStatus;
 }
 
 export class DetailsViewCommandBar extends React.Component<DetailsViewCommandBarProps> {
@@ -59,7 +62,7 @@ export class DetailsViewCommandBar extends React.Component<DetailsViewCommandBar
         return (
             <div className={styles.detailsViewCommandBar}>
                 {this.renderTargetPageInfo()}
-                {this.renderCommandButtons()}
+                {this.renderFarItems()}
             </div>
         );
     }
@@ -87,6 +90,14 @@ export class DetailsViewCommandBar extends React.Component<DetailsViewCommandBar
         );
     }
 
+    private renderFarItems(): JSX.Element {
+        if (this.props.narrowModeStatus.isCommandBarCollapsed) {
+            return this.renderCommandButtonsMenu();
+        } else {
+            return this.renderCommandButtons();
+        }
+    }
+
     private renderCommandButtons(): JSX.Element {
         const reportExportElement: JSX.Element = this.renderExportComponent();
         const startOverElement: JSX.Element = this.renderStartOverComponent();
@@ -101,6 +112,10 @@ export class DetailsViewCommandBar extends React.Component<DetailsViewCommandBar
         }
 
         return null;
+    }
+
+    private renderCommandButtonsMenu(): JSX.Element {
+        return <CommandBarButtonsMenu {...this.props} />;
     }
 
     private renderExportComponent(): JSX.Element {
