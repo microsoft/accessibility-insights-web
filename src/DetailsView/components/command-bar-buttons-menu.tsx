6f9664345d92778453ffb4f8f584544ee2fa2a1b
pownkel <55459788+pownkel@users.noreply.github.com>
2020-06-29 12:47:50 -0700

    feat(command-bar-reflow): create CommandBarButtonsMenu component (#2982)
    * create CommandBarButtonsMenu component

---
+++ src/DetailsView/components/command-bar-buttons-menu.tsx
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { NamedFC } from 'common/react/named-fc';
+import { CommandBarProps } from 'DetailsView/components/details-view-command-bar';
+import { CommandBarButton, IOverflowSetItemProps, OverflowSet } from 'office-ui-fabric-react';
+import * as React from 'react';
+
+export type CommandBarButtonsMenuProps = CommandBarProps;
+
+export const CommandBarButtonsMenu = NamedFC<CommandBarButtonsMenuProps>(
+    'CommandBarButtonsMenu',
+    props => {
+        const onRenderItem = (item: IOverflowSetItemProps): JSX.Element => {
+            return item.onRender(props);
+        };
+
+        const onRenderOverflowButton = (overflow: IOverflowSetItemProps[]): JSX.Element => {
+            return (
+                <CommandBarButton
+                    ariaLabel="More items"
+                    role="menuitem"
+                    menuIconProps={{ iconName: 'More' }}
+                    menuProps={{ items: overflow }}
+                />
+            );
+        };
+
+        const overflowItems = [
+            {
+                key: 'export report',
+                onRender: () => props.switcherNavConfiguration.ReportExportComponentFactory(props),
+            },
+            {
+                key: 'start over',
+                onRender: () => props.switcherNavConfiguration.StartOverComponentFactory(props),
+            },
+        ];
+
+        return (
+            <OverflowSet
+                onRenderItem={onRenderItem}
+                overflowItems={overflowItems}
+                onRenderOverflowButton={onRenderOverflowButton}
+            />
+        );
+    },
+);
---
+++ src/tests/unit/tests/DetailsView/components/command-bar-buttons-menu.test.tsx
@@ -0,0 +1,82 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import {
+    CommandBarButtonsMenu,
+    CommandBarButtonsMenuProps,
+} from 'DetailsView/components/command-bar-buttons-menu';
+import { DetailsViewSwitcherNavConfiguration } from 'DetailsView/components/details-view-switcher-nav';
+import { shallow } from 'enzyme';
+import { IOverflowSetItemProps } from 'office-ui-fabric-react';
+import * as React from 'react';
+import { IMock, Mock, Times } from 'typemoq';
+import {
+    DetailsViewCommandBarProps,
+    // tslint:disable-next-line: ordered-imports
+    ReportExportComponentFactory,
+    StartOverComponentFactory,
+} from '../../../../../DetailsView/components/details-view-command-bar';
+
+describe('CommandBarButtonsMenu', () => {
+    let reportExportComponentFactory: IMock<ReportExportComponentFactory>;
+    let startOverComponentFactory: IMock<StartOverComponentFactory>;
+    let commandBarButtonsMenuProps: CommandBarButtonsMenuProps;
+
+    beforeEach(() => {
+        reportExportComponentFactory = Mock.ofType<ReportExportComponentFactory>();
+        startOverComponentFactory = Mock.ofType<StartOverComponentFactory>();
+        commandBarButtonsMenuProps = {
+            switcherNavConfiguration: {
+                ReportExportComponentFactory: reportExportComponentFactory.object,
+                StartOverComponentFactory: startOverComponentFactory.object,
+            } as DetailsViewSwitcherNavConfiguration,
+        } as DetailsViewCommandBarProps;
+    });
+
+    it('renders CommandBarButtonsMenu', () => {
+        const wrapper = shallow(<CommandBarButtonsMenu {...commandBarButtonsMenuProps} />);
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+
+    it('renders child buttons', () => {
+        reportExportComponentFactory
+            .setup(r => r(commandBarButtonsMenuProps))
+            .returns(() => <></>)
+            .verifiable(Times.once());
+        startOverComponentFactory
+            .setup(s => s(commandBarButtonsMenuProps))
+            .returns(() => <></>)
+            .verifiable(Times.once());
+
+        const wrapper = shallow(<CommandBarButtonsMenu {...commandBarButtonsMenuProps} />);
+        const renderedProps = wrapper.getElement().props;
+        const overflowItems: IOverflowSetItemProps[] = renderedProps.overflowItems;
+        const onRenderItem: (props: IOverflowSetItemProps) => JSX.Element =
+            renderedProps.onRenderItem;
+        expect(overflowItems).toBeDefined();
+        expect(overflowItems).toHaveLength(2);
+        expect(onRenderItem).toBeDefined();
+
+        overflowItems.forEach(item => {
+            onRenderItem(item);
+        });
+
+        reportExportComponentFactory.verifyAll();
+        startOverComponentFactory.verifyAll();
+    });
+
+    it('renders overflow button', () => {
+        const overflowItems: IOverflowSetItemProps[] = [
+            {
+                key: 'item 1',
+            },
+            {
+                key: 'item 2',
+            },
+        ];
+        const wrapper = shallow(<CommandBarButtonsMenu {...commandBarButtonsMenuProps} />);
+        const onRenderOverflowButton = wrapper.getElement().props.onRenderOverflowButton;
+        expect(onRenderOverflowButton).toBeDefined();
+
+        expect(onRenderOverflowButton(overflowItems)).toMatchSnapshot();
+    });
+});
