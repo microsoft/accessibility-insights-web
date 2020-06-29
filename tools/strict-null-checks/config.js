74792d329f8d16d081c3c44b1e2024d91ce5730c
Dan Bjorge <danielbj@microsoft.com>
2020-06-29 15:25:25 -0700

    chore(strict-null): add strict null check migration tools to repo (#2957)
    #### Description of changes
    
    This integrates the strict null checking tools that I migrated from VS Code's as an experiment in https://github.com/dbjorge/aiweb-strict-null-check-migration-tools into the `/tools/` folder of our repo, to make it easier for others to use them.
    
    As part of the migration, I eliminated the separate `package.json` machinery for the tools and had them just re-use the same `node_modules` as the main project (in particular, re-using the correct typescript version). I also added entry point run-scripts for the tools to our root-level package.json, so the source for the tools should be an implementation detail for most purposes of actually running null check migration work; usage is just `yarn null:find` and `yarn null:autoadd` from the root, to match the previously-added `yarn null:check`.
    
    I've updated #2869 with instructions for how to use these scripts to contribute to the strict null check migration work.
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a" in the checkbox -->
    - [x] Addresses an existing issue: tools to help address #2869 
    - [x] Ran `yarn fastpass`
    - [n/a] Added/updated relevant unit test(s) (and ran `yarn test`)
    - [n/a] Verified code coverage for the changes made. Check coverage report at: `<rootDir>/test-results/unit/coverage`
    - [x] PR title *AND* final merge commit title both start with a semantic tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`). See `CONTRIBUTING.md`.
    - [n/a] (UI changes only) Added screenshots/GIFs to description above
    - [n/a] (UI changes only) Verified usability with NVDA/JAWS

---
+++ tools/strict-null-checks/config.js
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+module.exports = {
+    repoRoot: `${__dirname}/../..`,
+    targetTsconfig: 'tsconfig.strictNullChecks.json',
+    skippedFiles: new Set([]),
+};
