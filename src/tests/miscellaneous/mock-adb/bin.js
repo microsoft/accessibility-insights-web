c44c7cc29284ab2f28ae0e2aa3c47f0016ea2ab4
Dan Bjorge <dan@dbjorge.net>
2020-06-29 18:09:06 -0700

    Implement basic single device config

--- src/tests/miscellaneous/mock-adb/bin.js
+++ src/tests/miscellaneous/mock-adb/bin.js
@@ -5,13 +5,18 @@ const fs = require('fs');
 const path = require('path');
 const process = require('process');
 
-const defaultConfigPath = path.join(__dirname, 'mock_adb_config.json');
+const defaultConfigPath = path.join(path.dirname(process.execPath), 'mock_adb_config.json');
 const configPath = process.env['MOCK_ADB_CONFIG'] || defaultConfigPath;
 
 const configContent = fs.readFileSync(configPath);
 const config = JSON.parse(configContent);
 
-const inputCommand = process.argv.slice(2).join(' ');
+let ignoredPrefixArgs = 2; // node.exe bin.js
+if (process.argv[2] === '-P') {
+    ignoredPrefixArgs += 2; // -P 5037
+}
+
+const inputCommand = process.argv.slice(ignoredPrefixArgs).join(' ');
 if (config[inputCommand] === undefined) {
     console.error(`unrecognized command: ${inputCommand}`);
     process.exit(1);
--- src/tests/miscellaneous/mock-adb/common-adb-configs.ts
+++ src/tests/miscellaneous/mock-adb/common-adb-configs.ts
@@ -1,8 +1,25 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { apkVersionName } from 'accessibility-insights-for-android-service-bin';
 import { MockAdbConfig } from './mock-adb';
 
 export const singleDeviceConfig: MockAdbConfig = {
-    devices: { stdout: 'List of devices attached\ndevice-id-1      device' },
-    // etc
+    'start-server': {},
+    devices: {
+        stdout: 'List of devices attached\ndevice-1\tdevice',
+    },
+    '-s device-1 shell getprop ro.product.model': {
+        stdout: 'mock-adb device 1',
+    },
+    '-s device-1 shell dumpsys package com.microsoft.accessibilityinsightsforandroidservice': {
+        stdout: `versionName=${apkVersionName}\nversionCode=12345`,
+    },
+    '-s device-1 shell dumpsys accessibility': {
+        stdout: 'label=Accessibility Insights',
+    },
+    '-s device-1 shell dumpsys media_projection': {
+        stdout: 'com.microsoft.accessibilityinsightsforandroidservice',
+    },
+    // TODO after implementing test port support with portfinder:
+    // [`-s device-1 forward tcp:${TEST_PORT} tcp:62442`]: {},
 };
