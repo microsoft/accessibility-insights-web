3971843131ae90d865add45009d6a2d5611547dc
karan <karansin@microsoft.com>
2020-07-01 15:03:03 -0700

    more logging

--- src/common/logging/default-logger.ts
+++ src/common/logging/default-logger.ts
@@ -1,10 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import * as fs from 'fs';
 import { Logger } from './logger';
 
 export const createDefaultLogger = (): Logger => {
     return {
-        log: console.log,
-        error: console.error,
+        log: (message, ...params) =>
+            fs.writeFileSync('drop/mock-adb/logs.txt', message + JSON.stringify(params), {
+                flag: 'a',
+            }),
+        error: (message, ...params) =>
+            fs.writeFileSync('drop/mock-adb/logs.txt', message + JSON.stringify(params), {
+                flag: 'a',
+            }),
     };
 };
