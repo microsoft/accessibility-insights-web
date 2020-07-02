0708bfaa97c5b27bf63ef2171c81422c5e7d3839
Dan Bjorge <dan@dbjorge.net>
2020-07-01 17:00:14 -0700

    Implement a ready message to avoid adb forward exiting before server is initialized

--- src/tests/miscellaneous/mock-adb/app/port-forward-server.js
+++ src/tests/miscellaneous/mock-adb/app/port-forward-server.js
@@ -4,30 +4,82 @@ const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const process = require('process');
+const { startMockService } = require('../../mock-service-for-android/mock-service');
 
 // This is helpful for debugging, but causes us to not be able to exit before "detached" child
 // process (the "correct" behavior), so we leave it off except when debugging mock-adb itself
 const LOG_DETACHED_PROCESS = false;
 
 const portForwardSentinelArgument = '__PORT_FORWARD_SERVER';
+const portForwardProcessReadyMessage = 'PORT_FORWARD_PROCESS_READY';
+const portForwardProcessStartTimeoutMs = 10000;
 
 function portFile(port, fileExtension) {
     return path.join(path.dirname(process.execPath), `test-server-${port}.${fileExtension}`);
 }
 
-function startDetachedPortForwardServer(port, path) {
-    const testServerProcess = child_process.spawn(
-        process.argv[0],
-        [process.argv[1], portForwardSentinelArgument, port, path],
+// Returns whether argv indicated it *should* be handled as a port server
+async function tryHandleAsPortForwardServer(argv) {
+    if (argv[2] !== portForwardSentinelArgument) {
+        return false;
+    }
+
+    const port = parseInt(process.argv[3], 10);
+    const path = process.argv[4];
+
+    await startMockService(port, path);
+
+    process.send(portForwardProcessReadyMessage);
+    return true;
+}
+
+async function waitForReadyMessage(testServerProcess) {
+    return new Promise((resolve, reject) => {
+        let canceled = false;
+
+        const onMessage = message => {
+            if (message === portForwardProcessReadyMessage) {
+                testServerProcess.off('message', onMessage);
+                clearTimeout(timeoutId);
+                resolve();
+            }
+        };
+
+        const onTimeout = () => {
+            canceled = true;
+            testServerProcess.off('message', onMessage);
+            reject(new Error('Timeout: child port-forwarding server never sent ready message'));
+        };
+
+        const timeoutId = setTimeout(onTimeout, portForwardProcessStartTimeoutMs);
+        testServerProcess.on('message', onMessage);
+    });
+}
+
+async function startDetachedPortForwardServer(port, path) {
+    const testServerProcess = child_process.fork(
+        process.argv[1],
+        [portForwardSentinelArgument, port, path],
         {
             detached: true, // avoid having test server exit when adb exits
-            stdio: LOG_DETACHED_PROCESS ? ['ignore', 'pipe', 'pipe'] : 'ignore',
+            stdio: LOG_DETACHED_PROCESS
+                ? ['ignore', 'pipe', 'pipe', 'ipc']
+                : ['ignore', 'ignore', 'ignore', 'ipc'],
         },
     );
     if (LOG_DETACHED_PROCESS) {
         testServerProcess.stderr.pipe(fs.createWriteStream(portFile(port, 'err'), { flags: 'a' }));
         testServerProcess.stdout.pipe(fs.createWriteStream(portFile(port, 'out'), { flags: 'a' }));
     }
+
+    try {
+        await waitForReadyMessage(testServerProcess);
+    } catch (e) {
+        testServerProcess.kill();
+        throw e;
+    }
+
+    testServerProcess.disconnect();
     testServerProcess.unref(); // avoid having adb wait for test server before exiting
 
     const pidFile = portFile(port, 'pid');
@@ -59,7 +111,7 @@ function stopDetachedPortForwardServer(port) {
 }
 
 module.exports = {
-    portForwardSentinelArgument,
     startDetachedPortForwardServer,
     stopDetachedPortForwardServer,
+    tryHandleAsPortForwardServer,
 };
