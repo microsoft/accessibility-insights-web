67b02a059929bf2e77a5d7d73ec18d2e5eb07a91
Kelsey Pownell <kepownel@microsoft.com>
2020-07-07 15:49:44 -0700

    Revert "send telemetry when button is clicked"
    This reverts commit 8082f38a44ee86214768ab1c0008bdc3e10f9435.

--- src/tests/unit/tests/common/components/left-nav-hamburger-button.test.tsx
+++ src/tests/unit/tests/common/components/left-nav-hamburger-button.test.tsx
@@ -4,89 +4,41 @@ import {
     LeftNavHamburgerButton,
     LeftNavHamburgerButtonProps,
 } from 'common/components/left-nav-hamburger-button';
-import { DetailsViewActionMessageCreator } from 'DetailsView/actions/details-view-action-message-creator';
 import { shallow } from 'enzyme';
 import { IconButton } from 'office-ui-fabric-react';
 import * as React from 'react';
-import { IMock, Mock, Times } from 'typemoq';
+import { Mock, Times } from 'typemoq';
 
 describe('LeftNavHamburgerButton', () => {
-    const ariaLabel: string = 'test-aria-label';
-    const eventStub = {} as React.MouseEvent;
-
-    let setSideNavOpenMock: IMock<(isOpen: boolean) => void>;
-    let detailsViewActionMessageCreatorMock: IMock<DetailsViewActionMessageCreator>;
-
-    beforeEach(() => {
-        setSideNavOpenMock = Mock.ofInstance((isOpen: boolean) => {});
-        detailsViewActionMessageCreatorMock = Mock.ofType<DetailsViewActionMessageCreator>();
-    });
-
     it('renders per snapshot', () => {
+        const ariaLabel: string = 'test-aria-label';
         const wrapper = shallow(
             <LeftNavHamburgerButton
                 isSideNavOpen={false}
                 setSideNavOpen={null}
                 ariaLabel={ariaLabel}
-                deps={{
-                    detailsViewActionMessageCreator: detailsViewActionMessageCreatorMock.object,
-                }}
             />,
         );
 
         expect(wrapper.getElement()).toMatchSnapshot();
     });
 
-    test.each([true, false])(
-        'sets side nav state to correct value when isSideNavOpen=%o',
-        isSideNavOpen => {
-            setSideNavOpenMock.setup(sm => sm(!isSideNavOpen)).verifiable(Times.once());
-            const props = getProps(isSideNavOpen);
-
-            testButtonClick(props);
-
-            setSideNavOpenMock.verifyAll();
-        },
-    );
-
-    it('sends telemetry when opening side nav', () => {
+    it('sets side nav state with correct value', () => {
+        const ariaLabel: string = 'test-aria-label';
+        const setSideNavOpenMock = Mock.ofInstance((isOpen: boolean) => {});
         const isSideNavOpen = false;
-        detailsViewActionMessageCreatorMock
-            .setup(d => d.leftNavPanelExpanded(eventStub))
-            .verifiable(Times.once());
-        const props = getProps(isSideNavOpen);
-
-        testButtonClick(props);
-
-        detailsViewActionMessageCreatorMock.verifyAll();
-    });
-
-    it('does not send telemetry when closing side nav', () => {
-        const isSideNavOpen = true;
-        detailsViewActionMessageCreatorMock
-            .setup(d => d.leftNavPanelExpanded(eventStub))
-            .verifiable(Times.never());
-        const props = getProps(isSideNavOpen);
-
-        testButtonClick(props);
-
-        detailsViewActionMessageCreatorMock.verifyAll();
-    });
-
-    function getProps(isSideNavOpen: boolean): LeftNavHamburgerButtonProps {
-        return {
+        setSideNavOpenMock.setup(sm => sm(!isSideNavOpen)).verifiable(Times.once());
+        const props: LeftNavHamburgerButtonProps = {
             ariaLabel,
             isSideNavOpen,
             setSideNavOpen: setSideNavOpenMock.object,
-            deps: {
-                detailsViewActionMessageCreator: detailsViewActionMessageCreatorMock.object,
-            },
         } as LeftNavHamburgerButtonProps;
-    }
 
-    function testButtonClick(props: LeftNavHamburgerButtonProps): void {
         const wrapper = shallow(<LeftNavHamburgerButton {...props} />);
+
         const button = wrapper.find(IconButton);
-        button.simulate('click', eventStub);
-    }
+        button.simulate('click');
+
+        setSideNavOpenMock.verifyAll();
+    });
 });
