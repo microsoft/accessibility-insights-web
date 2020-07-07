8082f38a44ee86214768ab1c0008bdc3e10f9435
Kelsey Pownell <kepownel@microsoft.com>
2020-07-07 13:59:44 -0700

    send telemetry when button is clicked

--- src/common/components/left-nav-hamburger-button.tsx
+++ src/common/components/left-nav-hamburger-button.tsx
@@ -1,12 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { NamedFC } from 'common/react/named-fc';
+import { DetailsViewActionMessageCreator } from 'DetailsView/actions/details-view-action-message-creator';
 import { IconButton } from 'office-ui-fabric-react';
 import * as React from 'react';
-
 import * as styles from './left-nav-hamburger-button.scss';
 
+export type LeftNavHamburgerButtonDeps = {
+    detailsViewActionMessageCreator: DetailsViewActionMessageCreator;
+};
+
 export type LeftNavHamburgerButtonProps = {
+    deps: LeftNavHamburgerButtonDeps;
     ariaLabel: string;
     isSideNavOpen: boolean;
     setSideNavOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -15,8 +20,12 @@ export type LeftNavHamburgerButtonProps = {
 export const LeftNavHamburgerButton = NamedFC<LeftNavHamburgerButtonProps>(
     'LeftNavHamburgerButton',
     props => {
-        const onClick = () => {
-            props.setSideNavOpen(!props.isSideNavOpen);
+        const onClick = (event: React.MouseEvent<any>) => {
+            const openSideNav = !props.isSideNavOpen;
+            props.setSideNavOpen(openSideNav);
+            if (openSideNav) {
+                props.deps.detailsViewActionMessageCreator.leftNavPanelExpanded(event);
+            }
         };
 
         return (
--- src/tests/unit/tests/common/components/left-nav-hamburger-button.test.tsx
+++ src/tests/unit/tests/common/components/left-nav-hamburger-button.test.tsx
@@ -4,41 +4,89 @@ import {
     LeftNavHamburgerButton,
     LeftNavHamburgerButtonProps,
 } from 'common/components/left-nav-hamburger-button';
+import { DetailsViewActionMessageCreator } from 'DetailsView/actions/details-view-action-message-creator';
 import { shallow } from 'enzyme';
 import { IconButton } from 'office-ui-fabric-react';
 import * as React from 'react';
-import { Mock, Times } from 'typemoq';
+import { IMock, Mock, Times } from 'typemoq';
 
 describe('LeftNavHamburgerButton', () => {
+    const ariaLabel: string = 'test-aria-label';
+    const eventStub = {} as React.MouseEvent;
+
+    let setSideNavOpenMock: IMock<(isOpen: boolean) => void>;
+    let detailsViewActionMessageCreatorMock: IMock<DetailsViewActionMessageCreator>;
+
+    beforeEach(() => {
+        setSideNavOpenMock = Mock.ofInstance((isOpen: boolean) => {});
+        detailsViewActionMessageCreatorMock = Mock.ofType<DetailsViewActionMessageCreator>();
+    });
+
     it('renders per snapshot', () => {
-        const ariaLabel: string = 'test-aria-label';
         const wrapper = shallow(
             <LeftNavHamburgerButton
                 isSideNavOpen={false}
                 setSideNavOpen={null}
                 ariaLabel={ariaLabel}
+                deps={{
+                    detailsViewActionMessageCreator: detailsViewActionMessageCreatorMock.object,
+                }}
             />,
         );
 
         expect(wrapper.getElement()).toMatchSnapshot();
     });
 
-    it('sets side nav state with correct value', () => {
-        const ariaLabel: string = 'test-aria-label';
-        const setSideNavOpenMock = Mock.ofInstance((isOpen: boolean) => {});
+    test.each([true, false])(
+        'sets side nav state to correct value when isSideNavOpen=%o',
+        isSideNavOpen => {
+            setSideNavOpenMock.setup(sm => sm(!isSideNavOpen)).verifiable(Times.once());
+            const props = getProps(isSideNavOpen);
+
+            testButtonClick(props);
+
+            setSideNavOpenMock.verifyAll();
+        },
+    );
+
+    it('sends telemetry when opening side nav', () => {
         const isSideNavOpen = false;
-        setSideNavOpenMock.setup(sm => sm(!isSideNavOpen)).verifiable(Times.once());
-        const props: LeftNavHamburgerButtonProps = {
+        detailsViewActionMessageCreatorMock
+            .setup(d => d.leftNavPanelExpanded(eventStub))
+            .verifiable(Times.once());
+        const props = getProps(isSideNavOpen);
+
+        testButtonClick(props);
+
+        detailsViewActionMessageCreatorMock.verifyAll();
+    });
+
+    it('does not send telemetry when closing side nav', () => {
+        const isSideNavOpen = true;
+        detailsViewActionMessageCreatorMock
+            .setup(d => d.leftNavPanelExpanded(eventStub))
+            .verifiable(Times.never());
+        const props = getProps(isSideNavOpen);
+
+        testButtonClick(props);
+
+        detailsViewActionMessageCreatorMock.verifyAll();
+    });
+
+    function getProps(isSideNavOpen: boolean): LeftNavHamburgerButtonProps {
+        return {
             ariaLabel,
             isSideNavOpen,
             setSideNavOpen: setSideNavOpenMock.object,
+            deps: {
+                detailsViewActionMessageCreator: detailsViewActionMessageCreatorMock.object,
+            },
         } as LeftNavHamburgerButtonProps;
+    }
 
+    function testButtonClick(props: LeftNavHamburgerButtonProps): void {
         const wrapper = shallow(<LeftNavHamburgerButton {...props} />);
-
         const button = wrapper.find(IconButton);
-        button.simulate('click');
-
-        setSideNavOpenMock.verifyAll();
-    });
+        button.simulate('click', eventStub);
+    }
 });
