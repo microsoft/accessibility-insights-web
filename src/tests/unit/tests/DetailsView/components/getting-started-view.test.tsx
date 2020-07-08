9758cf65fe4b4dc04b00c29c4da19265aa0628d6
Wahaj Abid <waabid@microsoft.com>
2020-07-08 15:54:21 -0700

    updated tests

--- src/tests/unit/tests/DetailsView/components/getting-started-view.test.tsx
+++ src/tests/unit/tests/DetailsView/components/getting-started-view.test.tsx
@@ -14,22 +14,27 @@ import { Mock } from 'typemoq';
 import { ContentPageComponent } from 'views/content/content-page';
 
 describe('GettingStartedViewTest', () => {
-    it('renders with content from props', () => {
-        const messageCreatorMock = Mock.ofType(DetailsViewActionMessageCreator);
-        const eventStub = {} as React.MouseEvent<HTMLElement>;
-        const props: GettingStartedViewProps = {
-            deps: {
-                detailsViewActionMessageCreator: messageCreatorMock.object,
-            } as GettingStartedViewDeps,
-            gettingStartedContent: <div>test-getting-started-content</div>,
-            title: 'some title',
-            guidance: { pageTitle: 'some page title' } as ContentPageComponent,
-            nextRequirement: {
-                key: 'some requirement key',
-            } as Requirement,
-            currentTest: -1,
-        };
+    const messageCreatorMock = Mock.ofType(DetailsViewActionMessageCreator);
+    const eventStub = {} as React.MouseEvent<HTMLElement>;
+    const props: GettingStartedViewProps = {
+        deps: {
+            detailsViewActionMessageCreator: messageCreatorMock.object,
+        } as GettingStartedViewDeps,
+        gettingStartedContent: <div>test-getting-started-content</div>,
+        title: 'some title',
+        guidance: { pageTitle: 'some page title' } as ContentPageComponent,
+        nextRequirement: {
+            key: 'some requirement key',
+        } as Requirement,
+        currentTest: -1,
+    };
 
+    it('renders with content from props', () => {
+        const rendered = shallow(<GettingStartedView {...props} />);
+        expect(rendered.getElement()).toMatchSnapshot();
+    });
+
+    it('validate next requirement button', () => {
         messageCreatorMock
             .setup(mock =>
                 mock.selectRequirement(eventStub, props.nextRequirement.key, props.currentTest),
@@ -38,8 +43,6 @@ describe('GettingStartedViewTest', () => {
 
         const rendered = shallow(<GettingStartedView {...props} />);
         rendered.find(DefaultButton).prop('onClick')(eventStub);
-
-        expect(rendered.getElement()).toMatchSnapshot();
         messageCreatorMock.verifyAll();
     });
 });
