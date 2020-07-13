dce05bd330c6ab5c6915b57eb8fef9983d6def27
Rob Gallo <43080011+RobGallo@users.noreply.github.com>
2020-07-13 17:58:36 -0400

    Chore: Add null checks (#3053)

--- src/electron/common/get-electron-icon-path.ts
+++ src/electron/common/get-electron-icon-path.ts
@@ -4,8 +4,12 @@ import { ConfigAccessor } from 'common/configuration/configuration-types';
 import { OSType } from 'electron/window-management/platform-info';
 import * as path from 'path';
 
-export function getElectronIconPath(config: ConfigAccessor, os: OSType): string {
+export function getElectronIconPath(config: ConfigAccessor, os: OSType): string | undefined {
     const baseIconPath = config.getOption('electronIconBaseName');
+    if (baseIconPath === undefined) {
+        return undefined;
+    }
+
     const iconBaseName = path.join(__dirname, '..', baseIconPath);
     const iconExtension = os === OSType.Windows ? 'ico' : os === OSType.Mac ? 'icns' : 'png';
     return `${iconBaseName}.${iconExtension}`;
