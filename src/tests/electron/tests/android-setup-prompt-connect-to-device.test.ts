693534590948f1a98ccfa71a6fc9aae78c4182b4
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2020-07-14 17:06:54 -0700

    PR feedback: Use existing values

--- src/tests/electron/tests/android-setup-prompt-connect-to-device.test.ts
+++ src/tests/electron/tests/android-setup-prompt-connect-to-device.test.ts
@@ -4,6 +4,7 @@ import {
     leftFooterButtonAutomationId,
     rightFooterButtonAutomationId,
 } from 'electron/views/device-connect-view/components/android-setup/android-setup-step-layout';
+import { detectDeviceAutomationId } from 'electron/views/device-connect-view/components/android-setup/prompt-connect-to-device-step';
 import { getAutomationIdSelector } from 'tests/common/get-automation-id-selector';
 import { createApplication } from 'tests/electron/common/create-application';
 import { scanForAccessibilityIssues } from 'tests/electron/common/scan-for-accessibility-issues';
@@ -17,7 +18,6 @@ import {
 
 describe('Android setup - prompt-connect-to-device ', () => {
     const defaultDeviceConfig = commonAdbConfigs['multiple-devices'];
-    const detectButtonId = 'detect-device';
     let app: AppController;
     let dialog: AndroidSetupViewController;
 
@@ -37,12 +37,14 @@ describe('Android setup - prompt-connect-to-device ', () => {
         const [closeId, nextId] = [leftFooterButtonAutomationId, rightFooterButtonAutomationId];
         expect(await dialog.isEnabled(getAutomationIdSelector(closeId))).toBe(true);
         expect(await dialog.isEnabled(getAutomationIdSelector(nextId))).toBe(false);
-        expect(await dialog.isEnabled(getAutomationIdSelector(detectButtonId))).toBe(true);
+        expect(await dialog.isEnabled(getAutomationIdSelector(detectDeviceAutomationId))).toBe(
+            true,
+        );
     });
 
     it('detect button triggers new detection', async () => {
         await setupMockAdb(defaultDeviceConfig);
-        await dialog.client.click(getAutomationIdSelector(detectButtonId));
+        await dialog.client.click(getAutomationIdSelector(detectDeviceAutomationId));
         await dialog.waitForDialogVisible('prompt-choose-device');
     });
 
