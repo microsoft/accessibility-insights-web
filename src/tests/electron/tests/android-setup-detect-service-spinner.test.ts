961260f6528251bd34c71a637ffeec5bbb39976b
karan <karansin@microsoft.com>
2020-07-20 16:50:34 -0700

    add a11y test for detect spinner

---
+++ src/tests/electron/tests/android-setup-detect-service-spinner.test.ts
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { createApplication } from 'tests/electron/common/create-application';
+import { scanForAccessibilityIssuesInAllModes } from 'tests/electron/common/scan-for-accessibility-issues';
+import { AppController } from 'tests/electron/common/view-controllers/app-controller';
+import {
+    commonAdbConfigs,
+    delayAllCommands,
+    setupMockAdb,
+    simulateServiceNotInstalled,
+} from '../../miscellaneous/mock-adb/setup-mock-adb';
+
+describe('Android setup - detect-service', () => {
+    const defaultDeviceConfig = commonAdbConfigs['single-device'];
+    let app: AppController;
+
+    beforeEach(async () => {
+        await setupMockAdb(
+            delayAllCommands(3000, simulateServiceNotInstalled(defaultDeviceConfig)),
+        );
+        app = await createApplication({ suppressFirstTimeDialog: true });
+    });
+
+    afterEach(async () => {
+        if (app != null) {
+            await app.stop();
+        }
+    });
+
+    it('should pass accessibility validation in both contrast modes', async () => {
+        await scanForAccessibilityIssuesInAllModes(app);
+    });
+});
