1cdec64214a0e743410c8ee5f18e93ffb066f966
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2020-07-20 16:13:20 -0700

    feat(android-setup-e2e): Add tests for prompt-install-failed

---
+++ src/tests/electron/tests/android-setup-prompt-install-failed.test.ts
@@ -0,0 +1,74 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import {
+    leftFooterButtonAutomationId,
+    rightFooterButtonAutomationId,
+} from 'electron/views/device-connect-view/components/android-setup/android-setup-step-layout';
+import { tryAgainAutomationId } from 'electron/views/device-connect-view/components/android-setup/prompt-configuring-port-forwarding-failed-step';
+import { installAutomationId } from 'electron/views/device-connect-view/components/android-setup/prompt-install-service-step';
+import { getAutomationIdSelector } from 'tests/common/get-automation-id-selector';
+import { createApplication } from 'tests/electron/common/create-application';
+import { scanForAccessibilityIssuesInAllModes } from 'tests/electron/common/scan-for-accessibility-issues';
+import { AndroidSetupViewController } from 'tests/electron/common/view-controllers/android-setup-view-controller';
+import { AppController } from 'tests/electron/common/view-controllers/app-controller';
+import {
+    commonAdbConfigs,
+    delayAllCommands,
+    setupMockAdb,
+    simulateServiceInstallationError,
+    simulateServiceNotInstalled,
+} from '../../miscellaneous/mock-adb/setup-mock-adb';
+
+describe('Android setup - prompt-install-service-failed ', () => {
+    const defaultDeviceConfig = commonAdbConfigs['single-device'];
+    let app: AppController;
+    let dialog: AndroidSetupViewController;
+
+    beforeEach(async () => {
+        await setupMockAdb(simulateServiceNotInstalled(defaultDeviceConfig));
+        app = await createApplication({ suppressFirstTimeDialog: true });
+        dialog = await app.openAndroidSetupView('prompt-install-service');
+        await setupMockAdb(simulateServiceInstallationError(defaultDeviceConfig));
+        await dialog.click(getAutomationIdSelector(installAutomationId));
+        await dialog.waitForDialogVisible('prompt-install-failed');
+    });
+
+    afterEach(async () => {
+        if (app != null) {
+            await app.stop();
+        }
+    });
+
+    it('initial component state is correct', async () => {
+        const [closeId, nextId] = [leftFooterButtonAutomationId, rightFooterButtonAutomationId];
+        expect(await dialog.isEnabled(getAutomationIdSelector(closeId))).toBe(true);
+        expect(await dialog.isEnabled(getAutomationIdSelector(nextId))).toBe(false);
+        expect(await dialog.isEnabled(getAutomationIdSelector(tryAgainAutomationId))).toBe(true);
+    });
+
+    it('try again button triggers installation, prompts for permission on success', async () => {
+        await setupMockAdb(defaultDeviceConfig);
+        await dialog.client.click(getAutomationIdSelector(tryAgainAutomationId));
+        await dialog.waitForDialogVisible('prompt-grant-permissions');
+    });
+
+    it('install button triggers installation, prompts correctly on failure', async () => {
+        await setupMockAdb(simulateServiceInstallationError(defaultDeviceConfig));
+        await dialog.client.click(getAutomationIdSelector(tryAgainAutomationId));
+        await dialog.waitForDialogVisible('prompt-install-failed');
+    });
+
+    it('should pass accessibility validation in all contrast modes', async () => {
+        await scanForAccessibilityIssuesInAllModes(app);
+    });
+
+    it('installing service spinner should pass accessibility validation in all contrast modes', async () => {
+        await setupMockAdb(
+            delayAllCommands(3000, simulateServiceInstallationError(defaultDeviceConfig)),
+        );
+        await dialog.client.click(getAutomationIdSelector(tryAgainAutomationId));
+        await dialog.waitForDialogVisible('installing-service');
+        await scanForAccessibilityIssuesInAllModes(app);
+        await dialog.waitForDialogVisible('prompt-install-failed'); // Let mock-adb finish
+    });
+});
