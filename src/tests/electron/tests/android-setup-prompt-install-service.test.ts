457263046053575ef63fe50ac11f9da1cf9460a1
karan <karansin@microsoft.com>
2020-07-20 16:09:32 -0700

    undo spinner change

--- src/tests/electron/tests/android-setup-prompt-install-service.test.ts
+++ src/tests/electron/tests/android-setup-prompt-install-service.test.ts
@@ -12,7 +12,6 @@ import { AndroidSetupViewController } from 'tests/electron/common/view-controlle
 import { AppController } from 'tests/electron/common/view-controllers/app-controller';
 import {
     commonAdbConfigs,
-    delayAllCommands,
     setupMockAdb,
     simulateServiceInstallationError,
     simulateServiceNotInstalled,
@@ -24,11 +23,9 @@ describe('Android setup - prompt-install-service ', () => {
     let dialog: AndroidSetupViewController;
 
     beforeEach(async () => {
-        await setupMockAdb(
-            delayAllCommands(2500, simulateServiceNotInstalled(defaultDeviceConfig)),
-        );
+        await setupMockAdb(simulateServiceNotInstalled(defaultDeviceConfig));
         app = await createApplication({ suppressFirstTimeDialog: true });
-        dialog = await app.openAndroidSetupView('detect-service');
+        dialog = await app.openAndroidSetupView('prompt-install-service');
     });
 
     afterEach(async () => {
@@ -38,7 +35,6 @@ describe('Android setup - prompt-install-service ', () => {
     });
 
     it('initial component state is correct', async () => {
-        await dialog.waitForDialogVisible('prompt-install-service');
         const [closeId, nextId] = [leftFooterButtonAutomationId, rightFooterButtonAutomationId];
         expect(await dialog.isEnabled(getAutomationIdSelector(closeId))).toBe(true);
         expect(await dialog.isEnabled(getAutomationIdSelector(nextId))).toBe(false);
@@ -46,22 +42,18 @@ describe('Android setup - prompt-install-service ', () => {
     });
 
     it('install button triggers installation, prompts for permission on success', async () => {
-        await dialog.waitForDialogVisible('prompt-install-service');
         await setupMockAdb(defaultDeviceConfig);
         await dialog.client.click(getAutomationIdSelector(installAutomationId));
         await dialog.waitForDialogVisible('prompt-grant-permissions');
     });
 
     it('install button triggers installation, prompts correctly on failure', async () => {
-        await dialog.waitForDialogVisible('prompt-install-service');
         await setupMockAdb(simulateServiceInstallationError(defaultDeviceConfig));
         await dialog.client.click(getAutomationIdSelector(installAutomationId));
         await dialog.waitForDialogVisible('prompt-install-failed');
     });
 
-    it('spinner & prompt dialogs pass accessibility validation in both contrast modes', async () => {
-        await scanForAccessibilityIssuesInAllModes(app);
-        await dialog.waitForDialogVisible('prompt-install-service');
+    it('should pass accessibility validation in both contrast modes', async () => {
         await scanForAccessibilityIssuesInAllModes(app);
     });
 });
