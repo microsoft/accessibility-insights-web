14e5493551b7b58bd5f407052a77b7522293e340
karan <karansin@microsoft.com>
2020-07-21 16:54:28 -0700

    undo purposeful failure

--- src/tests/electron/tests/android-setup-detect-service-spinner.test.ts
+++ src/tests/electron/tests/android-setup-detect-service-spinner.test.ts
@@ -32,7 +32,6 @@ describe(`Android setup - ${description}`, () => {
     });
 
     it('should pass accessibility validation in both contrast modes', async () => {
-        expect(true).toBeFalsy(); // on purpose to trigger logs
         await scanForAccessibilityIssuesInAllModes(app);
     });
 });
