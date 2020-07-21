a45a7b30b26ea9b88c8219cb6767c947e8047914
RobGallo <43080011+RobGallo@users.noreply.github.com>
2020-07-21 14:30:45 -0400

    Add null checks
    - device-connect-connected-device.tsx
    	- Use exhaustiveness checking with Typescript to ensure all cases in the new switch statement are handled
    - client-utils.ts
    	- The matcher cannot be bound to an undefined value

--- src/electron/views/device-connect-view/components/device-connect-connected-device.tsx
+++ src/electron/views/device-connect-view/components/device-connect-connected-device.tsx
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { assertNever } from 'common/components/assert-never';
 import { NamedFC } from 'common/react/named-fc';
 import { Icon, Spinner, SpinnerSize } from 'office-ui-fabric-react';
 import * as React from 'react';
@@ -14,40 +15,44 @@ export interface DeviceConnectConnectedDeviceProps {
 export const DeviceConnectConnectedDevice = NamedFC<DeviceConnectConnectedDeviceProps>(
     'DeviceConnectConnectedDevice',
     (props: DeviceConnectConnectedDeviceProps) => {
-        const renderContents = (): JSX.Element => {
-            if (props.deviceConnectState === DeviceConnectState.Connecting) {
-                return (
-                    <Spinner
-                        className={styles.deviceConnectSpinner}
-                        labelPosition="right"
-                        size={SpinnerSize.xSmall}
-                        label="Connecting to mobile device"
-                    />
-                );
-            }
-
-            if (props.deviceConnectState === DeviceConnectState.Error) {
-                return (
-                    <>
-                        <Icon
-                            iconName="statusErrorFull"
-                            className={styles.connectionErrorIcon}
-                            ariaLabel="Connection failed"
-                        ></Icon>
-                        <span className={styles.scannedText}>
-                            No active applications were found at the provided local host.
-                        </span>
-                    </>
-                );
-            }
-
-            if (props.deviceConnectState === DeviceConnectState.Default) {
-                return null;
-            }
-
-            if (props.connectedDevice) {
-                return <span className={styles.scannedText}>{props.connectedDevice}</span>;
-            }
+        const renderContents = (): JSX.Element | null => {
+            switch (props.deviceConnectState) {
+                case DeviceConnectState.Error: {
+                    return (
+                        <>
+                            <Icon
+                                iconName="statusErrorFull"
+                                className={styles.connectionErrorIcon}
+                                ariaLabel="Connection failed"
+                            ></Icon>
+                            <span className={styles.scannedText}>
+                                No active applications were found at the provided local host.
+                            </span>
+                        </>
+                    );
+                }
+                case DeviceConnectState.Connecting: {
+                    return (
+                        <Spinner
+                            className={styles.deviceConnectSpinner}
+                            labelPosition="right"
+                            size={SpinnerSize.xSmall}
+                            label="Connecting to mobile device"
+                        />
+                    );
+                }
+                case DeviceConnectState.Connected: {
+                    return props.connectedDevice ? (
+                        <span className={styles.scannedText}>{props.connectedDevice}</span>
+                    ) : null;
+                }
+                case DeviceConnectState.Default: {
+                    return null;
+                }
+                default: {
+                    return assertNever(props.deviceConnectState);
+                }
+            } // switch
         };
 
         return (
