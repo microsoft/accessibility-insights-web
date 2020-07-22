2a78b7ebe0554ee762424263903cee6f808d9218
DaveTryon <DaveTryon@users.noreply.github.com>
2020-07-22 07:17:55 -0700

    Fix a reference that I overlooked

--- src/tests/electron/common/view-controllers/android-setup-view-controller.ts
+++ src/tests/electron/common/view-controllers/android-setup-view-controller.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { AndroidSetupStepId } from 'electron/platform/android/setup/android-setup-step-id';
-import { rightFooterButtonAutomationId } from 'electron/views/device-connect-view/components/android-setup/android-setup-step-layout';
+import { rightFooterButtonAutomationId } from 'electron/views/device-connect-view/components/automation-ids';
 import { getAutomationIdSelector } from 'tests/common/get-automation-id-selector';
 import { SpectronAsyncClient } from 'tests/electron/common/view-controllers/spectron-async-client';
 import { ViewController } from './view-controller';
