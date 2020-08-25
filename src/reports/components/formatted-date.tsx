aa54a798a302be3bd841aec7af3bda5eb730e537
RobGallo <43080011+RobGallo@users.noreply.github.com>
2020-08-25 17:52:20 -0400

    Fix: Change the date format of the FormattedDate component to be ISO 8601 proper
    This is an SDL compliance global readiness requirement. The requirement is specifically that the date/time format must either be localized or universal.

--- src/reports/components/formatted-date.tsx
+++ src/reports/components/formatted-date.tsx
@@ -1,13 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { padStart } from 'lodash';
+import * as moment from 'moment';
 import * as React from 'react';
 
 export interface FormattedDateProps {
     date: Date;
 }
-
-type timeRange = 'AM' | 'PM';
 
 export class FormattedDate extends React.Component<FormattedDateProps> {
     public render(): JSX.Element {
@@ -15,47 +13,8 @@ export class FormattedDate extends React.Component<FormattedDateProps> {
     }
 
     private formatDateTime(date: Date): string {
-        return (
-            date.getFullYear() +
-            '-' +
-            this.padStartWithZero(date.getMonth() + 1, 2) +
-            '-' +
-            this.padStartWithZero(date.getDate(), 2) +
-            ' ' +
-            this.getTwelveHour(date.getHours()) +
-            ':' +
-            this.padStartWithZero(date.getMinutes(), 2) +
-            ' ' +
-            this.getAmPm(date.getHours()) +
-            ' ' +
-            this.getTimeZone(date)
-        );
-    }
-
-    private getTwelveHour(hour: number): number {
-        hour = hour % 12;
-        if (hour === 0) {
-            return 12;
-        }
-        return hour;
-    }
-
-    private getAmPm(hour: number): timeRange {
-        if (hour < 12) {
-            return 'AM';
-        } else {
-            return 'PM';
-        }
-    }
-
-    private getTimeZone(date: Date): string {
-        const timeString = date
-            .toLocaleTimeString(undefined, { timeZoneName: 'short' })
-            .replace(/\u200E/g, '');
-        return timeString.substr(timeString.lastIndexOf(' ') + 1);
-    }
-
-    private padStartWithZero(num: number, digits: number): string {
-        return padStart(num.toString(), digits, '0');
+        const keepTimeZoneOffset = true;
+        const momentDate = moment(this.props.date);
+        return momentDate.toISOString(keepTimeZoneOffset);
     }
 }
--- src/tests/unit/tests/reports/components/formatted-date.test.tsx
+++ src/tests/unit/tests/reports/components/formatted-date.test.tsx
@@ -5,11 +5,11 @@ import { FormattedDate, FormattedDateProps } from 'reports/components/formatted-
 describe('FormattedDate', () => {
     describe('render', () => {
         test('end of last millennium', () => {
-            testDate(new Date(1999, 11, 31, 23, 59, 59), '1999-12-31 11:59 PM FTZ');
+            testDate(new Date(1999, 11, 31, 23, 59, 59), '1999-12-31T23:59:59.000-05:00');
         });
 
         test('start of this millennium', () => {
-            testDate(new Date(2000, 0, 1, 0, 0, 0), '2000-01-01 12:00 AM FTZ');
+            testDate(new Date(2000, 0, 1, 0, 0, 0), '2000-01-01T00:00:00.000-05:00');
         });
 
         function testDate(date: Date, expected: string): void {
