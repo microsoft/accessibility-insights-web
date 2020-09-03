261e4aede76afc5362af30e4dcd9b160b88a7739
Kelsey Pownell <kepownel@microsoft.com>
2020-09-03 15:49:31 -0700

    make ReportSectionFactory accept generic SectionProps type

--- src/reports/components/report-sections/report-body.tsx
+++ src/reports/components/report-sections/report-body.tsx
@@ -1,49 +1,56 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { NamedFC } from 'common/react/named-fc';
 import * as React from 'react';
 
 import { ReportSectionFactory, SectionProps } from './report-section-factory';
 
-export type ReportBodyProps = {
-    sectionFactory: ReportBodySectionFactory;
-} & SectionProps;
+export type ReportBodyProps<SectionPropsType = SectionProps> = {
+    sectionFactory: ReportBodySectionFactory<SectionPropsType>;
+} & SectionPropsType;
 
-export type ReportBodySectionFactory = Omit<ReportSectionFactory, 'HeadSection'>;
+export type ReportBodySectionFactory<SectionPropsType = SectionProps> = Omit<
+    ReportSectionFactory<SectionPropsType>,
+    'HeadSection'
+>;
 
-export const ReportBody = NamedFC<ReportBodyProps>('ReportBody', props => {
-    const { sectionFactory, ...sectionProps } = props;
-    const {
-        BodySection,
-        ContentContainer,
-        HeaderSection,
-        TitleSection,
-        SummarySection,
-        DetailsSection,
-        ResultsContainer,
-        FailedInstancesSection,
-        PassedChecksSection,
-        NotApplicableChecksSection,
-        FooterSection,
-        FooterText,
-    } = sectionFactory;
+export class ReportBody<SectionPropsType = SectionProps> extends React.Component<
+    ReportBodyProps<SectionPropsType>
+> {
+    public render(): JSX.Element {
+        const sectionFactory = this.props.sectionFactory;
+        const sectionProps: SectionPropsType = this.props;
+        const {
+            BodySection,
+            ContentContainer,
+            HeaderSection,
+            TitleSection,
+            SummarySection,
+            DetailsSection,
+            ResultsContainer,
+            FailedInstancesSection,
+            PassedChecksSection,
+            NotApplicableChecksSection,
+            FooterSection,
+            FooterText,
+        } = sectionFactory;
 
-    return (
-        <BodySection>
-            <HeaderSection {...sectionProps} />
-            <ContentContainer>
-                <TitleSection />
-                <SummarySection {...sectionProps} />
-                <DetailsSection {...sectionProps} />
-                <ResultsContainer {...sectionProps}>
-                    <FailedInstancesSection {...sectionProps} />
-                    <PassedChecksSection {...sectionProps} />
-                    <NotApplicableChecksSection {...sectionProps} />
-                </ResultsContainer>
-            </ContentContainer>
-            <FooterSection>
-                <FooterText {...sectionProps} />
-            </FooterSection>
-        </BodySection>
-    );
-});
+        return (
+            <BodySection>
+                <HeaderSection {...sectionProps} />
+                <ContentContainer>
+                    <TitleSection />
+                    <SummarySection {...sectionProps} />
+                    <DetailsSection {...sectionProps} />
+                    <ResultsContainer {...sectionProps}>
+                        <FailedInstancesSection {...sectionProps} />
+                        <PassedChecksSection {...sectionProps} />
+                        <NotApplicableChecksSection {...sectionProps} />
+                    </ResultsContainer>
+                </ContentContainer>
+                <FooterSection>
+                    <FooterText {...sectionProps} />
+                </FooterSection>
+            </BodySection>
+        );
+    }
+}
