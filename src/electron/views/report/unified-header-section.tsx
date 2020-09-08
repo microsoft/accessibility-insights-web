bb5b902a6708adaa60a8743b80c96c939a840703
Kelsey Pownell <kepownel@microsoft.com>
2020-09-08 16:46:41 -0700

    use HeaderBar in unified report

--- src/electron/views/report/unified-header-section.tsx
+++ src/electron/views/report/unified-header-section.tsx
@@ -2,17 +2,13 @@
 // Licensed under the MIT License.
 import { NamedFC } from 'common/react/named-fc';
 import { androidAppTitle } from 'content/strings/application';
-import { BrandWhite } from 'icons/brand/white/brand-white';
 import * as React from 'react';
-import * as styles from 'reports/components/report-sections/header-section.scss';
+import { HeaderBar } from 'reports/components/header-bar';
 
 export const UnifiedHeaderSection = NamedFC('UnifiedHeaderSection', () => {
     return (
         <header>
-            <div className={styles.reportHeaderBar}>
-                <BrandWhite />
-                <div className={styles.headerText}>{androidAppTitle}</div>
-            </div>
+            <HeaderBar headerText={androidAppTitle} />
         </header>
     );
 });
