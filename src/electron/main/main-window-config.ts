9e1495e0bcebb289fbe3e793ae9c572e23d9e65f
Dan Bjorge <dan@dbjorge.net>
2020-09-16 15:17:36 -0400

    Reduce minimum window size to make debugging easier

--- src/electron/main/main-window-config.ts
+++ src/electron/main/main-window-config.ts
@@ -1,8 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+const largestSupportedZoomFactor = 4; // 400%
+const smallestSupportedScreenWidth = 1280;
+const smallestSupportedScreenHeight = 1024;
+
 export const mainWindowConfig = {
     defaultWidth: 600,
     defaultHeight: 391,
-    minWidth: 400,
-    minHeight: 300,
+    minWidth: smallestSupportedScreenWidth / largestSupportedZoomFactor,
+    minHeight: smallestSupportedScreenHeight / largestSupportedZoomFactor,
 };
