9741e08c004b45aa81452f7bcf935e124a3cbb26
Wahaj <32555133+waabid@users.noreply.github.com>
2020-09-18 15:17:04 -0700

    feat(cli-summary-report): fix up some formatting/styling for report (#3374)

--- src/reports/components/summary-report-head.tsx
+++ src/reports/components/summary-report-head.tsx
@@ -1,9 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { NamedFC } from 'common/react/named-fc';
-import { title } from 'content/strings/application';
+import { brand } from 'content/strings/application';
 import * as React from 'react';
-
 import * as bundledStyles from '../../DetailsView/bundled-details-view-styles';
 import * as summaryReportStyles from '../summary-report.styles';
 
@@ -12,7 +11,7 @@ export const SummaryReportHead = NamedFC('SummaryReportHead', () => {
     return (
         <head>
             <meta charSet="UTF-8" />
-            <title>{title} automated checks result</title>
+            <title>{brand} automated checks result</title>
             <style dangerouslySetInnerHTML={{ __html: summaryReportStyles.styleSheet }} />
             <style dangerouslySetInnerHTML={{ __html: bundledStyles.styleSheet }} />
         </head>
