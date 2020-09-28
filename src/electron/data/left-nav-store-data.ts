ba508949c499e1a3f8e9bad21fd51716ee28f973
RobGallo <43080011+RobGallo@users.noreply.github.com>
2020-09-28 18:39:11 -0400

    feat(android-left-nav): Remove generic key parameter from left nav types
    The genericity won't be necessary because it only adds complexity to have different stores and actions based on a key type. Instead, we will have one store that handles all the left nav actions and data, and only specific left nav items will be instantiated based on the need at the time.

--- src/electron/data/left-nav-store-data.ts
+++ src/electron/data/left-nav-store-data.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-export type LeftNavStoreData<KeyT> = {
-    selectedKey: KeyT;
+import { LeftNavItemKey } from 'electron/types/left-nav-item-key';
+
+export type LeftNavStoreData = {
+    selectedKey: LeftNavItemKey;
 };
