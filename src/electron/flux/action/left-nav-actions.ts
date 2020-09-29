29afbaa4a99abdf2216a1238caed4e728d60e29a
RobGallo <43080011+RobGallo@users.noreply.github.com>
2020-09-29 13:45:46 -0400

    feat(android-left-nav): Add left nav actions
    - Create `LeftNavActionsz class
    - Create `LeftNavActionsCreator` class
    - Remove the `defaultKey` from the `LeftNavStore` constructor because it can be set in the `initialize` function
    - implement `LeftNavStore.addActionListeners`
    - Move `LeftNavStore` and `LeftNavStoreData` alongside similar store and data classes under the flux directory
    
    Next change will be to create the left nav store in render-initializer.

---
+++ src/electron/flux/action/left-nav-actions.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { Action } from 'common/flux/action';
+import { LeftNavItemKey } from 'electron/types/left-nav-item-key';
+
+export class LeftNavActions {
+    public readonly itemSelected = new Action<LeftNavItemKey>();
+}
