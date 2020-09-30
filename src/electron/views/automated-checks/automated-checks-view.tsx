41b530d07721fd6b88bc37d117e0c6fb6a1ca303
Wahaj Abid <waabid@microsoft.com>
2020-09-29 17:21:07 -0700

    feat(android-left-nav): re-factored automated checks view

--- src/electron/views/automated-checks/automated-checks-view.tsx
+++ src/electron/views/automated-checks/automated-checks-view.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { ScanningSpinner } from 'common/components/scanning-spinner/scanning-spinner';
 import { GetCardSelectionViewData } from 'common/get-card-selection-view-data';
 import { IsResultHighlightUnavailable } from 'common/is-result-highlight-unavailable';
 import { GetCardViewData } from 'common/rule-based-view-model-provider';
@@ -13,26 +12,25 @@ import {
     UnifiedScanResultStoreData,
 } from 'common/types/store-data/unified-data-interface';
 import { UserConfigurationStoreData } from 'common/types/store-data/user-configuration-store';
-import { CardsView, CardsViewDeps } from 'DetailsView/components/cards-view';
+import { CardsViewDeps } from 'DetailsView/components/cards-view';
 import {
     SettingsPanel,
     SettingsPanelDeps,
 } from 'DetailsView/components/details-view-overlay/settings-panel/settings-panel';
 import { ScanActionCreator } from 'electron/flux/action-creator/scan-action-creator';
 import { WindowStateActionCreator } from 'electron/flux/action-creator/window-state-action-creator';
+import { AndroidSetupStoreData } from 'electron/flux/types/android-setup-store-data';
 import { ScanStatus } from 'electron/flux/types/scan-status';
 import { ScanStoreData } from 'electron/flux/types/scan-store-data';
 import { WindowStateStoreData } from 'electron/flux/types/window-state-store-data';
 import { TitleBar, TitleBarDeps } from 'electron/views/automated-checks/components/title-bar';
+import { TestView } from 'electron/views/automated-checks/test-view';
 import { DeviceDisconnectedPopup } from 'electron/views/device-disconnected-popup/device-disconnected-popup';
 import { ScreenshotView } from 'electron/views/screenshot/screenshot-view';
 import { ScreenshotViewModelProvider } from 'electron/views/screenshot/screenshot-view-model-provider';
 import * as React from 'react';
-
-import { AndroidSetupStoreData } from 'electron/flux/types/android-setup-store-data';
 import * as styles from './automated-checks-view.scss';
 import { CommandBar, CommandBarDeps } from './components/command-bar';
-import { HeaderSection } from './components/header-section';
 
 export const automatedChecksViewAutomationId = 'automated-checks-view';
 
@@ -67,49 +65,59 @@ export class AutomatedChecksView extends React.Component<AutomatedChecksViewProp
 
     public render(): JSX.Element {
         const { status } = this.props.scanStoreData;
-        if (status === ScanStatus.Failed) {
-            return this.renderLayout(null, null, this.renderDeviceDisconnected());
-        } else if (status === ScanStatus.Completed) {
-            const { unifiedScanResultStoreData, cardSelectionStoreData, deps } = this.props;
-            const { rules, results, toolInfo } = unifiedScanResultStoreData;
-            const cardSelectionViewData = deps.getCardSelectionViewData(
-                cardSelectionStoreData,
-                unifiedScanResultStoreData,
-                deps.isResultHighlightUnavailable,
-            );
-            const cardsViewData = deps.getCardsViewData(rules, results, cardSelectionViewData);
-            deps.toolData = toolInfo;
-            const highlightedResultUids = Object.keys(
-                cardSelectionViewData.resultsHighlightStatus,
-            ).filter(uid => cardSelectionViewData.resultsHighlightStatus[uid] === 'visible');
-            const screenshotViewModel = deps.screenshotViewModelProvider(
-                unifiedScanResultStoreData,
-                highlightedResultUids,
-            );
+        const {
+            unifiedScanResultStoreData,
+            cardSelectionStoreData,
+            deps,
+            userConfigurationStoreData,
+        } = this.props;
+        const { rules, results, toolInfo } = unifiedScanResultStoreData;
+        const cardSelectionViewData = deps.getCardSelectionViewData(
+            cardSelectionStoreData,
+            unifiedScanResultStoreData,
+            deps.isResultHighlightUnavailable,
+        );
+        const cardsViewData = deps.getCardsViewData(rules, results, cardSelectionViewData);
+        deps.toolData = toolInfo;
 
-            const scanMetadata: ScanMetadata = {
-                timestamp: unifiedScanResultStoreData.timestamp,
-                toolData: unifiedScanResultStoreData.toolInfo,
-                targetAppInfo: this.props.unifiedScanResultStoreData.targetAppInfo,
-                deviceName: this.props.unifiedScanResultStoreData.platformInfo.deviceName,
-            };
+        const highlightedResultUids = Object.keys(
+            cardSelectionViewData.resultsHighlightStatus,
+        ).filter(uid => cardSelectionViewData.resultsHighlightStatus[uid] === 'visible');
 
-            return this.renderLayout(
-                cardsViewData,
-                scanMetadata,
-                this.renderResults(cardsViewData, scanMetadata),
-                <ScreenshotView viewModel={screenshotViewModel} />,
-            );
-        } else {
-            return this.renderLayout(null, null, this.renderScanningSpinner());
-        }
+        const screenshotViewModel = deps.screenshotViewModelProvider(
+            unifiedScanResultStoreData,
+            highlightedResultUids,
+        );
+
+        const scanMetadata: ScanMetadata =
+            status !== ScanStatus.Completed
+                ? null
+                : {
+                      timestamp: unifiedScanResultStoreData.timestamp,
+                      toolData: unifiedScanResultStoreData.toolInfo,
+                      targetAppInfo: unifiedScanResultStoreData.targetAppInfo,
+                      deviceName: unifiedScanResultStoreData.platformInfo.deviceName,
+                  };
+
+        return this.renderLayout(
+            cardsViewData,
+            scanMetadata,
+            <TestView
+                deps={deps}
+                scanStatus={status}
+                scanMetadata={scanMetadata}
+                userConfigurationStoreData={userConfigurationStoreData}
+                cardsViewData={cardsViewData}
+            />,
+            <ScreenshotView viewModel={screenshotViewModel} />,
+        );
     }
 
     private renderLayout(
         cardsViewData: CardsViewModel,
         scanMetadata: ScanMetadata,
-        primaryContent: JSX.Element,
-        optionalSidePanel?: JSX.Element,
+        mainContent: JSX.Element,
+        screenshotPanel: JSX.Element,
     ): JSX.Element {
         return (
             <div
@@ -131,12 +139,10 @@ export class AutomatedChecksView extends React.Component<AutomatedChecksViewProp
                             cardsViewData={cardsViewData}
                             scanMetadata={scanMetadata}
                         />
-                        <main>
-                            <HeaderSection />
-                            {primaryContent}
-                        </main>
+                        <main>{mainContent}</main>
                     </div>
-                    {optionalSidePanel}
+                    {screenshotPanel}
+                    {this.renderDeviceDisconnected()}
                 </div>
                 <SettingsPanel
                     layerClassName={styles.settingsPanelLayerHost}
@@ -158,6 +164,10 @@ export class AutomatedChecksView extends React.Component<AutomatedChecksViewProp
     }
 
     private renderDeviceDisconnected(): JSX.Element {
+        if (this.props.scanStoreData.status !== ScanStatus.Failed) {
+            return;
+        }
+
         return (
             <DeviceDisconnectedPopup
                 deviceName={this.getConnectedDeviceName()}
@@ -167,27 +177,6 @@ export class AutomatedChecksView extends React.Component<AutomatedChecksViewProp
                     })
                 }
                 onRescanDevice={() => this.props.deps.scanActionCreator.scan(this.getScanPort())}
-            />
-        );
-    }
-
-    private renderScanningSpinner(): JSX.Element {
-        return (
-            <ScanningSpinner
-                isSpinning={this.props.scanStoreData.status === ScanStatus.Scanning}
-                label="Scanning..."
-                aria-live="assertive"
-            />
-        );
-    }
-
-    private renderResults(cardsViewData: CardsViewModel, scanMetadata: ScanMetadata): JSX.Element {
-        return (
-            <CardsView
-                deps={this.props.deps}
-                scanMetadata={scanMetadata}
-                userConfigurationStoreData={this.props.userConfigurationStoreData}
-                cardsViewData={cardsViewData}
             />
         );
     }
---
+++ src/tests/unit/tests/electron/views/automated-checks/test-view.test.tsx
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { CardsViewModel } from 'common/types/store-data/card-view-model';
+import { ScanMetadata } from 'common/types/store-data/unified-data-interface';
+import { UserConfigurationStoreData } from 'common/types/store-data/user-configuration-store';
+import { ScanStatus } from 'electron/flux/types/scan-status';
+import { TestView, TestViewDeps, TestViewProps } from 'electron/views/automated-checks/test-view';
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+describe('TestView', () => {
+    let props: TestViewProps;
+    let scanMetadataStub: ScanMetadata;
+    let cardsViewDataStub: CardsViewModel;
+    let userConfigurationStoreDataStub: UserConfigurationStoreData;
+
+    beforeEach(() => {
+        scanMetadataStub = {
+            timestamp: 'some time',
+        } as ScanMetadata;
+        cardsViewDataStub = {
+            visualHelperEnabled: true,
+        } as CardsViewModel;
+        userConfigurationStoreDataStub = {
+            isFirstTime: false,
+        } as UserConfigurationStoreData;
+    });
+
+    const scanStatuses = [
+        undefined,
+        ScanStatus[ScanStatus.Scanning],
+        ScanStatus[ScanStatus.Failed],
+        ScanStatus[ScanStatus.Completed],
+    ];
+
+    it.each(scanStatuses)('when status scan <%s>', scanStatusName => {
+        props = {
+            deps: {} as TestViewDeps,
+            scanMetadata: scanMetadataStub,
+            userConfigurationStoreData: userConfigurationStoreDataStub,
+            cardsViewData: cardsViewDataStub,
+            scanStatus: ScanStatus[scanStatusName],
+        };
+
+        const testSubject = shallow(<TestView {...props} />);
+
+        expect(testSubject.getElement()).toMatchSnapshot();
+    });
+});
