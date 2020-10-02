a614d98ef9cbe809db7ef18477508955925d41a0
RobGallo <43080011+RobGallo@users.noreply.github.com>
2020-10-02 17:04:16 -0400

    Use automated checks `HeaderSection` component as the `description` of the android automated checks content page

--- src/electron/platform/android/test-configs/automated-checks/test-config.tsx
+++ src/electron/platform/android/test-configs/automated-checks/test-config.tsx
@@ -1,13 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import { HeaderSection } from 'electron/views/automated-checks/components/header-section';
 import * as React from 'react';
 import { TestConfig } from '../../../../types/test-config';
-
-const description = <>Here's the description for automated checks</>;
 
 export const automatedChecksTestConfig: TestConfig = {
     key: 'automated-checks',
     title: 'Automated checks',
-    description,
+    description: <HeaderSection />,
 };
