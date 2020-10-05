d4e2e9663de3e5504d39d5634447ced379fb157d
Lisa Li <lisli@microsoft.com>
2020-10-05 16:41:39 -0700

    add needsReviewTestConfig

--- src/electron/platform/android/test-configs/android-test-configs.ts
+++ src/electron/platform/android/test-configs/android-test-configs.ts
@@ -1,7 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { automatedChecksTestConfig } from 'electron/platform/android/test-configs/automated-checks/test-config';
+import {
+    automatedChecksTestConfig,
+    needsReviewTestConfig,
+} from 'electron/platform/android/test-configs/automated-checks/test-config';
 import { TestConfig } from 'electron/types/test-config';
 
-export const androidTestConfigs: TestConfig[] = [automatedChecksTestConfig];
+export const androidTestConfigs: TestConfig[] = [automatedChecksTestConfig, needsReviewTestConfig];
--- src/electron/platform/android/test-configs/automated-checks/test-config.tsx
+++ src/electron/platform/android/test-configs/automated-checks/test-config.tsx
@@ -1,12 +1,28 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { HeaderSection } from 'electron/views/automated-checks/components/header-section';
 import * as React from 'react';
 import { TestConfig } from '../../../../types/test-config';
 
 export const automatedChecksTestConfig: TestConfig = {
     key: 'automated-checks',
     title: 'Automated checks',
-    description: <HeaderSection />,
+    description: (
+        <>
+            Automated checks can detect some common accessibility problems such as missing or
+            invalid properties. But most accessibility problems can only be discovered through
+            manual testing.
+        </>
+    ),
+};
+
+export const needsReviewTestConfig: TestConfig = {
+    key: 'needs-review',
+    title: 'Needs review',
+    description: (
+        <>
+            Sometimes automated checks identify <i>possible</i> accessibility problems that need to
+            be reviewed and verified by a human.
+        </>
+    ),
 };
