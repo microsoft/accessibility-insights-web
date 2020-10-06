ee8b17d04ce58417ec4e6f477f1391fd6d03963c
Lisa Li <lisli@microsoft.com>
2020-10-05 17:16:10 -0700

    update header-section test and snapshot

--- src/tests/unit/tests/electron/views/automated-checks/components/header-section.test.tsx
+++ src/tests/unit/tests/electron/views/automated-checks/components/header-section.test.tsx
@@ -1,12 +1,19 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { HeaderSection } from 'electron/views/automated-checks/components/header-section';
+import {
+    HeaderSection,
+    HeaderSectionProps,
+} from 'electron/views/automated-checks/components/header-section';
 import { shallow } from 'enzyme';
 import * as React from 'react';
 
 describe('HeaderSection', () => {
+    const props: HeaderSectionProps = {
+        title: 'test-title',
+        description: <>test-description</>,
+    };
     it('renders', () => {
-        const wrapper = shallow(<HeaderSection />);
+        const wrapper = shallow(<HeaderSection {...props} />);
 
         expect(wrapper.getElement()).toMatchSnapshot();
     });
