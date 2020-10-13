1b81c18868c16c53ccb0ae8c27eb270af7980820
Wahaj Abid <waabid@microsoft.com>
2020-10-13 16:48:15 -0700

    chore: use bundled css for reporter package specifically

---
+++ src/reports/components/web-report-head.tsx
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { NamedFC } from 'common/react/named-fc';
+import { title } from 'content/strings/application';
+import * as React from 'react';
+import { Head } from 'reports/components/head';
+import * as detailsViewBundledStyles from '../../DetailsView/bundled-details-view-styles';
+
+export const WebReportHead = NamedFC('WebReportHead', () => {
+    return <Head titlePreface={title} bundledStyles={detailsViewBundledStyles} />;
+});
