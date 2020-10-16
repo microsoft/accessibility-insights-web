037371b2bb3855e20e3b54402172d177498b95b6
Wahaj Abid <waabid@microsoft.com>
2020-10-16 11:20:51 -0700

    updated with tests and better styling

--- src/electron/views/left-nav/fluent-left-nav.tsx
+++ src/electron/views/left-nav/fluent-left-nav.tsx
@@ -5,7 +5,7 @@ import { NamedFC } from 'common/react/named-fc';
 import { GenericPanel } from 'DetailsView/components/generic-panel';
 import { NarrowModeStatus } from 'DetailsView/components/narrow-mode-detector';
 import { LeftNavItemKey } from 'electron/types/left-nav-item-key';
-import { leftNavPanel, titleBarLookalike } from 'electron/views/left-nav/fluent-left-nav.scss';
+import * as styles from 'electron/views/left-nav/fluent-left-nav.scss';
 import { LeftNav, LeftNavDeps } from 'electron/views/left-nav/left-nav';
 import { PanelType } from 'office-ui-fabric-react';
 import * as React from 'react';
@@ -29,16 +29,15 @@ export const FluentLeftNav = NamedFC<FluentLeftNavProps>('LeftNav', props => {
 
     return (
         <GenericPanel
-            className={leftNavPanel}
+            layerProps={{ className: styles.leftNavPanelHost }}
+            className={styles.leftNavPanel}
             isOpen={isNavOpen}
             isLightDismiss
             hasCloseButton={false}
             onRenderNavigationContent={() => null}
             onRenderNavigation={() => null}
-            // onDismiss={dismissPanel}
             type={PanelType.customNear}
         >
-            <div className={titleBarLookalike} />
             {leftNav}
         </GenericPanel>
     );
