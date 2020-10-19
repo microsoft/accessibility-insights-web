7bc73dc0cb2c6e945cf88ab9d6cc732dc8b86169
Dan Bjorge <dan@dbjorge.net>
2020-10-19 19:25:54 -0400

    Fix jest config

--- packages/report-e2e-tests/jest.config.js
+++ packages/report-e2e-tests/jest.config.js
@@ -3,7 +3,26 @@
 const baseConfig = require('../../src/tests/unit/jest.config');
 const package = require('./package');
 
+const rootDir = '../../';
+const currentDir = '<rootDir>/packages/report-e2e-tests';
+const resultDir = `<rootDir>/test-results/${package.name}`;
+
 module.exports = {
     ...baseConfig,
     displayName: package.name,
+    setupFiles: [],
+    rootDir: rootDir,
+    roots: [currentDir],
+    collectCoverage: false,
+    collectCoverageFrom: [],
+    coverageDirectory: `${resultDir}/coverage`,
+    testEnvironment: 'jsdom',
+    testMatch: [`${currentDir}/**/*.test.(ts|tsx|js)`],
+    reporters: [
+        'default',
+        [
+            'jest-junit',
+            { outputDirectory: '.', outputName: `${resultDir}/junit.xml` },
+        ],
+    ],
 };
