0f899f1e0cbfaa1c350ee07c658e7945eefdf6dd
Dan Bjorge <dan@dbjorge.net>
2020-10-19 19:58:25 -0400

    Hook up tests to run in CI/PR builds

--- packages/report-e2e-tests/src/summary-report.test.ts
+++ packages/report-e2e-tests/src/summary-report.test.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
-test('sanity test', () => {
+test('placeholder smoke test', () => {
     expect(1 + 1).toEqual(2);
 });
