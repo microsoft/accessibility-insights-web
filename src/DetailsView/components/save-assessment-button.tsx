399273035b3897adf131e1d691f2941f442ab2b2
shanise barona <shanisebarona@gmail.com>
2020-10-21 12:50:01 -0700

    remove unnecessary file imports

--- src/DetailsView/components/save-assessment-button.tsx
+++ src/DetailsView/components/save-assessment-button.tsx
@@ -1,12 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { InsightsCommandButton } from 'common/components/controls/insights-command-button';
-import { NamedFC } from 'common/react/named-fc';
-import { IButton, IRefObject } from 'office-ui-fabric-react';
 import * as React from 'react';
-import { FeatureFlags } from 'common/feature-flags';
-import { FeatureFlagStoreData } from 'common/types/store-data/feature-flag-store-data';
-import { FlaggedComponent } from 'common/components/flagged-component';
+import { InsightsCommandButton } from 'common/components/controls/insights-command-button';
 
 export interface SaveAssessmentButtonProps {}
 
