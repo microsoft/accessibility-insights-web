c9b3f54a69f9717a13f85f73729e1280c8aa644a
pownkel <55459788+pownkel@users.noreply.github.com>
2020-10-22 11:57:14 -0700

    refactor(combined-report-ui): separate SummaryReportSummarySection logic from results type (#3514)
    * create UrlsSummarySection component
    
    * make SummaryReportSummarySection a wrapper around UrlsSummarySection

---
+++ src/tests/unit/tests/reports/components/report-sections/urls-summary-section.test.tsx
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import {
+    UrlsSummarySection,
+    UrlsSummarySectionProps,
+} from 'reports/components/report-sections/urls-summary-section';
+
+describe(UrlsSummarySection, () => {
+    const props: UrlsSummarySectionProps = {
+        passedUrlsCount: 1,
+        failedUrlsCount: 2,
+        notScannedUrlsCount: 3,
+        failureInstancesCount: 10,
+    };
+
+    it('renders', () => {
+        const wrapper = shallow(<UrlsSummarySection {...props} />);
+
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+});
