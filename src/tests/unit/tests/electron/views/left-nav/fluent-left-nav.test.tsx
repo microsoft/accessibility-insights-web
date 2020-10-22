c3ffbf294bbe01881e8f4482db84cc58102ff4a5
Wahaj <32555133+waabid@users.noreply.github.com>
2020-10-22 11:30:15 -0700

    feat(android-nav-reflow): add hamburger button to fluent left nav (#3508)

--- src/tests/unit/tests/electron/views/left-nav/fluent-left-nav.test.tsx
+++ src/tests/unit/tests/electron/views/left-nav/fluent-left-nav.test.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import { FastPassLeftNavHamburgerButton } from 'common/components/expand-collapse-left-nav-hamburger-button';
 import { NarrowModeStatus } from 'DetailsView/components/narrow-mode-detector';
 import {
     FluentLeftNav,
@@ -13,23 +14,30 @@ import * as React from 'react';
 describe('FluentLeftNav', () => {
     let props: FluentLeftNavProps;
     let narrowModeStatusStub: NarrowModeStatus;
+    let setSideNavOpenStub: (value: boolean) => void;
 
     beforeEach(() => {
         narrowModeStatusStub = {
             isHeaderAndNavCollapsed: true,
         } as NarrowModeStatus;
 
+        setSideNavOpenStub = _ => null;
+
         props = {
             deps: {} as FluentLeftNavDeps,
             selectedKey: 'automated-checks',
             isNavOpen: true,
             narrowModeStatus: narrowModeStatusStub,
+            setSideNavOpen: setSideNavOpenStub,
         };
     });
 
     test('render left nav inside panel', () => {
         const testSubject = shallow(<FluentLeftNav {...props} />);
         expect(testSubject.getElement()).toMatchSnapshot();
+        expect(testSubject.find(FastPassLeftNavHamburgerButton).prop('setSideNavOpen')).toEqual(
+            setSideNavOpenStub,
+        );
     });
 
     test('render left nav by itself', () => {
