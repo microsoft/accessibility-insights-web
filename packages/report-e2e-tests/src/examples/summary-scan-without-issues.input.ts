34709442f9f7aebb6f4394cf719a09316aeadec6
Dan Bjorge <dan@dbjorge.net>
2020-10-23 14:28:40 -0400

    Remove extra whitespace

--- packages/report-e2e-tests/src/examples/summary-scan-without-issues.input.ts
+++ packages/report-e2e-tests/src/examples/summary-scan-without-issues.input.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 import { SummaryReportParameters } from 'accessibility-insights-report';
 
 export const summaryScanWithoutIssues: SummaryReportParameters = {
