1b203027665c3226db87a35598ffc3e466bfbb7b
Dan Bjorge <dan@dbjorge.net>
2020-10-23 14:27:32 -0400

    Fix watchPathIgnorePatterns to match new pattern

--- packages/report-e2e-tests/jest.config.js
+++ packages/report-e2e-tests/jest.config.js
@@ -8,5 +8,5 @@ module.exports = {
     collectCoverage: false,
     displayName: package.name,
     setupFilesAfterEnv: [`<rootDir>/src/jest-setup.ts`],
-    watchPathIgnorePatterns: ['src/examples/*.output.html'],
+    watchPathIgnorePatterns: ['*.snap.html'],
 };
