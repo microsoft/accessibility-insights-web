138c39a69a0a159ee3d4207e720f04459f726428
Dan Bjorge <danielbj@microsoft.com>
2020-10-26 13:33:07 -0400

    test(combined-report-ui): migrate report integration tests as e2e tests (#3522)
    #### Description of changes
    
    This PR:
    
    * Migrates the existing report integration tests to act as e2e tests instead
    * Updates the snapshotting strategy to snapshot whole html files at a time, to make it easier to manually verify them during development
    * Updates the PR/CI build to use a separate job for the new E2E tests (a PR later today will split this out to a separate pipeline, along with all the other e2e jobs)
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a" in the checkbox -->
    - [x] Addresses an existing issue: 1783601
    - [x] Ran `yarn fastpass`
    - [x] Added/updated relevant unit test(s) (and ran `yarn test`)
    - [x] Verified code coverage for the changes made. Check coverage report at: `<rootDir>/test-results/unit/coverage`
    - [x] PR title *AND* final merge commit title both start with a semantic tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`). See `CONTRIBUTING.md`.
    - [n/a] (UI changes only) Added screenshots/GIFs to description above
    - [n/a] (UI changes only) Verified usability with NVDA/JAWS

---
+++ packages/report-e2e-tests/src/from-axe-result.test.ts
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { AxeReportParameters, reporterFactory } from 'accessibility-insights-report';
+import * as path from 'path';
+import * as prettier from 'prettier';
+
+import { axeResultsWithIssues } from './examples/axe-results-with-issues.input';
+import { axeResultsWithoutIssues } from './examples/axe-results-without-issues.input';
+
+describe('fromAxeResult', () => {
+    const examples = {
+        'axe-results-with-issues': axeResultsWithIssues,
+        'axe-results-without-issues': axeResultsWithoutIssues,
+    };
+
+    describe.each(Object.keys(examples))('with example input "%s"', (exampleName: string) => {
+        const input: AxeReportParameters = examples[exampleName];
+
+        it('produces pinned HTML file', () => {
+            const output = reporterFactory().fromAxeResult(input).asHTML();
+            const formattedOutput = prettier.format(output, {
+                parser: 'html',
+                htmlWhitespaceSensitivity: 'strict',
+            });
+
+            const snapshotFile = path.join(__dirname, 'examples', `${exampleName}.snap.html`);
+            expect(formattedOutput).toMatchFile(snapshotFile);
+        });
+    });
+});
--- packages/report-e2e-tests/src/jest-setup.ts
+++ packages/report-e2e-tests/src/jest-setup.ts
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-test('placeholder smoke test', () => {
-    expect(1 + 1).toEqual(2);
-});
+import { toMatchFile } from 'jest-file-snapshot';
+
+expect.extend({ toMatchFile });
