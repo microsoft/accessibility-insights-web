a7a439d0a184ae91ab4671a8d0893447b2528aaf
pownkel <55459788+pownkel@users.noreply.github.com>
2020-10-30 10:50:36 -0700

    feat(combined-report-ui): Rules results section (#3566)
    * create results section with "rules" text
    
    * Update report e2e snapshots
    
    * remove old tslint comments

---
+++ src/reports/components/report-sections/rules-results-container.tsx
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { NamedFC } from 'common/react/named-fc';
+import * as React from 'react';
+import { CombinedReportSectionProps } from 'reports/components/report-sections/combined-report-section-factory';
+import * as styles from './rules-results-container.scss';
+
+export type RulesResultsContainerProps = Pick<CombinedReportSectionProps, 'getCollapsibleScript'>;
+
+export const RulesResultsContainer = NamedFC<RulesResultsContainerProps>(
+    'RulesResultsContainer',
+    ({ children, getCollapsibleScript }) => {
+        return (
+            <>
+                <div className={styles.rulesResultsContainer}>
+                    <div className={styles.resultsHeading}>Rules</div>
+                    {children}
+                </div>
+                <script dangerouslySetInnerHTML={{ __html: getCollapsibleScript() }} />
+            </>
+        );
+    },
+);
