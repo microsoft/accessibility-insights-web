d1ec06e3550b1782e4694e17de8a7241bb064c2e
Dan Bjorge <dan@dbjorge.net>
2020-11-02 15:09:21 -0500

    revert failed experiment

--- src/reports/bundled-reporter-styles.ts
+++ src/reports/bundled-reporter-styles.ts
@@ -1,5 +1,3 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import styles from 'to-string!file!../../packages/report/bundle/report.css';
-
-export const styleSheet = styles;
+export const styleSheet = `<<CSS:../../packages/report/bundle/report.css>>`;
