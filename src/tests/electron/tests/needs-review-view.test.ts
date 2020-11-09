eb008963dbdce5caf639ad4bc51e2a8faed7afa1
Rob Gallo <43080011+RobGallo@users.noreply.github.com>
2020-11-09 15:37:16 -0500

    feat(android-needs-review): Add e2e accessibility checks (#3630)

--- src/tests/electron/tests/needs-review-view.test.ts
+++ src/tests/electron/tests/needs-review-view.test.ts
@@ -4,6 +4,7 @@ import { getNarrowModeThresholdsForUnified } from 'electron/common/narrow-mode-t
 import { UnifiedFeatureFlags } from 'electron/common/unified-feature-flags';
 import * as path from 'path';
 import { createApplication } from 'tests/electron/common/create-application';
+import { scanForAccessibilityIssuesInAllModes } from 'tests/electron/common/scan-for-accessibility-issues';
 import { AppController } from 'tests/electron/common/view-controllers/app-controller';
 import { AutomatedChecksViewController } from 'tests/electron/common/view-controllers/automated-checks-view-controller';
 import { commonAdbConfigs, setupMockAdb } from 'tests/miscellaneous/mock-adb/setup-mock-adb';
@@ -36,5 +37,9 @@ describe('NeedsReviewView', () => {
 
     it('should use the expected window title', async () => {
         await app.waitForTitle('Accessibility Insights for Android - Needs review');
+    });
+
+    it('should pass accessibility validation in all contrast modes', async () => {
+        await scanForAccessibilityIssuesInAllModes(app);
     });
 });
