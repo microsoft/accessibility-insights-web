312408770a283e6dffcf64aea960159cecd03b31
RobGallo <43080011+RobGallo@users.noreply.github.com>
2020-11-09 13:41:20 -0500

    Ensure window is big enough so that the left nav bar is visible by default.

--- src/tests/electron/tests/needs-review-view.test.ts
+++ src/tests/electron/tests/needs-review-view.test.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { getNarrowModeThresholdsForUnified } from 'electron/common/narrow-mode-thresholds';
 import { UnifiedFeatureFlags } from 'electron/common/unified-feature-flags';
 import * as path from 'path';
 import { createApplication } from 'tests/electron/common/create-application';
@@ -10,6 +11,8 @@ import { commonAdbConfigs, setupMockAdb } from 'tests/miscellaneous/mock-adb/set
 describe('NeedsReviewView', () => {
     let app: AppController;
     let automatedChecksViewController: AutomatedChecksViewController;
+    const windowWidth = getNarrowModeThresholdsForUnified().collapseHeaderAndNavThreshold + 5;
+    const windowHeight = 1000;
 
     beforeEach(async () => {
         await setupMockAdb(
@@ -19,6 +22,7 @@ describe('NeedsReviewView', () => {
         );
 
         app = await createApplication({ suppressFirstTimeDialog: true });
+        app.client.browserWindow.setSize(windowWidth, windowHeight);
         await app.setFeatureFlag(UnifiedFeatureFlags.leftNavBar, true);
         automatedChecksViewController = await app.openAutomatedChecksView();
         await automatedChecksViewController.clickLeftNavItem('needs-review');
