9e6a1c2341de6011872e781fdc8cf6f629777e88
shanise barona <shanisebarona@gmail.com>
2020-11-11 16:32:17 -0800

    update formatting

--- src/DetailsView/components/save-assessment-factory.tsx
+++ src/DetailsView/components/save-assessment-factory.tsx
@@ -4,10 +4,10 @@ import * as React from 'react';
 import { CommandBarProps } from 'DetailsView/components/details-view-command-bar';
 import { FileURLProvider } from '../../common/file-url-provider';
 import { SaveAssessmentButton } from 'DetailsView/components/save-assessment-button';
-import { AssessmentDataFormatter } from 'common/assessment-data-formatter'
+import { AssessmentDataFormatter } from 'common/assessment-data-formatter';
 
 export type SaveAssessmentFactoryDeps = {
-    fileURLProvider: FileURLProvider,
+    fileURLProvider: FileURLProvider;
     assessmentDataFormatter: AssessmentDataFormatter;
 };
 
@@ -16,15 +16,12 @@ export type SaveAssessmentFactoryProps = CommandBarProps & {
 };
 
 export function getSaveButtonForAssessment(props: SaveAssessmentFactoryProps): JSX.Element {
-    const assessmentData = props.deps.assessmentDataFormatter.formatAssessmentData(props.assessmentStoreData.assessments)
+    const assessmentData = props.deps.assessmentDataFormatter.formatAssessmentData(
+        props.assessmentStoreData.assessments,
+    );
     const fileURL = props.deps.fileURLProvider.provideURL([assessmentData], 'text/html');
 
-    return (
-        <SaveAssessmentButton
-            download={'filename'}
-            href={fileURL}
-        />
-    );
+    return <SaveAssessmentButton download={'filename'} href={fileURL} />;
 }
 
 export function getSaveButtonForFastPass(props: SaveAssessmentFactoryProps): JSX.Element | null {
--- src/common/assessment-data-formatter.ts
+++ src/common/assessment-data-formatter.ts
@@ -5,7 +5,6 @@ export class AssessmentDataFormatter {
 
     public formatAssessmentData = (assessmentData: {}): string => {
         const assessmentReportData = JSON.stringify(assessmentData);
-        // add versioning for backwards compatibility
         return assessmentReportData;
     };
 }
