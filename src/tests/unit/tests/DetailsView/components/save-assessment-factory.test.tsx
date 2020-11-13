614cfdfcb3a3ce960d087e41083fd5a9b094a733
shanise barona <shanisebarona@gmail.com>
2020-11-13 11:53:50 -0800

    add download functionality and update relevant tests

--- src/tests/unit/tests/DetailsView/components/save-assessment-factory.test.tsx
+++ src/tests/unit/tests/DetailsView/components/save-assessment-factory.test.tsx
@@ -1,13 +1,39 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { IMock, Mock } from 'typemoq';
+import { AssessmentDataFormatter } from 'common/assessment-data-formatter';
+import { FileURLProvider } from 'common/file-url-provider';
+import { AssessmentStoreData } from 'common/types/store-data/assessment-result-data';
 import {
     getSaveButtonForAssessment,
     getSaveButtonForFastPass,
+    SaveAssessmentFactoryDeps,
     SaveAssessmentFactoryProps,
 } from 'DetailsView/components/save-assessment-factory';
 
 describe('SaveAssessmentFactory', () => {
+    let deps: SaveAssessmentFactoryDeps;
     let props: SaveAssessmentFactoryProps;
+
+    beforeEach(() => {
+        const fileURLProviderMock: IMock<FileURLProvider> = Mock.ofType(FileURLProvider);
+        const assessmentDataFormatterMock: IMock<AssessmentDataFormatter> = Mock.ofType(
+            AssessmentDataFormatter,
+        );
+
+        const assessmentStoreData = {
+            assessments: null,
+        } as AssessmentStoreData;
+
+        deps = {
+            assessmentDataFormatter: assessmentDataFormatterMock.object,
+            fileURLProvider: fileURLProviderMock.object,
+        } as SaveAssessmentFactoryDeps;
+        props = {
+            deps,
+            assessmentStoreData,
+        } as SaveAssessmentFactoryProps;
+    });
 
     describe('getSaveButtonForAssessment', () => {
         test('renders save assessment button', () => {
