a2fcb2401c3b09380591653c939780aacae8b855
shanise barona <shanisebarona@gmail.com>
2020-11-18 09:56:17 -0800

    add filename builder

--- src/DetailsView/components/save-assessment-factory.tsx
+++ src/DetailsView/components/save-assessment-factory.tsx
@@ -5,9 +5,11 @@ import { FileURLProvider } from '../../common/file-url-provider';
 import { SaveAssessmentButton } from 'DetailsView/components/save-assessment-button';
 import { AssessmentDataFormatter } from 'common/assessment-data-formatter';
 import { AssessmentStoreData } from 'common/types/store-data/assessment-result-data';
+import { FileNameBuilder } from 'common/filename-builder'
 
 export type SaveAssessmentFactoryDeps = {
     fileURLProvider: FileURLProvider;
+    fileNameBuilder: FileNameBuilder;
     assessmentDataFormatter: AssessmentDataFormatter;
 };
 
@@ -20,9 +22,15 @@ export function getSaveButtonForAssessment(props: SaveAssessmentFactoryProps): J
     const assessmentData = props.deps.assessmentDataFormatter.formatAssessmentData(
         props.assessmentStoreData.assessments,
     );
+
+    const currentDate = new Date();
+    const fileDate = props.deps.fileNameBuilder.getDateSegment(currentDate)
+    const targetPageTitle = props.assessmentStoreData.persistedTabInfo.title;
+    const fileTitle = props.deps.fileNameBuilder.getTitleSegment(targetPageTitle);
+    const fileName = `SavedAssessment_<${fileDate}>_${fileTitle}.allyextension`;
     const fileURL = props.deps.fileURLProvider.provideURL([assessmentData], 'application/json');
 
-    return <SaveAssessmentButton download={'filename.a11yextension'} href={fileURL} />;
+    return <SaveAssessmentButton download={fileName} href={fileURL} />;
 }
 
 export function getSaveButtonForFastPass(props: SaveAssessmentFactoryProps): JSX.Element | null {
