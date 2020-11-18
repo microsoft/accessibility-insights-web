f770430bdcd084e62804136cc5719eb1ea4b55e2
Kelsey Pownell <kepownel@microsoft.com>
2020-11-18 14:28:41 -0800

    add white outline to tooltip in high contrast mode

--- src/common/components/new-tab-link-with-tooltip.tsx
+++ src/common/components/new-tab-link-with-tooltip.tsx
@@ -1,8 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { NewTabLink } from 'common/components/new-tab-link';
-import { ILinkProps, ITooltipHostStyles, TooltipHost } from 'office-ui-fabric-react';
+import { ICalloutProps, ILinkProps, ITooltipHostStyles, TooltipHost } from 'office-ui-fabric-react';
 import * as React from 'react';
+import * as styles from './new-tab-link-with-tooltip.scss';
 
 import { NamedFC } from '../react/named-fc';
 
@@ -15,8 +16,14 @@ export const NewTabLinkWithTooltip = NamedFC<NewTabLinkWithTooltipProps>(
         const hostStyles: Partial<ITooltipHostStyles> = {
             root: { display: 'inline-block', minWidth: 0 },
         };
+        const calloutProps: ICalloutProps = {
+            styles: {
+                root: styles.insightsTooltip,
+                beak: styles.insightsTooltip,
+            },
+        };
         return (
-            <TooltipHost content={tooltipContent} styles={hostStyles}>
+            <TooltipHost content={tooltipContent} styles={hostStyles} calloutProps={calloutProps}>
                 <NewTabLink {...linkProps} />
             </TooltipHost>
         );
