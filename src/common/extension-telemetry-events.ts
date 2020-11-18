9bc7dcfe40a7a94462cf56b1f7a047066c2e1be9
Dan Bjorge <dan@dbjorge.net>
2020-11-18 16:14:50 -0500

    Reset state of cards view on left nav change

--- src/common/extension-telemetry-events.ts
+++ src/common/extension-telemetry-events.ts
@@ -68,6 +68,7 @@ export const SCAN_INCOMPLETE_WARNINGS: string = 'scanIncompleteWarnings';
 export const ALL_URLS_PERMISSION_UPDATED: string = 'allUrlsPermissionUpdated';
 export const LEFT_NAV_PANEL_EXPANDED: string = 'leftNavPanelExpanded';
 export const NEEDS_REVIEW_TOGGLE: string = 'NeedsReviewToggled';
+export const NAVIGATE_TO_NEW_CARDS_VIEW: string = 'NavigateToNewCardsView';
 
 export const TriggeredByNotApplicable: TriggeredBy = 'N/A';
 export type TriggeredBy = 'mouseclick' | 'keypress' | 'shortcut' | 'N/A';
--- src/electron/flux/action-creator/left-nav-action-creator.ts
+++ src/electron/flux/action-creator/left-nav-action-creator.ts
@@ -1,12 +1,21 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import { CardSelectionActions } from 'background/actions/card-selection-actions';
 import { LeftNavActions } from 'electron/flux/action/left-nav-actions';
 import { LeftNavItemKey } from 'electron/types/left-nav-item-key';
 
 export class LeftNavActionCreator {
-    constructor(private readonly actions: LeftNavActions) {}
+    constructor(
+        private readonly leftNavActions: LeftNavActions,
+        private readonly cardSelectionActions: CardSelectionActions,
+    ) {}
 
-    public itemSelected = (itemKey: LeftNavItemKey) => this.actions.itemSelected.invoke(itemKey);
-    public setLeftNavVisible = (value: boolean) => this.actions.setLeftNavVisible.invoke(value);
+    public itemSelected = (itemKey: LeftNavItemKey) => {
+        this.leftNavActions.itemSelected.invoke(itemKey);
+        this.cardSelectionActions.navigateToNewCardsView.invoke(null);
+    };
+
+    public setLeftNavVisible = (value: boolean) =>
+        this.leftNavActions.setLeftNavVisible.invoke(value);
 }
