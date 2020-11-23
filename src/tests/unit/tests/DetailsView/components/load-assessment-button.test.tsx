255634a1256ed25a9906501f6896da640ebb1894
shanise barona <shanisebarona@gmail.com>
2020-11-23 13:50:01 -0800

    add load assessment button test

---
+++ src/tests/unit/tests/DetailsView/components/load-assessment-button.test.tsx
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+import {
+    LoadAssessmentButton,
+    LoadAssessmentButtonProps,
+} from 'DetailsView/components/load-assessment-button';
+
+describe('LoadAssessmentButton', () => {
+    let props: LoadAssessmentButtonProps;
+
+    it('should render per the snapshot', () => {
+        const rendered = shallow(
+            <LoadAssessmentButton />,
+        );
+
+        expect(rendered.getElement()).toMatchSnapshot();
+    });
+});
