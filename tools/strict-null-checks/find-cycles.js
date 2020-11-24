45abf459273ff2235dd7289f53445ab2043f086a
Dan Bjorge <danielbj@microsoft.com>
2020-11-24 16:44:24 -0500

    chore(null): implement a yarn null:find-cycles command (#3675)
    #### Description of changes
    
    This adds a `yarn null:find-cycles` command to find import cycles.
    
    [eslint-plugin-import/no-cycle](https://github.com/benmosher/eslint-plugin-import/blob/HEAD/docs/rules/no-cycle.md) would be a better way to solve the same problem; unfortunately, I wasn't able to get it to cooperate with our repo settings (I didn't find a way to get it to recognize the `baseUrl: './src'` part of our tsconfig, in particular)
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a" in the checkbox -->
    - [x] Addresses an existing issue: part of #2869
    - [x] Ran `yarn fastpass`
    - [n/a] Added/updated relevant unit test(s) (and ran `yarn test`)
    - [n/a] Verified code coverage for the changes made. Check coverage report at: `<rootDir>/test-results/unit/coverage`
    - [x] PR title *AND* final merge commit title both start with a semantic tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`). See `CONTRIBUTING.md`.
    - [n/a] (UI changes only) Added screenshots/GIFs to description above
    - [n/a] (UI changes only) Verified usability with NVDA/JAWS

---
+++ tools/strict-null-checks/find-cycles.js
@@ -0,0 +1,58 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// @ts-check
+const process = require('process');
+const { srcRoot } = require('./config');
+const { getAllEligibleFiles } = require('./eligible-file-finder');
+const { getMemoizedImportsForFile } = require('./import-finder');
+
+if (process.argv.includes('--help')) {
+    console.log('yarn null:find-cycles');
+    process.exit(0);
+}
+
+async function main() {
+    const eligibleFiles = await getAllEligibleFiles();
+    const fileState = {};
+    for (const rootFile of eligibleFiles) {
+        const transitiveImportsWithImportPath = {};
+        const directImports = await getMemoizedImportsForFile(rootFile, srcRoot);
+        const untraversedImportChains = directImports.map(directImport => ({
+            file: directImport,
+            path: [rootFile],
+        }));
+        while (untraversedImportChains.length > 0) {
+            const currentImportChain = untraversedImportChains.pop();
+            const currentImport = currentImportChain.file;
+            if (transitiveImportsWithImportPath[currentImport] != undefined) {
+                continue;
+            }
+            transitiveImportsWithImportPath[currentImport] = currentImportChain;
+            const nextImports = await getMemoizedImportsForFile(currentImport, srcRoot);
+            const nextImportChains = nextImports.map(file => ({
+                file,
+                path: [...currentImportChain.path, currentImport],
+            }));
+            untraversedImportChains.push(...nextImportChains);
+        }
+        fileState[rootFile] = {
+            transitiveImportsWithImportPath,
+            selfImportPath:
+                transitiveImportsWithImportPath[rootFile] &&
+                transitiveImportsWithImportPath[rootFile].path,
+        };
+    }
+
+    const selfImportsByLength = Object.values(fileState)
+        .map(s => s.selfImportPath)
+        .filter(path => path != null)
+        .sort((a, b) => b.length - a.length);
+
+    console.log(JSON.stringify(selfImportsByLength, null, 2));
+}
+
+main().catch(error => {
+    console.error(error.stack);
+    process.exit(1);
+});
