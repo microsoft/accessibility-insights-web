540a0b9710eef06888ffbc48ec9ac1816419ec07
shanise barona <shanisebarona@gmail.com>
2020-12-05 19:50:28 -0800

    add parser class for uploaded file contents

---
+++ src/common/assessment-data-parser.ts
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+import { VersionedAssessmentData } from "common/types/versioned-assessment-data";
+
+// Licensed under the MIT License.
+export class AssessmentDataParser {
+    public parseAssessmentData = (assessmentData: string ): VersionedAssessmentData => {
+        const uploadedAssessmentData = JSON.parse(assessmentData);
+        return uploadedAssessmentData;
+    };
+
+}
