244bba78e86769f6bb559ea21d2b2024bb6970bd
John Alkire <john.alkire@microsoft.com>
2021-02-04 15:07:54 -0800

    Create testConfig for Tab Stops test

--- src/electron/platform/android/test-configs/android-test-configs.ts
+++ src/electron/platform/android/test-configs/android-test-configs.ts
@@ -3,6 +3,11 @@
 
 import { automatedChecksTestConfig } from 'electron/platform/android/test-configs/automated-checks/test-config';
 import { needsReviewTestConfig } from 'electron/platform/android/test-configs/needs-review/test-config';
+import { tabStopsTestConfig } from 'electron/platform/android/test-configs/tab-stops/test-config';
 import { TestConfig } from 'electron/types/test-config';
 
-export const androidTestConfigs: TestConfig[] = [automatedChecksTestConfig, needsReviewTestConfig];
+export const androidTestConfigs: TestConfig[] = [
+    automatedChecksTestConfig,
+    tabStopsTestConfig,
+    needsReviewTestConfig,
+];
