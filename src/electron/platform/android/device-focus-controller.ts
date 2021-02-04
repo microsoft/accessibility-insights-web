9a6de9023e2213ae7f7619f86bac2e095f1b523e
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2021-02-04 10:15:02 -0800

    Add focus controller

---
+++ src/electron/platform/android/device-focus-controller.ts
@@ -0,0 +1,66 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { AdbWrapper, KeyEventCode } from 'electron/platform/android/adb-wrapper';
+import {
+    DeviceFocusCommand,
+    FocusCommandSender,
+} from 'electron/platform/android/send-focus-command';
+
+export class DeviceFocusController {
+    private deviceId: string;
+    private port: number;
+
+    constructor(
+        private readonly adbWrapper: AdbWrapper,
+        private readonly commandSender: FocusCommandSender,
+    ) {}
+
+    public setDeviceId(deviceId: string) {
+        this.deviceId = deviceId;
+    }
+
+    public setPort(port: number) {
+        this.port = port;
+    }
+
+    public SendResumeTrackingCommandToDevice(): Promise<void> {
+        return this.commandSender(this.port, DeviceFocusCommand.Resume);
+    }
+
+    public SendPauseTrackingCommandToDevice(): Promise<void> {
+        return this.commandSender(this.port, DeviceFocusCommand.Pause);
+    }
+
+    public SendEndTrackingCommandToDevice(): Promise<void> {
+        return this.commandSender(this.port, DeviceFocusCommand.End);
+    }
+
+    public SendUpKeyToDevice(): Promise<void> {
+        return this.SendKeyEventToDevice(KeyEventCode.Up);
+    }
+
+    public SendDownKeyToDevice(): Promise<void> {
+        return this.SendKeyEventToDevice(KeyEventCode.Down);
+    }
+
+    public SendLeftKeyToDevice(): Promise<void> {
+        return this.SendKeyEventToDevice(KeyEventCode.Left);
+    }
+
+    public SendRightKeyToDevice(): Promise<void> {
+        return this.SendKeyEventToDevice(KeyEventCode.Right);
+    }
+
+    public SendEnterToDevice(): Promise<void> {
+        return this.SendKeyEventToDevice(KeyEventCode.Enter);
+    }
+
+    public SendTabKeyToDevice(): Promise<void> {
+        return this.SendKeyEventToDevice(KeyEventCode.Tab);
+    }
+
+    private SendKeyEventToDevice(keyEventCode: KeyEventCode): Promise<void> {
+        return this.adbWrapper.sendKeyEvent(this.deviceId, keyEventCode);
+    }
+}
