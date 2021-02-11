f0d857ba1dc8e5d08069eef08e902023103e829f
John Alkire <john.alkire@microsoft.com>
2021-02-11 15:04:15 -0800

    Re-fix import order

--- src/tests/miscellaneous/mock-service-for-android/mock-service.js
+++ src/tests/miscellaneous/mock-service-for-android/mock-service.js
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 const fs = require('fs');
-const express = require('express');
 const path = require('path');
+const express = require('express');
 
 function startMockService(port, filesPath, testLogsDir) {
     return new Promise(resolve => {
