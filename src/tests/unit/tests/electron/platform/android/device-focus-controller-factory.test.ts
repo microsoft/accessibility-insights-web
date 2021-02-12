6ddb42f91b546a020375286d090f25f5e7b0ca6b
Wahaj Abid <waabid@microsoft.com>
2021-02-12 11:05:57 -0800

    lint fix

--- src/tests/unit/tests/electron/platform/android/device-focus-controller-factory.test.ts
+++ src/tests/unit/tests/electron/platform/android/device-focus-controller-factory.test.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { AdbWrapper } from 'electron/platform/android/adb-wrapper';
 import { TelemetryEventHandler } from 'background/telemetry/telemetry-event-handler';
+import { AdbWrapper } from 'electron/platform/android/adb-wrapper';
 import { DeviceFocusCommandSender } from 'electron/platform/android/device-focus-command-sender';
 import { DeviceFocusController } from 'electron/platform/android/device-focus-controller';
 import { DeviceFocusControllerFactory } from 'electron/platform/android/device-focus-controller-factory';
