59f4ccb30dde8ec1e42bf92418735adea0223abb
John Alkire <john.alkire@microsoft.com>
2021-02-17 11:01:35 -0800

    E2E test the virtual keyboard

--- src/tests/electron/tests/tab-stops-view.test.ts
+++ src/tests/electron/tests/tab-stops-view.test.ts
@@ -7,25 +7,34 @@ import { createApplication } from 'tests/electron/common/create-application';
 import { ResultsViewSelectors } from 'tests/electron/common/element-identifiers/results-view-selectors';
 import { scanForAccessibilityIssuesInAllModes } from 'tests/electron/common/scan-for-accessibility-issues';
 import { AppController } from 'tests/electron/common/view-controllers/app-controller';
+import { LogController } from 'tests/electron/common/view-controllers/log-controller';
 import { ResultsViewController } from 'tests/electron/common/view-controllers/results-view-controller';
-import { commonAdbConfigs, setupMockAdb } from 'tests/miscellaneous/mock-adb/setup-mock-adb';
+import { VirtualKeyboardViewController } from 'tests/electron/common/view-controllers/virtual-keyboard-view-controller';
+import {
+    commonAdbConfigs,
+    mockAdbFolder,
+    setupMockAdb,
+} from 'tests/miscellaneous/mock-adb/setup-mock-adb';
 
 describe('TabStopsView', () => {
     let app: AppController;
     let resultsViewController: ResultsViewController;
+    let virtualKeyboardViewController: VirtualKeyboardViewController;
+    const enterKeyEvent = 'keyevent 66';
+    const logController = new LogController(mockAdbFolder);
+    const mockAdbLogsBase = path.basename(__filename);
+    const mockAdbLogsFolder = 'tabStopsLogs';
     const windowWidth = getNarrowModeThresholdsForUnified().collapseHeaderAndNavThreshold + 5;
     const windowHeight = 1000;
+    const logsContext = path.join(mockAdbLogsBase, mockAdbLogsFolder);
 
     beforeEach(async () => {
-        await setupMockAdb(
-            commonAdbConfigs['single-device'],
-            path.basename(__filename),
-            'beforeEach',
-        );
+        await setupMockAdb(commonAdbConfigs['single-device'], mockAdbLogsBase, mockAdbLogsFolder);
 
         app = await createApplication({ suppressFirstTimeDialog: true });
         app.setFeatureFlag(UnifiedFeatureFlags.tabStops, true);
         app.client.browserWindow.setSize(windowWidth, windowHeight);
+        virtualKeyboardViewController = new VirtualKeyboardViewController(app.client);
         resultsViewController = await app.openResultsView();
         await resultsViewController.clickLeftNavItem('tab-stops');
     });
@@ -38,6 +47,21 @@ describe('TabStopsView', () => {
 
     it('should use the expected window title', async () => {
         await app.waitForTitle('Accessibility Insights for Android - Tab stops');
+    });
+
+    it('virtual keyboard keys send corresponding adb commands', async () => {
+        logController.resetAdbLog(logsContext);
+
+        await virtualKeyboardViewController.clickVirtualKey('Up');
+        await virtualKeyboardViewController.clickVirtualKey('Down');
+        await virtualKeyboardViewController.clickVirtualKey('Left');
+        await virtualKeyboardViewController.clickVirtualKey('Right');
+        await virtualKeyboardViewController.clickVirtualKey('Tab');
+        await virtualKeyboardViewController.clickVirtualKey('Enter');
+        await logController.waitForAdbLogToContain(enterKeyEvent, logsContext, app.client);
+
+        const adbLog = await logController.getAdbLog(logsContext);
+        expect(adbLog).toMatchSnapshot();
     });
 
     it('should pass accessibility validation in all contrast modes', async () => {
