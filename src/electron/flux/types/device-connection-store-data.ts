626b28dded5d26d4570255abd43af235a8daeeae
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2021-02-17 15:50:10 -0800

    Single quote strings

--- src/electron/flux/types/device-connection-store-data.ts
+++ src/electron/flux/types/device-connection-store-data.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { DeviceConnectionStatus } from "electron/flux/types/device-connection-status";
+import { DeviceConnectionStatus } from 'electron/flux/types/device-connection-status';
 
 export interface DeviceConnectionStoreData {
     status: DeviceConnectionStatus;
