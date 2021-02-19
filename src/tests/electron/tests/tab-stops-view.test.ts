220d0e34f73510122efa0b77a520ae93858cb259
John Alkire <4615491+jalkire@users.noreply.github.com>
2021-02-19 21:02:27 +0000

    feat(focus-mvp-client): Add E2E tests for virtual keyboard (#3929)
    Details
    This PR adds E2E tests to ensure clicking the virtual keyboard sends the appropriate adb commands. To test this, AutomationIds are added to the keys and VirtualKeyboardViewController is introduced.
    
    Motivation
    E2E tests are a part of the feature spec
    
    Context
    This relies on the logging added in #3906

--- src/tests/electron/tests/tab-stops-view.test.ts
+++ src/tests/electron/tests/tab-stops-view.test.ts
@@ -3,29 +3,38 @@
 import * as path from 'path';
 import { getNarrowModeThresholdsForUnified } from 'electron/common/narrow-mode-thresholds';
 import { UnifiedFeatureFlags } from 'electron/common/unified-feature-flags';
+import { KeyEventCode } from 'electron/platform/android/adb-wrapper';
 import { createApplication } from 'tests/electron/common/create-application';
 import { ResultsViewSelectors } from 'tests/electron/common/element-identifiers/results-view-selectors';
 import { scanForAccessibilityIssuesInAllModes } from 'tests/electron/common/scan-for-accessibility-issues';
 import { AppController } from 'tests/electron/common/view-controllers/app-controller';
+import { LogController } from 'tests/electron/common/view-controllers/log-controller';
 import { ResultsViewController } from 'tests/electron/common/view-controllers/results-view-controller';
-import { commonAdbConfigs, setupMockAdb } from 'tests/miscellaneous/mock-adb/setup-mock-adb';
+import { VirtualKeyboardViewController } from 'tests/electron/common/view-controllers/virtual-keyboard-view-controller';
+import {
+    commonAdbConfigs,
+    mockAdbFolder,
+    setupMockAdb,
+} from 'tests/miscellaneous/mock-adb/setup-mock-adb';
 
 describe('TabStopsView', () => {
     let app: AppController;
     let resultsViewController: ResultsViewController;
+    let virtualKeyboardViewController: VirtualKeyboardViewController;
+    const logController = new LogController(mockAdbFolder);
+    const mockAdbLogsBase = path.basename(__filename);
+    const mockAdbLogsFolder = 'tabStopsLogs';
     const windowWidth = getNarrowModeThresholdsForUnified().collapseHeaderAndNavThreshold + 5;
     const windowHeight = 1000;
+    const logsContext = path.join(mockAdbLogsBase, mockAdbLogsFolder);
 
     beforeEach(async () => {
-        await setupMockAdb(
-            commonAdbConfigs['single-device'],
-            path.basename(__filename),
-            'beforeEach',
-        );
+        await setupMockAdb(commonAdbConfigs['single-device'], mockAdbLogsBase, mockAdbLogsFolder);
 
         app = await createApplication({ suppressFirstTimeDialog: true });
         app.setFeatureFlag(UnifiedFeatureFlags.tabStops, true);
         app.client.browserWindow.setSize(windowWidth, windowHeight);
+        virtualKeyboardViewController = new VirtualKeyboardViewController(app.client);
         resultsViewController = await app.openResultsView();
         await resultsViewController.clickLeftNavItem('tab-stops');
     });
@@ -38,6 +47,25 @@ describe('TabStopsView', () => {
 
     it('should use the expected window title', async () => {
         await app.waitForTitle('Accessibility Insights for Android - Tab stops');
+    });
+
+    it('virtual keyboard keys send corresponding adb commands', async () => {
+        logController.resetAdbLog(logsContext);
+
+        await virtualKeyboardViewController.clickVirtualKey(KeyEventCode.Up);
+        await virtualKeyboardViewController.clickVirtualKey(KeyEventCode.Down);
+        await virtualKeyboardViewController.clickVirtualKey(KeyEventCode.Left);
+        await virtualKeyboardViewController.clickVirtualKey(KeyEventCode.Right);
+        await virtualKeyboardViewController.clickVirtualKey(KeyEventCode.Tab);
+        await virtualKeyboardViewController.clickVirtualKey(KeyEventCode.Enter);
+        await logController.waitForAdbLogToContain(
+            KeyEventCode.Enter.toString(),
+            logsContext,
+            app.client,
+        );
+
+        const adbLog = await logController.getAdbLog(logsContext);
+        expect(adbLog).toMatchSnapshot();
     });
 
     it('should pass accessibility validation in all contrast modes', async () => {
