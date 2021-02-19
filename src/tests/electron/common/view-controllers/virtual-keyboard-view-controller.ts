88a43d11857e56db237eed927e5f9a6dd554158b
John Alkire <john.alkire@microsoft.com>
2021-02-19 10:20:40 -0800

    Fix merge issue

--- src/tests/electron/common/view-controllers/virtual-keyboard-view-controller.ts
+++ src/tests/electron/common/view-controllers/virtual-keyboard-view-controller.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { KeyEventCode } from 'electron/platform/android/adb-wrapper';
-import { buildKeyboardButtonAutomationId } from 'electron/views/virtual-keyboard/virtual-keyboard-buttons';
+import { buildKeyboardButtonAutomationId } from 'electron/views/tab-stops/virtual-keyboard-buttons';
 import { getAutomationIdSelector } from 'tests/common/get-automation-id-selector';
 import { SpectronAsyncClient } from 'tests/electron/common/view-controllers/spectron-async-client';
 import { ViewController } from './view-controller';
