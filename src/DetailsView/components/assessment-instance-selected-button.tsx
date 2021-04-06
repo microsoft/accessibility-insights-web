cfa2b18e0e23be2e4ae6dd27a103e33c3d0db07d
Dan Bjorge <dan@dbjorge.net>
2021-04-06 13:34:54 -0400

    Adapt to undocumented breaking change

--- src/DetailsView/components/assessment-instance-selected-button.tsx
+++ src/DetailsView/components/assessment-instance-selected-button.tsx
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as classNames from 'classnames';
+import classNames from 'classnames';
 import { VisualizationType } from 'common/types/visualization-type';
 import { IconButton } from 'office-ui-fabric-react';
 import * as React from 'react';
--- src/reports/components/outcome-summary-bar.tsx
+++ src/reports/components/outcome-summary-bar.tsx
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as classNames from 'classnames';
+import classNames from 'classnames';
 import { NamedFC } from 'common/react/named-fc';
 import { kebabCase } from 'lodash';
 import * as React from 'react';
