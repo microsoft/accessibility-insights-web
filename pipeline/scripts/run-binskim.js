90b7c64370867126b4a91711b98e8f8157e14086
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2021-04-16 18:00:41 -0700

    Run BinSkim from NuGet package

---
+++ pipeline/scripts/run-binskim.js
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+const child_process = require('child_process');
+const fs = require('fs-extra');
+const path = require('path');
+
+const runBinSkim = async () => {
+    const buildDir = process.env['Agent.BuildDirectory'];
+    const symbolsPath = path.resolve('electron-symbols');
+
+    fs.readdir(buildDir, (error, files) => {
+        if (error) {
+            console.log(error);
+        } else {
+            let foundFile = false;
+
+            files.forEach(file => {
+                if (file.startsWith('Microsoft.CodeAnalysis.BinSkim')) {
+                    console.log(`found ${file}`);
+                    const exe = path.resolve(
+                        buildDir,
+                        file,
+                        'tools',
+                        'netcoreapp3.1',
+                        'win-x64',
+                        'BinSkim.exe',
+                    );
+                    console.log(`exe = ${exe}`);
+                    if (fs.existsSync(exe)) {
+                        foundFile = true;
+                        child_process.execFileSync(exe, ['analyze', symbolsPath, '--recurse'], {
+                            stdio: ['pipe', process.stdout, process.stderr],
+                        });
+                    }
+                }
+            });
+
+            if (!foundFile) {
+                throw new Error('Could not find BinSkim.exe');
+            }
+        }
+    });
+};
+
+runBinSkim().catch(err => {
+    console.error(err);
+    process.exit(1);
+});
