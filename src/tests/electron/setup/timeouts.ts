193c199284894e97aa6b69be3af63594520cce4c
Dan Bjorge <danielbj@microsoft.com>
2021-05-27 15:45:35 -0400

    Move timeout scaleFactor to web e2es

--- src/tests/electron/setup/timeouts.ts
+++ src/tests/electron/setup/timeouts.ts
@@ -1,30 +1,24 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import os from 'os';
-import process from 'process';
-
-// See https://github.com/microsoft/accessibility-insights-web/issues/4271
-const isMacCIAgent = process.env['CI'] != null && os.platform() === 'darwin';
-const scaleFactor = isMacCIAgent ? 1.5 : 1;
 
 // How long to allow an entire test to execute, including startup/setup
-export const DEFAULT_ELECTRON_TEST_TIMEOUT_MS = 60000 * scaleFactor;
+export const DEFAULT_ELECTRON_TEST_TIMEOUT_MS = 60000;
 
 // These govern how long to allow connection to the test app to take
 // The product of these should be significantly less than the test timeout
-export const DEFAULT_APP_CONNECT_TIMEOUT_MS = 10000 * scaleFactor;
+export const DEFAULT_APP_CONNECT_TIMEOUT_MS = 10000;
 export const DEFAULT_APP_CONNECT_RETRIES = 3;
 
 // These govern how long Spectron should allow for ChromeDriver to start
 // The product of these should be significantly less than the test timeout
-export const DEFAULT_CHROMEDRIVER_START_TIMEOUT_MS = 5000 * scaleFactor;
+export const DEFAULT_CHROMEDRIVER_START_TIMEOUT_MS = 5000;
 export const DEFAULT_CHROMEDRIVER_START_RETRIES = 3;
 
 // How long to wait for an element to be visible
-export const DEFAULT_WAIT_FOR_ELEMENT_TO_BE_VISIBLE_TIMEOUT_MS = 5000 * scaleFactor;
+export const DEFAULT_WAIT_FOR_ELEMENT_TO_BE_VISIBLE_TIMEOUT_MS = 5000;
 
 // How long of a wait to artificially inject between element hover/mousedown/mouseup during clicks
-export const DEFAULT_CLICK_HOVER_DELAY_MS = 100 * scaleFactor;
+export const DEFAULT_CLICK_HOVER_DELAY_MS = 100;
 
 // How long to wait for a mock adb or service log to contain a value
-export const DEFAULT_WAIT_FOR_LOG_TIMEOUT_MS = 1500 * scaleFactor;
+export const DEFAULT_WAIT_FOR_LOG_TIMEOUT_MS = 1500;
--- src/tests/end-to-end/common/timeouts.ts
+++ src/tests/end-to-end/common/timeouts.ts
@@ -1,6 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import os from 'os';
+import process from 'process';
+
+// See https://github.com/microsoft/accessibility-insights-web/issues/4271
+const isMacCIAgent = process.env['CI'] != null && os.platform() === 'darwin';
+const scaleFactor = isMacCIAgent ? 1.5 : 1;
+
 // How long to allow an entire test to execute
 //
 // We want this to be greater than default timeout of Playwright's waitFor* functions (30s),
@@ -8,24 +15,24 @@
 // more actionable than test failures of the form "such and such test timed out".
 //
 // Every other default timeout should be lower than this!
-export const DEFAULT_E2E_TEST_TIMEOUT_MS: number = 30000;
+export const DEFAULT_E2E_TEST_TIMEOUT_MS: number = 30000 * scaleFactor;
 
 // How long to wait for a new browser instance to initialize.
-export const DEFAULT_BROWSER_LAUNCH_TIMEOUT_MS = 15000;
+export const DEFAULT_BROWSER_LAUNCH_TIMEOUT_MS = 15000 * scaleFactor;
 
 // How long to wait for an existing page to stop churning in response to a UI action
-export const DEFAULT_PAGE_ELEMENT_WAIT_TIMEOUT_MS = 5000;
+export const DEFAULT_PAGE_ELEMENT_WAIT_TIMEOUT_MS = 5000 * scaleFactor;
 
 // How long to wait for an operation that involves scanning the target page
-export const DEFAULT_TARGET_PAGE_SCAN_TIMEOUT_MS = 15000;
+export const DEFAULT_TARGET_PAGE_SCAN_TIMEOUT_MS = 15000 * scaleFactor;
 
 // How long to wait for a new page to load in response to the UI action that launched it
-export const DEFAULT_NEW_PAGE_WAIT_TIMEOUT_MS = 5000;
+export const DEFAULT_NEW_PAGE_WAIT_TIMEOUT_MS = 5000 * scaleFactor;
 
 // How long of a wait to artificially inject between element hover/mousedown/mouseup during clicks
-export const DEFAULT_CLICK_HOVER_DELAY_MS = 100;
-export const DEFAULT_CLICK_MOUSEUP_DELAY_MS = 50;
+export const DEFAULT_CLICK_HOVER_DELAY_MS = 100 * scaleFactor;
+export const DEFAULT_CLICK_MOUSEUP_DELAY_MS = 50 * scaleFactor;
 
 // How long to allow screenshotOnError to take (we don't want to allow arbitrarily long because the
 // error stacks are better if an individual test operation fails than if the whole test times out)
-export const DEFAULT_SCREENSHOT_TIMEOUT_MS = 5000;
+export const DEFAULT_SCREENSHOT_TIMEOUT_MS = 5000 * scaleFactor;
