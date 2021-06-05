f2af855708048876f85509877e8f975385f9c5f1
Dan Bjorge <danielbj@microsoft.com>
2021-06-04 21:33:11 -0400

    Refine chunking process and apply to unified e2es too

--- src/tests/common/chunk-tests.js
+++ src/tests/common/chunk-tests.js
@@ -2,24 +2,25 @@
 // Licensed under the MIT License.
 
 // Usage:
-// > yarn test --listTests | node chunk-tests.js --index 0 --total 2
-// ::set-output name=test_match::...
+// > yarn test --listTests | node chunk-tests.js --index 2 --total 2
+// path/to/test1.ts|path/to/test2.ts
 
-const fs = require('fs');
-const process = require('process');
+const { execSync } = require('child_process');
 const { chunk } = require('lodash');
 const yargs = require('yargs');
-const { index, total, testListFile } = yargs.demandOption(['index', 'total', 'testListFile']).argv;
 
-const testFilePaths = fs
-    .readFileSync(testListFile, 'utf-8')
+const { index, total, scenario } = yargs.demandOption(['scenario', 'index', 'total']).argv;
+
+const listTestsOutput = execSync(`yarn test:${scenario} --listTests`);
+
+const testFilePaths = listTestsOutput
     .toString()
     .split(/\r?\n/)
     .sort()
     .filter(path => /\.test\./.test(path)); // filter extra yarn/jest output
 
 const filesPerChunk = Math.ceil(testFilePaths.length / total);
-const selectedFilePaths = chunk(testFilePaths, filesPerChunk)[index];
+const selectedFilePaths = chunk(testFilePaths, filesPerChunk)[index - 1];
 
 const pathRegexes = selectedFilePaths.map(path => path.replace(/\\/g, '/'));
 const testMatch = pathRegexes.join('|');
