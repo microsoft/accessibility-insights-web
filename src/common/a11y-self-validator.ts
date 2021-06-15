619776be8e6c5f166406739a130930af1f6552b5
Dan Bjorge <danielbj@microsoft.com>
2021-06-15 15:11:36 -0400

    Set up a11y-self-validator to handle needs review

--- src/common/a11y-self-validator.ts
+++ src/common/a11y-self-validator.ts
@@ -1,5 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { IssuesAdHocVisualization } from 'ad-hoc-visualizations/issues/visualization';
+import { NeedsReviewAdHocVisualization } from 'ad-hoc-visualizations/needs-review/visualization';
+import { Analyzer, RuleAnalyzerConfiguration } from 'injected/analyzers/analyzer';
+import { AnalyzerProvider } from 'injected/analyzers/analyzer-provider';
 import { ScannerUtils } from '../injected/scanner-utils';
 import { ScanResults } from '../scanner/iruleresults';
 import { HTMLElementUtils } from './html-element-utils';
@@ -15,34 +19,46 @@ export interface LoggedNode {
     target: string[];
     domElement: HTMLElement;
     all: FormattedCheckResult[];
-    // tslint:disable-next-line: no-reserved-keywords
     any: FormattedCheckResult[];
     none: FormattedCheckResult[];
 }
 
-export class A11YSelfValidator {
+export type SelfValidatorAnalyzerDeps = {
+    scannerUtils: ScannerUtils;
+    htmlUtils: HTMLElementUtils;
+    logger: Logger;
+};
+
+type ResultType = 'incomplete' | 'violations';
+
+class SelfValidatorAnalyzer implements Analyzer {
     constructor(
-        private scannerUtils: ScannerUtils,
-        private docUtils: HTMLElementUtils,
-        private logger: Logger,
+        private readonly deps: SelfValidatorAnalyzerDeps,
+        private readonly config: RuleAnalyzerConfiguration,
+        private readonly relevantResultTypes: ResultType[],
     ) {}
 
-    public validate(): void {
-        this.scannerUtils.scan(null, this.logAxeResults);
+    analyze(): void {
+        const scanOptions = { testsToRun: this.config.rules };
+        this.deps.scannerUtils.scan(scanOptions, this.onScanComplete);
     }
 
-    private logAxeResults = (axeResults: ScanResults): void => {
-        const violations = axeResults.violations;
-        const loggedViolations: LoggedRule[] = [];
+    teardown(): void {}
 
-        if (violations && violations.length > 0) {
-            violations.forEach(violation => {
+    onScanComplete = (axeResults: ScanResults): void => {
+        for (const resultType of this.relevantResultTypes) {
+            const results = axeResults[resultType] ?? [];
+            const loggedViolations: LoggedRule[] = [];
+
+            for (const result of results) {
                 loggedViolations.push({
-                    id: violation.id,
-                    description: violation.description,
-                    nodes: violation.nodes.map(node => {
+                    id: result.id,
+                    description: result.description,
+                    nodes: result.nodes.map(node => {
                         return {
-                            domElement: this.docUtils.querySelector(node.target[0]) as HTMLElement,
+                            domElement: this.deps.htmlUtils.querySelector(
+                                node.target[0],
+                            ) as HTMLElement,
                             all: node.all,
                             any: node.any,
                             none: node.none,
@@ -50,9 +66,46 @@ export class A11YSelfValidator {
                         } as LoggedNode;
                     }),
                 });
-            });
-        }
+            }
 
-        this.logger.log(loggedViolations);
+            this.deps.logger.log(resultType);
+            this.deps.logger.log(loggedViolations);
+        }
     };
+}
+
+class SelfValidatorAnalyzerProvider extends AnalyzerProvider {
+    constructor(private readonly deps: SelfValidatorAnalyzerDeps) {
+        super(null, null, null, null, null, null, null, null, null, null, null);
+    }
+
+    public override createRuleAnalyzerUnifiedScanForNeedsReview(
+        config: RuleAnalyzerConfiguration,
+    ): Analyzer {
+        return new SelfValidatorAnalyzer(this.deps, config, ['violations', 'incomplete']);
+    }
+
+    public override createRuleAnalyzerUnifiedScan(config: RuleAnalyzerConfiguration): Analyzer {
+        return new SelfValidatorAnalyzer(this.deps, config, ['violations']);
+    }
+}
+
+export class A11YSelfValidator {
+    private readonly analyzerProvider: AnalyzerProvider;
+
+    constructor(scannerUtils: ScannerUtils, htmlUtils: HTMLElementUtils, logger: Logger) {
+        this.analyzerProvider = new SelfValidatorAnalyzerProvider({
+            scannerUtils,
+            htmlUtils,
+            logger,
+        });
+    }
+
+    public automatedChecks(): void {
+        IssuesAdHocVisualization.getAnalyzer(this.analyzerProvider).analyze();
+    }
+
+    public needsReview(): void {
+        NeedsReviewAdHocVisualization.getAnalyzer(this.analyzerProvider).analyze();
+    }
 }
