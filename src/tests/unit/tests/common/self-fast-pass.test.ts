73f3a9622eb6386224677bb76de851ed9e8503e8
Dan Bjorge <danielbj@microsoft.com>
2021-06-16 15:21:47 -0400

    Add unit tests for needsReview

--- src/tests/unit/tests/common/self-fast-pass.test.ts
+++ src/tests/unit/tests/common/self-fast-pass.test.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { IssuesAdHocVisualization } from 'ad-hoc-visualizations/issues/visualization';
 import { HTMLElementUtils } from 'common/html-element-utils';
 import { Logger } from 'common/logging/logger';
 import { SelfFastPass, LoggedNode, LoggedRule } from 'common/self-fast-pass';
@@ -16,9 +15,14 @@ describe('SelfFastPass', () => {
     let testObject: SelfFastPass;
 
     const failedSelectors: string[] = ['failed-div1', 'failed-div2'];
+    const incompleteSelectors: string[] = ['incomplete-div1', 'incomplete-div2'];
     const failedDomElements: DictionaryStringTo<string> = {
         [failedSelectors[0]]: failedSelectors[0] + 'element',
         [failedSelectors[1]]: failedSelectors[1] + 'element',
+    };
+    const incompleteDomElements: DictionaryStringTo<string> = {
+        [incompleteSelectors[0]]: incompleteSelectors[0] + 'element',
+        [incompleteSelectors[1]]: incompleteSelectors[1] + 'element',
     };
 
     beforeEach(() => {
@@ -37,12 +41,7 @@ describe('SelfFastPass', () => {
         const scannerResultStub = getAxeScanResult();
 
         scannerUtilsMock
-            .setup(ksu =>
-                ksu.scan(
-                    { testsToRun: null },
-                    It.is(param => typeof param === 'function'),
-                ),
-            )
+            .setup(ksu => ksu.scan({ testsToRun: null }, It.isAny()))
             .callback((rules, handleAxeResult) => {
                 handleAxeResult(scannerResultStub);
             })
@@ -66,10 +65,60 @@ describe('SelfFastPass', () => {
         loggerMock.verifyAll();
     });
 
+    test('needsReview', () => {
+        const scannerResultStub = getAxeScanResult();
+
+        scannerUtilsMock
+            .setup(ksu =>
+                ksu.scan(
+                    {
+                        testsToRun: [
+                            'aria-input-field-name',
+                            'color-contrast',
+                            'th-has-data-cells',
+                            'link-in-text-block',
+                        ],
+                    },
+                    It.isAny(),
+                ),
+            )
+            .callback((rules, handleAxeResult) => {
+                handleAxeResult(scannerResultStub);
+            })
+            .verifiable(Times.once());
+
+        failedSelectors.forEach(selector => {
+            htmlElementUtilsMock
+                .setup(utils => utils.querySelector(selector))
+                .returns(() => failedDomElements[selector] as any)
+                .verifiable(Times.once());
+        });
+        incompleteSelectors.forEach(selector => {
+            htmlElementUtilsMock
+                .setup(utils => utils.querySelector(selector))
+                .returns(() => incompleteDomElements[selector] as any)
+                .verifiable(Times.once());
+        });
+
+        loggerMock
+            .setup(logger => logger.log(It.isValue(getLoggedViolationScanResult())))
+            .verifiable(Times.once());
+        loggerMock
+            .setup(logger => logger.log(It.isValue(getLoggedIncompleteScanResult())))
+            .verifiable(Times.once());
+
+        testObject.needsReview();
+
+        scannerUtilsMock.verifyAll();
+        htmlElementUtilsMock.verifyAll();
+        loggerMock.verifyAll();
+    });
+
     function getAxeScanResult(): ScanResults {
         return {
             passes: getPassScanResult(),
             violations: getViolationScanResult(),
+            incomplete: getIncompleteScanResult(),
         } as ScanResults;
     }
 
@@ -109,6 +158,29 @@ describe('SelfFastPass', () => {
         return violationRules;
     }
 
+    function getIncompleteScanResult(): AxeRule[] {
+        const incompleteRules: AxeRule[] = [];
+
+        incompleteSelectors.forEach((incompleteSelector, index) => {
+            incompleteRules.push({
+                id: 'incomplete-rule' + index,
+                help: 'help content',
+                nodes: [
+                    {
+                        target: [incompleteSelector],
+                        all: [],
+                        any: null,
+                        none: [],
+                        failureSummary: 'failure summary',
+                        html: 'html data',
+                    },
+                ],
+            } as AxeRule);
+        });
+
+        return incompleteRules;
+    }
+
     function getLoggedViolationScanResult(): LoggedRule[] {
         const violationRules = getViolationScanResult();
         const loggedViolationRules: LoggedRule[] = [];
@@ -130,5 +202,28 @@ describe('SelfFastPass', () => {
         });
 
         return loggedViolationRules;
+    }
+
+    function getLoggedIncompleteScanResult(): LoggedRule[] {
+        const incompleteRules = getIncompleteScanResult();
+        const loggedIncompleteRules: LoggedRule[] = [];
+
+        incompleteRules.forEach(rule => {
+            loggedIncompleteRules.push({
+                id: rule.id,
+                description: rule.description,
+                nodes: rule.nodes.map(node => {
+                    return {
+                        all: node.all,
+                        any: node.any,
+                        none: node.none,
+                        target: node.target,
+                        domElement: incompleteDomElements[node.target[0]] as any,
+                    } as LoggedNode;
+                }),
+            });
+        });
+
+        return loggedIncompleteRules;
     }
 });
