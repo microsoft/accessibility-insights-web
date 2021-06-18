52353ef8bdc789e3f329ef191b1bc9d4945d40f1
John Alkire <john.alkire@microsoft.com>
2021-06-18 13:22:47 -0700

    Fix list structure in fix-instruction-processor

--- src/common/components/fix-instruction-processor.tsx
+++ src/common/components/fix-instruction-processor.tsx
@@ -26,37 +26,45 @@ export class FixInstructionProcessor {
     );
 
     private readonly foregroundRecommendedColorText = 'Use foreground color: ';
+    // eslint-disable-next-line security/detect-non-literal-regexp
     private readonly foregroundRecommendedRegExp = new RegExp(
         `${this.foregroundRecommendedColorText}${this.colorValueMatcher}`,
         'i',
     );
     private readonly backgroundRecommendedColorText = 'Use background color: ';
+    // eslint-disable-next-line security/detect-non-literal-regexp
     private readonly backgroundRecommendedRegExp = new RegExp(
         `${this.backgroundRecommendedColorText}${this.colorValueMatcher}`,
         'i',
     );
 
     private readonly contrastRatioText = 'Expected contrast ratio of ';
+    // eslint-disable-next-line security/detect-non-literal-regexp
     private readonly contrastRatioRegExp = new RegExp(`${this.contrastRatioText}`, 'i');
     private readonly originalMiddleSentence = ' and the original foreground color: ';
 
-    private readonly recommendEndSentence = ' to meet a contrast ratio of #.##:1';
-
-    public process(fixInstruction: string, recommendation: RecommendColor): JSX.Element {
+    public process(fixInstruction: string, recommendColor: RecommendColor): JSX.Element {
         const matches = this.getColorMatches(fixInstruction);
 
+        let recommendationSentences: string[] = [];
         if (matches.length === 2 && fixInstruction != null) {
-            fixInstruction = this.getColorRecommendation(fixInstruction, matches, recommendation);
+            recommendationSentences = this.getColorRecommendations(
+                fixInstruction,
+                matches,
+                recommendColor,
+            );
         }
-        return this.splitFixInstruction(fixInstruction, matches);
+        return this.splitFixInstruction(fixInstruction, recommendationSentences, matches);
     }
 
-    private getColorRecommendation(
+    private getColorRecommendations(
         fixInstruction: string,
         matches: ColorMatch[],
-        recommendation: RecommendColor,
+        recommendColor: RecommendColor,
     ) {
+        const sentenceDivider = '. ';
         let contrastRatio: number = 4.5;
+
         const regularExpExpectation = this.contrastRatioRegExp.exec(fixInstruction);
         if (regularExpExpectation != null) {
             const indexContrast = regularExpExpectation.index;
@@ -66,16 +74,21 @@ export class FixInstructionProcessor {
             contrastRatio = parseFloat(contrastIndex.substring(0, contrastIndex.indexOf(':')));
         }
 
-        if (recommendation.sentence !== 'Color suggestion text') {
-            fixInstruction += recommendation.getRecommendColor(
-                matches[0].colorHexValue,
-                matches[1].colorHexValue,
-                contrastRatio,
-            );
-            this.getRecommendedColorMatches(fixInstruction, matches);
+        const recommendSentence = recommendColor.getRecommendColor(
+            matches[0].colorHexValue,
+            matches[1].colorHexValue,
+            contrastRatio,
+        );
+
+        const recommendationSentences = recommendSentence.split(sentenceDivider);
+
+        this.getRecommendedColorMatches(recommendationSentences[0], matches);
+        if (recommendationSentences.length === 1) {
+            return recommendationSentences;
         }
 
-        return fixInstruction;
+        this.getRecommendedColorMatches(recommendationSentences[1], matches);
+        return recommendationSentences;
     }
 
     private getColorMatches(fixInstruction: string): ColorMatch[] {
@@ -143,130 +156,80 @@ export class FixInstructionProcessor {
         };
     }
 
-    private splitFixInstruction(fixInstruction: string, matches: ColorMatch[]): JSX.Element {
-        const sortedMatches = matches.sort((a, b) => a.splitIndex - b.splitIndex);
-        if (sortedMatches.length === 0) {
+    private splitFixInstruction(
+        fixInstruction: string,
+        recommendationStrings: string[],
+        matches: ColorMatch[],
+    ): JSX.Element {
+        if (matches.length === 0) {
             return <>{fixInstruction}</>;
         }
 
-        const insertionIndex = 0;
-        const keyIndex = 0;
+        const recommendations: JSX.Element[][] = [];
+        const howToFixMatches = matches.slice(0, 2);
+        const recommendationMatches = matches.slice(2);
 
-        const result: JSX.Element[] = [];
-        if (matches.length >= 2) {
-            this.bulletAndColorBoxOutput(
-                sortedMatches,
-                fixInstruction,
-                insertionIndex,
-                result,
-                keyIndex,
+        const results = this.getInstructionWithBoxes(howToFixMatches, fixInstruction);
+
+        if (recommendationStrings.length === 0) {
+            return <>{results}</>;
+        }
+
+        const recommendationOne = this.getInstructionWithBoxes(
+            recommendationMatches.slice(0, 2),
+            recommendationStrings[0],
+        );
+
+        recommendations.push(recommendationOne);
+
+        if (recommendationStrings.length > 1) {
+            const recommendationTwo = this.getInstructionWithBoxes(
+                recommendationMatches.slice(2),
+                recommendationStrings[1],
             );
+            recommendations.push(recommendationTwo);
         }
 
-        return (
-            <>
-                <span aria-hidden="true">{result}</span>
-                <span className={styles.screenReaderOnly}>{fixInstruction}</span>
-            </>
+        this.addRecommendationsToResults(results, recommendations);
+
+        return <>{results}</>;
+    }
+
+    private addRecommendationsToResults(results: JSX.Element[], recommendations: JSX.Element[][]) {
+        results.push(
+            <ul key="recommendations-list">
+                {recommendations.map((rec, idx) => (
+                    <li key={`recommendation-${idx}`}>{rec}</li>
+                ))}
+            </ul>,
         );
     }
 
-    private bulletAndColorBoxOutput(
-        sortedMatches: ColorMatch[],
-        fixInstruction: string,
-        insertionIndex: number,
-        result: JSX.Element[],
-        keyIndex: number,
-    ) {
-        for (let i: number = 0; i < sortedMatches.length; i++) {
-            const match: ColorMatch = sortedMatches[i];
-            if (i === 0) {
-                insertionIndex = 0;
-            } else {
-                insertionIndex =
-                    match.splitIndex -
-                    this.foregroundRecommendedColorText.length -
-                    match.colorHexValue.length -
-                    1;
-                if (i > 2) {
-                    insertionIndex -= 3;
-                }
-            }
-            let colorEndIndex = match.splitIndex - match.colorHexValue.length;
-            const beforeColorBox = fixInstruction.substring(insertionIndex, colorEndIndex);
-            insertionIndex = colorEndIndex;
-            i++;
-            colorEndIndex = sortedMatches[i].splitIndex - match.colorHexValue.length;
-            const middleSubstring = fixInstruction.substring(insertionIndex, colorEndIndex);
-            insertionIndex = colorEndIndex;
-            let endSubstring = '';
-            if (i === 1) {
-                endSubstring = fixInstruction.substring(
-                    insertionIndex,
-                    fixInstruction.indexOf(this.contrastRatioText) +
-                        this.contrastRatioText.length +
-                        6,
-                );
-                result.push(
-                    <span key={`instruction-split-${keyIndex++}`}>
-                        {this.addBulletPoint(
-                            keyIndex,
-                            beforeColorBox,
-                            match,
-                            middleSubstring,
-                            sortedMatches,
-                            i,
-                            endSubstring,
-                        )}
-                    </span>,
-                );
-            } else {
-                colorEndIndex = sortedMatches[i].splitIndex + this.recommendEndSentence.length;
-                endSubstring = fixInstruction.substring(insertionIndex, colorEndIndex);
-                result.push(
-                    <span key={`instruction-split-${keyIndex++}`}>
-                        <ul key={`instruction-split-${keyIndex++}`}>
-                            <li key={`instruction-split-${keyIndex++}`}>
-                                {this.addBulletPoint(
-                                    keyIndex,
-                                    beforeColorBox,
-                                    match,
-                                    middleSubstring,
-                                    sortedMatches,
-                                    i,
-                                    endSubstring,
-                                )}
-                            </li>
-                        </ul>
-                    </span>,
-                );
-            }
-        }
-    }
+    private getInstructionWithBoxes(matches: ColorMatch[], fixInstruction: string) {
+        let insertionIndex = 0;
+        let keyIndex = 0;
+        const result: JSX.Element[] = [];
 
-    private addBulletPoint(
-        keyIndex: number,
-        beforeColorBox: string,
-        match: ColorMatch,
-        middleSubstring: string,
-        sortedMatches: ColorMatch[],
-        i: number,
-        endSubstring: string,
-    ): JSX.Element {
-        return (
-            <span key={`instruction-split-${keyIndex++}`}>
-                {beforeColorBox}
-                {this.createColorBox(match.colorHexValue, keyIndex++)}
-                {middleSubstring}
-                {this.createColorBox(sortedMatches[i].colorHexValue, keyIndex++)}
-                {endSubstring}
-            </span>
-        );
+        matches.forEach(match => {
+            const endIndex = match.splitIndex - match.colorHexValue.length;
+            const substring = fixInstruction.substring(insertionIndex, endIndex);
+
+            result.push(<span key={`instruction-split-${keyIndex++}`}>{substring}</span>);
+            result.push(this.createColorBox(match.colorHexValue, keyIndex++));
+
+            insertionIndex = endIndex;
+        });
+
+        const coda = fixInstruction.substr(insertionIndex);
+
+        result.push(<span key={`instruction-split-${keyIndex++}`}>{coda}</span>);
+        return result;
     }
 
     private createColorBox(colorHexValue: string, keyIndex: number): JSX.Element {
         return (
             <span
+                aria-hidden={true}
                 key={`instruction-split-${keyIndex}`}
                 className={styles.fixInstructionColorBox}
                 style={{ backgroundColor: colorHexValue }}
