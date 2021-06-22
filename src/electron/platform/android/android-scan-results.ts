939a8213f5a7057ffcb275b45ae86d784a4499d0
Lisa Li <lisli@microsoft.com>
2021-06-22 14:35:03 -0700

    update AndroidScanResults to handle v2 result data

--- src/electron/platform/android/android-scan-results.ts
+++ src/electron/platform/android/android-scan-results.ts
@@ -37,36 +37,40 @@ export class AndroidScanResults {
     constructor(readonly rawData: any) {}
 
     public get deviceInfo(): DeviceInfo | null {
-        return this.rawData?.axeContext?.axeDevice || null;
+        return this.getAxeResults()?.axeContext?.axeDevice || null;
     }
 
     public get deviceName(): string | null {
-        return this.rawData?.axeContext?.axeDevice?.name || null;
+        return this.getAxeResults()?.axeContext?.axeDevice?.name || null;
     }
 
     public get appIdentifier(): string | null {
-        return this.rawData?.axeContext?.axeMetaData?.appIdentifier || null;
+        return this.getAxeResults()?.axeContext?.axeMetaData?.appIdentifier || null;
     }
 
     public get viewElementTree(): ViewElementData | null {
-        return this.rawData?.axeContext?.axeView || null;
+        return this.getAxeResults()?.axeContext?.axeView || null;
     }
 
     public get ruleResults(): RuleResultsData[] {
-        return this.rawData?.axeRuleResults || [];
+        return this.getAxeResults()?.axeRuleResults || [];
     }
 
     public get axeVersion(): string {
-        return this.rawData?.axeContext?.axeMetaData?.axeVersion || 'no-version';
+        return this.getAxeResults()?.axeContext?.axeMetaData?.axeVersion || 'no-version';
     }
 
     public get analysisTimestamp(): string | null {
-        return this.rawData?.axeContext?.axeMetaData?.analysisTimestamp || null;
+        return this.getAxeResults()?.axeContext?.axeMetaData?.analysisTimestamp || null;
     }
 
     public get screenshot(): ScreenshotData | null {
-        const screenshot = this.rawData?.axeContext?.screenshot;
+        const screenshot = this.getAxeResults()?.axeContext?.screenshot;
 
         return screenshot ? { base64PngData: screenshot } : null;
+    }
+
+    private getAxeResults(): any {
+        return this.rawData?.AxeResults ?? this.rawData;
     }
 }
