9545007795926878ced2bcf6ac7bdfbaa90eba17
Lisa Li <lisli@microsoft.com>
2021-06-22 22:05:23 -0700

    use feature flag to determine which api to call in fetch-scan-results.ts

--- src/electron/platform/android/fetch-scan-results.ts
+++ src/electron/platform/android/fetch-scan-results.ts
@@ -1,15 +1,28 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import axios from 'axios';
+import { FeatureFlagStore } from 'background/stores/global/feature-flag-store';
+import { UnifiedFeatureFlags } from 'electron/common/unified-feature-flags';
 import { AndroidScanResults } from './android-scan-results';
 
 export type ScanResultsFetcher = (port: number) => Promise<AndroidScanResults>;
 
 export type HttpGet = typeof axios.get;
 
-export const createScanResultsFetcher = (httpGet: HttpGet): ScanResultsFetcher => {
+export const createScanResultsFetcher = (
+    httpGet: HttpGet,
+    featureFlagStore: FeatureFlagStore,
+): ScanResultsFetcher => {
     return async (port: number) => {
-        const response = await httpGet(`http://localhost:${port}/AccessibilityInsights/result`);
+        const featureFlagStoreData = featureFlagStore.getState();
+        let resultsVersion = ''; // API for v1 results doesn't have an explicit version number
+        if (featureFlagStoreData[UnifiedFeatureFlags.atfaResults]) {
+            resultsVersion = '_v2';
+        }
+
+        const response = await httpGet(
+            `http://localhost:${port}/AccessibilityInsights/result${resultsVersion}`,
+        );
         return new AndroidScanResults(response.data);
     };
 };
