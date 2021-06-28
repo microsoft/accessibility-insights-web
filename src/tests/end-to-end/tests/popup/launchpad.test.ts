df8d077fbb1a998b606d61747417e16472cb40e5
Wahaj Abid <waabid@microsoft.com>
2021-06-28 16:13:15 -0700

    Revert "added a failing e2e test"
    This reverts commit d65075a73ba20351b2874e86d292474a23e75f11.

--- src/tests/end-to-end/tests/popup/launchpad.test.ts
+++ src/tests/end-to-end/tests/popup/launchpad.test.ts
@@ -40,7 +40,6 @@ describe('Popup -> Launch Pad', () => {
 
             const results = await scanForAccessibilityIssues(popupPage, '*');
             expect(results).toHaveLength(0);
-            expect(false).toBeTruthy();
         },
     );
 });
