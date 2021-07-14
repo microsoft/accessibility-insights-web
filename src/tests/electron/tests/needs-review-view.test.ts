55990747ce544dc3ec2cfb84f046db535f79ac00
Lisa Li <lisli@microsoft.com>
2021-07-14 10:28:40 -0700

    add back old test and try first half of new test

--- src/tests/electron/tests/needs-review-view.test.ts
+++ src/tests/electron/tests/needs-review-view.test.ts
@@ -38,18 +38,32 @@ describe('NeedsReviewView', () => {
         await app.waitForTitle('Accessibility Insights for Android - Needs review');
     });
 
+    it('displays needs review results with one failing result (results v1)', async () => {
+        app.setFeatureFlag(UnifiedFeatureFlags.atfaResults, false);
+        await openNeedsReview();
+        const cardsView = resultsViewController.createCardsViewController();
+        await cardsView.waitForRuleGroupCount(1);
+        expect(await cardsView.queryRuleGroupContents()).toHaveLength(0);
+        await cardsView.waitForHighlightBoxCount(1);
+
+        await cardsView.toggleRuleGroupAtPosition(1);
+        await cardsView.assertExpandedRuleGroup(1, 'ColorContrast', 1);
+
+        expect(await cardsView.queryRuleGroupContents()).toHaveLength(1);
+    });
+
     it('displays needs review results with 5 failing results (results_v2)', async () => {
         app.setFeatureFlag(UnifiedFeatureFlags.atfaResults, true);
         await openNeedsReview();
         const cardsView = resultsViewController.createCardsViewController();
         await cardsView.waitForRuleGroupCount(1);
         expect(await cardsView.queryRuleGroupContents()).toHaveLength(0);
-        await cardsView.waitForHighlightBoxCount(5);
+        // await cardsView.waitForHighlightBoxCount(5);
 
-        await cardsView.toggleRuleGroupAtPosition(1);
-        await cardsView.assertExpandedRuleGroup(1, 'TextContrastCheck', 5);
+        // await cardsView.toggleRuleGroupAtPosition(1);
+        // await cardsView.assertExpandedRuleGroup(1, 'TextContrastCheck', 5);
 
-        expect(await cardsView.queryRuleGroupContents()).toHaveLength(5);
+        // expect(await cardsView.queryRuleGroupContents()).toHaveLength(5);
     });
 
     it('should pass accessibility validation in all contrast modes', async () => {
