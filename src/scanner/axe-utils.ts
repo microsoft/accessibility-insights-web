4a2aefd61f9141d1818ddfcd4db48171d201aa04
Wahaj Abid <waabid@microsoft.com>
2021-08-16 16:57:11 -0700

    moving back to d.ts for module augmentaiton

--- src/scanner/axe-utils.ts
+++ src/scanner/axe-utils.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as axe from 'axe-core';
-import './axe-extension';
 
 import { DictionaryStringTo } from '../types/common-types';
 
