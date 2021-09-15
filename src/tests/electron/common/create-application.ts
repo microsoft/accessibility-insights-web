cf1d0081902641fdfab0b47d0ec105aa1406499e
Wahaj Abid <waabid@microsoft.com>
2021-09-15 02:08:08 -0700

    fix build issues

--- src/tests/electron/common/create-application.ts
+++ src/tests/electron/common/create-application.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as Electron from 'electron';
 import { _electron as electron } from 'playwright';
 import { AppController } from './view-controllers/app-controller';
 export interface AppOptions {
