8d8a67a174362647444b28a7c589d446cf3d028f
John Alkire <4615491+jalkire@users.noreply.github.com>
2021-09-30 15:26:08 -0700

    feat(download-report-json): Introduce ExportDropdown with toJSON option and use it in ExportDialog (#4754)
    #### Details
    In order to add the export to JSON option, this PR introduces `ExportDropdown` and uses it in the `ExportDialog`. When multiple export options are available, `ExportDropdown` replaces the ordinary Export button and provides a menu with a list of export options on click. Some other notes:
    - To address an accessibility concern, `ExportDropdown` replaces the split button in the current export to Codepen preview feature
    - `ExportDropdown` is modelled after `StartOverDropdown`
    - `ReportExportService` is refactored to apply to all forms of exporting--HTML, Codepen, and now JSON--rather than just Codepen. As such, `HtmlReportExportService` and `JsonReportExportService` are introduced and `CodepenReportExportService` is updated
    - The "to JSON" option does not yet do anything
    - All uses of the dropdown are currently behind feature flags
    
    Screenshot with relevant feature flags enabled:
    ![image](https://user-images.githubusercontent.com/4615491/135504281-c6e5d0f4-75bc-4636-b5a8-ec737335a45e.png)
    
    Screenshot without feature flags (same as prod):
    ![image](https://user-images.githubusercontent.com/4615491/135504603-3d95584d-0bf6-452a-ba22-a95706fe319a.png)
    
    ##### Motivation
    Necessary for feature
    
    ##### Context
    - I considered leaving `ReportExportService` as-is, but that wouldn't work well because the JSON export option is more similar to the HTML export option than it is to Codepen
    -  A future PR will remove the to JSON option from the Automated Checks export dropdown
    - A future PR will make the to JSON option download JSON
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a" in the checkbox -->
    - [n/a] Addresses an existing issue: #0000
    - [x] Ran `yarn fastpass`
    - [x] Added/updated relevant unit test(s) (and ran `yarn test`)
    - [x] Verified code coverage for the changes made. Check coverage report at: `<rootDir>/test-results/unit/coverage`
    - [x] PR title *AND* final merge commit title both start with a semantic tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`). See `CONTRIBUTING.md`.
    - [x] (UI changes only) Added screenshots/GIFs to description above
    - [x] (UI changes only) Verified usability with NVDA/JAWS

---
+++ src/report-export/services/json-report-export-service.ts
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import {
+    ReportExportService,
+    ReportExportServiceKey,
+} from 'report-export/types/report-export-service';
+
+const jsonReportExportServiceKey: ReportExportServiceKey = 'json';
+
+export const JsonReportExportService: ReportExportService = {
+    key: jsonReportExportServiceKey,
+    displayName: 'JSON',
+    generateMenuItem: (onMenuItemClick, _, __) => {
+        return {
+            key: jsonReportExportServiceKey,
+            name: 'as JSON',
+            onClick: (e: React.MouseEvent<HTMLButtonElement>) => {
+                onMenuItemClick(e, jsonReportExportServiceKey);
+            },
+        };
+    },
+};
