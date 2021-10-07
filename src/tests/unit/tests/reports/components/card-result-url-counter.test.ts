8130d78ba540b998b462428e1e14f9e9b036f956
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2021-10-06 21:39:08 -0700

    fix double comment that is confusing license check

--- src/tests/unit/tests/reports/components/card-result-url-counter.test.ts
+++ src/tests/unit/tests/reports/components/card-result-url-counter.test.ts
@@ -1,5 +1,5 @@
-// // Copyright (c) Microsoft Corporation. All rights reserved.
-// // Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 import { CardResult } from 'common/types/store-data/card-view-model';
 import { getCardResultUrlCount } from 'reports/components/card-result-url-counter';
