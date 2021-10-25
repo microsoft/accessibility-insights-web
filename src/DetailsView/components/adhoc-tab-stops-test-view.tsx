2fc6ace78012501adf77428fb26b06e451507f27
shanisebarona <shanisebarona@gmail.com>
2021-10-25 16:26:26 -0400

    add content to ADhocTabstopsTestView

--- src/DetailsView/components/adhoc-tab-stops-test-view.tsx
+++ src/DetailsView/components/adhoc-tab-stops-test-view.tsx
@@ -2,8 +2,8 @@
 // Licensed under the MIT License.
 
 import { NamedFC } from 'common/react/named-fc';
-import * as React from 'react';
 import { RequirementInstructions } from 'DetailsView/components/requirement-instructions';
+import * as React from 'react';
 
 export interface AdhocTabStopsTestViewProps {}
 
