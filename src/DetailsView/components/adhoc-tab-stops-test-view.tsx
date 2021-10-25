30c20d1503b89415c9cee901f51b45c1dd5cd9f6
shanisebarona <shanisebarona@gmail.com>
2021-10-25 15:20:44 -0400

    add content to AdhocTabStopsestView component

--- src/DetailsView/components/adhoc-tab-stops-test-view.tsx
+++ src/DetailsView/components/adhoc-tab-stops-test-view.tsx
@@ -3,12 +3,31 @@
 
 import { NamedFC } from 'common/react/named-fc';
 import * as React from 'react';
+import { RequirementInstructions } from 'DetailsView/components/requirement-instructions';
 
 export interface AdhocTabStopsTestViewProps {}
 
+const howToTest: JSX.Element = (
+    <ol>
+        <li>
+            Locate the visual helper on the target page, it will highlight element in focus with an
+            empty circle.
+        </li>
+        <li>
+            Use your keyboard to move input focus through all the interactive elements in the page:
+        </li>
+        <ol>
+            <li>Use Tab and Shift+Tab to navigate between standalone controls. </li>
+            <li>
+                Use the arrow keys to navigate between the focusable elements within a composite
+                control.
+            </li>
+        </ol>
+    </ol>
+);
 export const AdhocTabStopsTestView = NamedFC<AdhocTabStopsTestViewProps>(
     'AdhocTabStopsTestView',
     () => {
-        return <div></div>;
+        return <RequirementInstructions howToTest={howToTest} />;
     },
 );
