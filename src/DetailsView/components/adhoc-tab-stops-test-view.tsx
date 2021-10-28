dd5994f49ec177438249167a4d9f1f2d5b30f892
shanisebarona <shanisebarona@gmail.com>
2021-10-28 10:42:59 -0400

    add content to adhoc-tabstops

--- src/DetailsView/components/adhoc-tab-stops-test-view.tsx
+++ src/DetailsView/components/adhoc-tab-stops-test-view.tsx
@@ -3,31 +3,52 @@
 
 import { NamedFC } from 'common/react/named-fc';
 import { RequirementInstructions } from 'DetailsView/components/requirement-instructions';
+import * as Markup from '../../assessments/markup';
+import * as styles from 'DetailsView/components/static-content-common.scss';
 import * as React from 'react';
 
 export interface AdhocTabStopsTestViewProps {}
 
-const howToTest: JSX.Element = (
-    <ol>
-        <li>
-            Locate the visual helper on the target page, it will highlight element in focus with an
-            empty circle.
-        </li>
-        <li>
-            Use your keyboard to move input focus through all the interactive elements in the page:
-            <ol>
-                <li>Use Tab and Shift+Tab to navigate between standalone controls. </li>
-                <li>
-                    Use the arrow keys to navigate between the focusable elements within a composite
-                    control.
-                </li>
-            </ol>
-        </li>
-    </ol>
-);
 export const AdhocTabStopsTestView = NamedFC<AdhocTabStopsTestViewProps>(
     'AdhocTabStopsTestView',
-    () => {
-        return <RequirementInstructions howToTest={howToTest} />;
+    props => {
+        const title = <h1>Tab stops Step 2 of 3</h1>;
+
+        const description = (
+            <p>
+                <Markup.Emphasis>
+                    Note: this test requires you to use a keyboard and to visually identify
+                    interactive elements.
+                </Markup.Emphasis>
+            </p>
+        );
+
+        const howToTest: JSX.Element = (
+            <ol>
+                <li>
+                    Locate the visual helper on the target page, it will highlight element in focus
+                    with an empty circle.
+                </li>
+                <li>
+                    Use your keyboard to move input focus through all the interactive elements in
+                    the page:
+                    <ol>
+                        <li>Use Tab and Shift+Tab to navigate between standalone controls. </li>
+                        <li>
+                            Use the arrow keys to navigate between the focusable elements within a
+                            composite control.
+                        </li>
+                    </ol>
+                </li>
+            </ol>
+        );
+
+        return (
+            <div className={styles.staticContentInDetailsView}>
+                {title}
+                {description}
+                <RequirementInstructions howToTest={howToTest} />
+            </div>
+        );
     },
 );
