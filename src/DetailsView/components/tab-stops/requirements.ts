b90def6b7918a2e2b350d211b800c1aba0a064c1
Wahaj <32555133+waabid@users.noreply.github.com>
2021-10-27 17:18:19 -0700

    feat(tab-stops-details-view): add requirements content (#4837)
    * feat(tab-stops-details-view): add requirements content
    
    * add missing export statement

---
+++ src/DetailsView/components/tab-stops/requirements.ts
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { TabStopRequirementInfo } from 'types/tab-stop-requirement-info';
+
+export const requirements: TabStopRequirementInfo = {
+    'keyboard-navigation': {
+        name: 'Keyboard navigation',
+        description: 'All interactive elements can be reached using the Tab and arrow keys. ',
+    },
+    'keyboard-traps': {
+        name: 'Keyboard traps',
+        description:
+            'There are no interactive elements that “trap” input focus and prevent navigating away.',
+    },
+    'focus-indicator': {
+        name: 'Focus indicator',
+        description:
+            'All interactive elements give a visible indication when they have input focus.',
+    },
+    'tab-order': {
+        name: 'Tab order',
+        description:
+            "The tab order is consistent with the logical order that's communicated visually.",
+    },
+    'input-focus': {
+        name: 'Input focus',
+        description: 'Input focus does not move unexpectedly without the user initiating it.',
+    },
+};
