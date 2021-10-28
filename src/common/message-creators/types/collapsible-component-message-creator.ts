9ccf5293fed7d57ac1b348dd2be4fecb3df80f3c
Brock Taylor <13286385+brocktaylor7@users.noreply.github.com>
2021-10-28 16:38:26 -0700

    remove CollapsibleComponentMessageCreator

--- src/common/message-creators/types/collapsible-component-message-creator.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-export interface CollapsibleComponentMessageCreator {
-    toggleRuleExpandCollapse: (ruleId: string, event: React.SyntheticEvent) => void;
-}
