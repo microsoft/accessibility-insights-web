b844f6ebf9876b4a92d2d62488db4f6728a98833
SB <shanisebarona@gmail.com>
2021-11-02 17:24:02 -0400

    feat(tab-stops-details-view): add static tabstops content  (#4841)
    feat(tab-stops-details-view): add static tabstops content  (#4841)

--- src/DetailsView/components/adhoc-tab-stops-test-view.tsx
+++ src/DetailsView/components/adhoc-tab-stops-test-view.tsx
@@ -1,33 +1,72 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import { VisualizationConfiguration } from 'common/configs/visualization-configuration';
 import { NamedFC } from 'common/react/named-fc';
+import { FeatureFlagStoreData } from 'common/types/store-data/feature-flag-store-data';
+import { VisualizationType } from 'common/types/visualization-type';
 import { RequirementInstructions } from 'DetailsView/components/requirement-instructions';
+import * as styles from 'DetailsView/components/static-content-common.scss';
+import { createFastPassProviderWithFeatureFlags } from 'fast-pass/fast-pass-provider';
 import * as React from 'react';
+import * as Markup from '../../assessments/markup';
 
-export interface AdhocTabStopsTestViewProps {}
+export interface AdhocTabStopsTestViewProps {
+    configuration: VisualizationConfiguration;
+    featureFlagStoreData: FeatureFlagStoreData;
+    selectedTest: VisualizationType;
+}
 
-const howToTest: JSX.Element = (
-    <ol>
-        <li>
-            Locate the visual helper on the target page, it will highlight element in focus with an
-            empty circle.
-        </li>
-        <li>
-            Use your keyboard to move input focus through all the interactive elements in the page:
-            <ol>
-                <li>Use Tab and Shift+Tab to navigate between standalone controls. </li>
-                <li>
-                    Use the arrow keys to navigate between the focusable elements within a composite
-                    control.
-                </li>
-            </ol>
-        </li>
-    </ol>
-);
 export const AdhocTabStopsTestView = NamedFC<AdhocTabStopsTestViewProps>(
     'AdhocTabStopsTestView',
-    () => {
-        return <RequirementInstructions howToTest={howToTest} />;
+    props => {
+        const description = (
+            <p>
+                <Markup.Emphasis>
+                    Note: this test requires you to use a keyboard and to visually identify
+                    interactive elements.
+                </Markup.Emphasis>
+            </p>
+        );
+
+        const howToTest: JSX.Element = (
+            <ol>
+                <li>
+                    Locate the visual helper on the target page, it will highlight element in focus
+                    with an empty circle.
+                </li>
+                <li>
+                    Use your keyboard to move input focus through all the interactive elements in
+                    the page:
+                    <ol>
+                        <li>
+                            Use <Markup.Term>Tab</Markup.Term> and{' '}
+                            <Markup.Term>Shift+Tab</Markup.Term> to navigate between standalone
+                            controls.{' '}
+                        </li>
+                        <li>
+                            Use the arrow keys to navigate between the focusable elements within a
+                            composite control.
+                        </li>
+                    </ol>
+                </li>
+            </ol>
+        );
+
+        const selectedTest = props.selectedTest;
+        const displayableData = props.configuration.displayableData;
+        const fastPassProvider = createFastPassProviderWithFeatureFlags(props.featureFlagStoreData);
+        const stepsText = fastPassProvider.getStepsText(selectedTest);
+
+        return (
+            <div className={styles.staticContentInDetailsView}>
+                <h1>
+                    {displayableData.title}
+                    {` ${stepsText} `}
+                </h1>
+                {description}
+                <RequirementInstructions howToTest={howToTest} />
+            </div>
+        );
     },
 );
