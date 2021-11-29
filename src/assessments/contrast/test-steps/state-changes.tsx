a12b12f35e4b4e5335fe74f5c678848e3389eb38
Tania Martinez Villagomez <taniamar@microsoft.com>
2021-11-29 14:56:29 -0800

    Fixing accessibility issue in assessment 24.2

--- src/assessments/contrast/test-steps/state-changes.tsx
+++ src/assessments/contrast/test-steps/state-changes.tsx
@@ -59,28 +59,30 @@ const howToTest: JSX.Element = (
                 testing on a Mac) to verify that the following visual information, if present, has a
                 contrast ratio of at least 3:1 against the adjacent background:
                 <ol>
-                    <li>Any visual information that's needed to identify the component</li>
-                    <ol>
-                        <li>
-                            Visual information is almost always needed to identify text inputs,
-                            checkboxes, and radio buttons.
-                        </li>
-                        <li>
-                            Visual information might not be needed to identify other components if
-                            they are identified by their position, text style, or context.
-                        </li>
-                    </ol>
+                    <li>
+                        Any visual information that's needed to identify the component
+                        <ol>
+                            <li>
+                                Visual information is almost always needed to identify text inputs,
+                                checkboxes, and radio buttons.
+                            </li>
+                            <li>
+                                Visual information might not be needed to identify other components
+                                if they are identified by their position, text style, or context.
+                            </li>
+                        </ol>
+                    </li>
                     <li>Any visual information that indicates the component's current state</li>
                 </ol>
                 Exceptions:
                 <ol>
                     <li>
                         No minimum contrast ratio is required if either of the following is true:
+                        <ol>
+                            <li>The component is inactive/disabled.</li>
+                            <li>The component's appearance is determined solely by the browser.</li>
+                        </ol>
                     </li>
-                    <ol>
-                        <li>The component is inactive/disabled.</li>
-                        <li>The component's appearance is determined solely by the browser.</li>
-                    </ol>
                     <li>
                         If a component has redundant state indicators (such as unique background
                         color and unique text style), only one indicator is required to have
