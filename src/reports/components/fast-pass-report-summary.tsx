2e6565e470940dfe733e79ea8a3032906e1dbe18
shanisebarona <shanisebarona@gmail.com>
2022-01-06 11:12:07 -0500

    hardcoded outcome summary bar

--- src/reports/components/fast-pass-report-summary.tsx
+++ src/reports/components/fast-pass-report-summary.tsx
@@ -1,16 +1,26 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as React from 'react';
-import { FastPassOutcomeSummaryBar } from 'reports/components/fast-pass-outcome-summary-bar';
+import { OutcomeSummaryBar } from 'reports/components/outcome-summary-bar';
+import { OutcomeStats } from 'reports/components/outcome-type';
+import { allRequirementOutcomeTypes } from 'reports/components/requirement-outcome-type';
 
 export interface FastPassReportSummaryProps {}
 
 export class FastPassReportSummary extends React.Component<FastPassReportSummaryProps> {
     public render(): JSX.Element {
+
+        const stats: Partial<OutcomeStats> = {
+            pass: 5,
+            incomplete: 10,
+            fail: 1
+        }
+
         return (
             <>
-                <h2>Summary</h2>
-                <FastPassOutcomeSummaryBar />
+                <h2>Summary 2</h2>
+                <OutcomeSummaryBar outcomeStats={stats} allOutcomeTypes={allRequirementOutcomeTypes}                    
+                />
             </>
         );
     }
