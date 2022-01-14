661f83a119e14eb45d5840c1528ee6e03307239c
Dan Bjorge <danielbj@microsoft.com>
2022-01-14 15:03:40 -0500

    Use setupFilesAfterEnv to flush promises after each test

---
+++ src/tests/common/flush-promises-after-each-test.ts
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { tick } from 'tests/unit/common/tick';
+
+// This is here to force Jest to flush any pending resolved Promises that a test might
+// have forgotten to handle *before* Jest considers that test to be completed. That way,
+// if a test leaks a Promise, the resulting error message gets logged as a failure in the
+// responsible test and includes a nice, Jest-formatted stack, instead of test "passing"
+// and the error as a Node UnhandledProjmiseRejectionWarning with no stack or test context.
+//
+// See https://github.com/facebook/jest/issues/10784#issuecomment-988400890
+afterEach(tick);
