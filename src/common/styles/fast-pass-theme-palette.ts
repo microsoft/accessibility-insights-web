3b8096468111720a0a1c5e19c57497389cd939fd
Dan Bjorge <danielbj@microsoft.com>
2022-01-25 18:59:42 -0500

    Adjust customizer colors for consistency with existing collapsible highlight colors

--- src/common/styles/fast-pass-theme-palette.ts
+++ src/common/styles/fast-pass-theme-palette.ts
@@ -3,11 +3,6 @@
 import { IPartialTheme } from '@uifabric/styling';
 import { DefaultThemePalette } from './default-theme-palette';
 
-// From color-definitions.scss
-const neutral2 = '#f8f8f8';
-const neutral4 = '#f4f4f4';
-const neutral6 = '#f2f2f2';
-
 // This is a copy of our DefaultThemePalette slightly adjusted to account for the page background
 // of FastPass-like content views using our "neutral2" instead of our "white" for a background color
 // (because they use Card-like elements that want to keep "white" background colors for themselves).
@@ -15,8 +10,16 @@ export const FastPassThemePalette: IPartialTheme = {
     ...DefaultThemePalette,
     palette: {
         ...DefaultThemePalette.palette,
-        white: neutral2,
-        neutralLighterAlt: neutral4,
-        neutralLighter: neutral6,
+
+        white: '#f8f8f8', // $neutral-2 from color-definitions.scss
+
+        // In the default theme, these correspond to $neutral-2, $neutral-4, and $neutral-8
+        // To ensure these end up progressively darker than our adjusted $white, we apply
+        // alpha-based versions of the same neutrals over the adjusted $white
+        //
+        // You can use a tool like https://borderleft.com/toolbox/rgba to calculate the values
+        neutralLighterAlt: '#f3f3f3', // $neutral-alpha-2 over $neutral-2
+        neutralLighter: '#eeeeee', // $neutral-alpha-4 over $neutral-2
+        neutralLight: '#e4e4e4', // $neutral-alpha-8 over $neutral-2
     },
 };
