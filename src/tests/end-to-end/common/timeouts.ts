336348034f807d9ea3dcabf5c10ba25c4e286ff2
karan <karansin@microsoft.com>
2022-01-28 13:25:25 -0800

    Try increasing clickSelector timeout

--- src/tests/end-to-end/common/timeouts.ts
+++ src/tests/end-to-end/common/timeouts.ts
@@ -14,10 +14,10 @@ export const DEFAULT_E2E_TEST_TIMEOUT_MS: number = 30000;
 export const DEFAULT_BROWSER_LAUNCH_TIMEOUT_MS = 15000;
 
 // How long to wait for an existing page to stop churning in response to a UI action
-export const DEFAULT_PAGE_ELEMENT_WAIT_TIMEOUT_MS = 5000;
+export const DEFAULT_PAGE_ELEMENT_WAIT_TIMEOUT_MS = 10000;
 
 // How long to wait for an operation that involves scanning the target page
-export const DEFAULT_TARGET_PAGE_SCAN_TIMEOUT_MS = 30000;
+export const DEFAULT_TARGET_PAGE_SCAN_TIMEOUT_MS = 15000;
 
 // How long to wait for a new page to load in response to the UI action that launched it
 export const DEFAULT_NEW_PAGE_WAIT_TIMEOUT_MS = 5000;
