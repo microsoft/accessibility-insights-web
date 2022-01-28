d399adcfd6b069ef867b047e66968def1723c26f
Dan Bjorge <danielbj@microsoft.com>
2022-01-28 15:14:02 -0500

    Adapt to breaking changes, rewrite router tests

---
+++ src/views/insights/insights-router.tsx
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { NamedFC } from 'common/react/named-fc';
+import * as React from 'react';
+import { HashRouter, Route, Routes, useParams } from 'react-router-dom';
+
+import { ContentDeps, ContentProps } from '../content/content';
+
+export type ContentRouteDeps = ContentDeps & {
+    ContentRootComponent: React.FC<ContentProps>;
+};
+export type ContentRouteProps = { deps: ContentRouteDeps };
+
+export const ContentRoute = NamedFC<ContentRouteProps>('ContentRoute', ({ deps }) => {
+    const params = useParams();
+    return <deps.ContentRootComponent deps={deps} reference={params['*']} />;
+});
+
+export const InsightsRoutes = NamedFC<ContentRouteProps>('ContentRoutes', ({ deps }) => (
+    <Routes>
+        <Route path="/content/*" element={<ContentRoute deps={deps} />} />
+    </Routes>
+));
+
+export const InsightsRouter = NamedFC<ContentRouteProps>('ContentHashRouter', ({ deps }) => (
+    <HashRouter>
+        <InsightsRoutes deps={deps} />
+    </HashRouter>
+));
