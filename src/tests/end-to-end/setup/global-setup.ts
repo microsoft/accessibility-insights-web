d81361cfed56bd1c6de46b696d7120b71701e7f8
karan <karansin@microsoft.com>
2022-01-28 10:28:23 -0800

    Fix import issue

--- src/tests/end-to-end/setup/global-setup.ts
+++ src/tests/end-to-end/setup/global-setup.ts
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as fs from 'fs';
+import * as testResourceServer from '../../miscellaneous/test-resource-server/resource-server';
 import {
     getExtensionPath,
     getManifestPath,
     originalManifestCopyPath,
-} from 'tests/end-to-end/common/extension-paths';
-import * as testResourceServer from '../../miscellaneous/test-resource-server/resource-server';
+} from '../common/extension-paths';
 import { testResourceServerConfigs } from './test-resource-server-config';
 
 // tslint:disable-next-line:no-default-export
--- src/tests/end-to-end/setup/global-teardown.ts
+++ src/tests/end-to-end/setup/global-teardown.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as fs from 'fs';
-import { originalManifestCopyPath } from 'tests/end-to-end/common/extension-paths';
 import * as testResourceServer from '../../miscellaneous/test-resource-server/resource-server';
+import { originalManifestCopyPath } from '../common/extension-paths';
 
 // tslint:disable-next-line:no-default-export
 export default function (): void {
