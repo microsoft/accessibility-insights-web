edce9dbab44a535a8719f890a56271ab9b9e05a8
Karan <karansin@microsoft.com>
2022-01-28 16:02:03 -0800

    fix: resolve e2e test flakiness in cross-origin-iframe.test.ts (#5125)

--- src/tests/end-to-end/setup/global-setup.ts
+++ src/tests/end-to-end/setup/global-setup.ts
@@ -1,6 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import * as fs from 'fs';
 import * as testResourceServer from '../../miscellaneous/test-resource-server/resource-server';
+import {
+    getExtensionPath,
+    getManifestPath,
+    originalManifestCopyPath,
+} from '../common/extension-paths';
 import { testResourceServerConfigs } from './test-resource-server-config';
 
 // tslint:disable-next-line:no-default-export
@@ -8,4 +14,6 @@ export default function (): void {
     for (const testResourceServerConfig of testResourceServerConfigs) {
         testResourceServer.startServer(testResourceServerConfig);
     }
+
+    fs.copyFileSync(getManifestPath(getExtensionPath()), originalManifestCopyPath);
 }
--- src/tests/end-to-end/setup/global-teardown.ts
+++ src/tests/end-to-end/setup/global-teardown.ts
@@ -1,8 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import * as fs from 'fs';
 import * as testResourceServer from '../../miscellaneous/test-resource-server/resource-server';
+import {
+    getExtensionPath,
+    getManifestPath,
+    originalManifestCopyPath,
+} from '../common/extension-paths';
 
 // tslint:disable-next-line:no-default-export
 export default function (): void {
     testResourceServer.stopAllServers();
+
+    if (fs.existsSync(originalManifestCopyPath)) {
+        fs.renameSync(originalManifestCopyPath, getManifestPath(getExtensionPath()));
+    }
 }
