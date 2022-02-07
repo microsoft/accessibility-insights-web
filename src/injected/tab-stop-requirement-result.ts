bebc472552a835d4e0006a0fa13f41eee9cde574
Wahaj Abid <waabid@microsoft.com>
2022-02-07 15:45:57 -0800

    added missing license text

--- src/injected/tab-stop-requirement-result.ts
+++ src/injected/tab-stop-requirement-result.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 import { TabStopRequirementId } from 'types/tab-stop-requirement-info';
 
 export type TabStopRequirementResult = {
