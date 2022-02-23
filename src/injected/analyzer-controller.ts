52df32a5a20933f796976491f1bd1cf9bd421e45
Lisa Li <lisli@microsoft.com>
2022-02-23 10:55:43 -0800

    teardown before updating the needToCollectTabbingResults flag so keyboard navigation instances are added to the store

--- src/injected/analyzer-controller.ts
+++ src/injected/analyzer-controller.ts
@@ -72,8 +72,8 @@ export class AnalyzerController {
     private onResultsChangedState = (): void => {
         const state = this.visualizationResultsStore.getState();
         if (state.tabStops.tabbingCompleted && state.tabStops.needToCollectTabbingResults) {
-            this.tabStopRequirementActionMessageCreator.updateNeedToCollectTabbingResults(false);
             this.teardown(AdHocTestkeys.TabStops);
+            this.tabStopRequirementActionMessageCreator.updateNeedToCollectTabbingResults(false);
         }
     };
 
