93413dcbff017dad24edbd0db6c2458f03b826a2
Lisa Li <lisli@microsoft.com>
2022-02-23 18:26:48 -0800

    wip tab-stops-requirement-result-processor

---
+++ src/injected/analyzers/tab-stops-requirement-result-processor.ts
@@ -0,0 +1,76 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { BaseStore } from 'common/base-store';
+import { FeatureFlags } from 'common/feature-flags';
+import { FeatureFlagStoreData } from 'common/types/store-data/feature-flag-store-data';
+import { VisualizationScanResultData } from 'common/types/store-data/visualization-scan-result-data';
+import { TabStopRequirementActionMessageCreator } from 'DetailsView/actions/tab-stop-requirement-action-message-creator';
+import { AllFrameRunner } from 'injected/all-frame-runner';
+import { TabStopsDoneAnalyzingTracker } from 'injected/analyzers/tab-stops-done-analyzing-tracker';
+import { AutomatedTabStopRequirementResult } from 'injected/tab-stop-requirement-result';
+import { isEqual } from 'lodash';
+
+export class TabStopsRequirementResultProcessor {
+    private seenTabStopRequirementResults: AutomatedTabStopRequirementResult[] = [];
+
+    constructor(
+        private readonly featureFlagStore: BaseStore<FeatureFlagStoreData>,
+        private readonly tabStopRequirementRunner: AllFrameRunner<AutomatedTabStopRequirementResult>,
+        private readonly tabStopRequirementActionMessageCreator: TabStopRequirementActionMessageCreator,
+        private readonly tabStopsDoneAnalyzingTracker: TabStopsDoneAnalyzingTracker,
+        private readonly visualizationResultsStore: BaseStore<VisualizationScanResultData>,
+    ) {}
+
+    public start = (): void => {
+        if (
+            this.tabStopRequirementRunner &&
+            this.featureFlagStore.getState()[FeatureFlags.tabStopsAutomation] === true
+        ) {
+            this.seenTabStopRequirementResults = [];
+            this.tabStopsDoneAnalyzingTracker.reset();
+            this.tabStopRequirementRunner.topWindowCallback = this.processTabStopRequirementResults;
+            this.tabStopRequirementRunner.start();
+        }
+    };
+
+    public onStateChange = (): void => {
+        const state = this.visualizationResultsStore.getState();
+        if (state.tabStops.tabbingCompleted && state.tabStops.needToCollectTabbingResults) {
+            this.stop();
+        }
+    };
+
+    public stop = (): void => {
+        if (this.tabStopRequirementRunner) {
+            this.tabStopRequirementRunner.stop();
+        }
+
+        const state = this.visualizationResultsStore.getState();
+        if (state.tabStops.tabbingCompleted && state.tabStops.needToCollectTabbingResults) {
+            this.tabStopRequirementActionMessageCreator.automatedTabbingResultsCompleted(
+                this.seenTabStopRequirementResults,
+            );
+
+            this.tabStopRequirementActionMessageCreator.updateNeedToCollectTabbingResults(false);
+        }
+    };
+
+    private processTabStopRequirementResults = (
+        tabStopRequirementResult: AutomatedTabStopRequirementResult,
+    ): void => {
+        const duplicateResult = this.seenTabStopRequirementResults.some(r =>
+            isEqual(r, tabStopRequirementResult),
+        );
+
+        if (!duplicateResult) {
+            this.tabStopRequirementActionMessageCreator.addTabStopInstance({
+                description: tabStopRequirementResult.description,
+                requirementId: tabStopRequirementResult.requirementId,
+                selector: tabStopRequirementResult.selector,
+                html: tabStopRequirementResult.html,
+            });
+            this.seenTabStopRequirementResults.push(tabStopRequirementResult);
+        }
+    };
+}
