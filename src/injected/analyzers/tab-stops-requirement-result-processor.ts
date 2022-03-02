642f2c290c14189a1afb38a520a3d8e2ccf8248f
lisli1 <48259897+lisli1@users.noreply.github.com>
2022-03-01 19:57:56 -0800

    feat(tabstops-auto-target-page-vis): Add TabStopsRequirementResultProcessor (#5219)
    Details
    Create TabStopsRequirementResultProcessor to take some of the responsibility of the result processing away from the TabStopsAnalyzer and AnalyzerController.
    
    Motivation
    Part of feature work.
    
    Context
    AnalyzerController included a function that was used only in the TabStops case and was calling teardown() when it should not have. TabStopsAnalyzer also had too much responsibility in processing results and managing the requirement runner. This PR moves these functionalities to a separate class, TabStopsRequirementResultProcessor, to fix these issues. This also fixes an issue that #5202 tried to address, in which the keyboard navigation errors weren't being added to the store/failure instances list.

---
+++ src/injected/analyzers/tab-stops-requirement-result-processor.ts
@@ -0,0 +1,77 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { BaseStore } from 'common/base-store';
+import { VisualizationScanResultData } from 'common/types/store-data/visualization-scan-result-data';
+import { TabStopRequirementActionMessageCreator } from 'DetailsView/actions/tab-stop-requirement-action-message-creator';
+import { AllFrameRunner } from 'injected/all-frame-runner';
+import { AutomatedTabStopRequirementResult } from 'injected/tab-stop-requirement-result';
+import { isEqual } from 'lodash';
+
+export class TabStopsRequirementResultProcessor {
+    private seenTabStopRequirementResults: AutomatedTabStopRequirementResult[] = [];
+    private isStopped: boolean = true;
+
+    constructor(
+        private readonly tabStopRequirementRunner: AllFrameRunner<AutomatedTabStopRequirementResult>,
+        private readonly tabStopRequirementActionMessageCreator: TabStopRequirementActionMessageCreator,
+        private readonly visualizationResultsStore: BaseStore<VisualizationScanResultData>,
+    ) {}
+
+    public start = (): void => {
+        if (!this.isStopped) {
+            return;
+        }
+
+        this.visualizationResultsStore.addChangedListener(this.onStateChange);
+
+        this.seenTabStopRequirementResults = [];
+        this.tabStopRequirementRunner.topWindowCallback = this.processTabStopRequirementResults;
+        this.tabStopRequirementRunner.start();
+
+        this.isStopped = false;
+    };
+
+    private onStateChange = (): void => {
+        // Checking state here rather than in stop(), to ensure results are recorded when stop() is
+        // called during teardown, when tabbing may not have been completed (i.e. user disables toggle)
+        const state = this.visualizationResultsStore.getState();
+        if (state.tabStops.tabbingCompleted && state.tabStops.needToCollectTabbingResults) {
+            this.stop();
+        }
+    };
+
+    public stop = (): void => {
+        if (this.isStopped) {
+            return;
+        }
+
+        this.tabStopRequirementRunner.stop();
+        this.tabStopRequirementActionMessageCreator.automatedTabbingResultsCompleted(
+            this.seenTabStopRequirementResults,
+        );
+        this.tabStopRequirementActionMessageCreator.updateNeedToCollectTabbingResults(false);
+
+        this.visualizationResultsStore.removeChangedListener(this.onStateChange);
+
+        this.isStopped = true;
+    };
+
+    private processTabStopRequirementResults = (
+        tabStopRequirementResult: AutomatedTabStopRequirementResult,
+    ): void => {
+        const duplicateResult = this.seenTabStopRequirementResults.some(r =>
+            isEqual(r, tabStopRequirementResult),
+        );
+
+        if (!duplicateResult) {
+            this.tabStopRequirementActionMessageCreator.addTabStopInstance({
+                description: tabStopRequirementResult.description,
+                requirementId: tabStopRequirementResult.requirementId,
+                selector: tabStopRequirementResult.selector,
+                html: tabStopRequirementResult.html,
+            });
+            this.seenTabStopRequirementResults.push(tabStopRequirementResult);
+        }
+    };
+}
