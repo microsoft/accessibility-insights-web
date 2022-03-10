9efb82b5b45f0ab5d4c8801f152dae9537fbc463
Dan Bjorge <danielbj@microsoft.com>
2022-03-10 16:52:11 -0500

    Update serializer to support mock functions, too

--- src/tests/common/typemoq-snapshot-serializer.ts
+++ src/tests/common/typemoq-snapshot-serializer.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import { MockException } from 'typemoq';
+
 // This file is a Jest Snapshot Serializer which serializes all typemoq mock objects as a
 // constant string, similar to how Jest serializes functions as the constant string "[Function]"
 //
@@ -13,10 +15,35 @@
 // See https://github.com/florinn/typemoq/blob/master/src/Consts.ts
 const typemoqProxyIdValue = 'BCDF5CE5-F0DF-40B7-8BA0-69DF395033C8';
 
-export function test(val: unknown): boolean {
-    return val != null && typeof val === 'object' && val['___id'] === typemoqProxyIdValue;
+function readingPropertyThrowsMockException(val: unknown): boolean {
+    try {
+        val['___any_property'];
+    } catch (e: any) {
+        if (e instanceof MockException) {
+            return true;
+        }
+    }
+    return false;
 }
 
-export function serialize(): string {
-    return '[typemoq mock object]';
+export function test(val: unknown): boolean {
+    if (val == null) {
+        return false;
+    }
+
+    if (typeof val === 'object' && val['___id'] === typemoqProxyIdValue) {
+        // This is a typemoq mock object (eg, Mock.of<Type>().object)
+        return true;
+    }
+
+    if (typeof val === 'function' && readingPropertyThrowsMockException(val)) {
+        // This is a method of a typemoq mock object (eg, Mock.of<Type>().object.someMethod)
+        return true;
+    }
+
+    return false;
+}
+
+export function serialize(val: unknown): string {
+    return `[typemoq mock ${typeof val}]`;
 }
