91c6d8d16be670afde61fbe0bee0080dadfaafb1
Wahaj Abid <waabid@microsoft.com>
2022-03-25 17:27:36 -0700

    use 7zip without -r flag

--- pipeline/scripts/zip-mac-folder.js
+++ pipeline/scripts/zip-mac-folder.js
@@ -17,24 +17,12 @@ const parentDir = process.argv[2];
 const files = fs.readdirSync(parentDir);
 const existingDmg = files.find(f => path.extname(f) === '.dmg');
 const appName = path.basename(existingDmg, path.extname(existingDmg));
-// const cmd = `${sevenBin.path7za}`;
-// const args = [
-//     'a',
-//     '-bd',
-//     '-mx=7',
-//     '-mtc=off',
-//     '-mm=Deflate',
-//     '-mcu',
-//     `${appName}.zip`,
-//     '-r',
-//     'mac',
-// ];
-const cmd = `/usr/bin/ditto`;
-const args = ['-c', '-k', '--keepParent', 'mac', `${appName}.zip`];
+const cmd = `${sevenBin.path7za}`;
+const args = ['a', '-bd', '-mx=7', '-mtc=off', '-mm=Deflate', '-mcu', `${appName}.zip`, 'mac'];
 
 console.log(`existingDmg: ${existingDmg}`);
 console.log(`appName: ${appName}`);
-// console.log(`path to 7z: ${sevenBin.path7za}`);
+console.log(`path to 7z: ${sevenBin.path7za}`);
 
 child_process.execFileSync(cmd, args, {
     cwd: parentDir,
