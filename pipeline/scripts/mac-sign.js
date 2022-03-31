b0af759832e0e46de9886dea83eeb1eb9c69e2a7
Wahaj Abid <waabid@microsoft.com>
2022-03-31 16:27:00 -0700

    initial signing script

---
+++ pipeline/scripts/mac-sign.js
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+function sign(path) {
+    console.log(`signing ${path}`);
+    const identity = '$(mac-notarization-developerid-certificate-identity)';
+    const entitlementsPath =
+        '$(System.DefaultWorkingDirectory)/src/electron/resources/entitlements.plist';
+    execSync(
+        `codesign -s ${identity} --timestamp --force --options runtime --entitlements ${entitlementsPath} ${path}`,
+    );
+}
+
+const appLocations = [
+    path.resolve(__dirname, './drop/electron/unified-canary/packed/mac/'),
+    path.resolve(__dirname, './drop/electron/unified-insider/packed/mac/'),
+    path.resolve(__dirname, './drop/electron/unified-production/packed/mac/'),
+];
+
+appLocations.forEach(console.log);
