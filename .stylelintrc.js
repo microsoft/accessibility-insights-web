4bdcea346d001ade9d21a9792f64f4663b6a3ce9
Katy DeCorah <katydecorah@microsoft.com>
2022-04-13 16:25:20 -0400

    Copyedits: prefer longhand, add other possible properties for longhand

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,6 +5,8 @@ module.exports = {
     rules: {
         'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global'] }],
         'scss/at-import-partial-extension': 'always',
+        'declaration-block-no-redundant-longhand-properties': null, // Prefer longhand to improve readability
+        'shorthand-property-no-redundant-values': null, // Prefer longhand to improve readability
 
         // TO BE ENABLED: Recommended fixes
         'scss/at-extend-no-missing-placeholder': null,
@@ -14,16 +16,16 @@ module.exports = {
         'scss/no-global-function-names': null,
         'scss/comment-no-empty': null,
         'no-descending-specificity': null,
-        'declaration-block-no-redundant-longhand-properties': null,
 
-        // TO BE CONFIGURED: Limit shorthand for margin and padding
+        // TO BE CONFIGURED: Limit shorthand for margin, padding
+        // Stretch goal: border-width, border-radius, border-color, border-style, grid-gap
         // Example: 'declaration-property-max-values': { padding: 1 },
-        'shorthand-property-no-redundant-values': null, // After the limit shorthand for padding and margin is complete, remove this rules and make recommended changes.
 
         // TO BE CONFIGURED: Enforce variable values
         // Example: 'declaration-property-value-allowed-list': { 'font-weight': ['/^\\$.*$/']},
 
         // TO BE CONFIGURED: Variable, selector, mixin case
+        // https://stylelint.io/user-guide/rules/regex
         'selector-class-pattern': null,
         'scss/at-mixin-pattern': null,
         'scss/dollar-variable-pattern': null,
