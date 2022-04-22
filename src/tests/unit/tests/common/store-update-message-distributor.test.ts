27dec8f1efb4403647b162dcf9a9d14f22be9767
pownkel <kepownel@microsoft.com>
2022-04-22 15:24:06 -0700

    create StoreUpdateMessageDistributor to track storeProxy message handlers

---
+++ src/tests/unit/tests/common/store-update-message-distributor.test.ts
@@ -0,0 +1,143 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { IMock, It, Mock } from 'typemoq';
+import { BrowserAdapter } from '../../../../common/browser-adapters/browser-adapter';
+import { Logger } from '../../../../common/logging/logger';
+import { StoreUpdateMessageDistributor } from '../../../../common/store-update-message-distributor';
+import { StoreType } from '../../../../common/types/store-type';
+import { StoreUpdateMessage } from '../../../../common/types/store-update-message';
+
+describe(StoreUpdateMessageDistributor, () => {
+    const tabId = 1;
+    const storeId = 'TestStore';
+    let browserAdapterMock: IMock<BrowserAdapter>;
+    let loggerMock: IMock<Logger>;
+    let onMessage: (message: StoreUpdateMessage<any>, sender?: any) => void;
+    let registeredListener: jest.Mock;
+
+    let tabContextMessage: StoreUpdateMessage<string>;
+    let globalStoreMessage: StoreUpdateMessage<string>;
+
+    let testSubject: StoreUpdateMessageDistributor;
+
+    beforeEach(() => {
+        browserAdapterMock = Mock.ofType<BrowserAdapter>();
+        browserAdapterMock
+            .setup(b => b.addListenerOnMessage(It.isAny()))
+            .callback(listener => (onMessage = listener));
+
+        loggerMock = Mock.ofType<Logger>();
+
+        registeredListener = jest.fn();
+
+        tabContextMessage = {
+            isStoreUpdateMessage: true,
+            storeId,
+            payload: 'store update payload',
+            tabId,
+            storeType: StoreType.TabContextStore,
+        } as StoreUpdateMessage<string>;
+
+        globalStoreMessage = {
+            isStoreUpdateMessage: true,
+            storeId,
+            payload: 'store update payload',
+            tabId: null,
+            storeType: StoreType.GlobalStore,
+        } as StoreUpdateMessage<string>;
+
+        testSubject = new StoreUpdateMessageDistributor(
+            tabId,
+            browserAdapterMock.object,
+            loggerMock.object,
+        );
+
+        testSubject.initialize();
+        testSubject.registerStoreUpdateListener(storeId, registeredListener);
+    });
+
+    afterEach(() => {
+        browserAdapterMock.verifyAll();
+        loggerMock.verifyAll();
+    });
+
+    const invalidMessages: StoreUpdateMessage<string>[] = [
+        { ...tabContextMessage, isStoreUpdateMessage: false },
+        { ...tabContextMessage, payload: undefined },
+        { ...tabContextMessage, storeId: undefined },
+        { ...tabContextMessage, tabId: tabId + 10 },
+    ];
+    it.each(invalidMessages)('logs and ignores invalid message: %o', message => {
+        loggerMock
+            .setup(l =>
+                l.log(
+                    It.is(x => x.includes('Unable to interpret message')),
+                    It.isAny(),
+                ),
+            )
+            .verifiable();
+
+        onMessage(message);
+
+        expect(registeredListener).toBeCalledTimes(0);
+    });
+
+    it('logs and ignores if no listener is registered for this message', () => {
+        const message = {
+            ...tabContextMessage,
+            storeId: 'AnotherStore',
+        };
+
+        loggerMock
+            .setup(l =>
+                l.log(
+                    It.is(x => x.includes('No listeners registered for message')),
+                    It.isAny(),
+                ),
+            )
+            .verifiable();
+
+        onMessage(message);
+
+        expect(registeredListener).toBeCalledTimes(0);
+    });
+
+    it('Calls registered listener for tab context store message', () => {
+        onMessage(tabContextMessage);
+
+        expect(registeredListener).toBeCalledWith(tabContextMessage);
+    });
+
+    it('Calls registered listener for global store message', () => {
+        onMessage(globalStoreMessage);
+
+        expect(registeredListener).toBeCalledWith(globalStoreMessage);
+    });
+
+    it('Does not overwrite listeners for the same store', () => {
+        expect(() => testSubject.registerStoreUpdateListener(storeId, () => null)).toThrow();
+    });
+
+    it('Registers multiple listeners and distributes messages correctly', () => {
+        const anotherStoreId = 'AnotherStore';
+        const anotherListener = jest.fn();
+
+        const messageForStore = { ...tabContextMessage };
+        const messageForAnotherStore = { ...tabContextMessage, storeId: anotherStoreId };
+
+        testSubject.registerStoreUpdateListener(anotherStoreId, anotherListener);
+
+        onMessage(messageForStore);
+        onMessage(messageForAnotherStore);
+
+        expect(registeredListener).toBeCalledWith(messageForStore);
+        expect(anotherListener).toBeCalledWith(messageForAnotherStore);
+    });
+
+    it('dispose removes message listener', () => {
+        browserAdapterMock.setup(o => o.removeListenerOnMessage(onMessage)).verifiable();
+
+        testSubject.dispose();
+    });
+});
