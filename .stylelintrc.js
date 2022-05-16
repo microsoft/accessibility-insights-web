36469ba6da45985fad4a78d82dbcad33507b76ee
Katy DeCorah <katydecorah@gmail.com>
2022-05-16 12:17:05 -0400

    Update comment to include issue link to comment-no-empty false positive

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,7 +8,7 @@ module.exports = {
         'declaration-block-no-duplicate-properties': [true, { ignore: ['consecutive-duplicates'] }],
         'declaration-block-no-redundant-longhand-properties': null, // Prefer longhand to improve readability
         'shorthand-property-no-redundant-values': null, // Prefer longhand to improve readability
-        'scss/comment-no-empty': null, // Disabled to allow for paragraph breaks in longer comments
+        'scss/comment-no-empty': null, // Disabled to allow for paragraph breaks in longer comments which is not supported yet: https://github.com/stylelint-scss/stylelint-scss/issues/606
 
         // TO BE ENABLED: Recommended fixes
         'scss/at-extend-no-missing-placeholder': null,
