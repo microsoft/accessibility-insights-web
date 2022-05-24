7d99013693d7bfb105b4ed712ee783a585cc681f
Dan Bjorge <danielbj@microsoft.com>
2022-05-24 16:00:29 -0400

    Implement further updates to pipe promises along drawing controller

--- src/injected/drawing-controller.ts
+++ src/injected/drawing-controller.ts
@@ -71,34 +71,41 @@ export class DrawingController {
     };
 
     private async enableVisualization(
-        elementResultsByFrames: HTMLIFrameResult[],
+        elementResultsByFrames: HTMLIFrameResult[] | null,
         configId: string,
     ): Promise<void> {
         if (elementResultsByFrames) {
-            for (let pos = 0; pos < elementResultsByFrames.length; pos++) {
-                const resultsForFrame = elementResultsByFrames[pos];
-                if (resultsForFrame.frame == null) {
-                    await this.enableVisualizationInCurrentFrame(
-                        resultsForFrame.elementResults,
-                        configId,
-                    );
-                } else {
-                    this.enableVisualizationInIFrames(
-                        resultsForFrame.frame,
-                        resultsForFrame.elementResults,
-                        configId,
-                    );
-                }
-            }
+            await Promise.all(
+                elementResultsByFrames.map(frameResults =>
+                    this.enableVisualizationForFrameResults(frameResults, configId),
+                ),
+            );
         } else {
             await this.enableVisualizationInCurrentFrame(null, configId);
 
-            const iframes = this.getAllFrames();
-            for (let pos = 0; pos < iframes.length; pos++) {
-                this.enableVisualizationInIFrames(iframes[pos], null, configId);
-            }
+            const childFrames = this.getChildFrames();
+            await Promise.all(
+                childFrames.map(iframe =>
+                    this.enableVisualizationInChildFrame(iframe, null, configId),
+                ),
+            );
         }
     }
+
+    private enableVisualizationForFrameResults = async (
+        frameResults: HTMLIFrameResult,
+        configId: string,
+    ): Promise<void> => {
+        if (frameResults.frame == null) {
+            await this.enableVisualizationInCurrentFrame(frameResults.elementResults, configId);
+        } else {
+            await this.enableVisualizationInChildFrame(
+                frameResults.frame,
+                frameResults.elementResults,
+                configId,
+            );
+        }
+    };
 
     private enableVisualizationInCurrentFrame = async (
         currentFrameResults: AssessmentVisualizationInstance[],
@@ -112,11 +119,11 @@ export class DrawingController {
         await drawer.drawLayout();
     };
 
-    private enableVisualizationInIFrames = (
+    private enableVisualizationInChildFrame = async (
         frame: HTMLIFrameElement,
         frameResults: AssessmentVisualizationInstance[],
         configId: string,
-    ): void => {
+    ): Promise<void> => {
         const message: VisualizationWindowMessage = {
             elementResults: frameResults,
             isEnabled: true,
@@ -124,14 +131,15 @@ export class DrawingController {
             configId: configId,
         };
 
-        // This message recursively triggers enableVisualization in all iframes.
-        // Intentionally floating this promise.
-        void this.frameMessenger.sendMessageToFrame(frame, this.createFrameRequestMessage(message));
+        await this.frameMessenger.sendMessageToFrame(
+            frame,
+            this.createFrameRequestMessage(message),
+        );
     };
 
     private async disableVisualization(configId: string): Promise<void> {
         this.disableVisualizationInCurrentFrame(configId);
-        await this.disableVisualizationInIFrames(configId);
+        await this.disableVisualizationInChildFrames(configId);
     }
 
     private createFrameRequestMessage(message: VisualizationWindowMessage): CommandMessage {
@@ -141,23 +149,29 @@ export class DrawingController {
         };
     }
 
-    private disableVisualizationInIFrames = async (configId: string): Promise<void> => {
-        const iframes = this.getAllFrames();
+    private disableVisualizationInChildFrames = async (configId: string): Promise<void> => {
+        const iframes = this.getChildFrames();
 
-        for (let i = 0; i < iframes.length; i++) {
-            const iframe = iframes[i];
-            if (iframe != null) {
-                const message: VisualizationWindowMessage = {
-                    isEnabled: false,
-                    configId: configId,
-                };
+        await Promise.all(
+            iframes.map(async iframe => {
+                this.disableVisualizationInChildFrame(configId, iframe);
+            }),
+        );
+    };
 
-                await this.frameMessenger.sendMessageToFrame(
-                    iframe,
-                    this.createFrameRequestMessage(message),
-                );
-            }
-        }
+    private disableVisualizationInChildFrame = async (
+        configId: string,
+        iframe: HTMLIFrameElement,
+    ): Promise<void> => {
+        const message: VisualizationWindowMessage = {
+            isEnabled: false,
+            configId: configId,
+        };
+
+        await this.frameMessenger.sendMessageToFrame(
+            iframe,
+            this.createFrameRequestMessage(message),
+        );
     };
 
     private disableVisualizationInCurrentFrame(configId: string): void {
@@ -165,10 +179,12 @@ export class DrawingController {
         drawer.eraseLayout();
     }
 
-    private getAllFrames(): HTMLCollectionOf<HTMLIFrameElement> {
-        return this.htmlElementUtils.getAllElementsByTagName(
-            'iframe',
-        ) as HTMLCollectionOf<HTMLIFrameElement>;
+    private getChildFrames(): HTMLIFrameElement[] {
+        return Array.from(
+            this.htmlElementUtils.getAllElementsByTagName(
+                'iframe',
+            ) as HTMLCollectionOf<HTMLIFrameElement>,
+        );
     }
 
     private getDrawer(configId: string): Drawer {
