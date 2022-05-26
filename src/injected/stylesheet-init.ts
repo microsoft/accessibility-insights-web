4ca5381dbd1d4d976a8405bf3e98d005ca4c085e
Wahaj Abid <waabid@microsoft.com>
2022-05-26 11:11:35 -0700

    remove stylesheet-init since it's no longer required

--- src/injected/stylesheet-init.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-import { Stylesheet } from '@fluentui/merge-styles';
-
-const stylesheet = Stylesheet.getInstance();
-
-stylesheet.setConfig({
-    defaultPrefix: 'insights',
-});
