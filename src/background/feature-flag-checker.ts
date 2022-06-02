b5d32accd90fe745f8d4a46944e5357fa4012492
sfoslund <sfoslund@microsoft.com>
2022-06-02 12:34:26 -0700

    Revert unneeded changes

--- src/background/feature-flag-checker.ts
+++ src/background/feature-flag-checker.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 export type FeatureFlagChecker = {
     isEnabled: (feature: string) => boolean;
 };
