1382ddb7b9dd459cb2a4407efafc56c0dadf9e70
Wahaj <32555133+waabid@users.noreply.github.com>
2022-06-02 12:09:43 -0700

    feat(esbuild): use esbuild to build web extension (#5528)
    * feat(esbuild): use esbuild to build web extension
    
    * undo typings change due to bad merge
    
    * undo unnecessary changes to naming of details-view
    
    * update build and add script options for type-checking
    
    * update github ci build to type-check
    
    * update timeout for type-checking to 3 minutes
    
    * update when type-checking is run
    
    * use type-checked build in ci
    
    * change ordering for type-check
    
    * devM3 renamed to dev-mv3
    
    * adding type-check to fastpass
    
    * update to use devDependencies instead
    
    * use extension name function for css module/style paths
    
    * updated to use json stringify for args.path in plugin
    
    * rename type-check to type:check
    
    * use console.error in esbuild when catching an error
    
    * do not minify identifiers when bundling details view
    
    * add stylesheet-init content to client-init
    
    * added comment in building-web.md regarding tbuild
    
    * use compile instead of renderSync + comment
    
    * use dynamic imports for client-init that only import when we have not previously initialized the target page
    
    * added a way to use react-devtools like webpack
    
    * remove stylesheet-init since it's no longer required
    
    * fix issue with react-devtools build
    
    * add comment adding references for style plugin
    
    * remove references to stylesheet-init
    
    * embed styles in map files as well
    
    * update report package e2e tests (new line characters) as a result of embedded style change
    
    * smaller paths for css-modules in style plugin
    
    * use iife for extension format; fixes axe minification issue
    
    * errors exit appropriately and specifically mention map files in embed styles
    
    * fix lint issue

--- webpack.config.js
+++ webpack.config.js
@@ -18,10 +18,7 @@ const commonPlugins = [
 ];
 
 const commonEntryFiles = {
-    injected: [
-        path.resolve(__dirname, 'src/injected/stylesheet-init.ts'),
-        path.resolve(__dirname, 'src/injected/client-init.ts'),
-    ],
+    injected: [path.resolve(__dirname, 'src/injected/client-init.ts')],
     popup: [path.resolve(__dirname, 'src/popup/popup-init.ts')],
     insights: [path.resolve(__dirname, 'src/views/insights/initializer.ts')],
     detailsView: [path.resolve(__dirname, 'src/DetailsView/details-view-initializer.ts')],
