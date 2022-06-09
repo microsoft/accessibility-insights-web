microsoft/accessibility-insights-web

commit 6a6bf67c727ef933c6265832cb1f178b9b3f963d

feat(mv3-part-4): Return a promise from StoreUpdateMessageHub (#5518)
* Allow StoreUpdateMessageHub to register async listeners
* Return a promise from StoreUpdateMessageHub if a listener is registered for that message

src/debug-tools/debug-tools-message-distributor.ts
@@ -17,8 +17,8 @@ export class DebugToolsMessageDistributor {
         this.browserAdapter.addListenerOnMessage(this.distributeMessage);
     }
 
-    private distributeMessage = (message: any) => {
+    private distributeMessage = async (message: any) => {
         this.telemetryListener.onTelemetryMessage(message);
-        this.storeUpdateMessageHub.handleMessage(message as StoreUpdateMessage<unknown>);
+        await this.storeUpdateMessageHub.handleMessage(message as StoreUpdateMessage<unknown>);
     };
 }

src/Devtools/dev-tool-message-distributor.ts
@@ -18,12 +18,14 @@ export class DevToolsMessageDistributor {
         this.browserAdapter.addListenerOnMessage(this.distributeMessage);
     }
 
-    private distributeMessage = (message: Message): void | Promise<DevToolsStatusResponse> => {
+    private distributeMessage = (
+        message: Message,
+    ): void | Promise<void> | Promise<DevToolsStatusResponse> => {
         if (this.isStatusRequestForTab(message)) {
             // Must return a promise for the response to send correctly
             return Promise.resolve({ isActive: true });
         } else {
-            this.storeUpdateHub.handleMessage(message as StoreUpdateMessage<unknown>);
+            return this.storeUpdateHub.handleMessage(message as StoreUpdateMessage<unknown>);
         }
     };
 

src/common/store-update-message-hub.ts
@@ -5,7 +5,7 @@ import _ from 'lodash';
 import { StoreType } from './types/store-type';
 import { StoreUpdateMessage, storeUpdateMessageType } from './types/store-update-message';
 
-type StoreUpdateMessageListener = (message: StoreUpdateMessage<any>) => void;
+type StoreUpdateMessageListener = (message: StoreUpdateMessage<any>) => void | Promise<void>;
 
 export class StoreUpdateMessageHub {
     private readonly registeredUpdateListeners: { [key: string]: StoreUpdateMessageListener } = {};
@@ -22,14 +22,14 @@ export class StoreUpdateMessageHub {
         this.registeredUpdateListeners[storeId] = listener;
     }
 
-    public readonly handleMessage = (message: StoreUpdateMessage<any>): void => {
+    public readonly handleMessage = (message: StoreUpdateMessage<any>): void | Promise<void> => {
         if (!this.isValidMessage(message)) {
             return;
         }
 
         const listener = this.registeredUpdateListeners[message.storeId];
         if (listener) {
-            listener(message);
+            return listener(message);
         }
     };
 

src/tests/unit/tests/DevTools/dev-tool-message-distributor.test.ts
@@ -88,5 +88,23 @@ describe(DevToolsMessageDistributor, () => {
 
             expect(response).toBeUndefined();
         });
+
+        it('Returns a promise if storeUpdateMessageHub returns a promise', async () => {
+            const message = {
+                messageType: 'another message type',
+            };
+            storeUpdateHubMock
+                .setup(s => s.handleMessage(message as StoreUpdateMessage<unknown>))
+                .returns(() => Promise.resolve())
+                .verifiable(Times.once());
+
+            const responsePromise = distributeMessage(message);
+
+            expect(responsePromise).toBeInstanceOf(Promise);
+
+            const response = await responsePromise;
+
+            expect(response).toBeUndefined();
+        });
     });
 });

src/tests/unit/tests/common/store-update-message-hub.test.ts
@@ -15,11 +15,13 @@ describe(StoreUpdateMessageHub, () => {
 
     let tabContextMessage: StoreUpdateMessage<string>;
     let globalStoreMessage: StoreUpdateMessage<string>;
+    let listenerPromise: Promise<void>;
 
     let testSubject: StoreUpdateMessageHub;
 
     beforeEach(() => {
-        registeredListener = jest.fn();
+        listenerPromise = Promise.resolve();
+        registeredListener = jest.fn(() => listenerPromise);
 
         tabContextMessage = {
             messageType: storeUpdateMessageType,
@@ -52,9 +54,10 @@ describe(StoreUpdateMessageHub, () => {
         { ...tabContextMessage, tabId: tabId + 10 },
     ];
     it.each(invalidMessages)('ignores invalid message: %o', message => {
-        testSubject.handleMessage(message);
+        const result = testSubject.handleMessage(message);
 
         expect(registeredListener).toBeCalledTimes(0);
+        expect(result).toBeUndefined();
     });
 
     it('ignores if no listener is registered for this message', () => {
@@ -63,28 +66,38 @@ describe(StoreUpdateMessageHub, () => {
             storeId: 'AnotherStore',
         };
 
-        testSubject.handleMessage(message);
+        const result = testSubject.handleMessage(message);
 
         expect(registeredListener).toBeCalledTimes(0);
+        expect(result).toBeUndefined();
     });
 
-    it('Calls registered listener for tab context store message', () => {
-        testSubject.handleMessage(tabContextMessage);
+    it('Calls registered listener for tab context store message', async () => {
+        const resultPromise = testSubject.handleMessage(tabContextMessage);
+
+        expect(resultPromise).toBe(listenerPromise);
+        await resultPromise;
 
         expect(registeredListener).toBeCalledWith(tabContextMessage);
     });
 
-    it('Calls registered listener for global store message', () => {
-        testSubject.handleMessage(globalStoreMessage);
+    it('Calls registered listener for global store message', async () => {
+        const resultPromise = testSubject.handleMessage(globalStoreMessage);
+
+        expect(resultPromise).toBe(listenerPromise);
+        await resultPromise;
 
         expect(registeredListener).toBeCalledWith(globalStoreMessage);
     });
 
-    it('Calls registered listener if not created with a tab id', () => {
+    it('Calls registered listener if not created with a tab id', async () => {
         testSubject = new StoreUpdateMessageHub();
         testSubject.registerStoreUpdateListener(storeId, registeredListener);
 
-        testSubject.handleMessage(tabContextMessage);
+        const resultPromise = testSubject.handleMessage(tabContextMessage);
+
+        expect(resultPromise).toBe(listenerPromise);
+        await resultPromise;
 
         expect(registeredListener).toBeCalledWith(tabContextMessage);
     });
@@ -93,17 +106,23 @@ describe(StoreUpdateMessageHub, () => {
         expect(() => testSubject.registerStoreUpdateListener(storeId, () => null)).toThrow();
     });
 
-    it('Registers multiple listeners and distributes messages correctly', () => {
+    it('Registers multiple listeners and distributes messages correctly', async () => {
         const anotherStoreId = 'AnotherStore';
-        const anotherListener = jest.fn();
+        const anotherListener = jest.fn(() => Promise.resolve());
 
         const messageForStore = { ...tabContextMessage };
         const messageForAnotherStore = { ...tabContextMessage, storeId: anotherStoreId };
 
         testSubject.registerStoreUpdateListener(anotherStoreId, anotherListener);
 
-        testSubject.handleMessage(messageForStore);
-        testSubject.handleMessage(messageForAnotherStore);
+        const resultPromise1 = testSubject.handleMessage(messageForStore);
+        const resultPromise2 = testSubject.handleMessage(messageForAnotherStore);
+
+        expect(resultPromise1).toBeInstanceOf(Promise);
+        expect(resultPromise2).toBeInstanceOf(Promise);
+
+        await resultPromise1;
+        await resultPromise2;
 
         expect(registeredListener).toBeCalledWith(messageForStore);
         expect(anotherListener).toBeCalledWith(messageForAnotherStore);

commit 488f06372a823c1be34bbd898484910342178472

fix(mv3-part-4): prevent debug tools window from eating other windows' messages (#5602)
#### Details

This PR fixes an issue that @sfoslund and I discovered while validating #5591. @pownkel and @sfoslund did some outstanding debugging to find the root cause.

The symptom we observed that this fixes is that, with #5591, we would sometimes (~30-60% of the time) see messages get lost between different frames of a target page with iframes. The root cause of this wasn't actually loss of the window messages between the frames, it was the loss of some of the messages used by out background backchannel infrastructure in communication between one of the frames and the background page.

We were very confused in debugging this for a while because it was inconsistent; sometimes it would repro half the time, and sometimes not at all. @sfoslund eventually narrowed down that the issue seemed to only repro at the 50% rate if the debug tools page was open (we had been using it to test whether the unhandledError telemetry was flowing, and then to see the unhandledErrors from the target page that indicated this error was occurring). @pownkel had the key insight that maybe the reason we "lost" messages that appeared to be sent successfully from the child frames but never received could be that something unexpected was receiving them and throwing them away.

The root cause is that the listener this PR is changing registers a browser message listener which always returns a promise, even if it doesn't want to indicate that the message is being handled. That means if the debug tools window is open and happens to process a given message before its real destination page does, it accidentally intercepts the message and responds with "undefined", and the original message sender sees the message as "handled, with response undefined" before the real destination gets a chance to handle it and respond back with a real response.

In practice, this only causes problems for messages intended for the background page's postMessageContentHandler - unlike most of our message-based communication, it actually responds to messages with non-undefined responses, so intercepting its messages results in broken behavior rather than just race conditions. This is why the only apparent impact of the issue was breaking communication between frames of a target page.

This was an issue introduced in #5518, but which was being masked by the browser event manager that had been introduced in #5490. #5591 changes the timing of the browser events such that this issue was more likely to be hit.

src/debug-tools/debug-tools-message-distributor.ts
@@ -13,12 +13,12 @@ export class DebugToolsMessageDistributor {
         private readonly telemetryListener: TelemetryListener,
     ) {}
 
-    public initialize() {
+    public initialize(): void {
         this.browserAdapter.addListenerOnMessage(this.distributeMessage);
     }
 
-    private distributeMessage = async (message: any) => {
+    private distributeMessage = (message: any): void | Promise<void> => {
         this.telemetryListener.onTelemetryMessage(message);
-        await this.storeUpdateMessageHub.handleMessage(message as StoreUpdateMessage<unknown>);
+        return this.storeUpdateMessageHub.handleMessage(message as StoreUpdateMessage<unknown>);
     };
 }

src/tests/unit/tests/debug-tools/debug-tools-message-distributor.test.ts
@@ -3,7 +3,6 @@
 
 import { BrowserAdapter } from 'common/browser-adapters/browser-adapter';
 import { StoreUpdateMessageHub } from 'common/store-update-message-hub';
-import { StoreUpdateMessage } from 'common/types/store-update-message';
 import { TelemetryListener } from 'debug-tools/controllers/telemetry-listener';
 import { DebugToolsMessageDistributor } from 'debug-tools/debug-tools-message-distributor';
 import { IMock, It, Mock, Times } from 'typemoq';
@@ -29,8 +28,6 @@ describe(DebugToolsMessageDistributor, () => {
     });
 
     afterEach(() => {
-        telemetryListenerMock.verifyAll();
-        storeUpdateHubMock.verifyAll();
         browserAdapterMock.verifyAll();
     });
 
@@ -43,20 +40,62 @@ describe(DebugToolsMessageDistributor, () => {
         testSubject.initialize();
     });
 
-    it('registers listener that calls both message handlers', () => {
-        const message = { messageType: 'message type' };
+    describe('listener', () => {
+        let message: any;
+        beforeEach(() => {
+            message = { messageType: 'message type' };
 
-        browserAdapterMock
-            .setup(b => b.addListenerOnMessage(It.isAny()))
-            .returns(listener => (registeredListener = listener));
-        telemetryListenerMock.setup(t => t.onTelemetryMessage(message)).verifiable();
-        storeUpdateHubMock
-            .setup(s => s.handleMessage(message as StoreUpdateMessage<unknown>))
-            .verifiable();
+            browserAdapterMock
+                .setup(b => b.addListenerOnMessage(It.isAny()))
+                .returns(listener => (registeredListener = listener));
 
-        testSubject.initialize();
+            testSubject.initialize();
+            expect(registeredListener).toBeDefined();
+        });
 
-        expect(registeredListener).toBeDefined();
-        registeredListener(message);
+        it('calls onTelemetryMessage', () => {
+            telemetryListenerMock
+                .setup(t => t.onTelemetryMessage(message))
+                .verifiable(Times.once());
+
+            registeredListener(message);
+
+            telemetryListenerMock.verifyAll();
+        });
+
+        it('calls and propagates a promise rejection from storeUpdateHub', async () => {
+            const errorFromStoreUpdateHub = new Error('from storeUpdateHub');
+
+            storeUpdateHubMock
+                .setup(m => m.handleMessage(message))
+                .returns(() => Promise.reject(errorFromStoreUpdateHub))
+                .verifiable(Times.once());
+
+            await expect(registeredListener(message)).rejects.toThrowError(errorFromStoreUpdateHub);
+
+            storeUpdateHubMock.verifyAll();
+        });
+
+        it('calls and propagates a promise fulfillment from storeUpdateHub', async () => {
+            storeUpdateHubMock
+                .setup(m => m.handleMessage(message))
+                .returns(() => Promise.resolve())
+                .verifiable(Times.once());
+
+            await expect(registeredListener(message)).resolves.toBeUndefined();
+
+            storeUpdateHubMock.verifyAll();
+        });
+
+        it('calls and propagates a void response from storeUpdateHub', () => {
+            storeUpdateHubMock
+                .setup(m => m.handleMessage(message))
+                .returns(() => {})
+                .verifiable(Times.once());
+
+            expect(registeredListener(message)).toBeUndefined();
+
+            storeUpdateHubMock.verifyAll();
+        });
     });
 });