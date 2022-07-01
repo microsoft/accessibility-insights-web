99a562d9480ae8a5a0efd67c84c07d18821e32f7
Zakiya Nasiru <81589466+ZakiyaN0@users.noreply.github.com>
2022-07-01 11:11:50 -0700

    chore: Create a hidden feature flag (#5689)
    * fix: Accessibility insights icon visibility in high contrast mode
    
    * merged with main
    
    * Merged with main
    
    * reverted changes to building-web.md
    
    * reverted changes
    
    * create a hidden feature flag
    
    * create feature flag

--- src/common/feature-flags.ts
+++ src/common/feature-flags.ts
@@ -13,6 +13,7 @@ export class FeatureFlags {
     public static readonly manualInstanceDetails = 'manualInstanceDetails';
     public static readonly debugTools = 'debugTools';
     public static readonly exportReportOptions = 'exportReportOptions';
+    public static readonly showAccessibleNames = 'showAccessibleNames';
 }
 
 export interface FeatureFlagDetail {
@@ -95,6 +96,14 @@ export function getAllFeatureFlagDetails(): FeatureFlagDetail[] {
             displayableName: 'More export options',
             displayableDescription: 'Enables exporting reports to external services',
             isPreviewFeature: true,
+            forceDefault: false,
+        },
+        {
+            id: FeatureFlags.showAccessibleNames,
+            defaultValue: false,
+            displayableName: 'Show accessible names',
+            displayableDescription: 'Displays the accessible names of user interface elements',
+            isPreviewFeature: false,
             forceDefault: false,
         },
     ];
