ba79e12c539913e6b89e8b77731e9ea16fa19259
pownkel <kepownel@microsoft.com>
2022-07-14 14:50:17 -0700

    use mergePromiseResponses in AsyncAction

--- src/common/flux/async-action.ts
+++ src/common/flux/async-action.ts
@@ -4,20 +4,23 @@
 // Licensed under the MIT License.
 import { Action } from 'common/flux/action';
 import { ScopeMutex } from 'common/flux/scope-mutex';
+import { mergePromiseResponses } from 'common/merge-promise-responses';
 
 export class AsyncAction<TPayload> implements Action<TPayload, Promise<void>> {
     private listeners: ((payload: TPayload) => Promise<void>)[] = [];
 
-    constructor(private readonly scopeMutex: ScopeMutex = new ScopeMutex()) {}
+    constructor(
+        private readonly scopeMutex: ScopeMutex = new ScopeMutex(),
+        private mergePromises: (
+            promises: Promise<unknown>[],
+        ) => Promise<void> = mergePromiseResponses,
+    ) {}
 
     public async invoke(payload: TPayload, scope?: string): Promise<void> {
         this.scopeMutex.tryLockScope(scope);
 
         try {
-            const results = await Promise.allSettled(
-                this.listeners.map((listener: (payload: TPayload) => void) => listener(payload)),
-            );
-            this.throwCombinedRejections(results);
+            await this.mergePromises(this.listeners.map(listener => listener(payload)));
         } finally {
             this.scopeMutex.unlockScope(scope);
         }
@@ -25,20 +28,5 @@ export class AsyncAction<TPayload> implements Action<TPayload, Promise<void>> {
 
     public addListener(listener: (payload: TPayload) => Promise<void>): void {
         this.listeners.push(listener);
-    }
-
-    private throwCombinedRejections(promiseResults: PromiseSettledResult<void>[]): void {
-        const rejectedResults = promiseResults.filter(
-            (r): r is PromiseRejectedResult => r.status === 'rejected',
-        );
-
-        if (rejectedResults.length === 1) {
-            throw rejectedResults[0].reason;
-        }
-
-        if (rejectedResults.length > 0) {
-            const rejectedReasons = rejectedResults.map(r => r.reason);
-            throw new AggregateError(rejectedReasons);
-        }
     }
 }
--- src/tests/unit/tests/common/flux/async-action.test.ts
+++ src/tests/unit/tests/common/flux/async-action.test.ts
@@ -2,121 +2,115 @@
 // Licensed under the MIT License.
 import { AsyncAction } from 'common/flux/async-action';
 import { ScopeMutex } from 'common/flux/scope-mutex';
-import { IMock, Mock, MockBehavior, Times } from 'typemoq';
+import { IMock, It, Mock, MockBehavior, Times } from 'typemoq';
 
 describe(AsyncAction, () => {
     let listenerMock: IMock<(payload: TestPayload) => Promise<void>>;
     const testPayload: TestPayload = { key: 'value' };
     let testObject: AsyncAction<TestPayload>;
     let scopeMutexMock: IMock<ScopeMutex>;
+    let mergePromisesMock: IMock<(promises: Promise<unknown>[]) => Promise<void>>;
+    let mergedPromiseInternalMock: IMock<() => void>;
 
     beforeEach(() => {
         listenerMock = Mock.ofInstance(_ => Promise.resolve(), MockBehavior.Strict);
         scopeMutexMock = Mock.ofType<ScopeMutex>();
+        mergePromisesMock = Mock.ofInstance(async _ => null);
+        mergedPromiseInternalMock = Mock.ofInstance(() => null);
 
-        testObject = new AsyncAction<TestPayload>(scopeMutexMock.object);
+        testObject = new AsyncAction<TestPayload>(scopeMutexMock.object, mergePromisesMock.object);
     });
 
     afterEach(() => {
         listenerMock.verifyAll();
         scopeMutexMock.verifyAll();
+        mergePromisesMock.verifyAll();
+        mergedPromiseInternalMock.verifyAll();
     });
 
     test('addListener and invoke', async () => {
-        listenerMock.setup(l => l(testPayload)).verifiable(Times.once());
+        const listenerPromise = Promise.resolve();
+        listenerMock
+            .setup(l => l(testPayload))
+            .returns(() => listenerPromise)
+            .verifiable(Times.once());
 
         testObject.addListener(listenerMock.object);
 
-        scopeMutexMock.setup(m => m.tryLockScope(undefined)).verifiable(Times.once());
-        scopeMutexMock.setup(m => m.unlockScope(undefined)).verifiable(Times.once());
+        setupScopeMutex();
+        setupMergePromises([listenerPromise]);
 
         await testObject.invoke(testPayload);
     });
 
     test('addListener and invoke with scope', async () => {
         const scope = 'test_scope';
-        listenerMock.setup(l => l(testPayload)).verifiable(Times.once());
+        const listenerPromise = Promise.resolve();
+        listenerMock
+            .setup(l => l(testPayload))
+            .returns(() => listenerPromise)
+            .verifiable(Times.once());
 
         testObject.addListener(listenerMock.object);
 
-        scopeMutexMock.setup(m => m.tryLockScope(scope)).verifiable(Times.once());
-        scopeMutexMock.setup(m => m.unlockScope(scope)).verifiable(Times.once());
+        setupScopeMutex(scope);
+        setupMergePromises([listenerPromise]);
 
         await testObject.invoke(testPayload, scope);
     });
 
-    test('invoke calls all registered listeners', async () => {
-        const listenerMocks: IMock<(payload: TestPayload) => Promise<void>>[] = [
-            Mock.ofInstance(_ => Promise.resolve(), MockBehavior.Strict),
-            Mock.ofInstance(_ => Promise.resolve(), MockBehavior.Strict),
-            Mock.ofInstance(_ => Promise.resolve(), MockBehavior.Strict),
+    test('invoke merges all registered listeners', async () => {
+        const listenerPromises: Promise<void>[] = [
+            Promise.resolve(),
+            Promise.resolve(),
+            Promise.resolve(),
         ];
+        const listenerMocks: IMock<(payload: TestPayload) => Promise<void>>[] =
+            listenerPromises.map(listenerPromise => {
+                const listenerMock = Mock.ofInstance(_ => Promise.resolve(), MockBehavior.Strict);
+                listenerMock
+                    .setup(l => l(testPayload))
+                    .returns(() => listenerPromise)
+                    .verifiable(Times.once());
+                testObject.addListener(listenerMock.object);
 
-        listenerMocks.forEach(mock => {
-            mock.setup(l => l(testPayload)).verifiable(Times.once());
-            testObject.addListener(mock.object);
-        });
+                return listenerMock;
+            });
 
-        scopeMutexMock.setup(m => m.tryLockScope(undefined)).verifiable(Times.once());
-        scopeMutexMock.setup(m => m.unlockScope(undefined)).verifiable(Times.once());
+        setupScopeMutex();
+        setupMergePromises(listenerPromises);
 
         await testObject.invoke(testPayload);
 
         listenerMocks.forEach(mock => mock.verifyAll());
     });
 
-    test('invoke throws if a listener throws error', async () => {
+    test('invoke throws if merged promise throws', async () => {
         const testError = new Error('test error');
-        listenerMock
-            .setup(l => l(testPayload))
-            .returns(async () => {
-                throw testError;
-            })
+
+        testObject.addListener(() => null);
+
+        setupScopeMutex();
+        mergePromisesMock
+            .setup(m => m(It.isAny()))
+            .returns(() => Promise.reject(testError))
             .verifiable(Times.once());
-
-        testObject.addListener(listenerMock.object);
-
-        scopeMutexMock.setup(m => m.tryLockScope(undefined)).verifiable(Times.once());
-        scopeMutexMock.setup(m => m.unlockScope(undefined)).verifiable(Times.once());
 
         await expect(() => testObject.invoke(testPayload)).rejects.toThrow(testError);
     });
 
-    test('invoke aggregates errors if multiple listeners throw', async () => {
-        const testErrors: Error[] = [];
-        const listenerMocks: IMock<(payload: TestPayload) => Promise<void>>[] = [
-            Mock.ofInstance(_ => Promise.resolve(), MockBehavior.Strict),
-            Mock.ofInstance(_ => Promise.resolve(), MockBehavior.Strict),
-        ];
-        listenerMocks.forEach(mock => {
-            const testError = new Error('test error');
-            testErrors.push(testError);
-            mock.setup(l => l(testPayload))
-                .returns(async () => {
-                    throw testError;
-                })
-                .verifiable(Times.once());
-            testObject.addListener(mock.object);
-        });
+    function setupScopeMutex(scope?: string) {
+        scopeMutexMock.setup(m => m.tryLockScope(scope)).verifiable(Times.once());
+        scopeMutexMock.setup(m => m.unlockScope(scope)).verifiable(Times.once());
+    }
 
-        scopeMutexMock.setup(m => m.tryLockScope(undefined)).verifiable(Times.once());
-        scopeMutexMock.setup(m => m.unlockScope(undefined)).verifiable(Times.once());
-
-        try {
-            await testObject.invoke(testPayload);
-            fail('Expected invoke to throw error');
-        } catch (e) {
-            expect(e).toBeInstanceOf(AggregateError);
-
-            listenerMocks.forEach(mock => mock.verifyAll());
-
-            const aggregateError = e as AggregateError;
-            expect(aggregateError.errors.length).toBe(testErrors.length);
-            testErrors.forEach(error => {
-                expect(aggregateError.errors).toContain(error);
-            });
-        }
-    });
+    function setupMergePromises(promises: Promise<void>[]) {
+        mergedPromiseInternalMock.setup(m => m()).verifiable(Times.once());
+        mergePromisesMock
+            .setup(m => m(promises))
+            .returns(async () => mergedPromiseInternalMock.object())
+            .verifiable(Times.once());
+    }
 });
 
 interface TestPayload {
