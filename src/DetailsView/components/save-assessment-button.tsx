3604c027865341f71ab67d677c4361fb12095b21
Katy DeCorah <katydecorah@microsoft.com>
2022-07-15 15:12:53 -0400

    fix: Show user an "Assessment saved" dialog after they save the assessment

--- src/DetailsView/components/save-assessment-button.tsx
+++ src/DetailsView/components/save-assessment-button.tsx
@@ -1,7 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { Checkbox, Dialog, DialogFooter, DialogType, PrimaryButton, Stack } from '@fluentui/react';
+import { useBoolean } from '@fluentui/react-hooks';
 import { InsightsCommandButton } from 'common/components/controls/insights-command-button';
+import { NamedFC } from 'common/react/named-fc';
 import { DetailsViewActionMessageCreator } from 'DetailsView/actions/details-view-action-message-creator';
+import styles from 'DetailsView/components/common-dialog-styles.scss';
 import * as React from 'react';
 
 export interface SaveAssessmentButtonDeps {
@@ -13,17 +17,58 @@ export interface SaveAssessmentButtonProps {
     deps: SaveAssessmentButtonDeps;
 }
 
-export class SaveAssessmentButton extends React.Component<SaveAssessmentButtonProps> {
-    public render(): JSX.Element {
+export const SaveAssessmentButton = NamedFC<SaveAssessmentButtonProps>(
+    'SaveAssessmentButton',
+    props => {
+        const [hideDialog, { toggle: toggleHideDialog }] = useBoolean(true);
+        const [showDialogAgain, { toggle: toggleShowDialogAgain }] = useBoolean(true);
+
+        function handleSaveAssessmentClick() {
+            props.deps.detailsViewActionMessageCreator.saveAssessment;
+
+            if (showDialogAgain) {
+                toggleHideDialog();
+            }
+        }
+
         return (
-            <InsightsCommandButton
-                iconProps={{ iconName: 'Save' }}
-                download={this.props.download}
-                href={this.props.href}
-                onClick={this.props.deps.detailsViewActionMessageCreator.saveAssessment}
-            >
-                Save assessment
-            </InsightsCommandButton>
+            <>
+                <InsightsCommandButton
+                    iconProps={{ iconName: 'Save' }}
+                    download={props.download}
+                    href={props.href}
+                    onClick={handleSaveAssessmentClick}
+                >
+                    Save assessment
+                </InsightsCommandButton>
+                <Dialog
+                    hidden={hideDialog}
+                    onDismiss={toggleHideDialog}
+                    dialogContentProps={{
+                        type: DialogType.normal,
+                        title: 'Assessment saved',
+                    }}
+                    modalProps={{
+                        isBlocking: false,
+                        containerClassName: styles.insightsDialogMainOverride,
+                    }}
+                >
+                    <div className={styles.dialogBody}>
+                        To load this assessment, use the <strong>Load assessment</strong> button in
+                        the Accessibility Insights Assessment command bar.
+                    </div>
+                    <DialogFooter>
+                        <Stack horizontal horizontalAlign="space-between" verticalAlign="center">
+                            <Checkbox
+                                value={showDialogAgain}
+                                label="Don't show again"
+                                onChange={toggleShowDialogAgain}
+                            />
+                            <PrimaryButton onClick={toggleHideDialog} text="Got it" />
+                        </Stack>
+                    </DialogFooter>
+                </Dialog>
+            </>
         );
-    }
-}
+    },
+);
--- src/tests/unit/tests/DetailsView/components/save-assessment-button.test.tsx
+++ src/tests/unit/tests/DetailsView/components/save-assessment-button.test.tsx
@@ -1,5 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { Checkbox, Dialog, PrimaryButton } from '@fluentui/react';
+import * as reactHooks from '@fluentui/react-hooks';
+import { useBoolean } from '@fluentui/react-hooks';
 import { InsightsCommandButton } from 'common/components/controls/insights-command-button';
 import { DetailsViewActionMessageCreator } from 'DetailsView/actions/details-view-action-message-creator';
 import {
@@ -9,39 +12,78 @@ import {
 import { shallow } from 'enzyme';
 import * as React from 'react';
 import { EventStubFactory } from 'tests/unit/common/event-stub-factory';
-import { IMock, It, Mock, MockBehavior } from 'typemoq';
+import { IMock, It, Mock } from 'typemoq';
+
+jest.mock('@fluentui/react-hooks', () => ({
+    __esModule: true,
+    ...jest.requireActual('@fluentui/react-hooks'),
+}));
 
 describe('SaveAssessmentButton', () => {
     let propsStub: SaveAssessmentButtonProps;
     let detailsViewActionMessageCreatorMock: IMock<DetailsViewActionMessageCreator>;
+    let eventStub: any;
 
     beforeEach(() => {
-        detailsViewActionMessageCreatorMock = Mock.ofType(
-            DetailsViewActionMessageCreator,
-            MockBehavior.Strict,
-        );
+        detailsViewActionMessageCreatorMock = Mock.ofType<DetailsViewActionMessageCreator>();
         propsStub = {
             deps: { detailsViewActionMessageCreator: detailsViewActionMessageCreatorMock.object },
             download: 'download',
             href: 'url',
         };
+        eventStub = new EventStubFactory().createMouseClickEvent();
     });
 
     it('should render per the snapshot', () => {
         const rendered = shallow(<SaveAssessmentButton {...propsStub} />);
-
         expect(rendered.getElement()).toMatchSnapshot();
     });
 
-    it('should call saveAssessment on click', async () => {
-        const eventStub = new EventStubFactory().createMouseClickEvent() as any;
+    it('should call saveAssessment and dialog on click', async () => {
         const rendered = shallow(<SaveAssessmentButton {...propsStub} />);
-        const button = rendered.find(InsightsCommandButton);
-
+        // Click "Save assessment", Dialog should be visible and telemetry event is tracked
+        const saveAssessmentButton = rendered.find(InsightsCommandButton);
         detailsViewActionMessageCreatorMock.setup(m => m.saveAssessment(It.isAny())).verifiable();
 
-        await button.simulate('click', eventStub);
-
+        saveAssessmentButton.prop('onClick')(eventStub);
+        expect(rendered.find(Dialog).props().hidden).toEqual(false);
         detailsViewActionMessageCreatorMock.verifyAll();
+    });
+
+    it('should handle onDismiss properly', () => {
+        jest.spyOn(reactHooks, 'useBoolean');
+        const rendered = shallow(<SaveAssessmentButton {...propsStub} />);
+        // Click "Save assessment", Dialog should be visible
+        const saveAssessmentButton = rendered.find(InsightsCommandButton);
+        saveAssessmentButton.prop('onClick')(eventStub);
+        expect(rendered.find(Dialog).props().hidden).toEqual(false);
+
+        // Close Dialog, Dialog should be hidden
+        rendered.find(PrimaryButton).simulate('click');
+        expect(useBoolean).toHaveBeenCalled();
+        expect(rendered.find(Dialog).props().hidden).toEqual(true);
+
+        // Click "Save assessment" again, Dialog should be visible
+        rendered.find(InsightsCommandButton).prop('onClick')(eventStub);
+        expect(rendered.find(Dialog).props().hidden).toEqual(false);
+    });
+
+    it("should handle don't show again checkbox properly", () => {
+        jest.spyOn(reactHooks, 'useBoolean');
+        const rendered = shallow(<SaveAssessmentButton {...propsStub} />);
+        // Click "Save assessment"
+        const saveAssessmentButton = rendered.find(InsightsCommandButton);
+        saveAssessmentButton.prop('onClick')(eventStub);
+
+        // Click "don't show again"
+        rendered.find(Checkbox).simulate('change', { target: { checked: true } });
+        expect(useBoolean).toHaveBeenCalled();
+
+        // Close dialog
+        rendered.find(PrimaryButton).simulate('click');
+
+        // Click "Save assessment" again,  Dialog should be hidden
+        rendered.find(InsightsCommandButton).prop('onClick')(eventStub);
+        expect(rendered.find(Dialog).props().hidden).toEqual(true);
     });
 });
