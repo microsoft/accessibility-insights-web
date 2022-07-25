a0d9d55b3f9b4e73b4cfca83a49cf5b0f3f70cf6
Sarah Oslund <sfoslund@microsoft.com>
2022-07-25 10:07:02 -0700

    feat(mv3-part-5): Convert PathSnippetActions to async

--- src/background/actions/path-snippet-actions.ts
+++ src/background/actions/path-snippet-actions.ts
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { SyncAction } from 'common/flux/sync-action';
+import { AsyncAction } from 'common/flux/async-action';
 
 export class PathSnippetActions {
-    public readonly getCurrentState = new SyncAction<void>();
-    public readonly onAddPath = new SyncAction<string>();
-    public readonly onAddSnippet = new SyncAction<string>();
-    public readonly onClearData = new SyncAction<void>();
+    public readonly getCurrentState = new AsyncAction<void>();
+    public readonly onAddPath = new AsyncAction<string>();
+    public readonly onAddSnippet = new AsyncAction<string>();
+    public readonly onClearData = new AsyncAction<void>();
 }
