da6ccb4ec49de34d4c502eb4807892798383c5dd
Dan Bjorge <dan@dbjorge.net>
2022-07-25 13:13:33 -0400

    Update src/tests/unit/tests/scanner/custom-rules/widget-function.test.ts
    Co-authored-by: Katy DeCorah <katydecorah@gmail.com>

--- src/tests/unit/tests/scanner/custom-rules/widget-function.test.ts
+++ src/tests/unit/tests/scanner/custom-rules/widget-function.test.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-//import { difference, map } from 'lodash';
 import { difference } from 'lodash';
 import { hasCustomWidgetMarkup, withAxeSetup } from 'scanner/axe-utils';
 import {
