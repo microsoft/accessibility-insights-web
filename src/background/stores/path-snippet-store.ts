69c3963ca4fed613816ebe9e25e56bafbe2412f5
Sarah Oslund <sfoslund@microsoft.com>
2022-07-26 10:49:08 -0700

    feat(mv3-part-5): Convert PathSnippetActions to async (#5799)

--- src/background/stores/path-snippet-store.ts
+++ src/background/stores/path-snippet-store.ts
@@ -47,12 +47,15 @@ export class PathSnippetStore extends PersistentStore<PathSnippetStoreData> {
         this.pathSnippetActions.onClearData.addListener(this.onClearState);
     }
 
-    private onChangeProperty = (property: keyof PathSnippetStoreData, payload: string): void => {
+    private onChangeProperty = async (
+        property: keyof PathSnippetStoreData,
+        payload: string,
+    ): Promise<void> => {
         this.state[property] = payload;
         this.emitChanged();
     };
 
-    private onClearState = () => {
+    private onClearState = async (): Promise<void> => {
         this.state = this.getDefaultState();
         this.emitChanged();
     };
