edff74b2252064393c0680407e85f640143a93a3
pownkel <kepownel@microsoft.com>
2022-07-28 12:05:58 -0700

    Conver UnifiedScanResultActions to async

--- src/background/stores/card-selection-store.ts
+++ src/background/stores/card-selection-store.ts
@@ -156,7 +156,7 @@ export class CardSelectionStore extends PersistentStore<CardSelectionStoreData> 
         this.emitChanged();
     };
 
-    private onScanCompleted = (payload: UnifiedScanCompletedPayload): void => {
+    private onScanCompleted = async (payload: UnifiedScanCompletedPayload): Promise<void> => {
         this.state = this.getDefaultState();
         this.state.rules = {};
 
