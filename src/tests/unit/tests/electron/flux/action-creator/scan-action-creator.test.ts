15f91a306743455efb1ffdf7108c84a500d9237c
pownkel <55459788+pownkel@users.noreply.github.com>
2022-07-29 17:12:16 -0700

    feat(mv3-part-5): Convert UnifiedScanResultActions to async (#5841)
    #### Details
    - Convert SyncActions in UnifiedScanResultActions to AsyncActions
    - Convert ScanActions.scanStarted to an AsyncAction because the listener already contains a floating promise
    
    ##### Motivation
    Feature work
    
    ##### Context
    ScanActions is only used in the electron app, so not all actions need to be made async, but it seemed like a good idea to convert the one that was already essentially async. I've verified this change in Unified as well.

--- src/tests/unit/tests/electron/flux/action-creator/scan-action-creator.test.ts
+++ src/tests/unit/tests/electron/flux/action-creator/scan-action-creator.test.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { AsyncAction } from 'common/flux/async-action';
 import { SyncAction } from 'common/flux/sync-action';
 import { ScanActionCreator } from 'electron/flux/action-creator/scan-action-creator';
 import { DeviceConnectionActions } from 'electron/flux/action/device-connection-actions';
@@ -7,8 +8,9 @@ import { ScanActions } from 'electron/flux/action/scan-actions';
 import { IMock, Mock, Times } from 'typemoq';
 
 describe('ScanActionCreator', () => {
+    const actionExecutingScope = 'ScanActionCreator';
     let scanActionsMock: IMock<ScanActions>;
-    let scanStartedMock: IMock<SyncAction<void>>;
+    let scanStartedMock: IMock<AsyncAction<void>>;
     let deviceConnectionActionsMock: IMock<DeviceConnectionActions>;
     let statusUnknown: IMock<SyncAction<void>>;
 
@@ -16,7 +18,7 @@ describe('ScanActionCreator', () => {
 
     beforeEach(() => {
         scanActionsMock = Mock.ofType<ScanActions>();
-        scanStartedMock = Mock.ofType<SyncAction<void>>();
+        scanStartedMock = Mock.ofType<AsyncAction<void>>();
 
         scanActionsMock.setup(actions => actions.scanStarted).returns(() => scanStartedMock.object);
 
@@ -33,10 +35,16 @@ describe('ScanActionCreator', () => {
         );
     });
 
-    it('scans', () => {
-        testSubject.scan();
+    it('scans', async () => {
+        await testSubject.scan();
 
-        scanStartedMock.verify(scanStarted => scanStarted.invoke(), Times.once());
-        statusUnknown.verify(statusUnknown => statusUnknown.invoke(), Times.once());
+        scanStartedMock.verify(
+            async scanStarted => scanStarted.invoke(undefined, actionExecutingScope),
+            Times.once(),
+        );
+        statusUnknown.verify(
+            statusUnknown => statusUnknown.invoke(undefined, actionExecutingScope),
+            Times.once(),
+        );
     });
 });
