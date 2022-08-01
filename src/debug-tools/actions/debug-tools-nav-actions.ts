16944874ba54d6cbe4bae503446d8de6a04e2cab
Sarah Oslund <sfoslund@microsoft.com>
2022-08-01 10:53:17 -0700

    feat(mv3-part-5): Convert DebugToolsNavActions to async (#5855)

--- src/debug-tools/actions/debug-tools-nav-actions.ts
+++ src/debug-tools/actions/debug-tools-nav-actions.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { SyncAction } from 'common/flux/sync-action';
+import { AsyncAction } from 'common/flux/async-action';
 import { ToolsNavKey } from 'debug-tools/stores/debug-tools-nav-store';
 
 export class DebugToolsNavActions {
-    public readonly setSelectedTool = new SyncAction<ToolsNavKey>();
+    public readonly setSelectedTool = new AsyncAction<ToolsNavKey>();
 }
