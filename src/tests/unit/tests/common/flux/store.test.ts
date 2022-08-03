bfdbebdef21ab35f831464ca5e7999c028412dda
Sarah Oslund <sfoslund@microsoft.com>
2022-08-03 15:10:47 -0700

    feat(mv3-part-5): Allow event handler list and store to support async and sync listeners (#5877)
    * feat(mv3-part-5): Make event handler list and store generic
    
    * Remove unnecessary cast
    
    * Remove unnecessary cast

--- src/tests/unit/tests/common/flux/store.test.ts
+++ src/tests/unit/tests/common/flux/store.test.ts
@@ -1,55 +1,84 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { IMock, It, Mock, Times } from 'typemoq';
-
 import { Store } from '../../../../../common/flux/store';
 import { IsSameObject } from '../../../common/typemoq-helper';
 
 describe('StoreTest', () => {
-    let testObject: TestableStore;
-    let handlerMock: IMock<(store: TestableStore, args: any) => void>;
+    describe('SyncStore', () => {
+        let testObject: TestableStore<void>;
+        let handlerMock: IMock<(store: TestableStore<void>, args: any) => void>;
 
-    beforeEach(() => {
-        handlerMock = Mock.ofInstance((store, args) => {});
-        testObject = new TestableStore();
+        beforeEach(() => {
+            handlerMock = Mock.ofInstance((store, args) => {});
+            testObject = new TestableStore<void>();
+        });
+
+        test('addChangedListener', () => {
+            handlerMock
+                .setup(h => h(IsSameObject(testObject), It.isAny()))
+                .verifiable(Times.once());
+
+            testObject.addChangedListener(handlerMock.object);
+
+            testObject.emitChanged();
+
+            handlerMock.verifyAll();
+        });
+
+        test('removeChangedListener', () => {
+            handlerMock
+                .setup(h => h(IsSameObject(testObject), It.isAny()))
+                .verifiable(Times.once());
+
+            testObject.addChangedListener(handlerMock.object);
+
+            testObject.emitChanged();
+
+            handlerMock.verifyAll();
+
+            handlerMock.reset();
+
+            handlerMock.setup(h => h(It.isAny(), It.isAny())).verifiable(Times.never());
+
+            testObject.removeChangedListener(handlerMock.object);
+
+            testObject.emitChanged();
+
+            handlerMock.verifyAll();
+        });
     });
 
-    test('addChangedListener', () => {
-        handlerMock.setup(h => h(IsSameObject(testObject), It.isAny())).verifiable(Times.once());
+    describe('AsyncStore', () => {
+        let testObject: TestableStore<Promise<void>>;
+        let handlerMock: IMock<(store: TestableStore<Promise<void>>, args: any) => Promise<void>>;
 
-        testObject.addChangedListener(handlerMock.object);
+        beforeEach(() => {
+            handlerMock = Mock.ofInstance((store, args) => Promise.resolve());
+            testObject = new TestableStore<Promise<void>>();
+        });
 
-        testObject.emitChanged();
+        test('emitChanged returns a promise', async () => {
+            handlerMock
+                .setup(h => h(IsSameObject(testObject), It.isAny()))
+                .returns(h => Promise.resolve())
+                .verifiable(Times.once());
 
-        handlerMock.verifyAll();
+            testObject.addChangedListener(handlerMock.object);
+
+            const result = testObject.emitChanged();
+            await expect(result).resolves.toBeUndefined();
+
+            handlerMock.verifyAll();
+        });
     });
 
-    test('removeChangedListener', () => {
-        handlerMock.setup(h => h(IsSameObject(testObject), It.isAny())).verifiable(Times.once());
-
-        testObject.addChangedListener(handlerMock.object);
-
-        testObject.emitChanged();
-
-        handlerMock.verifyAll();
-
-        handlerMock.reset();
-
-        handlerMock.setup(h => h(It.isAny(), It.isAny())).verifiable(Times.never());
-
-        testObject.removeChangedListener(handlerMock.object);
-
-        testObject.emitChanged();
-
-        handlerMock.verifyAll();
-    });
-
-    class TestableStore extends Store {
+    class TestableStore<TReturn extends void | Promise<void>> extends Store<TReturn> {
         constructor() {
             super();
         }
-        public emitChanged(): void {
-            super.emitChanged();
+        public override emitChanged(): TReturn {
+            return super.emitChanged();
         }
     }
 });
