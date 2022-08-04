b42d2a9fbf56aadb1cad35b2c29f37ef7d62ed4a
Katy DeCorah <katydecorah@microsoft.com>
2022-08-04 11:30:13 -0400

    Remove double license

--- src/DetailsView/components/details-view-overlay/settings-panel/settings/save-assessment-dialog/save-assessment-dialog-settings.tsx
+++ src/DetailsView/components/details-view-overlay/settings-panel/settings/save-assessment-dialog/save-assessment-dialog-settings.tsx
@@ -1,7 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
 import { NamedFC } from 'common/react/named-fc';
 import * as React from 'react';
 import { GenericToggle } from '../../../../generic-toggle';
