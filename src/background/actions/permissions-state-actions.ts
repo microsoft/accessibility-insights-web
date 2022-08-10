6885ef87e7c92c45e9d295a5db29890cc5dc70fa
Sarah Oslund <sfoslund@microsoft.com>
2022-08-10 15:43:27 -0700

    feat(mv3-part-6): Make scoping and permission state actions async (#5903)
    * Make PermissionStateActions async
    
    * Make ScopingActions async

--- src/background/actions/permissions-state-actions.ts
+++ src/background/actions/permissions-state-actions.ts
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { SetAllUrlsPermissionStatePayload } from 'background/actions/action-payloads';
-import { SyncAction } from 'common/flux/sync-action';
+import { AsyncAction } from 'common/flux/async-action';
 
 export class PermissionsStateActions {
-    public readonly getCurrentState = new SyncAction<void>();
-    public readonly setPermissionsState = new SyncAction<SetAllUrlsPermissionStatePayload>();
+    public readonly getCurrentState = new AsyncAction<void>();
+    public readonly setPermissionsState = new AsyncAction<SetAllUrlsPermissionStatePayload>();
 }
