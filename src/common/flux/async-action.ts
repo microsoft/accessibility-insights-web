bd3061872dfecedb42c3654ad562b0eedc59ee1d
sfoslund <sfoslund@microsoft.com>
2022-08-11 11:19:48 -0700

    Don't await promises before unlocking scope

--- src/common/flux/async-action.ts
+++ src/common/flux/async-action.ts
@@ -17,12 +17,14 @@ export class AsyncAction<TPayload> implements Action<TPayload, Promise<void>> {
     ) {}
 
     public async invoke(payload: TPayload, scope?: string): Promise<void> {
+        let promiseResult: Promise<void>;
         this.scopeMutex.tryLockScope(scope);
 
         try {
-            await this.mergePromises(this.listeners.map(listener => listener(payload)));
+            promiseResult = this.mergePromises(this.listeners.map(listener => listener(payload)));
         } finally {
             this.scopeMutex.unlockScope(scope);
+            return promiseResult;
         }
     }
 
