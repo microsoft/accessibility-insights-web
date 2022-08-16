7c7f09cd6a0a3a2f53a51741c7f8b786709dd425
pownkel <55459788+pownkel@users.noreply.github.com>
2022-08-16 13:23:50 -0700

    fix: Create reflowable card footer buttons (#5852)
    #### Details
    Extracts out some of the work in #5745, plus additional refactors:
    - Add a new narrowModeThreshold for reflowing card footers
    - Create a new component, CardFooterFarButtons, a reflowable and refactored version of CardKebabMenuButton
    
    ##### Motivation
    Partially addresses #5609
    
    ##### Context
    Deferred to future PRs:
    - Further refactor CardFooterFarButtons to use store state instead of UI state to determine whether the issue filing dialog is open (this will allow renderIssueFilingSettingContent() to be extracted to a different file)
    - Replace CardKebabMenuButton with CardFooterFarButtons in the cards UI
    - Pass NarrowModeStatus through to the cards components

--- src/tests/unit/tests/DetailsView/components/issue-filing-dialog.test.tsx
+++ src/tests/unit/tests/DetailsView/components/issue-filing-dialog.test.tsx
@@ -87,6 +87,7 @@ describe('IssueFilingDialog', () => {
             selectedIssueFilingService: issueFilingServiceStub,
             selectedIssueData: selectedIssueDataStub,
             issueFilingServicePropertiesMap: issueFilingServicePropertiesMapStub,
+            afterClosed: () => null,
         };
 
         getSettingsFromStoreDataMock
@@ -258,7 +259,7 @@ describe('IssueFilingDialog', () => {
                 ...props,
                 isOpen: isOpenVal,
                 ...additionalProperties,
-            } as IssueFilingDialogProps;
+            } as Pick<any, keyof IssueFilingDialogProps>;
             const differentServiceData = {
                 differentProperty: 'different_property',
             };
