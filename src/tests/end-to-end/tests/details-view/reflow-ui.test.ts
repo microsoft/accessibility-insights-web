abf9d7c21cf752b0bbeb84e85745b138af9382f4
pownkel <kepownel@microsoft.com>
2022-10-04 17:13:29 -0700

    Add e2e test for card footer button reflow states

--- src/tests/end-to-end/tests/details-view/reflow-ui.test.ts
+++ src/tests/end-to-end/tests/details-view/reflow-ui.test.ts
@@ -1,13 +1,22 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { getNarrowModeThresholdsForWeb } from 'common/narrow-mode-thresholds';
+import { TargetPage } from 'tests/end-to-end/common/page-controllers/target-page';
+import {
+    DEFAULT_PAGE_ELEMENT_WAIT_TIMEOUT_MS,
+    DEFAULT_TARGET_PAGE_SCAN_TIMEOUT_MS,
+} from 'tests/end-to-end/common/timeouts';
 import { Browser } from '../../common/browser';
 import { launchBrowser } from '../../common/browser-factory';
-import { navMenuSelectors } from '../../common/element-identifiers/details-view-selectors';
+import {
+    detailsViewSelectors,
+    fastPassAutomatedChecksSelectors,
+    navMenuSelectors,
+} from '../../common/element-identifiers/details-view-selectors';
 import { DetailsViewPage } from '../../common/page-controllers/details-view-page';
 import { scanForAccessibilityIssues } from '../../common/scan-for-accessibility-issues';
 
-describe('Details View -> Assessment -> Reflow', () => {
+describe('Details View ->', () => {
     let browser: Browser;
     let detailsViewPage: DetailsViewPage;
     const height = 400;
@@ -18,53 +27,125 @@ describe('Details View -> Assessment -> Reflow', () => {
             suppressFirstTimeDialog: true,
             addExtraPermissionsToManifest: 'fake-activeTab',
         });
-
-        detailsViewPage = (await browser.newAssessment()).detailsViewPage;
     });
 
     afterAll(async () => {
         await browser?.close();
     });
 
-    const { commandBarMenuButtonSelectors, hamburgerMenuButtonSelectors } = navMenuSelectors;
-
-    const commandBarWindowWidth = narrowModeThresholds.collapseCommandBarThreshold - 1;
-    const hamburgerButtonWindowWidth = narrowModeThresholds.collapseHeaderAndNavThreshold - 1;
-
-    describe.each`
-        componentName           | componentSelectors               | width
-        ${'command bar button'} | ${commandBarMenuButtonSelectors} | ${commandBarWindowWidth}
-        ${'hamburger button'}   | ${hamburgerMenuButtonSelectors}  | ${hamburgerButtonWindowWidth}
-    `('With $componentName visible', ({ componentName, componentSelectors, width }) => {
+    describe('Assessment -> Reflow', () => {
         beforeAll(async () => {
-            await detailsViewPage.setViewport(width, height);
-            await detailsViewPage.waitForSelector(componentSelectors.collapsed);
+            detailsViewPage = (await browser.newAssessment()).detailsViewPage;
         });
 
-        it.each([true, false])(
-            `should pass accessibility validation with high contrast mode=%s`,
-            async highContrastMode => {
-                await scanForA11yIssuesWithHighContrast(highContrastMode);
-            },
-        );
+        afterAll(async () => {
+            await detailsViewPage.close();
+        });
 
-        describe(`with ${componentName} expanded`, () => {
+        const { commandBarMenuButtonSelectors, hamburgerMenuButtonSelectors } = navMenuSelectors;
+
+        const commandBarWindowWidth = narrowModeThresholds.collapseCommandBarThreshold - 1;
+        const hamburgerButtonWindowWidth = narrowModeThresholds.collapseHeaderAndNavThreshold - 1;
+
+        describe.each`
+            componentName           | componentSelectors               | width
+            ${'command bar button'} | ${commandBarMenuButtonSelectors} | ${commandBarWindowWidth}
+            ${'hamburger button'}   | ${hamburgerMenuButtonSelectors}  | ${hamburgerButtonWindowWidth}
+        `('With $componentName visible', ({ componentName, componentSelectors, width }) => {
             beforeAll(async () => {
-                await setButtonExpandedState(componentSelectors, true);
-            });
-
-            afterAll(async () => {
-                await setButtonExpandedState(componentSelectors, false);
+                await resizeDetailsView(width, componentSelectors.collapsed);
             });
 
             it.each([true, false])(
-                `should pass accessibility validation with command bar menu open and high contrast mode=%s`,
+                `should pass accessibility validation with high contrast mode=%s`,
                 async highContrastMode => {
                     await scanForA11yIssuesWithHighContrast(highContrastMode);
                 },
             );
+
+            describe(`with ${componentName} expanded`, () => {
+                beforeAll(async () => {
+                    await setButtonExpandedState(componentSelectors, true);
+                });
+
+                afterAll(async () => {
+                    await setButtonExpandedState(componentSelectors, false);
+                });
+
+                it.each([true, false])(
+                    `should pass accessibility validation with command bar menu open and high contrast mode=%s`,
+                    async highContrastMode => {
+                        await scanForA11yIssuesWithHighContrast(highContrastMode);
+                    },
+                );
+            });
         });
     });
+
+    describe('Fastpass -> Reflow', () => {
+        let targetPage: TargetPage;
+
+        const cardFooterWindowWidth = narrowModeThresholds.collapseCardFooterThreshold - 1;
+        const cardFooterSelectors = fastPassAutomatedChecksSelectors.cardFooterKebabButton;
+
+        beforeAll(async () => {
+            targetPage = await browser.newTargetPage({
+                testResourcePath: 'all.html',
+            });
+            await browser.newPopupPage(targetPage);
+            detailsViewPage = await browser.newDetailsViewPage(targetPage);
+            await detailsViewPage.waitForSelector(
+                detailsViewSelectors.automatedChecksResultSection,
+                {
+                    timeout: DEFAULT_TARGET_PAGE_SCAN_TIMEOUT_MS,
+                },
+            );
+            await detailsViewPage.waitForSelector(fastPassAutomatedChecksSelectors.expandButton, {
+                timeout: DEFAULT_PAGE_ELEMENT_WAIT_TIMEOUT_MS,
+            });
+
+            await resizeDetailsView(cardFooterWindowWidth);
+            await expandCard();
+        });
+
+        afterAll(async () => {
+            await detailsViewPage.close();
+            await targetPage.close();
+        });
+
+        describe.each([true, false])('With high contrast mode=%s', highContrastMode => {
+            it('and card kebab menu collapsed', async () => {
+                await scanForA11yIssuesWithHighContrast(highContrastMode);
+            });
+
+            it('and card kebab menu expanded', async () => {
+                await setButtonExpandedState(cardFooterSelectors, true);
+
+                await scanForA11yIssuesWithHighContrast(highContrastMode);
+
+                await setButtonExpandedState(cardFooterSelectors, false);
+            });
+        });
+
+        async function expandCard(): Promise<void> {
+            await detailsViewPage.waitForSelector(fastPassAutomatedChecksSelectors.expandButton, {
+                timeout: DEFAULT_PAGE_ELEMENT_WAIT_TIMEOUT_MS,
+            });
+
+            await detailsViewPage.clickSelector(fastPassAutomatedChecksSelectors.expandButton);
+            await detailsViewPage.waitForSelector(cardFooterSelectors.collapsed);
+        }
+    });
+
+    async function resizeDetailsView(
+        screenWidth: number,
+        expectedSelector?: string,
+    ): Promise<void> {
+        await detailsViewPage.setViewport(screenWidth, height);
+        if (expectedSelector) {
+            await detailsViewPage.waitForSelector(expectedSelector);
+        }
+    }
 
     async function scanForA11yIssuesWithHighContrast(highContrastMode: boolean): Promise<void> {
         await browser.setHighContrastMode(highContrastMode);
