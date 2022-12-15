d5a9ba7a4ef1f6315a7fac4ddce933cc23fc4e87
Dan Bjorge <danielbj@microsoft.com>
2022-12-15 17:59:29 -0500

    Update popup-view per feedback from ferbonn

--- src/popup/components/popup-view.tsx
+++ src/popup/components/popup-view.tsx
@@ -1,5 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { Link } from '@fluentui/react';
+import { TelemetryEventSource } from 'common/extension-telemetry-events';
+import { DetailsViewPivotType } from 'common/types/store-data/details-view-pivot-type';
 import * as React from 'react';
 import { BrowserAdapter } from '../../common/browser-adapters/browser-adapter';
 import { TelemetryPermissionDialogDeps } from '../../common/components/telemetry-permission-dialog';
@@ -50,31 +53,74 @@ export class PopupView extends React.Component<PopupViewProps> {
         super(props);
     }
 
+    private openAssessment = event =>
+        this.props.deps.popupActionMessageCreator.openDetailsView(
+            event,
+            null,
+            TelemetryEventSource.LaunchPadAssessment,
+            DetailsViewPivotType.assessment,
+        );
+
     public render(): JSX.Element {
+        const openTheAssessmentLink = (
+            <Link
+                role="link"
+                className="insights-link"
+                id="open-assessment-link"
+                onClick={this.openAssessment}
+            >
+                open the assessment
+            </Link>
+        );
+
         return (
             <div className="ms-Fabric end-of-life-panel">
                 <div className="main-section">
                     <div className="popup-grid">
                         <div className="launch-panel-general-container">
-                            <>
-                                <div>
-                                    Thank you for helping the Accessibility Insights team test. This
-                                    extension has now reached its end of life, please return to
-                                    using our mainline extensions:
-                                </div>
-                                <br />
-                                <a href="https://chrome.google.com/webstore/detail/accessibility-insights-fo/hbcplehnakffdldhldncjlnbpfgogbem">
-                                    Canary (released continuously)
-                                </a>
-                                <br />
-                                <a href="https://chrome.google.com/webstore/detail/accessibility-insights-fo/nnmjfbmebeckhpejobgjjjnchlljiagp">
-                                    Insider (on feature completion)
-                                </a>
-                                <br />
-                                <a href="https://chrome.google.com/webstore/detail/accessibility-insights-fo/pbjjkligggfmakdaogkfomddhfmpjeni">
-                                    Production (after validation in insider)
-                                </a>
-                            </>
+                            <p>
+                                Thank you for helping the Accessibility Insights team test this
+                                preview extension. The "Insider (M3)" build now been released into
+                                production. Please continue using Accessibility Insights for Web by
+                                switching to the Production version:
+                            </p>
+                            <ul>
+                                <li>
+                                    <Link
+                                        role="link"
+                                        className="insights-link"
+                                        id="install-production-chrome-link"
+                                        target="_blank"
+                                        href="https://chrome.google.com/webstore/detail/accessibility-insights-fo/pbjjkligggfmakdaogkfomddhfmpjeni"
+                                    >
+                                        Install for Google Chrome
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        role="link"
+                                        className="insights-link"
+                                        id="install-production-edge-link"
+                                        target="_blank"
+                                        href="https://microsoftedge.microsoft.com/addons/detail/accessibility-insights-fo/ghbhpcookfemncgoinjblecnilppimih"
+                                    >
+                                        Install for Microsoft Edge
+                                    </Link>
+                                </li>
+                            </ul>
+                            <p>
+                                If you have an assessment in progress which you would like to
+                                continue working on in the Production version,{' '}
+                                {openTheAssessmentLink} and select the "Save Assessment" button to
+                                save your assessment in progress to a file, then use the "Load
+                                Assessment" button from the Production version to continue where you
+                                left off.
+                            </p>
+                            <p>
+                                Once you have switched to the Production version, you can remove the
+                                "Accessibility Insights for Web - Insider (M3)" build from your
+                                browser's "Extensions" settings page.
+                            </p>
                         </div>
                     </div>
                 </div>
