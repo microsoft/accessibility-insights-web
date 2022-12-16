microsoft/accessibility-insights-web

commit 7532456e2d5f2c9f069fede45f66b278e9b074e5

feat(electron-update): Upgrade electron to 22.0.0 (#6243)
#### Details

Upgrade electron to version +22

##### Motivation

Only the three most recent major versions of Electron are supported. We
are currently on 19.1.3and the latest version is 22.0.0 so we need to
update.

#### Pull request checklist
<!-- If a checklist item is not applicable to this change, write "n/a"
in the checkbox -->
- [n/a] Addresses an existing issue: #0000
- [x] Ran `yarn null:autoadd`
- [x] Ran `yarn fastpass`
- [n/a] Added/updated relevant unit test(s) (and ran `yarn test`)
- [ ] Verified code coverage for the changes made. Check coverage report
at: `<rootDir>/test-results/unit/coverage`
- [x] PR title *AND* final merge commit title both start with a semantic
tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`). See
`CONTRIBUTING.md`.
- [n/a] (UI changes only) Added screenshots/GIFs to description above
- [n/a] (UI changes only) Verified usability with NVDA/JAWS

Co-authored-by: Tania Martinez Villagomez <taniamar@microsoft.com>

pipeline/scripts/electron-build-id.js
@@ -6,4 +6,4 @@ This number identifies an internally-maintained Electron build
 used in our releases. Each Electron update in package.json requires
 a corresponding change here.
 */
-exports.electronBuildId = '16272414';
+exports.electronBuildId = '14306133';

src/tests/electron/tests/framework-no-codecs.test.ts
@@ -58,8 +58,8 @@ it('electron versions in package.json and build id are updated together', async 
     };
     expect(versions).toMatchInlineSnapshot(`
         {
-          "electronBuildId": "16272414",
-          "electronVersion": "19.1.3",
+          "electronBuildId": "14306133",
+          "electronVersion": "22.0.0",
         }
     `);
 });

commit ba04582f61ac8a91a8de5486389269210f7bc91d

Fix: Correcting Electron v22.0.0 build id. (#6269)
#### Details

Fixing the wrong Electron build id was making the Canary Unified
pipeline fail.

##### Motivation

Fix Canary Unified Pipeline runs.

##### Context

Error introduced in #6243, failed pipeline example:


https://dev.azure.com/accessibility-insights-private/Accessibility%20Insights%20(private)/_build/results?buildId=40498&view=results

#### Pull request checklist
<!-- If a checklist item is not applicable to this change, write "n/a"
in the checkbox -->
- [ ] Addresses an existing issue: #0000
- [ ] Ran `yarn null:autoadd`
- [x] Ran `yarn fastpass`
- [x] Added/updated relevant unit test(s) (and ran `yarn test`)
- [ ] Verified code coverage for the changes made. Check coverage report
at: `<rootDir>/test-results/unit/coverage`
- [x] PR title *AND* final merge commit title both start with a semantic
tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`). See
`CONTRIBUTING.md`.
- [n/a] (UI changes only) Added screenshots/GIFs to description above
- [n/a] (UI changes only) Verified usability with NVDA/JAWS

Co-authored-by: Tania Martinez Villagomez <taniamar@microsoft.com>

pipeline/scripts/electron-build-id.js
@@ -6,4 +6,4 @@ This number identifies an internally-maintained Electron build
 used in our releases. Each Electron update in package.json requires
 a corresponding change here.
 */
-exports.electronBuildId = '14306133';
+exports.electronBuildId = '17374396';

src/tests/electron/tests/framework-no-codecs.test.ts
@@ -58,7 +58,7 @@ it('electron versions in package.json and build id are updated together', async 
     };
     expect(versions).toMatchInlineSnapshot(`
         {
-          "electronBuildId": "14306133",
+          "electronBuildId": "17374396",
           "electronVersion": "22.0.0",
         }
     `);