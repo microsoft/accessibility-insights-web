a14fbd765e69906d5089a984e6530808b1f2ce59
Madalyn <3230904+madalynrose@users.noreply.github.com>
2023-01-06 10:52:39 -0800

    fix(medium-pass-2): fix requirements list to contain correct contrast requirement (#6315)
    #### Details
    
    Currently, the requirements list for quick assess lists `Adaptable
    Content > Contrast` when it should list `Contrast > UI components`.
    Also, `Native Widgets > Instructions` is missing from the list. This PR
    corrects this.
    
    ##### Motivation
    
    Feature work
    
    ##### Context
    
    N/A
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a"
    in the checkbox -->
    - [n/a] Addresses an existing issue: #0000
    - [x] Ran `yarn fastpass`
    - [x] Added/updated relevant unit test(s) (and ran `yarn test`)
    - [x] Verified code coverage for the changes made. Check coverage report
    at: `<rootDir>/test-results/unit/coverage`
    - [x] PR title *AND* final merge commit title both start with a semantic
    tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`). See
    `CONTRIBUTING.md`.
    - [n/a] (UI changes only) Added screenshots/GIFs to description above
    - [n/a] (UI changes only) Verified usability with NVDA/JAWS
    
    Co-authored-by: madalynrose <madparker@microsoft.com>

--- src/assessments/medium-pass-requirements.ts
+++ src/assessments/medium-pass-requirements.ts
@@ -1,10 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { AdaptableContentTestStep } from 'assessments/adaptable-content/test-steps/test-step';
+import { ContrastTestStep } from 'assessments/contrast/test-steps/test-steps';
 import { HeadingsTestStep } from 'assessments/headings/test-steps/test-steps';
 import { ImagesTestStep } from 'assessments/images/test-steps/test-steps';
 import { KeyboardInteractionTestStep } from 'assessments/keyboard-interaction/test-steps/test-steps';
 import { LinksTestStep } from 'assessments/links/test-steps/test-steps';
+import { NativeWidgetsTestStep } from 'assessments/native-widgets/test-steps/test-steps';
 import { RepetitiveContentTestStep } from 'assessments/repetitive-content/test-steps/test-steps';
 import { visibleFfocusOrderTestStep } from 'assessments/visible-focus-order/test-steps/test-steps';
 import { VisualizationType } from 'common/types/visualization-type';
@@ -14,11 +16,12 @@ export const MediumPassRequirementKeys: string[] = [
     KeyboardInteractionTestStep.keyboardNavigation,
     LinksTestStep.linkPurpose,
     ImagesTestStep.imageFunction,
-    visibleFfocusOrderTestStep.visibleFocus,
-    AdaptableContentTestStep.contrast,
+    visibleFfocusOrderTestStep.focusOrder,
+    ContrastTestStep.uiComponents,
     HeadingsTestStep.missingHeadings,
     HeadingsTestStep.headingLevel,
     RepetitiveContentTestStep.bypassBlocks,
+    NativeWidgetsTestStep.instructions,
     AdaptableContentTestStep.reflow,
 ];
 
@@ -27,10 +30,11 @@ export const MediumPassRequirementMap: DictionaryStringTo<VisualizationType> = {
         VisualizationType.KeyboardInteractionMediumPass,
     [LinksTestStep.linkPurpose]: VisualizationType.LinksMediumPass,
     [ImagesTestStep.imageFunction]: VisualizationType.ImagesMediumPass,
-    [visibleFfocusOrderTestStep.visibleFocus]: VisualizationType.VisibleFocusOrderMediumPass,
-    [AdaptableContentTestStep.contrast]: VisualizationType.AdaptableContentMediumPass,
+    [visibleFfocusOrderTestStep.focusOrder]: VisualizationType.VisibleFocusOrderMediumPass,
+    [ContrastTestStep.uiComponents]: VisualizationType.ContrastMediumPass,
     [HeadingsTestStep.missingHeadings]: VisualizationType.HeadingsMediumPass,
     [HeadingsTestStep.headingLevel]: VisualizationType.HeadingsMediumPass,
     [RepetitiveContentTestStep.bypassBlocks]: VisualizationType.RepetitiveContentMediumPass,
+    [NativeWidgetsTestStep.instructions]: VisualizationType.NativeWidgetsMediumPass,
     [AdaptableContentTestStep.reflow]: VisualizationType.AdaptableContentMediumPass,
 };
