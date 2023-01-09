942a6f3fc6ab9bac12cf975de1e05a60259848da
madalynrose <madparker@microsoft.com>
2023-01-09 16:11:02 -0500

    add help-links files back in (merge fail)

---
+++ src/DetailsView/components/help-links.tsx
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { ExternalLink, ExternalLinkDeps } from 'common/components/external-link';
+import { NamedFC } from 'common/react/named-fc';
+import { HyperlinkDefinition } from 'common/types/hyperlink-definition';
+import * as React from 'react';
+
+import styles from './help-links.scss';
+
+export type HelpLinksDeps = ExternalLinkDeps;
+
+export interface HelpLinksProps {
+    deps: HelpLinksDeps;
+    linkInformation: HyperlinkDefinition[];
+}
+
+export const HelpLinks = NamedFC('HelpLinks', (props: HelpLinksProps) => {
+    const { linkInformation } = props;
+    return (
+        <>
+            {linkInformation.map((link: HyperlinkDefinition) => (
+                <div className={styles.helpLink} key={link.href}>
+                    <ExternalLink deps={props.deps} href={link.href}>
+                        {link.text}
+                    </ExternalLink>
+                </div>
+            ))}
+        </>
+    );
+});
