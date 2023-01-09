f0b60fdf13e66099f268bad76f5c947b360592f9
Katy DeCorah <katydecorah@microsoft.com>
2023-01-09 10:23:28 -0500

    chore: Add message to stylelint `selector-class-pattern` and improve the pattern

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -9,8 +9,13 @@ module.exports = {
         'declaration-block-no-redundant-longhand-properties': null, // Prefer longhand to improve readability
         'shorthand-property-no-redundant-values': null, // Prefer longhand to improve readability
         'scss/comment-no-empty': null, // Disabled to allow for paragraph breaks in longer comments which is not supported yet: https://github.com/stylelint-scss/stylelint-scss/issues/606
-        'selector-class-pattern':
-            '^(ms-([A-Z][a-z0-9]*)(-[a-z0-9]+)*)|(([a-z][a-z0-9]*)(-[a-z0-9]+)*)$', // Allows: kebab case and ms-Kebab-case
+        'selector-class-pattern': [
+            '^(ms-([A-Z][a-z0-9]*)(-[a-z0-9]+)*)|^(is-[a-z][a-zA-Z0-9]+)|^(([a-z][a-z0-9]*)(-[a-z0-9]+)*)$',
+            {
+                message:
+                    'Selectors should use `kebab-case`. For FluentUI selectors, use `ms-Upper-kebab-case` or `is-lowerCamelCase`.',
+            },
+        ],
         'declaration-property-max-values': { padding: 1, margin: 1 }, // Limit shorthand to improve readability
         'annotation-no-unknown': null, // Disabled per guidance at https://github.com/stylelint-scss/stylelint-config-recommended-scss/issues/149
         'property-no-vendor-prefix': null, // Disabled, we decided against using a tool like autoprefixer: https://github.com/microsoft/accessibility-insights-web/pull/6278#issuecomment-1372793392
