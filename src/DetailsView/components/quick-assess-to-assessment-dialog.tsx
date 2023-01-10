be18fac7101f7fd1a1ce94768f5b9e35799f9ac5
Wahaj Abid <waabid@microsoft.com>
2023-01-09 17:58:11 -0800

    fixed typos + removed unnecessary css usage

--- src/DetailsView/components/quick-assess-to-assessment-dialog.tsx
+++ src/DetailsView/components/quick-assess-to-assessment-dialog.tsx
@@ -1,14 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import {
-    css,
-    DefaultButton,
-    Dialog,
-    DialogFooter,
-    IDialogProps,
-    PrimaryButton,
-} from '@fluentui/react';
+import { DefaultButton, Dialog, DialogFooter, IDialogProps, PrimaryButton } from '@fluentui/react';
 import * as Markup from 'assessments/markup';
 import { NamedFC } from 'common/react/named-fc';
 import { DetailsViewActionMessageCreator } from 'DetailsView/actions/details-view-action-message-creator';
@@ -33,11 +26,11 @@ export const QuickAssessToAssessmentDialog = NamedFC<QuickAssessToAssessmentDial
             hidden: !props.isShown,
             title: 'Continue to assessment',
             onDismiss: dataTransferViewController.hideQuickAssessToAssessmentConfirmDialog,
-            containerClassName: css(commonDialogStyles.insightsDialogMainOverride),
+            containerClassName: commonDialogStyles.insightsDialogMainOverride,
         };
 
         const descriptionText =
-            'This will allow you to continue accessibility tesitng with addtitional tests and requirements in assessment. All your existing data in quick assess will be transfered into assessment.';
+            'This will allow you to continue accessibility testing with additional tests and requirements in assessment. All your existing data in quick assess will be transfered into assessment.';
 
         const noteText =
             'If you have an assessment in progress, the current quick assess evaluation data will replace the assessment evaluation and previous progress will be lost.';
