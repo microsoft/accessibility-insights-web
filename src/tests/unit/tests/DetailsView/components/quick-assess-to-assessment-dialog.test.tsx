8fc2529daecf9b0c280282f6398b90fbee268883
Wahaj <32555133+waabid@users.noreply.github.com>
2023-01-10 14:29:36 -0800

    feat(medium-pass-2): add quick assess to assessment dialog (#6329)
    #### Details
    
    Adds a new dialog to confirm transfer from quick assess to assessment.
    
    ##### Motivation
    
    Feature work.
    
    ##### Context
    
    Not wired up in this PR but this is what it looks like when shown
    locally:
    
    
    ![image](https://user-images.githubusercontent.com/32555133/211444209-f0366e83-c874-483c-add0-bbe577f76d72.png)
    
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a"
    in the checkbox -->
    - [ ] Addresses an existing issue: #0000
    - [x] Ran `yarn fastpass`
    - [x] Added/updated relevant unit test(s) (and ran `yarn test`)
    - [x] Verified code coverage for the changes made. Check coverage report
    at: `<rootDir>/test-results/unit/coverage`
    - [x] PR title *AND* final merge commit title both start with a semantic
    tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`). See
    `CONTRIBUTING.md`.
    - [x] (UI changes only) Added screenshots/GIFs to description above
    - [ ] (UI changes only) Verified usability with NVDA/JAWS

---
+++ src/tests/unit/tests/DetailsView/components/quick-assess-to-assessment-dialog.test.tsx
@@ -0,0 +1,87 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { DefaultButton, PrimaryButton } from '@fluentui/react';
+import { SupportedMouseEvent } from 'common/telemetry-data-factory';
+import { DetailsViewPivotType } from 'common/types/store-data/details-view-pivot-type';
+import { DetailsViewActionMessageCreator } from 'DetailsView/actions/details-view-action-message-creator';
+import {
+    QuickAssessToAssessmentDialog,
+    QuickAssessToAssessmentDialogProps,
+} from 'DetailsView/components/quick-assess-to-assessment-dialog';
+import { DataTransferViewController } from 'DetailsView/data-transfer-view-controller';
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import { IMock, Mock, Times } from 'typemoq';
+
+describe('QuickAssessToAssessmentDialog', () => {
+    let dataTransferViewControllerMock: IMock<DataTransferViewController>;
+    let detailsViewActionMessageCreatorMock: IMock<DetailsViewActionMessageCreator>;
+    let props: QuickAssessToAssessmentDialogProps;
+
+    beforeEach(() => {
+        dataTransferViewControllerMock = Mock.ofType(DataTransferViewController);
+        detailsViewActionMessageCreatorMock = Mock.ofType<DetailsViewActionMessageCreator>();
+        props = {
+            deps: {
+                detailsViewActionMessageCreator: detailsViewActionMessageCreatorMock.object,
+                dataTransferViewController: dataTransferViewControllerMock.object,
+            },
+        } as QuickAssessToAssessmentDialogProps;
+    });
+
+    test('dialog is hidden when isShown is false', () => {
+        props.isShown = false;
+        const testSubject = shallow(
+            <QuickAssessToAssessmentDialog {...props}></QuickAssessToAssessmentDialog>,
+        );
+        expect(testSubject.getElement()).toMatchSnapshot();
+    });
+
+    test('dialog is not hidden when isShown is true', () => {
+        props.isShown = true;
+        const testSubject = shallow(
+            <QuickAssessToAssessmentDialog {...props}></QuickAssessToAssessmentDialog>,
+        );
+        expect(testSubject.getElement()).toMatchSnapshot();
+    });
+
+    test('onclick: cancel', async () => {
+        props.isShown = true;
+
+        const testSubject = shallow(
+            <QuickAssessToAssessmentDialog {...props}></QuickAssessToAssessmentDialog>,
+        );
+        const onClick = testSubject.find(DefaultButton).prop('onClick');
+        await onClick(null);
+
+        dataTransferViewControllerMock.verify(
+            m => m.hideQuickAssessToAssessmentConfirmDialog(),
+            Times.once(),
+        );
+    });
+
+    test('onclick: continue to assessment', async () => {
+        props.isShown = true;
+        const eventStub = {} as SupportedMouseEvent;
+        const testSubject = shallow(
+            <QuickAssessToAssessmentDialog {...props}></QuickAssessToAssessmentDialog>,
+        );
+        const onClick = testSubject.find(PrimaryButton).prop('onClick');
+
+        await onClick(eventStub as any);
+
+        detailsViewActionMessageCreatorMock.verify(
+            m => m.confirmDataTransferToAssessment(eventStub),
+            Times.once(),
+        );
+        detailsViewActionMessageCreatorMock.verify(
+            m => m.sendPivotItemClicked(DetailsViewPivotType[DetailsViewPivotType.assessment]),
+            Times.once(),
+        );
+        dataTransferViewControllerMock.verify(
+            m => m.hideQuickAssessToAssessmentConfirmDialog(),
+            Times.once(),
+        );
+    });
+});
