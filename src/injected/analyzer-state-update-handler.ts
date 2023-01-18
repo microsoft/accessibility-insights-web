microsoft/accessibility-insights-web

commit c10ca4c0a7355098f723e0d304eba1d244d5c697

fix: Add logic to detect failed injection in edge IE mode (#6183)
#### Details

Add logic to detect failed injections into the target page, retry, and
if failure continues, sends a notification to the user.

##### Motivation

Addresses issue
https://github.com/microsoft/accessibility-insights-web/issues/5802

##### Context

When investigating
https://github.com/microsoft/accessibility-insights-web/issues/5802, it
appears that we're failing because we attempt to inject the content
scripts into the target page, the injection fails, and we never handle
the error, which causes the details view to show that it's 'scanning'
forever. To address this, this PR adds logic to handle an injection
failure and, if it persists, send a notification to the user.

I'm open to suggestions for how to display the error when the injection
fails. Right now I'm sending a notification since it feels minimally
intrusive, but originally I implemented an 'injection failed' view
similar to the ['no content'
view](https://github.com/microsoft/accessibility-insights-web/blob/main/src/DetailsView/components/no-content-available/no-content-available.tsx)
to be displayed in the details view. The problem with this was that it
prevented users from accessing settings/ the left nav bar when the
target page cannot be communicated with. This causes issues for our e2e
tests as well because we grant minimal permissions for most of those
tests, meaning often we can't communicate with the target page because
of these limited permissions and as a result the injection failed path
is triggered. This is fine when we're just sending a notification on
injection failed, but would mean significant changes to e2e tests if we
were to use a more intrusive view.

#### Pull request checklist
<!-- If a checklist item is not applicable to this change, write "n/a"
in the checkbox -->
- [x] Addresses an existing issue:

src/injected/analyzer-state-update-handler.ts
@@ -4,7 +4,10 @@ import { Requirement } from 'assessments/types/requirement';
 import { VisualizationType } from 'common/types/visualization-type';
 import { VisualizationConfiguration } from '../common/configs/visualization-configuration';
 import { VisualizationConfigurationFactory } from '../common/configs/visualization-configuration-factory';
-import { VisualizationStoreData } from '../common/types/store-data/visualization-store-data';
+import {
+    InjectingState,
+    VisualizationStoreData,
+} from '../common/types/store-data/visualization-store-data';
 
 export class AnalyzerStateUpdateHandler {
     protected prevState: VisualizationStoreData;
@@ -68,11 +71,14 @@ export class AnalyzerStateUpdateHandler {
         prevState: VisualizationStoreData,
         currState: VisualizationStoreData,
     ): void {
-        if (currState.scanning != null && currState.injectingRequested !== true) {
+        if (
+            currState.scanning != null &&
+            currState.injectingState !== InjectingState.injectingRequested
+        ) {
             if (
                 prevState == null ||
                 prevState.scanning !== currState.scanning ||
-                prevState.injectingRequested !== currState.injectingRequested
+                prevState.injectingState !== currState.injectingState
             ) {
                 this.startScan(currState.scanning);
             }

src/tests/unit/tests/injected/analyzer-state-update-handler.test.ts
@@ -9,6 +9,7 @@ import {
     VisualizationConfigurationFactory,
 } from '../../../../common/configs/visualization-configuration-factory';
 import {
+    InjectingState,
     ScanData,
     VisualizationStoreData,
 } from '../../../../common/types/store-data/visualization-store-data';
@@ -69,7 +70,7 @@ describe('AnalyzerStateUpdateHandlerTest', () => {
     test('do not start scan if inject in progress and no prevState', () => {
         const state = new VisualizationStoreDataBuilder()
             .with('scanning', 'landmarks')
-            .with('injectingRequested', true)
+            .with('injectingState', InjectingState.injectingRequested)
             .withLandmarksEnable()
             .build();
 
@@ -110,7 +111,7 @@ describe('AnalyzerStateUpdateHandlerTest', () => {
     test('start scan w/o teardowns: inject just completed', () => {
         const scanningTestStub = 'some test id';
         const prevState = new VisualizationStoreDataBuilder()
-            .with('injectingRequested', true)
+            .with('injectingState', InjectingState.injectingRequested)
             .with('scanning', scanningTestStub)
             .build();
         const currState = new VisualizationStoreDataBuilder()

src/DetailsView/components/adhoc-issues-test-view.tsx
@@ -13,12 +13,10 @@ import { UserConfigurationStoreData } from 'common/types/store-data/user-configu
 import { VisualizationStoreData } from 'common/types/store-data/visualization-store-data';
 import { VisualizationType } from 'common/types/visualization-type';
 import styles from 'DetailsView/components/adhoc-issues-test-view.scss';
+import { BannerWarnings } from 'DetailsView/components/banner-warnings';
 import { DetailsViewSwitcherNavConfiguration } from 'DetailsView/components/details-view-switcher-nav';
 import { NarrowModeStatus } from 'DetailsView/components/narrow-mode-detector';
-import {
-    ScanIncompleteWarning,
-    ScanIncompleteWarningDeps,
-} from 'DetailsView/components/scan-incomplete-warning';
+import { ScanIncompleteWarningDeps } from 'DetailsView/components/scan-incomplete-warning';
 import { DetailsViewToggleClickHandlerFactory } from 'DetailsView/handlers/details-view-toggle-click-handler-factory';
 import * as React from 'react';
 import { NamedFC, ReactFCWithDisplayName } from '../../common/react/named-fc';
@@ -78,11 +76,12 @@ function createTargetPageChangedView(props: AdhocIssuesTestViewProps): JSX.Eleme
 function createTestView(props: AdhocIssuesTestViewProps): JSX.Element {
     return (
         <>
-            <ScanIncompleteWarning
+            <BannerWarnings
                 deps={props.deps}
                 warnings={props.scanIncompleteWarnings}
                 warningConfiguration={props.switcherNavConfiguration.warningConfiguration}
                 test={props.selectedTest}
+                visualizationStoreData={props.visualizationStoreData}
             />
             <DetailsListIssuesView {...props} />
         </>

src/DetailsView/components/assessment-test-view.tsx
@@ -1,16 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { AssessmentsProvider } from 'assessments/types/assessments-provider';
+import { BannerWarnings } from 'DetailsView/components/banner-warnings';
 import { DetailsViewSwitcherNavConfiguration } from 'DetailsView/components/details-view-switcher-nav';
 import {
     GettingStartedView,
     GettingStartedViewDeps,
 } from 'DetailsView/components/getting-started-view';
 import { RequirementView, RequirementViewDeps } from 'DetailsView/components/requirement-view';
-import {
-    ScanIncompleteWarning,
-    ScanIncompleteWarningDeps,
-} from 'DetailsView/components/scan-incomplete-warning';
+import { ScanIncompleteWarningDeps } from 'DetailsView/components/scan-incomplete-warning';
 import {
     TargetChangeDialog,
     TargetChangeDialogDeps,
@@ -71,11 +69,12 @@ export const AssessmentTestView = NamedFC<AssessmentTestViewProps>(
 
         return (
             <>
-                <ScanIncompleteWarning
+                <BannerWarnings
                     deps={deps}
                     warnings={assessmentData.scanIncompleteWarnings}
                     warningConfiguration={props.switcherNavConfiguration.warningConfiguration}
                     test={assessmentNavState.selectedTestType}
+                    visualizationStoreData={props.visualizationStoreData}
                 />
                 <TargetChangeDialog deps={deps} prevTab={prevTarget} newTab={currentTarget} />
                 {isGettingStartedSelected ? (

src/DetailsView/components/banner-warnings.tsx
@@ -1,0 +1,31 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import {
+    InjectionFailedWarning,
+    InjectionFailedWarningProps,
+} from 'DetailsView/components/injection-failed-warning';
+import {
+    ScanIncompleteWarning,
+    ScanIncompleteWarningProps,
+} from 'DetailsView/components/scan-incomplete-warning';
+import * as React from 'react';
+
+export type BannerWarningsProps = ScanIncompleteWarningProps & InjectionFailedWarningProps;
+
+export class BannerWarnings extends React.PureComponent<BannerWarningsProps> {
+    public render() {
+        return (
+            <>
+                <ScanIncompleteWarning
+                    deps={this.props.deps}
+                    warnings={this.props.warnings}
+                    warningConfiguration={this.props.warningConfiguration}
+                    test={this.props.test}
+                />
+                <InjectionFailedWarning
+                    visualizationStoreData={this.props.visualizationStoreData}
+                />
+            </>
+        );
+    }
+}

src/DetailsView/components/injection-failed-warning.tsx
@@ -1,0 +1,33 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { MessageBar, MessageBarType } from '@fluentui/react';
+import { DisplayableStrings } from 'common/constants/displayable-strings';
+import { NamedFC } from 'common/react/named-fc';
+import {
+    InjectingState,
+    VisualizationStoreData,
+} from 'common/types/store-data/visualization-store-data';
+import * as React from 'react';
+
+export const InjectionFailedWarningContainerAutomationId = 'injection-failed-warning-container';
+
+export type InjectionFailedWarningProps = {
+    visualizationStoreData: VisualizationStoreData;
+};
+
+export const InjectionFailedWarning = NamedFC<InjectionFailedWarningProps>(
+    'injection-failed-warning',
+    props => {
+        if (props.visualizationStoreData.injectingState !== InjectingState.injectingFailed) {
+            return null;
+        } else {
+            return (
+                <MessageBar key="injection-failed" messageBarType={MessageBarType.warning}>
+                    <div data-automation-id={InjectionFailedWarningContainerAutomationId}>
+                        {DisplayableStrings.injectionFailed}
+                    </div>
+                </MessageBar>
+            );
+        }
+    },
+);

src/background/actions/action-payloads.ts
@@ -265,3 +265,8 @@ export interface AutoDetectedFailuresDialogStatePayload extends BaseActionPayloa
 export interface SaveAssessmentDialogStatePayload extends BaseActionPayload {
     enabled: boolean;
 }
+
+export interface InjectionFailedPayload extends BaseActionPayload {
+    failedAttempts: number;
+    shouldRetry: boolean;
+}

src/background/actions/injection-action-creator.ts
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { InjectionFailedPayload } from 'background/actions/action-payloads';
+import { DisplayableStrings } from 'common/constants/displayable-strings';
 import { Messages } from 'common/messages';
+import { NotificationCreator } from 'common/notification-creator';
 import { InjectionActions } from '../actions/injection-actions';
 import { Interpreter } from '../interpreter';
 
@@ -8,6 +11,7 @@ export class InjectionActionCreator {
     constructor(
         private readonly interpreter: Interpreter,
         private readonly injectionActions: InjectionActions,
+        private readonly notificationCreator: NotificationCreator,
     ) {}
 
     public registerCallbacks(): void {
@@ -19,6 +23,10 @@ export class InjectionActionCreator {
             Messages.Visualizations.State.InjectionCompleted,
             this.injectionCompleted,
         );
+        this.interpreter.registerTypeToPayloadCallback(
+            Messages.Visualizations.State.InjectionFailed,
+            this.injectionFailed,
+        );
     }
 
     private injectionStarted = async (): Promise<void> =>
@@ -26,4 +34,11 @@ export class InjectionActionCreator {
 
     private injectionCompleted = async (): Promise<void> =>
         await this.injectionActions.injectionCompleted.invoke(null);
+
+    private injectionFailed = async (payload: InjectionFailedPayload): Promise<void> => {
+        if (!payload.shouldRetry) {
+            this.notificationCreator.createNotification(DisplayableStrings.injectionFailed);
+        }
+        return await this.injectionActions.injectionFailed.invoke(payload);
+    };
 }

commit ffee465ab77c4fa5137f9668292aac4b8226685a

fix: avoid trying to start scan twice due to injectionState confusion (#6361)
#### Details

This PR fixes an issue introduced by #6183 where most events that
trigger an axe-core scan will accidentally trigger the scan to start
twice instead of once. This causes a few problems:

* It creates a race condition - in pages with iframes, the first scan
can begin concurrently with our scripts being injected into child
frames, so it can result in spurious `frame-tested` failures
* It creates console error spew of the form `failed to scan with error -
Axe is already running. Use `await axe.run()` to wait for the previous
run to finish before starting a new run.`

The root cause lies in the `analyzer-state-update-handler` component
that was updated as part of that PR. That PR changed how we track
whether injecting our scripts into the target page's frames is complete
from a series of boolean variables to a single state variable. However,
when this component was updated to read the new state variable instead
of the old booleans, the update was incomplete; the component is
intended to allow scans to start only when there is no ongoing injection
(ie, either the `injectingRequested` or `injectingStarted` states), but
the update mistakenly only prevents scans in the `injectingRequested`
state and allowed them to start in the `injectingStarted` state. This
was an easy mistake to make - the old code before #6183 just checked for
`injectingRequested !== true`, which actually encompassed both of those
states (the confusing naming is why we changed to a state variable in
the first place).

##### Motivation

Fix race condition and error spew

##### Context

see #6183

#### Pull request checklist
<!-- If a checklist item is not applicable to this change, write "n/a"
in the checkbox -->
- [n/a] Addresses an existing issue: #0000
- [x] Ran `yarn fastpass`
- [x] Added/updated relevant unit test(s) (and ran `yarn test`)

src/injected/analyzer-state-update-handler.ts
@@ -71,10 +71,11 @@ export class AnalyzerStateUpdateHandler {
         prevState: VisualizationStoreData,
         currState: VisualizationStoreData,
     ): void {
-        if (
-            currState.scanning != null &&
-            currState.injectingState !== InjectingState.injectingRequested
-        ) {
+        const injectingInProgress =
+            currState.injectingState === InjectingState.injectingRequested ||
+            currState.injectingState === InjectingState.injectingStarted;
+
+        if (currState.scanning != null && !injectingInProgress) {
             if (
                 prevState == null ||
                 prevState.scanning !== currState.scanning ||

src/tests/unit/tests/injected/analyzer-state-update-handler.test.ts
@@ -67,17 +67,20 @@ describe('AnalyzerStateUpdateHandlerTest', () => {
         startScanMock.verify(m => m(It.isAny()), Times.never());
     });
 
-    test('do not start scan if inject in progress and no prevState', () => {
-        const state = new VisualizationStoreDataBuilder()
-            .with('scanning', 'landmarks')
-            .with('injectingState', InjectingState.injectingRequested)
-            .withLandmarksEnable()
-            .build();
+    test.each([InjectingState.injectingRequested, InjectingState.injectingStarted])(
+        'do not start scan if injectingState is %s and no prevState',
+        injectingState => {
+            const state = new VisualizationStoreDataBuilder()
+                .with('scanning', 'landmarks')
+                .with('injectingState', injectingState)
+                .withLandmarksEnable()
+                .build();
 
-        testObject.handleUpdate(state);
+            testObject.handleUpdate(state);
 
-        startScanMock.verify(m => m(It.isAny()), Times.never());
-    });
+            startScanMock.verify(m => m(It.isAny()), Times.never());
+        },
+    );
 
     test('do not start scan or terminate if state is not changed', () => {
         const prevState = new VisualizationStoreDataBuilder()
@@ -111,10 +114,35 @@ describe('AnalyzerStateUpdateHandlerTest', () => {
     test('start scan w/o teardowns: inject just completed', () => {
         const scanningTestStub = 'some test id';
         const prevState = new VisualizationStoreDataBuilder()
-            .with('injectingState', InjectingState.injectingRequested)
+            .with('injectingState', InjectingState.injectingStarted)
             .with('scanning', scanningTestStub)
             .build();
         const currState = new VisualizationStoreDataBuilder()
+            .with('injectingState', InjectingState.notInjecting)
+            .with('scanning', scanningTestStub)
+            .build();
+        testObject.setPrevState(prevState);
+        setupDefaultVisualizationConfigFactory();
+        setupIsTestTerminated(configMock, requirementConfig, prevState, currState, true, true);
+
+        testObject.handleUpdate(currState);
+        tearDownCallback(configMock.object, -1, requirementConfig);
+
+        startScanMock.verify(m => m(scanningTestStub), Times.once());
+        teardownMock.verify(m => m(It.isAny()), Times.never());
+    });
+
+    // We still scan if injecting failed because it might have failed only for some frames - in
+    // that case, we preform a partial scan and a different component detects a corresponding
+    // axe-core "frame-tested" failure to display a warning to the user.
+    test('start scan w/o teardowns: inject just failed', () => {
+        const scanningTestStub = 'some test id';
+        const prevState = new VisualizationStoreDataBuilder()
+            .with('injectingState', InjectingState.injectingStarted)
+            .with('scanning', scanningTestStub)
+            .build();
+        const currState = new VisualizationStoreDataBuilder()
+            .with('injectingState', InjectingState.injectingFailed)
             .with('scanning', scanningTestStub)
             .build();
         testObject.setPrevState(prevState);