microsoft/accessibility-insights-web

commit 2186a7eae1783417a3106a5b37afbe4dbb4c9e1c

fix: add banner for unresponsive iframes (#6330)
#### Details

This PR adds logic to detect when iframes have been skipped in an axe
scan via that frames-tested rule. When iframes have been skipped, it
shows a yellow banner to notify the user.

##### Motivation

Addresses issue
[#6128](https://github.com/microsoft/accessibility-insights-web/issues/6128)

##### Context

Screenshot of banner with fastpass (shows up similarly in assessment
UI):

[Screenshot description: Yellow banner across top of details view with
text "there are iframes in the target page that are non-responsive.
These frames have been skipped and are not included in the result.]
<img width="636" alt="iframes-skipped-screenshot"
src="https://user-images.githubusercontent.com/16010855/211618204-cfc09d02-4db5-44f6-a4b9-ed235ef863fb.png">

#### Pull request checklist
<!-- If a checklist item is not applicable to this change, write "n/a"
in the checkbox -->
- [x] Addresses an existing issue:
[#6128](https://github.com/microsoft/accessibility-insights-web/issues/6128)
- [x] Ran `yarn fastpass`
- [x] Added/updated relevant unit test(s) (and ran `yarn test`)
- [x] Verified code coverage for the changes made. Check coverage report
at: `<rootDir>/test-results/unit/coverage`
- [x] PR title *AND* final merge commit title both start with a semantic
tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`). See
`CONTRIBUTING.md`.
- [x] (UI changes only) Added screenshots/GIFs to description above
- [x] (UI changes only) Verified usability with NVDA/JAWS

src/scanner/get-rule-inclusions.ts
@@ -4,7 +4,10 @@ import { IRuleConfiguration } from 'scanner/iruleresults';
 import { DictionaryStringTo } from 'types/common-types';
 
 export type RuleIncluded =
+    // Rules included in automated checks by default
     | { status: 'included'; reason?: string }
+    // Rules included when running any scan
+    | { status: 'included-always'; reason: string }
     | { status: 'excluded'; reason: string };
 
 export const explicitRuleOverrides: DictionaryStringTo<RuleIncluded> = {
@@ -35,6 +38,10 @@ export const explicitRuleOverrides: DictionaryStringTo<RuleIncluded> = {
     'presentation-role-conflict': {
         status: 'included',
         reason: 'best practice rule that was investigated with no known false positives, implemented as an automated check.',
+    },
+    'frame-tested': {
+        status: 'included-always',
+        reason: 'Tests for unresponsive frames, enables iframe skipped warning',
     },
 };
 

src/tests/unit/tests/scanner/__snapshots__/get-rule-inclusions.test.ts.snap
@@ -142,8 +142,8 @@ exports[`getRuleInclusions matches snapshotted list of production rules 1`] = `
     "status": "included",
   },
   "frame-tested": {
-    "reason": "rule is tagged best-practice",
-    "status": "excluded",
+    "reason": "Tests for unresponsive frames, enables iframe skipped warning",
+    "status": "included-always",
   },
   "frame-title": {
     "status": "included",

src/DetailsView/components/iframe-skipped-warning.tsx
@@ -1,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { NewTabLink } from 'common/components/new-tab-link';
+import { NamedFC } from 'common/react/named-fc';
+import * as React from 'react';
+
+export const IframeSkippedWarningContainerAutomationId = 'iframe-skipped-warning-container';
+
+export const IframeSkippedWarning = NamedFC('IframeSkippedWarning', () => (
+    <div data-automation-id={IframeSkippedWarningContainerAutomationId}>
+        There are iframes in the target page that are non-responsive. These frames have been skipped
+        and are not included in results. If you get this warning unexpectedly in a page where you
+        expect the iframes to be responsive, please
+        <NewTabLink href={'https://github.com/microsoft/accessibility-insights-web/issues/6347'}>
+            let us know.
+        </NewTabLink>
+    </div>
+));

src/DetailsView/components/warning-configuration.tsx
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 import { ReactFCWithDisplayName } from 'common/react/named-fc';
 import { ScanIncompleteWarningId } from 'common/types/store-data/scan-incomplete-warnings';
+import { IframeSkippedWarning } from 'DetailsView/components/iframe-skipped-warning';
 import {
     AssessmentIframeWarning,
     AssessmentIframeWarningDeps,
@@ -24,8 +25,10 @@ export type WarningConfiguration = {
 
 export const assessmentWarningConfiguration: WarningConfiguration = {
     'missing-required-cross-origin-permissions': AssessmentIframeWarning,
+    'frame-skipped': IframeSkippedWarning,
 };
 
 export const fastpassWarningConfiguration: WarningConfiguration = {
     'missing-required-cross-origin-permissions': FastPassIframeWarning,
+    'frame-skipped': IframeSkippedWarning,
 };

src/common/types/store-data/scan-incomplete-warnings.ts
@@ -2,4 +2,4 @@
 // Licensed under the MIT License.
 
 // This is a string-enum, but we only have one type right now
-export type ScanIncompleteWarningId = 'missing-required-cross-origin-permissions';
+export type ScanIncompleteWarningId = 'missing-required-cross-origin-permissions' | 'frame-skipped';

src/injected/analyzers/base-analyzer.ts
@@ -49,7 +49,7 @@ export class BaseAnalyzer implements Analyzer {
             scanResult: originalAxeResult,
             testType: config.testType,
             scanIncompleteWarnings:
-                this.scanIncompleteWarningDetector.detectScanIncompleteWarnings(),
+                this.scanIncompleteWarningDetector.detectScanIncompleteWarnings(originalAxeResult),
         };
 
         return {

src/injected/analyzers/notification-text-creator.ts
@@ -16,7 +16,7 @@ export class NotificationTextCreator {
     };
 
     public needsReviewText: TextGenerator = (results: UnifiedResult[]) => {
-        const warnings = this.scanIncompleteWarningDetector.detectScanIncompleteWarnings();
+        const warnings = this.scanIncompleteWarningDetector.detectScanIncompleteWarnings(null);
         if (isEmpty(results) && isEmpty(warnings)) {
             return 'Congratulations!\n\nNeeds review found no instances to review on this page.';
         }

src/injected/analyzers/unified-result-sender.ts
@@ -64,7 +64,7 @@ export class UnifiedResultSender {
         notificationMessage: TextGenerator,
     ): UnifiedScanCompletedPayload => {
         const scanIncompleteWarnings =
-            this.scanIncompleteWarningDetector.detectScanIncompleteWarnings();
+            this.scanIncompleteWarningDetector.detectScanIncompleteWarnings(results);
 
         let telemetry: ScanIncompleteWarningsTelemetryData = null;
 

commit 767311ed4a6aa19da75ee0c3be516c9371434063

fix: filter always-included rules out of assessment instances map (#6405)
#### Details

Fixes
https://github.com/microsoft/accessibility-insights-web/issues/6403.
This bug was introduced by
https://github.com/microsoft/accessibility-insights-web/commit/2186a7eae1783417a3106a5b37afbe4dbb4c9e1c
when we added a `always-included` rule. This PR adds logic to ignore
always-included rules when generating the assessment instances map.

##### Motivation

Addresses issue
https://github.com/microsoft/accessibility-insights-web/issues/6403

#### Pull request checklist
<!-- If a checklist item is not applicable to this change, write "n/a"
in the checkbox -->
- [x] Addresses an existing issue:
https://github.com/microsoft/accessibility-insights-web/issues/6403
- [x] Ran `yarn fastpass`
- [x] Added/updated relevant unit test(s) (and ran `yarn test`)
- [x] Verified code coverage for the changes made. Check coverage report
at: `<rootDir>/test-results/unit/coverage`
- [x] PR title *AND* final merge commit title both start with a semantic
tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`). See
`CONTRIBUTING.md`.
- [n/a] (UI changes only) Added screenshots/GIFs to description above
- [n/a] (UI changes only) Verified usability with NVDA/JAWS

src/background/assessment-data-converter.ts
@@ -34,6 +34,7 @@ export class AssessmentDataConverter {
         generateInstanceIdentifier: (instance: UniquelyIdentifiableInstances) => string,
         getInstanceStatus: (result: DecoratedAxeNodeResult) => ManualTestStatus,
         isVisualizationSupported: (result: DecoratedAxeNodeResult) => boolean,
+        getIncludedAlwaysRules: () => string[],
     ): AssessmentInstancesMap {
         let instancesMap: AssessmentInstancesMap = {};
 
@@ -41,8 +42,11 @@ export class AssessmentDataConverter {
             instancesMap = previouslyGeneratedInstances;
         }
 
+        const includedAlwaysRules = getIncludedAlwaysRules();
         forOwn(selectorMap, elementAxeResult => {
-            const rule = Object.keys(elementAxeResult.ruleResults).pop();
+            const rule = Object.keys(elementAxeResult.ruleResults)
+                .filter(rule => !includedAlwaysRules.includes(rule))
+                .pop();
             if (rule) {
                 const ruleResult = elementAxeResult.ruleResults[rule];
                 const identifier = generateInstanceIdentifier({

src/background/stores/assessment-store.ts
@@ -22,6 +22,7 @@ import {
     ScanUpdatePayload,
 } from 'injected/analyzers/analyzer';
 import { forEach, isEmpty, pickBy } from 'lodash';
+import { getIncludedAlwaysRules } from 'scanner/get-rule-inclusions';
 import { DictionaryStringTo } from 'types/common-types';
 import {
     AddResultDescriptionPayload,
@@ -407,6 +408,7 @@ export class AssessmentStore extends PersistentStore<AssessmentStoreData> {
                 config.getInstanceIdentiferGenerator(step),
                 stepConfig.getInstanceStatus,
                 stepConfig.isVisualizationSupportedForResult,
+                getIncludedAlwaysRules,
             );
         assessmentData.generatedAssessmentInstancesMap = generatedAssessmentInstancesMap;
         assessmentData.testStepStatus[step].isStepScanned = true;

src/scanner/get-rule-inclusions.ts
@@ -69,6 +69,12 @@ export const getNeedsReviewRulesConfig: () => DictionaryStringTo<RuleIncluded> =
     return needsReviewRulesConfig;
 };
 
+export const getIncludedAlwaysRules: () => string[] = () => {
+    return Object.entries(explicitRuleOverrides)
+        .filter(rule => rule[1].status === 'included-always')
+        .map(rule => rule[0]);
+};
+
 export const getRuleInclusions = (
     ruleset: IRuleConfiguration[],
     ruleOverrides: DictionaryStringTo<RuleIncluded>,

src/tests/unit/tests/background/assessment-data-converter.test.ts
@@ -59,6 +59,34 @@ describe('AssessmentDataConverter', () => {
                 undefined,
                 null,
                 null,
+                () => [],
+            );
+
+            expect(instanceMap).toEqual(previouslyGeneratedInstances);
+        });
+
+        it('should ignore selectors with only included always rule results.', () => {
+            const selectorMap: DictionaryStringTo<HtmlElementAxeResults> = {
+                [selectorStub]: {
+                    ruleResults: {
+                        rule1: {
+                            html: htmlStub,
+                            id: 'id1',
+                            status: false,
+                        } as DecoratedAxeNodeResult,
+                    },
+                    target: [selectorStub],
+                },
+            };
+            const previouslyGeneratedInstances = {};
+            const instanceMap = testSubject.generateAssessmentInstancesMap(
+                previouslyGeneratedInstances,
+                selectorMap,
+                testStep,
+                undefined,
+                null,
+                null,
+                () => ['rule1'],
             );
 
             expect(instanceMap).toEqual(previouslyGeneratedInstances);
@@ -79,6 +107,7 @@ describe('AssessmentDataConverter', () => {
                 undefined,
                 null,
                 null,
+                () => [],
             );
 
             expect(instanceMap).toEqual(expectedResult);
@@ -126,6 +155,7 @@ describe('AssessmentDataConverter', () => {
                 generateInstanceIdentifierMock.object,
                 () => ManualTestStatus.UNKNOWN,
                 () => true,
+                () => [],
             );
 
             expect(instanceMap).toEqual(expectedResult);
@@ -166,6 +196,7 @@ describe('AssessmentDataConverter', () => {
                 generateInstanceIdentifierMock.object,
                 () => ManualTestStatus.UNKNOWN,
                 () => true,
+                () => [],
             );
 
             expect(instanceMap[identifierStub].propertyBag).toEqual(expectedPropertyBag);
@@ -198,6 +229,7 @@ describe('AssessmentDataConverter', () => {
                     generateInstanceIdentifierMock.object,
                     () => getInstanceStatusResult,
                     () => true,
+                    () => [],
                 );
 
                 expect(instanceMap[identifierStub].testStepResults[testStep].status).toEqual(
@@ -233,6 +265,7 @@ describe('AssessmentDataConverter', () => {
                     generateInstanceIdentifierMock.object,
                     () => ManualTestStatus.UNKNOWN,
                     () => isVisualizationSupportedResult,
+                    () => [],
                 );
 
                 expect(
@@ -318,6 +351,7 @@ describe('AssessmentDataConverter', () => {
                 generateInstanceIdentifierMock.object,
                 () => ManualTestStatus.UNKNOWN,
                 () => true,
+                () => [],
             );
 
             expect(instanceMap).toEqual(expectedResult);

src/tests/unit/tests/background/stores/assessment-store.test.ts
@@ -52,6 +52,7 @@ import {
     ScanUpdatePayload,
 } from 'injected/analyzers/analyzer';
 import { cloneDeep } from 'lodash';
+import { getIncludedAlwaysRules } from 'scanner/get-rule-inclusions';
 import { ScanResults } from 'scanner/iruleresults';
 import { failTestOnErrorLogger } from 'tests/unit/common/fail-test-on-error-logger';
 import { IMock, It, Mock, MockBehavior, Times } from 'typemoq';
@@ -762,6 +763,7 @@ describe('AssessmentStore', () => {
                     instanceIdentifierGeneratorStub,
                     stepConfig.getInstanceStatus,
                     stepConfig.isVisualizationSupportedForResult,
+                    getIncludedAlwaysRules,
                 ),
             )
             .returns(() => expectedInstanceMap);
@@ -886,6 +888,7 @@ describe('AssessmentStore', () => {
                     instanceIdentifierGeneratorStub,
                     stepConfig.getInstanceStatus,
                     stepConfig.isVisualizationSupportedForResult,
+                    getIncludedAlwaysRules,
                 ),
             )
             .returns(() => fullInstanceMap);
@@ -988,6 +991,7 @@ describe('AssessmentStore', () => {
                     instanceIdentifierGeneratorStub,
                     stepConfig.getInstanceStatus,
                     stepConfig.isVisualizationSupportedForResult,
+                    getIncludedAlwaysRules,
                 ),
             )
             .returns(() => expectedInstanceMap);

src/tests/unit/tests/scanner/__snapshots__/get-rule-inclusions.test.ts.snap
@@ -1,6 +1,6 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
-exports[`getRuleInclusions matches snapshotted list of production rules 1`] = `
+exports[`getRuleInclusions getRuleInclusions matches snapshotted list of production rules 1`] = `
 {
   "accesskeys": {
     "reason": "rule is tagged best-practice",

src/tests/unit/tests/scanner/get-rule-inclusions.test.ts
@@ -3,6 +3,7 @@
 import * as axe from 'axe-core';
 import {
     explicitRuleOverrides,
+    getIncludedAlwaysRules,
     getRuleInclusions,
     RuleIncluded,
 } from 'scanner/get-rule-inclusions';
@@ -10,119 +11,129 @@ import { IRuleConfiguration } from 'scanner/iruleresults';
 import { DictionaryStringTo } from 'types/common-types';
 
 describe('getRuleInclusions', () => {
-    const fakeRules: IRuleConfiguration[] = [
-        { id: 'axe-enabled', selector: 'fake-selector', enabled: true, tags: [] },
-        { id: 'axe-disabled', selector: 'fake-selector', enabled: false, tags: [] },
-    ];
-
-    it('respects initial enabled/disabled config and populates reason', () => {
-        const inclusions = getRuleInclusions(fakeRules, {});
-        expect(inclusions).toMatchObject({
-            'axe-enabled': {
-                status: 'included',
-            },
-            'axe-disabled': {
-                status: 'excluded',
-                reason: 'disabled in axe config',
-            },
-        });
-    });
-    it('respects explicit overrides and populates reason', () => {
-        const overrideReason = 'unit test override';
-        const fakeOverrides: DictionaryStringTo<RuleIncluded> = {
-            'axe-enabled': {
-                status: 'excluded',
-                reason: overrideReason,
-            },
-        };
-
-        const inclusions = getRuleInclusions(fakeRules, fakeOverrides);
-        expect(inclusions).toMatchObject({
-            'axe-enabled': {
-                status: 'excluded',
-                reason: overrideReason,
-            },
-            'axe-disabled': {
-                status: 'excluded',
-                reason: 'disabled in axe config',
-            },
+    describe('getIncludedAlwaysRules', () => {
+        it('returns expected results', () => {
+            const inclusions = getIncludedAlwaysRules();
+            expect(inclusions).toMatchObject(['frame-tested']);
         });
     });
 
-    it("excludes rules mapped to null tags (because it's probably one of our custom rules) and populates reason", () => {
-        const noTagRule = [
-            {
-                id: 'no-tag-rule',
-                selector: 'fake-selector',
-                enabled: true,
-                // tags: ...
-            },
+    describe('getRuleInclusions', () => {
+        const fakeRules: IRuleConfiguration[] = [
+            { id: 'axe-enabled', selector: 'fake-selector', enabled: true, tags: [] },
+            { id: 'axe-disabled', selector: 'fake-selector', enabled: false, tags: [] },
         ];
-        const inclusions = getRuleInclusions(noTagRule, {});
-        expect(inclusions).toMatchObject({
-            'no-tag-rule': {
-                status: 'excluded',
-                reason: 'rule does not define a tags property',
-            },
-        });
-    });
 
-    it('excludes rules mapped to best-practice tag and populates reason', () => {
-        const bestPracticeRule = [
-            {
-                id: 'best-practice-rule',
-                selector: 'fake-selector',
-                enabled: true,
-                tags: ['best-practice'],
-            },
-        ];
-        const inclusions = getRuleInclusions(bestPracticeRule, {});
-        expect(inclusions).toMatchObject({
-            'best-practice-rule': {
-                status: 'excluded',
-                reason: 'rule is tagged best-practice',
-            },
+        it('respects initial enabled/disabled config and populates reason', () => {
+            const inclusions = getRuleInclusions(fakeRules, {});
+            expect(inclusions).toMatchObject({
+                'axe-enabled': {
+                    status: 'included',
+                },
+                'axe-disabled': {
+                    status: 'excluded',
+                    reason: 'disabled in axe config',
+                },
+            });
         });
-    });
 
-    it('excludes rules mapped to wcag2aaa tag and populates reason', () => {
-        const aaaRule = [
-            {
-                id: 'aaa-rule',
-                selector: 'fake-selector',
-                enabled: true,
-                tags: ['wcag2aaa'],
-            },
-        ];
-        const inclusions = getRuleInclusions(aaaRule, {});
-        expect(inclusions).toMatchObject({
-            'aaa-rule': {
-                status: 'excluded',
-                reason: 'rule is tagged wcag2aaa',
-            },
+        it('respects explicit overrides and populates reason', () => {
+            const overrideReason = 'unit test override';
+            const fakeOverrides: DictionaryStringTo<RuleIncluded> = {
+                'axe-enabled': {
+                    status: 'excluded',
+                    reason: overrideReason,
+                },
+            };
+
+            const inclusions = getRuleInclusions(fakeRules, fakeOverrides);
+            expect(inclusions).toMatchObject({
+                'axe-enabled': {
+                    status: 'excluded',
+                    reason: overrideReason,
+                },
+                'axe-disabled': {
+                    status: 'excluded',
+                    reason: 'disabled in axe config',
+                },
+            });
         });
-    });
 
-    it('excludes rules mapped to experimental tag and populates reason', () => {
-        const experimentalRule = [
-            {
-                id: 'experimental-rule',
-                selector: 'fake-selector',
-                enabled: true,
-                tags: ['experimental'],
-            },
-        ];
-        const inclusions = getRuleInclusions(experimentalRule, {});
-        expect(inclusions).toMatchObject({
-            'experimental-rule': {
-                status: 'excluded',
-                reason: 'rule is tagged experimental',
-            },
+        it("excludes rules mapped to null tags (because it's probably one of our custom rules) and populates reason", () => {
+            const noTagRule = [
+                {
+                    id: 'no-tag-rule',
+                    selector: 'fake-selector',
+                    enabled: true,
+                    // tags: ...
+                },
+            ];
+            const inclusions = getRuleInclusions(noTagRule, {});
+            expect(inclusions).toMatchObject({
+                'no-tag-rule': {
+                    status: 'excluded',
+                    reason: 'rule does not define a tags property',
+                },
+            });
         });
-    });
 
-    it('matches snapshotted list of production rules', () => {
-        const inclusions = getRuleInclusions(axe._audit.rules, explicitRuleOverrides);
-        expect(inclusions).toMatchSnapshot();
+        it('excludes rules mapped to best-practice tag and populates reason', () => {
+            const bestPracticeRule = [
+                {
+                    id: 'best-practice-rule',
+                    selector: 'fake-selector',
+                    enabled: true,
+                    tags: ['best-practice'],
+                },
+            ];
+            const inclusions = getRuleInclusions(bestPracticeRule, {});
+            expect(inclusions).toMatchObject({
+                'best-practice-rule': {
+                    status: 'excluded',
+                    reason: 'rule is tagged best-practice',
+                },
+            });
+        });
+
+        it('excludes rules mapped to wcag2aaa tag and populates reason', () => {
+            const aaaRule = [
+                {
+                    id: 'aaa-rule',
+                    selector: 'fake-selector',
+                    enabled: true,
+                    tags: ['wcag2aaa'],
+                },
+            ];
+            const inclusions = getRuleInclusions(aaaRule, {});
+            expect(inclusions).toMatchObject({
+                'aaa-rule': {
+                    status: 'excluded',
+                    reason: 'rule is tagged wcag2aaa',
+                },
+            });
+        });
+
+        it('excludes rules mapped to experimental tag and populates reason', () => {
+            const experimentalRule = [
+                {
+                    id: 'experimental-rule',
+                    selector: 'fake-selector',
+                    enabled: true,
+                    tags: ['experimental'],
+                },
+            ];
+            const inclusions = getRuleInclusions(experimentalRule, {});
+            expect(inclusions).toMatchObject({
+                'experimental-rule': {
+                    status: 'excluded',
+                    reason: 'rule is tagged experimental',
+                },
+            });
+        });
+
+        it('matches snapshotted list of production rules', () => {
+            const inclusions = getRuleInclusions(axe._audit.rules, explicitRuleOverrides);
+            expect(inclusions).toMatchSnapshot();
+        });
     });
 });