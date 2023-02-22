56325332bbd58fbe0bae65ebf01d0a1ad59ac9fb
Dan Bjorge <danielbj@microsoft.com>
2023-02-21 19:09:29 -0500

    remove unnecessary axe-config rules

--- src/scanner/get-rule-inclusions.ts
+++ src/scanner/get-rule-inclusions.ts
@@ -19,6 +19,10 @@ export const explicitRuleOverrides: DictionaryStringTo<RuleIncluded> = {
         status: 'included',
         reason: 'for parity with video-caption, which axe-core includes by default',
     },
+    bypass: {
+        status: 'excluded',
+        reason: "only reports needs-review results, but we haven't implemented needs-review content for it yet",
+    },
     'duplicate-id': {
         status: 'excluded',
         reason: 'Based on feedback from users we tested to check the user impact of duplicate ID failures on static elements. We found no user impact on the experience with any of the ATs including Narrator, JAWS, NVDA, Dragon, and Windows Speech Recognition. After further discussions with the Tooling Advisory Board, we decided to make this a best practice rule. See #4102.',
