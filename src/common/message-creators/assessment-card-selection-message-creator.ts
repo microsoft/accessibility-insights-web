fcddadc5e054a8512fdc85ea980897abd6ceca23
Lisa Li <lisli@microsoft.com>
2023-02-24 09:58:33 -0800

    add AssessmentCardSelectionMessageCreator and tests

---
+++ src/common/message-creators/assessment-card-selection-message-creator.ts
@@ -0,0 +1,82 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import {
+    BaseActionPayload,
+    CardSelectionPayload,
+    RuleExpandCollapsePayload,
+} from 'background/actions/action-payloads';
+import { TelemetryEventSource } from 'common/extension-telemetry-events';
+import { CardSelectionMessageCreator } from 'common/message-creators/card-selection-message-creator';
+import { ActionMessageDispatcher } from 'common/message-creators/types/dispatcher';
+import { Messages } from 'common/messages';
+import { SupportedMouseEvent, TelemetryDataFactory } from 'common/telemetry-data-factory';
+
+export class AssessmentCardSelectionMessageCreator implements CardSelectionMessageCreator {
+    constructor(
+        private readonly dispatcher: ActionMessageDispatcher,
+        private readonly telemetryFactory: TelemetryDataFactory,
+        private readonly source: TelemetryEventSource,
+    ) {}
+
+    public toggleCardSelection = (
+        ruleId: string,
+        resultInstanceUid: string,
+        event: React.SyntheticEvent,
+    ) => {
+        const payload: CardSelectionPayload = {
+            resultInstanceUid,
+            ruleId,
+            telemetry: this.telemetryFactory.withTriggeredByAndSource(event, this.source),
+        };
+
+        this.dispatcher.dispatchMessage({
+            messageType: Messages.AssessmentCardSelection.CardSelectionToggled,
+            payload,
+        });
+    };
+
+    public toggleRuleExpandCollapse = (ruleId: string, event: React.SyntheticEvent) => {
+        const payload: RuleExpandCollapsePayload = {
+            ruleId,
+            telemetry: this.telemetryFactory.withTriggeredByAndSource(event, this.source),
+        };
+
+        this.dispatcher.dispatchMessage({
+            messageType: Messages.AssessmentCardSelection.RuleExpansionToggled,
+            payload,
+        });
+    };
+
+    public collapseAllRules = (event: SupportedMouseEvent) => {
+        const payload: BaseActionPayload = {
+            telemetry: this.telemetryFactory.withTriggeredByAndSource(event, this.source),
+        };
+
+        this.dispatcher.dispatchMessage({
+            messageType: Messages.AssessmentCardSelection.CollapseAllRules,
+            payload,
+        });
+    };
+
+    public expandAllRules = (event: SupportedMouseEvent) => {
+        const payload: BaseActionPayload = {
+            telemetry: this.telemetryFactory.withTriggeredByAndSource(event, this.source),
+        };
+
+        this.dispatcher.dispatchMessage({
+            messageType: Messages.AssessmentCardSelection.ExpandAllRules,
+            payload,
+        });
+    };
+
+    public toggleVisualHelper = (event: SupportedMouseEvent) => {
+        const payload: BaseActionPayload = {
+            telemetry: this.telemetryFactory.withTriggeredByAndSource(event, this.source),
+        };
+
+        this.dispatcher.dispatchMessage({
+            messageType: Messages.AssessmentCardSelection.ToggleVisualHelper,
+            payload,
+        });
+    };
+}
---
+++ src/tests/unit/tests/common/message-creators/assessment-card-selection-message-creator.test.ts
@@ -0,0 +1,137 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import {
+    BaseActionPayload,
+    CardSelectionPayload,
+    RuleExpandCollapsePayload,
+} from 'background/actions/action-payloads';
+import { BaseTelemetryData, TelemetryEventSource } from 'common/extension-telemetry-events';
+import { Message } from 'common/message';
+import { AssessmentCardSelectionMessageCreator } from 'common/message-creators/assessment-card-selection-message-creator';
+import { ActionMessageDispatcher } from 'common/message-creators/types/dispatcher';
+import { Messages } from 'common/messages';
+import { TelemetryDataFactory } from 'common/telemetry-data-factory';
+import { IMock, Mock, Times } from 'typemoq';
+
+describe('AssessmentCardSelectionMessageCreator', () => {
+    let dispatcherMock: IMock<ActionMessageDispatcher>;
+    let testSubject: AssessmentCardSelectionMessageCreator;
+    let telemetryDataFactoryMock: IMock<TelemetryDataFactory>;
+    let sourceStub: TelemetryEventSource;
+    let eventStub: React.SyntheticEvent;
+    let telemetryStub: BaseTelemetryData;
+
+    beforeEach(() => {
+        dispatcherMock = Mock.ofType<ActionMessageDispatcher>();
+        telemetryDataFactoryMock = Mock.ofType<TelemetryDataFactory>();
+        sourceStub = -1;
+        eventStub = {} as React.SyntheticEvent;
+        telemetryStub = {} as BaseTelemetryData;
+        testSubject = new AssessmentCardSelectionMessageCreator(
+            dispatcherMock.object,
+            telemetryDataFactoryMock.object,
+            sourceStub,
+        );
+    });
+
+    it('dispatches message for toggleCardSelection', () => {
+        const resultInstanceUid = 'test-uid';
+        const ruleId = 'test-rule-id';
+        const payload: CardSelectionPayload = {
+            resultInstanceUid,
+            ruleId,
+            telemetry: telemetryStub,
+        };
+
+        const expectedMessage: Message = {
+            messageType: Messages.AssessmentCardSelection.CardSelectionToggled,
+            payload,
+        };
+
+        telemetryDataFactoryMock
+            .setup(tdfm => tdfm.withTriggeredByAndSource(eventStub, sourceStub))
+            .returns(() => telemetryStub);
+
+        testSubject.toggleCardSelection(ruleId, resultInstanceUid, eventStub);
+
+        dispatcherMock.verify(handler => handler.dispatchMessage(expectedMessage), Times.once());
+    });
+
+    test('toggleRuleExpandCollapse', () => {
+        const ruleId = 'test-rule-id';
+        const payload: RuleExpandCollapsePayload = {
+            ruleId,
+            telemetry: telemetryStub,
+        };
+
+        const expectedMessage: Message = {
+            messageType: Messages.AssessmentCardSelection.RuleExpansionToggled,
+            payload,
+        };
+
+        telemetryDataFactoryMock
+            .setup(tdfm => tdfm.withTriggeredByAndSource(eventStub, sourceStub))
+            .returns(() => telemetryStub);
+
+        testSubject.toggleRuleExpandCollapse(ruleId, eventStub);
+
+        dispatcherMock.verify(handler => handler.dispatchMessage(expectedMessage), Times.once());
+    });
+
+    test('collapseAllRules', () => {
+        const payload: BaseActionPayload = {
+            telemetry: telemetryStub,
+        };
+
+        const expectedMessage: Message = {
+            messageType: Messages.AssessmentCardSelection.CollapseAllRules,
+            payload,
+        };
+
+        telemetryDataFactoryMock
+            .setup(tdfm => tdfm.withTriggeredByAndSource(eventStub, sourceStub))
+            .returns(() => telemetryStub);
+
+        testSubject.collapseAllRules(eventStub);
+
+        dispatcherMock.verify(dm => dm.dispatchMessage(expectedMessage), Times.once());
+    });
+
+    test('expandAllRules', () => {
+        const payload: BaseActionPayload = {
+            telemetry: telemetryStub,
+        };
+
+        const expectedMessage: Message = {
+            messageType: Messages.AssessmentCardSelection.ExpandAllRules,
+            payload,
+        };
+
+        telemetryDataFactoryMock
+            .setup(tdfm => tdfm.withTriggeredByAndSource(eventStub, sourceStub))
+            .returns(() => telemetryStub);
+
+        testSubject.expandAllRules(eventStub);
+
+        dispatcherMock.verify(dm => dm.dispatchMessage(expectedMessage), Times.once());
+    });
+
+    test('toggleVisualHelper', () => {
+        const payload: BaseActionPayload = {
+            telemetry: telemetryStub,
+        };
+
+        const expectedMessage: Message = {
+            messageType: Messages.AssessmentCardSelection.ToggleVisualHelper,
+            payload,
+        };
+
+        telemetryDataFactoryMock
+            .setup(tdfm => tdfm.withTriggeredByAndSource(eventStub, sourceStub))
+            .returns(() => telemetryStub);
+
+        testSubject.toggleVisualHelper(eventStub);
+
+        dispatcherMock.verify(dm => dm.dispatchMessage(expectedMessage), Times.once());
+    });
+});
