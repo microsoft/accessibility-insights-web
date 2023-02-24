4ab9088399a910973c9af6834d3a881230a76abd
Lisa Li <lisli@microsoft.com>
2023-02-24 09:17:04 -0800

    create AssessmentCardSelectionActions

---
+++ src/background/actions/assessment-card-selection-actions.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { AsyncAction } from 'common/flux/async-action';
+import { CardSelectionPayload, RuleExpandCollapsePayload } from './action-payloads';
+
+export class AssessmentCardSelectionActions {
+    public readonly getCurrentState = new AsyncAction();
+    public readonly navigateToNewCardsView = new AsyncAction();
+    public readonly toggleRuleExpandCollapse = new AsyncAction<RuleExpandCollapsePayload>();
+    public readonly toggleCardSelection = new AsyncAction<CardSelectionPayload>();
+    public readonly collapseAllRules = new AsyncAction();
+    public readonly expandAllRules = new AsyncAction();
+    public readonly toggleVisualHelper = new AsyncAction();
+    public readonly resetFocusedIdentifier = new AsyncAction();
+}
