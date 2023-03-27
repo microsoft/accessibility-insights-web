7680a82d2d07f0485011631dacbbeb13c8ecab24
Sarah Oslund <sfoslund@microsoft.com>
2023-03-27 10:19:44 -0700

    feat(automated-checks): fix assessment automated checks visualization refresh behavior (#6540)
    #### Details
    
    Fix bug where, upon refresh of the target page, the visualizations in
    Assessment > Automated Checks are not re-enabled. This PR updates that
    the assessment issues test view to behave similarly to the
    RequirementView, which is responsible for handling this behavior (via
    the AssessmentViewUpdateHandler).
    
    ##### Motivation
    
    Feature work
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a"
    in the checkbox -->
    - [n/a] Addresses an existing issue: #0000
    - [x] Ran `yarn fastpass`
    - [x] Added/updated relevant unit test(s) (and ran `yarn test`)
    - [x] Verified code coverage for the changes made. Check coverage report
    at: `<rootDir>/test-results/unit/coverage`
    - [x] PR title *AND* final merge commit title both start with a semantic
    tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`). See
    `CONTRIBUTING.md`.
    - [n/a] (UI changes only) Added screenshots/GIFs to description above
    - [n/a] (UI changes only) Verified usability with NVDA/JAWS
    
    ---------
    
    Co-authored-by: Wahaj Abid <waabid@microsoft.com>

--- src/DetailsView/components/assessment-issues-test-view.tsx
+++ src/DetailsView/components/assessment-issues-test-view.tsx
@@ -1,34 +1,129 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { AssessmentsProvider } from 'assessments/types/assessments-provider';
+import {
+    AssessmentNavState,
+    AssessmentStoreData,
+} from 'common/types/store-data/assessment-result-data';
 import { AdhocIssuesTestViewProps } from 'DetailsView/components/adhoc-issues-test-view';
 import styles from 'DetailsView/components/adhoc-issues-test-view.scss';
-import { BannerWarnings } from 'DetailsView/components/banner-warnings';
-import { DetailsListIssuesView } from 'DetailsView/components/details-list-issues-view';
+import {
+    AssessmentViewUpdateHandler,
+    AssessmentViewUpdateHandlerDeps,
+    AssessmentViewUpdateHandlerProps,
+} from 'DetailsView/components/assessment-view-update-handler';
+import { BannerWarnings, BannerWarningsDeps } from 'DetailsView/components/banner-warnings';
+import {
+    DetailsListIssuesView,
+    DetailsListIssuesViewDeps,
+} from 'DetailsView/components/details-list-issues-view';
+import {
+    TargetChangeDialog,
+    TargetChangeDialogDeps,
+} from 'DetailsView/components/target-change-dialog';
+import { isEqual } from 'lodash';
 import * as React from 'react';
-import { NamedFC } from '../../common/react/named-fc';
 
-export type AssessmentIssuesTestViewProps = AdhocIssuesTestViewProps;
+export type AssessmentIssuesTestViewDeps = {
+    assessmentViewUpdateHandler: AssessmentViewUpdateHandler;
+    getProvider: () => AssessmentsProvider;
+} & BannerWarningsDeps &
+    TargetChangeDialogDeps &
+    AssessmentViewUpdateHandlerDeps &
+    DetailsListIssuesViewDeps;
 
-export const AssessmentIssuesTestView = NamedFC<AssessmentIssuesTestViewProps>(
-    'AssessmentIssuesTestView',
-    props => {
-        const view = createTestView(props);
+export type AssessmentIssuesTestViewProps = {
+    deps: AssessmentIssuesTestViewDeps;
+    assessmentStoreData: AssessmentStoreData;
+} & Omit<AdhocIssuesTestViewProps, 'deps'>;
+
+export class AssessmentIssuesTestView extends React.Component<AssessmentIssuesTestViewProps> {
+    public componentDidMount(): void {
+        this.props.deps.assessmentViewUpdateHandler.onMount(this.getUpdateHandlerProps(this.props));
+    }
+
+    public componentDidUpdate(prevProps: AssessmentIssuesTestViewProps): void {
+        const prevUpdateHandlerProps = this.getUpdateHandlerProps(prevProps);
+        const newUpdateHandlerProps = this.getUpdateHandlerProps(this.props);
+
+        if (isEqual(prevUpdateHandlerProps, newUpdateHandlerProps)) {
+            return;
+        }
+
+        this.props.deps.assessmentViewUpdateHandler.update(
+            prevUpdateHandlerProps,
+            newUpdateHandlerProps,
+        );
+    }
+
+    public componentWillUnmount(): void {
+        this.props.deps.assessmentViewUpdateHandler.onUnmount(
+            this.getUpdateHandlerProps(this.props),
+        );
+    }
+
+    private getUpdateHandlerProps(
+        props: AssessmentIssuesTestViewProps,
+    ): AssessmentViewUpdateHandlerProps {
+        const scanData = props.configuration.getStoreData(props.visualizationStoreData.tests);
+        const selectedRequirementIsEnabled = props.configuration.getTestStatus(
+            scanData,
+            props.assessmentStoreData.assessmentNavState.selectedTestSubview,
+        );
+        const assessmentData = props.configuration.getAssessmentData!(props.assessmentStoreData);
+        const assessment = props.deps
+            .getProvider()
+            .forType(props.assessmentStoreData.assessmentNavState.selectedTestType);
+        const navState = {
+            ...props.assessmentStoreData.assessmentNavState,
+            // since no test subview/requirement is specifically selected in automated checks, we default to first requirement.
+            selectedTestSubview: assessment!.requirements[0].key,
+        } as AssessmentNavState;
+
+        return {
+            deps: props.deps,
+            selectedRequirementIsEnabled: selectedRequirementIsEnabled,
+            assessmentNavState: navState,
+            assessmentData: assessmentData,
+            prevTarget: props.assessmentStoreData.persistedTabInfo,
+            currentTarget: this.getCurrentTarget(),
+        };
+    }
+
+    public render = () => {
+        const view = this.createTestView(this.props);
 
         return <div className={styles.issuesTestView}>{view}</div>;
-    },
-);
+    };
 
-function createTestView(props: AssessmentIssuesTestViewProps): JSX.Element {
-    return (
-        <>
-            <BannerWarnings
-                deps={props.deps}
-                warnings={props.scanIncompleteWarnings}
-                warningConfiguration={props.switcherNavConfiguration.warningConfiguration}
-                test={props.selectedTest}
-                visualizationStoreData={props.visualizationStoreData}
-            />
-            <DetailsListIssuesView {...props} />
-        </>
-    );
+    private getCurrentTarget = () => {
+        return {
+            id: this.props.tabStoreData.id,
+            url: this.props.tabStoreData.url,
+            title: this.props.tabStoreData.title,
+        };
+    };
+
+    private createTestView(props: AssessmentIssuesTestViewProps): JSX.Element {
+        const { deps } = props;
+        const prevTarget = props.assessmentStoreData.persistedTabInfo;
+
+        return (
+            <>
+                <BannerWarnings
+                    deps={props.deps}
+                    warnings={props.scanIncompleteWarnings}
+                    warningConfiguration={props.switcherNavConfiguration.warningConfiguration}
+                    test={props.selectedTest}
+                    visualizationStoreData={props.visualizationStoreData}
+                />
+                <TargetChangeDialog
+                    deps={deps}
+                    prevTab={prevTarget}
+                    newTab={this.getCurrentTarget()}
+                />
+                <DetailsListIssuesView {...props} />
+            </>
+        );
+    }
 }
