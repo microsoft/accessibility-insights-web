e223678a699d172f619ef6ad4114bd6707982431
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2023-06-21 13:37:51 -0700

    feat(remove-android): Remove electron tests (#6742)
    #### Details
    
    Deprecating Android support will touch hundreds of files, so we're
    breaking it up into more manageable units. This PR removes the files
    associated with testing the Unified pipelines (which have already been
    disabled), as well as one test that only ran in the pipeline context.
    
    ##### Motivation
    
    Android deprecation feature
    
    ##### Context
    
    <!-- Are there any parts that you've intentionally left out-of-scope for
    a later PR to handle? -->
    
    <!-- Were there any alternative approaches you considered? What
    tradeoffs did you consider? -->
    
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a"
    in the checkbox -->
    - [n/a] Addresses an existing issue (part of Android deprecation
    feature)
    - [x] Ran `yarn fastpass`
    - [x] Added/updated relevant unit test(s) (and ran `yarn test`)
    - [x] Verified code coverage for the changes made. Check coverage report
    at: `<rootDir>/test-results/unit/coverage`
    - [x] PR title *AND* final merge commit title both start with a semantic
    tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`). See
    `CONTRIBUTING.md`.
    - [n/a] (UI changes only) Added screenshots/GIFs to description above
    - [n/a] (UI changes only) Verified usability with NVDA/JAWS

--- src/tests/electron/setup/global-setup.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-import { commonAdbConfigs, setupMockAdb } from '../../miscellaneous/setup-mock-adb/setup-mock-adb';
-
-// tslint:disable-next-line:no-default-export
-export default async function (): Promise<void> {
-    await setupMockAdb(commonAdbConfigs['single-device'], 'global-setup');
-}
