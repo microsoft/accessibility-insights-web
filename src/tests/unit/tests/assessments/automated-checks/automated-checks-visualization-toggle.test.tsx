628e43614894a437408b6d4407d3418d7f02d0f0
madalynrose <hello@madalyn.dev>
2023-11-09 12:14:18 -0500

    migrate automated checks visualization toggle

--- src/tests/unit/tests/assessments/automated-checks/automated-checks-visualization-toggle.test.tsx
+++ src/tests/unit/tests/assessments/automated-checks/automated-checks-visualization-toggle.test.tsx
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { render, RenderResult } from '@testing-library/react';
 import { AutomatedChecksVisualizationToggle } from 'assessments/automated-checks/automated-checks-visualization-enabled-toggle';
 import { AssessmentActionMessageCreator } from 'DetailsView/actions/assessment-action-message-creator';
-import { shallow, ShallowWrapper } from 'enzyme';
 import * as React from 'react';
 import { IMock, Mock } from 'typemoq';
 import {
@@ -12,8 +12,14 @@ import {
 import { visualHelperText } from '../../../../../DetailsView/components/base-visual-helper-toggle';
 import { VisualHelperToggleConfigBuilder } from '../../../common/visual-helper-toggle-config-builder';
 import { VisualizationTogglePropsBuilder } from '../../../common/visualization-toggle-props-builder';
+import {
+    getMockComponentClassPropsForCall,
+    mockReactComponents,
+} from 'tests/unit/mock-helpers/mock-module-helpers';
 
+jest.mock('common/components/visualization-toggle');
 describe('AutomatedChecksVisualizationToggle', () => {
+    mockReactComponents([VisualizationToggle]);
     const assessmentActionMessageCreatorMock: IMock<AssessmentActionMessageCreator> = Mock.ofType(
         AssessmentActionMessageCreator,
     );
@@ -26,26 +32,24 @@ describe('AutomatedChecksVisualizationToggle', () => {
             .withEmptyFilteredMap()
             .build();
 
-        const wrapper = shallow(<AutomatedChecksVisualizationToggle {...props} />);
+        const renderResult = render(<AutomatedChecksVisualizationToggle {...props} />);
 
         const visualHelperClass = 'visual-helper';
-        const toggleDiv = wrapper.find(`.${visualHelperClass}`);
-        const textDiv = toggleDiv.find(`.${visualHelperClass}-text`);
+        const toggleDiv = renderResult.container.querySelector(`.${visualHelperClass}`);
+        const textDiv = toggleDiv.querySelector(`.${visualHelperClass}-text`);
         const noMatchesWarningClass = 'no-matching-elements';
 
-        expect(toggleDiv.exists()).toBe(true);
-        expect(textDiv.exists()).toBe(true);
-        expect(wrapper.find(`.${noMatchesWarningClass}`).exists()).toBe(true);
-
-        const toggle = wrapper.find(VisualizationToggle);
+        expect(toggleDiv).not.toBeNull();
+        expect(textDiv).not.toBeNull();
+        expect(renderResult.container.querySelector(`.${noMatchesWarningClass}`)).not.toBeNull();
 
         const expectedToggleProps = getDefaultVisualizationTogglePropsBuilder()
             .with('checked', false)
             .with('disabled', true)
             .build();
 
-        assertVisualizationToggle(expectedToggleProps, toggle);
-        assertSnapshotMatch(wrapper);
+        assertVisualizationToggle(expectedToggleProps);
+        assertSnapshotMatch(renderResult);
     });
 
     it('render: toggle not disabled', () => {
@@ -56,27 +60,26 @@ describe('AutomatedChecksVisualizationToggle', () => {
             .withNonEmptyFilteredMap()
             .build();
 
-        const wrapper = shallow(<AutomatedChecksVisualizationToggle {...props} />);
+        const renderResult = render(<AutomatedChecksVisualizationToggle {...props} />);
 
         const visualHelperClass = 'visual-helper';
-        const toggleDiv = wrapper.find(`.${visualHelperClass}`);
+        const toggleDiv = renderResult.container.querySelector(`.${visualHelperClass}`);
 
-        expect(toggleDiv.exists()).toBe(true);
+        expect(toggleDiv).not.toBeNull();
 
-        const textDiv = toggleDiv.find(`.${visualHelperClass}-text`);
+        const textDiv = toggleDiv.querySelector(`.${visualHelperClass}-text`);
 
-        expect(textDiv.exists()).toBe(true);
+        expect(textDiv).not.toBeNull();
 
-        expect(wrapper.find('strong').exists()).toBeFalsy();
-        const toggle = wrapper.find(VisualizationToggle);
+        expect(renderResult.container.querySelector('strong')).toBeNull();
 
         const expectedToggleProps = getDefaultVisualizationTogglePropsBuilder()
             .with('checked', false)
             .with('disabled', false)
             .build();
 
-        assertVisualizationToggle(expectedToggleProps, toggle);
-        assertSnapshotMatch(wrapper);
+        assertVisualizationToggle(expectedToggleProps);
+        assertSnapshotMatch(renderResult);
     });
 
     it('render: toggle disabled when there are no failing instances for automated checks', () => {
@@ -87,44 +90,39 @@ describe('AutomatedChecksVisualizationToggle', () => {
             .withPassingFilteredMap()
             .build();
 
-        const wrapper = shallow(<AutomatedChecksVisualizationToggle {...props} />);
+        const renderResult = render(<AutomatedChecksVisualizationToggle {...props} />);
         const visualHelperClass = 'visual-helper';
-        const toggleDiv = wrapper.find(`.${visualHelperClass}`);
+        const toggleDiv = renderResult.container.querySelector(`.${visualHelperClass}`);
 
-        expect(toggleDiv.exists()).toBe(true);
+        expect(toggleDiv).not.toBeNull();
 
-        const textDiv = toggleDiv.find(`.${visualHelperClass}-text`);
+        const textDiv = toggleDiv.querySelector(`.${visualHelperClass}-text`);
 
-        expect(textDiv.exists()).toBe(true);
+        expect(textDiv).not.toBeNull();
 
         const noMatchesWarningClass = 'no-matching-elements';
 
-        const toggle = wrapper.find(VisualizationToggle);
-
-        expect(wrapper.find(`.${noMatchesWarningClass}`).exists()).toBe(true);
+        expect(renderResult.container.querySelector(`.${noMatchesWarningClass}`)).not.toBeNull();
 
         const expectedToggleProps = getDefaultVisualizationTogglePropsBuilder()
             .with('checked', false)
             .with('disabled', true)
             .build();
 
-        assertVisualizationToggle(expectedToggleProps, toggle);
-        assertSnapshotMatch(wrapper);
+        assertVisualizationToggle(expectedToggleProps);
+        assertSnapshotMatch(renderResult);
     });
 
-    function assertVisualizationToggle(
-        expectedProps: VisualizationToggleProps,
-        visualizationToggle: ShallowWrapper<VisualizationToggleProps>,
-    ): void {
-        expect(visualizationToggle.exists()).toBe(true);
+    function assertVisualizationToggle(expectedProps: VisualizationToggleProps): void {
+        expect(VisualizationToggle).toBeCalled();
 
-        const actualProps = visualizationToggle.props();
+        const actualProps = getMockComponentClassPropsForCall(VisualizationToggle);
 
         expect(actualProps).toMatchObject(expectedProps);
     }
 
-    function assertSnapshotMatch(wrapper: ShallowWrapper): void {
-        expect(wrapper.getElement()).toMatchSnapshot();
+    function assertSnapshotMatch(wrapper: RenderResult): void {
+        expect(wrapper.asFragment()).toMatchSnapshot();
     }
 
     function getDefaultVisualizationTogglePropsBuilder(): VisualizationTogglePropsBuilder {
