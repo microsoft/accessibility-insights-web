fb090b999aa6d9340d675da3c1ac34578fc1dd50
madalynrose <hello@madalyn.dev>
2023-11-09 12:45:17 -0500

    migrate customWidgetsColumnRendererFactory

--- src/tests/unit/tests/assessments/custom-widgets/custom-widgets-column-renderer-factory.test.tsx
+++ src/tests/unit/tests/assessments/custom-widgets/custom-widgets-column-renderer-factory.test.tsx
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { render } from '@testing-library/react';
 import { CustomWidgetsColumnRendererFactory } from 'assessments/custom-widgets/custom-widgets-column-renderer-factory';
 import { InstanceTableRow } from 'assessments/types/instance-table-data';
 import { PropertyBagColumnRendererConfig } from 'common/types/property-bag/property-bag-column-renderer-config';
-import { shallow } from 'enzyme';
 import * as React from 'react';
 
 import { ColumnValueBag } from '../../../../../common/types/property-bag/column-value-bag';
@@ -44,32 +44,32 @@ describe('CustomWidgetsColumnRendererFactory', () => {
     it('getWithLink should render link', () => {
         const result = CustomWidgetsColumnRendererFactory.getWithLink(configs);
 
-        checkPropertyBagAndTag(result, 'NewTabLink');
+        checkPropertyBagAndTag(result, 'A');
     });
 
     it('getWithoutLink should render text', () => {
         const result = CustomWidgetsColumnRendererFactory.getWithoutLink(configs);
 
-        checkPropertyBagAndTag(result, 'span');
+        checkPropertyBagAndTag(result, 'SPAN');
     });
 
     function checkPropertyBagAndTag(result: Function, tag: string): void {
         const renderer = () => result(item);
 
-        const wrapper = shallow(<RendererWrapper render={renderer} />);
+        const renderResult = render(<RendererWrapper render={renderer} />);
 
-        const div = wrapper.find('.property-bag-container');
-        expect(div.exists()).toBeTruthy();
-        expect(div.children()).toHaveLength(2);
+        const div = renderResult.container.querySelector('.property-bag-container');
+        expect(div).not.toBeNull();
+        expect(div.children).toHaveLength(2);
 
-        const designPatternSpan = div.childAt(1);
+        const designPatternSpan = div.children[1];
         expect(designPatternSpan).not.toBeUndefined();
-        expect(designPatternSpan.hasClass('property-bag-div')).toBeTruthy();
-        expect(designPatternSpan.children()).toHaveLength(2);
+        expect(designPatternSpan.classList.contains('property-bag-div')).toBeTruthy();
+        expect(designPatternSpan.children).toHaveLength(2);
 
-        const contentElement = designPatternSpan.childAt(1).childAt(0);
-        expect(contentElement.exists()).toBeTruthy();
-        expect(contentElement.is(tag)).toBeTruthy();
+        const contentElement = designPatternSpan.children[1].children[0];
+        expect(contentElement).not.toBeNull();
+        expect(contentElement.tagName).toBe(tag);
     }
 });
 
