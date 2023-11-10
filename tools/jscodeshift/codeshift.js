f43b11bb7d4c9ffec5e58fe21b9f65896bc9ff97
madalynrose <hello@madalyn.dev>
2023-11-10 13:09:24 -0500

    fix linting errors

--- tools/jscodeshift/codeshift.js
+++ tools/jscodeshift/codeshift.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-const { run: jscodeshift } = require('jscodeshift/src/Runner');
 const path = require('node:path');
+const { run: jscodeshift } = require('jscodeshift/src/Runner');
 
 const transformPath = path.resolve('./tools/jscodeshift/convert-enzyme-to-rtl.js');
 const paths = ['./src/tests/'];
