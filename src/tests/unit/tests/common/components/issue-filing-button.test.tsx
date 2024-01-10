444eb288823acfe8ee7fc760244cc7bdcf2dca14
v-sharmachir <v-sharmachir@microsoft.com>
2024-01-10 11:12:51 +0530

    removed unsed reference

--- src/tests/unit/tests/common/components/issue-filing-button.test.tsx
+++ src/tests/unit/tests/common/components/issue-filing-button.test.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { DefaultButton } from '@fluentui/react';
 import { render } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import {
