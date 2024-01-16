605f847b4d79fc61269ec66a567ce6f21f2ae580
Saanica Ghate <v-sghate@microsoft.com>
2024-01-16 10:02:11 +0530

    Revert "Mocked rule content and unmocked the CardsCollapsibleControl component."
    This reverts commit bad84126754ecf3d6797cb7d9e5f2ecfa7df3ccf.

--- src/tests/unit/tests/common/components/cards/rules-with-instances.test.tsx
+++ src/tests/unit/tests/common/components/cards/rules-with-instances.test.tsx
@@ -15,13 +15,12 @@ import {
     mockReactComponents,
 } from 'tests/unit/mock-helpers/mock-module-helpers';
 import { IMock, Mock } from 'typemoq';
-import { RuleContent } from '../../../../../../common/components/cards/rule-content';
 import { exampleUnifiedRuleResult } from './sample-view-model-data';
 
-jest.mock('../../../../../../common/components/cards/rule-content');
+jest.mock('common/components/cards/collapsible-component-cards');
 
 describe('RulesWithInstances', () => {
-    mockReactComponents([RuleContent]);
+    mockReactComponents([CardsCollapsibleControl]);
     let fixInstructionProcessorMock: IMock<FixInstructionProcessor>;
     let cardSelectionMessageCreatorMock: IMock<CardSelectionMessageCreator>;
 
@@ -52,6 +51,6 @@ describe('RulesWithInstances', () => {
         );
 
         expect(renderResult.asFragment()).toMatchSnapshot();
-        expectMockedComponentPropsToMatchSnapshots([RuleContent]);
+        expectMockedComponentPropsToMatchSnapshots([CardsCollapsibleControl]);
     });
 });
