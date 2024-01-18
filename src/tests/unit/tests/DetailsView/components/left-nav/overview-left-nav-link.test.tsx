1202582b7770a196f8321cf479fcbab98b05e808
v-sharmachir <v-sharmachir@microsoft.com>
2024-01-18 11:22:35 +0530

    added mock for icon in overview-left-nav-link.test.tsx

--- src/tests/unit/tests/DetailsView/components/left-nav/overview-left-nav-link.test.tsx
+++ src/tests/unit/tests/DetailsView/components/left-nav/overview-left-nav-link.test.tsx
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { Icon } from '@fluentui/react';
 import { render } from '@testing-library/react';
 import * as React from 'react';
 
@@ -8,8 +9,13 @@ import {
     BaseLeftNavLinkProps,
 } from '../../../../../../DetailsView/components/base-left-nav';
 import { OverviewLeftNavLink } from '../../../../../../DetailsView/components/left-nav/overview-left-nav-link';
+import { mockReactComponents } from '../../../../mock-helpers/mock-module-helpers';
 
+jest.mock('@fluentui/react', () => ({
+    Icon: jest.fn(),
+}));
 describe('OverviewLeftNavLink', () => {
+    mockReactComponents([Icon]);
     it('renders', () => {
         const props: BaseLeftNavLinkProps = {
             link: {
