942019e6ddfe274c28fa21bcd935bd834b161988
Saanica Ghate <v-sghate@microsoft.com>
2024-02-05 18:37:53 +0530

    Enzyme replacement done.

--- src/tests/unit/tests/views/page/page.test.tsx
+++ src/tests/unit/tests/views/page/page.test.tsx
@@ -1,13 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import * as React from 'react';
 
 import { Page, PageDeps } from 'views/page/page';
+import { NarrowModeDetector } from '../../../../../DetailsView/components/narrow-mode-detector';
+import { mockReactComponents } from '../../../mock-helpers/mock-module-helpers';
 
+jest.mock('../../../../../DetailsView/components/narrow-mode-detector');
 describe('page view', () => {
+    mockReactComponents([NarrowModeDetector]);
     it('renders', () => {
-        const wrapper = shallow(<Page deps={{} as PageDeps}>INSIDE</Page>);
-        expect(wrapper.getElement()).toMatchSnapshot();
+        const renderResult = render(<Page deps={{} as PageDeps}>INSIDE</Page>);
+        expect(renderResult.asFragment()).toMatchSnapshot();
     });
 });
