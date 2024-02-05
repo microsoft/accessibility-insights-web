f95eb450a3e7a3ce4de5b3676d8a15defc649ef0
Saanica Ghate <v-sghate@microsoft.com>
2024-02-05 19:11:41 +0530

    Enzyme replacement done

--- src/tests/unit/tests/views/content/content.test.tsx
+++ src/tests/unit/tests/views/content/content.test.tsx
@@ -1,12 +1,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import * as React from 'react';
 
 import { Content, ContentDeps } from 'views/content/content';
 import { ContentProvider } from 'views/content/content-page';
+import { NarrowModeDetector } from '../../../../../DetailsView/components/narrow-mode-detector';
+import { mockReactComponents } from '../../../mock-helpers/mock-module-helpers';
 
+jest.mock('../../../../../DetailsView/components/narrow-mode-detector');
 describe('content', () => {
+    mockReactComponents([NarrowModeDetector]);
     it('renders', () => {
         const contentFromReference = jest.fn().mockReturnValue('THE-CONTENT');
         const contentProvider = {
@@ -15,9 +19,9 @@ describe('content', () => {
         const deps = { contentProvider } as Partial<ContentDeps> as ContentDeps;
 
         const component = <Content deps={deps} reference="content/path" />;
-        const result = shallow(component);
+        const renderResult = render(component);
 
-        expect(result.debug()).toMatchSnapshot();
+        expect(renderResult.asFragment()).toMatchSnapshot();
         expect(contentFromReference).toBeCalledWith('content/path');
     });
 });
