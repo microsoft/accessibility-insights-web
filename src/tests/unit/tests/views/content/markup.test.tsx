da97cace6ae95fc87b31ab26cc208928817f4e4a
Saanica Ghate <v-sghate@microsoft.com>
2024-02-07 16:51:55 +0530

    Fixed lint check issues.

--- src/tests/unit/tests/views/content/markup.test.tsx
+++ src/tests/unit/tests/views/content/markup.test.tsx
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT Liceimport { userEvent } from '@testing-library/user-event';
+// Licensed under the MIT License
 
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
