409151efa1296108eb7f6cfd42bd0acde18eccb7
Saanica Ghate <v-sghate@microsoft.com>
2024-02-19 17:29:26 +0530

    fixed lint check issue

--- src/tests/unit/tests/views/content/content-link.test.tsx
+++ src/tests/unit/tests/views/content/content-link.test.tsx
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { Icon } from '@fluentui/react';
 import { render } from '@testing-library/react';
 import { NewTabLink } from 'common/components/new-tab-link';
 import { NewTabLinkWithTooltip } from 'common/components/new-tab-link-with-tooltip';
@@ -12,7 +13,6 @@ import {
 import { ContentLink } from 'views/content/content-link';
 import { ContentPage } from 'views/content/content-page';
 import { ContentActionMessageCreator } from '../../../../../common/message-creators/content-action-message-creator';
-import { Icon } from '@fluentui/react';
 
 jest.mock('common/components/new-tab-link-with-tooltip');
 jest.mock('common/components/new-tab-link');
