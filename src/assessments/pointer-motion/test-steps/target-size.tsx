61264fe5a9b223127001dbb4fc5d0a048385e36f
madalynrose <hello@madalyn.dev>
2024-04-30 11:58:13 -0400

    use any checks to display messages in instances table

--- src/assessments/pointer-motion/test-steps/target-size.tsx
+++ src/assessments/pointer-motion/test-steps/target-size.tsx
@@ -3,10 +3,23 @@
 import { link } from 'content/link';
 import * as content from 'content/test/pointer-motion/target-size';
 import * as React from 'react';
+import { PropertyBagColumnRendererConfig } from '../../../common/types/property-bag/property-bag-column-renderer-config';
 import { ManualTestRecordYourResults } from '../../common/manual-test-record-your-results';
 import * as Markup from '../../markup';
 import { Requirement } from '../../types/requirement';
 import { PointerMotionTestStep } from './test-steps';
+import { TargetSizePropertyBag } from 'common/types/property-bag/target-size-property-bag';
+import { PropertyBagColumnRendererFactory } from 'assessments/common/property-bag-column-renderer-factory';
+import { ReportInstanceField } from 'assessments/types/report-instance-field';
+import { AnalyzerProvider } from 'injected/analyzers/analyzer-provider';
+import { AnalyzerConfigurationFactory } from 'assessments/common/analyzer-configuration-factory';
+import { AnalyzerConfiguration } from 'injected/analyzers/analyzer';
+import { ScannerUtils } from 'injected/scanner-utils';
+import { AssessmentVisualizationEnabledToggle } from 'DetailsView/components/assessment-visualization-enabled-toggle';
+import { DecoratedAxeNodeResult } from 'common/types/store-data/visualization-scan-result-data';
+import { isEmpty } from 'lodash';
+import { ChecksType } from 'background/assessment-data-converter';
+import { getTargetSizeMessage } from 'scanner/target-size-utils';
 
 const description: JSX.Element = (
     <span>
@@ -73,12 +86,78 @@ const howToTest: JSX.Element = (
     </div>
 );
 
+const displayPropertyBagConfig: PropertyBagColumnRendererConfig<TargetSizePropertyBag>[] = [
+    {
+        propertyName: 'sizeMessage',
+        displayName: 'Size',
+        defaultValue: null,
+    },
+    {
+        propertyName: 'offsetMessage',
+        displayName: 'Offset',
+        defaultValue: null,
+    },
+];
+
+const generateTargetSizePropertyBagFrom = (
+    ruleResult: DecoratedAxeNodeResult,
+    checkName: ChecksType,
+): TargetSizePropertyBag => {
+    if (
+        ruleResult[checkName] &&
+        !isEmpty(ruleResult[checkName]) &&
+        ruleResult[checkName].some(r => r.data)
+    ) {
+        const status =
+            ruleResult.status === true
+                ? 'pass'
+                : ruleResult.status === false
+                  ? 'fail'
+                  : 'incomplete';
+        const data = Object.assign(
+            {},
+            ...ruleResult[checkName].map(r => {
+                console.log(r);
+                return {
+                    ...r.data,
+                    [`${r.id.split('-')[1]}Message`]: getTargetSizeMessage(r.id, status, r.data),
+                };
+            }),
+        );
+        return data;
+    }
+    return null;
+};
+
 export const TargetSize: Requirement = {
     key: PointerMotionTestStep.targetSize,
     name: 'Target size',
     description,
     howToTest,
     ...content,
-    isManual: true,
+    isManual: false,
+    columnsConfig: [
+        {
+            key: 'touch-target-info',
+            name: 'Touch target info',
+            onRender:
+                PropertyBagColumnRendererFactory.getRenderer<TargetSizePropertyBag>(
+                    displayPropertyBagConfig,
+                ),
+        },
+    ],
+    reportInstanceFields: ReportInstanceField.fromColumns(displayPropertyBagConfig),
+    getAnalyzer: (provider: AnalyzerProvider, analyzerConfig: AnalyzerConfiguration) =>
+        provider.createRuleAnalyzer(
+            AnalyzerConfigurationFactory.forScanner({
+                rules: ['target-size'],
+                resultProcessor: (scanner: ScannerUtils) => scanner.getAllApplicableInstances,
+                ...analyzerConfig,
+            }),
+        ),
     guidanceLinks: [link.WCAG_2_5_8],
+    getDrawer: provider => provider.createHighlightBoxDrawer(),
+    getVisualHelperToggle: props => <AssessmentVisualizationEnabledToggle {...props} />,
+    generatePropertyBagFrom: generateTargetSizePropertyBagFrom,
+    // getCompletedRequirementDetailsForTelemetry: labelInNameGetCompletedRequirementDetails,
 };
