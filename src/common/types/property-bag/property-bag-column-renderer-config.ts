a63bf6617720d9876fe9e4b6f9f5eed111aecd00
madalynrose <hello@madalyn.dev>
2024-05-09 16:42:28 -0400

    undo unnecessary changes

--- src/common/types/property-bag/property-bag-column-renderer-config.ts
+++ src/common/types/property-bag/property-bag-column-renderer-config.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { TargetSizePropertyBag } from 'common/types/property-bag/target-size-property-bag';
 import { ColumnValue, ColumnValueBag } from './column-value-bag';
-import * as React from 'react';
 
 export const NoValue = '(no value)';
 export interface PropertyBagColumnRendererConfig<TPropertyBag extends ColumnValueBag> {
@@ -10,7 +8,4 @@ export interface PropertyBagColumnRendererConfig<TPropertyBag extends ColumnValu
     displayName: string;
     defaultValue?: ColumnValue;
     expand?: boolean;
-    neededPropertyBagValues: (keyof TPropertyBag & string)[];
-    compute: (propertyBag: TargetSizePropertyBag) => typeof React.Component;
-    render?: (element: JSX.Element) => JSX.Element;
 }
