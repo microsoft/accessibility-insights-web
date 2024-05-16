b51e830739e3599a05a03ce53139ac1582e1795c
JGibson2019 <jacqueline.gibson@microsoft.com>
2024-05-16 11:41:55 -0500

    added how to check guidance for needs review rule

--- src/common/components/cards/rich-resolution-content.tsx
+++ src/common/components/cards/rich-resolution-content.tsx
@@ -108,6 +108,16 @@ export const RichResolutionContent = NamedFC<RichResolutionContentProps>(
                     </span>
                 );
             }
+            case 'web/target-size': {
+                return (
+                    <div>
+                        Targets must be at least 24 by 24 CSS pixels in size or a target must have a 
+                        24 by 24 CSS pixel spacing circle that does not intersect with another 
+                        target of any size. Check all targets and be sure they have sufficient size 
+                        or spacing from another target.
+                    </div>
+                );
+            }
             default: {
                 throw new Error(
                     `Cannot render RichResolutionContent with unrecognized contentId ${contentId}`,
