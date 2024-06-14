2dfc282457447612e1c60f126171bbae820b2d72
Saanica Ghate <v-sghate@microsoft.com>
2024-06-14 16:52:25 +0530

    Migrated Action Button from v8 to v9 for dropdown

--- src/common/components/cards/expand-collapse-all-button.tsx
+++ src/common/components/cards/expand-collapse-all-button.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { ActionButton } from '@fluentui/react';
+import { Button } from '@fluentui/react-components';
+import { ChevronDownRegular, ChevronRightRegular } from '@fluentui/react-icons';
 import { CardSelectionMessageCreator } from 'common/message-creators/card-selection-message-creator';
 import { NamedFC } from 'common/react/named-fc';
 import * as React from 'react';
@@ -18,26 +19,27 @@ export const ExpandCollapseAllButton = NamedFC<ExpandCollapseAllButtonProps>(
 
         let expandCollapseAllButtonHandler = cardSelectionMessageCreator.collapseAllRules;
         let buttonText = 'Collapse all';
-        let iconName = 'ChevronDown';
+        let iconName = <ChevronDownRegular className={styles.chevronDownRegular} />;
         let ariaLabel: string | undefined = undefined;
 
         if (allCardsCollapsed) {
             expandCollapseAllButtonHandler = cardSelectionMessageCreator.expandAllRules;
             buttonText = 'Expand all';
-            iconName = 'ChevronRight';
+            iconName = <ChevronRightRegular className={styles.chevronRightRegular} />;
             ariaLabel = 'Expand all rules to show failed instances.';
         }
 
         return (
-            <ActionButton
-                iconProps={{ iconName }}
+            <Button
+                appearance="transparent"
+                icon={iconName}
                 onClick={expandCollapseAllButtonHandler}
                 aria-expanded={!allCardsCollapsed}
                 aria-label={ariaLabel}
                 className={styles.expandCollapseAllButton}
             >
                 {buttonText}
-            </ActionButton>
+            </Button>
         );
     },
 );
