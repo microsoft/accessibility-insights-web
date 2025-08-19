0b2a9a57159d4a3c986c3eda05d9e30b31b3da63
Deepak T N (TATA CONSULTANCY SERVICES LTD) <v-deepaktn@microsoft.com>
2025-08-19 19:24:09 +0530

    removed unnecessary unit tests and bumped the package version

--- src/tests/unit/tests/reports/components/report-sections/report-section-factory.test.tsx
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-import { ResultSectionTypes } from 'reports/components/report-sections/report-section-factory';
-
-test('ResultSectionTypes includes incomplete', () => {
-    expect(ResultSectionTypes.incomplete).toBe('IncompleteChecksSection');
-});
