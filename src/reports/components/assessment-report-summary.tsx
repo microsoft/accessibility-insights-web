14184620a83983f26e6a59fa172bbedaf9f9ca1e
Soham Mondal (TATA CONSULTANCY SERVICES LTD) <v-sohmondal@microsoft.com>
2026-04-29 18:12:27 +0530

    feat: Enhance assessment report summary with outcome legend and icons - Bug - 2337263

--- src/reports/components/assessment-report-summary.tsx
+++ src/reports/components/assessment-report-summary.tsx
@@ -4,7 +4,9 @@ import * as React from 'react';
 import styles from 'reports/components/assessment-report-summary.scss';
 import { OverviewSummaryReportModel } from '../assessment-report-model';
 import { AssessmentSummaryDetails } from './assessment-summary-details';
+import { OutcomeIcon } from './outcome-icon';
 import { OutcomeSummaryBar } from './outcome-summary-bar';
+import { outcomeTypeSemantics } from './outcome-type';
 import { allRequirementOutcomeTypes } from './requirement-outcome-type';
 
 export interface AssessmentReportSummaryProps {
@@ -20,8 +22,22 @@ export class AssessmentReportSummary extends React.Component<AssessmentReportSum
                     outcomeStats={this.props.summary.byPercentage}
                     countSuffix="%"
                     allOutcomeTypes={allRequirementOutcomeTypes}
+                    textLabel={true}
                 />
                 <h3 className={styles.testDetailsText}>Test details</h3>
+                <div className={styles.outcomeLegend}>
+                    {allRequirementOutcomeTypes.map(outcomeType => (
+                        <span key={outcomeType} className={styles.outcomeLegendItem}>
+                            <span
+                                className={`outcome-icon outcome-icon-${outcomeType}`}
+                                aria-hidden="true"
+                            >
+                                <OutcomeIcon outcomeType={outcomeType} />
+                            </span>
+                            <span>{outcomeTypeSemantics[outcomeType].pastTense}</span>
+                        </span>
+                    ))}
+                </div>
                 {this.renderDetails()}
             </div>
         );
