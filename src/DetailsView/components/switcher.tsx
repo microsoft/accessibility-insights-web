b9a23e3cdaa7c1df450a4aabe5450266da8cf338
Sai Prasad Reddy (TATA CONSULTANCY SERVICES LTD) <v-chekreddy@microsoft.com>
2026-05-08 17:36:52 +0530

    Visual label is missing for "Select activity" combo box: bug

--- src/DetailsView/components/switcher.tsx
+++ src/DetailsView/components/switcher.tsx
@@ -88,6 +88,7 @@ export class Switcher extends React.Component<SwitcherProps, SwitcherState> {
             <div className={styles.leftNavSwitcher} role="region" aria-label="activity">
                 <Dropdown
                     className={styles.leftNavSwitcherDropdown}
+                    label="Select activity"
                     ariaLabel="select activity"
                     responsiveMode={ResponsiveMode.large}
                     selectedKey={this.state.selectedKey}
