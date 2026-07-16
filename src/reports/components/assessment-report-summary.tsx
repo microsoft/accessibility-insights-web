635498b3e45998d1360763e6fbdde06c0ee9cf5b
Soham Mondal (TATA CONSULTANCY SERVICES LTD) <v-sohmondal@microsoft.com>
2026-07-16 11:42:20 +0530

    Fix Formatting Issue

--- src/reports/components/assessment-report-summary.tsx
+++ src/reports/components/assessment-report-summary.tsx
@@ -25,7 +25,11 @@ export class AssessmentReportSummary extends React.Component<AssessmentReportSum
                     textLabel={true}
                 />
                 <h3 className={styles.testDetailsText}>Test details</h3>
-                <div className={`${styles.outcomeLegend} outcome-icon-set`} role="group" aria-label="Outcome legend">
+                <div
+                    className={`${styles.outcomeLegend} outcome-icon-set`}
+                    role="group"
+                    aria-label="Outcome legend"
+                >
                     {allRequirementOutcomeTypes.map(outcomeType => (
                         <span key={outcomeType} className={styles.outcomeLegendItem}>
                             <span
